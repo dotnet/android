43fd738e622c02de37b23b9a24524a76bf6ca4ef
Jonathan Pryor <jonpryor@vt.edu>
2014-01-02 22:06:33 -0500

    [Java.Interop] Remove JniLocalReference log messages.

--- external/Java.Interop/src/Java.Interop/Java.Interop/JniLocalReference.cs
+++ external/Java.Interop/src/Java.Interop/Java.Interop/JniLocalReference.cs
@@ -8,12 +8,10 @@ namespace Java.Interop
 	public class JniLocalReference : JniReferenceSafeHandle {
 		JniLocalReference ()
 		{
-			Debug.WriteLine ("# JniLocalReference..ctor: handle=0x{0}", handle.ToString ("x"));
 		}
 
 		protected override bool ReleaseHandle ()
 		{
-			Debug.WriteLine ("# JniLocalReference.ReleaseHandle: handle=0x{0}", handle.ToString ("x"));
 			JniHandles._DeleteLocalRef (handle);
 			return true;
 		}
