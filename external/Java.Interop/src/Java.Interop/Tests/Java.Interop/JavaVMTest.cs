8d270fe8ea0a9f47ab7db5e3ea3c022d78895f8e
Jonathan Pryor <jonpryor@vt.edu>
2014-01-05 14:32:17 -0500

    [Java.Interop-Tests] Add more JavaVM tests.

--- external/Java.Interop/src/Java.Interop/Tests/Java.Interop/JavaVMTest.cs
+++ external/Java.Interop/src/Java.Interop/Tests/Java.Interop/JavaVMTest.cs
@@ -33,6 +33,25 @@ namespace Java.InteropTests
 				Assert.Fail ("Expected NotSupportedException; got: {0}", e);
 			}
 		}
+
+		[Test, ExpectedException (typeof (ArgumentNullException))]
+		public void CreateJavaVMWithNullBuilder ()
+		{
+			new JavaVMWithNullBuilder ();
+		}
+
+		class JavaVMWithNullBuilder : JavaVM {
+			public JavaVMWithNullBuilder ()
+				: base ((JavaVMBuilder) null)
+			{
+			}
+		}
+
+		[Test]
+		public void FromHandle_ExistingInstance ()
+		{
+			Assert.AreEqual (JavaVM.Current, JavaVM.FromHandle (JavaVM.Current.SafeHandle));
+		}
 	}
 }
 
