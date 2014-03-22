8433507e323a942f6388cf4775b24f19b8f124d6
Jonathan Pryor <jonpryor@vt.edu>
2014-03-21 22:03:33 -0400

    [Java.Interop] Add JavaProxyThrowable.java.
    Oops; this should have been in e04930a9.

---
+++ external/Java.Interop/src/Java.Interop/java/com/xamarin/android/internal/JavaProxyThrowable.java
@@ -0,0 +1,12 @@
+package com.xamarin.android.internal;
+
+/* package */ class JavaProxyThrowable extends java.lang.Throwable {
+
+	public JavaProxyThrowable () {
+	}
+
+	public JavaProxyThrowable (String message) {
+		super (message);
+	}
+}
+
