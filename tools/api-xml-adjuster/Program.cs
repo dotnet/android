a549be340488fd86df8646badfd5eb4e222c7a52
Jonathan Pryor <jonpryor@vt.edu>
2016-04-24 18:58:57 -0400

    [api-xml-adjuster] Import from monodroid/301e7238
    api-xml-adjuster is a tool to update API XML descriptions.

---
+++ tools/api-xml-adjuster/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Xamarin.Android.Tools.ApiXmlAdjuster
+{
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			var inputXmlFile    = args [0];
+			var outputXmlFile   = args [1];
+
+			var api = new JavaApi ();
+			api.Load (inputXmlFile);
+			api.Resolve ();
+			api.CreateGenericInheritanceMapping ();
+			api.MarkOverrides ();
+			api.FindDefects ();
+			api.Save (outputXmlFile);
+		}
+	}
+}
