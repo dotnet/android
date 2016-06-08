dotnet/android

commit 4ec06ac92ccad2a4e7598a44b999c35645096234

[Xamarin.Android.Build.Tasks] Remove the usage of Ionic.Zip from the Build System (#46)
There were issues with the Ionic.Zip Nuget package that was being
used in the Build Tasks project. The main one being it did not
seem to handle Mac/Linux '/' path seperators (see commit 4c816684).

This commit reworks ALL the Ionic.Zip code to make use of
System.IO.Compression instead. A new ZipArchiveExtensions class
has been introduced which mimics the Ionic.Zip Api to some extent
to try to keep the code changes to a minimum.

src/Xamarin.Android.Build.Tasks/Tasks/CreateNativeLibraryArchive.cs
@@ -8,7 +8,7 @@ using System.Xml.Linq;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using System.Text.RegularExpressions;
-using Ionic.Zip;
+using System.IO.Compression;
 
 using Xamarin.Android.Tools;
 
@@ -61,13 +61,19 @@ namespace Xamarin.Android.Tasks
 					outDirInfo.CreateSubdirectory (abi);
 				MonoAndroidHelper.CopyIfChanged (lib.ItemSpec, Path.Combine (OutputDirectory, abi, Path.GetFileName (lib.ItemSpec)));
 			}
-				
+
 			// Archive native libraries in a zip.
-			var zip = new ZipFile (new System.Text.UTF8Encoding (false));
-			zip.AddDirectory (OutputDirectory, outDirInfo.Name);
-			string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidNativeLibraries__.zip");
-			if (Files.ArchiveZip (outpath, f => zip.Save (f)))
-				Log.LogDebugMessage ("Saving contents to " + outpath);
+			using (var stream = new MemoryStream ())
+			using (var zip = new ZipArchive (stream, ZipArchiveMode.Create, true, new System.Text.UTF8Encoding (false))) {
+				zip.AddDirectory (OutputDirectory, outDirInfo.Name);
+				string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidNativeLibraries__.zip");
+				if (Files.ArchiveZip (outpath, f => {
+					using (var fs = new FileStream (f, FileMode.CreateNew)) {
+						stream.CopyTo (fs);
+					}
+				}))
+					Log.LogDebugMessage ("Saving contents to " + outpath);
+			}
 			
 			return true;
 		}

src/Xamarin.Android.Build.Tasks/Tasks/BuildApk.cs
@@ -8,8 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
-using Ionic.Zlib;
-using Ionic.Zip;
+using System.IO.Compression;
 
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
@@ -110,8 +109,9 @@ namespace Xamarin.Android.Tasks
 		void ExecuteWithAbi (string supportedAbis, string apkInputPath, string apkOutputPath)
 		{
 			ArchiveFileList files = new ArchiveFileList ();
-			using (ZipFile apk = apkInputPath != null ? MonoAndroidHelper.ReadZipFile (apkInputPath) : new ZipFile ()) {
-
+			if (apkInputPath != null)
+				File.Copy (apkInputPath, apkOutputPath + "new", overwrite: true);
+			using (var apk = ZipFile.Open (apkOutputPath + "new", apkInputPath != null ? ZipArchiveMode.Update : ZipArchiveMode.Create)) {
 				apk.AddEntry ("NOTICE",
 						Assembly.GetExecutingAssembly ().GetManifestResourceStream ("NOTICE.txt"));
 
@@ -128,7 +128,7 @@ namespace Xamarin.Android.Tasks
 				AddNativeLibrariesFromAssemblies (apk, supportedAbis);
 
 				foreach (ITaskItem typemap in TypeMappings) {
-					apk.AddFile (typemap.ItemSpec, "").CompressionLevel = CompressionLevel.None;
+					apk.AddFile (typemap.ItemSpec, directoryPathInZip: "", compressionLevel: CompressionLevel.NoCompression);
 				}
 
 				foreach (var file in files) {
@@ -156,28 +156,27 @@ namespace Xamarin.Android.Tasks
 				jarFilePaths = MonoAndroidHelper.DistinctFilesByContent (jarFilePaths);
 
 				foreach (var jarFile in jarFilePaths) {
-					using (var jar = MonoAndroidHelper.ReadZipFile (jarFile)) {
-						foreach (var jarItem in jar.Where (ze => !ze.IsDirectory && !ze.FileName.StartsWith ("META-INF") && !ze.FileName.EndsWith (".class") && !ze.FileName.EndsWith (".java") && !ze.FileName.EndsWith ("MANIFEST.MF"))) {
+					using (var jar = new ZipArchive (File.Open (jarFile, FileMode.Open), ZipArchiveMode.Read)) {
+						foreach (var jarItem in jar.Entries.Where (ze => !ze.IsDirectory () && !ze.FullName.StartsWith ("META-INF") && !ze.FullName.EndsWith (".class") && !ze.FullName.EndsWith (".java") && !ze.FullName.EndsWith ("MANIFEST.MF"))) {
 							byte[] data;
 							using (var d = new System.IO.MemoryStream ())
-							using (var i = jarItem.OpenReader ()) {
+							using (var i = jarItem.Open ()) {
 								i.CopyTo (d);
 								data = d.ToArray ();
 							}
-							if (apk.Entries.Any (e => e.FileName == jarItem.FileName))
-								Log.LogMessage ("Warning: failed to add jar entry {0} from {1}: the same file already exists in the apk", jarItem.FileName, Path.GetFileName (jarFile));
+							if (apk.Entries.Any (e => e.FullName == jarItem.FullName))
+								Log.LogMessage ("Warning: failed to add jar entry {0} from {1}: the same file already exists in the apk", jarItem.FullName, Path.GetFileName (jarFile));
 							else
-								apk.AddEntry (jarItem.FileName, data);
+								apk.AddEntry (jarItem.FullName, data);
 						}
 					}
 				}
 				if (StubApplicationDataFile != null && File.Exists (StubApplicationDataFile))
 					AddZipEntry (apk, StubApplicationDataFile, string.Empty);
-				
-				apk.Save (apkOutputPath + "new");
-				MonoAndroidHelper.CopyIfZipChanged (apkOutputPath + "new", apkOutputPath);
-				File.Delete (apkOutputPath + "new");
 			}
+			MonoAndroidHelper.CopyIfZipChanged (apkOutputPath + "new", apkOutputPath);
+			//File.Delete (apkOutputPath + "new");
+			File.Move (apkOutputPath + "new", apkOutputPath + DateTime.Now.ToShortTimeString ());
 
 		}
 
@@ -246,19 +245,19 @@ namespace Xamarin.Android.Tasks
 			return !Log.HasLoggedErrors;
 		}
 
-		void AddZipEntry (ZipFile apk, string file, string path)
+		void AddZipEntry (ZipArchive apk, string file, string path)
 		{
 			apk.AddFile (file, path);
 		}
 
-		private void AddAssemblies (ZipFile apk)
+		private void AddAssemblies (ZipArchive apk)
 		{
 			bool debug = _Debug;
 			bool use_shared_runtime = String.Equals (UseSharedRuntime, "true", StringComparison.OrdinalIgnoreCase);
 
 			foreach (ITaskItem assembly in ResolvedUserAssemblies) {
 				// Add assembly
-				apk.AddFile (assembly.ItemSpec, GetTargetDirectory (assembly.ItemSpec)).CompressionLevel = CompressionLevel.None;
+				apk.AddFile (assembly.ItemSpec, GetTargetDirectory (assembly.ItemSpec), compressionLevel: CompressionLevel.NoCompression);
 
 				// Try to add config if exists
 				var config = Path.ChangeExtension (assembly.ItemSpec, "dll.config");
@@ -269,7 +268,7 @@ namespace Xamarin.Android.Tasks
 					var symbols = Path.ChangeExtension (assembly.ItemSpec, "dll.mdb");
 
 					if (File.Exists (symbols))
-						apk.AddFile (symbols, "assemblies").CompressionLevel = CompressionLevel.None;
+						apk.AddFile (symbols, "assemblies", compressionLevel: CompressionLevel.NoCompression);
 				}
 			}
 
@@ -278,7 +277,7 @@ namespace Xamarin.Android.Tasks
 
 			// Add framework assemblies
 			foreach (ITaskItem assembly in ResolvedFrameworkAssemblies) {
-				apk.AddFile (assembly.ItemSpec, "assemblies").CompressionLevel = CompressionLevel.None;
+				apk.AddFile (assembly.ItemSpec, "assemblies", compressionLevel: CompressionLevel.NoCompression);
 				var config = Path.ChangeExtension (assembly.ItemSpec, "dll.config");
 				AddAssemblyConfigEntry (apk, config);
 				// Try to add symbols if Debug
@@ -286,12 +285,12 @@ namespace Xamarin.Android.Tasks
 					var symbols = Path.ChangeExtension (assembly.ItemSpec, "dll.mdb");
 
 					if (File.Exists (symbols))
-						apk.AddFile (symbols, "assemblies").CompressionLevel = CompressionLevel.None;
+						apk.AddFile (symbols, "assemblies", compressionLevel: CompressionLevel.NoCompression);
 				}
 			}
 		}
 
-		void AddAssemblyConfigEntry (ZipFile apk, string configFile)
+		void AddAssemblyConfigEntry (ZipArchive apk, string configFile)
 		{
 			if (!File.Exists (configFile))
 				return;
@@ -301,7 +300,7 @@ namespace Xamarin.Android.Tasks
 				source.CopyTo (dest);
 				dest.WriteByte (0);
 				dest.Position = 0;
-				apk.AddEntry ("assemblies/" + Path.GetFileName (configFile), dest).CompressionLevel = CompressionLevel.None;
+				apk.AddEntry ("assemblies/" + Path.GetFileName (configFile), dest, compressionLevel: CompressionLevel.NoCompression);
 			}
 		}
 
@@ -314,7 +313,7 @@ namespace Xamarin.Android.Tasks
 			return "assemblies";
 		}
 
-		void AddEnvironment (ZipFile apk)
+		void AddEnvironment (ZipArchive apk)
 		{
 			var environment = new StringWriter () {
 				NewLine = "\n",
@@ -419,7 +418,7 @@ namespace Xamarin.Android.Tasks
 			return results;
 		}
 
-		void AddProfilers (ZipFile apk, string abi)
+		void AddProfilers (ZipArchive apk, string abi)
 		{
 			var root = Path.GetDirectoryName (typeof(BuildApk).Assembly.Location);
 			if (!string.IsNullOrEmpty (AndroidEmbedProfilers)) {
@@ -431,7 +430,7 @@ namespace Xamarin.Android.Tasks
 			}
 		}
 
-		void AddRuntimeLibraries (ZipFile apk, string supportedAbis)
+		void AddRuntimeLibraries (ZipArchive apk, string supportedAbis)
 		{
 			var root = Path.GetDirectoryName (typeof(BuildApk).Assembly.Location);
 			bool use_shared_runtime = String.Equals (UseSharedRuntime, "true", StringComparison.OrdinalIgnoreCase);
@@ -450,7 +449,7 @@ namespace Xamarin.Android.Tasks
 			}
 		}
 
-		void AddNativeLibrariesFromAssemblies (ZipFile apk, string supportedAbis)
+		void AddNativeLibrariesFromAssemblies (ZipArchive apk, string supportedAbis)
 		{
 			var abis = supportedAbis.Split (new char[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
 			var res = new DirectoryAssemblyResolver (Console.WriteLine, loadDebugSymbols: false);
@@ -464,11 +463,11 @@ namespace Xamarin.Android.Tasks
 						continue;
 					var data = ressozip.GetResourceData ();
 					using (var ms = new MemoryStream (data)) {
-						using (var zip = ZipFile.Read (ms)) {
-							foreach (var e in zip.Entries.Where (x => abis.Any (a => x.FileName.Contains (a)))) {
-								if (e.IsDirectory)
+						using (var zip = new ZipArchive (ms, ZipArchiveMode.Read)) {
+							foreach (var e in zip.Entries.Where (x => abis.Any (a => x.FullName.Contains (a)))) {
+								if (e.IsDirectory ())
 									continue;
-								var key = e.FileName.Replace ("native_library_imports", "lib");
+								var key = e.FullName.Replace ("native_library_imports", "lib");
 								if (apk.ContainsEntry (key)) {
 									Log.LogCodedWarning ("4301", "Apk already contains the item {0}; ignoring.", key);
 									continue;
@@ -540,7 +539,7 @@ namespace Xamarin.Android.Tasks
 			files.Add (new Tuple<string, string> (path, string.Format ("lib/{0}", abi)));
 		}
 
-		private void AddGdbservers (ZipFile apk, ArchiveFileList files, string supportedAbis, AndroidDebugServer debugServer)
+		private void AddGdbservers (ZipArchive apk, ArchiveFileList files, string supportedAbis, AndroidDebugServer debugServer)
 		{
 			if (string.IsNullOrEmpty (AndroidNdkDirectory))
 				return;

src/Xamarin.Android.Build.Tasks/Tasks/CreateLibraryResourceArchive.cs
@@ -8,7 +8,7 @@ using System.Xml.Linq;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using System.Text.RegularExpressions;
-using Ionic.Zip;
+using System.IO.Compression;
 
 using Xamarin.Android.Tools;
 
@@ -79,10 +79,8 @@ namespace Xamarin.Android.Tasks
 					string tmpname = Path.Combine (Path.GetTempPath (), "monodroid_import_" + Guid.NewGuid ().ToString ());
 					try {
 						Directory.CreateDirectory (tmpname);
-						using (var lz = new ZipFile (p, new System.Text.UTF8Encoding (false))) {
-							Files.ExtractAll (lz, tmpname, ExtractExistingFileAction.DoNotOverwrite);
-						}
-						
+						ZipFile.ExtractToDirectory (p, tmpname, new System.Text.UTF8Encoding (false));
+
 						if (!CopyLibraryContent (tmpname, p.EndsWith (".aar", StringComparison.OrdinalIgnoreCase)))
 							return false;
 					} finally {
@@ -90,15 +88,20 @@ namespace Xamarin.Android.Tasks
 					}
 				}
 			}
-				
-			// Archive them in a zip.
-			var zip = new ZipFile (new System.Text.UTF8Encoding (false));
-			zip.AddDirectory (OutputDirectory, outDirInfo.Name);
 
-			string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidLibraryProjects__.zip");
-			if (Files.ArchiveZip (outpath, f => zip.Save (f)))
-				Log.LogDebugMessage ("Saving contents to " + outpath);
-			
+			// Archive them in a zip.
+			using (var stream = new MemoryStream ())
+			using (var zip = new ZipArchive (stream, ZipArchiveMode.Create, true, new System.Text.UTF8Encoding (false))) {
+				zip.AddDirectory (OutputDirectory, outDirInfo.Name);
+
+				string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidLibraryProjects__.zip");
+				if (Files.ArchiveZip (outpath, f => {
+					using (var fs = new FileStream (f, FileMode.CreateNew)) {
+						stream.CopyTo (fs);
+					}
+				}))
+					Log.LogDebugMessage ("Saving contents to " + outpath);
+			}
 			return true;
 		}
 			

src/Xamarin.Android.Build.Tasks/Tasks/CreateManagedLibraryResourceArchive.cs
@@ -8,7 +8,7 @@ using System.Xml.Linq;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using System.Text.RegularExpressions;
-using Ionic.Zip;
+using System.IO.Compression;
 
 using Xamarin.Android.Tools;
 
@@ -105,12 +105,18 @@ namespace Xamarin.Android.Tasks
 			}
 
 			// Archive them in a zip.
-			var zip = new ZipFile (new System.Text.UTF8Encoding (false));
-			zip.AddDirectory (OutputDirectory, outDirInfo.Name);
+			using (var stream = new MemoryStream ())
+			using (var zip = new ZipArchive (stream, ZipArchiveMode.Create, true, new System.Text.UTF8Encoding (false))) {
+				zip.AddDirectory (OutputDirectory, outDirInfo.Name);
 
-			string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidLibraryProjects__.zip");
-			if (Files.ArchiveZip (outpath, f => zip.Save (f)))
-				Log.LogDebugMessage ("Saving contents to " + outpath);
+				string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidLibraryProjects__.zip");
+				if (Files.ArchiveZip (outpath, f => {
+					using (var fs = new FileStream (f, FileMode.CreateNew)) {
+						stream.CopyTo (fs);
+					}
+				}))
+					Log.LogDebugMessage ("Saving contents to " + outpath);
+			}
 
 			return true;
 		}

src/Xamarin.Android.Build.Tasks/Tasks/GetAdditionalResourcesFromAssemblies.cs
@@ -163,13 +163,12 @@ namespace Xamarin.Android.Tasks {
 					using (var zip = MonoAndroidHelper.ReadZipFile (file)) {
 						int extracted = 0;
 						var o = Math.Max(1, (zip.Entries.Count / 10));
-						zip.ExtractProgress += (object sender, Ionic.Zip.ExtractProgressEventArgs e) => {
-							if ((e.EntriesExtracted % o) != 0 || extracted == e.EntriesExtracted || e.EntriesExtracted == 0)
+						Files.ExtractAll (zip, contentDir, (progress, total) => {
+							if ((progress % o) != 0 || extracted == progress || progress == 0)
 								return;
-							LogMessage ("Extracted {0} of {1} files", e.EntriesExtracted, e.EntriesTotal);
-							extracted = e.EntriesExtracted;
-						};
-						Files.ExtractAll (zip, contentDir);
+							LogMessage ("Extracted {0} of {1} files", progress, total);
+							extracted = progress;
+						});
 					}
 				}
 				catch (Exception e) {

src/Xamarin.Android.Build.Tasks/Tasks/GetImportedLibraries.cs
@@ -9,7 +9,6 @@ using Mono.Cecil;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using System.Text.RegularExpressions;
-using Ionic.Zip;
 
 namespace Xamarin.Android.Tasks
 {

src/Xamarin.Android.Build.Tasks/Tasks/Proguard.cs
@@ -12,6 +12,7 @@ using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 using System.Text.RegularExpressions;
 using Xamarin.Android.Build.Utilities;
+using System.IO.Compression;
 
 namespace Xamarin.Android.Tasks
 {
@@ -122,11 +123,10 @@ namespace Xamarin.Android.Tasks
 
 			if (File.Exists (ProguardJarInput))
 				File.Delete (ProguardJarInput);
-			var zip = new Ionic.Zip.ZipFile (ProguardJarInput, new System.Text.UTF8Encoding (false));
-			foreach (var file in Directory.GetFiles (classesFullPath, "*", SearchOption.AllDirectories))
-				zip.AddFile (file, Path.GetDirectoryName (file.Substring (classesFullPath.Length)));
-			zip.Save ();
-			zip.Dispose ();
+			using (var zip = ZipFile.Open (ProguardJarInput, ZipArchiveMode.Create, new System.Text.UTF8Encoding (false))) {
+				foreach (var file in Directory.GetFiles (classesFullPath, "*", SearchOption.AllDirectories))
+					zip.AddFile (file, Path.GetDirectoryName (file.Substring (classesFullPath.Length)));
+			}
 
 			var acwLines = File.ReadAllLines (AcwMapFile);
 			using (var appcfg = File.CreateText (ProguardGeneratedApplicationConfiguration))

commit ac6a450f97043a60e454df98f63f036dfd3d0564

[Xamarin.Android.Build.Tasks] Fix Native Library Archive
The commit 4ec06ac broke the Narive Library Archive
creation because the zip was not closed before we
write the stream to the file.

This commit fixes that issue.

src/Xamarin.Android.Build.Tasks/Tasks/CreateNativeLibraryArchive.cs
@@ -63,9 +63,11 @@ namespace Xamarin.Android.Tasks
 			}
 
 			// Archive native libraries in a zip.
-			using (var stream = new MemoryStream ())
-			using (var zip = new ZipArchive (stream, ZipArchiveMode.Create, true, new System.Text.UTF8Encoding (false))) {
-				zip.AddDirectory (OutputDirectory, outDirInfo.Name);
+			using (var stream = new MemoryStream ()) {
+				using (var zip = new ZipArchive (stream, ZipArchiveMode.Create, true, new System.Text.UTF8Encoding (false))) {
+					zip.AddDirectory (OutputDirectory, outDirInfo.Name);
+				}
+				stream.Position = 0;
 				string outpath = Path.Combine (outDirInfo.Parent.FullName, "__AndroidNativeLibraries__.zip");
 				if (Files.ArchiveZip (outpath, f => {
 					using (var fs = new FileStream (f, FileMode.CreateNew)) {