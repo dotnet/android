dotnet/android

commit 29addc7f872655e82ca7a26996f54a54d83ad408

[build] Ensure submodules init'd before using them (#131)
`make jenkins` after a fresh checkout was failing:

	$ make jenkins
	cp Configuration.Java.Interop.Override.props external/Java.Interop/Configuration.Override.props
	./build-tools/scripts/generate-os-info Configuration.OperatingSystem.props
	cp `xbuild /p:Configuration=Debug  /nologo /v:minimal /t:GetMonoSourceFullPath build-tools/scripts/Paths.targets`/mcs/class/msfinal.pub .
	cp: /Volumes/Seagate4TB/work/xamarin-android/external/mono/mcs/class/msfinal.pub: No such file or directory
	make: *** [prepare-props] Error 1

The reason it failed is because `make prepare-props` ran *before*
`git submodule update` ran, meaning the file
`external/mono/mcs/class/msfinal.pub` didn't exist yet.

Move the git submodule initialization logic into a new
`make prepare-external` target, which is run before
`make prepare-props`. This ensures that required files exist before
they are used.

Additionally, fix the `make prepare-external` logic so that if the
`$(JavaInteropSourceDirectory)` MSBuild property is overridden, we run
`nuget restore` from the appropriate directory.

Makefile
@@ -22,10 +22,12 @@ endif
 all:
 	$(MSBUILD)
 
-prepare:: prepare-props
+prepare:: prepare-external prepare-props
+
+prepare-external:
 	git submodule update --init --recursive
 	nuget restore
-	(cd external/Java.Interop && nuget restore)
+	(cd `$(MSBUILD) /nologo /v:minimal /t:GetJavaInteropFullPath build-tools/scripts/Paths.targets` && nuget restore)
 
 prepare-props:
 	cp Configuration.Java.Interop.Override.props external/Java.Interop/Configuration.Override.props

build-tools/scripts/Paths.targets
@@ -13,6 +13,12 @@
         Importance="High"
     />
   </Target>
+  <Target Name="GetJavaInteropFullPath">
+    <Message
+        Text="$(JavaInteropFullPath)"
+        Importance="High"
+    />
+  </Target>
   <Target Name="GetMonoSourceFullPath">
     <Message
         Text="$(MonoSourceFullPath)"

commit dbc104ff034b50b3bdd1aca2062c258070996bb9

[build] Fix `make prepare`
Commit 29addc7f broke `make jenkins`:

	build-tools/scripts/Paths.targets: error :
	.../xamarin-android/build-tools/scripts/Paths.targets:
	Exception has been thrown by the target of an invocation.

The cause was due to the use of `build-tools/scripts/Paths.targets`
from `make prepare-external`, which runs before `make prepare-props`,
and thus before `Configuration.OperatingSystem.props` is created.

Not including `Configuration.OperatingSystem.props` in turn meant that
`$(HostOS)` wasn't set, and thus `$(AndroidMxeInstallPrefix)` wasn't
set, causing the `Path.GetFullPath()` for `$(AndroidMxeFullPath)` to
throw, and nothing worked.

Fix that by altering `$(AndroidMxeFullPath)` to be conditional on
`$(AndroidMxeInstallPrefix)` containing a value, and make importing of
`Configuration.OperatingSystem.props` conditional on the
`$(DoNotLoadOSProperties)` MSBuild property *not* being True.
(By default, it won't be set, and thus
`Configuration.OperatingSystem.props` will be included.)

Configuration.props
@@ -7,11 +7,10 @@
       Project="$(MSBuildThisFileDirectory)Configuration.Override.props"
       Condition="Exists('$(MSBuildThisFileDirectory)Configuration.Override.props')"
   />
-
-  <!--
-      Note: this file *must* be imported *after* Configuration.Override.props
-  -->
-  <Import Project="$(MSBuildThisFileDirectory)Configuration.OperatingSystem.props" />
+  <Import
+      Project="$(MSBuildThisFileDirectory)Configuration.OperatingSystem.props"
+      Condition=" '$(DoNotLoadOSProperties)' != 'True' "
+  />
   <PropertyGroup>
     <HostOS Condition=" '$(HostOS)' == '' And '$(OS)' == 'Windows_NT' ">Windows</HostOS>
     <HostCc Condition=" '$(HostCc)' == '' ">$(HostCc64)</HostCc>
@@ -55,7 +54,7 @@
     <MingwCommandPrefix64>x86_64-w64-mingw32$(_MingwPrefixTail)</MingwCommandPrefix64>
   </PropertyGroup>
   <PropertyGroup>
-    <AndroidMxeFullPath>$([System.IO.Path]::GetFullPath ('$(AndroidMxeInstallPrefix)'))</AndroidMxeFullPath>
+    <AndroidMxeFullPath Condition=" '$(AndroidMxeInstallPrefix)' != '' ">$([System.IO.Path]::GetFullPath ('$(AndroidMxeInstallPrefix)'))</AndroidMxeFullPath>
     <AndroidNdkFullPath>$([System.IO.Path]::GetFullPath ('$(AndroidNdkDirectory)'))</AndroidNdkFullPath>
     <AndroidSdkFullPath>$([System.IO.Path]::GetFullPath ('$(AndroidSdkDirectory)'))</AndroidSdkFullPath>
     <JavaInteropFullPath>$([System.IO.Path]::GetFullPath ('$(JavaInteropSourceDirectory)'))</JavaInteropFullPath>

Makefile
@@ -27,7 +27,7 @@ prepare:: prepare-external prepare-props
 prepare-external:
 	git submodule update --init --recursive
 	nuget restore
-	(cd `$(MSBUILD) /nologo /v:minimal /t:GetJavaInteropFullPath build-tools/scripts/Paths.targets` && nuget restore)
+	(cd `$(MSBUILD) /p:DoNotLoadOSProperties=True /nologo /v:minimal /t:GetJavaInteropFullPath build-tools/scripts/Paths.targets` && nuget restore)
 
 prepare-props:
 	cp Configuration.Java.Interop.Override.props external/Java.Interop/Configuration.Override.props