12f7f95f781b4ee406d2ad73c719f78bd7d5dbc1
Jonathan Pryor <jonpryor@vt.edu>
2016-08-03 12:33:39 -0400

    [bundle] Add `CreateBundle` target.
    Building Mono on Windows is a PITA, and commit 0c073f67 added support
    to cross-compile Mono for Windows so that Mono could be built from
    macOS or Linux.
    
    The unstated goal of that work was to -- eventually -- build the Mono
    runtimes *for Windows* on e.g. macOS, *bundle* those runtimes, and
    then we could update the build process to *download* a pre-built
    bundle of mono runtimes so that we can ~sanely build xamarin-android
    on Windows without building *Mono* on Windows.
    
    Part Two of this cunning plan involves creating the bundle of built
    mono runtimes. This in turn got rather complicated because instead of
    doing the *simple* thing of invoking **zip**(1), I instead wanted to
    create and use a `<Zip/>` MSBuild task, as this would allow us to
    specify in one place -- `mono-runtimes.targets` -- which files should
    be bundled, instead of bundling e.g. everything in `bin/Debug`, which
    would grab considerably more than just the mono bits.
    
    Adding a `<Zip/>` task in turn involves determining which Zip
    implementation to use, and what assembly to put it in; the obvious
    answer is to use `libzip`/`libZipSharp.dll` and place it into
    `Xamarin.Android.Tools.BootstrapTasks.dll` -- which is how we did it
    -- but this answer was tricky, due to a circular dependency problem:
    `libzip.mdproj` depends on `android-runtimes.mdproj` which depends on
    `Xamarin.Android.Tools.BootstrapTasks.dll`. Consequently, adding
    `libzip.mdproj` as a dependency to
    `Xamarin.Android.Tools.BootstrapTasks.dll` *doesn't work*.
    
    (Question: Why use libzip? Why not use System.IO.Compression?
    Answer: Because I'd like to be able to update the
    `<UnzipDirectoryChildren/>` task to use `libzip` instead of
    "shelling out" to **zip**(1) *like an animal*.)
    
    Step 1 is removing the circular dependency. `libzip.mdproj` depends on
    `android-runtimes.mdproj` because of MXE: `android-runtimes.mdproj`
    configures the MXE build environment to generate a Windows
    `libzip.dll` native library, if needed.
    
    The fix: introduce a new `libzip-windows.mdproj` project which
    performs the MXE-based build. This allows `libzip.mdproj` to no longer
    depend on `android-runtimes.mdproj`; `libzip-windows.mdproj` does.
    This also involves "refactoring" `libzip.mdproj` so that the build
    logic can be shared with `libzip-windows.mdproj`.
    
    Additionally, improve `libzip.mdproj` so that the native libraries are
    also set as a `@(Content)` build action. This allows e.g.
    `libzip.3.0.dylib` to be properly copied into `bin/BuildDebug`, as
    that's the `$(OutputPath)` of
    `Xamarin.Android.Tools.BootstrapTasks.csproj`.
    
    Step 2 is adding `libZipSharp.csproj` as a `@(ProjectReference)` to
    `Xamarin.Android.Tools.BootstrapTasks.csproj`. This requires bumping
    `Xamarin.Android.Tools.BootstrapTasks.csproj` to target .NET 4.5.1,
    which allows us to add the new `<Zip/>` task, as well as a new
    `<GitCommit/>` task (so we can determine the abbreviated commit hash
    of the mono checkout, for encoding into the filename).
    
    Step 3 is updating `mono-runtimes.targets` so that the Windows
    cross-compilers are built into `bin/$(Configuration)/lib/mandroid`,
    as this matches what is currently done internally.
    
    Step 4 is adding a new `build-tools/bundle.mdproj` project with a
    `CreateBundle` targtet, which will create the file:
    
    	$(OutputPath)\bundle-$(Configuration)-libzip=$(libzip-hash),llvm=$(llvm-hash),mono=$(mono-hash).zip`.
    
    Uploading the `bundle-*.zip` files will be handled by Jenkins as a
    post-build task.
    
    Not yet done: *using* `bundle-*.zip` so that mono/etc. builds can be
    skipped, *greatly* improving build times.

---
+++ src/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/GitBranch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Xamarin.Tools.Zip;
+
+using IOFile = System.IO.File;
+
+namespace Xamarin.Android.Tools.BootstrapTasks
+{
+	public sealed class GitBranch : Git
+	{
+		[Output]
+		public                  string      Branch              { get; set; }
+
+		protected   override    bool        LogTaskMessages     {
+			get { return false; }
+		}
+
+		public GitBranch ()
+		{
+			// `git rev-parse --abbrev-ref HEAD` currently writes `HEAD` to stderr; no idea why.
+			LogStandardErrorAsError = false;
+		}
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (GitBranch)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (WorkingDirectory)}: {WorkingDirectory.ItemSpec}");
+
+			base.Execute ();
+
+			Log.LogMessage (MessageImportance.Low, $"  [Output] {nameof (Branch)}: {Branch}");
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			return "rev-parse --abbrev-ref HEAD";
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			if (string.IsNullOrEmpty (singleLine))
+				return;
+			Branch  = singleLine;
+		}
+	}
+}
+
---
+++ src/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/GitCommitHash.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Xamarin.Tools.Zip;
+
+using IOFile = System.IO.File;
+
+namespace Xamarin.Android.Tools.BootstrapTasks
+{
+	public sealed class GitCommitHash : Git
+	{
+		[Output]
+		public                  string      AbbreviatedCommitHash       { get; set; }
+
+		protected   override    bool        LogTaskMessages             {
+			get { return false; }
+		}
+
+		public GitCommitHash ()
+		{
+		}
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (GitCommitHash)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (WorkingDirectory)}: {WorkingDirectory.ItemSpec}");
+
+			base.Execute ();
+
+			Log.LogMessage (MessageImportance.Low, $"  [Output] {nameof (AbbreviatedCommitHash)}: {AbbreviatedCommitHash}");
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			return "log --no-color --first-parent -n1 --pretty=format:%h";
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			if (string.IsNullOrEmpty (singleLine))
+				return;
+			AbbreviatedCommitHash   = singleLine;
+		}
+	}
+}
+
