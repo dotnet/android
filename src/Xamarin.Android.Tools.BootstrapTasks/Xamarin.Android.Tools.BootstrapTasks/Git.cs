fbfd676c102c63e4e06e750857b178725e33450c
Jonathan Pryor <jonpryor@vt.edu>
2016-08-10 17:28:53 -0400

    [bundle] Add `CreateBundle` target. (#140)
    Building Mono on Windows is a PITA, and commit 0c073f67 added support
    to cross-compile Mono for Windows so that Mono could be built from
    macOS or Linux.
    
    The unstated goal of that work was to -- eventually -- build the Mono
    runtimes *for Windows* on e.g. macOS, *bundle* those runtimes, and
    then we could update the build process to *download* a pre-built
    bundle of mono runtimes so that we can ~sanely build xamarin-android
    on Windows without building *Mono* on Windows.
    
    Part Two of this cunning plan involves creating the bundle of built
    mono runtimes. This in turn got rather complicated because instead of
    doing the *simple* thing of invoking **zip**(1), I instead wanted to
    create and use a `<Zip/>` MSBuild task, as this would allow us to
    specify in one place -- `mono-runtimes.targets` -- which files should
    be bundled, instead of bundling e.g. everything in `bin/Debug`, which
    would grab considerably more than just the mono bits.
    
    Adding a `<Zip/>` task in turn involves determining which Zip
    implementation to use, and what assembly to put it in; the obvious
    answer is to use `libzip`/`libZipSharp.dll` and place it into
    `Xamarin.Android.Tools.BootstrapTasks.dll` -- which is how we did it
    -- but this answer was tricky, due to a circular dependency problem:
    `libzip.mdproj` depends on `android-runtimes.mdproj` which depends on
    `Xamarin.Android.Tools.BootstrapTasks.dll`. Consequently, adding
    `libzip.mdproj` as a dependency to
    `Xamarin.Android.Tools.BootstrapTasks.dll` *doesn't work*.
    
    (Question: Why use libzip? Why not use System.IO.Compression?
    Answer: Because I'd like to be able to update the
    `<UnzipDirectoryChildren/>` task to use `libzip` instead of
    "shelling out" to **zip**(1) *like an animal*.)
    
    Step 1 is removing the circular dependency. `libzip.mdproj` depends on
    `android-runtimes.mdproj` because of MXE: `android-runtimes.mdproj`
    configures the MXE build environment to generate a Windows
    `libzip.dll` native library, if needed.
    
    The fix: introduce a new `libzip-windows.mdproj` project which
    performs the MXE-based build. This allows `libzip.mdproj` to no longer
    depend on `android-runtimes.mdproj`; `libzip-windows.mdproj` does.
    This also involves "refactoring" `libzip.mdproj` so that the build
    logic can be shared with `libzip-windows.mdproj`.
    
    Additionally, improve `libzip.mdproj` so that the native libraries are
    also set as a `@(Content)` build action. This allows e.g.
    `libzip.3.0.dylib` to be properly copied into `bin/BuildDebug`, as
    that's the `$(OutputPath)` of
    `Xamarin.Android.Tools.BootstrapTasks.csproj`.
    
    Step 2 is adding `libZipSharp.csproj` as a `@(ProjectReference)` to
    `Xamarin.Android.Tools.BootstrapTasks.csproj`. This requires bumping
    `Xamarin.Android.Tools.BootstrapTasks.csproj` to target .NET 4.5.1,
    which allows us to add the new `<Zip/>` task, as well as a new
    `<GitCommit/>` task (so we can determine the abbreviated commit hash
    of the mono checkout, for encoding into the filename).
    
    Step 3 is updating `mono-runtimes.targets` so that the Windows
    cross-compilers are built into `bin/$(Configuration)/lib/mandroid`,
    as this matches what is currently done internally.
    
    Step 4 is adding a new `build-tools/bundle.mdproj` project with a
    `CreateBundle` targtet, which will create the file:
    
    	$(OutputPath)\bundle-$(Configuration)-libzip=$(libzip-hash),llvm=$(llvm-hash),mono=$(mono-hash).zip`.
    
    Uploading the `bundle-*.zip` files will be handled by Jenkins as a
    post-build task.
    
    Not yet done: *using* `bundle-*.zip` so that mono/etc. builds can be
    skipped, *greatly* improving build times.

---
+++ src/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/Git.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Xamarin.Tools.Zip;
+
+using IOFile = System.IO.File;
+
+namespace Xamarin.Android.Tools.BootstrapTasks
+{
+	public class Git : ToolTask
+	{
+		[Required]
+		public                  ITaskItem       WorkingDirectory            { get; set; }
+
+		public                  string          Arguments                   { get; set; }
+
+		[Output]
+		public                  string[]        Output                      { get; set; }
+
+		protected   virtual     bool            LogTaskMessages {
+			get { return true; }
+		}
+
+		protected   override    string          ToolName {
+			get {
+				return "git" + (Path.DirectorySeparatorChar == '\\' ? ".exe" : "");
+			}
+		}
+
+		List<string> lines;
+		List<string> Lines {
+			get { return lines ?? (lines = new List<string> ()); }
+		}
+
+		public override bool Execute ()
+		{
+			if (LogTaskMessages) {
+				Log.LogMessage (MessageImportance.Low, $"Task {nameof (Git)}");
+				Log.LogMessage (MessageImportance.Low, $"  {nameof (WorkingDirectory)}: {WorkingDirectory.ItemSpec}");
+			}
+
+			base.Execute ();
+
+			Output  = Lines?.ToArray ();
+
+			if (LogTaskMessages) {
+				Log.LogMessage (MessageImportance.Low, $"  [Output] {nameof (Output)}:");
+				foreach (var line in (Output ?? new string [0]))
+					Log.LogMessage (MessageImportance.Low, $"    {line}");
+			}
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected override string GenerateFullPathToTool ()
+		{
+			var app     = string.IsNullOrEmpty (ToolExe) ? ToolName: ToolExe;
+			var path    = !string.IsNullOrEmpty (ToolPath) ? ToolPath : GetDirectoryFromPath (app);
+			return Path.Combine (path, app);
+		}
+
+		string GetDirectoryFromPath (string app)
+		{
+			foreach (var p in Environment.GetEnvironmentVariable ("PATH").Split (new [] { Path.PathSeparator.ToString () }, StringSplitOptions.RemoveEmptyEntries)) {
+				if (File.Exists (Path.Combine (p, app))) {
+					return p;
+				}
+			}
+			return null;
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			return Arguments;
+		}
+
+		protected override string GetWorkingDirectory ()
+		{
+			return WorkingDirectory.ItemSpec;
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			Lines.Add (singleLine);
+		}
+	}
+}
+
---
+++ src/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/Zip.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Xamarin.Tools.Zip;
+
+using IOFile        = System.IO.File;
+
+namespace Xamarin.Android.Tools.BootstrapTasks
+{
+	public sealed class Zip : Task
+	{
+		[Required]
+		public  ITaskItem           File            { get; set; }
+
+		public  ITaskItem[]         Entries         { get; set; }
+
+		public  ITaskItem           Prefix          { get; set; }
+
+		public  bool                Overwrite       { get; set; }
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (Zip)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (File)}: {File.ItemSpec}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (Entries)}:");
+			foreach (var p in Entries ?? new ITaskItem [0]) {
+				Log.LogMessage (MessageImportance.Low, $"    {p.ItemSpec}");
+			}
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (Overwrite)}: {Overwrite}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (Prefix)}: {Prefix?.ItemSpec}");
+
+			if (Overwrite) {
+				IOFile.Delete (File.ItemSpec);
+			}
+
+			var prefix  = Prefix == null ? null : Path.GetFullPath (Prefix.ItemSpec);
+			if (!string.IsNullOrEmpty (prefix) && !prefix.EndsWith (Path.DirectorySeparatorChar.ToString (), StringComparison.OrdinalIgnoreCase)) {
+				prefix  += Path.DirectorySeparatorChar;
+			}
+
+			using (var zip  = ZipArchive.Open (File.ItemSpec, FileMode.OpenOrCreate)) {
+				if (Entries == null)
+					return !Log.HasLoggedErrors;
+				foreach (var entry in Entries) {
+					if (!IOFile.Exists (entry.ItemSpec)) {
+						Log.LogWarning ($"Could not add file '{entry.ItemSpec}' to file '{File.ItemSpec}'. Skipping...");
+						continue;
+					}
+					var zipDir      = (string) null;
+					var entryPath   = Path.GetFullPath (entry.ItemSpec);
+					var entryDir    = Path.GetDirectoryName (entryPath);
+					if (prefix != null && entryDir.StartsWith (prefix, StringComparison.OrdinalIgnoreCase)) {
+						zipDir = entryDir.Substring (prefix.Length);
+					}
+					zip.AddFileToDirectory (entryPath, zipDir, useFileDirectory: false);
+				}
+			}
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
+
