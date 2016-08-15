a8f7db057334be3124cabccd2cb2fd7dcf45f458
Atsushi Eno <atsushieno@gmail.com>
2016-08-15 21:42:11 +0900

    [Xamarin.Android.Build.Tasks] Add LayoutAttribute for API-24. (#158)

---
+++ src/Xamarin.Android.Build.Tasks/Mono.Android/LayoutAttribute.Partial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+using Mono.Cecil;
+
+using Java.Interop.Tools.Cecil;
+
+using Xamarin.Android.Manifest;
+
+using Android.Content.PM;
+using Android.Views;
+
+namespace Android.App
+{
+
+	partial class LayoutAttribute
+	{
+		static ManifestDocumentElement<LayoutAttribute> mapping = new ManifestDocumentElement<LayoutAttribute> ("layout") {
+			{
+			  "DefaultWidth",
+			  "defaultWidth",
+			  self          => self.DefaultWidth,
+			  (self, value) => self.DefaultWidth  = (string) value
+			}, {
+			  "DefaultHeight",
+			  "defaultHeight",
+			  self          => self.DefaultHeight,
+			  (self, value) => self.DefaultHeight  = (string) value
+			}, {
+			  "Gravity",
+			  "gravity",
+			  self          => self.Gravity,
+			  (self, value) => self.Gravity = (string) value
+			}, {
+			  "MinHeight",
+			  "minHeight",
+			  self          => self.MinHeight,
+			  (self, value) => self.MinHeight = (string) value
+			}, {
+			  "MinWidth",
+			  "minWidth",
+			  self          => self.MinWidth,
+			  (self, value) => self.MinWidth  = (string) value
+			},
+		};
+
+		TypeDefinition type;
+		ICollection<string> specified;
+
+		public static LayoutAttribute FromTypeDefinition (TypeDefinition type)
+		{
+			CustomAttribute attr = type.GetCustomAttributes ("Android.App.LayoutAttribute")
+				.SingleOrDefault ();
+			if (attr == null)
+				return null;
+			LayoutAttribute self = new LayoutAttribute () {
+				type = type,
+			};
+			self.specified = mapping.Load (self, attr);
+			return self;
+		}
+
+		internal XElement ToElement (IAssemblyResolver resolver, string packageName)
+		{
+			return mapping.ToElement (this, specified, packageName, type, resolver);
+		}
+	}
+}
