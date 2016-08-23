e4b8f2c13a677587735bdf66e9007d54428858b6
mathieubourgeois <mathieu.bourgeois@gameloft.com>
2016-08-23 07:57:07 -0400

    [Xamarin.Android.Build.Utilities] Fixed AndroidSdkBase.ValidateAndroidNdkLocation() to work on old and new ndk versions (ndk-stack.exe or ndk-stack.cmd from r12b onwards) by searching with extension replacing instead of by looking for the specific file. This fixes the case where passing an android ndk repository through a property by command line (such as calling MsBuild.exe /p:AndroidNdkDirectory=<dir>) would not be validated, making Xamarin.Android consider the ndk 64 bits instead of 32 bits. (#179)
    Thanks for the contribution :)

--- src/Xamarin.Android.Build.Utilities/Sdks/AndroidSdkBase.cs
+++ src/Xamarin.Android.Build.Utilities/Sdks/AndroidSdkBase.cs
@@ -119,25 +119,33 @@ namespace Xamarin.Android.Build.Utilities
 		/// </summary>
 		public bool ValidateAndroidNdkLocation (string loc)
 		{
-			return !string.IsNullOrEmpty (loc) && File.Exists (Path.Combine (loc, NdkStack));
+			return !string.IsNullOrEmpty (loc) && FindExecutableInDirectory(NdkStack, loc).Any();
 		}
 
 		protected IEnumerable<string> FindExecutableInPath (string executable)
 		{
 			var path = Environment.GetEnvironmentVariable ("PATH");
 			var pathDirs = path.Split (new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
-			var pathExt = Environment.GetEnvironmentVariable ("PATHEXT");
-			var pathExts = pathExt?.Split (new char [] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
 
 			foreach (var dir in pathDirs) {
-				if (File.Exists (Path.Combine (dir, (executable))))
-					yield return dir;
-				if (pathExts == null)
-					continue;
-				foreach (var ext in pathExts)
-					if (File.Exists (Path.Combine (dir, Path.ChangeExtension (executable, ext))))
-						yield return dir;
+				foreach (var directory in FindExecutableInDirectory(executable, dir)) {
+					yield return directory;
+				}
 			}
+		}
+		
+		protected IEnumerable<string> FindExecutableInDirectory(string executable, string dir)
+		{			
+			var pathExt = Environment.GetEnvironmentVariable ("PATHEXT");
+			var pathExts = pathExt?.Split (new char [] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
+			
+			if (File.Exists (Path.Combine (dir, (executable))))
+				yield return dir;
+			if (pathExts == null)
+				yield break;
+			foreach (var ext in pathExts)
+				if (File.Exists (Path.Combine (dir, Path.ChangeExtension (executable, ext))))
+					yield return dir;
 		}
 
 		protected string NullIfEmpty (string s)
