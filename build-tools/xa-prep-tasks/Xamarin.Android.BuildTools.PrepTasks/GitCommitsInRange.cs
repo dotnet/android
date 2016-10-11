b620689ddf0214b1e38d5feab9e2ea7a653614ff
Jonathan Pryor <jonpryor@vt.edu>
2016-10-11 10:51:19 -0400

    [Mono.Android] Generate AssemblyInformationalVersionAttribute (#259)
    `Mono.Android.dll` is [supposed to contain][0] an
    [`AssemblyInformationalVersionAttribute`][1] `assembly:`-level
    attribute which contains the version number, git commit, and various
    other bits of information related to which commit `Mono.Android.dll`
    "came from."
    
    	var attrs = (System.Reflection.AssemblyInformationalVersionAttribute[])
    		typeof (Java.Lang.Object)
    		.Assembly
    		.GetCustomAttributes(typeof(System.Reflection.AssemblyInformationalVersionAttribute), inherit: true);
    	string version = attrs [0].InformationalVersion;
    	// version is like "7.0.99.113; git-rev-head:d02845e; git-branch:master"
    
    Unfortunately, in the open-source migration this was lost, and the
    above code would throw an `IndexOutOfRangeException`, as
    `attrs.Length` would be 0. (Xamarin.Android 7.0 likewise doesn't
    contain the `AssemblyInformationalVersionAttribute`. Oops!)
    
    Add `<GitBlame/>`, `<GitCommitsInRange/>`, and
    `<ReplaceFileContents/>` tasks, along with various bits of MSBuild-fu,
    to use `src/Mono.Android/Properties/AssemblyInfo.cs.in` as a
    "template" for the `AssemblyInfo.cs` to include in the
    `Mono.Android.dll` build, so that an appropriate
    `AssemblyInformationalVersionAttribute` value is present.
    
    [0]: https://developer.xamarin.com/releases/android/xamarin.android_4/xamarin.android_4.14/#Xamarin.Android_4.14.0
    [1]: https://msdn.microsoft.com/en-us/library/system.reflection.assemblyinformationalversionattribute(v=vs.110).aspx

---
+++ build-tools/xa-prep-tasks/Xamarin.Android.BuildTools.PrepTasks/GitCommitsInRange.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Android.BuildTools.PrepTasks
+{
+	public class GitCommitsInRange : Git
+	{
+		[Output]
+		public                  int         CommitCount     { get; set; }
+
+		[Required]
+		public                  string      StartCommit     { get; set; }
+
+		public                  string      EndCommit       { get; set; }
+
+		protected   override    bool        LogTaskMessages {
+			get { return false; }
+		}
+
+		public GitCommitsInRange ()
+		{
+		}
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (GitCommitsInRange)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (StartCommit)}: {StartCommit}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (EndCommit)}: {EndCommit}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (WorkingDirectory)}: {WorkingDirectory.ItemSpec}");
+
+			base.Execute ();
+
+			Log.LogMessage (MessageImportance.Low, $"  [Output] {nameof (CommitCount)}: {CommitCount}");
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			string endCommit    = string.IsNullOrEmpty (EndCommit)
+				? "HEAD"
+				: EndCommit;
+			return $"log {StartCommit}..{endCommit} --oneline";
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			if (string.IsNullOrEmpty (singleLine))
+				return;
+			CommitCount++;
+		}
+	}
+}
