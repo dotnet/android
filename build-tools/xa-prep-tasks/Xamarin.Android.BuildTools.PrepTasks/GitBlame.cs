a6ba076a090d814d99a6b04f17c340f00f2eb30b
Jonathan Pryor <jonpryor@vt.edu>
2016-10-10 21:01:31 -0400

    [Mono.Android] Generate AssemblyInformationalVersionAttribute
    `Mono.Android.dll` is [supposed to contain][0] an
    [`AssemblyInformationalVersionAttribute`][1] `assembly:`-level
    attribute which contains the version number, git commit, and various
    other bits of information related to which commit `Mono.Android.dll`
    "came from."
    
    	var attrs = (System.Reflection.AssemblyInformationalVersionAttribute[])
    		typeof (Java.Lang.Object)
    		.Assembly
    		.GetCustomAttributes(typeof(System.Reflection.AssemblyInformationalVersionAttribute), inherit: true);
    	string version = attrs [0].InformationalVersion;
    	// version is like "7.0.99.113; git-rev-head:d02845e; git-branch:master"
    
    Unfortunately, in the open-source migration this was lost, and the
    above code would throw an `IndexOutOfRangeException`, as
    `attrs.Length` would be 0. (Xamarin.Android 7.0 likewise doesn't
    contain the `AssemblyInformationalVersionAttribute`. Oops!)
    
    Add `<GitBlame/>`, `<GitCommitsInRange/>`, and
    `<ReplaceFileContents/>` tasks, along with various bits of MSBuild-fu,
    to use `src/Mono.Android/Properties/AssemblyInfo.cs.in` as a
    "template" for the `AssemblyInfo.cs` to include in the
    `Mono.Android.dll` build, so that an appropriate
    `AssemblyInformationalVersionAttribute` value is present.
    
    [0]: https://developer.xamarin.com/releases/android/xamarin.android_4/xamarin.android_4.14/#Xamarin.Android_4.14.0
    [1]: https://msdn.microsoft.com/en-us/library/system.reflection.assemblyinformationalversionattribute(v=vs.110).aspx

---
+++ build-tools/xa-prep-tasks/Xamarin.Android.BuildTools.PrepTasks/GitBlame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Android.BuildTools.PrepTasks
+{
+	public class GitBlame : Git
+	{
+		[Required]
+		public                  ITaskItem       FileName        { get; set; }
+
+		public                  string          LineFilter      { get; set; }
+
+		[Output]
+		public                  ITaskItem[]     Commits         { get; set; }
+
+
+		protected   override    bool            LogTaskMessages {
+			get { return false; }
+		}
+
+		List<ITaskItem>     commits = new List<ITaskItem> ();
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (GitBlame)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (FileName)}: {FileName.ItemSpec}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (LineFilter)}: {LineFilter}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (WorkingDirectory)}: {WorkingDirectory.ItemSpec}");
+
+			base.Execute ();
+
+			Commits     = commits.ToArray ();
+
+			Log.LogMessage (MessageImportance.Low, $"  [Output] {nameof (Commits)}:");
+			foreach (var c in Commits) {
+				Log.LogMessage (MessageImportance.Low, $"  [Output]   {c.GetMetadata ("CommitHash")} '{c.GetMetadata ("Line")}'");
+			}
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			return $"blame \"{Path.GetFullPath (FileName.ItemSpec)}\"";
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			if (string.IsNullOrEmpty (singleLine))
+				return;
+			if (LineFilter != null && !singleLine.Contains (LineFilter))
+				return;
+			var commitHash  = GetCommitHash (singleLine);
+			var commit      = new TaskItem (commitHash);
+			commit.SetMetadata ("CommitHash",   commitHash);
+			commit.SetMetadata ("Line",         singleLine);
+			commits.Add (commit);
+		}
+
+		static string GetCommitHash (string line)
+		{
+			int i   = line?.IndexOf (' ') ?? -1;
+			return i >= 0
+				? line?.Substring (0, i)
+				: line;
+		}
+	}
+}
---
+++ build-tools/xa-prep-tasks/Xamarin.Android.BuildTools.PrepTasks/ReplaceFileContents.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Android.BuildTools.PrepTasks
+{
+	public class ReplaceFileContents : Task
+	{
+		[Required]
+		public  ITaskItem   SourceFile          { get; set; }
+
+		[Required]
+		public  ITaskItem   DestinationFile     { get; set; }
+
+		public  string[]    Replacements        { get; set; }
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (ReplaceFileContents)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (SourceFile)}: {SourceFile.ItemSpec}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (DestinationFile)}: {DestinationFile.ItemSpec}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (Replacements)}:");
+			foreach (var replacement in Replacements) {
+				Log.LogMessage (MessageImportance.Low, $"    {replacement}");
+			}
+
+			File.Delete (DestinationFile.ItemSpec);
+
+			var r   = GetReplacementInfo (Replacements);
+			using (var i    = File.OpenText (SourceFile.ItemSpec))
+			using (var o    = File.CreateText (DestinationFile.ItemSpec)) {
+				string line;
+				while ((line = i.ReadLine ()) != null) {
+					foreach (var e in r) {
+						line = line.Replace (e.Key, e.Value);
+					}
+					o.WriteLine (line);
+				}
+			}
+
+			return !Log.HasLoggedErrors;
+		}
+
+		static  readonly    char[]  Separator   = new [] { '=' };
+
+		static Dictionary<string, string> GetReplacementInfo (string[] replacements)
+		{
+			var r   = new Dictionary<string, string> (replacements?.Length ?? 0);
+			if (replacements == null)
+				return r;
+			foreach (var e in replacements) {
+				if (string.IsNullOrEmpty (e))
+					continue;
+				var kvp = e.Split (Separator, 2, StringSplitOptions.RemoveEmptyEntries);
+				r.Add (kvp [0], kvp.Length > 1 ? kvp [1] : "");
+			}
+			return r;
+		}
+	}
+}
