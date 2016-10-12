25663510be01785f625951da93799ae965ef9049
Atsushi Eno <atsushieno@gmail.com>
2016-10-12 23:53:18 +0900

    [Mono.Android] fix incorrect Tile enum type. (#262)
    The consts should have been mapped to `Android.Service.QuickSettings`,
    not `Android.Service.Quicksettings`.

---
+++ src/Mono.Android/Android.Service.Quicksettings/Android.Service.Quicksettings.TileState.cs
@@ -0,0 +1,8 @@
+namespace Android.Service.Quicksettings {
+  [Obsolete ("It was incorrectly generated and will be removed in the future version")]
+  public enum TileState {
+    Active = 2,
+    Inactive = 1,
+    Unavailable = 0,
+  }
+}
