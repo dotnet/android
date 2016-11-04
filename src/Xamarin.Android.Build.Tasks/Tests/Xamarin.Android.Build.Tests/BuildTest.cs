f62791daed57bd82813829f9a1ec1d2c49e23768
Dean Ellis <dellis1972@googlemail.com>
2016-11-04 14:53:17 +0000

    [Xamarin.Android.Build.Tests] Add Test for AndroidTlsProvider (#281)
    This commit adds a unit test for the AndroidTlsProvider. It
    makes sure that we include/exclude the libmono-btls-shared.so
    depending on the setting provided.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -34,6 +34,43 @@ namespace Xamarin.Android.Build.Tests
 										"Nuget Package Xamarin.Android.Support.v4.21.0.3.0 should have been restored.");
 			}
 		}
+
+		static object [] TlsProviderTestCases =
+		{
+			// androidTlsProvider, isRelease, extpected
+			new object[] { "", true, false, },
+			new object[] { "default", true, false, },
+			new object[] { "legacy", true, false, },
+			new object[] { "btls", true, true, }
+		};
+
+
+		[Test]
+		[TestCaseSource ("TlsProviderTestCases")]
+		public void BuildWithTlsProvider (string androidTlsProvider, bool isRelease, bool expected)
+		{
+			var proj = new XamarinAndroidApplicationProject () {
+				IsRelease = isRelease,
+			};
+			using (var b = CreateApkBuilder (Path.Combine ("temp", $"BuildWithTlsProvider_{androidTlsProvider}_{isRelease}_{expected}"))) {
+				proj.SetProperty ("AndroidTlsProvider", androidTlsProvider);
+				Assert.IsTrue (b.Build (proj), "Build should have succeeded.");
+				var apk = Path.Combine(b.ProjectDirectory,
+					proj.IntermediateOutputPath,"android", "bin", "UnnamedProject.UnnamedProject.apk");
+				using (var zipFile = ZipHelper.OpenZip (apk)) {
+					if (expected) {
+						Assert.IsNotNull (ZipHelper.ReadFileFromZip (zipFile,
+						   "lib/armeabi-v7a/libmono-btls-shared.so"),
+						   "lib/armeabi-v7a/libmono-btls-shared.so should exist in the apk.");
+					}
+					else {
+						Assert.IsNull (ZipHelper.ReadFileFromZip (zipFile,
+						   "lib/armeabi-v7a/libmono-btls-shared.so"),
+						   "lib/armeabi-v7a/libmono-btls-shared.so should not exist in the apk.");
+					}
+				}
+			}
+		}
 	}
 }
 
