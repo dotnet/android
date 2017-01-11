dotnet/android

commit f93149eb4bc456301d7e9b4fb8c9b8228a20cf2f

[Xamarin.Android.Build.Tasks] Asyncify Aapt Task (#352)
We currently use MSBuild Batching to call the aapt tool
when processing the resources. This results in about 12 seperate
calls to aapt when building a standard Xamarin.Forms app.

The problem with Batching is that it is sequential. MSBuild and
xbuild are unable to run those batches in parallel. So this
commit reworks the Aapt task to be an AsyncTask. Which can take
multiple AndroidManifest files at once. It will then spin up
aapt in parallel rather than running them sequentially.

The difference between the two systems is clear

Using Batching:

6156.275 ms  Aapt  10 calls

Using Async:

3207.201 ms  Aapt  3 calls

This is a first build on a vanilla Xamarin.Form app with no changes.

src/Xamarin.Android.Build.Tasks/Tasks/Aapt.cs
@@ -11,10 +11,11 @@ using Microsoft.Build.Framework;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using Xamarin.Android.Build.Utilities;
+using ThreadingTasks = System.Threading.Tasks;
 
 namespace Xamarin.Android.Tasks
 {
-	public class Aapt : ToolTask
+	public class Aapt : AsyncTask
 	{
 		private const string error_regex_string = @"(?<file>.*)\s*:\s*(?<line>\d*)\s*:\s*error:\s*(?<error>.+)";
 		private Regex error_regex = new Regex (error_regex_string, RegexOptions.Compiled);
@@ -28,7 +29,7 @@ namespace Xamarin.Android.Tasks
 		public string AssetDirectory { get; set; }
 
 		[Required]
-		public string ManifestFile { get; set; }
+		public ITaskItem[] ManifestFiles { get; set; }
 
 		[Required]
 		public string ResourceDirectory { get; set; }
@@ -55,7 +56,11 @@ namespace Xamarin.Android.Tasks
 
 		public string ExtraArgs { get; set; }
 
-		protected override string ToolName { get { return OS.IsWindows ? "aapt.exe" : "aapt"; } }
+		protected string ToolName { get { return OS.IsWindows ? "aapt.exe" : "aapt"; } }
+
+		public string ToolPath { get; set; }
+
+		public string ToolExe { get; set; }
 
 		public string ApiLevel { get; set; }
 
@@ -73,7 +78,6 @@ namespace Xamarin.Android.Tasks
 
 		public bool ExplicitCrunch { get; set; }
 
-		string currentAbi;
 		string currentResourceOutputFile;
 		Dictionary<string,string> resource_name_case_map = new Dictionary<string,string> ();
 
@@ -84,10 +88,43 @@ namespace Xamarin.Android.Tasks
 				File.GetLastWriteTime (AndroidComponentResgenFlagFile) > File.GetLastWriteTime (manifestFile);
 		}
 
-		bool ExecuteForAbi (string abi)
+		bool RunAapt (string commandLine)
 		{
-			currentAbi = abi;
-			var ret = base.Execute ();
+			var psi = new ProcessStartInfo () {
+				FileName = GenerateFullPathToTool (),
+				Arguments = commandLine,
+				UseShellExecute = false,
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				CreateNoWindow = true,
+				WindowStyle = ProcessWindowStyle.Hidden,
+			};
+
+			var proc = new Process ();
+			proc.OutputDataReceived += (sender, e) => {
+				LogEventsFromTextOutput (e.Data, MessageImportance.Normal);
+			};
+			proc.ErrorDataReceived += (sender, e) => {
+				LogEventsFromTextOutput (e.Data, MessageImportance.Normal);
+			};
+			proc.StartInfo = psi;
+			proc.Start ();
+			proc.BeginOutputReadLine ();
+			proc.BeginErrorReadLine ();
+			Token.Register (() => {
+				try {
+					proc.Kill ();
+				} catch (Exception) {
+				}
+			});
+			LogDebugMessage ("Executing {0}", commandLine);
+			proc.WaitForExit ();
+			return proc.ExitCode == 0;
+		}
+
+		bool ExecuteForAbi (string cmd)
+		{
+			var ret = RunAapt (cmd);
 			if (ret && !string.IsNullOrEmpty (currentResourceOutputFile)) {
 				var tmpfile = currentResourceOutputFile + ".bk";
 				MonoAndroidHelper.CopyIfZipChanged (tmpfile, currentResourceOutputFile);
@@ -96,11 +133,39 @@ namespace Xamarin.Android.Tasks
 			return ret;
 		}
 
+		int DoExecute (ITaskItem manifestFile, ThreadingTasks.ParallelLoopState state, int loop)
+		{
+			if (!File.Exists (manifestFile.ItemSpec)) {
+				LogDebugMessage ("{0} does not exists. Skipping", manifestFile.ItemSpec);
+				return 0;
+			}
+
+			bool upToDate = ManifestIsUpToDate (manifestFile.ItemSpec);
+
+			if (AdditionalAndroidResourcePaths != null)
+				foreach (var dir in AdditionalAndroidResourcePaths)
+					if (!string.IsNullOrEmpty (dir.ItemSpec))
+						upToDate = upToDate && ManifestIsUpToDate (string.Format ("{0}{1}{2}{3}{4}", dir, Path.DirectorySeparatorChar, "manifest", Path.DirectorySeparatorChar, "AndroidManifest.xml"));
+
+			if (upToDate) {
+				LogMessage ("  Additional Android Resources manifsets files are unchanged. Skipping.");
+				return 0;
+			}
+
+			var defaultAbi = new string [] { null };
+			var abis = SupportedAbis?.Split (new char [] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+			foreach (var abi in (CreatePackagePerAbi && abis?.Length > 1) ? defaultAbi.Concat (abis) : defaultAbi) {
+				ExecuteForAbi (GenerateCommandLineCommands (manifestFile.ItemSpec, abi));
+			}
+
+			return 0;
+		}
+
 		public override bool Execute ()
 		{
 			Log.LogDebugMessage ("Aapt Task");
 			Log.LogDebugMessage ("  AssetDirectory: {0}", AssetDirectory);
-			Log.LogDebugMessage ("  ManifestFile: {0}", ManifestFile);
+			Log.LogDebugTaskItems ("  ManifestFiles: ", ManifestFiles);
 			Log.LogDebugMessage ("  ResourceDirectory: {0}", ResourceDirectory);
 			Log.LogDebugMessage ("  JavaDesignerOutputDirectory: {0}", JavaDesignerOutputDirectory);
 			Log.LogDebugMessage ("  PackageName: {0}", PackageName);
@@ -115,34 +180,18 @@ namespace Xamarin.Android.Tasks
 			if (CreatePackagePerAbi)
 				Log.LogDebugMessage ("  SupportedAbis: {0}", SupportedAbis);
 
-			bool upToDate = ManifestIsUpToDate (ManifestFile);
-
 			if (ResourceNameCaseMap != null)
 				foreach (var arr in ResourceNameCaseMap.Split (';').Select (l => l.Split ('|')).Where (a => a.Length == 2))
 					resource_name_case_map [arr [1]] = arr [0]; // lowercase -> original
 
-			if (AdditionalAndroidResourcePaths != null)
-				foreach (var dir in AdditionalAndroidResourcePaths)
-					if (!string.IsNullOrEmpty (dir.ItemSpec))
-						upToDate = upToDate && ManifestIsUpToDate (string.Format ("{0}{1}{2}", dir, Path.DirectorySeparatorChar, "manifest", Path.DirectorySeparatorChar, "AndroidManifest.xml"));
+			ThreadingTasks.Parallel.ForEach (ManifestFiles, () => 0, DoExecute, (obj) => { Complete (); });
 
-			if (upToDate) {
-				Log.LogMessage (MessageImportance.Normal, "  Additional Android Resources manifsets files are unchanged. Skipping.");
-				return true;
-			}
+			base.Execute ();
 
-			ExecuteForAbi (null);
-
-			if (CreatePackagePerAbi) {
-				var abis = SupportedAbis.Split (new char[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
-				if (abis.Length > 1)
-					foreach (var abi in abis)
-						ExecuteForAbi (abi);
-			}
 			return !Log.HasLoggedErrors;
 		}
 
-		protected override string GenerateCommandLineCommands ()
+		protected string GenerateCommandLineCommands (string ManifestFile, string currentAbi)
 		{
 			// For creating Resource.Designer.cs:
 			//   Running command: C:\Program Files (x86)\Android\android-sdk-windows\platform-tools\aapt
@@ -260,17 +309,19 @@ namespace Xamarin.Android.Tasks
 				return s;
 		}
 
-		protected override string GenerateFullPathToTool ()
+		protected string GenerateFullPathToTool ()
 		{
-			return Path.Combine (ToolPath, ToolExe);
+			return Path.Combine (ToolPath, string.IsNullOrEmpty (ToolExe) ? ToolName : ToolExe);
 		}
 
-		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		protected void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
 		{
 			// Aapt errors looks like this:
 			//   C:\Users\Jonathan\Documents\Visual Studio 2010\Projects\AndroidMSBuildTest\AndroidMSBuildTest\obj\Debug\res\layout\main.axml:7: error: No resource identifier found for attribute 'id2' in package 'android' (TaskId:22)
 			// Look for them and convert them to MSBuild compatible errors.
-			
+			if (string.IsNullOrEmpty (singleLine)) 
+				return;
+
 			var match = error_regex.Match (singleLine);
 
 			if (match.Success) {
@@ -296,11 +347,11 @@ namespace Xamarin.Android.Tasks
 
 			// Handle additional error that doesn't match the regex
 			if (singleLine.Trim ().StartsWith ("invalid resource directory name:")) {
-				Log.LogError ("", "", "", ToolName, -1, -1, -1, -1, "Invalid resource directory name: \"{0}\".", singleLine.Substring (singleLine.LastIndexOfAny (new char[] { '\\', '/' }) + 1));
+				LogError ("", "", "", ToolName, -1, -1, -1, -1, "Invalid resource directory name: \"{0}\".", singleLine.Substring (singleLine.LastIndexOfAny (new char[] { '\\', '/' }) + 1));
 				messageImportance = MessageImportance.High;
 			}
 
-			base.LogEventsFromTextOutput (singleLine, messageImportance);
+			LogMessage (singleLine, messageImportance);
 		}
 	}
 }

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -1187,35 +1187,19 @@ because xbuild doesn't support framework reference assemblies.
   Outputs="$(_AndroidComponentResgenFlagFile)"
   DependsOnTargets="$(_GenerateJavaDesignerForComponentDependsOnTargets)">
 
-<!--
-  This copy is required because the Aapt statement below will be Batched.
-  Aapt will be called for each Path in _AdditonalAndroidResourceCachePaths
-  this is beause of the ManifestFile and Contition properties being set to 
-
-  "%(_AdditonalAndroidResourceCachePaths.Identity)\AndroidManifest.xml"
-
-  the use of .Identify will cause the batch. However we do need ALL of the 
-  resource cache paths to be passed in to correctly resolve resources. If
-  we don't do this only the current "batched" path will be included. 
--->
- <ItemGroup>
-  <_AdditionalAndroidResourceCachePathsCopy Include="@(_AdditonalAndroidResourceCachePaths)"/>
- </ItemGroup>
-
  <!-- Run aapt to generate R.java for additional Android resources-->
  <Aapt
-   Condition="Exists('%(_AdditonalAndroidResourceCachePaths.Identity)\AndroidManifest.xml')"
    ImportsDirectory="$(_LibraryProjectImportsDirectoryName)"
    OutputImportDirectory="$(IntermediateOutputPath)__library_projects__\"
    UseShortFileNames="$(UseShortFileNames)"
-   ManifestFile="%(_AdditonalAndroidResourceCachePaths.Identity)\AndroidManifest.xml"
+   ManifestFiles="@(_AdditonalAndroidResourceCachePaths->'%(Identity)\AndroidManifest.xml')"
    ApplicationName="$(_AndroidPackage)"
    JavaPlatformJarPath="$(JavaPlatformJarPath)"
    NonConstantId="true"
    JavaDesignerOutputDirectory="$(IntermediateOutputPath)android\src"
    ResourceDirectory="$(MonoAndroidResDirIntermediate)"
    AdditionalResourceDirectories="@(LibraryResourceDirectories)"
-   AdditionalAndroidResourcePaths="@(_AdditionalAndroidResourceCachePathsCopy)"
+   AdditionalAndroidResourcePaths="@(_AdditonalAndroidResourceCachePaths)"
    AndroidComponentResgenFlagFile="$(_AndroidComponentResgenFlagFile)"
    ToolPath="$(AaptToolPath)"
    ToolExe="$(AaptToolExe)"
@@ -1274,7 +1258,7 @@ because xbuild doesn't support framework reference assemblies.
 		OutputImportDirectory="$(IntermediateOutputPath)__library_projects__\"
 		UseShortFileNames="$(UseShortFileNames)"
 		JavaPlatformJarPath="$(JavaPlatformJarPath)"
-		ManifestFile="$(ResgenTemporaryDirectory)\AndroidManifest.xml"
+		ManifestFiles="$(ResgenTemporaryDirectory)\AndroidManifest.xml"
 		PackageName="$(_AndroidPackage)"
 		ApplicationName="$(_AndroidPackage)"
 		ResourceDirectory="$(MonoAndroidResDirIntermediate)"
@@ -1787,7 +1771,7 @@ because xbuild doesn't support framework reference assemblies.
 	OutputImportDirectory="$(IntermediateOutputPath)__library_projects__\"
 	UseShortFileNames="$(UseShortFileNames)"
     JavaPlatformJarPath="$(JavaPlatformJarPath)"
-    ManifestFile="$(IntermediateOutputPath)android\AndroidManifest.xml"
+    ManifestFiles="$(IntermediateOutputPath)android\AndroidManifest.xml"
     ResourceDirectory="$(MonoAndroidResDirIntermediate)"
     JavaDesignerOutputDirectory="$(AaptTemporaryDirectory)"
     ResourceOutputFile="$(_PackagedResources)"

commit 278d33886059158294bed55c4639484cb2cd2a87

[Xamarin.Android.Build.Tasks] Fix regression in Aapt Task
Commit f93149eb introduced a regression where the abi specific
packages were NOT being produced. As a result when using
PackagePerAbi the build would fail.

Thie commit fixes that issue by ensuring that a package is
generated per abi when required.

src/Xamarin.Android.Build.Tasks/Tasks/Aapt.cs
@@ -78,7 +78,6 @@ namespace Xamarin.Android.Tasks
 
 		public bool ExplicitCrunch { get; set; }
 
-		string currentResourceOutputFile;
 		Dictionary<string,string> resource_name_case_map = new Dictionary<string,string> ();
 
 		bool ManifestIsUpToDate (string manifestFile)
@@ -122,7 +121,7 @@ namespace Xamarin.Android.Tasks
 			return proc.ExitCode == 0;
 		}
 
-		bool ExecuteForAbi (string cmd)
+		bool ExecuteForAbi (string cmd, string currentResourceOutputFile)
 		{
 			var ret = RunAapt (cmd);
 			if (ret && !string.IsNullOrEmpty (currentResourceOutputFile)) {
@@ -155,7 +154,8 @@ namespace Xamarin.Android.Tasks
 			var defaultAbi = new string [] { null };
 			var abis = SupportedAbis?.Split (new char [] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
 			foreach (var abi in (CreatePackagePerAbi && abis?.Length > 1) ? defaultAbi.Concat (abis) : defaultAbi) {
-				ExecuteForAbi (GenerateCommandLineCommands (manifestFile.ItemSpec, abi));
+				var currentResourceOutputFile = abi != null ? string.Format ("{0}-{1}", ResourceOutputFile, abi) : ResourceOutputFile;
+				ExecuteForAbi (GenerateCommandLineCommands (manifestFile.ItemSpec, abi, currentResourceOutputFile), currentResourceOutputFile);
 			}
 
 			return 0;
@@ -191,7 +191,7 @@ namespace Xamarin.Android.Tasks
 			return !Log.HasLoggedErrors;
 		}
 
-		protected string GenerateCommandLineCommands (string ManifestFile, string currentAbi)
+		protected string GenerateCommandLineCommands (string ManifestFile, string currentAbi, string currentResourceOutputFile)
 		{
 			// For creating Resource.Designer.cs:
 			//   Running command: C:\Program Files (x86)\Android\android-sdk-windows\platform-tools\aapt
@@ -235,7 +235,6 @@ namespace Xamarin.Android.Tasks
 				manifest.SetAbi (currentAbi);
 			manifest.ApplicationName = ApplicationName;
 			manifest.Save (manifestFile);
-			currentResourceOutputFile = currentAbi != null ? string.Format ("{0}-{1}", ResourceOutputFile, currentAbi) : ResourceOutputFile;
 
 			cmd.AppendSwitchIfNotNull ("-M ", manifestFile);
 			Directory.CreateDirectory (JavaDesignerOutputDirectory);

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -1220,6 +1220,7 @@ because xbuild doesn't support framework reference assemblies.
 		_GetAdditionalResourcesFromAssemblies;
 		_CreateAdditionalResourceCache;
 		_GenerateAndroidResourceDir;
+		_DefineBuildTargetAbis;
 	</_UpdateAndroidResgenDependsOnTargets>
 </PropertyGroup>
 
@@ -1274,6 +1275,7 @@ because xbuild doesn't support framework reference assemblies.
 		AndroidUseLatestPlatformSdk="$(AndroidUseLatestPlatformSdk)"
 		ResourceNameCaseMap="$(_AndroidResourceNameCaseMap)"
 		ExplicitCrunch="$(AndroidExplicitCrunch)"
+		SupportedAbis="$(_BuildTargetAbis)"
 	/>
 	
 	<CopyGeneratedJavaResourceClasses