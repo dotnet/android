fdae7c82865961c62ba82ec9736d550c174d3477
Atsushi Eno <atsushieno@gmail.com>
2017-02-10 06:26:20 +0900

    [Xamarin.Android.Build.Tasks] Add @(JavaDocJar) build action (#426)
    Modern Android Java libraries ship with `(library)-javadoc.jar`, which is
    based on how Maven builds documentation from the library sources using
    javadoc (which are now most likely based on Java8 doclet).
    
    Since it is annoying to manually extract the javadocs and specify its
    "index.html" as `@(JavaDocIndex)` build item, it is better to simply have
    a new build action that we can just specify those `-javadoc.jar` files.

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/Unzip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Xamarin.Tools.Zip;
+
+namespace Xamarin.Android.Tasks
+{
+	public class Unzip : Task
+	{
+		public ITaskItem [] Sources { get; set; }
+		public ITaskItem [] DestinationDirectories { get; set; }
+
+		public override bool Execute ()
+		{
+			Log.LogDebugMessage ("Unzip Task");
+			Log.LogDebugTaskItems ("  Sources: ", Sources);
+			Log.LogDebugTaskItems ("  DestinationDirectories: ", DestinationDirectories);
+
+			foreach (var pair in Sources.Zip (DestinationDirectories, (s, d) => new { Source = s, Destination = d })) {
+				if (!Directory.Exists (pair.Destination.ItemSpec))
+					Directory.CreateDirectory (pair.Destination.ItemSpec);
+				using (var z = ZipArchive.Open (pair.Source.ItemSpec, FileMode.Open))
+					z.ExtractAll (pair.Destination.ItemSpec);
+			}
+
+			return true;
+		}
+	}
+}
