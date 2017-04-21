8ce03f48bf93c45c829061686edc4084e9056736
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-04-21 14:57:51 +0200

    Remove Microsoft.Build.BuildEngine using which breaks build with new reference assemblies
    The updated reference assemblies in Mono 5.0 don't depend on Microsoft.Build.Engine.dll anymore which contained the Microsoft.Build.BuildEngine namespace so this using isn't found anymore.
    
    It was unnecessary anyway so remove it.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/ProjectBuilder.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/ProjectBuilder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
 using Microsoft.Build.Framework;
-using Microsoft.Build.BuildEngine;
 
 namespace Xamarin.ProjectTools
 {
