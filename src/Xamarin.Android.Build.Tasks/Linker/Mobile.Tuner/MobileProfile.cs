732d60df5b422ed11c60ba4322bfc73f71c88bca
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-04-21 22:06:08 +0200

    Build netstandard.dll and System.Drawing.Primitives.dll facades (#572)
    Build `netstandard.dll` and `System.Drawing.Primitives.dll` facades
    
    The old project for `System.Drawing.Primitives` was removed in favor
    of building the assembly with the existing logic in Mono.
    
    Also add them to the bundle since we can't rebuild them if we just
    have the bundle.
    
    Update facades list in `MobileProfile.cs`;
    `netstandard.dll` and `System.ValueTuple.dll` were missing.

--- src/Xamarin.Android.Build.Tasks/Linker/Mobile.Tuner/MobileProfile.cs
+++ src/Xamarin.Android.Build.Tasks/Linker/Mobile.Tuner/MobileProfile.cs
@@ -179,6 +179,7 @@ namespace Mobile.Tuner {
 			"System.Threading.ThreadPool",
 			"System.Threading.Timer",
 			"System.Threading",
+			"System.ValueTuple",
 			"System.Xml.ReaderWriter",
 			"System.Xml.XDocument",
 			"System.Xml.XPath.XDocument",
@@ -186,6 +187,7 @@ namespace Mobile.Tuner {
 			"System.Xml.XmlDocument",
 			"System.Xml.XmlSerializer",
 			"System.Xml.Xsl.Primitives",
+			"netstandard",
 		};
 
 		protected override bool IsSdk (string assemblyName)
