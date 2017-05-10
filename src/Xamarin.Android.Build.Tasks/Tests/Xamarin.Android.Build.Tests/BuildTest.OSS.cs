346423eaa8e5111e7cb94c38a07cdc02f1d18793
Dean Ellis <dellis1972@googlemail.com>
2017-05-10 10:00:13 +0100

    [Xamarin.Android.Build.Tests] Migrate most of the remaining unit tests for msbuild.
    We had a bunch of unit tests left over which need to be migrated.
    This commit brings over most the those tests. Excluding Wear tests
    since we don't install the wear api's by default, and tests to do
    with FastDev.
    
    We need a new BuildTest.OSS.cs file because we made the BuildTest
    class partial. This allows us to pass in slightly different options
    to the tests for the OSS repo. Mostly this is to do with the abi's
    which are available. For example abi armeabi is NOT built by
    default in this repo, but armeabi-v7a and x86 are.

---
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.OSS.cs
@@ -0,0 +1,152 @@
+﻿﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Build.Framework;
+using NUnit.Framework;
+using Xamarin.ProjectTools;
+
+namespace Xamarin.Android.Build.Tests
+{
+	public partial class BuildTest : BaseTest
+	{
+#pragma warning disable 414
+		static object [] AotChecks = new object [] {
+			new object[] {
+				/* supportedAbis */   "armeabi-v7a",
+				/* enableLLVM */      false,
+				/* expectedResult */  true,
+			},
+			new object[] {
+				/* supportedAbis */   "armeabi-v7a",
+				/* enableLLVM */      true,
+				/* expectedResult */  true,
+			},
+			new object[] {
+				/* supportedAbis */   "x86",
+				/* enableLLVM */      false,
+				/* expectedResult */  true,
+			},
+			new object[] {
+				/* supportedAbis */   "x86",
+				/* enableLLVM */      true,
+				/* expectedResult */  true,
+			},
+		};
+
+		static object [] ProguardChecks = new object [] {
+			new Object [] {
+				/* isRelease */ true,
+				/* enableProguard */ true,
+				/* useLatestSdk */ true,
+			},
+			new Object [] {
+				/* isRelease */ true,
+				/* enableProguard */ false,
+				/* useLatestSdk */ true,
+			},
+			new Object [] {
+				/* isRelease */ false,
+				/* enableProguard */ true,
+				/* useLatestSdk */ true,
+			},
+			new Object [] {
+				/* isRelease */ false,
+				/* enableProguard */ false,
+				/* useLatestSdk */ true,
+			},
+		};
+		static object [] TakeSimpleFlag = new object [] {
+			new Object [] { false },
+			// Disabled because Jack DOESN'T work
+			// re-enable once it does.
+			//new Object [] { true },
+		};
+		// useJackAndJill, useLatestSdk
+		static object [] JackFlagAndFxVersion = new object [] {
+			new Object [] { false, "v7.1" },
+			// Disabled because Jack DOESN'T work
+			// re-enable once it does.
+			//new Object [] { true, false },
+			//new Object [] { true, true },
+		};
+
+		static object [] SequencePointChecks = new object [] {
+			new object[] {
+				/* isRelease */          false,
+				/* monoSymbolArchive */  false ,
+				/* aotAssemblies */      false,
+				/* debugSymbols */       true,
+				/* debugType */          "Full",
+				/* embedMdb */           true, // because we don't use FastDev in the OSS repo
+				/* expectedRuntime */    "debug",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  false,
+				/* aotAssemblies */      false,
+				/* debugSymbols */       true,
+				/* debugType */          "Full",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  true,
+				/* aotAssemblies */      false,
+				/* debugSymbols */       true,
+				/* debugType */          "Full",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  true ,
+				/* aotAssemblies */      false,
+				/* debugSymbols */       true,
+				/* debugType */          "PdbOnly",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  true ,
+				/* aotAssemblies */      true,
+				/* debugSymbols */       true,
+				/* debugType */          "PdbOnly",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  false ,
+				/* aotAssemblies */      false,
+				/* debugSymbols */       true,
+				/* debugType */          "PdbOnly",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  false ,
+				/* aotAssemblies */      true,
+				/* debugSymbols */       false,
+				/* debugType */          "",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+			new object[] {
+				/* isRelease */          true,
+				/* monoSymbolArchive */  true ,
+				/* aotAssemblies */      true,
+				/* debugSymbols */       false,
+				/* debugType */          "",
+				/* embedMdb */           false,
+				/* expectedRuntime */    "release",
+			},
+		};
+#pragma warning restore 414
+	}
+}
+
