dotnet/android

commit 611bb66ec846cffad14cef6186175a898f37a905

[create-vsix] Allow overriding the created .vsix filename (#577)
We want to use the `make create-vsix` target to create the commercial
`Xamarin.Android.Sdk.vsix` file, but we want it to have a different
filename, matching commit + commit count information from the internal
`monodroid` repo, not the xamarin-android repo.

Uppdate `create-vsix.csproj` to accept a new `$(VsixPath)` property,
which is the path of the .vsix file to create.

Update the default `$(VsixPath)` filename to be
`Xamarin.Android.Sdk-OSS*`, so that if a customer downloads both a
Jenkins OSS build and a commercial .vsix build, it will be easier to
differentiate between them. (The `.vsix` package name information is
*not* changed, thus it is not possible to concurrently have both the
commercial and OSS builds installed in the same VS 2017 SKU.)

Update the `make create-vsix` target so that the output path and other
MSBuild properties can more easily be overridden:

	make create-vsix CONFIGURATIONS=Debug VSIX=`pwd`/MyNewPackage.vsix

The `$(VSIX)` **make**(1) variable sets the `$(VsixPath)` MSBuild
property.

*Note*: the current-working-directory for `make create-vsix` is
`build-tools/create-vsix`; consequently, this command:

	make create-vsix CONFIGURATIONS=Debug VSIX=MyNewPackage.vsix

will create `./build-tools/create-vsix/MyNewPackage.vsix`, *not*
`./MyNewPackage.vsix`.

build-tools/create-vsix/create-vsix.csproj
@@ -7,7 +7,7 @@
     <ProjectGuid>{94756FEB-1F64-411D-A18E-81B5158F776A}</ProjectGuid>
     <OutputType>Library</OutputType>
     <RootNamespace>Xamarin.Android.Sdk</RootNamespace>
-    <AssemblyName>Xamarin.Android.Sdk</AssemblyName>
+    <AssemblyName>Xamarin.Android.Sdk-OSS</AssemblyName>
     <TargetFrameworkVersion>v4.6</TargetFrameworkVersion>
     <CreateVsixContainer Condition=" '$(CreateVsixContainer)' == '' ">False</CreateVsixContainer>
     <CopyBuildOutputToOutputDirectory>False</CopyBuildOutputToOutputDirectory>

build-tools/create-vsix/create-vsix.targets
@@ -1,9 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <UsingTask AssemblyFile="..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"   TaskName="Xamarin.Android.BuildTools.PrepTasks.GitBlame" />
-  <UsingTask AssemblyFile="..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"   TaskName="Xamarin.Android.BuildTools.PrepTasks.GitBranch" />
-  <UsingTask AssemblyFile="..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"   TaskName="Xamarin.Android.BuildTools.PrepTasks.GitCommitHash" />
-  <UsingTask AssemblyFile="..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"   TaskName="Xamarin.Android.BuildTools.PrepTasks.GitCommitsInRange" />
+  <Import Project="..\scripts\XAVersionInfo.targets" />
   <UsingTask AssemblyFile="..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"   TaskName="Xamarin.Android.BuildTools.PrepTasks.ReplaceFileContents" />
   <PropertyGroup>
     <!-- Don't ever deploy, since that won't work on Mac -->
@@ -81,7 +78,7 @@
     </ItemGroup>
   </Target>
   <Target Name="GetVsixVersion"
-      DependsOnTargets="_GetVersionInfo"
+      DependsOnTargets="GetXAVersionInfo"
       Returns="$(VsixVersion)">
     <PropertyGroup>
       <VsixVersion>$(ProductVersion).$(_XACommitCount)</VsixVersion>
@@ -94,57 +91,34 @@
       <IsExperimental Condition=" '$(IsProductComponent)' == 'True' ">false</IsExperimental>
     </PropertyGroup>
   </Target>
-  <Target Name="_GetVersionInfo">
-    <GitBlame
-        FileName="$(XamarinAndroidSourcePath)\Configuration.props"
-        LineFilter="&lt;ProductVersion&gt;"
-        WorkingDirectory="$(XamarinAndroidSourcePath)"
-        ToolPath="$(GitToolPath)"
-        ToolExe="$(GitToolExe)">
-      <Output TaskParameter="Commits"                 ItemName="_XAVersionCommit" />
-    </GitBlame>
-    <GitCommitsInRange
-        StartCommit="%(_XAVersionCommit.CommitHash)"
-        WorkingDirectory="$(XamarinAndroidSourcePath)"
-        ToolPath="$(GitToolPath)"
-        ToolExe="$(GitToolExe)">
-      <Output TaskParameter="CommitCount"             PropertyName="_XACommitCount" />
-    </GitCommitsInRange>
-    <GitCommitHash
-        WorkingDirectory="$(XamarinAndroidSourcePath)"
-        ToolPath="$(GitToolPath)"
-        ToolExe="$(GitToolExe)">
-      <Output TaskParameter="AbbreviatedCommitHash"   PropertyName="_XAHash" />
-    </GitCommitHash>
-    <GitBranch
-        WorkingDirectory="$(XamarinAndroidSourcePath)"
-        ToolPath="$(GitToolPath)"
-        ToolExe="$(GitToolExe)">
-      <Output TaskParameter="Branch"                  PropertyName="_XABranch" />
-    </GitBranch>
-  </Target>
   <Target Name="_CreateDependencies"
-      DependsOnTargets="_GetVersionInfo"
+      DependsOnTargets="GetXAVersionInfo"
       BeforeTargets="Build"
       Inputs="Xamarin.Android.Sdk.pkgdef.in"
       Outputs="Xamarin.Android.Sdk.pkgdef">
     <ReplaceFileContents
         SourceFile="Xamarin.Android.Sdk.pkgdef.in"
         DestinationFile="Xamarin.Android.Sdk.pkgdef"
-        Replacements="@PACKAGE_VERSION@=$(ProductVersion);@PACKAGE_VERSION_BUILD@=$(_XACommitCount);@PACKAGE_HEAD_REV@=$(_XAHash);@PACKAGE_HEAD_BRANCH@=$(_XABranch)">
+        Replacements="@PACKAGE_VERSION@=$(ProductVersion);@PACKAGE_VERSION_BUILD@=$(XAVersionCommitCount);@PACKAGE_HEAD_REV@=$(XAVersionHash);@PACKAGE_HEAD_BRANCH@=$(XAVersionBranch)">
     </ReplaceFileContents>
   </Target>
-  <Target Name="_CopyToBuildConfiguration"
-      DependsOnTargets="_GetVersionInfo"
-      Inputs="$(OutputPath)$(AssemblyName).vsix"
-      Outputs="..\..\bin\Build$(Configuration)\$(AssemblyName).vsix">
+  <Target Name="_GetVsixPath"
+      DependsOnTargets="GetXAVersionInfo">
     <PropertyGroup>
-      <_Branch>$(_XABranch.Replace ('/', '-'))</_Branch>
+      <_Branch>$(XAVersionBranch.Replace ('/', '-'))</_Branch>
       <_Branch>$(_Branch.Replace ('\', '-'))</_Branch>
     </PropertyGroup>
+    <PropertyGroup>
+      <VsixPath Condition=" '$(VsixPath)' == '' ">..\..\bin\Build$(Configuration)\$(AssemblyName)-$(ProductVersion).$(XAVersionCommitCount)_$(XAVersionBranch)_$(XAVersionHash).vsix</VsixPath>
+    </PropertyGroup>
+  </Target>
+  <Target Name="_CopyToBuildConfiguration"
+      DependsOnTargets="_GetVsixPath"
+      Inputs="$(OutputPath)$(AssemblyName).vsix"
+      Outputs="$(VsixPath)">
     <Copy
         SourceFiles="$(OutputPath)$(AssemblyName).vsix"
-        DestinationFiles="..\..\bin\Build$(Configuration)\$(AssemblyName)-$(ProductVersion).$(_XACommitCount)_$(_Branch)_$(_XAHash).vsix"
+        DestinationFiles="$(VsixPath)"
     />
   </Target>
 </Project>

build-tools/scripts/BuildEverything.mk
@@ -130,7 +130,14 @@ _BUNDLE_ZIPS_EXCLUDE  = \
 
 create-vsix:
 	$(foreach conf, $(CONFIGURATIONS), \
-		MONO_IOMAP=all MONO_OPTIONS=--arch=64 msbuild $(MSBUILD_FLAGS) build-tools/create-vsix/create-vsix.csproj /p:Configuration=$(conf) /p:CreateVsixContainer=True ; )
+		MONO_IOMAP=all MONO_OPTIONS=--arch=64 msbuild $(MSBUILD_FLAGS) /p:Configuration=$(conf) /p:CreateVsixContainer=True \
+			build-tools/create-vsix/create-vsix.csproj \
+			$(if $(VSIX),"/p:VsixPath=$(VSIX)") \
+			$(if $(PRODUCT_COMPONENT),/p:IsProductComponent="$(PRODUCT_COMPONENT)") \
+			$(if $(PACKAGE_VERSION),/p:ProductVersion="$(PACKAGE_VERSION)") \
+			$(if $(PACKAGE_HEAD_BRANCH),/p:XAVersionBranch="$(PACKAGE_HEAD_BRANCH)") \
+			$(if $(PACKAGE_VERSION_REV),/p:XAVersionCommitCount="$(PACKAGE_VERSION_REV)") \
+			$(if $(COMMIT),/p:XAVersionHash="$(COMMIT)") ; )
 
 package-oss-name:
 	@echo ZIP_OUTPUT=$(ZIP_OUTPUT)

commit dcb568ec87c0639190f8e3230a662f6d37d7bd4d

[create-vsix] Improve compatibility with commercial .vsix
Reduce the number of differences between the `.vsix` file we build and
the `Xamarin.Android.Sdk*.vsix` file which is provided commercially:

* The `$(VsixVersion)` wasn't a valid version value; it was e.g.
    `7.3.99.` (note trailing `.`). This was caused by
    611bb66e, which removed `$(_XACommitCount)`, replacing it with
    `$(XAVersionCommitCount).

* Default to `$(IsProductComponent)`=True, and separate its logic from
    `$(IsExperimental)`. `$(IsProductComponent)` alters the value of
    the `packages[0].id` value within `catalog.json`: when
    `$(IsProductComponent)` is False, `packages[0].id *starts with*
    `Component`, e.g. `Component.Xamarin.Android.Sdk`, which is wrong.

    I'm still not sure why `$(IsProductComponent)` and
    `$(IsExperimental)` were previously tied together as they were.

* Set `$(ExtensionInstallationFolder)` so that the
    `packages[0].extensionDir` value within `catalog.json` doesn't
    contain a random filename. Previously, it would be e.g.:

        "extensionDir":"[installdir]\\Common7\\IDE\\Extensions/x11jqb13.6u5

    Setting `$(ExtensionInstallationFolder)` allows this to instead be:

        "extensionDir":"[installdir]\\Common7\\IDE\\Extensions/Xamarin.Android.Sdk

    TODO/Note: I suspect that the `Microsoft.VSSDK.BuildTools` code
    which creates `extensionDir` is using `Path.Combine()`, because
    we're getting a `.../Xamarin.Android.Sdk` value (`/` is used).
    The existing commercial package uses `\\` consistently.

    I don't know if this will be a problem.

* Set `%(Content.IncludeInVSIX)`=True for `AndroidSdkPackage.ico`, so
    that it is included in the `.vsix` file.

* Create the files
    `bin\$(Configuration)\lib\xbuild\Xamarin\Android\Version.txt` and
    `bin\$(Configuration)\lib\mandroid\MULTIDEX_JAR_LICENSE.txt`
    to work around Bug #54804 so that we have *some* form of
    `Version*` and `MULTIDEX_JAR_LICENSE` file within the `.vsix`.

build-tools/create-vsix/create-vsix.csproj
@@ -17,14 +17,19 @@
     <IncludeCopyLocalReferencesInVSIXContainer>False</IncludeCopyLocalReferencesInVSIXContainer>
     <IncludeDebugSymbolsInLocalVSIXDeployment>False</IncludeDebugSymbolsInLocalVSIXDeployment>
     <IncludeDebugSymbolsInVSIXContainer>False</IncludeDebugSymbolsInVSIXContainer>
-    <IsProductComponent Condition=" '$(IsProductComponent)' == '' ">False</IsProductComponent>
+    <IsExperimental Condition=" '$(IsExperimental)' == '' ">true</IsExperimental>
+    <IsProductComponent Condition=" '$(IsProductComponent)' == '' ">True</IsProductComponent>
     <_BuildVsix Condition=" '$(CreateVsixContainer)' == 'True' And Exists ('$(VsSDKInstall)') ">True</_BuildVsix>
     <_BuildVsix Condition=" '$(_BuildVsix)' == '' ">False</_BuildVsix>
+    <ExtensionInstallationFolder>Xamarin.Android.Sdk</ExtensionInstallationFolder>
     <OutputPath>bin\$(Configuration)</OutputPath>
   </PropertyGroup>
   <Import Project="..\..\Configuration.props" />
   <ItemGroup>
-    <Content Include="Resources\AndroidSdkPackage.ico" />
+    <Content Include="Resources\AndroidSdkPackage.ico">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+      <IncludeInVSIX>true</IncludeInVSIX>
+    </Content>
     <Content Include="Xamarin.Android.Sdk.pkgdef">
       <CopyToOutputDirectory>Always</CopyToOutputDirectory>
       <IncludeInVSIX>True</IncludeInVSIX>

build-tools/create-vsix/create-vsix.targets
@@ -27,15 +27,14 @@
       Returns="@(MSBuild);@(ReferenceAssemblies)">
     <ItemGroup>
       <MSBuild Include="$(LibDir)xbuild\**\*.*" />
-      <MSBuild Remove="$(LibDir)xbuild\Xamarin\Android\lib\host-*\**\*.*" />
+      <MSBuild Remove="$(LibDir)xbuild\Xamarin\Android\lib\host-Darwin\**\*.*" />
+      <MSBuild Remove="$(LibDir)xbuild\Xamarin\Android\lib\host-Linux\**\*.*" />
       <MSBuild>
         <VSIXSubPath Condition=" '%(VSIXSubPath)' == '' ">%(RecursiveDir)</VSIXSubPath>
       </MSBuild>
-      <MSBuild Include="$(LibDir)xbuild\Xamarin\Android\lib\host-mxe-Win64\*.*" />
       <MSBuild>
         <VSIXSubPath Condition=" '%(VSIXSubPath)' == '' ">Xamarin/Android</VSIXSubPath>
       </MSBuild>
-      <MSBuild Include="$(LibDir)xbuild\Xamarin\Android\lib\host-win\*.*" />
       <MSBuild>
         <VSIXSubPath Condition=" '%(VSIXSubPath)' == '' ">Xamarin/Android/lib/host-win</VSIXSubPath>
       </MSBuild>
@@ -45,7 +44,6 @@
       </MSBuild>
       <MSBuild Include="..\..\bin\$(Configuration)\lib\mandroid\**\*.*" />
       <MSBuild Remove="..\..\bin\$(Configuration)\lib\mandroid\**\*.d.exe" />
-      <MSBuild Remove="..\..\bin\$(Condition)\lib\mandroid\host-Darwin\**\*.*" />
       <MSBuild>
         <VSIXSubPath Condition=" '%(VSIXSubPath)' == '' ">Xamarin/Android/%(RecursiveDir)</VSIXSubPath>
       </MSBuild>
@@ -81,15 +79,11 @@
       DependsOnTargets="GetXAVersionInfo"
       Returns="$(VsixVersion)">
     <PropertyGroup>
-      <VsixVersion>$(ProductVersion).$(_XACommitCount)</VsixVersion>
+      <VsixVersion>$(ProductVersion).$(XAVersionCommitCount)</VsixVersion>
     </PropertyGroup>
   </Target>
   <Target Name="GetIsExperimental"
       Returns="$(IsExperimental)">
-    <PropertyGroup>
-      <IsExperimental>true</IsExperimental>
-      <IsExperimental Condition=" '$(IsProductComponent)' == 'True' ">false</IsExperimental>
-    </PropertyGroup>
   </Target>
   <Target Name="_CreateDependencies"
       DependsOnTargets="GetXAVersionInfo"

build-tools/scripts/BuildEverything.mk
@@ -138,6 +138,7 @@ create-vsix:
 		MONO_IOMAP=all MONO_OPTIONS=--arch=64 msbuild $(MSBUILD_FLAGS) /p:Configuration=$(conf) /p:CreateVsixContainer=True \
 			build-tools/create-vsix/create-vsix.csproj \
 			$(if $(VSIX),"/p:VsixPath=$(VSIX)") \
+			$(if $(EXPERIMENTAL),/p:IsExperimental="$(EXPERIMENTAL)") \
 			$(if $(PRODUCT_COMPONENT),/p:IsProductComponent="$(PRODUCT_COMPONENT)") \
 			$(if $(PACKAGE_VERSION),/p:ProductVersion="$(PACKAGE_VERSION)") \
 			$(if $(PACKAGE_HEAD_BRANCH),/p:XAVersionBranch="$(PACKAGE_HEAD_BRANCH)") \

build-tools/xa-prep-tasks/xa-prep-tasks.targets
@@ -19,6 +19,7 @@
       <_VersionCommitFile>..\..\bin\$(Configuration)\Version.commit</_VersionCommitFile>
       <_VersionRevFile>..\..\bin\$(Configuration)\Version.rev</_VersionRevFile>
       <_XAVersionFile>$(_XAPrefix)\Version</_XAVersionFile>
+      <_XAVersionTxtFile>$(_XAPrefix)\Version.txt</_XAVersionTxtFile>
       <_XAVersionCommitFile>$(_XAPrefix)\Version.commit</_XAVersionCommitFile>
       <_XAVersionRevFile>$(_XAPrefix)\Version.rev</_XAVersionRevFile>
     </PropertyGroup>
@@ -29,6 +30,11 @@
     />
     <WriteLinesToFile
         File="$(_XAVersionFile)"
+        Lines="$(ProductVersion)"
+        Overwrite="True"
+    />
+    <WriteLinesToFile
+        File="$(_XAVersionTxtFile)"
         Lines="$(ProductVersion)"
         Overwrite="True"
     />

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Build.Tasks.targets
@@ -157,7 +157,11 @@
     <Copy
       SourceFiles="$(_SupportLicense)"
       DestinationFiles="$(OutputPath)..\..\..\mandroid\MULTIDEX_JAR_LICENSE" />
-    <Touch Files="$(OutputPath)..\..\..\mandroid\MULTIDEX_JAR_LICENSE" />
+    <Copy
+        SourceFiles="$(_SupportLicense)"
+        DestinationFiles="$(OutputPath)..\..\..\mandroid\MULTIDEX_JAR_LICENSE.txt"
+    />
+    <Touch Files="$(OutputPath)..\..\..\mandroid\MULTIDEX_JAR_LICENSE;$(OutputPath)..\..\..\mandroid\MULTIDEX_JAR_LICENSE.txt" />
   </Target>
 
   <ItemGroup>