b31e9af85b1c5effae6870786ada385d5ae49d05
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-06-22 10:21:06 -0500

    Implement running `vswhere.exe` to locate Xamarin SDKs
    - We can run `vswhere -latest -requires Component.Xamarin` which will
    locate the latest VS installation that contains Xamarin
    - Using `-property installationPath` causes stdout to only contain the
    single value
    - Included comments containing various URLs of interest

--- external/xamarin-android-tools/src/Xamarin.Android.Tools/Sdks/MonoDroidSdkWindows.cs
+++ external/xamarin-android-tools/src/Xamarin.Android.Tools/Sdks/MonoDroidSdkWindows.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Xamarin.Android.Tools
 {
@@ -18,19 +19,36 @@ namespace Xamarin.Android.Tools
 			string xamarinSdk = Path.Combine (OS.ProgramFilesX86, "MSBuild", "Xamarin", "Android");
 			if (Directory.Exists(xamarinSdk))
 				return xamarinSdk;
-			xamarinSdk = Path.Combine(OS.ProgramFilesX86, "Microsoft Visual Studio", "2017");
-			if (Directory.Exists(xamarinSdk))
-			{
-				xamarinSdk = Directory.GetDirectories(xamarinSdk).FirstOrDefault();
-				if (!string.IsNullOrEmpty(xamarinSdk))
-				{
-					xamarinSdk = Path.Combine(xamarinSdk, "MSBuild", "Xamarin", "Android");
-					if (Directory.Exists(xamarinSdk))
-						return xamarinSdk;
-				}
-			}
-
+			if (TryVSWhere(out xamarinSdk))
+				return xamarinSdk;
 			return OS.ProgramFilesX86 + @"\MSBuild\Novell";
+		}
+
+		bool TryVSWhere(out string xamarinSdk)
+		{
+			xamarinSdk = null;
+
+			//Docs on this tool here: https://github.com/Microsoft/vswhere
+			string vswhere = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Microsoft Visual Studio", "Installer", "vswhere.exe");
+			if (!File.Exists(vswhere))
+				return false;
+
+			//VS Workload ID here: https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
+			var p = Process.Start(new ProcessStartInfo
+			{
+				FileName = vswhere,
+				Arguments = "-latest -requires Component.Xamarin -property installationPath",
+				RedirectStandardError = true,
+				RedirectStandardOutput = true,
+				UseShellExecute = false,
+			});
+			p.WaitForExit();
+
+			if (p.ExitCode != 0)
+				return false;
+
+			xamarinSdk = Path.Combine(p.StandardOutput.ReadToEnd().Trim(), "MSBuild", "Xamarin", "Android");
+			return true;
 		}
 
 		static readonly string[] RuntimeToFrameworkPaths = new []{
