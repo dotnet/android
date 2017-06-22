46acdaf2c56ac3e019b8d6b37befff5027a5a66b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-06-22 10:18:23 -0500

    Setup a test project for validating `MonoDroidSdk` class
    This is basically just a smoke test making sure all the paths exist, and
    `AndroidLogger` doesn't throw errors out.

---
+++ external/xamarin-android-tools/src/Xamarin.Android.Tools.Tests/MonoDroidSdkTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+
+namespace Xamarin.Android.Tools.Tests
+{
+	[TestFixture]
+	class MonoDroidSdkTests
+	{
+		[TestFixtureSetUp]
+		public void FixtureSetUp()
+		{
+			AndroidLogger.Info += OnInfo;
+			AndroidLogger.Warning += OnWarning;
+			AndroidLogger.Error += OnError;
+		}
+
+		[TestFixtureTearDown]
+		public void FixtureTearDown()
+		{
+			AndroidLogger.Info -= OnInfo;
+			AndroidLogger.Warning -= OnWarning;
+			AndroidLogger.Error -= OnError;
+		}
+
+		void OnInfo(string task, string message)
+		{
+			Debug.WriteLine(task + ": " + message);
+		}
+
+		void OnWarning(string task, string message)
+		{
+			Assert.Fail(task + ": " + message);
+		}
+
+		void OnError(string task, string message)
+		{
+			Assert.Fail(task + ": " + message);
+		}
+
+		[Test]
+		public void RefreshWithoutParameters()
+		{
+			//Just checking for exceptions, or AndroidLogger
+			MonoDroidSdk.Refresh();
+		}
+
+		[Test]
+		public void BinPathExists()
+		{
+			string path = MonoDroidSdk.BinPath;
+			Assert.IsTrue(Directory.Exists(path), path + " does not exist!"); 
+		}
+
+		[Test]
+		public void FrameworkPathExists()
+		{
+			string path = MonoDroidSdk.FrameworkPath;
+			Assert.IsTrue(Directory.Exists(path), path + " does not exist!");
+		}
+
+		[Test]
+		public void RuntimePathExists()
+		{
+			string path = MonoDroidSdk.RuntimePath;
+			Assert.IsTrue(Directory.Exists(path), path + " does not exist!");
+		}
+	}
+}
---
+++ external/xamarin-android-tools/src/Xamarin.Android.Tools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Xamarin.Android.Tools.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xamarin.Android.Tools.Tests")]
+[assembly: AssemblyCopyright("Copyright © Xamarin 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
