8ebb9cb2bdc1429930f0d072289b964b6fb44f1e
Tim Wanders <Tim241@ziggo.nl>
2017-06-27 22:31:22 +0200

    [build] Add build dependencies for Arch (#664)
    Add `make prepare` support for Arch Linux.
    
    Re-factor the `make prepare` logic for Linux into a set of
    `build-tools/scripts/dependencies/linux-prepare-*.sh` shell scripts,
    so that instead of having every possible Linux distro combined into
    `Makefile`, we can split them up in a more sensible manner.
    Dependency scripts should have one of the names:
    
    	linux-prepare-$(LINUX_DISTRO).sh
    	linux-prepare-$(LINUX_DISTRO)-$(LINUX_DISTRO_RELEASE).sh
    
    where `$(LINUX_DISTRO)` is from `lsb_release -i -s`, and
    `$(LINUX_DISTRO_RELEASE)` is from `lsb_release -r -s`. The dependency
    script should install all required dependencies to build the
    xamarin-android repo, *unless* `$NO_SUDO` is set.
    
    If the `$NO_SUDO` environment variable is `true`, then the dependency
    script should check to see if all dependencies are installed. If
    they are not installed, it should exit with a non-0 (zero) value.
    If `$NO_SUDO` is `false`, then `sudo` should be used to install
    the dependencies.

---
+++ build-tools/scripts/dependencies/linux-prepare-Arch.sh
@@ -0,0 +1,38 @@
+ARCH_DEPS="autoconf
+	automake
+	binutils
+	bison
+	curl
+	fakeroot
+	file
+	findutils
+	flex
+	gawk
+	gcc
+	gettext
+	git
+	grep
+	groff
+	gtk-sharp-2
+	gzip
+	jdk8-openjdk
+	libtool
+	libzip
+	m4
+	make
+	nuget
+	patch
+	pkg-config
+	pkg-config
+	referenceassemblies-pcl
+	sed
+	texinfo
+	unzip
+	which
+	xxd
+	"
+if [ $NO_SUDO = "false" ]; then
+	sudo pacman -S --noconfirm $ARCH_DEPS
+else
+	echo "Sudo is required!"
+fi
