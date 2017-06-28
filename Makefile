dotnet/android

commit 8ebb9cb2bdc1429930f0d072289b964b6fb44f1e

[build] Add build dependencies for Arch (#664)
Add `make prepare` support for Arch Linux.

Re-factor the `make prepare` logic for Linux into a set of
`build-tools/scripts/dependencies/linux-prepare-*.sh` shell scripts,
so that instead of having every possible Linux distro combined into
`Makefile`, we can split them up in a more sensible manner.
Dependency scripts should have one of the names:

	linux-prepare-$(LINUX_DISTRO).sh
	linux-prepare-$(LINUX_DISTRO)-$(LINUX_DISTRO_RELEASE).sh

where `$(LINUX_DISTRO)` is from `lsb_release -i -s`, and
`$(LINUX_DISTRO_RELEASE)` is from `lsb_release -r -s`. The dependency
script should install all required dependencies to build the
xamarin-android repo, *unless* `$NO_SUDO` is set.

If the `$NO_SUDO` environment variable is `true`, then the dependency
script should check to see if all dependencies are installed. If
they are not installed, it should exit with a non-0 (zero) value.
If `$NO_SUDO` is `false`, then `sudo` should be used to install
the dependencies.

Makefile
@@ -1,5 +1,6 @@
-OS            := $(shell uname)
-OS_ARCH       := $(shell uname -m)
+export OS            := $(shell uname)
+export OS_ARCH       := $(shell uname -m)
+export NO_SUDO ?= false
 V             ?= 0
 CONFIGURATION = Debug
 RUNTIME       := $(shell if [ -f "`which mono64`" ] ; then echo mono64 ; else echo mono; fi) --debug=casts
@@ -18,8 +19,13 @@ ifneq ($(MONO_OPTIONS),)
 export MONO_OPTIONS
 endif
 
-include build-tools/scripts/msbuild.mk
+ifeq ($(OS),Linux)
+export LINUX_DISTRO         := $(shell lsb_release -i -s || true)
+export LINUX_DISTRO_RELEASE := $(shell lsb_release -r -s || true)
+prepare:: linux-prepare
+endif # $(OS)=Linux
 
+include build-tools/scripts/msbuild.mk
 all::
 	$(MSBUILD) $(MSBUILD_FLAGS) $(SOLUTION)
 
@@ -27,6 +33,23 @@ all-tests::
 	MSBUILD="$(MSBUILD)" tools/scripts/xabuild $(MSBUILD_FLAGS) Xamarin.Android-Tests.sln
 
 prepare:: prepare-msbuild
+ 	
+linux-prepare::
+	BINFMT_MISC_TROUBLE="cli win" \
+	BINFMT_WARN=no ; \
+	for m in $BINFMT_MISC_TROUBLE; do \
+		if [ -f /proc/sys/fs/binfmt_misc/$$m ]; then \
+			BINFMT_WARN=yes ; \
+		fi ; \
+	done ; \
+	if [ "x$$BINFMT_WARN" = "xyes" ]; then \
+		cat Documentation/binfmt_misc-warning-Linux.txt ; \
+	fi; \
+	if [ -f build-tools/scripts/dependencies/linux-prepare-$(LINUX_DISTRO).sh ]; then \
+		sh build-tools/scripts/dependencies/linux-prepare-$(LINUX_DISTRO).sh; \
+	elif [ -f build-tools/scripts/dependencies/linux-prepare-$(LINUX_DISTRO)-$(LINUX_DISTRO_RELEASE).sh ]; then \
+		sh build-tools/scripts/dependencies/linux-prepare-$(LINUX_DISTRO)-$(LINUX_DISTRO_RELEASE).sh; \
+	fi; \
 
 # $(call GetPath,path)
 GetPath   = $(shell $(MSBUILD) $(MSBUILD_FLAGS) /p:DoNotLoadOSProperties=True /nologo /v:minimal /t:Get$(1)FullPath build-tools/scripts/Paths.targets | tr -d '[[:space:]]' )
@@ -55,87 +78,7 @@ ifeq ($(USE_MSBUILD),1)
 		$(MSBUILD) $(MSBUILD_FLAGS) "$$proj"; \
 	done
 endif	# msbuild
-
 include build-tools/scripts/BuildEverything.mk
-
-# Please keep the package names sorted
-ifeq ($(OS),Linux)
-NO_SUDO ?= false
-
-UBUNTU_DEPS          = \
-	autoconf \
-	autotools-dev \
-	automake \
-	clang \
-	curl \
-	g++-mingw-w64 \
-	gcc-mingw-w64 \
-	git \
-	libtool \
-	libz-mingw-w64-dev \
-	libzip4 \
-	linux-libc-dev \
-	make \
-	openjdk-8-jdk \
-	unzip \
-	vim-common
-
-ifeq ($(OS_ARCH),x86_64)
-UBUNTU_DEPS          += \
-	lib32stdc++6 \
-	lib32z1 \
-	libx32tinfo-dev \
-	linux-libc-dev:i386 \
-	zlib1g-dev:i386
-endif
-LINUX_DISTRO         := $(shell lsb_release -i -s || true)
-LINUX_DISTRO_RELEASE := $(shell lsb_release -r -s || true)
-BINFMT_MISC_TROUBLE  := cli win
-
-prepare:: linux-prepare-$(LINUX_DISTRO) linux-prepare-$(LINUX_DISTRO)-$(LINUX_DISTRO_RELEASE)
-	@BINFMT_WARN=no ; \
-	for m in $(BINFMT_MISC_TROUBLE); do \
-		if [ -f /proc/sys/fs/binfmt_misc/$$m ]; then \
-			BINFMT_WARN=yes ; \
-		fi ; \
-	done ; \
-	if [ "x$$BINFMT_WARN" = "xyes" ]; then \
-		cat Documentation/binfmt_misc-warning-Linux.txt ; \
-	fi
-
-ifeq ($(NO_SUDO),false)
-linux-prepare-Ubuntu::
-	@echo
-	@echo Installing build depedencies for $(LINUX_DISTRO)
-	@echo Will use sudo, please provide your password as needed
-	@echo
-	sudo apt-get -f -u install $(UBUNTU_DEPS)
-else
-linux-prepare-Ubuntu::
-	@echo
-	@echo sudo is disabled, cannot install dependencies
-	@echo Listing status of all the dependencies
-	@PACKAGES_MISSING=no ; \
-	for p in $(UBUNTU_DEPS); do \
-		if dpkg -l $$p > /dev/null 2>&1 ; then \
-			echo "[INSTALLED] $$p" ; \
-		else \
-			echo "[ MISSING ] $$p" ; \
-			PACKAGES_MISSING=yes ; \
-		fi ; \
-	done ; \
-	echo ; \
-	if [ "x$$PACKAGES_MISSING" = "xyes" ]; then \
-		echo Some packages are missing, cannot continue ; \
-		echo ; \
-		false ; \
-	fi
-endif
-
-linux-prepare-$(LINUX_DISTRO)::
-
-linux-prepare-$(LINUX_DISTRO)-$(LINUX_DISTRO_RELEASE)::
-endif
 
 run-all-tests: run-nunit-tests run-ji-tests run-apk-tests
 

build-tools/scripts/dependencies/linux-prepare-Arch.sh
@@ -1,0 +1,38 @@
+ARCH_DEPS="autoconf
+	automake
+	binutils
+	bison
+	curl
+	fakeroot
+	file
+	findutils
+	flex
+	gawk
+	gcc
+	gettext
+	git
+	grep
+	groff
+	gtk-sharp-2
+	gzip
+	jdk8-openjdk
+	libtool
+	libzip
+	m4
+	make
+	nuget
+	patch
+	pkg-config
+	pkg-config
+	referenceassemblies-pcl
+	sed
+	texinfo
+	unzip
+	which
+	xxd
+	"
+if [ $NO_SUDO = "false" ]; then
+	sudo pacman -S --noconfirm $ARCH_DEPS
+else
+	echo "Sudo is required!"
+fi

build-tools/scripts/dependencies/linux-prepare-Ubuntu.sh
@@ -1,0 +1,44 @@
+UBUNTU_DEPS="autoconf 
+	autotools-dev 
+	automake 
+	clang 
+	curl 
+	g++-mingw-w64 
+	gcc-mingw-w64 
+	git 
+	libtool 
+	libz-mingw-w64-dev 
+	libzip4 
+	linux-libc-dev 
+	make 
+	openjdk-8-jdk 
+	unzip 
+	vim-common
+	"
+
+if [ $OS_ARCH = "x86_64" ]; then
+UBUNTU_DEPS="$UBUNTU_DEPS 
+	lib32stdc++6 
+	lib32z1 
+	libx32tinfo-dev 
+	linux-libc-dev:i386 
+	zlib1g-dev:i386
+	"
+fi 
+if [ $NO_SUDO = "true" ]; then
+	for p in $UBUNTU_DEPS; do 
+		if dpkg -l $$p > /dev/null 2>&1 ; then 
+			echo "[INSTALLED] $$p" 
+		else 
+			echo "[ MISSING ] $$p" 
+			PACKAGES_MISSING=yes 
+		fi 
+	done 
+	if [ "x$$PACKAGES_MISSING" = "xyes" ]; then 
+		echo Some packages are missing, cannot continue 
+		echo 
+		exit 1
+	fi
+else
+	sudo apt-get -f -u install $UBUNTU_DEPS
+fi

commit 54327464f6a1f06519ee11785fd888e1001b0ce0

[build] `make all` is default; Bump to LibZipSharp/1b217dab (#677)
Commit 8ebb9cb2 inadvertently made `make prepare` the default target
when running on Linux, instead of `make all`, breaking semantics:

	$ make
	# pre-8ebb9cb2, invoked `make all` everywhere.
	# Invokes `make prepare` on Linux in 8ebb9cb2, but
	# `make all` on macOS. Confusion!

Revert the default `make` target to `make all`.

Bump to LibZipSharp/master/1b217dab to increase reliability of
`libZipSharp.dll.config` creation on Linux if/when
`ldconfig -p | grep libzip` returns more than one shared library.

Makefile
@@ -19,18 +19,18 @@ ifneq ($(MONO_OPTIONS),)
 export MONO_OPTIONS
 endif
 
-ifeq ($(OS),Linux)
-export LINUX_DISTRO         := $(shell lsb_release -i -s || true)
-export LINUX_DISTRO_RELEASE := $(shell lsb_release -r -s || true)
-prepare:: linux-prepare
-endif # $(OS)=Linux
-
 include build-tools/scripts/msbuild.mk
 all::
 	$(MSBUILD) $(MSBUILD_FLAGS) $(SOLUTION)
 
 all-tests::
 	MSBUILD="$(MSBUILD)" tools/scripts/xabuild $(MSBUILD_FLAGS) Xamarin.Android-Tests.sln
+	
+ifeq ($(OS),Linux)
+export LINUX_DISTRO         := $(shell lsb_release -i -s || true)
+export LINUX_DISTRO_RELEASE := $(shell lsb_release -r -s || true)
+prepare:: linux-prepare
+endif # $(OS)=Linux
 
 prepare:: prepare-msbuild
  	