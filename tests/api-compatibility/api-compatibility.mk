cffc69969606884a8b187aa679a5458ff0a4e46b
Jonathan Pryor <jonpryor@vt.edu>
2017-07-03 23:04:24 -0700

    [api-compatibility] Check API Compatibility (#683)
    Developers like API compatibility; it helps ensure that their
    investment in our platform isn't a waste of time.
    
    Add `tests/api-compatibility` and a new
    `make run-api-compatibility-tests` target, part of
    `make run-all-tests`, which compares the API of the assemblies in the
    current build tree against a "known good" API description maintained
    in `tests/api-compatibility/reference/*.xml.gz`, one `.xml.gz` file
    per assembly that we are interested in.
    
    The new [`xamarin-android-api-compatibility`][0] submodule contains
    the current API description and Make targets to check and update the
    API description. `xamarin-android-api-compatibility` uses the
    utilities `mono-api-info` and `mono-api-html` to generate and compare
    the API descriptions.
    
    A submodule is used because the API descriptions can be quite large --
    Mono.Android.xml is currently 54MB -- and we don't want the change
    history on these files to "clog up" the xamarin-android repo.
    
    [0]: https://github.com/xamarin/xamarin-android-api-compatibility

---
+++ tests/api-compatibility/api-compatibility.mk
@@ -0,0 +1,32 @@
+# This file must be included from ../../Makefile
+
+CSC               = csc
+RUNTIME           = mono --debug
+
+MONO_PATH         = $(call GetPath,MonoSource)
+MONO_API_HTML_DIR = $(MONO_PATH)/mcs/tools/mono-api-html
+MONO_API_HTML     = bin/Build$(CONFIGURATION)/mono-api-html.exe
+MONO_API_INFO_DIR = $(MONO_PATH)/mcs/tools/corcompare
+MONO_API_INFO     = bin/Build$(CONFIGURATION)/mono-api-info.exe
+MONO_OPTIONS_SRC  = $(MONO_PATH)/mcs/class/Mono.Options/Mono.Options/Options.cs
+FRAMEWORK_DIR     = bin/$(CONFIGURATION)/lib/xbuild-frameworks/MonoAndroid
+
+
+run-api-compatibility-tests: $(MONO_API_HTML) $(MONO_API_INFO)
+	make -C external/xamarin-android-api-compatibility check \
+		MONO_API_HTML="$(RUNTIME) $(abspath $(MONO_API_HTML))" \
+		MONO_API_INFO="$(RUNTIME) $(abspath $(MONO_API_INFO))" \
+		STABLE_FRAMEWORKS="$(STABLE_FRAMEWORKS)" \
+		XA_FRAMEWORK_DIR="$(abspath $(FRAMEWORK_DIR))"
+
+$(MONO_API_HTML): $(wildcard $(MONO_API_HTML_DIR)/*.cs) $(MONO_OPTIONS_SRC)
+	$(CSC) -out:$@ $(wildcard $(MONO_API_HTML_DIR)/*.cs) \
+		$(MONO_OPTIONS_SRC) \
+		-r:System.Xml.dll -r:System.Xml.Linq.dll
+
+MONO_API_INFO_REFS  = \
+  bin/$(CONFIGURATION)/lib/mandroid/Xamarin.Android.Cecil.dll
+
+$(MONO_API_INFO): $(wildcard $(MONO_API_INFO_DIR)/*.cs) $(MONO_OPTIONS_SRC)
+	$(CSC) -out:$@ $^ /main:CorCompare.Driver \
+		$(MONO_API_INFO_REFS:%=-r:%)
