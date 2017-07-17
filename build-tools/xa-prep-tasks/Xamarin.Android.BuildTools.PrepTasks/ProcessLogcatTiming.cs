df79d3d422c8c2dccac485a5ac38b91bb89c348c
Radek Doulik <radekdoulik@users.noreply.github.com>
2017-07-17 18:44:00 +0200

    [tests] Process logcat output to measure startup times (#680)
    - setup emulator with bigger logcat buffers, enable debug timing
       output
    
     - dump logcat to file after test is run and process it with the new
       task
    
     - the task also creates csv files with data to be used by Jenkins,
       plot plugin https://wiki.jenkins.io/display/JENKINS/Plot+Plugin
    
       the resulting plot can be seen here
       https://jenkins.mono-project.com/job/xamarin-android-pr-builder/plot/
    
       the init value means time to get to the Runtime.init end, in
       milliseconds
    
       the last value means time to get to the last timing line
       (monodroid-timing) in the logcat ouput for the application, in
       milliseconds
    
     - the intent is to be aware about basic performance and to be able to
       detect performance regressions. it will be extended to measure more
       interesting times, like getting to the point where UI gets
       responsive in test app like out Hello world sample or Forms
       application sample
    
     - it produces 2 outputs. csv file for plot plugin (when requested),
       which is simple csv file with 2 lines, column names and column
       data. example:
    
        init,last
        799,2935
    
       second output uses the Log class, performance summary with normal
       priority and detailed output with low priority

---
+++ build-tools/xa-prep-tasks/Xamarin.Android.BuildTools.PrepTasks/ProcessLogcatTiming.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Android.BuildTools.PrepTasks
+{
+	public class ProcessLogcatTiming : Task
+	{
+		[Required]
+		public string LogcatFilename { get; set; }
+
+		[Required]
+		public string ApplicationPackageName { get; set; }
+
+		public string ResultsFilename { get; set; }
+
+		public override bool Execute ()
+		{
+			using (var reader = new StreamReader (LogcatFilename)) {
+				string line;
+				int pid = -1;
+				var procStartRegex = new Regex ($@"^(?<timestamp>\d+-\d+\s+[\d:\.]+)\s+.*ActivityManager: Start proc.*for added application {ApplicationPackageName}: pid=(?<pid>\d+)");
+				Regex timingRegex = null;
+				var runtimeInitRegex = new Regex (@"Runtime\.init: end native-to-managed");
+				DateTime start = DateTime.Now;
+				DateTime last = start;
+				DateTime initEnd = start;
+
+				while ((line = reader.ReadLine ()) != null) {
+					if (pid == -1) {
+						var match = procStartRegex.Match (line);
+						if (!match.Success)
+							continue;
+
+						last = start = ParseTime (match.Groups ["timestamp"].Value);
+						pid = Int32.Parse (match.Groups ["pid"].Value);
+						Log.LogMessage (MessageImportance.Low, $"Time:      0ms process start, application: '{ApplicationPackageName}' PID: {pid}");
+						timingRegex = new Regex ($@"^(?<timestamp>\d+-\d+\s+[\d:\.]+)\s+{pid}\s+.*I monodroid-timing:\s(?<message>.*)$");
+					} else {
+						var match = timingRegex.Match (line);
+						if (!match.Success)
+							continue;
+
+						var time = ParseTime (match.Groups ["timestamp"].Value);
+						var span = time - start;
+						Log.LogMessage (MessageImportance.Low, $"Time: {span.TotalMilliseconds.ToString ().PadLeft (6)}ms Message: {match.Groups ["message"].Value}");
+
+						match = runtimeInitRegex.Match (match.Groups ["message"].Value);
+						if (match.Success)
+							initEnd = time;
+						last = time;
+					}
+				}
+
+				if (pid != -1) {
+					Log.LogMessage (MessageImportance.Normal, " -- Performance summary --");
+					Log.LogMessage (MessageImportance.Normal, $"Runtime init end: {(initEnd - start).TotalMilliseconds}ms");
+					Log.LogMessage (MessageImportance.Normal, $"Last timing message: {(last - start).TotalMilliseconds}ms");
+
+					if (ResultsFilename != null)
+						File.WriteAllText (Path.Combine (Path.GetDirectoryName (ResultsFilename), $"Test-{ApplicationPackageName}-times.csv"),
+						                   $"init,last\n{(initEnd - start).TotalMilliseconds},{(last - start).TotalMilliseconds}");
+				} else
+					Log.LogWarning ("Wasn't able to collect the performance data");
+
+				reader.Close ();
+			}
+
+			return true;
+		}
+
+		static Regex timeRegex = new Regex (@"(?<month>\d+)-(?<day>\d+)\s+(?<hour>\d+):(?<minute>\d+):(?<second>\d+)\.(?<millisecond>\d+)");
+		DateTime ParseTime (string s)
+		{
+			var match = timeRegex.Match (s);
+			if (!match.Success)
+				throw new InvalidOperationException ($"Unable to parse time: '{s}'");
+
+			// we don't handle year boundary here as the logcat timestamp doesn't include year information
+			return new DateTime (DateTime.Now.Year,
+					     int.Parse (match.Groups ["month"].Value),
+					     int.Parse (match.Groups ["day"].Value),
+					     int.Parse (match.Groups ["hour"].Value),
+					     int.Parse (match.Groups ["minute"].Value),
+					     int.Parse (match.Groups ["second"].Value),
+					     int.Parse (match.Groups ["millisecond"].Value));
+		}
+	}
+}
