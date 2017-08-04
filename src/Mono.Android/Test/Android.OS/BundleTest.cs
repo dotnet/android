c04c9529a16e7b5af4b3b45dd9af70d4778ca9a1
Marek Habersack <grendel@twistedcode.net>
2017-08-04 22:10:25 +0200

    [Mono.Android] PeekObject() should consider target type (#717)
    Fixes: https://bugzilla.xamarin.com/show_bug.cgi?id=58405
    
    When using `Android.OS.Bundle` to retrieve a collection for a specific
    key one can either use the untyped `Get` method or one of the
    generic `Get*ArrayList` methods to retrieve the list. In the first
    case we will get back an `IList` instance, in the second case we'll
    get an `IList<T>` instance. However, if one calls `Get` first and then
    one of the generic methods next, for the same key, the result will be
    a thrown `InvalidCastException` because we attempt to cast the `IList`
    obtained from `Get` to `IList<T>` needed by the generic `Get*ArrayList`
    method. This happens because the same native handle is used in both cases
    and the first call to `Get` causes `Java.Lang.Object` to cache the `IList`
    instance so that the second call to `Get*ArrayList` gets the cached object
    instead of a new, properly typed, one.
    
    The solution is to extend `Java.Lang.Object.PeekObject` to allow specifying
    the desired type of the object corresponding to the native handle and, if
    the requirement isn't met, evict the entry from the cache returning `null`
    to the caller, thus letting it do the right thing by creating an instance
    of a class with correct type.

---
+++ src/Mono.Android/Test/Android.OS/BundleTest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Android.OS;
+
+using NUnit.Framework;
+
+namespace Xamarin.Android.RuntimeTests {
+
+	[TestFixture]
+	public class BundleTest 
+	{
+		[Test]
+		public void TestBundleIntegerArrayList()
+		{
+			var b = new Bundle();
+			b.PutIntegerArrayList("key", new List<Java.Lang.Integer>() { Java.Lang.Integer.ValueOf(1) });
+			var list = b.GetIntegerArrayList ("key");
+			Assert.NotNull (list, "'key' doesn't refer to a list of integers");
+			var obj = b.Get ("key");
+			Assert.NotNull (obj, "Missing 'key' in bundle");
+			try {
+				list = b.GetIntegerArrayList ("key");
+				Assert.NotNull (list, "'key' doesn't refer to a list of integers after non-generic call");
+			} catch (Exception e) {
+				Assert.Fail ("Java.Lang.Object caches too aggresively");
+			}
+		}
+
+		[Test]
+		public void TestBundleIntegerArrayList2 ()
+		{
+			var b = new Bundle();
+			b.PutIntegerArrayList ("key", new List<Java.Lang.Integer> () { Java.Lang.Integer.ValueOf (1) });
+			var obj = b.Get ("key");
+			Assert.NotNull (obj, "Missing 'key' in bundle");
+			try {
+				var list = b.GetIntegerArrayList ("key");
+				Assert.NotNull (list, "'key' doesn't refer to a list of integers");
+			} catch (Exception e) {
+				Assert.Fail ("Java.Lang.Object caches too aggresively");
+			}
+		}
+	}
+}
