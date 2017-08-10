1409f894fd53eb2b9036849d06fb5e3c5c8cbd9a
Jonathan Peppers <jonathan.peppers@gmail.com>
2017-08-10 14:43:02 -0500

    [xa-prep-tasks] Add <GitCommitTime/> (#734)
    Context: #181 (in comments)
    
    In several places throughout the build, we are running a command such
    as `<Exec Command="touch -m -t `git log -1 --format=%25cd
    --date=format-local:%25Y%25m%25d%25H%25M.%25S` Makefile" />`, which
    will not work on Windows.
    
    The solution is to create an MSBuild task in xa-prep-tasks named
    `GitCommitTime` that will work cross-platform. `GitCommitTime` returns
    a string value that can be passed to the `Touch` MSBuild task.

---
+++ build-tools/xa-prep-tasks/Xamarin.Android.BuildTools.PrepTasks/GitCommitTime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Microsoft.Build.Framework;
+
+namespace Xamarin.Android.BuildTools.PrepTasks
+{
+	public sealed class GitCommitTime : Git
+	{
+		[Output]
+		public string Time { get; set; }
+
+		protected override bool LogTaskMessages {
+			get { return false; }
+		}
+
+		public GitCommitTime ()
+		{
+		}
+
+		public override bool Execute ()
+		{
+			Log.LogMessage (MessageImportance.Low, $"Task {nameof (GitCommitTime)}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (WorkingDirectory)}: {WorkingDirectory.ItemSpec}");
+
+			base.Execute ();
+
+			Log.LogMessage (MessageImportance.Low, $"  [Output] {nameof (Time)}: {Time}");
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected override string GenerateCommandLineCommands ()
+		{
+			//NOTE: this command needs to return a string that is valid to pass to DateTime.Parse()
+			//	The <Touch> MSBuild task requires this: https://docs.microsoft.com/en-us/visualstudio/msbuild/touch-task
+			return "log -1 --format=%cd --date=format-local:\"%Y/%m/%d %H:%M:%S\"";
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			if (string.IsNullOrEmpty (singleLine))
+				return;
+			Time = singleLine;
+		}
+	}
+}
