4bb7ebdbfdeb8d6304a0a04fd39d5e4e417350b5
Dean Ellis <dellis1972@googlemail.com>
2017-08-14 17:33:28 +0100

    [Xamarin.Android.Build.Tasks] _CleanGeneratedDebuggingFiles removes *.pdb (#733)
    Fixes: https://bugzilla.xamarin.com/show_bug.cgi?id=58646
    
    Rather than just blindly delete ALL the debug files in the
    `$(OutputPath)` we should be a bit more selective.
    
    So instead we add the generated files to the file
    
    	$(IntermediateOutputPath)$(CleanFile)
    
    so that they can be cleaned up automatically using the in-build
    xbuild/msbuild Clean systems.

--- src/Xamarin.Android.Build.Tasks/Tasks/ConvertDebuggingFiles.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/ConvertDebuggingFiles.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Pdb2Mdb;
@@ -12,11 +13,15 @@ namespace Xamarin.Android.Tasks
 		[Required]
 		public ITaskItem[] Files { get; set; }
 
+		[Output]
+		public ITaskItem[] ConvertedFiles { get; set; }
+
 		public override bool Execute ()
 		{
 			Log.LogDebugMessage ("ConvertDebuggingFiles Task");
 			Log.LogDebugMessage ("  InputFiles: {0}", Files);
 
+			var convertedFiles = new List<ITaskItem> ();
 			foreach (var file in Files) {
 				var pdb = file.ToString ();
 
@@ -26,11 +31,13 @@ namespace Xamarin.Android.Tasks
 				try {
 					MonoAndroidHelper.SetWriteable (pdb);
 					Converter.Convert (Path.ChangeExtension (pdb, ".dll"));
+					convertedFiles.Add (new TaskItem (Path.ChangeExtension (pdb, ".dll")));
 				} catch (Exception ex) {
 					Log.LogWarningFromException (ex, true);
 				}
 			}
-
+			ConvertedFiles = convertedFiles.ToArray ();
+			Log.LogDebugTaskItems ("[Output] ConvertedFiles:", ConvertedFiles);
 			return true;
 		}
 	}
