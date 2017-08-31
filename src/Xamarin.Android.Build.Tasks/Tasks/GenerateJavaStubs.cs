dotnet/android

commit 9d131af4f8f51ce4104163c1a3b620b374d21900

[Xamarin.Android.Build.Tasks] Add `$(AndroidErrorOnCustomJavaObject)` (#797)
Fixes:  https://bugzilla.xamarin.com/show_bug.cgi?id=56819

Bumps to Java.Interop/master/ab3c2b26.

Nothing *prevents* "anybody" from custom implementing `IJavaObject`:

	class MyBadClass : Android.Runtime.IJavaObject {
	    public IntPtr Handle {
	        get {return IntPtr.Zero;}
	    }

	    public void Dispose () {}
	}

The problem is that the above doesn't actually work: the
`IJavaObject.Handle` value contains the JNI Object Reference to pass
into JNI. The above code will thus result in always passing `null`
into Java code, which could result in a `NullPointerException`, but
will always result in *not* doing what was intended.

While it is *theoretically* possible to manually implement
`IJavaObject`, it's not something *I* would want to contemplate, nor
is it for the faint of heart, so long ago we decided to emit a warning
if we encounter a type which:

 1. Implements `Android.Runtime.IJavaObject`, and
 2. Does *not* also inherit `Java.Lang.Object` or
    `Java.Lang.Throwable`.

As such, the above `MyBadClass` elicits the warning:

	Type 'MyBadClass' implements Android.Runtime.IJavaObject but does not inherit from Java.Lang.Object. It is not supported.

This is all well and good, but (effectively) *nobody* reads warnings,
*especially* since our toolchain emits so many warnings that enabling
`/warnaserror` is for the truly foolhardy, so *lots* of warnings is,
unfortunately, normal.

Which brings us to Bug #56819: Could we make this an error?

Answer: Of course we can. That said, I have no idea how much existing
code this will *break* if we turned it into an error. That might be
good and useful, but if there's no way to *disable* the error,
existing apps which (appear to) work will no longer build.

That's not desirable.

Add a new `$(AndroidErrorOnCustomJavaObject)` MSBuild property to

src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaStubs.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Microsoft.Build.Framework;
@@ -51,6 +52,8 @@ namespace Xamarin.Android.Tasks
 
 		public bool UseSharedRuntime { get; set; }
 
+		public bool ErrorOnCustomJavaObject { get; set; }
+
 		[Required]
 		public string ResourceDirectory { get; set; }
 
@@ -69,6 +72,7 @@ namespace Xamarin.Android.Tasks
 			Log.LogDebugMessage ("  PackageName: {0}", PackageName);
 			Log.LogDebugMessage ("  AndroidSdkDir: {0}", AndroidSdkDir);
 			Log.LogDebugMessage ("  AndroidSdkPlatform: {0}", AndroidSdkPlatform);
+			Log.LogDebugMessage ($"  {nameof (ErrorOnCustomJavaObject)}: {ErrorOnCustomJavaObject}");
 			Log.LogDebugMessage ("  OutputDirectory: {0}", OutputDirectory);
 			Log.LogDebugMessage ("  MergedAndroidManifestOutput: {0}", MergedAndroidManifestOutput);
 			Log.LogDebugMessage ("  UseSharedRuntime: {0}", UseSharedRuntime);
@@ -83,7 +87,7 @@ namespace Xamarin.Android.Tasks
 			try {
 				// We're going to do 3 steps here instead of separate tasks so
 				// we can share the list of JLO TypeDefinitions between them
-				using (var res = new DirectoryAssemblyResolver (Log.LogWarning, loadDebugSymbols: true)) {
+				using (var res = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: true)) {
 					Run (res);
 				}
 			}
@@ -117,9 +121,12 @@ namespace Xamarin.Android.Tasks
 			var fxAdditions = MonoAndroidHelper.GetFrameworkAssembliesToTreatAsUserAssemblies (ResolvedAssemblies)
 				.Where (a => assemblies.All (x => Path.GetFileName (x) != Path.GetFileName (a)));
 			assemblies = assemblies.Concat (fxAdditions).ToList ();
-			
+
 			// Step 1 - Find all the JLO types
-			var all_java_types = JavaTypeScanner.GetJavaTypes (assemblies, res, Log.LogWarning);
+			var scanner = new JavaTypeScanner (this.CreateTaskLogger ()) {
+				ErrorOnCustomJavaObject     = ErrorOnCustomJavaObject,
+			};
+			var all_java_types = scanner.GetJavaTypes (assemblies, res);
 
 			WriteTypeMappings (all_java_types);
 

src/Xamarin.Android.Build.Tasks/Tasks/AsyncTask.cs
@@ -78,27 +78,27 @@ namespace Xamarin.Android.Tasks
 				LogDebugMessage ("    {0}", item.ItemSpec);
 		}
 
-		protected void LogMessage (string message)
+		public void LogMessage (string message)
 		{
 			LogMessage (message, importance: MessageImportance.Normal);
 		}
 
-		protected void LogMessage (string message, params object[] messageArgs)
+		public void LogMessage (string message, params object[] messageArgs)
 		{
 			LogMessage (string.Format (message, messageArgs));
 		}
 
-		protected void LogDebugMessage (string message)
+		public void LogDebugMessage (string message)
 		{
 			LogMessage (message , importance: MessageImportance.Low);
 		}
 
-		protected void LogDebugMessage (string message, params object[] messageArgs)
+		public void LogDebugMessage (string message, params object[] messageArgs)
 		{
 			LogMessage (string.Format (message, messageArgs), importance: MessageImportance.Low);
 		}
 
-		protected void LogMessage (string message, MessageImportance importance = MessageImportance.Normal)
+		public void LogMessage (string message, MessageImportance importance = MessageImportance.Normal)
 		{
 			if (UIThreadId == Thread.CurrentThread.ManagedThreadId) {
 				#pragma warning disable 618
@@ -121,27 +121,27 @@ namespace Xamarin.Android.Tasks
 			}
 		}
 
-		protected void LogError (string message)
+		public void LogError (string message)
 		{
 			LogError (code: null, message: message, file: null, lineNumber: 0);
 		}
 
-		protected void LogError (string message, params object[] messageArgs)
+		public void LogError (string message, params object[] messageArgs)
 		{
 			LogError (code: null, message: string.Format (message, messageArgs));
 		}
 
-		protected void LogCodedError (string code, string message)
+		public void LogCodedError (string code, string message)
 		{
 			LogError (code: code, message: message, file: null, lineNumber: 0);
 		}
 
-		protected void LogCodedError (string code, string message, params object[] messageArgs)
+		public void LogCodedError (string code, string message, params object[] messageArgs)
 		{
 			LogError (code: code, message: string.Format (message, messageArgs), file: null, lineNumber: 0);
 		}
 
-		protected void LogError (string code, string message, string file = null, int lineNumber = 0)
+		public void LogError (string code, string message, string file = null, int lineNumber = 0)
 		{
 			if (UIThreadId == Thread.CurrentThread.ManagedThreadId) {
 				#pragma warning disable 618
@@ -180,12 +180,12 @@ namespace Xamarin.Android.Tasks
 			}
 		}
 
-		protected void LogWarning (string message, params object[] messageArgs)
+		public void LogWarning (string message, params object[] messageArgs)
 		{
 			LogWarning (string.Format (message, messageArgs));
 		}
 
-		protected void LogWarning (string message)
+		public void LogWarning (string message)
 		{
 			if (UIThreadId == Thread.CurrentThread.ManagedThreadId) {
 				#pragma warning disable 618

src/Xamarin.Android.Build.Tasks/Tasks/BuildApk.cs
@@ -522,7 +522,7 @@ namespace Xamarin.Android.Tasks
 		{
 			int count = 0;
 			var abis = supportedAbis.Split (new char[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
-			using (var res = new DirectoryAssemblyResolver (Console.WriteLine, loadDebugSymbols: false)) {
+			using (var res = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false)) {
 			foreach (var assembly in EmbeddedNativeLibraryAssemblies)
 				res.Load (assembly.ItemSpec);
 				foreach (var assemblyPath in EmbeddedNativeLibraryAssemblies) {

src/Xamarin.Android.Build.Tasks/Tasks/CheckTargetFrameworks.cs
@@ -50,7 +50,7 @@ namespace Xamarin.Android.Tasks
 			Log.LogDebugMessage ("  ProjectFile: {0}", ProjectFile);
 			Log.LogDebugTaskItems ("  ResolvedUserAssemblies: {0}", ResolvedAssemblies);
 
-			using (var res = new DirectoryAssemblyResolver (Log.LogWarning, loadDebugSymbols: false)) {
+			using (var res = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false)) {
 				foreach (var assembly in ResolvedAssemblies) {
 					res.Load (Path.GetFullPath (assembly.ItemSpec));
 					var apiLevel = ExtractApiLevel (res, assembly);

src/Xamarin.Android.Build.Tasks/Tasks/GenerateResourceDesigner.cs
@@ -106,7 +106,7 @@ namespace Xamarin.Android.Tasks
 			var assemblyNames = new List<string> ();
 			if (IsApplication && References != null && References.Any ()) {
 				// FIXME: should this be unified to some better code with ResolveLibraryProjectImports?
-				using (var resolver = new DirectoryAssemblyResolver (Log.LogWarning, loadDebugSymbols: false)) {
+				using (var resolver = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false)) {
 					foreach (var assemblyName in References) {
 						var suffix = assemblyName.ItemSpec.EndsWith (".dll") ? String.Empty : ".dll";
 						string hintPath = assemblyName.GetMetadata ("HintPath").Replace (Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);

src/Xamarin.Android.Build.Tasks/Tasks/GetAdditionalResourcesFromAssemblies.cs
@@ -394,7 +394,7 @@ namespace Xamarin.Android.Tasks {
 				? CachePath
 				: Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData), CacheBaseDir);
 
-				using (var resolver = new DirectoryAssemblyResolver (LogWarning, loadDebugSymbols: false)) {
+				using (var resolver = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false)) {
 					foreach (var assemblyItem in Assemblies) {
 						string fullPath = Path.GetFullPath (assemblyItem.ItemSpec);
 						if (assemblies.Contains (fullPath)) {

src/Xamarin.Android.Build.Tasks/Tasks/LinkAssemblies.cs
@@ -85,7 +85,7 @@ namespace Xamarin.Android.Tasks
 			var rp = new ReaderParameters {
 				InMemory    = true,
 			};
-			using (var res = new DirectoryAssemblyResolver (Log.LogWarning, loadDebugSymbols: false, loadReaderParameters: rp)) {
+			using (var res = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false, loadReaderParameters: rp)) {
 				return Execute (res);
 			}
 		}

src/Xamarin.Android.Build.Tasks/Tasks/ResolveAssemblies.cs
@@ -45,7 +45,7 @@ namespace Xamarin.Android.Tasks
 
 		public override bool Execute ()
 		{
-			using (var resolver = new DirectoryAssemblyResolver (Log.LogWarning, loadDebugSymbols: false)) {
+			using (var resolver = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false)) {
 				return Execute (resolver);
 			}
 		}

commit 170f859d5050eba72fcfb73226f1396b23e4287b

[Xamarin.Android.Build.Tasks] GenerateJavaStubs Rebuilds
Commit 9d131af4 introduced a [unit test failure][m573] on
xamarin-android/master:

[m573]: https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android/573/

	Xamarin.Android.Build.Tests.BuildTest.XA4212
	Expected: String containing "warning XA4"
	 But was: <log file contents...>

Aside: The XA4212 test creates a project with a bad `IJavaObject`
type, builds the project -- which triggers an XA4212 error, which *is*
observed in the unit test -- then *rebuilds* the project, but this
time with `$(AndroidErrorOnCustomJavaObject)`=False.

The intention is that the second build should instead elicit an XA4212
*warning*, but otherwise continue just fine.

What's interesting about the log file contents is that it shows that
the `<GenerateJavaStubs/>` task isn't executed *at all*, but it's the
`<GenerateJavaStubs/>` task which is supposed to emit the warning!

	Skipping target "_GenerateJavaStubs" because its outputs are up-to-date.

Update the `<GenerateJavaStubs/>` task so that if it errors out, it
*removes* any `<GenerateJavaStubs/>` output files. This will ensure
that on subsequent builds, the `_GenerateJavaStubs` target won't be
inadvertently skipped.

(This issue *should* have been caught during the
[**macOS+xbuild PR builder** build for PR #797][pr1491]. It *wasn't*,
because the `Xamarin.Android.Build.Tests` tests weren't reported. For
example, PR #1491 ran 869 tests, while PR #1492 ran 1085! I'm not sure
what can be done to better detect and prevent this in the future.)

[pr1491]: https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-pr-builder/1491/

src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaStubs.cs
@@ -97,6 +97,15 @@ namespace Xamarin.Android.Tasks
 					Log.LogMessage (e.ToString ());
 			}
 
+			if (Log.HasLoggedErrors) {
+				// Ensure that on a rebuild, we don't *skip* the `_GenerateJavaStubs` target,
+				// by ensuring that the target outputs have been deleted.
+				Files.DeleteFile (MergedAndroidManifestOutput, Log);
+				Files.DeleteFile (AcwMapFile, Log);
+				Files.DeleteFile (Path.Combine (OutputDirectory, "typemap.jm"), Log);
+				Files.DeleteFile (Path.Combine (OutputDirectory, "typemap.mj"), Log);
+			}
+
 			return !Log.HasLoggedErrors;
 		}
 