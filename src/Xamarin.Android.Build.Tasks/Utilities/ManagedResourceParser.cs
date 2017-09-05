168cb0ba6457a08efb4bf59357ee99a794346d96
Dean Ellis <dellis1972@googlemail.com>
2017-09-05 15:36:49 +0100

    Moved over to using an XmlReader based parser

--- src/Xamarin.Android.Build.Tasks/Utilities/ManagedResourceParser.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/ManagedResourceParser.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Xml.Linq;
+using System.Xml;
 
 namespace Xamarin.Android.Tasks
 {
 	class ManagedResourceParser : ResourceParser
 	{
 		CodeTypeDeclaration resources;
-		CodeTypeDeclaration layout, ids, drawable, strings, colors, dimension, raw, animation, attrib, boolean, ints, styleable, style;
+		CodeTypeDeclaration layout, ids, drawable, strings, colors, dimension, raw, animation, attrib, boolean, ints, styleable, style, arrays;
 		Dictionary<string, string> map;
 
-		void SortMembers(CodeTypeDeclaration decl)
+		void SortMembers (CodeTypeDeclaration decl)
 		{
 			CodeTypeMember [] members = new CodeTypeMember [decl.Members.Count];
 			decl.Members.CopyTo (members, 0);
@@ -31,6 +31,7 @@ namespace Xamarin.Android.Tasks
 			map = resourceMap ?? new Dictionary<string, string> ();
 			resources = CreateResourceClass ();
 			animation = CreateClass ("Animation");
+			arrays = CreateClass ("Array");
 			attrib = CreateClass ("Attribute");
 			boolean = CreateClass ("Boolean");
 			layout = CreateClass ("Layout");
@@ -60,6 +61,7 @@ namespace Xamarin.Android.Tasks
 			SortMembers (animation);
 			SortMembers (ids);
 			SortMembers (attrib);
+			SortMembers (arrays);
 			SortMembers (boolean);
 			SortMembers (colors);
 			SortMembers (dimension);
@@ -74,6 +76,8 @@ namespace Xamarin.Android.Tasks
 
 			if (animation.Members.Count > 1)
 				resources.Members.Add (animation);
+			if (arrays.Members.Count > 1)
+				resources.Members.Add (arrays);
 			if (attrib.Members.Count > 1)
 				resources.Members.Add (attrib);
 			if (boolean.Members.Count > 1)
@@ -112,14 +116,14 @@ namespace Xamarin.Android.Tasks
 			var path = Directory.GetParent (file).Name;
 			var ext = Path.GetExtension (file);
 			switch (ext) {
-				case ".xml":
-				case ".axml":
-					if (string.Compare (path, "raw", StringComparison.OrdinalIgnoreCase) == 0)
-						goto default;
-					ProcessXmlFile (file);
-					break;
-				default:
-					break;
+			case ".xml":
+			case ".axml":
+				if (string.Compare (path, "raw", StringComparison.OrdinalIgnoreCase) == 0)
+					goto default;
+				ProcessXmlFile (file);
+				break;
+			default:
+				break;
 			}
 			CreateResourceField (path, fileName);
 		}
@@ -143,7 +147,7 @@ namespace Xamarin.Android.Tasks
 
 		CodeTypeDeclaration CreateClass (string type)
 		{
-			var t = new CodeTypeDeclaration (JavaResourceParser.GetNestedTypeName (type)) {
+			var t = new CodeTypeDeclaration (ResourceParser.GetNestedTypeName (type)) {
 				IsPartial = true,
 				TypeAttributes = TypeAttributes.Public,
 			};
@@ -172,8 +176,8 @@ namespace Xamarin.Android.Tasks
 				Attributes = MemberAttributes.Static | MemberAttributes.Public,
 				InitExpression = new CodePrimitiveExpression (0),
 				Comments = {
-					new CodeCommentStatement ("aapt resource value: 0"),
-				},
+						new CodeCommentStatement ("aapt resource value: 0"),
+					},
 			};
 			parentType.Members.Add (f);
 		}
@@ -183,7 +187,7 @@ namespace Xamarin.Android.Tasks
 			string mappedName = GetResourceName (parentType.Name, name, map);
 			if (parentType.Members.OfType<CodeTypeMember> ().Any (x => string.Compare (x.Name, mappedName, StringComparison.OrdinalIgnoreCase) == 0))
 				return;
-			var f = new CodeMemberField (typeof (int[]), name) {
+			var f = new CodeMemberField (typeof (int []), name) {
 				// pity I can't make the member readonly...
 				Attributes = MemberAttributes.Public | MemberAttributes.Static,
 			};
@@ -191,20 +195,20 @@ namespace Xamarin.Android.Tasks
 			if (c == null) {
 				f.InitExpression = c = new CodeArrayCreateExpression (typeof (int []));
 			}
-			for (int i = 0; i < count;i++)
+			for (int i = 0; i < count; i++)
 				c.Initializers.Add (new CodePrimitiveExpression (0));
-			
+
 			parentType.Members.Add (f);
 		}
 
 		HashSet<string> resourceNamesToUseDirectly = new HashSet<string> () {
-			"integer-array",
-			"string-array",
-			"declare-styleable",
-			"add-resource",
-		};
+				"integer-array",
+				"string-array",
+				"declare-styleable",
+				"add-resource",
+			};
 
-		void CreateResourceField (string root, string fieldName, XElement element = null)
+		void CreateResourceField (string root, string fieldName, XmlReader element = null)
 		{
 			var i = root.IndexOf ('-');
 			var item = i < 0 ? root : root.Substring (0, i);
@@ -243,65 +247,98 @@ namespace Xamarin.Android.Tasks
 				break;
 			case "enum":
 			case "flag":
+			case "id":
 				CreateIntField (ids, fieldName);
 				break;
-			case "configVarying":
 			case "integer-array":
 			case "string-array":
+				CreateIntField (arrays, fieldName);
+				break;
+			case "configVarying":
 			case "add-resource":
 			case "declare-styleable":
 				ProcessStyleable (element);
 				break;
 			case "style":
-				// special case Style 
-				//ProcessStyle (element);
 				CreateIntField (style, fieldName.Replace (".", "_"));
 				break;
 			default:
-				//Log.LogDebugMessage ($"default {root} => {fieldName}");
 				break;
 			}
 		}
 
-		void ProcessStyleable (XElement element)
+		void ProcessStyleable (XmlReader reader)
 		{
-			var topName = element.Attribute ("name").Value;
-			var items = element.Descendants ().Where (x => x.Name.LocalName == "attr");
-			CreateIntArrayField (styleable, topName, items.Count ());
-			foreach (var item in items) {
-				if (item.Name.LocalName == "attr") {
-					CreateIntField (styleable, $"{topName}_{item.Attribute ("name").Value.Replace (":", "_")}");
+			string topName = null;
+			int fieldCount = 0;
+			while (reader.Read ()) {
+				if (reader.NodeType == XmlNodeType.Whitespace || reader.NodeType == XmlNodeType.Comment)
+					continue;
+				string name = null;
+				if (string.IsNullOrEmpty (topName)) {
+					if (reader.HasAttributes) {
+						while (reader.MoveToNextAttribute ()) {
+							if (reader.Name.Replace ("android:", "") == "name")
+								topName = reader.Value;
+						}
+					}
+				}
+				if (!reader.IsStartElement () || reader.LocalName == "declare-styleable")
+					continue;
+				if (reader.HasAttributes) {
+					while (reader.MoveToNextAttribute ()) {
+						if (reader.Name.Replace ("android:", "") == "name")
+							name = reader.Value;
+					}
+				}
+				reader.MoveToElement ();
+				if (reader.LocalName == "attr") {
+					CreateIntField (styleable, $"{topName}_{name.Replace (":", "_")}");
+					if (!name.StartsWith ("android:", StringComparison.OrdinalIgnoreCase))
+						CreateIntField (attrib, name);
+					fieldCount++;
+				} else {
+					if (name != null)
+						CreateIntField (ids, $"{name.Replace (":", "_")}");
 				}
 			}
+			CreateIntArrayField (styleable, topName, fieldCount);
 		}
 
-		void ProcessXmlFile (string file) {
-			var doc = XDocument.Load (file);
-			var ns = XNamespace.Get ("http://schemas.android.com/apk/res/android");
-			var nameAttributes = doc.Descendants ().Attributes (ns + "name")
-					.Concat (doc.Descendants ().Attributes ("name"));
-			foreach (var attr in nameAttributes) {
-				// skip android: prefixed items
-				if (attr.Value.Contains ("android:"))
-					continue;
-				if (attr.Parent.Name.LocalName == "item") {
-					if (attr.Parent.Attribute ("type") != null)
-						CreateResourceField (attr.Parent.Attribute ("type").Value, attr.Name.LocalName, attr.Parent);
-					else {
-						var f = attr.Parent.Parent.Name;
-						Log.LogDebugMessage ($"Item {attr.Name} {attr.Value} {attr.Parent.Attribute ("type")?.Value ?? "noval"} {f}");
+		void ProcessXmlFile (string file)
+		{
+			using (var reader = XmlReader.Create (file)) {
+				while (reader.Read ()) {
+					if (reader.NodeType == XmlNodeType.Whitespace || reader.NodeType == XmlNodeType.Comment)
+						continue;
+					if (reader.IsStartElement ()) {
+						var elementName = reader.Name;
+						if (reader.HasAttributes) {
+							string name = null;
+							string type = null;
+							string id = null;
+							while (reader.MoveToNextAttribute ()) {
+								if (reader.LocalName == "name")
+									name = reader.Value;
+								if (reader.LocalName == "type")
+									type = reader.Value;
+								if (reader.LocalName == "id")
+									id = reader.Value.Replace ("@+id/", "").Replace ("@id/", ""); ;
+							}
+							if (name?.Contains ("android:") ?? false)
+								continue;
+							if (id?.Contains ("android:") ?? false)
+								continue;
+							// Move the reader back to the element node.
+							reader.MoveToElement ();
+							if (!string.IsNullOrEmpty (name))
+								CreateResourceField (type ?? elementName, name, reader.ReadSubtree ());
+							if (!string.IsNullOrEmpty (id)) {
+								CreateIntField (ids, id);
+							}
+						}
 					}
-				} else
-					CreateResourceField (attr.Parent.Name.LocalName, attr.Value, attr.Parent);
-			}
-			var attrs = doc.Descendants ().Attributes (ns + "id")
-					.Concat (doc.Descendants ().Attributes ("id"));
-			foreach (var attr in attrs) {
-				var name = attr.Value.Replace ("@+id/", "").Replace ("@id/", "");
-				// skip android prefixed items
-				if (attr.Value.Contains ("android:"))
-					continue;
-				CreateIntField (ids, name);
+				}
 			}
 		}
 	}
