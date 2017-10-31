1209f1cb36a144f45366d2f1ef20209fd5be3d07
Redth <jondick@gmail.com>
2017-10-30 20:50:41 -0400

    Condense multi task parameters into one itemgroup
    We previously had JavaDocPaths, Java7DocPaths, etc. etc which were all separate task input parameters and itemgroups, but ultimately were concatenated into one single list anyway.
    
    This merges the existing separate itemgroups into one new itemgroup and one single ClassParse task parameter.

--- src/Xamarin.Android.Build.Tasks/Tasks/ClassParse.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/ClassParse.cs
@@ -21,46 +21,21 @@ namespace Xamarin.Android.Tasks
 		[Required]
 		public ITaskItem[] SourceJars { get; set; }
 
-		public ITaskItem[] JavaDocPaths { get; set; }
-
-		public ITaskItem[] Java7DocPaths { get; set; }
-
-		public ITaskItem[] Java8DocPaths { get; set; }
-
-		public ITaskItem[] DroidDocPaths { get; set; }
-
-		public ITaskItem [] DroidDoc2Paths { get; set; }
-
-		public ITaskItem [] ApiXmlDocPaths { get; set; }
-
-		public ITaskItem [] JavaDocs { get; set; }
-
-		public IEnumerable<ITaskItem> DocsPaths {
-			get {
-				Func<ITaskItem[],IEnumerable<ITaskItem>> f = l => l ?? Enumerable.Empty<ITaskItem> ();
-				return f (JavaDocPaths).Concat (f (Java7DocPaths)).Concat (f (Java8DocPaths)).Concat (f (DroidDocPaths)).Concat (f (DroidDoc2Paths)).Concat (f (ApiXmlDocPaths)).Concat (f (JavaDocs));
-			}
-		}
+		public ITaskItem [] DocumentationPaths { get; set; }
 
 		public override bool Execute ()
 		{
 			Log.LogDebugMessage ("ClassParse Task");
 			Log.LogDebugMessage ("  OutputFile: {0}", OutputFile);
 			Log.LogTaskItems ("  SourceJars: ", SourceJars);
-			Log.LogTaskItems ("  JavaDocPaths: ", JavaDocPaths);
-			Log.LogTaskItems ("  Java7DocPaths: ", Java7DocPaths);
-			Log.LogTaskItems ("  Java8DocPaths: ", Java8DocPaths);
-			Log.LogTaskItems ("  DroidDocPaths: ", DroidDocPaths);
-			Log.LogTaskItems ("  DroidDoc2Paths: ", DroidDoc2Paths);
-			Log.LogTaskItems ("  ApiXmlDocPaths: ", ApiXmlDocPaths);
-			Log.LogTaskItems ("  JavaDocs: ", JavaDocs);
+			Log.LogTaskItems ("  DocumentationPaths: ", DocumentationPaths);
 
 			using (var output = new StreamWriter (OutputFile, append: false, 
 						encoding: new UTF8Encoding (encoderShouldEmitUTF8Identifier: false))) {
 				Bytecode.Log.OnLog = LogEventHandler;
 				var classPath = new Bytecode.ClassPath () {
 					ApiSource = "class-parse",
-					DocumentationPaths = (DocsPaths ?? Enumerable.Empty<ITaskItem> ()).Select(x => x.ItemSpec)
+					DocumentationPaths = (DocumentationPaths ?? Enumerable.Empty<ITaskItem> ()).Select(x => x.ItemSpec)
 				};
 				foreach (var jar in SourceJars) {
 					if (Bytecode.ClassPath.IsJarFile (jar.ItemSpec)) {
