dotnet/android

commit e470b7c4c05f6e539027ca0db0d1a18018678b0b

Reduce Android.* dependencies from this not-only-about-Android repo.

external/Java.Interop/src/Java.Interop.Tools.TypeNameMappings/Java.Interop.Tools.TypeNameMappings/JavaNativeTypeManager.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
-using Java.Interop;
+using Android.Runtime;
 
 #if HAVE_CECIL
 using Mono.Cecil;
 using Java.Interop.Tools.Cecil;
-using Android.Runtime;
 using Java.Interop.Tools.JavaCallableWrappers;
 #endif  // HAVE_CECIL
 
@@ -262,35 +261,8 @@ namespace Java.Interop.Tools.TypeNameMappings
 		// Keep in sync with ToJniNameFromAttributes(TypeDefinition)
 		public static string ToJniNameFromAttributes (Type type)
 		{
-			var ras = (Android.Runtime.RegisterAttribute[]) type.GetCustomAttributes (typeof (Android.Runtime.RegisterAttribute), false);
-			if (ras.Length > 0 && !string.IsNullOrEmpty (ras [0].Name))
-				return ras [0].Name.Replace ('.', '/');
-
-			var aas = (Android.App.ActivityAttribute[]) type.GetCustomAttributes (typeof (Android.App.ActivityAttribute), false);
-			if (aas.Length > 0 && !string.IsNullOrEmpty (aas [0].Name))
-				return aas [0].Name.Replace ('.', '/');
-
-			var apps = (Android.App.ApplicationAttribute[]) type.GetCustomAttributes (typeof (Android.App.ApplicationAttribute), false);
-			if (apps.Length > 0 && !string.IsNullOrEmpty (apps [0].Name))
-				return apps [0].Name.Replace ('.', '/');
-
-			var sas = (Android.App.ServiceAttribute[]) type.GetCustomAttributes (typeof (Android.App.ServiceAttribute), false);
-			if (sas.Length > 0 && !string.IsNullOrEmpty (sas [0].Name))
-				return sas [0].Name.Replace ('.', '/');
-
-			var bras = (Android.Content.BroadcastReceiverAttribute[]) type.GetCustomAttributes (typeof (Android.Content.BroadcastReceiverAttribute), false);
-			if (bras.Length > 0 && !string.IsNullOrEmpty (bras [0].Name))
-				return bras [0].Name.Replace ('.', '/');
-
-			var cpas = (Android.Content.ContentProviderAttribute[]) type.GetCustomAttributes (typeof (Android.Content.ContentProviderAttribute), false);
-			if (cpas.Length > 0 && !string.IsNullOrEmpty (cpas [0].Name))
-				return cpas [0].Name.Replace ('.', '/');
-
-			var ias = (Android.App.InstrumentationAttribute[])type.GetCustomAttributes (typeof (Android.App.InstrumentationAttribute), false);
-			if (ias.Length > 0 && !string.IsNullOrEmpty (ias [0].Name))
-				return ias [0].Name.Replace ('.', '/');
-
-			return null;
+			var a = type.GetCustomAttributes ().OfType<IJniNameProviderAttribute> ().FirstOrDefault (j => !string.IsNullOrEmpty (j.Name));
+			return a == null ? null : a.Name.Replace ('.', '/');
 		}
 
 		/*
@@ -478,41 +450,25 @@ namespace Java.Interop.Tools.TypeNameMappings
 
 		static string ToJniNameFromAttributes (TypeDefinition type)
 		{
-#region CustomAttribute alternate name support
-			// ToJniName(Type) doesn't do this, as it's instead done in
-			// JNIEnv.FindClass(Type) for perf reasons.
-			var attr = type.GetCustomAttributes ("Android.Runtime.RegisterAttribute").SingleOrDefault ();
-			if (attr != null) {
-				string name = (string) attr.ConstructorArguments [0].Value;
+			#region CustomAttribute alternate name support
+			var attrs = type.CustomAttributes.Where (a => a.AttributeType.Resolve ().Interfaces.Any (it => it.InterfaceType.FullName == typeof (IJniNameProviderAttribute).FullName));
+			return attrs.Select (attr => {
+				var ap = attr.Properties.FirstOrDefault (p => p.Name == "Name");
+				string name = null;
+				if (ap.Name == null) {
+					var ca = attr.ConstructorArguments.FirstOrDefault ();
+					if (ca.Type == null || ca.Type.FullName != "System.String")
+						return null;
+					name = (string) ca.Value;
+				} else
+					name = (string) ap.Argument.Value;
 				if (!string.IsNullOrEmpty (name))
 					return name.Replace ('.', '/');
-			}
-			foreach (var attributeType in new []{
-					"Android.App.ActivityAttribute",
-					"Android.App.ApplicationAttribute",
-					"Android.App.InstrumentationAttribute",
-					"Android.App.ServiceAttribute",
-					"Android.Content.BroadcastReceiverAttribute",
-					"Android.Content.ContentProviderAttribute",
-					}) {
-				attr = type.GetCustomAttributes (attributeType).SingleOrDefault ();
-				if (attr != null) {
-					var ap = attr.Properties.FirstOrDefault (p => p.Name == "Name");
-					string name = null;
-					if (ap.Name == null) {
-						var ca = attr.ConstructorArguments.FirstOrDefault ();
-						if (ca.Type == null || ca.Type.FullName != "System.String")
-							continue;
-						name = (string) ca.Value;
-					}
-					else
-						name = (string) ap.Argument.Value;
-					if (!string.IsNullOrEmpty (name))
-						return name.Replace ('.', '/');
-				}
-			}
-#endregion
-			return null;
+				else
+					return null;
+				})
+				.FirstOrDefault (s => s != null);
+			#endregion
 		}
 
 		public static int GetArrayInfo (Mono.Cecil.TypeReference type, out Mono.Cecil.TypeReference elementType)

external/Java.Interop/src/Java.Interop.NamingCustomAttributes/Android.App/InstrumentationAttribute.cs
@@ -1,8 +1,0 @@
-using System;
-
-namespace Android.App {
-	sealed partial class InstrumentationAttribute : Attribute {
-		public string Name { get; set; }
-	}
-}
-

external/Java.Interop/src/Java.Interop.NamingCustomAttributes/Android.Content/BroadcastReceiverAttribute.cs
@@ -1,8 +1,0 @@
-using System;
-
-namespace Android.Content {
-
-	partial class BroadcastReceiverAttribute : Attribute {
-		public string Name { get; set; }
-	}
-}

external/Java.Interop/src/Java.Interop.NamingCustomAttributes/Android.Content/ContentProviderAttribute.cs
@@ -1,8 +1,0 @@
-using System;
-
-namespace Android.Content {
-
-	partial class ContentProviderAttribute : Attribute {
-		public string Name { get; set; }
-	}
-}

external/Java.Interop/src/Java.Interop.NamingCustomAttributes/Android.Runtime/RegisterAttribute.cs
@@ -6,7 +6,7 @@ namespace Android.Runtime {
 #if !JCW_ONLY_TYPE_NAMES
 	public
 #endif  // !JCW_ONLY_TYPE_NAMES
-	sealed class RegisterAttribute : Attribute {
+	sealed class RegisterAttribute : Attribute, Java.Interop.IJniNameProviderAttribute {
 
 		string connector;
 		string name;

external/Java.Interop/src/Java.Interop.NamingCustomAttributes/Java.Interop.NamingCustomAttributes.projitems
@@ -9,16 +9,14 @@
     <Import_RootNamespace>Java.Interop.NamingCustomAttributes</Import_RootNamespace>
   </PropertyGroup>
   <ItemGroup>
-    <Compile Include="$(MSBuildThisFileDirectory)Android.Content\BroadcastReceiverAttribute.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)Android.Content\ContentProviderAttribute.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)Android.Runtime\RegisterAttribute.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)Java.Interop\ExportAttribute.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)Java.Interop\ExportFieldAttribute.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)Java.Interop\ExportParameterAttribute.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)Java.Interop\ExportParameterKind.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)Android.App\ActivityAttribute.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)Android.App\ApplicationAttribute.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)Android.App\ServiceAttribute.cs" />
-    <Compile Include="$(MSBuildThisFileDirectory)Android.App\InstrumentationAttribute.cs" />
+    <Compile Include="$(MSBuildThisFileDirectory)Java.Interop\IJniNameProviderAttribute.cs" />
+    <Compile Include="$(MSBuildThisFileDirectory)Android.Runtime\RegisterAttribute.cs" />
+  </ItemGroup>
+  <ItemGroup>
+    <Folder Include="$(MSBuildThisFileDirectory)Android.Runtime\" />
   </ItemGroup>
 </Project>

external/Java.Interop/src/Java.Interop.NamingCustomAttributes/Java.Interop/IJniNameProviderAttribute.cs
@@ -1,0 +1,8 @@
+﻿using System;
+namespace Java.Interop
+{
+	public interface IJniNameProviderAttribute
+	{
+		string Name { get; }
+	}
+}

external/Java.Interop/src/Java.Interop.Tools.JavaCallableWrappers/Java.Interop.Tools.JavaCallableWrappers/JavaCallableWrapperGenerator.cs
@@ -9,9 +9,7 @@ using System.Text.RegularExpressions;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 
-using Android.App;
 using Android.Runtime;
-using Java.Interop;
 
 using Java.Interop.Tools.Cecil;
 using Java.Interop.Tools.Diagnostics;

commit 3012c492632ef5be136b3c8f1e11e11a33a5ca1d

[Java.Interop] Fix JNI signature generation (#249)
Fixes: https://github.com/xamarin/xamarin-android/issues/1149

Commit e470b7c4 broke the use of `[Java.Interop.ExportAttribute]` and
`[Java.Interop.ExportFieldAttribute]`.

Repro:

 1. [Create a class with an `[Export]` or `[ExportField]` method][0]:

		partial class MyParcelable : Java.Lang.Object, IParcelable {
			[ExportField ("CREATOR")]
			public static MyParcelableCreator InitializeCreator ()
			{
				/* ... */
			}
		}

 2. Launch the app, and cause the `MyParcelable` Java Callable Wrapper (JCW)
    to be initialized:
    
		// Will eventually cause JCW MyParcelable.class to be init'd
		var whatever = new MyParcelable();

Expected result: It works!

Actual result:

	System.InvalidOperationException: Specified managed method 'InitializeCreator' was not found. Signature: ()Lmd5<<whatever>>/MyParcelableCreator;
	  at Android.Runtime.JNIEnv.RegisterJniNatives (System.IntPtr typeName_ptr, System.Int32 typeName_len, System.IntPtr jniClass, System.IntPtr methods_ptr, System.Int32 methods_len) [0x001d2] in <36a2bb1be1a64b51b22cbef15eee5437>:0 
	  at (wrapper managed-to-native) Java.Interop.NativeMethods.java_interop_jnienv_alloc_object(intptr,intptr&,intptr)
	  at Java.Interop.JniEnvironment+Object.AllocObject (Java.Interop.JniObjectReference type) [0x00027] in <8e80eb3ca41f4c3eb745bd7c73458796>:0 
	  at Java.Interop.JniType.AllocObject () [0x0000c] in <8e80eb3ca41f4c3eb745bd7c73458796>:0 
	  at Java.Interop.JniPeerMembers+JniInstanceMethods.StartCreateInstance (System.String constructorSignature, System.Type declaringType, Java.Interop.JniArgumentValue* parameters) [0x0003e] in <8e80eb3ca41f4c3eb745bd7c73458796>:0 
	  at Java.Lang.Object..ctor () [0x00034] in <36a2bb1be1a64b51b22cbef15eee5437>:0 
	  ...

[0]: https://github.com/xamarin/monodroid-samples/blob/f944f42001fa134915069fa7bbb9cb493bfd446d/ExportAttribute/ExportAttributeTest/MainActivity.cs#L52-L86

external/Java.Interop/src/Java.Interop.Tools.TypeNameMappings/Java.Interop.Tools.TypeNameMappings/JavaNativeTypeManager.cs
@@ -262,8 +262,8 @@ namespace Java.Interop.Tools.TypeNameMappings
 		// Keep in sync with ToJniNameFromAttributes(TypeDefinition)
 		public static string ToJniNameFromAttributes (Type type)
 		{
-			var a = type.GetCustomAttributes ().OfType<IJniNameProviderAttribute> ().FirstOrDefault (j => !string.IsNullOrEmpty (j.Name));
-			return a == null ? null : a.Name.Replace ('.', '/');
+			var aa = (IJniNameProviderAttribute []) type.GetCustomAttributes (typeof (IJniNameProviderAttribute), inherit: false);
+			return aa.Length > 0 && !string.IsNullOrEmpty (aa [0].Name) ? aa [0].Name.Replace ('.', '/') : null;
 		}
 
 		/*