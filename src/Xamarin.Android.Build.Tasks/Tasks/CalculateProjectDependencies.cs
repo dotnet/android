d536c7258071dae427f69667b1d9fe051add2456
Dean Ellis <dellis1972@googlemail.com>
2018-02-10 15:03:29 +0000

    [Xamarin.Android.Build.Tasks] Add GetAndroidDependencies Target
    Fixes #1269
    
    This commit adds the `GetAndroidDependencies` target to the
    `Xamarin.Android.Common.targets`. Its purpose is to examine
    the various settings in the project and report which android
    sdk build-tools, platform-tools etc are required.
    
    `GetAndroidDependencies` will output an @(AndroidDependency)
    with Version metadata Valid component names are platform,
    build-tool, platform-tool and tool.

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/CalculateProjectDependencies.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Xamarin.Android.Tools;
+
+namespace Xamarin.Android.Tasks
+{
+	public class CalculateProjectDependencies : Task
+	{
+		const int DefaultMinSDKVersion = 11;
+
+		[Required]
+		public string TargetFrameworkVersion { get; set; }
+
+		[Required]
+		public ITaskItem ManifestFile { get; set; }
+
+		[Required]
+		public string BuildToolsVersion { get; set; }
+
+		public string PlatformToolsVersion { get; set; }
+
+		public string ToolsVersion { get; set; }
+
+		[Output]
+		public ITaskItem [] Dependencies { get; set; }
+
+		ITaskItem CreateAndroidDependency (string include, string version)
+		{
+			if (string.IsNullOrEmpty (version))
+				return new TaskItem (include);
+
+			return new TaskItem (include, new Dictionary<string, string> {
+				{ "Version", version }
+			});
+		}
+
+		public override bool Execute ()
+		{
+			var dependencies = new List<ITaskItem> ();
+			var targetApiLevel = MonoAndroidHelper.SupportedVersions.GetApiLevelFromFrameworkVersion (TargetFrameworkVersion);
+			var manifest = AndroidAppManifest.Load (ManifestFile.ItemSpec, MonoAndroidHelper.SupportedVersions);
+			var manifestApiLevel = manifest.TargetSdkVersion ?? manifest.MinSdkVersion ?? DefaultMinSDKVersion;
+			dependencies.Add (CreateAndroidDependency ("platform", $"{Math.Max (targetApiLevel.Value, manifestApiLevel)}"));
+			dependencies.Add (CreateAndroidDependency ("build-tool", BuildToolsVersion));
+			if (!string.IsNullOrEmpty (PlatformToolsVersion)) {
+				dependencies.Add (CreateAndroidDependency ("platform-tool", PlatformToolsVersion));
+			}
+			if (!string.IsNullOrEmpty (ToolsVersion)) {
+				dependencies.Add (CreateAndroidDependency ("tool", ToolsVersion));
+			}
+			Dependencies = dependencies.ToArray ();
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
