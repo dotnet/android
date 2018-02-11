dotnet/android

commit 0e1d1c8a96cb45d2c9f86f52f8770c4d8c582fff

[build] Cursory JDK 9 support (#1214)
We have a VSTS+macOS build machine which has JDK 9 as the default JDK,
thus causing *all* xamarin-android builds to fail, as `gradlew` and
JDK 9 don't *directly* mix:

	Executing: ./gradlew assembleDebug --stacktrace
	...
	org.gradle.api.ProjectConfigurationException: A problem occurred configuring project ':library'.
	  ...
	Caused by: org.gradle.internal.event.ListenerNotificationException:
	Failed to notify project evaluation listener.
	  ...
	Caused by: java.lang.NoClassDefFoundError: javax/xml/bind/annotation/XmlSchema
	  ...
	Caused by: java.lang.ClassNotFoundException: javax.xml.bind.annotation.XmlSchema

When using JDK 9, `gradlew` fails because *something* within it
attempts to use the deprecated type
`javax.xml.bind.annotation.XmlSchema`, and JDK 9 doesn't provide
deprecated modules in the default `$CLASSPATH`.

Knowing that the VSTS machine *also* has JDK 8 installed, we've tried
to "filter out" JDK 9 so that it wouldn't be used; see e.g. a3c43584.
Unfortunately that doesn't work, because on macOS the default JDK used
is [always the JDK with the highest version number][macOS-jdk], and
this can't be easily changed system-wide because changing this
behavior requires "removing" a `Info.plist` file to prevent the JDK
from being "seen" by `/usr/bin/java`:

[macOS-jdk]: https://stackoverflow.com/a/44169445

	# Don't use JDK 9 by default on macOS:
	$ cd /Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents
	$ mv Info.plist Info.plist.disabled

(Renaming `Info.plist.disabled` to `Info.plist` will restore JDK 9 as
the default JDK on the machine.)

A "proper" fix would presumably involve using and exporting
`$JAVA_HOME` *everywhere*, which isn't currently the case.
[This is something that *should* be explored.][xa-1213]

[xa-1213]: https://github.com/xamarin/xamarin-android/issues/1213

In the meantime, a question: *Can* we build with JDK 9?

src/Xamarin.Android.Build.Tasks/Tasks/ResolveSdksTask.cs
@@ -116,6 +116,12 @@ namespace Xamarin.Android.Tasks
 		[Output]
 		public bool AndroidUseApkSigner { get; set; }
 
+		[Output]
+		public string JdkVersion { get; set; }
+
+		[Output]
+		public string MinimumRequiredJdkVersion { get; set; }
+
 		static bool             IsWindows = Path.DirectorySeparatorChar == '\\';
 		static readonly string  ZipAlign  = IsWindows ? "zipalign.exe" : "zipalign";
 		static readonly string  Aapt      = IsWindows ? "aapt.exe" : "aapt";
@@ -140,6 +146,7 @@ namespace Xamarin.Android.Tasks
 			Log.LogDebugMessage ("  AndroidSdkBuildToolsVersion: {0}", AndroidSdkBuildToolsVersion);
 			Log.LogDebugMessage ($"  {nameof (AndroidSdkPath)}: {AndroidSdkPath}");
 			Log.LogDebugMessage ($"  {nameof (AndroidNdkPath)}: {AndroidNdkPath}");
+			Log.LogDebugMessage ($"  {nameof (JavaSdkPath)}: {JavaSdkPath}");
 			Log.LogDebugTaskItems ("  ReferenceAssemblyPaths: ", ReferenceAssemblyPaths);
 			Log.LogDebugMessage ("  TargetFrameworkVersion: {0}", TargetFrameworkVersion);
 			Log.LogDebugMessage ("  UseLatestAndroidPlatformSdk: {0}", UseLatestAndroidPlatformSdk);
@@ -280,6 +287,8 @@ namespace Xamarin.Android.Tasks
 			Log.LogDebugMessage ("  AndroidSdkBuildToolsBinPath: {0}", AndroidSdkBuildToolsBinPath);
 			Log.LogDebugMessage ("  AndroidSdkPath: {0}", AndroidSdkPath);
 			Log.LogDebugMessage ("  JavaSdkPath: {0}", JavaSdkPath);
+			Log.LogDebugMessage ("  JdkVersion: {0}", JdkVersion);
+			Log.LogDebugMessage ("  MinimumRequiredJdkVersion: {0}", MinimumRequiredJdkVersion);
 			Log.LogDebugMessage ("  MonoAndroidBinPath: {0}", MonoAndroidBinPath);
 			Log.LogDebugMessage ("  MonoAndroidToolsPath: {0}", MonoAndroidToolsPath);
 			Log.LogDebugMessage ("  TargetFrameworkVersion: {0}", TargetFrameworkVersion);
@@ -320,7 +329,10 @@ namespace Xamarin.Android.Tasks
 			return !Log.HasLoggedErrors;
 		}
 
-		static readonly Regex javaVersionRegex = new Regex (@"""(?<version>[\d\.]+)_\d+""");
+		// `java -version` will produce values such as:
+		//  java version "9.0.4"
+		//  java version "1.8.0_77"
+		static readonly Regex javaVersionRegex = new Regex (@"version ""(?<version>[\d\.]+)(_\d+)?""");
 
 		Version GetJavaVersionForFramework (string targetFrameworkVersion)
 		{
@@ -350,6 +362,8 @@ namespace Xamarin.Android.Tasks
 			Version requiredJavaForBuildTools = GetJavaVersionForBuildTools (buildToolsVersion);
 
 			Version required = requiredJavaForFrameworkVersion > requiredJavaForBuildTools ? requiredJavaForFrameworkVersion : requiredJavaForBuildTools;
+
+			MinimumRequiredJdkVersion = required.ToString ();
 			
 			var sb = new StringBuilder ();
 			
@@ -372,6 +386,7 @@ namespace Xamarin.Android.Tasks
 			var versionNumberMatch = javaVersionRegex.Match (versionInfo);
 			Version versionNumber;
 			if (versionNumberMatch.Success && Version.TryParse (versionNumberMatch.Groups ["version"]?.Value, out versionNumber)) {
+				JdkVersion  = versionNumberMatch.Groups ["version"].Value;
 				Log.LogMessage (MessageImportance.Normal, $"Found Java SDK version {versionNumber}.");
 				if (versionNumber < requiredJavaForFrameworkVersion) {
 					Log.LogError ($"Java SDK {requiredJavaForFrameworkVersion} or above is required when targeting FrameworkVerison {targetFrameworkVersion}.");
@@ -379,8 +394,11 @@ namespace Xamarin.Android.Tasks
 				if (versionNumber < requiredJavaForBuildTools) {
 					Log.LogError ($"Java SDK {requiredJavaForBuildTools} or above is required when using build-tools {buildToolsVersion}.");
 				}
+				if (versionNumber > Version.Parse (LatestSupportedJavaVersion)) {
+					Log.LogWarning ($"JDK Version `{versionNumber}` is later than latest suppored JDK version `{LatestSupportedJavaVersion}`.");
+				}
 			} else
-				Log.LogWarning ($"Failed to get the Java SDK version. Found {versionInfo} but this does not seem to contain a valid version number.");
+				Log.LogWarning ($"Failed to get the Java SDK version as it does not appear to contain a valid version number. `javac -version` returned: ```{versionInfo}```");
 			return !Log.HasLoggedErrors;
 		}
 

build-tools/scripts/Jar.targets
@@ -1,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Target Name="_GetJavacVersions"
+      DependsOnTargets="AndroidPrepareForBuild">
+    <PropertyGroup>
+      <_JavacSourceVersion Condition="$(_JdkVersion.StartsWith ('9'))">1.8</_JavacSourceVersion>
+      <_JavacSourceVersion Condition=" '$(_JavacSourceVersion)' == '' ">1.5</_JavacSourceVersion>
+      <_JavacTargetVersion Condition="$(_JdkVersion.StartsWith ('9'))">1.8</_JavacTargetVersion>
+      <_JavacTargetVersion Condition=" '$(_JavacTargetVersion)' == '' ">1.6</_JavacTargetVersion>
+    </PropertyGroup>
+  </Target>
+  <Target Name="BuildTestJarFile"
+      DependsOnTargets="_GetJavacVersions"
+      Inputs="@(TestJarEntry)"
+      Outputs="%(TestJarEntry.OutputFile)">
+    <ItemGroup>
+      <_JavacSource
+          Include="@(TestJarEntry)"
+      />
+    </ItemGroup>
+    <PropertyGroup>
+      <_Javac>"$(JavaCPath)"</_Javac>
+      <_Jar>"$(JarPath)"</_Jar>
+      <_Targets>-source $(_JavacSourceVersion) -target $(_JavacTargetVersion)</_Targets>
+      <_DestDir>$(IntermediateOutputPath)__CreateTestJarFile-bin</_DestDir>
+      <_AndroidJar>-cp "$(AndroidSdkDirectory)\platforms\android-$(_AndroidApiLevelName)\android.jar"</_AndroidJar>
+    </PropertyGroup>
+    <MakeDir Directories="$(_DestDir)" />
+    <Exec Command="$(_Javac) $(_Targets) -d &quot;$(_DestDir)&quot; $(_AndroidJar) @(_JavacSource->'&quot;%(Identity)&quot;', ' ')" />
+    <Exec
+        Command="$(_Jar) cf &quot;classes.jar&quot; ."
+        WorkingDirectory="$(_DestDir)"
+    />
+    <Copy
+        SourceFiles="$(_DestDir)\classes.jar"
+        DestinationFiles="%(TestJarEntry.OutputFile)"
+    />
+    <RemoveDir Directories="$(_DestDir)" />
+  </Target>
+  <Target Name="CleanTestJarFile">
+    <Delete Files="%(TestJarEntry.OutputFile)" />
+  </Target>
+</Project>

src/Xamarin.Android.Build.Tasks/Tasks/AdjustJavacVersionArguments.cs
@@ -7,10 +7,13 @@ using Xamarin.Android.Tools;
 
 namespace Xamarin.Android.Tasks
 {
-	public class AdjustJavacVersionArguments : ToolTask
+	public class AdjustJavacVersionArguments : Task
 	{
 		[Required]
-		public string ToolPath { get; set; }
+		public string JdkVersion { get; set; }
+
+		[Required]
+		public string DefaultJdkVersion { get; set; }
 
 		public bool EnableProguard { get; set; }
 
@@ -24,17 +27,17 @@ namespace Xamarin.Android.Tasks
 		[Output]
 		public string SourceVersion { get; set; }
 
-		protected override string ToolName {
-			get { return OS.IsWindows ? "javac.exe" : "javac"; }
-		}
-
 		public override bool Execute ()
 		{
-			Log.LogDebugMessage ("ToolPath: {0}", ToolPath);
-			Log.LogDebugMessage ("ToolExe: {0}", ToolExe);
+			Log.LogDebugMessage ($"{nameof (DefaultJdkVersion)}: {DefaultJdkVersion}");
 			Log.LogDebugMessage ("EnableProguard: {0}", EnableProguard);
 			Log.LogDebugMessage ("EnableMultiDex: {0}", EnableMultiDex);
+			Log.LogDebugMessage ($"{nameof (JdkVersion)}: {JdkVersion}");
 			Log.LogDebugMessage ("SkipJavacVersionCheck: {0}", SkipJavacVersionCheck);
+
+			if (JdkVersion.StartsWith ("9", StringComparison.OrdinalIgnoreCase)) {
+				TargetVersion = SourceVersion = DefaultJdkVersion;
+			}
 
 			if (SkipJavacVersionCheck)
 				return true;
@@ -43,34 +46,13 @@ namespace Xamarin.Android.Tasks
 			if (!EnableProguard && !EnableMultiDex)
 				return true;
 
-			var psi = new ProcessStartInfo (Path.Combine (ToolPath, ToolExe ?? ToolName), "-version") {
-				RedirectStandardOutput = true,
-				RedirectStandardError = true,
-				UseShellExecute = false,
-				CreateNoWindow = true,
-				WindowStyle = ProcessWindowStyle.Hidden,
-				};
-			var proc = Process.Start (psi);
-			proc.WaitForExit ();
-			var line = proc.StandardError.ReadLine ();
-			if (!line.StartsWith ("javac "))
-				// otherwise ignore.
-				return true;
-
-			var version = line.Substring (6);
-
-			if (version.StartsWith ("1.8")) {
+			if (JdkVersion.StartsWith ("1.8", StringComparison.OrdinalIgnoreCase)) {
 				TargetVersion = SourceVersion = "1.7";
 				Log.LogDebugMessage ("Javac TargetVersion adjusted to: {0}", TargetVersion);
 				Log.LogDebugMessage ("Javac SourceVersion adjusted to: {0}", SourceVersion);
 			}
 
-			return true;
-		}
-
-		protected override string GenerateFullPathToTool ()
-		{
-			return Path.Combine (ToolPath, ToolExe);
+			return !Log.HasLoggedErrors;
 		}
 	}
 }

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Bindings.targets
@@ -190,6 +190,8 @@ Copyright (C) 2012 Xamarin Inc. All rights reserved.
 		<Output TaskParameter="JavaSdkPath"               PropertyName="JavaSdkDirectory"           Condition="'$(JavaSdkDirectory)' == ''" />
 		<Output TaskParameter="MonoAndroidToolsPath"      PropertyName="MonoAndroidToolsDirectory" />
 		<Output TaskParameter="AndroidSdkBuildToolsBinPath" PropertyName="AndroidSdkBuildToolsBinPath" Condition="'$(AndroidSdkBuildToolsBinPath)' == ''" />
+		<Output TaskParameter="JdkVersion"                PropertyName="_JdkVersion" />
+		<Output TaskParameter="MinimumRequiredJdkVersion" PropertyName="_DefaultJdkVersion" />
 	</ResolveSdks>
     <CreateProperty Value="$(TargetFrameworkIdentifier),Version=$(TargetFrameworkVersion),Profile=$(TargetFrameworkProfile)">
       <Output TaskParameter="Value" PropertyName="TargetFrameworkMoniker"

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -664,6 +664,8 @@ Copyright (C) 2011-2012 Xamarin. All rights reserved.
 		<Output TaskParameter="LintToolPath"              PropertyName="LintToolPath"               Condition="'$(LintToolPath)' == ''" />
 		<Output TaskParameter="ApkSignerToolExe"          PropertyName="ApkSignerToolExe"           Condition="'$(ApkSignerToolExe)' == ''" />
 		<Output TaskParameter="AndroidUseApkSigner"       PropertyName="AndroidUseApkSigner"        Condition="'$(AndroidUseApkSigner)' == ''" />
+		<Output TaskParameter="JdkVersion"                PropertyName="_JdkVersion" />
+		<Output TaskParameter="MinimumRequiredJdkVersion" PropertyName="_DefaultJdkVersion" />
 	</ResolveSdks>
 	<CreateProperty Value="$(TargetFrameworkIdentifier),Version=$(_TargetFrameworkVersion),Profile=$(TargetFrameworkProfile)">
 		<Output TaskParameter="Value" PropertyName="TargetFrameworkMoniker"
@@ -2007,12 +2009,12 @@ because xbuild doesn't support framework reference assemblies.
 <Target Name="_AdjustJavacVersionArguments">
 
 	<AdjustJavacVersionArguments
-		Condition="'$(JavacTargetVersion)'=='' or '$(JavacSourceVersion)' == ''"
-		ToolPath="$(JavacToolPath)"
-	    ToolExe="$(JavacToolExe)"
-	    SkipJavacVersionCheck="$(AndroidSkipJavacVersionCheck)"
-	    EnableProguard="$(AndroidEnableProguard)"
-	    EnableMultiDex="$(AndroidEnableMultiDex)">
+			Condition=" '$(JavacTargetVersion)' == '' or '$(JavacSourceVersion)' == '' "
+			JdkVersion="$(_JdkVersion)"
+			DefaultJdkVersion="$(_DefaultJdkVersion)"
+			SkipJavacVersionCheck="$(AndroidSkipJavacVersionCheck)"
+			EnableProguard="$(AndroidEnableProguard)"
+			EnableMultiDex="$(AndroidEnableMultiDex)">
 	    <Output TaskParameter="TargetVersion" PropertyName="JavacTargetVersion" />
 	    <Output TaskParameter="SourceVersion" PropertyName="JavacSourceVersion" />
 	</AdjustJavacVersionArguments>

tests/CodeGen-Binding/Xamarin.Android.FixJavaAbstractMethod-APIv1Binding/Xamarin.Android.FixJavaAbstractMethod-APIv1Binding.csproj
@@ -61,18 +61,22 @@
     <InputJar Include="Jars\Cursor.jar" />
   </ItemGroup>
   <ItemGroup>
-    <InputJarSource Include="java\test\bindings\Cursor.java" />
+    <TestJarEntry Include="java\test\bindings\Cursor.java">
+      <OutputFile>Jars/Cursor.jar</OutputFile>
+    </TestJarEntry>
   </ItemGroup>
   <Import Project="$(MSBuildExtensionsPath)\Xamarin\Android\Xamarin.Android.Bindings.targets" />
+  <Import Project="..\..\..\build-tools\scripts\Jar.targets" />
   <Import Project="Xamarin.Android.FixJavaAbstractMethod-APIv1Binding.targets" />
   <PropertyGroup>
     <BuildDependsOn>
-      BuildJars;
+      BuildTestJarFile;
       $(BuildDependsOn)
     </BuildDependsOn>
   </PropertyGroup>
   <PropertyGroup>
     <CleanDependsOn>
+      CleanTestJarFile;
       $(CleanDependsOn);
       CleanLocal;
     </CleanDependsOn>

tests/CodeGen-Binding/Xamarin.Android.FixJavaAbstractMethod-APIv1Binding/Xamarin.Android.FixJavaAbstractMethod-APIv1Binding.targets
@@ -1,14 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <Target Name="BuildJars"
-      DependsOnTargets="AndroidPrepareForBuild"
-      Inputs="@(InputJarSource)"
-      Outputs="@(InputJar)">
-    <MakeDir Directories="Jars\classes" />
-    <Exec Command="&quot;$(JavaCPath)&quot; -source 1.5 -target 1.6 -d &quot;Jars\classes&quot; @(InputJarSource -&gt; '%(Identity)', ' ')" />
-    <Exec Command="&quot;$(JarPath)&quot; cf &quot;@(InputJar)&quot; -C &quot;Jars\classes&quot; ." />
-  </Target>
   <Target Name="CleanLocal">
-    <RemoveDir Directories="bin;obj;Jars\classes;@(InputJar)"/>
+    <RemoveDir Directories="bin;obj;"/>
   </Target>
 </Project>

tests/CodeGen-Binding/Xamarin.Android.FixJavaAbstractMethod-APIv2Binding/Xamarin.Android.FixJavaAbstractMethod-APIv2Binding.csproj
@@ -61,18 +61,22 @@
     <InputJar Include="Jars\Cursor.jar" />
   </ItemGroup>
   <ItemGroup>
-    <InputJarSource Include="java\test\bindings\Cursor.java" />
+    <TestJarEntry Include="java\test\bindings\Cursor.java">
+      <OutputFile>Jars/Cursor.jar</OutputFile>
+    </TestJarEntry>
   </ItemGroup>
   <Import Project="$(MSBuildExtensionsPath)\Xamarin\Android\Xamarin.Android.Bindings.targets" />
+  <Import Project="..\..\..\build-tools\scripts\Jar.targets" />
   <Import Project="Xamarin.Android.FixJavaAbstractMethod-APIv2Binding.targets" />
   <PropertyGroup>
     <BuildDependsOn>
-      BuildJars;
+      BuildTestJarFile;
       $(BuildDependsOn)
     </BuildDependsOn>
   </PropertyGroup>
   <PropertyGroup>
     <CleanDependsOn>
+      CleanTestJarFile;
       $(CleanDependsOn);
       CleanLocal;
     </CleanDependsOn>

commit 5d6b4cb113a817d96bc63a1726649566278d1dfc

[Xamarin.Android.Build.Tasks] Improve `javac -version` parsing
The [Ubuntu build][0] is [failing][1]:

	warning : Failed to get the Java SDK version as it does not appear to contain a valid version number.
	`javac -version` returned:
	```openjdk version "1.8.0_03-Ubuntu"
	OpenJDK Runtime Environment (build 1.8.0_03-Ubuntu-8u77-b03-3ubuntu3-b03)
	OpenJDK 64-Bit Server VM (build 25.03-b03, mixed mode)
	```
	...
	Task "AdjustJavacVersionArguments"
	  …/bin/Debug/lib/xamarin.android/xbuild/Xamarin/Android/Xamarin.Android.Common.targets:
	  error : Error executing task AdjustJavacVersionArguments: Required property 'JdkVersion' not set.

[0]: https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/779/
[1]: https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/779/consoleText

This was introduced/broken by commit 0e1d1c8a, which turned
`AdjustJavacVersionArguments.JdkVersion` into a `[Required]`
parameter. The value for `AdjustJavacVersionArguments.JdkVersion`
comes from the `<ResolveSdks/>` task `JdkVersion` output parameter,
which itself comes from parsing `javac -version` output.

The problem is that the regex we used to parse the output of
`javac -version` wasn't properly parsing the output on Ubuntu:

	openjdk version "1.8.0_03-Ubuntu"

Specifically, the `-Ubuntu` text was causing the regex to not match.

Update the regex to be a bit more lenient, enabling it to work with
the Ubuntu `javac -version` output, which *should* allow the build to
progress further.

src/Xamarin.Android.Build.Tasks/Tasks/ResolveSdksTask.cs
@@ -332,7 +332,7 @@ namespace Xamarin.Android.Tasks
 		// `java -version` will produce values such as:
 		//  java version "9.0.4"
 		//  java version "1.8.0_77"
-		static readonly Regex javaVersionRegex = new Regex (@"version ""(?<version>[\d\.]+)(_\d+)?""");
+		static readonly Regex javaVersionRegex = new Regex (@"version ""(?<version>[\d\.]+)(_d+)?[^""]*""");
 
 		Version GetJavaVersionForFramework (string targetFrameworkVersion)
 		{