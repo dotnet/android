a4ed574707da90fab341d160cabfe291555910e0
Dean Ellis <dellis1972@googlemail.com>
2018-02-13 16:04:44 +0000

    [Xamarin.Android.Build.Tasks] Only report `ndk-bundle` if required. (#1298)
    We should only list `ndk-bundle` in the `@(AndroidDependencies)` if
    the user is using AOT or MkBundle. Otherwise we will end up
    downloading a ton of stuff we don't need.
    
    This commit fixes the `CalculateProjectDependencies` to report
    `ndk-bundle` if needed. It also adds a unit test to make sure
    it is not included when it shouldn't be.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/GetDependenciesTests.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/GetDependenciesTests.cs
@@ -13,6 +13,46 @@ namespace Xamarin.Android.Build.Tests {
 	[TestFixture]
 	[Parallelizable (ParallelScope.Children)]
 	public class GetDependenciesTest : BaseTest {
+
+		[Test]
+		public void CheckNdkBundle ([Values(true, false)] bool ndkRequred)
+		{
+			var path = Path.Combine ("temp", TestName);
+			var referencePath = CreateFauxReferencesDirectory (Path.Combine (path, "references"), new ApiInfo [] {
+				new ApiInfo () { Id = 26, Level = 26, Name = "Oreo", FrameworkVersion = "v8.0", Stable = true },
+			});
+			MonoAndroidHelper.RefreshSupportedVersions (new string [] { referencePath });
+			IBuildEngine engine = new MockBuildEngine (TestContext.Out);
+			var task = new CalculateProjectDependencies {
+				BuildEngine = engine
+			};
+
+			task.PlatformToolsVersion = "26.0.3";
+			task.ToolsVersion = "26.0.1";
+			task.NdkVersion = "12.1";
+			task.NdkRequired = ndkRequred;
+			task.BuildToolsVersion = "26.0.1";
+			task.TargetFrameworkVersion = "v8.0";
+			task.ManifestFile = new TaskItem (Path.Combine (path, "AndroidManifest.xml"));
+			Assert.IsTrue (task.Execute ());
+			Assert.IsNotNull (task.Dependencies);
+			Assert.AreEqual (ndkRequred ? 5 : 4, task.Dependencies.Length);
+			Assert.IsNotNull (task.Dependencies.FirstOrDefault (x => x.ItemSpec == "build-tools;26.0.1" && x.GetMetadata ("Version") == "26.0.1"),
+				"Dependencies should contains a build-tools version 26.0.1");
+			Assert.IsNotNull (task.Dependencies.FirstOrDefault (x => x.ItemSpec == "tools" && x.GetMetadata ("Version") == "26.0.1"),
+				"Dependencies should contains a tools version 26.0.1");
+			Assert.IsNotNull (task.Dependencies.FirstOrDefault (x => x.ItemSpec == "platforms;android-26" && x.GetMetadata ("Version") == ""),
+				"Dependencies should contains a platform version android-26");
+			Assert.IsNotNull (task.Dependencies.FirstOrDefault (x => x.ItemSpec == "platform-tools" && x.GetMetadata ("Version") == "26.0.3"),
+				"Dependencies should contains a platform-tools version 26.0.3");
+			if (ndkRequred) {
+				Assert.IsNotNull (task.Dependencies.FirstOrDefault (x => x.ItemSpec == "ndk-bundle" && x.GetMetadata ("Version") == "12.1"),
+					"Dependencies should contain a ndk-bundle version 12.1");
+			} else {
+				Assert.IsNull (task.Dependencies.FirstOrDefault (x => x.ItemSpec == "ndk-bundle"),
+					"Dependencies should not contain a ndk-bundle item");
+			}
+		}
 		
 		[Test]
 		public void ManifestFileDoesNotExist ()
@@ -30,6 +70,7 @@ namespace Xamarin.Android.Build.Tests {
 			task.PlatformToolsVersion = "26.0.3";
 			task.ToolsVersion = "26.0.1";
 			task.NdkVersion = "12.1";
+			task.NdkRequired = true;
 			task.BuildToolsVersion = "26.0.1";
 			task.TargetFrameworkVersion = "v8.0";
 			task.ManifestFile = new TaskItem (Path.Combine (path, "AndroidManifest.xml"));
@@ -72,6 +113,7 @@ namespace Xamarin.Android.Build.Tests {
 			task.PlatformToolsVersion = "26.0.3";
 			task.ToolsVersion = "26.0.1";
 			task.NdkVersion = "12.1";
+			task.NdkRequired = true;
 			task.BuildToolsVersion = "26.0.1";
 			task.TargetFrameworkVersion = "v8.0";
 			task.ManifestFile = new TaskItem (manifestFile);
