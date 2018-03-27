b430bf4407c24385dc9147692594cd1195adb75c
Dean Ellis <dellis1972@googlemail.com>
2018-03-27 17:14:02 +0100

    Added Text for failures

--- src/Mono.Android/Test/Android.Graphics/GraphicsTest.cs
+++ src/Mono.Android/Test/Android.Graphics/GraphicsTest.cs
@@ -13,8 +13,8 @@ namespace Android.GraphicsTests
 		public void NinePatch ()
 		{
 			var d = Android.App.Application.Context.Resources.GetDrawable (Xamarin.Android.RuntimeTests.Resource.Drawable.image);
-			Assert.IsNotNull (d);
-			Assert.IsNotNull (d as Android.Graphics.Drawables.NinePatchDrawable);
+			Assert.IsNotNull (d, "An image should have been retrieved.");
+			Assert.IsNotNull (d as Android.Graphics.Drawables.NinePatchDrawable, "The image should be a NinePatchDrawable.");
 		}
 	}
 }
