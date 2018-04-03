e472b88935eb0345f81c27a1e586ea484f34ef0c
Radek Doulik <rodo@xamarin.com>
2018-04-03 23:32:40 +0200

    [plots] Get logcat output for timing in the tasks
    Lately the BCL tests started to produce a lot of logcat output, way
    over the 4M limit of the buffer.
    
    Thus we need to get the logcat output after every apk test run.
    
    I have considered to batch the `RunTestApks` target. That would not
    work very well though, as we are already batching the tasks in this
    target with two related different item groups. So I moved the
    collection of the logcat output to the `RunInstrumentationTests` and
    `RunUITests` tasks.

--- src/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/RunUITests.cs
+++ src/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/RunUITests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Build.Framework;
+using System.IO;
 
 namespace Xamarin.Android.Tools.BootstrapTasks
 {
@@ -10,9 +11,16 @@ namespace Xamarin.Android.Tools.BootstrapTasks
 		[Required]
 		public                  string              Activity                    { get; set; }
 
+		[Required]
+		public                  string              LogcatFilename              { get; set; }
+
 		protected   override    bool                LogTaskMessages {
 			get { return false; }
 		}
+
+		bool getLogcat;
+		TextWriter logcatWriter;
+
 
 		public override bool Execute ()
 		{
@@ -20,18 +28,34 @@ namespace Xamarin.Android.Tools.BootstrapTasks
 			Log.LogMessage (MessageImportance.Low, $"  {nameof (AdbTarget)}: {AdbTarget}");
 			Log.LogMessage (MessageImportance.Low, $"  {nameof (AdbOptions)}: {AdbOptions}");
 			Log.LogMessage (MessageImportance.Low, $"  {nameof (Activity)}: {Activity}");
+			Log.LogMessage (MessageImportance.Low, $"  {nameof (LogcatFilename)}: {LogcatFilename}");
 
 			base.Execute ();
 
 			Log.LogMessage(MessageImportance.Low, $"  going to wait for 15 seconds");
 			System.Threading.Thread.Sleep (15000);
 
+			using (logcatWriter = File.Exists (LogcatFilename) ? File.AppendText (LogcatFilename) : File.CreateText (LogcatFilename)) {
+				getLogcat = true;
+				base.Execute ();
+			}
+
 			return !Log.HasLoggedErrors;
 		}
 
 		protected override string GenerateCommandLineCommands ()
 		{
-			return $"{AdbTarget} {AdbOptions} shell am start -n \"{Activity}\"";
+			return getLogcat ? $"{AdbTarget} {AdbOptions} logcat -v threadtime -d" : $"{AdbTarget} {AdbOptions} shell am start -n \"{Activity}\"";
+		}
+
+		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		{
+			if (getLogcat) {
+				logcatWriter.WriteLine (singleLine);
+				return;
+			}
+
+			base.LogEventsFromTextOutput (singleLine, messageImportance);
 		}
 	}
 }
