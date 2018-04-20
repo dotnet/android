dotnet/android

commit e937f9b444cc5d0c324cf21ba86b3e1baa3e4bc5

[build] Generate source file with XA build paths (#1568)
Generate `bin/Test$(Configuration)/XABuildPaths.cs`, which contains
paths to the top source directory, `xabuild`, `xabuild.exe`, current
`bin` directory, current test output directory and a constant with
the current configuration name.

The paths can be useful in test projects to find various bits and
pieces which are currently referred to using paths relative to a
specific location and thus not really "portable" between various test
projects.

Makefile
@@ -64,7 +64,17 @@ export LINUX_DISTRO_RELEASE := $(shell lsb_release -r -s || true)
 prepare:: linux-prepare
 endif # $(OS_NAME)=Linux
 
-prepare:: prepare-msbuild
+prepare:: prepare-paths prepare-msbuild
+
+XA_BUILD_PATHS_OUT = bin/Test$(CONFIGURATION)/XABuildPaths.cs
+
+prepare-paths: $(XA_BUILD_PATHS_OUT)
+
+$(XA_BUILD_PATHS_OUT): build-tools/scripts/XABuildPaths.cs.in
+	mkdir -p $(shell dirname $@)
+	sed -e 's;@CONFIGURATION@;$(CONFIGURATION);g' \
+	    -e 's;@TOP_DIRECTORY@;$(shell pwd);g' < $< > $@
+	cat $@
 
 linux-prepare::
 	BINFMT_MISC_TROUBLE="cli win" \

build-tools/scripts/PrepareWindows.targets
@@ -34,6 +34,12 @@
         DestinationFile="$(_TopDir)\Configuration.OperatingSystem.props"
         Replacements="@JAVA_HOME@=$(_JavaSdkDirectory)"
     />
+    <MakeDir Directories="$(_TopDir)\bin\Test$(Configuration)" />
+    <ReplaceFileContents
+        SourceFile="$(MSBuildThisFileDirectory)XABuildPaths.cs.in"
+        DestinationFile="$(_TopDir)\bin\Test$(Configuration)\XABuildPaths.cs"
+        Replacements="@TOP_DIRECTORY@=$(_TopDir);@CONFIGURATION@=$(Configuration)"
+    />
     <MSBuild 
         Projects="$(_TopDir)\build-tools\ThirdPartyNotices\ThirdPartyNotices.csproj"
         Properties="ThirdPartyNoticeFile=$(_TopDir)\ThirdPartyNotices.txt;ThirdPartyNoticeLicenseType=foundation"

build-tools/scripts/XABuildPaths.cs.in
@@ -1,0 +1,17 @@
+using System;
+using System.IO;
+
+namespace Xamarin.Android.Build
+{
+	public static class Paths
+	{
+		public const string Configuration = "@CONFIGURATION@";
+		public const string TopDirectory = @"@TOP_DIRECTORY@";
+
+		public static readonly string PrefixDirectory = Path.Combine (TopDirectory, "bin", Configuration);
+		public static readonly string BinDirectory = Path.Combine (PrefixDirectory, "bin");
+		public static readonly string XABuildScript = Path.Combine (BinDirectory, "xabuild");
+		public static readonly string XABuildExe = Path.Combine (BinDirectory, "xabuild.exe");
+		public static readonly string TestOutputDirectory = Path.Combine (TopDirectory, "bin", "Test@CONFIGURATION@");
+	}
+}

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/Builder.cs
@@ -8,12 +8,16 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading;
 
+using XABuildPaths = Xamarin.Android.Build.Paths;
+
 namespace Xamarin.ProjectTools
 {
 	public class Builder : IDisposable
 	{
 		const string SigSegvError = "Got a SIGSEGV while executing native code";
 		const string ConsoleLoggerError = "[ERROR] FATAL UNHANDLED EXCEPTION: System.ArgumentException: is negative";
+
+		string root;
 		string buildLogFullPath;
 		public bool IsUnix { get; set; }
 		public bool RunningMSBuild { get; set; }
@@ -61,11 +65,8 @@ namespace Xamarin.ProjectTools
 					if (!string.IsNullOrEmpty (useMSBuild) && useMSBuild == "0" && !RequiresMSBuild) {
 						RunningMSBuild = false;
 					}
-					#if DEBUG
-					xabuild = Path.GetFullPath (Path.Combine (Root, "..", "Debug", "bin", "xabuild"));
-					#else
-					xabuild = Path.GetFullPath (Path.Combine (Root, "..", "Release", "bin", "xabuild"));
-					#endif
+
+					xabuild = XABuildPaths.XABuildScript;
 					if (File.Exists (xabuild))
 						return xabuild;
 					xabuild = Path.GetFullPath (Path.Combine (Root, "..", "..", "..", "..", "..", "..", "..", "out", "bin", "xabuild"));
@@ -74,11 +75,7 @@ namespace Xamarin.ProjectTools
 					return RunningMSBuild ? "msbuild" : "xbuild";
 				}
 
-				#if DEBUG
-				xabuild = Path.GetFullPath (Path.Combine (Root, "..", "Debug", "bin", "xabuild.exe"));
-				#else
-				xabuild =  Path.GetFullPath (Path.Combine (Root, "..", "Release", "bin", "xabuild.exe"));
-				#endif
+				xabuild = XABuildPaths.XABuildExe;
 				if (File.Exists (xabuild))
 					return xabuild;
 				return "msbuild";
@@ -98,20 +95,18 @@ namespace Xamarin.ProjectTools
 		public string FrameworkLibDirectory {
 			get {
 				var outdir = Environment.GetEnvironmentVariable ("XA_BUILD_OUTPUT_PATH");
-				#if DEBUG
-				var configuraton = Environment.GetEnvironmentVariable ("CONFIGURATION") ?? "Debug";
-				#else
-				var configuraton = Environment.GetEnvironmentVariable ("CONFIGURATION") ?? "Release";
-				#endif
+				string configuration = Environment.GetEnvironmentVariable ("CONFIGURATION") ?? XABuildPaths.Configuration;
 				var libmonodroidPath = Path.Combine ("lib", "xamarin.android", "xbuild", "Xamarin", "Android", "lib", "armeabi-v7a", "libmono-android.release.so");
 				if (String.IsNullOrEmpty(outdir))
 					outdir = Path.GetFullPath (Path.Combine (Root, "..", "..", "..", "..", "..", "..", "..", "out"));
 				if (!Directory.Exists (Path.Combine (outdir, "lib")) || !File.Exists (Path.Combine (outdir, libmonodroidPath)))
-					outdir = Path.GetFullPath (Path.Combine (Root, "..", "..", "bin", configuraton));
+					outdir = Path.Combine (XABuildPaths.TopDirectory, "bin", configuration);
 				if (!Directory.Exists (Path.Combine (outdir, "lib")) || !File.Exists (Path.Combine (outdir, libmonodroidPath)))
-					outdir = Path.GetFullPath (Path.Combine (Root, "..", "..", "bin", "Debug"));
+					outdir = XABuildPaths.PrefixDirectory;
 				if (!Directory.Exists (Path.Combine (outdir, "lib")) || !File.Exists (Path.Combine (outdir, libmonodroidPath)))
-					outdir = Path.GetFullPath (Path.Combine (Root, "..", "..", "bin", "Release"));
+					outdir = Path.Combine (XABuildPaths.TopDirectory, "bin", "Debug");
+				if (!Directory.Exists (Path.Combine (outdir, "lib")) || !File.Exists (Path.Combine (outdir, libmonodroidPath)))
+					outdir = Path.Combine (XABuildPaths.TopDirectory, "bin", "Release");
 				if (IsUnix) {
 					if (!Directory.Exists (Path.Combine (outdir, "lib")) || !File.Exists (Path.Combine (outdir, libmonodroidPath)))
 						outdir = "/Library/Frameworks/Xamarin.Android.framework/Versions/Current";
@@ -207,8 +202,9 @@ namespace Xamarin.ProjectTools
 
 		public string Root {
 			get {
-				return Path.GetDirectoryName (new Uri (typeof (XamarinProject).Assembly.CodeBase).LocalPath);
+				return String.IsNullOrEmpty (root) ? Path.GetDirectoryName (new Uri (typeof (XamarinProject).Assembly.CodeBase).LocalPath) : root;
 			}
+			set { root = value; }
 		}
 
 		public Builder ()
@@ -245,7 +241,7 @@ namespace Xamarin.ProjectTools
 		protected bool BuildInternal (string projectOrSolution, string target, string [] parameters = null, Dictionary<string, string> environmentVariables = null)
 		{
 			buildLogFullPath = (!string.IsNullOrEmpty (BuildLogFile))
-				? Path.GetFullPath (Path.Combine (Root, Path.GetDirectoryName (projectOrSolution), BuildLogFile))
+				? Path.GetFullPath (Path.Combine (XABuildPaths.TestOutputDirectory, Path.GetDirectoryName (projectOrSolution), BuildLogFile))
 				: null;
 			string processLog = !string.IsNullOrEmpty (BuildLogFile)
 				? Path.Combine (Path.GetDirectoryName (buildLogFullPath), "process.log")
@@ -273,7 +269,7 @@ namespace Xamarin.ProjectTools
 			var args  = new StringBuilder ();
 			var psi   = new ProcessStartInfo (XABuildExe);
 			args.AppendFormat ("{0} /t:{1} {2}",
-				QuoteFileName(Path.Combine (Root, projectOrSolution)), target, logger);
+				QuoteFileName(Path.Combine (XABuildPaths.TestOutputDirectory, projectOrSolution)), target, logger);
 			if (RunningMSBuild)
 				args.Append (" /p:BuildingOutOfProcess=true");
 			else
@@ -291,7 +287,7 @@ namespace Xamarin.ProjectTools
 			}
 			if (RunningMSBuild) {
 				psi.EnvironmentVariables ["MSBUILD"] = "msbuild";
-				args.Append ($" /bl:\"{Path.GetFullPath (Path.Combine (Root, Path.GetDirectoryName (projectOrSolution), "msbuild.binlog"))}\"");
+				args.Append ($" /bl:\"{Path.GetFullPath (Path.Combine (XABuildPaths.TestOutputDirectory, Path.GetDirectoryName (projectOrSolution), "msbuild.binlog"))}\"");
 			}
 			if (environmentVariables != null) {
 				foreach (var kvp in environmentVariables) {

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/ProjectBuilder.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Diagnostics;
 using Microsoft.Build.Framework;
 
+using XABuildPaths = Xamarin.Android.Build.Paths;
+
 namespace Xamarin.ProjectTools
 {
 	public class ProjectBuilder : Builder
@@ -60,7 +62,7 @@ namespace Xamarin.ProjectTools
 
 			Output = project.CreateBuildOutput (this);
 
-			project.NuGetRestore (Path.Combine (Root, ProjectDirectory), PackagesDirectory);
+			project.NuGetRestore (Path.Combine (XABuildPaths.TestOutputDirectory, ProjectDirectory), PackagesDirectory);
 
 			bool result = BuildInternal (Path.Combine (ProjectDirectory, project.ProjectFilePath), Target, parameters, environmentVariables);
 			built_before = true;
@@ -103,7 +105,7 @@ namespace Xamarin.ProjectTools
 				return;
 			built_before = false;
 
-			var projectDirectory = Path.Combine (Root, ProjectDirectory);
+			var projectDirectory = Path.Combine (XABuildPaths.TestOutputDirectory, ProjectDirectory);
 			if (Directory.Exists (projectDirectory)) {
 				FileSystemUtils.SetDirectoryWriteable (projectDirectory);
 				Directory.Delete (projectDirectory, true);

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/XamarinProject.cs
@@ -7,6 +7,8 @@ using Microsoft.Build.Construction;
 using System.Diagnostics;
 using System.Text;
 
+using XABuildPaths = Xamarin.Android.Build.Paths;
+
 namespace Xamarin.ProjectTools
 {
 	public abstract class XamarinProject 
@@ -190,7 +192,7 @@ namespace Xamarin.ProjectTools
 
 		public string Root {
 			get {
-				return Path.GetDirectoryName (new Uri (typeof (XamarinProject).Assembly.CodeBase).LocalPath);
+				return XABuildPaths.TestOutputDirectory;
 			}
 		}
 

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Xamarin.ProjectTools.csproj
@@ -80,6 +80,9 @@
     <Compile Include="Common\ContentBuilder.cs" />
     <Compile Include="Common\DotNetStandard.cs" />
     <Compile Include="Common\DotNetXamarinProject.cs" />
+    <Compile Include="..\..\..\..\bin\Test$(CONFIGURATION)\XABuildPaths.cs">
+       <Link>XABuildPaths.cs</Link>
+    </Compile>
   </ItemGroup>
   <ItemGroup>
     <Folder Include="Common\" />

commit 9d24356b66af82d0709eb2a0031388b72d619655

[build] Generate XABuildPaths.cs for all $(CONFIGURATIONS)
Commit e937f9b4 broke `make prepare` for the Release configuration.
Jenkins assumes that `make prepare` is a single invocation for *both*
Debug & Release builds; see e.g. `make prepare-external`:

	$(foreach conf, $(CONFIGURATIONS), ...)

Commit e937f9b4 didn't abide by this requirement, and thus only
generated `XABuildPaths.cs` for the Debug configuration. As such, when
attempting to build in the Release configuration, [things broke][0]:

[0]: https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android/958/

	CSC: error CS2001: Source file '…/xamarin-android/src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/../../../../bin/TestRelease/XABuildPaths.cs' could not be found.

The fix is to update `make prepare-paths` so that it generates
`bin/Test$(Configuration)/XABuildPaths.cs` for *all*
`$(CONFIGURATIONS)` values.

Makefile
@@ -66,16 +66,6 @@ endif # $(OS_NAME)=Linux
 
 prepare:: prepare-paths prepare-msbuild
 
-XA_BUILD_PATHS_OUT = bin/Test$(CONFIGURATION)/XABuildPaths.cs
-
-prepare-paths: $(XA_BUILD_PATHS_OUT)
-
-$(XA_BUILD_PATHS_OUT): build-tools/scripts/XABuildPaths.cs.in
-	mkdir -p $(shell dirname $@)
-	sed -e 's;@CONFIGURATION@;$(CONFIGURATION);g' \
-	    -e 's;@TOP_DIRECTORY@;$(shell pwd);g' < $< > $@
-	cat $@
-
 linux-prepare::
 	BINFMT_MISC_TROUBLE="cli win" \
 	BINFMT_WARN=no ; \
@@ -137,6 +127,18 @@ include build-tools/scripts/Packaging.mk
 include tests/api-compatibility/api-compatibility.mk
 
 topdir  := $(shell pwd)
+
+
+XA_BUILD_PATHS_OUT = $(CONFIGURATIONS:%=bin/Test%/XABuildPaths.cs)
+
+prepare-paths: $(XA_BUILD_PATHS_OUT)
+
+$(XA_BUILD_PATHS_OUT): bin/Test%/XABuildPaths.cs: build-tools/scripts/XABuildPaths.cs.in
+	mkdir -p $(shell dirname $@)
+	sed -e 's;@CONFIGURATION@;$*;g' \
+	    -e 's;@TOP_DIRECTORY@;$(topdir);g' < $< > $@
+	cat $@
+
 
 # Usage: $(call CALL_CREATE_THIRD_PARTY_NOTICES,configuration,path,licenseType,includeExternalDeps,includeBuildDeps)
 define CREATE_THIRD_PARTY_NOTICES