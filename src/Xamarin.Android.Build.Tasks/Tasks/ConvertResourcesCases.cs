dotnet/android

commit a50e00bb5e5fd3cdb5d0832718b8b154a611f888

[Xamarin.Android.Build.Tasks] Speed up ConvertResourcesCases (#1522)
We currently call the `<ConvertResourcesCases/>` task in [three][1]
[different][2] [places][3]. For the most part they process the same
files, over and over and over again. On a large project this is
totally a waste of time. If the file has been processed once and not
updated then we should not need to process it again.

[1]: https://github.com/xamarin/xamarin-android/blob/5d64fb4bbad606eb4a3478085db0ca98616f9966/src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets#L1951
[2]: https://github.com/xamarin/xamarin-android/blob/5d64fb4bbad606eb4a3478085db0ca98616f9966/src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets#L1436
[3]: https://github.com/xamarin/xamarin-android/blob/5d64fb4bbad606eb4a3478085db0ca98616f9966/src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets#L2011

The only exception here is `<GenerateJavaStubs/>`, which needs to be
done *after* we have compiled the app, but we *also* need to replace
any instances of `Foo.MyClass` with `<md5hash>.MyClass` which is
generated in the Java code. We thus still need to process the
resource files for the app after the main assembly has been built.

That said if we know the `UpdateAndroidResgen` target has run, then
we *don't* need to run this again in the `_CreateBaseApk` target.

This is an attempt to see if we can only process the files which have
changed between each of those calls. Alternatively we could rework
the targets so that we only have one call at the right place in the
build system (probably as part of the `UpdateAndroidResources`).

If this does not work then the next task will be to rework
`AndroidResource.UpdateXmlResources()` to *not* use `XDocument` but
try to use `XPathDocument` or `XmlReader/Writer` instead since it is
a streaming version and should be a lot quicker.

~~ Timing Results ~~

On a basic Android App which references the support libraries:

src/Xamarin.Android.Build.Tasks/Tasks/ConvertResourcesCases.cs
@@ -18,11 +18,14 @@ namespace Xamarin.Android.Tasks
 		[Required]
 		public string AcwMapFile { get; set; }
 
+		public string AndroidConversionFlagFile { get; set; }
+
 		public override bool Execute ()
 		{
 			Log.LogDebugMessage ("ConvertResourcesCases Task");
 			Log.LogDebugMessage ("  ResourceDirectories: {0}", ResourceDirectories);
 			Log.LogDebugMessage ("  AcwMapFile: {0}", AcwMapFile);
+			Log.LogDebugMessage ("  AndroidConversionFlagFile: {0}", AndroidConversionFlagFile);
 
 			var acw_map = MonoAndroidHelper.LoadAcwMapFile (AcwMapFile);
 
@@ -50,10 +53,19 @@ namespace Xamarin.Android.Tasks
 				.SelectMany (dir => Directory.EnumerateFiles (dir, "*.xml")
 					.Concat (Directory.EnumerateFiles (dir, "*.axml")));
 
+			var lastUpdate = DateTime.MinValue;
+			if (!string.IsNullOrEmpty (AndroidConversionFlagFile) && File.Exists (AndroidConversionFlagFile)) {
+				lastUpdate = File.GetLastWriteTimeUtc (AndroidConversionFlagFile);
+			}
+			Log.LogDebugMessage ("  AndroidResgenFlagFile modified: {0}", lastUpdate);
 			// Fix up each file
 			foreach (string file in xmls) {
-				Log.LogDebugMessage ("  Processing: {0}", file);
 				var srcmodifiedDate = File.GetLastWriteTimeUtc (file);
+				if (srcmodifiedDate <= lastUpdate) {
+					Log.LogDebugMessage ("  Skipping: {0}  {1} <= {2}", file, srcmodifiedDate, lastUpdate);
+					continue;
+				}
+				Log.LogDebugMessage ("  Processing: {0}   {1} > {2}", file, srcmodifiedDate, lastUpdate);
 				var tmpdest = Path.GetTempFileName ();
 				MonoAndroidHelper.CopyIfChanged (file, tmpdest);
 				MonoAndroidHelper.SetWriteable (tmpdest);
@@ -70,7 +82,6 @@ namespace Xamarin.Android.Tasks
 
 					if (MonoAndroidHelper.CopyIfChanged (tmpdest, file)) {
 						MonoAndroidHelper.SetWriteable (file);
-						MonoAndroidHelper.SetLastAccessAndWriteTimeUtc (file, srcmodifiedDate, Log);
 					}
 				} finally {
 					File.Delete (tmpdest);

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -1436,6 +1436,7 @@ because xbuild doesn't support framework reference assemblies.
 	<ConvertResourcesCases
 		ResourceDirectories="$(MonoAndroidResDirIntermediate);@(LibraryResourceDirectories)"
 		AcwMapFile="$(_AcwMapFile)"
+		AndroidConversionFlagFile="$(_AndroidResgenFlagFile)"
 	/>
 
 	<GetExtraPackages
@@ -2006,11 +2007,6 @@ because xbuild doesn't support framework reference assemblies.
   <CreateTemporaryDirectory>
     <Output TaskParameter="TemporaryDirectory" PropertyName="AaptTemporaryDirectory" />
   </CreateTemporaryDirectory>
-
-	<!-- Change cases so we support mixed case resource names -->
-	<ConvertResourcesCases
-		ResourceDirectories="$(MonoAndroidResDirIntermediate);@(LibraryResourceDirectories)"
-		AcwMapFile="$(_AcwMapFile)" />
 
   <GetExtraPackages
   	IntermediateOutputPath="$(IntermediateOutputPath)"

commit ce510d968c9d9b2c1fffcbf2f62901512dfb330c

[Xamarin.Android.Build.Tasks] _UpdateAndroidResgen always runs.
Commit a50e00bb broke the build in such that `_UpdateAndroidResgen`
always runs. This is because the resource file last modified date
was always newer than the `R.cs.flag`.

src/Xamarin.Android.Build.Tasks/Tasks/ConvertResourcesCases.cs
@@ -82,6 +82,7 @@ namespace Xamarin.Android.Tasks
 
 					if (MonoAndroidHelper.CopyIfChanged (tmpdest, file)) {
 						MonoAndroidHelper.SetWriteable (file);
+						File.SetLastWriteTimeUtc (srcmodifiedDate);
 					}
 				} finally {
 					File.Delete (tmpdest);