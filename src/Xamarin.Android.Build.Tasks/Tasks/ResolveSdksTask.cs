a6e4d3725c6277148bffb7e76f826731f1364a73
Dean Ellis <dellis1972@googlemail.com>
2018-06-28 12:50:04 +0100

    [Xamarin.Android.Build.Tasks] Add Warnings for minimum Google Play requirements.
    Fixes https://devdiv.visualstudio.com/DevDiv/_workitems/edit/635168
    
    Google as of August and November 2018 will required a minimum
    of TargetFrameworkVersion v8.0 to publish/update apps.
    
    This commit adds a new check to `ResolveSdksTask` which will
    emit warnings when we hit those dates. This will only happen
    if the `TargetFrameworkVersion` < `v8.0`
    
    [1] [Google Play's target API level requirement](https://developer.android.com/distribute/best-practices/develop/target-sdk)

--- src/Xamarin.Android.Build.Tasks/Tasks/ResolveSdksTask.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/ResolveSdksTask.cs
@@ -309,6 +309,16 @@ namespace Xamarin.Android.Tasks
 				return false;
 			}
 
+			int apiLevel;
+			var augustDeadline = new DateTime (2018, 8, 1);
+			var novemberDeadline = new DateTime (2018, 11, 1);
+			if (int.TryParse (AndroidApiLevel, out apiLevel)) {
+				if (apiLevel < 26 && DateTime.Now >= augustDeadline)
+					Log.LogCodedWarning ("XA0113", $"Google Play requires that new applications must use a TargetFrameworkVersion of v8.0 (API level 26) or above. You are currently targeting {TargetFrameworkVersion} (API level {AndroidApiLevel}).");
+				if (apiLevel < 26 && DateTime.Now >= novemberDeadline)
+					Log.LogCodedWarning ("XA0114", $"Google Play requires that application updates must use a TargetFrameworkVersion of v8.0 (API level 26) or above. You are currently targeting {TargetFrameworkVersion} (API level {AndroidApiLevel}).");
+			}
+
 			SequencePointsMode mode;
 			if (!Aot.TryGetSequencePointsMode (SequencePointsMode ?? "None", out mode))
 				Log.LogCodedError ("XA0104", "Invalid Sequence Point mode: {0}", SequencePointsMode);
