6fe65a8590da4bf4a4c2405ed3aedfd22415884b
Marek Habersack <grendel@twistedcode.net>
2018-07-09 13:43:03 +0200

    Ignore <merge> when generating code-behind source
    Fixes: https://github.com/xamarin/xamarin-android/issues/1929
    
    `<merge>` is an element used by Android to avoid stacking too many view groups
    when one layout is included into another. In such cases `<merge>` can be used as
    a place-holder which is replaced by the enclosing view group of the layout that
    includes another layout with `<merge>` as the root element. However, as much as
    you can call `FindViewById` to find an instance of `<merge>` which was given the
    `android:id` attribute, Android will always return `null` in such case, as the
    element has no type counterpart in the `Android.Views` collection of views.
    Therefore, we must ignore the element whenever we process a layout that contains
    it decorated with `android:id` in order to avoid confusion in generated
    code (i.e. the property generated for the element would be of type `merge` which
    does not exist). We could instead generate a property of type
    `Android.Views.View` and always return `null` from it, but that unnecessarily
    increases code size - we'd be adding effectively dead code.

---
+++ tests/CodeBehind/BuildTests/MainMergeActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Xamarin.Android.Tests.CodeBehindBuildTests
+{
+	[Activity (Label = "MainMergeActivity")]
+	public partial class MainMergeActivity : Activity
+	{
+		int count = 1;
+		bool onSetContentViewCalled_01 = false;
+		bool onSetContentViewCalled_02 = false;
+		bool onSetContentViewCalled_03 = false;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.MainMerge);
+
+			// Compile time: check by assignment if correct types were generated
+			CommonSampleLibrary.LogFragment log = log_fragment;
+
+#if NOT_CONFLICTING_FRAGMENT
+			CommonSampleLibrary.LogFragment log2 = secondary_log_fragment;
+#else
+			global::Android.App.Fragment log2 = secondary_log_fragment;
+#endif
+			TextView t = Text1;
+
+			Button button = myButton;
+			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+		}
+
+		partial void OnSetContentView (global::Android.Views.View view, ref bool callBaseAfterReturn)
+		{
+			onSetContentViewCalled_01 = true;
+		}
+
+                partial void OnSetContentView (global::Android.Views.View view, global::Android.Views.ViewGroup.LayoutParams @params, ref bool callBaseAfterReturn)
+		{
+			onSetContentViewCalled_02 = true;
+		}
+
+                partial void OnSetContentView (int layoutResID, ref bool callBaseAfterReturn)
+		{
+			onSetContentViewCalled_03 = true;
+		}
+	}
+}
+
