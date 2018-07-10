5f040f12f4ec682cebf612231ce76a716f97be0d
Atsushi Eno <atsushieno@gmail.com>
2018-07-10 17:55:04 +0900

    Add tests for default interface method bindings.
    They fail so far, because generator is not generating expected code yet.

---
+++ tests/CodeGen-Binding/Xamarin.Android.DefaultInterfaceMethods-Tests/TestsSample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Xamarin.Android.DefaultInterfaceMethodsTests
+{
+	[TestFixture]
+	public class TestsSample
+	{
+
+		[SetUp]
+		public void Setup () { }
+
+
+		[TearDown]
+		public void Tear () { }
+
+		[Test]
+		public void Pass ()
+		{
+			var c = new Com.Xamarin.Test.ImplementedClass ();
+			Assert.AreEqual (-1, c.ToImplement (), "#1");
+			Assert.AreEqual (0, c.Foo (), "#2");
+			Assert.AreEqual (1, c.Bar, "#3");
+		}
+	}
+}
