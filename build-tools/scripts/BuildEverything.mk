09bc6ef42ab791864fbe58e0749a5199dd9828f4
Jonathan Pryor <jonpryor@vt.edu>
2018-07-27 16:21:25 -0400

    Revert "[build] Stop building armeabi libraries (#1995)"
    This reverts commit e89ba5691ac4c4026d6b625ab0639269cd572d9a.
    
    Commit e89ba569 [broke the build][0]:
    
    	  MONO_PATH=/Users/builder/jenkins/workspace/xamarin-android/xamarin-android/external/mono/tools/offsets-tool/CppSharp/osx_32 mono --arch=32 /Users/builder/jenkins/workspace/xamarin-android/xamarin-android/external/mono/tools/offsets-tool/MonoAotOffsetsDumper.exe --android-ndk="/Users/builder/android-toolchain/ndk" --mono="/Users/builder/jenkins/workspace/xamarin-android/xamarin-android/external/mono" --monodroid="/Users/builder/jenkins/workspace/xamarin-android/xamarin-android/" --abi="armv5-none-linux-androideabi" --targetdir="/Users/builder/jenkins/workspace/xamarin-android/xamarin-android/external/mono/sdks/builds/android-armeabi-debug" --out="/Users/builder/jenkins/workspace/xamarin-android/xamarin-android/src/mono-runtimes/obj/Debug//cross-arm"
    
    	  Processing triple: armv5-none-linux-androideabi
    
    	  Unhandled Exception:
    	  System.Exception: Could not find the target build directory: /Users/builder/jenkins/workspace/xamarin-android/xamarin-android/external/mono/sdks/builds/android-armeabi-debug
    	    at CppSharp.MonoAotOffsetsDumper.SetupMono (CppSharp.Driver driver, CppSharp.MonoAotOffsetsDumper+Target target) [0x00128] in <2f8bf518c8bd43fb8ac2a2d284bd85a5>:0
    	    at CppSharp.MonoAotOffsetsDumper.Setup (CppSharp.Driver driver, CppSharp.MonoAotOffsetsDumper+Target target) [0x000a2] in <2f8bf518c8bd43fb8ac2a2d284bd85a5>:0
    	    at CppSharp.MonoAotOffsetsDumper.Main (System.String[] args) [0x00118] in <2f8bf518c8bd43fb8ac2a2d284bd85a5>:0
    
    The Mono SDKs build step still requires armv5 support, so we can't
    stop building `armeabi` libraries until mono stops requiring them.
    
    [0]: https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android/1107/

--- build-tools/scripts/BuildEverything.mk
+++ build-tools/scripts/BuildEverything.mk
@@ -39,6 +39,7 @@ STABLE_FRAMEWORKS = $(foreach a, $(STABLE_API_LEVELS), $(word $(a),$(ALL_FRAMEWO
 PLATFORM_IDS      = $(foreach a, $(API_LEVELS), $(word $(a),$(ALL_PLATFORM_IDS)))
 
 ALL_JIT_ABIS  = \
+	armeabi \
 	armeabi-v7a \
 	arm64-v8a \
 	x86 \
