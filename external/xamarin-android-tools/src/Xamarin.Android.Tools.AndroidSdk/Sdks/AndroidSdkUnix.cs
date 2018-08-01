dotnet/android

commit 2f24c3f1bdb97f1ad0232933c0a0ea2fddd3e93a

[Xamarin.Android.Tools.AndroidSdk] Add JdkInfo
Fixes: https://github.com/xamarin/xamarin-android-tools/issues/26
Context: https://github.com/xamarin/xamarin-android-tools/pull/29#issuecomment-400048306

Refactor out the [underlying functionality][0] of the
[`<JdkInfo/>` task][1] so that it is more easily usable.

[0]: https://github.com/xamarin/java.interop/commit/4bd9297f311b3e0fb3e07335507a38278b83d255
[1]: https://github.com/xamarin/java.interop/blob/master/src/Java.Interop.BootstrapTasks/Java.Interop.BootstrapTasks/JdkInfo.cs

external/xamarin-android-tools/src/Xamarin.Android.Tools.AndroidSdk/Sdks/AndroidSdkUnix.cs
@@ -42,7 +42,7 @@ namespace Xamarin.Android.Tools
 
 		public override string PreferedAndroidSdkPath { 
 			get {
-				var config_file = GetUnixConfigFile ();
+				var config_file = GetUnixConfigFile (Logger);
 				var androidEl = config_file.Root.Element ("android-sdk");
 
 				if (androidEl != null) {
@@ -57,7 +57,7 @@ namespace Xamarin.Android.Tools
 
 		public override string PreferedAndroidNdkPath { 
 			get {
-				var config_file = GetUnixConfigFile ();
+				var config_file = GetUnixConfigFile (Logger);
 				var androidEl = config_file.Root.Element ("android-ndk");
 
 				if (androidEl != null) {
@@ -72,7 +72,7 @@ namespace Xamarin.Android.Tools
 
 		public override string PreferedJavaSdkPath { 
 			get {
-				var config_file = GetUnixConfigFile ();
+				var config_file = GetUnixConfigFile (Logger);
 				var javaEl = config_file.Root.Element ("java-sdk");
 
 				if (javaEl != null) {
@@ -92,7 +92,7 @@ namespace Xamarin.Android.Tools
 				yield return preferedSdkPath;
 
 			// Look in PATH
-			foreach (var path in FindExecutableInPath (Adb)) {
+			foreach (var path in ProcessUtils.FindExecutablesInPath (Adb)) {
 				// Strip off "platform-tools"
 				var dir = Path.GetDirectoryName (path);
 
@@ -113,7 +113,7 @@ namespace Xamarin.Android.Tools
 				return preferedJavaSdkPath;
 
 			// Look in PATH
-			foreach (var path in FindExecutableInPath (JarSigner)) {
+			foreach (var path in ProcessUtils.FindExecutablesInPath (JarSigner)) {
 				// Strip off "bin"
 				var dir = Path.GetDirectoryName (path);
 
@@ -160,7 +160,7 @@ namespace Xamarin.Android.Tools
 				yield return preferedNdkPath;
 
 			// Look in PATH
-			foreach (var path in FindExecutableInPath (NdkStack)) {
+			foreach (var path in ProcessUtils.FindExecutablesInPath (NdkStack)) {
 				if (ValidateAndroidNdkLocation (path))
 					yield return path;
 			}
@@ -176,7 +176,7 @@ namespace Xamarin.Android.Tools
 		{
 			path = NullIfEmpty (path);
 
-			var doc = GetUnixConfigFile ();
+			var doc = GetUnixConfigFile (Logger);
 			var androidEl = doc.Root.Element ("android-sdk");
 
 			if (androidEl == null) {
@@ -192,7 +192,7 @@ namespace Xamarin.Android.Tools
 		{
 			path = NullIfEmpty (path);
 
-			var doc = GetUnixConfigFile ();
+			var doc = GetUnixConfigFile (Logger);
 			var javaEl = doc.Root.Element ("java-sdk");
 
 			if (javaEl == null) {
@@ -208,7 +208,7 @@ namespace Xamarin.Android.Tools
 		{
 			path = NullIfEmpty (path);
 
-			var doc = GetUnixConfigFile ();
+			var doc = GetUnixConfigFile (Logger);
 			var androidEl = doc.Root.Element ("android-ndk");
 
 			if (androidEl == null) {
@@ -275,7 +275,7 @@ namespace Xamarin.Android.Tools
 			}
 		}
 
-		private XDocument GetUnixConfigFile ()
+		internal static XDocument GetUnixConfigFile (Action<TraceLevel, string> logger)
 		{
 			var file = UnixConfigPath;
 			XDocument doc = null;
@@ -283,8 +283,8 @@ namespace Xamarin.Android.Tools
 				try {
 					doc = XDocument.Load (file);
 				} catch (Exception ex) {
-					Logger (TraceLevel.Error, "Could not load monodroid configuration file");
-					Logger (TraceLevel.Verbose, ex.ToString ());
+					logger (TraceLevel.Error, "Could not load monodroid configuration file");
+					logger (TraceLevel.Verbose, ex.ToString ());
 
 					// move out of the way and create a new one
 					doc = new XDocument (new XElement ("monodroid"));

external/xamarin-android-tools/src/Xamarin.Android.Tools.AndroidSdk/JdkInfo.cs
@@ -1,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Xamarin.Android.Tools
+{
+	public class JdkInfo {
+
+		public      string                              HomePath                    {get;}
+
+		public      string                              JarPath                     {get;}
+		public      string                              JavaPath                    {get;}
+		public      string                              JavacPath                   {get;}
+		public      string                              JdkJvmPath                  {get;}
+		public      ReadOnlyCollection<string>          IncludePath                 {get;}
+
+		public      Version                             Version                     => javaVersion.Value;
+		public      string                              Vendor                      {
+			get {
+				if (GetJavaSettingsPropertyValue ("java.vendor", out string vendor))
+					return vendor;
+				return null;
+			}
+		}
+
+		public      ReadOnlyDictionary<string, string>  ReleaseProperties           {get;}
+		public      IEnumerable<string>                 JavaSettingsPropertyKeys    => javaProperties.Value.Keys;
+
+		Lazy<Dictionary<string, List<string>>>      javaProperties;
+		Lazy<Version>                               javaVersion;
+
+		public JdkInfo (string homePath)
+		{
+			if (homePath == null)
+				throw new ArgumentNullException (nameof (homePath));
+			if (!Directory.Exists (homePath))
+				throw new ArgumentException ("Not a directory", nameof (homePath));
+
+			HomePath            = homePath;
+
+			var binPath         = Path.Combine (HomePath, "bin");
+			JarPath             = ProcessUtils.FindExecutablesInDirectory (binPath, "jar").FirstOrDefault ();
+			JavaPath            = ProcessUtils.FindExecutablesInDirectory (binPath, "java").FirstOrDefault ();
+			JavacPath           = ProcessUtils.FindExecutablesInDirectory (binPath, "javac").FirstOrDefault ();
+			JdkJvmPath = OS.IsMac
+				? FindLibrariesInDirectory (HomePath, "jli").FirstOrDefault ()
+				: FindLibrariesInDirectory (Path.Combine (HomePath, "jre"), "jvm").FirstOrDefault ();
+
+			ValidateFile ("jar",    JarPath);
+			ValidateFile ("java",   JavaPath);
+			ValidateFile ("javac",  JavacPath);
+			ValidateFile ("jvm",    JdkJvmPath);
+
+			var includes        = new List<string> ();
+			var jdkInclude      = Path.Combine (HomePath, "include");
+
+			if (Directory.Exists (jdkInclude)) {
+				includes.Add (jdkInclude);
+				includes.AddRange (Directory.GetDirectories (jdkInclude));
+			}
+
+
+			ReleaseProperties   = GetReleaseProperties();
+
+			IncludePath         = new ReadOnlyCollection<string> (includes);
+
+			javaProperties      = new Lazy<Dictionary<string, List<string>>> (GetJavaProperties, LazyThreadSafetyMode.ExecutionAndPublication);
+			javaVersion         = new Lazy<Version> (GetJavaVersion, LazyThreadSafetyMode.ExecutionAndPublication);
+		}
+
+		public override string ToString()
+		{
+			return $"JdkInfo(Version={Version}, Vendor=\"{Vendor}\", HomePath=\"{HomePath}\")";
+		}
+
+		public bool GetJavaSettingsPropertyValues (string key, out IEnumerable<string> value)
+		{
+			value       = null;
+			var props   = javaProperties.Value;
+			if (props.TryGetValue (key, out var v)) {
+				value = v;
+				return true;
+			}
+			return false;
+		}
+
+		public bool GetJavaSettingsPropertyValue (string key, out string value)
+		{
+			value       = null;
+			var props   = javaProperties.Value;
+			if (props.TryGetValue (key, out var v)) {
+				if (v.Count > 1)
+					throw new InvalidOperationException ($"Requested to get one string value when property `{key}` contains `{v.Count}` values.");
+				value   = v [0];
+				return true;
+			}
+			return false;
+		}
+
+		static IEnumerable<string> FindLibrariesInDirectory (string dir, string libraryName)
+		{
+			var library = string.Format (OS.NativeLibraryFormat, libraryName);
+			return Directory.EnumerateFiles (dir, library, SearchOption.AllDirectories);
+		}
+
+		void ValidateFile (string name, string path)
+		{
+			if (path == null || !File.Exists (path))
+				throw new ArgumentException ($"Could not find required file `{name}` within `{HomePath}`; is this a valid JDK?", "homePath");
+		}
+
+		static  Regex   VersionExtractor  = new Regex (@"(?<version>[\d]+(\.\d+)+)(_(?<patch>\d+))?", RegexOptions.Compiled);
+
+		Version GetJavaVersion ()
+		{
+			string version = null;
+			if (!ReleaseProperties.TryGetValue ("JAVA_VERSION", out version)) {
+				if (GetJavaSettingsPropertyValue ("java.version", out string vs))
+					version = vs;
+			}
+			if (version == null)
+				throw new NotSupportedException ("Could not determine Java version");
+			var m = VersionExtractor.Match (version);
+			if (!m.Success)
+				return null;
+			version   = m.Groups ["version"].Value;
+			var patch = m.Groups ["patch"].Value;
+			if (!string.IsNullOrEmpty (patch))
+				version += "." + patch;
+			if (!version.Contains ("."))
+				version += ".0";
+			if (Version.TryParse (version, out Version v))
+				return v;
+			return null;
+		}
+
+		ReadOnlyDictionary<string, string> GetReleaseProperties ()
+		{
+			var releasePath = Path.Combine (HomePath, "release");
+			if (!File.Exists (releasePath))
+				return new ReadOnlyDictionary<string, string> (new Dictionary<string, string> ());
+
+			var props   = new Dictionary<string, string> ();
+			using (var release = File.OpenText (releasePath)) {
+				string line;
+				while ((line = release.ReadLine ()) != null) {
+					const string PropertyDelim  = "=\"";
+					int delim = line.IndexOf (PropertyDelim, StringComparison.Ordinal);
+					if (delim < 0) {
+						props [line] = "";
+					}
+					string  key     = line.Substring (0, delim);
+					string  value   = line.Substring (delim + PropertyDelim.Length, line.Length - delim - PropertyDelim.Length - 1);
+					props [key] = value;
+				}
+			}
+			return new ReadOnlyDictionary<string, string>(props);
+		}
+
+		Dictionary<string, List<string>> GetJavaProperties ()
+		{
+			return GetJavaProperties (ProcessUtils.FindExecutablesInDirectory (Path.Combine (HomePath, "bin"), "java").First ());
+		}
+
+		static Dictionary<string, List<string>> GetJavaProperties (string java)
+		{
+			var javaProps   = new ProcessStartInfo {
+				FileName    = java,
+				Arguments   = "-XshowSettings:properties -version",
+			};
+
+			var     props   = new Dictionary<string, List<string>> ();
+			string  curKey  = null;
+			ProcessUtils.Exec (javaProps, (o, e) => {
+					const string ContinuedValuePrefix   = "        ";
+					const string NewValuePrefix         = "    ";
+					const string NameValueDelim         = " = ";
+					if (string.IsNullOrEmpty (e.Data))
+						return;
+					if (e.Data.StartsWith (ContinuedValuePrefix, StringComparison.Ordinal)) {
+						if (curKey == null)
+							throw new InvalidOperationException ($"Unknown property key for value {e.Data}!");
+						props [curKey].Add (e.Data.Substring (ContinuedValuePrefix.Length));
+						return;
+					}
+					if (e.Data.StartsWith (NewValuePrefix, StringComparison.Ordinal)) {
+						var delim = e.Data.IndexOf (NameValueDelim, StringComparison.Ordinal);
+						if (delim <= 0)
+							return;
+						curKey      = e.Data.Substring (NewValuePrefix.Length, delim - NewValuePrefix.Length);
+						var value   = e.Data.Substring (delim + NameValueDelim.Length);
+						List<string> values;
+						if (!props.TryGetValue (curKey, out values))
+							props.Add (curKey, values = new List<string> ());
+						values.Add (value);
+					}
+			});
+
+			return props;
+		}
+
+		public static IEnumerable<JdkInfo> GetKnownSystemJdkInfos (Action<TraceLevel, string> logger)
+		{
+			return GetWindowsJdks (logger)
+				.Concat (GetConfiguredJdks (logger))
+				.Concat (GetMacOSMicrosoftJdks (logger))
+				.Concat (GetJavaHomeEnvironmentJdks (logger))
+				.Concat (GetLibexecJdks (logger))
+				.Concat (GetPathEnvironmentJdks (logger))
+				.Concat (GetJavaAlternativesJdks (logger))
+				;
+		}
+
+		static IEnumerable<JdkInfo> GetConfiguredJdks (Action<TraceLevel, string> logger)
+		{
+			return GetConfiguredJdkPaths (logger)
+				.Select (p => TryGetJdkInfo (p, logger))
+				.Where (jdk => jdk != null)
+				.OrderByDescending (jdk => jdk, JdkInfoVersionComparer.Default);
+		}
+
+		static IEnumerable<string> GetConfiguredJdkPaths (Action<TraceLevel, string> logger)
+		{
+			var config = AndroidSdkUnix.GetUnixConfigFile (logger);
+			foreach (var java_sdk in config.Root.Elements ("java-sdk")) {
+				var path    = (string) java_sdk.Attribute ("path");
+				yield return path;
+			}
+		}
+
+		static IEnumerable<JdkInfo> GetMacOSMicrosoftJdks (Action<TraceLevel, string> logger)
+		{
+			return GetMacOSMicrosoftJdkPaths ()
+				.Select (p => TryGetJdkInfo (p, logger))
+				.Where (jdk => jdk != null)
+				.OrderByDescending (jdk => jdk, JdkInfoVersionComparer.Default);
+		}
+
+		static IEnumerable<string> GetMacOSMicrosoftJdkPaths ()
+		{
+			var home    = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			var jdks    = Path.Combine (home, "Library", "Developer", "Xamarin", "jdk");
+			if (!Directory.Exists (jdks))
+				return Enumerable.Empty <string> ();
+
+			return Directory.EnumerateDirectories (jdks);
+		}
+
+		static JdkInfo TryGetJdkInfo (string path, Action<TraceLevel, string> logger)
+		{
+			JdkInfo jdk = null;
+			try {
+				jdk = new JdkInfo (path);
+			}
+			catch (Exception e) {
+				logger (TraceLevel.Warning, $"Not a valid JDK directory: `{path}`");
+				logger (TraceLevel.Verbose, e.ToString ());
+			}
+			return jdk;
+		}
+
+		static IEnumerable<JdkInfo> GetWindowsJdks (Action<TraceLevel, string> logger)
+		{
+			if (!OS.IsWindows)
+				return Enumerable.Empty<JdkInfo> ();
+			return AndroidSdkWindows.GetJdkInfos (logger);
+		}
+
+		static IEnumerable<JdkInfo> GetJavaHomeEnvironmentJdks (Action<TraceLevel, string> logger)
+		{
+			var java_home = Environment.GetEnvironmentVariable ("JAVA_HOME");
+			if (string.IsNullOrEmpty (java_home))
+				yield break;
+			var jdk = TryGetJdkInfo (java_home, logger);
+			if (jdk != null)
+				yield return jdk;
+		}
+
+		// macOS
+		static IEnumerable<JdkInfo> GetLibexecJdks (Action<TraceLevel, string> logger)
+		{
+			return GetLibexecJdkPaths (logger)
+				.Distinct ()
+				.Select (p => TryGetJdkInfo (p, logger))
+				.Where (jdk => jdk != null)
+				.OrderByDescending (jdk => jdk, JdkInfoVersionComparer.Default);
+		}
+
+		static IEnumerable<string> GetLibexecJdkPaths (Action<TraceLevel, string> logger)
+		{
+			var java_home	= Path.GetFullPath ("/usr/libexec/java_home");
+			if (!File.Exists (java_home)) {
+				yield break;
+			}
+			var jhp = new ProcessStartInfo {
+				FileName    = java_home,
+				Arguments   = "-X",
+			};
+			var xml = new StringBuilder ();
+			ProcessUtils.Exec (jhp, (o, e) => {
+					if (string.IsNullOrEmpty (e.Data))
+						return;
+					xml.Append (e.Data);
+			});
+			var plist   = XElement.Parse (xml.ToString ());
+			foreach (var info in plist.Elements ("array").Elements ("dict")) {
+				var JVMHomePath = (XNode) info.Elements ("key").FirstOrDefault (e => e.Value == "JVMHomePath");
+				if (JVMHomePath == null)
+					continue;
+				while (JVMHomePath.NextNode.NodeType != XmlNodeType.Element)
+					JVMHomePath = JVMHomePath.NextNode;
+				var strElement  = (XElement) JVMHomePath.NextNode;
+				var path        = strElement.Value;
+				yield return path;
+			}
+		}
+
+		// Linux; Ubuntu & Derivatives
+		static IEnumerable<JdkInfo> GetJavaAlternativesJdks (Action<TraceLevel, string> logger)
+		{
+			return GetJavaAlternativesJdkPaths ()
+				.Distinct ()
+				.Select (p => TryGetJdkInfo (p, logger))
+				.Where (jdk => jdk != null);
+		}
+
+		static IEnumerable<string> GetJavaAlternativesJdkPaths ()
+		{
+			var alternatives  = Path.GetFullPath ("/usr/sbin/update-java-alternatives");
+			if (!File.Exists (alternatives))
+				return Enumerable.Empty<string> ();
+
+			var psi     = new ProcessStartInfo {
+				FileName    = alternatives,
+				Arguments   = "-l",
+			};
+			var paths   = new List<string> ();
+			ProcessUtils.Exec (psi, (o, e) => {
+					if (string.IsNullOrWhiteSpace (e.Data))
+						return;
+					// Example line:
+					//  java-1.8.0-openjdk-amd64       1081       /usr/lib/jvm/java-1.8.0-openjdk-amd64
+					var columns = e.Data.Split (new[]{ ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					if (columns.Length <= 2)
+						return;
+					paths.Add (columns [2]);
+			});
+			return paths;
+		}
+
+		// Linux; Fedora
+		static IEnumerable<JdkInfo> GetLibJvmJdks (Action<TraceLevel, string> logger)
+		{
+			return GetLibJvmJdkPaths ()
+				.Distinct ()
+				.Select (p => TryGetJdkInfo (p, logger))
+				.Where (jdk => jdk != null)
+				.OrderByDescending (jdk => jdk, JdkInfoVersionComparer.Default);
+		}
+
+		static IEnumerable<string> GetLibJvmJdkPaths ()
+		{
+			var jvm = "/usr/lib/jvm";
+			if (!Directory.Exists (jvm))
+				yield break;
+
+			foreach (var jdk in Directory.EnumerateDirectories (jvm)) {
+				var release = Path.Combine (jdk, "release");
+				if (File.Exists (release))
+					yield return jdk;
+			}
+		}
+
+		// Last-ditch fallback!
+		static IEnumerable<JdkInfo> GetPathEnvironmentJdks (Action<TraceLevel, string> logger)
+		{
+			return GetPathEnvironmentJdkPaths ()
+				.Select (p => TryGetJdkInfo (p, logger))
+				.Where (jdk => jdk != null);
+		}
+
+		static IEnumerable<string> GetPathEnvironmentJdkPaths ()
+		{
+			foreach (var java in ProcessUtils.FindExecutablesInPath ("java")) {
+				var props   = GetJavaProperties (java);
+				if (props.TryGetValue ("java.home", out var java_homes)) {
+					var java_home   = java_homes [0];
+					// `java -XshowSettings:properties -version 2>&1 | grep java.home` ends with `/jre` on macOS.
+					// We need the parent dir so we can properly lookup the `include` directories
+					if (java_home.EndsWith ("jre", StringComparison.OrdinalIgnoreCase)) {
+						java_home = Path.GetDirectoryName (java_home);
+					}
+					yield return java_home;
+				}
+			}
+		}
+	}
+
+	class JdkInfoVersionComparer : IComparer<JdkInfo>
+	{
+		public  static  readonly    IComparer<JdkInfo> Default = new JdkInfoVersionComparer ();
+
+		public int Compare (JdkInfo x, JdkInfo y)
+		{
+			if (x.Version != null && y.Version != null)
+				return x.Version.CompareTo (y.Version);
+			return 0;
+		}
+	}
+}

external/xamarin-android-tools/src/Xamarin.Android.Tools.AndroidSdk/OS.cs
@@ -12,6 +12,8 @@ namespace Xamarin.Android.Tools
 
 		internal readonly static string ProgramFilesX86;
 
+		internal readonly static string NativeLibraryFormat;
+
 		static OS ()
 		{
 			IsWindows = Path.DirectorySeparatorChar == '\\';
@@ -20,6 +22,13 @@ namespace Xamarin.Android.Tools
 			if (IsWindows) {
 				ProgramFilesX86 = GetProgramFilesX86 ();
 			}
+
+			if (IsWindows)
+				NativeLibraryFormat = "{0}.dll";
+			if (IsMac)
+				NativeLibraryFormat = "lib{0}.dylib";
+			if (!IsWindows && !IsMac)
+				NativeLibraryFormat = "lib{0}.so";
 		}
 
 		//From Managed.Windows.Forms/XplatUI

external/xamarin-android-tools/src/Xamarin.Android.Tools.AndroidSdk/ProcessUtils.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Xamarin.Android.Tools
 {
 	public static class ProcessUtils
 	{
+		static string[] ExecutableFileExtensions;
+
+		static ProcessUtils ()
+		{
+			var pathExt     = Environment.GetEnvironmentVariable ("PATHEXT");
+			var pathExts    = pathExt?.Split (new char [] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries) ?? new string [0];
+
+			ExecutableFileExtensions    = pathExts;
+		}
+
 		public static async Task<int> StartProcess (ProcessStartInfo psi, TextWriter stdout, TextWriter stderr, CancellationToken cancellationToken, Action<Process> onStarted = null)
 		{
 			cancellationToken.ThrowIfCancellationRequested ();
@@ -118,6 +129,62 @@ namespace Xamarin.Android.Tools
 			}, TaskContinuationOptions.ExecuteSynchronously);
 
 			return tcs.Task;
+		}
+
+		internal static void Exec (ProcessStartInfo processStartInfo, DataReceivedEventHandler output)
+		{
+			processStartInfo.UseShellExecute         = false;
+			processStartInfo.RedirectStandardInput   = false;
+			processStartInfo.RedirectStandardOutput  = true;
+			processStartInfo.RedirectStandardError   = true;
+			processStartInfo.CreateNoWindow          = true;
+			processStartInfo.WindowStyle             = ProcessWindowStyle.Hidden;
+
+			var p = new Process () {
+				StartInfo   = processStartInfo,
+			};
+			p.OutputDataReceived    += output;
+			p.ErrorDataReceived     += output;
+
+			using (p) {
+				p.Start ();
+				p.BeginOutputReadLine ();
+				p.BeginErrorReadLine ();
+				p.WaitForExit ();
+			}
+		}
+
+		internal static IEnumerable<string> FindExecutablesInPath (string executable)
+		{
+			var path        = Environment.GetEnvironmentVariable ("PATH");
+			var pathDirs    = path.Split (new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
+
+			foreach (var dir in pathDirs) {
+				foreach (var exe in FindExecutablesInDirectory (dir, executable)) {
+					yield return exe;
+				}
+			}
+		}
+
+		internal static IEnumerable<string> FindExecutablesInDirectory (string dir, string executable)
+		{
+			foreach (var exe in ExecutableFiles (executable)) {
+				var exePath = Path.Combine (dir, exe);
+				if (File.Exists (exePath))
+					yield return exePath;
+			}
+		}
+
+		internal static IEnumerable<string> ExecutableFiles (string executable)
+		{
+			if (ExecutableFileExtensions == null || ExecutableFileExtensions.Length == 0) {
+				yield return executable;
+				yield break;
+			}
+
+			foreach (var ext in ExecutableFileExtensions)
+				yield return Path.ChangeExtension (executable, ext);
+			yield return executable;
 		}
 	}
 }

external/xamarin-android-tools/src/Xamarin.Android.Tools.AndroidSdk/Sdks/AndroidSdkBase.cs
@@ -122,7 +122,7 @@ namespace Xamarin.Android.Tools
 		/// </summary>
 		public bool ValidateAndroidSdkLocation (string loc)
 		{
-			return !string.IsNullOrEmpty (loc) && FindExecutableInDirectory (Adb, Path.Combine (loc, "platform-tools")).Any ();
+			return !string.IsNullOrEmpty (loc) && ProcessUtils.FindExecutablesInDirectory (Path.Combine (loc, "platform-tools"), Adb).Any ();
 		}
 
 		/// <summary>
@@ -130,7 +130,7 @@ namespace Xamarin.Android.Tools
 		/// </summary>
 		public virtual bool ValidateJavaSdkLocation (string loc)
 		{
-			return !string.IsNullOrEmpty (loc) && FindExecutableInDirectory (JarSigner, Path.Combine (loc, "bin")).Any ();
+			return !string.IsNullOrEmpty (loc) && ProcessUtils.FindExecutablesInDirectory (Path.Combine (loc, "bin"), JarSigner).Any ();
 		}
 
 		/// <summary>
@@ -138,42 +138,10 @@ namespace Xamarin.Android.Tools
 		/// </summary>
 		public bool ValidateAndroidNdkLocation (string loc)
 		{
-			return !string.IsNullOrEmpty (loc) && FindExecutableInDirectory (NdkStack, loc).Any ();
+			return !string.IsNullOrEmpty (loc) && ProcessUtils.FindExecutablesInDirectory (loc, NdkStack).Any ();
 		}
 
-		protected IEnumerable<string> FindExecutableInPath (string executable)
-		{
-			var path = Environment.GetEnvironmentVariable ("PATH");
-			var pathDirs = path.Split (new char[] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
-
-			foreach (var dir in pathDirs) {
-				foreach (var directory in FindExecutableInDirectory (executable, dir)) {
-					yield return directory;
-				}
-			}
-		}
-
-		protected IEnumerable<string> FindExecutableInDirectory (string executable, string dir)
-		{
-			foreach (var exe in Executables (executable))
-				if (File.Exists (Path.Combine (dir, exe)))
-					yield return dir;
-		}
-
-		IEnumerable<string> Executables (string executable)
-		{
-			yield return executable;
-			var pathExt = Environment.GetEnvironmentVariable ("PATHEXT");
-			var pathExts = pathExt?.Split (new char [] { Path.PathSeparator }, StringSplitOptions.RemoveEmptyEntries);
-
-			if (pathExts == null)
-				yield break;
-
-			foreach (var ext in pathExts)
-				yield return Path.ChangeExtension (executable, ext);
-		}
-
-		protected string NullIfEmpty (string s)
+		protected static string NullIfEmpty (string s)
 		{
 			if (s == null || s.Length != 0)
 				return s;
@@ -181,11 +149,12 @@ namespace Xamarin.Android.Tools
 			return null;
 		}
 
-		string GetExecutablePath (string dir, string exe)
+		static string GetExecutablePath (string dir, string exe)
 		{
 			if (string.IsNullOrEmpty (dir))
 				return exe;
-			foreach (var e in Executables (exe))
+
+			foreach (var e in ProcessUtils.ExecutableFiles (exe))
 				if (File.Exists (Path.Combine (dir, e)))
 					return e;
 			return exe;

commit c5cb7b3a43537203aa9bd8299985ab927952e492

Allow finding NDK location based on $PATH
Context: https://github.com/xamarin/androidtools/pull/119
Context: https://devdiv.visualstudio.com/DevDiv/DevDiv%20Team/_build?buildId=1908630

Context: https://github.com/xamarin/androidtools/pull/120
Context: https://devdiv.visualstudio.com/DevDiv/DevDiv%20Team/_build?buildId=1908776

How does `AndroidSdkInfo` determine the appropriate value to return
from `AndroidSdkInfo.AndroidNdkPath`?

Firstly, it checks `$HOME/.config/xbuild/monodroid-config.xml`.

If that file doesn't exist, or it doesn't contain a
`/monodroid/android-ndk/@path` attribute value, then `AndroidSdkInfo`
checks `$PATH`, and attempts to find `ndk-stack`.  If `ndk-stack` can
be found in `$PATH`, the containing directory is used:

	$ mv $HOME/.config/xbuild/monodroid-config.xml{,.none}

	$ PATH=/path/to/my/ndk:$PATH csharp -r:Xamarin.Android.Tools.AndroidSdk.dll
	csharp> using System.Diagnostics;
	csharp> using Xamarin.Android.Tools;
	csharp> Action<TraceLevel, string> logger = (level, value) => Console.WriteLine ($"[{level}] {value}");
	csharp> var info = new AndroidSdkInfo (logger);
	csharp> info.AndroidNdkPath;
	"/path/to/my/ndk"

Unfortunately, commit 2f24c3f1 broke this behavior; afterward,
`info.AndroidNdkPath` would return `null`, bceause commit 2f24c3f1
inadvertently broke `$PATH`-based probing for `ndk-stack`.

Oops. :-(

Specifically, it was the "cleanup" in 2f24c3f1, replacing
`AndroidSdkBase.FindExecutableInPath()` with
`ProcessUtils.FindExecutablesInPath()`.  While these are similarly
named, they had an important semantic difference: the former returned
the *directory* containing an executable, while the latter returned
the full path for the executable.

Double oops.

Review commit 2f24c3f1 and review all
`ProcessUtils.FindExecutablesInPath()` use to ensure that when a
directory is desired, we actually get a directory, not the path to
`ndk-stack` itself.

This allows `$PATH`-based NDK lookup to work again.

external/xamarin-android-tools/src/Xamarin.Android.Tools.AndroidSdk/Sdks/AndroidSdkUnix.cs
@@ -93,7 +93,8 @@ namespace Xamarin.Android.Tools
 				yield return preferedSdkPath;
 
 			// Look in PATH
-			foreach (var path in ProcessUtils.FindExecutablesInPath (Adb)) {
+			foreach (var adb in ProcessUtils.FindExecutablesInPath (Adb)) {
+				var path = Path.GetDirectoryName (adb);
 				// Strip off "platform-tools"
 				var dir = Path.GetDirectoryName (path);
 
@@ -119,9 +120,10 @@ namespace Xamarin.Android.Tools
 				yield return preferedNdkPath;
 
 			// Look in PATH
-			foreach (var path in ProcessUtils.FindExecutablesInPath (NdkStack)) {
-				if (ValidateAndroidNdkLocation (path))
-					yield return path;
+			foreach (var ndkStack in ProcessUtils.FindExecutablesInPath (NdkStack)) {
+				var ndkDir  = Path.GetDirectoryName (ndkStack);
+				if (ValidateAndroidNdkLocation (ndkDir))
+					yield return ndkDir;
 			}
 		}
 