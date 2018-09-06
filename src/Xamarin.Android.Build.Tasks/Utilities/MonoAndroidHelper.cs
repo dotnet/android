dotnet/android

commit 8f2ae24813ec3cdab56f688c4c3b611a33759333

[Xamarin.Android.Build.Tasks] Don't package reference assemblies (#706)
Fixes: https://bugzilla.xamarin.com/show_bug.cgi?id=57342

The investigation in the bug suggests that we are picking up a
reference assembly rather than a the actual implementation.
This is to do with the way netstandard nuget packages work, they
include both `ref` and `lib` folders.

In this case `ref` was being included in the package rather than
`lib`. This commit alters the `_ResolveAssemblies` to use

	@(ReferenceCopyLocalPaths)

This ItemGroup is populated with the correct items.

src/Xamarin.Android.Build.Tasks/Utilities/MonoAndroidHelper.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using Mono.Security.Cryptography;
 using Xamarin.Android.Build.Utilities;
 using Xamarin.Tools.Zip;
+using Mono.Cecil;
 
 #if MSBUILD
 using Microsoft.Build.Framework;
@@ -284,6 +285,14 @@ namespace Xamarin.Android.Tasks
 				return true;
 			}
 			return TargetFrameworkDirectories == null || !checkSdkPath ? false : ExistsInFrameworkPath (assembly);
+		}
+
+		public static bool IsReferenceAssembly (string assembly)
+		{
+			var a = AssemblyDefinition.ReadAssembly (assembly, new ReaderParameters() { InMemory = true, ReadSymbols = false, });
+			if (!a.HasCustomAttributes)
+				return false;
+			return a.CustomAttributes.Any (t => t.AttributeType.FullName == "System.Runtime.CompilerServices.ReferenceAssemblyAttribute");
 		}
 
 		public static bool ExistsInFrameworkPath (string assembly)

Makefile
@@ -149,7 +149,7 @@ TEST_APK_PROJECTS = \
 # Syntax: $(call BUILD_TEST_APK,path/to/project.csproj)
 define BUILD_TEST_APK
 	# Must use xabuild to ensure correct assemblies are resolved
-	MSBUILD="$(MSBUILD)" tools/scripts/xabuild /t:SignAndroidPackage $(1)
+	MSBUILD="$(MSBUILD)" tools/scripts/xabuild $(MSBUILD_FLAGS) /t:SignAndroidPackage $(1)
 endef	# BUILD_TEST_APK
 
 run-apk-tests:

src/Xamarin.Android.Build.Tasks/Tasks/BuildApk.cs
@@ -273,6 +273,10 @@ namespace Xamarin.Android.Tasks
 
 			int count = 0;
 			foreach (ITaskItem assembly in ResolvedUserAssemblies) {
+
+				if (MonoAndroidHelper.IsReferenceAssembly (assembly.ItemSpec)) {
+					Log.LogWarning ($"{assembly.ItemSpec} is a reference assembly!");
+				}
 				// Add assembly
 				apk.Archive.AddFile (assembly.ItemSpec, GetTargetDirectory (assembly.ItemSpec) + "/"  + Path.GetFileName (assembly.ItemSpec), compressionMethod: CompressionMethod.Store);
 
@@ -305,6 +309,9 @@ namespace Xamarin.Android.Tasks
 			count = 0;
 			// Add framework assemblies
 			foreach (ITaskItem assembly in ResolvedFrameworkAssemblies) {
+				if (MonoAndroidHelper.IsReferenceAssembly (assembly.ItemSpec)) {
+					Log.LogWarning ($"{assembly.ItemSpec} is a reference assembly!");
+				}
 				apk.Archive.AddFile (assembly.ItemSpec, "assemblies/" + Path.GetFileName (assembly.ItemSpec), compressionMethod: CompressionMethod.Store);
 				var config = Path.ChangeExtension (assembly.ItemSpec, "dll.config");
 				AddAssemblyConfigEntry (apk, config);

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -1417,7 +1417,11 @@ because xbuild doesn't support framework reference assemblies.
 <Target Name="_ResolveAssemblies">
 	<!--- Remove the ImplicitlyExpandDesignTimeFacades assemblies. We have already build the app there are not required for packaging  -->
 	<ItemGroup>
-		<FilteredAssemblies Include="@(ReferencePath)" Condition="'%(ReferencePath.ResolvedFrom)' != 'ImplicitlyExpandDesignTimeFacades'" />
+		<FilteredAssemblies Include="%(ReferenceCopyLocalPaths.Identity)"
+				Condition="'%(ReferenceCopyLocalPaths.ResolvedFrom)' != 'ImplicitlyExpandDesignTimeFacades' And '%(ReferenceCopyLocalPaths.Extension)' == '.dll' And '%(ReferenceCopyLocalPaths.DestinationSubDirectory)' == '' "/>
+		<!-- Fallback to @(ReferencePath) if @(ReferenceCopyLocalPaths) is empty. This is for xbuild support -->
+		<FilteredAssemblies Include="%(ReferencePath.Identity)"
+				Condition="'%(ReferencePath.ResolvedFrom)' != 'ImplicitlyExpandDesignTimeFacades' And '@(ReferenceCopyLocalPaths)' == '' "/>
 	</ItemGroup>
 	<!-- Find all the assemblies this app requires -->
 	<ResolveAssemblies

commit fc6b7e94557aec06870b6d0561ed55d9c1626c1d

[Xamarin.Android.Build.Tasks] should Dispose AssemblyDefinition (#2148)
Reviewing our codebase, we found a place where the `<BuildApk/>` task
was calling `MonoAndroidHelper.IsReferenceAssembly(string)`, and
`IsReferenceAssembly(string)` had two notable problems when it was
first introduced in 8f2ae248:

 1. It was using `ReaderParameters.InMemory`=true
 2. We were not calling `AssemblyDefinition.Dispose()`!

(1) is not ideal, as we load *every* assembly into memory, including
our ~20-30MB `Mono.Android.dll`!

	foreach (ITaskItem assembly in ResolvedUserAssemblies) {
	  if (MonoAndroidHelper.IsReferenceAssembly (assembly.ItemSpec)) {
	//...
	foreach (ITaskItem assembly in ResolvedFrameworkAssemblies) {
	  if (MonoAndroidHelper.IsReferenceAssembly (assembly.ItemSpec)) {

Update `MonoAndroidHelper.IsReferenceAssembly(string)` so that
instead of loading the assembly into memory, we instead wrap the
`AssemblyDefinition` use within a `using` block.  This prevents the
assembly file from being held open for an unduly long time, and
allows us to stop using `ReaderParameters.InMemory=true`, reducing
memory use.

This makes the `<BuildApk/>` task *slower*, but *improves* the
overall build time:

Before:

    5890 ms  BuildApk                                   1 calls
    Time Elapsed 00:01:02.59

After:

    6377 ms  BuildApk                                   1 calls
    Time Elapsed 00:00:59.37

I think the `AssemblyDefinition` instances must have been getting
cleaned up later in the build by the GC (if we were lucky), and also
used *a lot* more memory.  This memory use and related GC would
contribute to overall build time.

src/Xamarin.Android.Build.Tasks/Utilities/MonoAndroidHelper.cs
@@ -307,8 +307,9 @@ namespace Xamarin.Android.Tasks
 
 		public static bool IsReferenceAssembly (string assembly)
 		{
-			var a = AssemblyDefinition.ReadAssembly (assembly, new ReaderParameters () { InMemory = true, ReadSymbols = false, });
-			return IsReferenceAssembly (a);
+			var rp = new ReaderParameters { ReadSymbols = false };
+			using (var a = AssemblyDefinition.ReadAssembly (assembly, rp))
+				return IsReferenceAssembly (a);
 		}
 
 		public static bool IsReferenceAssembly (AssemblyDefinition assembly)