dotnet/android

commit b1d81460628e8cf2d78439ae760ef6b065db3a27

[Xamarin.Android.Build.Tasks] Add coded error for armeabi (#2180)
Fixes: https://github.com/xamarin/xamarin-android/issues/2173
Context: https://github.com/xamarin/xamarin-android/pull/2106

This commit checks for the presence of the now unsupported `armeabi`
architecture in the `$(AndroidSupportedAbis)` property by using a
conditional error in a new target.

Another option would be to to change the error to a warning and
*ignore* the ";armeabi;" substring in the property, but that seems a
little risky for cases where users might not notice the warning.
They would probably be surprised later when they discovered that
their `.apk` files did not contain `armeabi` support.

I also debated on whether it would be better to move this check into
the `<BuildApk/>` task.  I decided that since it is just a small test
to look for a substring in an MSBuild property, using a conditional
`<Error/>` task in a small new target seems fine.

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -520,6 +520,7 @@ Copyright (C) 2011-2012 Xamarin. All rights reserved.
     _CheckProjectItems;
     _CheckForContent;
     _CheckTargetFramework;
+    _CheckSupportedAbis;
     _RemoveLegacyDesigner;
     _ValidateAndroidPackageProperties;
     $(BuildDependsOn);
@@ -1030,6 +1031,16 @@ because xbuild doesn't support framework reference assemblies.
 
 <Target Name="_CheckTargetFramework">
 	<Warning Code="XA0109" Text="Unsupported or invalid %24(TargetFrameworkVersion) value of 'v4.5'. Please update your Project Options." Condition=" '$(TargetFrameworkVersion)' == 'v4.5' "/> 
+</Target>
+
+<Target Name="_CheckSupportedAbis">
+	<PropertyGroup>
+		<_RequestedAbis>;$(AndroidSupportedAbis);</_RequestedAbis>
+	</PropertyGroup>
+	<Error Code="XA0115"
+			Condition="$(_RequestedAbis.Contains(';armeabi;'))"
+			Text="Invalid value 'armeabi' in %24(AndroidSupportedAbis). This ABI is no longer supported. Please update your project properties."
+	/>
 </Target>
 
 <Target Name="_CheckForContent">

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -3021,6 +3021,20 @@ AAAAAAAAAAAAPQAAAE1FVEEtSU5GL01BTklGRVNULk1GUEsBAhQAFAAICAgAJZFnS7uHtAn+AQAA
 				}
 			}
 		}
+
+		[Test]
+		public void XA0115 ()
+		{
+			var proj = new XamarinAndroidApplicationProject ();
+			proj.SetProperty (KnownProperties.AndroidSupportedAbis, "armeabi;armeabi-v7a");
+			using (var builder = CreateApkBuilder (Path.Combine ("temp", TestContext.CurrentContext.Test.Name))) {
+				builder.ThrowOnBuildFailure = false;
+				Assert.IsFalse (builder.Build (proj), "Build should have failed with XA0115.");
+				StringAssertEx.Contains ($"error XA0115", builder.LastBuildOutput, "Error should be XA0115");
+				Assert.IsTrue (builder.Clean (proj), "Clean should have succeeded.");
+			}
+		}
+
 	}
 }
 

commit c32b9e02ffedd743f6d8bc9187ea4d9c8c2e9ed0

[Xamarin.Android.Build.Tasks] Backport XA0115 error but make it a warning.
Commit b1d81460 introduced a new error XA0115. This error is raised
if the user has `armeabi` in their `$(AndroidSupportedAbis)`.

We need to do a similar thing in the d15-9 release. However it should
not be an error but a warning. It will turn into an error for the next
release.

This commit also introduces a warning if the user is using a
`$(TargetFrameworkVersion)` less than 19. These platforms have
a very low market share and will be slowly removed from our
build system going forward. This warning is to make sure that users
get a heads up before their application starts failing to build.

src/Xamarin.Android.Build.Tasks/Tasks/ResolveAndroidTooling.cs
@@ -192,6 +192,8 @@ namespace Xamarin.Android.Tasks
 					Log.LogCodedWarning ("XA0113", $"Google Play requires that new applications must use a TargetFrameworkVersion of v8.0 (API level 26) or above. You are currently targeting {TargetFrameworkVersion} (API level {AndroidApiLevel}).");
 				if (apiLevel < 26)
 					Log.LogCodedWarning ("XA0114", $"Google Play requires that application updates must use a TargetFrameworkVersion of v8.0 (API level 26) or above. You are currently targeting {TargetFrameworkVersion} (API level {AndroidApiLevel}).");
+				if (apiLevel < 19)
+					Log.LogCodedWarning ("XA0116", $"The TargetFrameworkVersion {TargetFrameworkVersion} is deprecated. Please update it to be v4.4 or higher.");
 			}
 
 			SequencePointsMode mode;

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Common.targets
@@ -494,6 +494,16 @@ Copyright (C) 2011-2012 Xamarin. All rights reserved.
 		TargetFrameworkVersion="$(TargetFrameworkVersion)" />
 </Target>
 
+<Target Name="_CheckSupportedAbis">
+	<PropertyGroup>
+		<_RequestedAbis>;$(AndroidSupportedAbis);</_RequestedAbis>
+	</PropertyGroup>
+	<Warning Code="XA0115"
+			Condition="$(_RequestedAbis.Contains(';armeabi;'))"
+			Text="This ABI ('armeabi') is deprecated and will be removed in the next release. Please update your project properties."
+	/>
+</Target>
+
 <Target Name="_StripEmbeddedLibraries"
   Inputs="@(ResolvedAssemblies)" 
   Outputs="$(_AndroidStripFlag)"
@@ -528,6 +538,7 @@ Copyright (C) 2011-2012 Xamarin. All rights reserved.
     _CheckProjectItems;
     _CheckForContent;
     _CheckTargetFramework;
+	_CheckSupportedAbis;
     _RemoveLegacyDesigner;
     _ValidateAndroidPackageProperties;
     $(BuildDependsOn);