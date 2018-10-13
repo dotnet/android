5e47af3f6ebef1253c9cf4fa9368c4d2d9857604
Dean Ellis <dellis1972@googlemail.com>
2018-10-13 12:53:46 +0100

    [Xamarin.Android.Build.Tasks] Add CopyResource items to FileWrites (#2288)
    Not quite sure how we ever got away with this.  As part of the build
    process we extract from `@(EmbeddedResource)` a bunch of `.java`
    files into the intermediate output directory.
    
    These are used for a number of things like multidex and bootstrapping
    mono.  For some reason we *never* added those to the `@(FileWrites)`
    group.  So there is a good chance these files will get deleted by an
    incremental clean.
    
    This was picked up by a unit test in monodroid which deals with
    fast deployment.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/IncrementalBuildTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/IncrementalBuildTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Xamarin.ProjectTools;
 using System.IO;
@@ -12,6 +13,31 @@ namespace Xamarin.Android.Build.Tests
 	[Parallelizable (ParallelScope.Children)]
 	public class IncrementalBuildTest : BaseTest
 	{
+		[Test]
+		public void CheckNothingIsDeletedByIncrementalClean ([Values (true, false)] bool enableMultiDex, [Values (true, false)] bool useAapt2)
+		{
+			// do a release build
+			// change one of the properties (say AotAssemblies) 
+			// do another build. it should NOT hose the resource directory.
+			var path = Path.Combine ("temp", TestName);
+			var proj = new XamarinFormsAndroidApplicationProject () {
+				ProjectName = "App1",
+				IsRelease = true,
+			};
+			if (enableMultiDex)
+				proj.SetProperty ("AndroidEnableMultiDex", "True");
+			if (useAapt2)
+				proj.SetProperty ("AndroidUseAapt2", "True");
+			using (var b = CreateApkBuilder (path)) {
+				Assert.IsTrue (b.Build (proj), "First should have succeeded" );
+				IEnumerable<string> files = Directory.EnumerateFiles (Path.Combine (Root, path, proj.IntermediateOutputPath), "*.*", SearchOption.AllDirectories);
+				Assert.IsTrue (b.Build (proj, doNotCleanupOnUpdate: true, parameters: null, saveProject: false), "Second should have succeeded");
+				foreach (var file in files) {
+					FileAssert.Exists (file, $"{file} should not have been deleted!" );
+				}
+			}
+		}
+
 		[Test]
 		public void CheckResourceDirectoryDoesNotGetHosed ()
 		{
