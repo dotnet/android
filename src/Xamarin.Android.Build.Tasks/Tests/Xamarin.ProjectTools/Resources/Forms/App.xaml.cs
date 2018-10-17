d53b1fc9eae05a34ec7fb6935df5fadc4cfb61ce
Jonathan Peppers <jonathan.peppers@gmail.com>
2018-10-16 19:20:00 -0500

    [tests] use a "real" Xamarin.Forms app (#2304)
    In the past `XamarinFormsAndroidApplicationProject` was incomplete:
    
     1. It was not a "real app", but the resemblance of one that merely
        *compiled*.
     2. It did not have XAML or import/run Xamarin.Forms MSBuild tasks.
     3. It was not an app that could startup, and render Xamarin.Forms
        content.
    
    So I took the simplest Xamarin.Forms template from Visual Studio 2017
    15.8.7, a `Blank App`, and:
    
      - Added various new files as `@(EmbeddedResource)` in
        `Xamarin.ProjectTools`.
      - Used most files as-is, adding `${ROOT_NAMESPACE}` where appropriate.
      - Included `Tabbar.axml`, which has a "custom view".
      - Included proper XAML files.
      - Imported `Xamarin.Forms.targets`, and verified that `<XamlG/>`
        and `<XamlC/>` are now both running.
      - Added `Xamarin.Android.Support.Vector.Drawable`, which avoids a
        crash at runtime.
      - Updated to Xamarin.Forms 3.1.0.
    
    Overall, this should make any of our test cases using
    `XamarinFormsAndroidApplicationProject` much closer to reality.

---
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Resources/Forms/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace ${ROOT_NAMESPACE}
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent ();
+
+			MainPage = new MainPage ();
+		}
+	}
+}
