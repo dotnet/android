434d2d888633b11023dfee4cc7cc44ba88666b4e
Marek Habersack <grendel@twistedcode.net>
2019-01-14 15:57:08 +0100

    [monodroid] Introduce EmbeddedAssemblies class (#2606)
    As part of our continued "C++ification" effort (6c4939d0, etc.),
    introduce a new `EmbeddedAssemblies` which handles `.dll` loading.
    
    Remove a number of `MONO_API` functions such as
    `monodroid_embedded_assemblies_register_from()`, as they don't appear
    to be used anywhere.  (Some were intended to be used by Mono Unreal
    Engine, but we're not sure if that's still the case.)
    
    We can restore them if needed.
    
    Note: `monodroid_embedded_assemblies_set_assemblies_prefix()` is
    used externally by [Embeddinator-4000][0], and is preserved.
    
    Additionally, remove many `printf()` family calls for string
    generation, and use **strcat**(3) or **strdup**(3) instead, as
    `strcat()`/`strdup()` can be faster than `snprintf()` or
    `monodroid_strdup_printf()`.
    
    [0]: https://github.com/mono/Embeddinator-4000/blob/c237e21b63df40799ef251350ee961846c549553/support/mono_embeddinator.c#L62

--- src/monodroid/jni/embedded-assemblies.h
+++ src/monodroid/jni/embedded-assemblies.h
@@ -2,31 +2,86 @@
 #ifndef INC_MONODROID_EMBEDDED_ASSEMBLIES_H
 #define INC_MONODROID_EMBEDDED_ASSEMBLIES_H
 
+#include <cstring>
 #include "dylib-mono.h"
+#include "unzip.h"
+#include "ioapi.h"
 
-/* filename is e.g. System.dll, System.dll.mdb, System.pdb */
-typedef int (*monodroid_should_register)(const char *filename, void *user_data);
+namespace xamarin { namespace android { namespace internal {
+	struct TypeMappingInfo;
+	struct md_mmap_info;
 
-/* invoked for each assembly/debug symbol file to determine if the apk_file entry should be used */
-MONO_API void monodroid_embedded_assemblies_set_should_register (
-		monodroid_should_register   should_register,
-		void                       *user_data
-);
+	class EmbeddedAssemblies
+	{
+	private:
+		static constexpr char assemblies_prefix[] = "assemblies/";
+		static const char *suffixes[];
 
-/* mono_bool; returns previous value */
-MONO_API int monodroid_embedded_assemblies_set_register_debug_symbols (
-		int /* mono_bool */   register_debug_symbols
-);
+	public:
+		/* filename is e.g. System.dll, System.dll.mdb, System.pdb */
+		using monodroid_should_register = bool (*)(const char *filename);
 
-/* returns current number of *all* assemblies found from all invocations */
-MONO_API int monodroid_embedded_assemblies_register_from (
-		xamarin::android::DylibMono        *imports,
-		const char       *apk_file
-);
+	public:
+		void try_load_typemaps_from_directory (const char *path);
+		void install_preload_hooks ();
+		const char* typemap_java_to_managed (const char *java);
+		const char* typemap_managed_to_java (const char *managed);
 
-/* mono_bool; returns TRUE if the install succeeded. */
-MONO_API int monodroid_embedded_assemblies_install_preload_hook (xamarin::android::DylibMono *imports);
+		/* returns current number of *all* assemblies found from all invocations */
+		template<bool (*should_register_fn)(const char*)>
+		size_t register_from (const char *apk_file)
+		{
+			static_assert (should_register_fn != nullptr, "should_register_fn is a required template parameter");
+			return register_from (apk_file, should_register_fn);
+		}
 
-int try_load_typemaps_from_directory (const char *path);
+		bool get_register_debug_symbols () const
+		{
+			return register_debug_symbols;
+		}
+
+		void set_register_debug_symbols (bool value)
+		{
+			register_debug_symbols = value;
+		}
+
+		void set_assemblies_prefix (const char *prefix);
+
+	private:
+		size_t register_from (const char *apk_file, monodroid_should_register should_register);
+		bool gather_bundled_assemblies_from_apk (const char* apk, monodroid_should_register should_register);
+		MonoAssembly* open_from_bundles (MonoAssemblyName* aname, bool ref_only);
+		void extract_int (const char **header, const char *source_apk, const char *source_entry, const char *key_name, int *value);
+		bool add_type_mapping (TypeMappingInfo **info, const char *source_apk, const char *source_entry, const char *addr);
+		bool register_debug_symbols_for_assembly (const char *entry_name, MonoBundledAssembly *assembly, const mono_byte *debug_contents, int debug_size);
+
+		static md_mmap_info md_mmap_apk_file (int fd, uLong offset, uLong size, const char* filename, const char* apk);
+		static void* md_mmap_open_file (void *opaque, const char *filename, int mode);
+		static uLong md_mmap_read_file (void *opaque, void *stream, void *buf, uLong size);
+		static long md_mmap_tell_file (void *opaque, void *stream);
+		static long md_mmap_seek_file (void *opaque, void *stream, uLong offset, int origin);
+		static int md_mmap_close_file (void *opaque, void *stream);
+		static int md_mmap_error_file (void *opaque, void *stream);
+
+		static MonoAssembly* open_from_bundles_full (MonoAssemblyName *aname, char **assemblies_path, void *user_data);
+		static MonoAssembly* open_from_bundles_refonly (MonoAssemblyName *aname, char **assemblies_path, void *user_data);
+		static int TypeMappingInfo_compare_key (const void *a, const void *b);
+
+		const char* get_assemblies_prefix () const
+		{
+			return assemblies_prefix_override != nullptr ? assemblies_prefix_override : assemblies_prefix;
+		}
+
+	private:
+		bool                   register_debug_symbols;
+		MonoBundledAssembly  **bundled_assemblies;
+		size_t                 bundled_assemblies_count;
+		TypeMappingInfo       *java_to_managed_maps;
+		TypeMappingInfo       *managed_to_java_maps;
+		const char            *assemblies_prefix_override = nullptr;
+	};
+}}}
+
+MONO_API int monodroid_embedded_assemblies_set_assemblies_prefix (const char *prefix);
 
 #endif /* INC_MONODROID_EMBEDDED_ASSEMBLIES_H */
