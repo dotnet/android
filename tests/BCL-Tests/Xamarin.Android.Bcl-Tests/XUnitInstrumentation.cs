5d65346dff52302f7edc92d31c22788f9944fd35
Alexander Kyte <alkyte@microsoft.com>
2019-01-16 13:00:52 -0500

    Revert unintended changes in previous merge
    ```
    git diff 42656af718d31a9b02f59b6df492e6a53073f05c $THIS_COMMIT
    
    diff --git a/.gitmodules b/.gitmodules
    index b305840f..7f835877 100644
    --- a/.gitmodules
    +++ b/.gitmodules
    @@ -25,7 +25,7 @@
     [submodule "external/mono"]
         path = external/mono
         url = https://github.com/mono/mono.git
    -    branch = 2018-08
    +    branch = 2018-10
     [submodule "external/mxe"]
         path = external/mxe
         url = https://github.com/xamarin/mxe.git
    
    diff --git a/Configuration.props b/Configuration.props
    index a1785e4d..803438ab 100644
    --- a/Configuration.props
    +++ b/Configuration.props
    @@ -73,8 +73,8 @@
         <JavaInteropSourceDirectory Condition=" '$(JavaInteropSourceDirectory)' == '' ">$(MSBuildThisFileDirectory)external\Java.Interop</JavaInteropSourceDirectory>
         <LlvmSourceDirectory Condition=" '$(LlvmSourceDirectory)' == '' ">$(MSBuildThisFileDirectory)external\llvm</LlvmSourceDirectory>
         <MonoSourceDirectory>$(MSBuildThisFileDirectory)external\mono</MonoSourceDirectory>
    -    <MonoRequiredMinimumVersion Condition=" '$(MonoRequiredMinimumVersion)' == '' ">5.18.0</MonoRequiredMinimumVersion>
    -    <MonoRequiredMaximumVersion Condition=" '$(MonoRequiredMaximumVersion)' == '' ">5.19.0</MonoRequiredMaximumVersion>
    +    <MonoRequiredMinimumVersion Condition=" '$(MonoRequiredMinimumVersion)' == '' ">5.20.0</MonoRequiredMinimumVersion>
    +    <MonoRequiredMaximumVersion Condition=" '$(MonoRequiredMaximumVersion)' == '' ">5.21.0</MonoRequiredMaximumVersion>
         <IgnoreMaxMonoVersion Condition=" '$(IgnoreMaxMonoVersion)' == '' ">True</IgnoreMaxMonoVersion>
         <MonoRequiredDarwinMinimumVersion>$(MonoRequiredMinimumVersion).0</MonoRequiredDarwinMinimumVersion>
         <LinkerSourceDirectory>$(MSBuildThisFileDirectory)external\mono\external\linker</LinkerSourceDirectory>
    
    diff --git a/build-tools/dependencies/dependencies.projitems b/build-tools/dependencies/dependencies.projitems
    index c9a26b9e..37612ea3 100644
    --- a/build-tools/dependencies/dependencies.projitems
    +++ b/build-tools/dependencies/dependencies.projitems
    @@ -1,7 +1,7 @@
     <?xml version="1.0" encoding="utf-8"?>
     <Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
       <PropertyGroup>
    -    <_DarwinMonoFramework>MonoFramework-MDK-5.18.0.162.macos10.xamarin.universal.pkg</_DarwinMonoFramework>
    +    <_DarwinMonoFramework>MonoFramework-MDK-5.20.0.81.macos10.xamarin.universal.pkg</_DarwinMonoFramework>
         <_AptGetInstall>apt-get -f -u install</_AptGetInstall>
       </PropertyGroup>
       <ItemGroup>
    @@ -59,7 +59,7 @@
           <MaximumVersion Condition=" '$(IgnoreMaxMonoVersion)' == '' Or '$(IgnoreMaxMonoVersion)' == 'False' " >$(MonoRequiredMaximumVersion)</MaximumVersion>
           <DarwinMinimumVersion>$(MonoRequiredDarwinMinimumVersion)</DarwinMinimumVersion>
           <CurrentVersionCommand>$(MSBuildThisFileDirectory)..\scripts\mono-version</CurrentVersionCommand>
    -      <DarwinMinimumUrl>https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2018-08/128/bc9d709e704fb157eaf4935cb67d272f154cff75/$(_DarwinMonoFramework)</DarwinMinimumUrl>
    +      <DarwinMinimumUrl>https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2018-10/68/3d170b9afe0cd3b219eadeceb06ad49ac670f48d/$(_DarwinMonoFramework)</DarwinMinimumUrl>
           <DarwinInstall>installer -pkg "$(AndroidToolchainCacheDirectory)\$(_DarwinMonoFramework)" -target /</DarwinInstall>
         </RequiredProgram>
       </ItemGroup>
    
    diff --git a/external/mono b/external/mono
    index 51c4f454..64c566d8 160000
    --- a/external/mono
    +++ b/external/mono
    @@ -1 +1 @@
    -Subproject commit 51c4f454022c7a94115a82998556e92d4b8724a2
    +Subproject commit 64c566d8da21a29215c8abfc90fafe9e825404bf
    
    ```

--- tests/BCL-Tests/Xamarin.Android.Bcl-Tests/XUnitInstrumentation.cs
+++ tests/BCL-Tests/Xamarin.Android.Bcl-Tests/XUnitInstrumentation.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using System.Text;
 
 using Android.App;
 using Android.Runtime;
-using Xamarin.Android.UnitTests;
+
 using Xamarin.Android.UnitTests.XUnit;
 
 namespace xUnitTestRunner
@@ -35,6 +34,7 @@ namespace xUnitTestRunner
 
 		void CommonInit ()
 		{
+			TestAssembliesGlobPattern = "*_xunit-test.dll";
 			string cacheDir = Path.Combine (Application.Context.CacheDir.AbsolutePath, DefaultLogTag);
 			using (var files = typeof (XUnitInstrumentation).Assembly.GetManifestResourceStream ("bcl-tests.zip")) {
 				ExtractAssemblies (cacheDir, files);
@@ -78,18 +78,6 @@ namespace xUnitTestRunner
 			}
 
 			runner.SetFilters (filters);
-		}
-
-		protected override IEnumerable<TestAssemblyInfo> GetTestAssembliesFromDirectory (string directoryPath)
-		{
-			using (var reader = new StreamReader (typeof (XUnitInstrumentation).Assembly.GetManifestResourceStream ("xunit-assemblies.txt")))
-			{
-				string line;
-				while ((line = reader.ReadLine ()) != null)
-				{
-					yield return LoadTestAssembly (Path.Combine (directoryPath, line));
-				}
-			}
 		}
 	}
 }
