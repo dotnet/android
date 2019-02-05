75b049bd1ea42eb47be2d1f99d44cb2606d398d2
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-02-05 10:38:03 -0600

    [Xamarin.Android.Build.Tasks] warning if minSdkVersion < 9
    Fixes: https://github.com/xamarin/xamarin-android/issues/2692
    
    Xamarin.Android does not currently support devices less than API-9.
    The resulting `.apk` file will reference the
    [`ApplicationInfo.nativeLibraryDir` field][1], which was added in
    API-9, and thus may prevent `classes.dex` from being loaded.
    
    A XA4216 warning is now issued, stating that this configuration
    is not supported. Added a test verifying this warning.
    
    [1]: https://developer.android.com/reference/android/content/pm/ApplicationInfo#nativeLibraryDir

--- src/Xamarin.Android.Build.Tasks/Tasks/GetJavaPlatformJar.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/GetJavaPlatformJar.cs
@@ -13,6 +13,7 @@ namespace Xamarin.Android.Tasks
 {
 	public class GetJavaPlatformJar : Task
 	{
+		const int MinSupportedSdkVersion = 9;
 		private XNamespace androidNs = "http://schemas.android.com/apk/res/android";
 
 		[Required]
@@ -51,6 +52,23 @@ namespace Xamarin.Android.Tasks
 
 							if (target_sdk != null && !string.IsNullOrWhiteSpace (target_sdk.Value))
 								platform = target_sdk.Value;
+
+							var min_sdk = uses_sdk.Attribute (androidNs + "minSdkVersion");
+							if (!int.TryParse (min_sdk?.Value, out int minSdkVersion) || minSdkVersion < MinSupportedSdkVersion) {
+								LogWarningForManifest (
+										warningCode:      "XA4216",
+										attribute:        min_sdk,
+										message:          "AndroidManifest.xml //uses-sdk/@android:minSdkVersion '{0}' is less than API-{1}, this configuration is not supported.",
+										messageArgs:      new object [] { min_sdk?.Value, MinSupportedSdkVersion }
+								);
+							} else if (!int.TryParse (target_sdk?.Value, out int targetSdkVersion) || targetSdkVersion < MinSupportedSdkVersion) {
+								LogWarningForManifest (
+										warningCode:      "XA4216",
+										attribute:        target_sdk,
+										message:          "AndroidManifest.xml //uses-sdk/@android:targetSdkVersion '{0}' is less than API-{1}, this configuration is not supported.",
+										messageArgs:      new object [] { target_sdk?.Value, MinSupportedSdkVersion }
+								);
+							}
 						}
 					}
 				} catch (Exception ex) {
@@ -79,22 +97,9 @@ namespace Xamarin.Android.Tasks
 			if (int.TryParse (targetFrameworkVersion, out frameworkSdk) &&
 					int.TryParse (targetSdkVersion, out targetSdk) &&
 					targetSdk < frameworkSdk) {
-				int lineNumber    = 0;
-				int columnNumber  = 0;
-				var lineInfo      = target_sdk as IXmlLineInfo;
-				if (lineInfo != null && lineInfo.HasLineInfo ()) {
-					lineNumber    = lineInfo.LineNumber;
-					columnNumber  = lineInfo.LinePosition;
-				}
-				Log.LogWarning (
-						subcategory:      string.Empty,
+				LogWarningForManifest (
 						warningCode:      "XA4211",
-						helpKeyword:      string.Empty,
-						file:             AndroidManifest,
-						lineNumber:       lineNumber,
-						columnNumber:     columnNumber,
-						endLineNumber:    0,
-						endColumnNumber:  0,
+						attribute:        target_sdk,
 						message:          "AndroidManifest.xml //uses-sdk/@android:targetSdkVersion '{0}' is less than $(TargetFrameworkVersion) '{1}'. Using API-{2} for ACW compilation.",
 						messageArgs:      new[]{
 							targetSdkVersion,
@@ -105,6 +110,29 @@ namespace Xamarin.Android.Tasks
 				return targetFrameworkVersion;
 			}
 			return targetSdkVersion;
+		}
+
+		void LogWarningForManifest (string warningCode, XAttribute attribute, string message, params object[] messageArgs)
+		{
+			int lineNumber    = 0;
+			int columnNumber  = 0;
+			var lineInfo      = attribute as IXmlLineInfo;
+			if (lineInfo != null && lineInfo.HasLineInfo ()) {
+				lineNumber    = lineInfo.LineNumber;
+				columnNumber  = lineInfo.LinePosition;
+			}
+			Log.LogWarning (
+					subcategory:      string.Empty,
+					warningCode:      warningCode,
+					helpKeyword:      string.Empty,
+					file:             AndroidManifest,
+					lineNumber:       lineNumber,
+					columnNumber:     columnNumber,
+					endLineNumber:    0,
+					endColumnNumber:  0,
+					message:          message,
+					messageArgs:      messageArgs
+			);
 		}
 	}
 }
