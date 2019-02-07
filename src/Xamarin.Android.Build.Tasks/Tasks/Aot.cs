dotnet/android

commit cf7afcf444f4624db8d252c11c56244bc7957cc7

[Xamarin.Android.Build.Tasks] Improve Android NDK validation errors (#2709)
Fixes: https://github.com/xamarin/xamarin-android/issues/2708

Move the check for a `null` or empty `$(AndroidNdkDirectory)` from
`MakeBundleNativeCodeExternal.Execute()` into
`NdkUtil.ValidateNdkPlatform()`, and improve the error message so
that it suggests that the Android NDK be installed.

Make `NdkUtil.ValidateNdkPlatform()` fail with that same error if it
cannot get the version of the NDK.  This way, the
`<MakeBundleNativeCodeExternal/>` and `<Aot/>` tasks will both
generate the error before they try to call any other methods from
`NdkUtil`.

src/Xamarin.Android.Build.Tasks/Tasks/MakeBundleNativeCodeExternal.cs
@@ -60,10 +60,6 @@ namespace Xamarin.Android.Tasks
 			NdkUtil.Init (AndroidNdkDirectory);
 
 			try {
-				if (String.IsNullOrEmpty (AndroidNdkDirectory)) {
-					Log.LogCodedError ("XA5101", "Could not locate Android NDK. Please make sure to configure path to NDK in SDK Locations or set via /p:AndroidNdkDirectory in the MSBuild/xbuild argument.");
-					return false;
-				}
 				return DoExecute ();
 			} catch (XamarinAndroidException e) {
 				Log.LogCodedError (string.Format ("XA{0:0000}", e.Code), e.MessageWithoutCode);

src/Xamarin.Android.Build.Tasks/Tasks/NdkUtils.cs
@@ -33,7 +33,14 @@ namespace Xamarin.Android.Tasks
 
 			// Check that we have a compatible NDK version for the targeted ABIs.
 			Version ndkVersion;
-			bool hasNdkVersion = GetNdkToolchainRelease (ndkPath, out ndkVersion);
+			bool hasNdkVersion = GetNdkToolchainRelease (ndkPath ?? "", out ndkVersion);
+
+			if (!hasNdkVersion) {
+				log.LogCodedError ("XA5101",
+						"Could not locate the Android NDK. Please make sure the Android NDK is installed in the Android SDK Manager, " +
+						"or if using a custom NDK path, please ensure the $(AndroidNdkDirectory) MSBuild property is set to the custom path.");
+				return false;
+			}
 
 			if (hasNdkVersion && ndkVersion.Major < 19) {
 				log.LogMessage (MessageImportance.High,

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/MakeBundleNativeCodeExternalTests.cs
@@ -1,0 +1,67 @@
+﻿using Microsoft.Build.Framework;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Android.Tasks;
+
+namespace Xamarin.Android.Build.Tests {
+
+	[TestFixture]
+	public class MakeBundleNativeCodeExternalTests : BaseTest {
+		List<BuildErrorEventArgs> errors;
+		List<BuildWarningEventArgs> warnings;
+		List<BuildMessageEventArgs> messages;
+		MockBuildEngine engine;
+		string path;
+
+		[SetUp]
+		public void Setup ()
+		{
+			engine = new MockBuildEngine (TestContext.Out,
+				errors: errors = new List<BuildErrorEventArgs> (),
+				warnings: warnings = new List<BuildWarningEventArgs> (),
+				messages: messages = new List<BuildMessageEventArgs> ());
+
+			path = Path.Combine (Root, "temp", TestName);
+			TestContext.CurrentContext.Test.Properties ["Output"] = new string [] { path };
+		}
+
+		[TestCase (null)]
+		[TestCase ("")]
+		[TestCase ("DoesNotExist")]
+		public void XA5101AndroidNdkNotFound (string androidNdkDirectory)
+		{
+			var task1 = new MakeBundleNativeCodeExternal {
+				BuildEngine = engine,
+				AndroidNdkDirectory = androidNdkDirectory,
+				Assemblies = new ITaskItem [0],
+				SupportedAbis = "armeabi-v7a",
+				TempOutputPath = path,
+				ToolPath = "",
+				BundleApiPath = ""
+			};
+
+			Assert.IsFalse (task1.Execute (), "Task should fail!");
+			BuildErrorEventArgs error1 = errors [0];
+			Assert.AreEqual ("XA5101", error1.Code);
+			StringAssert.Contains (" NDK ", error1.Message);
+			StringAssert.Contains ("AndroidNdkDirectory", error1.Message);
+			StringAssert.Contains ("SDK Manager", error1.Message);
+
+			var task2 = new Aot {
+				BuildEngine = engine,
+				AndroidNdkDirectory = androidNdkDirectory,
+				AndroidAotMode = "normal",
+				AndroidApiLevel = "28",
+				ResolvedAssemblies = new ITaskItem [0],
+				SupportedAbis = "armeabi-v7a",
+				AotOutputDirectory = path,
+				IntermediateAssemblyDir = path
+			};
+
+			Assert.IsFalse (task2.Execute (), "Task should fail!");
+			BuildErrorEventArgs error2 = errors [1];
+			Assert.AreEqual (error1.Message, error2.Message, "Aot and MakeBundleNativeCodeExternal should produce the same error messages.");
+		}
+	}
+}

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Xamarin.Android.Build.Tests.csproj
@@ -85,6 +85,7 @@
     <Compile Include="Tasks\CopyResourceTests.cs" />
     <Compile Include="Tasks\KeyToolTests.cs" />
     <Compile Include="Aapt2Tests.cs" />
+    <Compile Include="Tasks\MakeBundleNativeCodeExternalTests.cs" />
     <Compile Include="Tasks\ValidateJavaVersionTests.cs" />
     <Compile Include="ZipArchiveExTests.cs" />
     <Compile Include="ConvertResourcesCasesTests.cs" />

commit e7db5f5870e9bd0f88091aff5baf935090a4aa4f

[Xamarin.Android.Build.Tasks] Correct Android NDK existence check
Fixes the test failures introduced in: https://github.com/xamarin/xamarin-android/pull/2709

I initially authored the changes in [#2709][0] against a `master` commit
that was a few before the [switch to Android NDK r19][1].  When I
locally merged the changes onto the tip of `master`, I didn't notice
that the `MakeBundleNativeCodeExternal` and `Aot` tasks were now calling
the new method `NdkUtil.Init()` before `NdkUtil.ValidateNdkPlatform()`.
And unfortunately since there was some urgency to get the corresponding
backport ready for the `d16-0` branch, I didn't get a chance to re-build
and re-test locally against the tip of `master`.  Long story short, to
support the new order of method calls, the NDK existence check now needs
to happen at the time of the call to `GetNdkToolchainRelease()` in
`NdkUtil.Init()`.

[0]: https://github.com/xamarin/xamarin-android/pull/2709
[1]: https://github.com/xamarin/xamarin-android/commit/06e920c18b71a165eded0a5782f1a285ef20a4bc

src/Xamarin.Android.Build.Tasks/Tasks/Aot.cs
@@ -81,7 +81,8 @@ namespace Xamarin.Android.Tasks
 
 		public override bool Execute ()
 		{
-			NdkUtil.Init (AndroidNdkDirectory);
+			if (!NdkUtil.Init (Log, AndroidNdkDirectory))
+				return false;
 
 			try {
 				return DoExecute ();

src/Xamarin.Android.Build.Tasks/Tasks/MakeBundleNativeCodeExternal.cs
@@ -57,7 +57,8 @@ namespace Xamarin.Android.Tasks
 			Log.LogDebugMessage ("SupportedAbis: {0}", SupportedAbis);
 			Log.LogDebugMessage ("AutoDeps: {0}", AutoDeps);
 
-			NdkUtil.Init (AndroidNdkDirectory);
+			if (!NdkUtil.Init (Log, AndroidNdkDirectory))
+				return false;
 
 			try {
 				return DoExecute ();

src/Xamarin.Android.Build.Tasks/Tasks/NdkUtils.cs
@@ -20,18 +20,8 @@ namespace Xamarin.Android.Tasks
 
 		public static bool UsingClangNDK => usingClangNDK;
 
-		public static void Init (string ndkPath)
+		public static bool Init (TaskLoggingHelper log, string ndkPath)
 		{
-			Version ndkVersion;
-			usingClangNDK = GetNdkToolchainRelease (ndkPath, out ndkVersion) && ndkVersion.Major >= 19;
-		}
-
-		public static bool ValidateNdkPlatform (TaskLoggingHelper log, string ndkPath, AndroidTargetArch arch, bool enableLLVM)
-		{
-			if (!UsingClangNDK)
-				return NdkUtilOld.ValidateNdkPlatform (log, ndkPath, arch, enableLLVM);
-
-			// Check that we have a compatible NDK version for the targeted ABIs.
 			Version ndkVersion;
 			bool hasNdkVersion = GetNdkToolchainRelease (ndkPath ?? "", out ndkVersion);
 
@@ -41,6 +31,20 @@ namespace Xamarin.Android.Tasks
 						"or if using a custom NDK path, please ensure the $(AndroidNdkDirectory) MSBuild property is set to the custom path.");
 				return false;
 			}
+
+			usingClangNDK = ndkVersion.Major >= 19;
+
+			return true;
+		}
+
+		public static bool ValidateNdkPlatform (TaskLoggingHelper log, string ndkPath, AndroidTargetArch arch, bool enableLLVM)
+		{
+			if (!UsingClangNDK)
+				return NdkUtilOld.ValidateNdkPlatform (log, ndkPath, arch, enableLLVM);
+
+			// Check that we have a compatible NDK version for the targeted ABIs.
+			Version ndkVersion;
+			bool hasNdkVersion = GetNdkToolchainRelease (ndkPath, out ndkVersion);
 
 			if (hasNdkVersion && ndkVersion.Major < 19) {
 				log.LogMessage (MessageImportance.High,

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/NdkUtilTests.cs
@@ -34,7 +34,7 @@ namespace Xamarin.Android.Build.Tests.Tasks {
 				var ndkDir = builder.AndroidNdkDirectory;
 				var sdkDir = builder.AndroidSdkDirectory;
 				MonoAndroidHelper.AndroidSdk = new AndroidSdkInfo ((arg1, arg2) => { }, sdkDir, ndkDir);
-				NdkUtil.Init (ndkDir);
+				NdkUtil.Init (log, ndkDir);
 				var platforms = NdkUtil.GetSupportedPlatforms (ndkDir);
 				Assert.AreNotEqual (0, platforms.Count (), "No platforms found");
 				var arch = AndroidTargetArch.X86;