376f8a6fa8c7ee60a2bdcc3e8776013d4c556a5c
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-02-19 09:32:12 -0600

    [BootstrapTasks] use libZipSharp in <UnzipDirectoryChildren/>
    Context: https://github.com/xamarin/xamarin-android/pull/2666
    
    In #2666, it appears we are hitting a bug in .NET framework on Windows
    server machines?
    
        System.AggregateException: One or more errors occurred. ---> System.NullReferenceException: Object reference not set to an instance of an object.
            at System.IO.Compression.Zip64EndOfCentralDirectoryLocator.TryReadBlock(BinaryReader reader, Zip64EndOfCentralDirectoryLocator& zip64EOCDLocator)
            at System.IO.Compression.ZipFileExtensions.ExtractToFile(ZipArchiveEntry source, String destinationFileName, Boolean overwrite)
            at Xamarin.Android.Tools.BootstrapTasks.UnzipDirectoryChildren.<ExtractFile>d__22.MoveNext()
            --- End of inner exception stack trace ---
            at System.Threading.Tasks.Task.WaitAll(Task[] tasks, Int32 millisecondsTimeout, CancellationToken cancellationToken)
            at Xamarin.Android.Tools.BootstrapTasks.UnzipDirectoryChildren.Execute()
            at Microsoft.Build.BackEnd.TaskExecutionHost.Microsoft.Build.BackEnd.ITaskExecutionHost.Execute()
            at Microsoft.Build.BackEnd.TaskBuilder.<ExecuteInstantiatedTask>d__26.MoveNext()
            ---> (Inner Exception #0) System.NullReferenceException: Object reference not set to an instance of an object.
            at System.IO.Compression.Zip64EndOfCentralDirectoryLocator.TryReadBlock(BinaryReader reader, Zip64EndOfCentralDirectoryLocator& zip64EOCDLocator)
            at System.IO.Compression.ZipFileExtensions.ExtractToFile(ZipArchiveEntry source, String destinationFileName, Boolean overwrite)
            at Xamarin.Android.Tools.BootstrapTasks.UnzipDirectoryChildren.<ExtractFile>d__22.MoveNext()<---
            [E:\A\_work\1439\s\src\Xamarin.Android.Build.Tasks\Xamarin.Android.Build.Tasks.csproj]
    
    Which occurs when unzipping:
    
        C:\Users\dlab14\android-toolchain\sdk\extras\android\m2repository\com\android\support\multidex\1.0.1\multidex-1.0.1.aar
    
    What is also weird about this, is it only seems to break when
    `$(TargetFrameworVersion)` is set to `v4.7.2`? xamarin-android/master
    is working fine.
    
    `<UnzipDirectoryChildren/>` was looking at `$(HostOS)` and:
    
    * Using `System.IO.Compression` on Windows
    * Using `$TMP` and `unzip` + `mv` commands on other platforms
    
    But `Xamarin.Android.Tools.BootstrapTasks` already had a reference to
    `libZipSharp`? We should just use it.
    
    I refactored this MSBuild task:
    
    * Same code for all platforms
    * No temp directory needed
    * Removed `HostOS` property
    * General cleanup
    
    Hopefully this will get #2666 to work.
    
    I had to make a couple changes to *use* libZipSharp in
    `Xamarin.Android.Tools.BootstrapTasks.csproj`:
    
    * Created a `libzip-references.projitems`, that we can import in
      projects using `libZipSharp`
    * Switched `Xamarin.ProjectTools` to use this as well as
      `BootstrapTasks`

--- build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/UnzipDirectoryChildren.cs
+++ build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/UnzipDirectoryChildren.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Diagnostics;
+﻿using Microsoft.Build.Framework;
 using System.IO;
-using System.IO.Compression;
 using System.Text;
-using System.Threading.Tasks;
-
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-
-using TTask = System.Threading.Tasks.Task;
+using Xamarin.Tools.Zip;
 using MTask = Microsoft.Build.Utilities.Task;
+using TTask = System.Threading.Tasks.Task;
 
-namespace Xamarin.Android.Tools.BootstrapTasks {
+namespace Xamarin.Android.Tools.BootstrapTasks
+{
 
 	public class UnzipDirectoryChildren : MTask
 	{
-		public UnzipDirectoryChildren ()
-		{
-		}
-
 		[Required]
 		public  ITaskItem[]     SourceFiles         { get; set; }
 
 		public  string          EntryNameEncoding   { get; set; }
-
-		public  string          HostOS              { get; set; }
 
 		[Required]
 		public  ITaskItem       DestinationFolder   { get; set; }
@@ -33,13 +22,9 @@ namespace Xamarin.Android.Tools.BootstrapTasks {
 
 		public override bool Execute ()
 		{
-			Log.LogMessage (MessageImportance.Low, $"{nameof (UnzipDirectoryChildren)}:");
-			Log.LogMessage (MessageImportance.Low, "  DestinationFolder: {0}", DestinationFolder.ItemSpec);
-			Log.LogMessage (MessageImportance.Low, "  EntryNameEncoding: {0}", EntryNameEncoding);
-			Log.LogMessage (MessageImportance.Low, "  SourceFiles:");
 			for (int i = 0; i < SourceFiles.Length; ++i) {
-				var sf  = SourceFiles [i].ItemSpec;
-				var rp  = SourceFiles [i].GetMetadata ("DestDir");
+				var sf  = SourceFiles [i];
+				var rp  = sf.GetMetadata ("DestDir");
 				rp      = string.IsNullOrEmpty (rp)
 					? ""
 					: " [ " + rp + " ]";
@@ -53,113 +38,41 @@ namespace Xamarin.Android.Tools.BootstrapTasks {
 
 			Directory.CreateDirectory (DestinationFolder.ItemSpec);
 
-			var tempDir = Path.Combine (Path.GetTempPath (), Path.GetRandomFileName ());
-			Directory.CreateDirectory (tempDir);
-			Log.LogMessage ($"# jonp: tempDir: '{tempDir}'");
-
 			var encoding = string.IsNullOrEmpty (EntryNameEncoding)
 				? null
 				: Encoding.GetEncoding (EntryNameEncoding);
 
 			var tasks = new TTask [SourceFiles.Length];
 			for (int i = 0; i < SourceFiles.Length; ++i) {
-				var td              = tempDir;
-				var sourceFile      = SourceFiles [i].ItemSpec;
-				var relativeDestDir = SourceFiles [i].GetMetadata ("DestDir");
+				var sourceFile      = SourceFiles [i];
+				var relativeDestDir = sourceFile.GetMetadata ("DestDir");
 				var enc             = encoding;
 				var destFolder      = DestinationFolder.ItemSpec;
-				tasks [i] = TTask.Run (() => ExtractFile (td, sourceFile, relativeDestDir, destFolder, enc));
+				tasks [i] = TTask.Run (() => ExtractFile (sourceFile.ItemSpec, relativeDestDir, destFolder, enc));
 			}
 
 			TTask.WaitAll (tasks);
 
-			Directory.Delete (tempDir, recursive: true);
-
 			return !Log.HasLoggedErrors;
 		}
 
-		// Ignore CS1998 because there's no async System.IO APIs to use here.
-		// Instead, we're using Tasks so that we can extract multiple files
-		// in parallel via Task.Run() and Task.WaitAll().
-#pragma warning disable 1998
-		async TTask ExtractFile (string tempDir, string sourceFile, string relativeDestDir, string destinationFolder, Encoding encoding)
+		void ExtractFile (string sourceFile, string relativeDestDir, string destinationFolder, Encoding encoding)
 		{
 			relativeDestDir = relativeDestDir?.Replace ('\\', Path.DirectorySeparatorChar);
 
-			if (string.Equals (HostOS, "Windows", StringComparison.OrdinalIgnoreCase)) {
-				//NOTE: to avoid MAX_PATH, we are not using %TEMP% on Windows
-				using (var source = File.OpenRead (sourceFile))
-				using (var zip = new ZipArchive (source)) {
-					foreach (var entry in zip.Entries) {
-						//Directory entries have empty names
-						if (!string.IsNullOrEmpty (entry.Name)) {
-							//entry.FullName can have / or \ depending on your .NET version
-							var entryPath = entry.FullName.Replace ('/', Path.DirectorySeparatorChar);
-							if (!NoSubdirectory) {
-								entryPath = entryPath.Substring (entryPath.IndexOf (Path.DirectorySeparatorChar) + 1);
-							}
-							var destinationPath = Path.Combine (destinationFolder, relativeDestDir, entryPath);
-							var destinationDir = Path.GetDirectoryName (destinationPath);
-							if (!Directory.Exists (destinationDir))
-								Directory.CreateDirectory (destinationDir);
-
-							entry.ExtractToFile (destinationPath, overwrite: true);
+			using (var zip = ZipArchive.Open (sourceFile, FileMode.Open)) {
+				foreach (var entry in zip) {
+					if (!entry.IsDirectory) {
+						var entryPath = entry.NativeFullName;
+						if (!NoSubdirectory) {
+							entryPath = entryPath.Substring (entryPath.IndexOf (Path.DirectorySeparatorChar) + 1);
 						}
-					}
-				}
-			} else {
-				var tempName    = Path.GetRandomFileName ();
-				var nestedTemp  = Path.Combine (tempDir, tempName);
-				Directory.CreateDirectory (nestedTemp);
-
-				//NOTE: using unzip to preserve file attributes
-				var start   = new ProcessStartInfo ("unzip", $"\"{sourceFile}\" -d \"{nestedTemp}\"") {
-					CreateNoWindow  = true,
-					UseShellExecute = false,
-					RedirectStandardOutput = true,
-					RedirectStandardError = true,
-				};
-				Log.LogMessage (MessageImportance.Low, $"unzip \"{sourceFile}\" -d \"{nestedTemp}\"");
-				using (var p = Process.Start (start)) {
-					p.OutputDataReceived += (sender, e) => {
-						if (string.IsNullOrEmpty(e.Data))
-							return;
-						Console.Out.Write(e.Data + "\n");
-					};
-					p.ErrorDataReceived += (sender, e) => {
-						if (string.IsNullOrEmpty(e.Data))
-							return;
-						Console.Error.Write(e.Data + "\n");
-					};
-					p.BeginOutputReadLine ();
-					p.BeginErrorReadLine ();
-					p.WaitForExit ();
-				}
-
-
-				var dirs = NoSubdirectory ? new string [] { nestedTemp } : Directory.EnumerateDirectories (nestedTemp, "*");
-
-				foreach (var dir in dirs) {
-					foreach (var fse in Directory.EnumerateFileSystemEntries (dir)) {
-						var name    = Path.GetFileName (fse);
-						var destDir = string.IsNullOrEmpty (relativeDestDir)
-							? destinationFolder
-							: Path.Combine (destinationFolder, relativeDestDir);
-						Directory.CreateDirectory (destDir);
-						var dest    = Path.Combine (destDir, name);
-						Log.LogMessage (MessageImportance.Low, $"mv '{fse}' '{dest}'");
-						if (Directory.Exists (fse))
-							Process.Start ("/bin/mv", $@"""{fse}"" ""{dest}""").WaitForExit ();
-						else {
-							if (File.Exists (dest))
-								File.Delete (dest);
-							File.Move (fse, dest);
-						}
+						var destinationPath = Path.Combine (destinationFolder, relativeDestDir, entryPath);
+						entry.Extract (Path.GetDirectoryName (destinationPath), Path.GetFileName (destinationPath));
 					}
 				}
 			}
 		}
-#pragma warning restore 1998
 	}
 }
 
