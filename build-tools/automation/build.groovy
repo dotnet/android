6598e1533cf67f54ce1286ceb92dd3cbe5f887cc
Mike Bond <mjbond-msft@outlook.com>
2019-02-22 11:22:36 -0800

    publishPackages: Use env variable directly w/ no '$'

--- build-tools/automation/build.groovy
+++ build-tools/automation/build.groovy
@@ -35,10 +35,10 @@ def publishPackages(filePaths) {
     def status = 0
     try {
          // Note: The following function is provided by the Azure Blob Jenkins plugin
-         azureUpload(storageCredentialId: $env.StorageCredentialId,
+         azureUpload(storageCredentialId: env.StorageCredentialId,
                  storageType: "blobstorage",
-                 containerName: $env.ContainerName,
-                 virtualPath: $env.StorageVirtualPath,
+                 containerName: env.ContainerName,
+                 virtualPath: env.StorageVirtualPath,
                  filesPath: $filePaths,
                  allowAnonymousAccess: true,
                  pubAccessible: true,
