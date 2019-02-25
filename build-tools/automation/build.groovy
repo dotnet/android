41786a14a10af00764d4ebf700f9116de429c066
Mike Bond <mjbond-msft@outlook.com>
2019-02-25 14:16:49 -0800

    Ensure current directory is set to the xamarin-android directory prior to building package-test-errors

--- build-tools/automation/build.groovy
+++ build-tools/automation/build.groovy
@@ -133,7 +133,11 @@ timestamps {
 
         stageWithTimeout('publish test error logs to Azure', 10, 'MINUTES', '', false) {  // Typically takes less than a minute
             echo "packaging test error logs"
-            sh "make package-test-errors"
+
+            sh """
+                cd ${XADir}
+                make package-test-errors
+               """
 
             def publishTestFilePaths = "${XADir}/xa-test-errors*"
             echo "publishTestFilePaths: ${publishTestFilePaths}"
