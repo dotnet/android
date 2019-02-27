90e4bbb5fc9f21af7391c670e7e873055a07810a
Peter Collins <peter.collins@xamarin.com>
2019-02-27 15:22:56 -0500

    [Mono.Android-Tests] Import additional tests
    Imports tests from https://github.com/xamarin/QualityAssurance/tree/b5e5c842b06dd88357443ceb5a4fa547b0943186/Automation/XARegressionTests.
    
    Some tests which represented duplicate coverage were ignored during this
    import. There is however likely still _some_ level of duplication in the
    imported tests, but given how quickly they execute it didn't seem
    worthwhile to hunt down alternate coverage areas for every test.
    
    Both Runtime test projects have had their targetSdkVersion bumped to 28,
    and as a result the `android:usesCleartextTraffic="true"` application
    attribute has been added to their AndroidManifest.xml files. This allows
    us to avoid a breaking behavior change for applications targeting API 28:
    https://developer.android.com/about/versions/pie/android-9.0-changes-28#tls-enabled
    
    Additional changes have been made to improve the ability to work with
    Mono.Android-Test.csproj (and related projects) after opening
    Xamarin.Android-Tests.sln in Visual Studio on macOS or Windows.
    
    The new .png resources come from https://undraw.co/.

---
+++ src/Mono.Android/Test/Android.Widget/CustomWidgetTests.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using NUnit.Framework;
+
+namespace Xamarin.Android.RuntimeTests
+{
+	[TestFixture]
+	public class CustomWidgetTests
+	{
+		// https://bugzilla.xamarin.com/show_bug.cgi?id=23880
+		[Test]
+		public void UpperCaseCustomWidget_ShouldNotThrowInflateException ()
+		{
+			Assert.DoesNotThrow (() => {
+				var inflater = (LayoutInflater)Application.Context.GetSystemService (Context.LayoutInflaterService);
+				inflater.Inflate (Resource.Layout.uppercase_custom, null);
+			}, "Regression test for widget with uppercase namespace (bug #23880) failed");
+		}
+
+		[Test]
+		public void LowerCaseCustomWidget_ShouldNotThrowInflateException ()
+		{
+			Assert.DoesNotThrow (() => {
+				var inflater = (LayoutInflater)Application.Context.GetSystemService(Context.LayoutInflaterService);
+				inflater.Inflate(Resource.Layout.lowercase_custom, null);
+			}, "Regression test for widget with lowercase namespace (bug #23880) failed.");
+		}
+
+		[Test]
+		public void UpperAndLowerCaseCustomWidget_FromLibrary_ShouldNotThrowInflateException ()
+		{
+			Assert.DoesNotThrow (() => {
+				var inflater = (LayoutInflater)Application.Context.GetSystemService (Context.LayoutInflaterService);
+				inflater.Inflate (Resource.Layout.upper_lower_custom, null);
+			}, "Regression test for widgets with uppercase and lowercase namespace (bug #23880) failed.");
+		}
+	}
+
+	public class CustomButton : Button
+	{
+		public CustomButton (Context context) : base (context)
+		{
+		}
+
+		public CustomButton (Context context, IAttributeSet attributes) : base (context, attributes)
+		{
+		}
+	}
+}
---
+++ src/Mono.Android/Test/Mono.Android-Test.Library/CustomTextView.cs
@@ -0,0 +1,13 @@
+﻿using Android.Content;
+using Android.Util;
+using Android.Widget;
+
+namespace Mono.Android_Test.Library
+{
+	public class CustomTextView : TextView
+	{
+		public CustomTextView (Context context, IAttributeSet attributes) : base (context, attributes)
+		{
+		}
+	}
+}
