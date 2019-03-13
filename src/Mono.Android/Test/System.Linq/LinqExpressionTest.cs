239b3eb93732430d33b1408718acee72f6a1a7be
Peter Collins <pecolli@microsoft.com>
2019-03-12 21:57:00 -0400

    [Mono.Android-Tests] Import additional tests (#2773)
    Imports tests from [xamarin/QualityAssurance@b5e5c842][0].
    
    Some tests which represented duplicate coverage were ignored during
    this import.  There is however likely still *some* level of
    duplication in the imported tests, but given how quickly they execute
    it didn't seem worthwhile to hunt down alternate coverage areas for
    every test.
    
    Both Runtime test projects have had their `targetSdkVersion` value
    bumped to 28, and as a result the
    `//application[@android:usesCleartextTraffic="true"]` attribute has
    been added to their AndroidManifest.xml files.  This allows us to
    avoid a [breaking behavior change][1] for applications targeting API-28:
    
    > **Network TLS enabled by default**
    >
    > If your app targets Android 9 or higher, the
    > [`isCleartextTrafficPermitted()`][2] method returns `false` by
    > default.  If your app needs to enable cleartext for specific
    > domains, you must explicitly set `cleartextTrafficPermitted` to
    > `true` for those domains in your app's
    > [Network Security Configuration][3].
    
    Additional changes have been made to improve the ability to work with
    `Mono.Android-Test.csproj` (and related projects) after opening
    `Xamarin.Android-Tests.sln` in Visual Studio on macOS or Windows.
    
    The new `.png` resources come from <https://undraw.co/>.
    
    [0]: https://github.com/xamarin/QualityAssurance/tree/b5e5c842b06dd88357443ceb5a4fa547b0943186/Automation/XARegressionTests
    [1]: https://developer.android.com/about/versions/pie/android-9.0-changes-28#tls-enabled
    [2]: https://developer.android.com/reference/android/security/NetworkSecurityPolicy.html#isCleartextTrafficPermitted()
    [3]: https://developer.android.com/training/articles/security-config.html

---
+++ src/Mono.Android/Test/System.Linq/LinqExpressionTest.cs
@@ -0,0 +1,26 @@
+﻿using Android.Runtime;
+using NUnit.Framework;
+using System.Linq.Expressions;
+
+namespace System.LinqTests
+{
+	// https://github.com/xamarin/xamarin-macios/blob/6b5870d668fe98b61b707101a0b9491480a535fa/tests/linker/ios/link%20all/LinqExpressionTest.cs
+	[TestFixture]
+	// we want the tests to be available because we use the linker
+	[Preserve (AllMembers = true)]
+	public class LinqExpressionTest
+	{
+		delegate object Bug14863Delegate ();
+
+		[Test]
+		public void Expression_T_Ctor ()
+		{
+			var ctor = typeof (LinqExpressionTest).GetConstructor (Type.EmptyTypes);
+			var expr = Expression.New (ctor, new Expression[0]);
+			Assert.NotNull (Expression.Lambda (typeof (Bug14863Delegate), expr, null), "Lambda");
+			// note: reflection is used to create an instance of Expression<TDelegate> using an internal ctor
+			// it can be indirectly "preserved" by other code (in Expression) but it can fail in other cases
+			// ref: https://bugzilla.xamarin.com/show_bug.cgi?id=14863
+		}
+	}
+}
---
+++ src/Mono.Android/Test/System.Net/WebSocketTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.NetTests
+{
+	[TestFixture]
+	public class WebSocketTests
+	{
+		[Test, Category ("InetAccess")]
+		public void TestSocketConnection()
+		{
+			string testMessage = "This is a test!";
+			var messageBytes = CustomWebSocket.GetBytes (testMessage);
+			CustomWebSocket.BytesSize = messageBytes.Length;
+			var result = CustomWebSocket.Connect ("ws://echo.websocket.org", messageBytes).Result;
+			Assert.AreEqual (result, testMessage, $"Socket test failed. Expected: {testMessage}, Received: {result}");
+		}
+	}
+
+	public class CustomWebSocket
+	{
+		static string ResponseMessage { get; set; }
+		public static int BytesSize { get; set; }
+		static object consoleLock = new object ();
+		static readonly TimeSpan delay = TimeSpan.FromMilliseconds (1000);
+
+		public static async Task<string> Connect (string uri, byte[] message)
+		{
+			ClientWebSocket clientWebSocket = null;
+			clientWebSocket = new ClientWebSocket ();
+			await clientWebSocket.ConnectAsync (new Uri (uri), CancellationToken.None).ConfigureAwait (false);
+			await Task.WhenAll (Receive (clientWebSocket), Send(clientWebSocket, message)).ConfigureAwait (false);
+
+			if (clientWebSocket != null)
+				clientWebSocket.Dispose ();
+
+			lock (consoleLock)
+			{
+				Console.WriteLine ("Client WebSocket closed");
+				return ResponseMessage;
+			}
+		}
+
+		static async Task Send (ClientWebSocket webSocket, byte[] message)
+		{
+			if (webSocket.State == WebSocketState.Open)
+			{
+				await webSocket.SendAsync (new ArraySegment<byte> (message), WebSocketMessageType.Binary, false, CancellationToken.None);
+				LogStatus (false, message, message.Length);
+				await Task.Delay (delay);
+			}
+		}
+
+		static async Task Receive (ClientWebSocket webSocket)
+		{
+			byte[] buffer = new byte[BytesSize];
+			if (webSocket.State == WebSocketState.Open)
+			{
+				var result = await webSocket.ReceiveAsync (new ArraySegment<byte> (buffer), CancellationToken.None);
+				if (result.MessageType == WebSocketMessageType.Close)
+				{
+					await webSocket.CloseAsync (WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+				}
+				else
+				{
+					LogStatus (true, buffer, result.Count);
+				}
+			}
+		}
+
+		static void LogStatus (bool receiving, byte[] buffer, int length)
+		{
+			lock (consoleLock)
+			{
+				Console.Write (string.Format ("{0} {1} bytes", receiving ? "Received" : "Sent", length));
+				Console.WriteLine (GetString (buffer));
+
+				if (receiving)
+					ResponseMessage = GetString (buffer);
+			}
+		}
+
+		public static byte[] GetBytes (string str)
+		{
+			byte[] bytes = new byte[str.Length * sizeof (char)];
+			Buffer.BlockCopy (str.ToCharArray (), 0, bytes, 0, bytes.Length);
+			return bytes;
+		}
+
+		static string GetString (byte[] bytes)
+		{
+			char[] chars = new char[bytes.Length / sizeof (char)];
+			Buffer.BlockCopy (bytes, 0, chars, 0, bytes.Length);
+			return new string (chars);
+		}
+	}
+}
