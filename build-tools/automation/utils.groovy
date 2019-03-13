058f5044f0f0c26fdd6c89efe84809425a511d82
Mike Bond <mjbond-msft@outlook.com>
2019-03-13 13:05:40 -0700

    Can shared groovy lib be initialized in global scope?

--- build-tools/automation/utils.groovy
+++ build-tools/automation/utils.groovy
@@ -1,3 +1,8 @@
+// UNDONE: TEST: Can this be called via an instance variable initialized in the global scope
+def loadTest() {
+    echo "loadTest: we're in"
+}
+
 def publishPackages(filePaths) {
     def status = 0
     try {
