e3878b4e946c5cefc8a0d61493fcb821217ca185
Dean Ellis <dellis1972@googlemail.com>
2019-03-13 17:09:36 +0000

    [Xamarin.Android.Build.Tasks] Add custom meta data to @(ReferencePath)
    As part of ongoing work with the support libraries
    we will want to start adding custom metadata to
    the assembly references.
    
    This data will inform the build process that it
    can skip processing of certain items. Or remove
    that assembly completely from the final package.
    
    We did some initial work on this in commit 02c07ed6.
    However that was strickly limited to the
    `ConvertResourcesCases` task. We now need a more
    generic solution.
    
    In this case we have a new Task
    
    	`AppendCustomMetadataToItemGroup`
    
    This task takes two item groups and will copy over
    the metadata between the two. This is based on
    a filename comparision. The output will be
    the `Inputs` list but with any additional metadata
    added to the appropriate items. We can then use
    this list in the build process and qurey the
    metadata as needed.
    
    The `MetaDataItems` list can contain a number of
    different ItemGroup with different metadata. If
    this is the case the result will be a union of the
    two.
    
    ```
    <ItemGroup>
      <I Include="Foo" />
      <A Include="Foo">
        <Meta1>True</Meta1>
      </A>
      <B Include="Foo">
        <Meta2>False</Meta2>
      </B>
    </ItemGroup
    ```
    
    Given the above the result will be as follows
    when `@(A);@(B)` are passed as the `MetaDataItems`
    
    ```
    <ItemGroup>
      <I Include="Foo>
        <Meta1>True</Meta1>
        <Meta2>False</Meta2>
      </I>
    </ItemGroup>
    ```

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/AppendCustomMetadataToItemGroup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Android.Tasks.Tasks {
+	public class AppendCustomMetadataToItemGroup : Task {
+		[Required]
+		public ITaskItem[] Inputs { get; set; }
+
+		[Required]
+		public ITaskItem[] MetaDataItems { get; set; }
+
+		[Output]
+		public ITaskItem[] Output { get; set; }
+
+		public override bool Execute ()
+		{
+			var output = new List<ITaskItem> ();
+
+			foreach (var item in Inputs) {
+				var fn = Path.GetFileNameWithoutExtension (item.ItemSpec);
+				output.Add (item);
+				foreach (var metaData in MetaDataItems) {
+					if (metaData.ItemSpec != fn)
+						continue;
+					Log.LogDebugMessage ($"Copying MetaData for {item.ItemSpec}");
+					metaData.CopyMetadataTo (item);
+				}
+			}
+
+			Output = output.ToArray ();
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
