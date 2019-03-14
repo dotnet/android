53296a72acef56e1d72f0f2a09fced9bd7a9c5fc
Mike Bond <mjbond-msft@outlook.com>
2019-03-14 09:53:00 -0700

    Refactor stageWithTimeout to utils.groovy

--- build-tools/automation/build.linux.groovy
+++ build-tools/automation/build.linux.groovy
@@ -6,39 +6,6 @@ def pBuilderBindMounts = null
 def chRootPackages = 'xvfb xauth mono-devel autoconf automake build-essential vim-common p7zip-full cmake gettext libtool libgdk-pixbuf2.0-dev intltool pkg-config ruby scons wget xz-utils git nuget ca-certificates-mono clang g++-mingw-w64 gcc-mingw-w64 libzip-dev openjdk-8-jdk unzip lib32stdc++6 lib32z1 libtinfo-dev:i386 linux-libc-dev:i386 zlib1g-dev:i386 gcc-multilib g++-multilib referenceassemblies-pcl zip fsharp psmisc libz-mingw-w64-dev msbuild mono-csharp-shell devscripts fakeroot debhelper libsqlite3-dev sqlite3 libc++-dev cli-common-dev mono-llvm-support curl'
 def utils = null
 
-def stageWithTimeout(stageName, timeoutValue, timeoutUnit, directory, fatal, ctAttempts = 0, Closure body) {
-    try {
-        stage(stageName) {
-            def retryAttempt = 0
-            def waitSecondsBeforeRetry = 15
-            retry(ctAttempts) {     // Retry will always invoke the body at least once for an attempt count of 0 or 1
-                timeout(time: timeoutValue, unit: timeoutUnit) {
-                    dir(directory) {
-                        if (retryAttempt > 0) {
-                            echo "WARNING : Stage ${stageName} failed on try #${retryAttempt}. Waiting ${waitSecondsBeforeRetry} seconds"
-                            sleep(waitSecondsBeforeRetry)
-                            echo "Retrying ..."
-                            waitSecondsBeforeRetry = waitSecondsBeforeRetry * 2
-                        }
-
-                        retryAttempt++
-                        body()
-                    }
-                }
-            }
-        }
-    } catch (error) {
-        def result = fatal ? 'ERROR' : 'UNSTABLE'
-        echo "ERROR : ${stageName}: Unexpected error: ${error}. Marking build as ${result}."
-        currentBuild.result = result
-        if (fatal) {
-            throw error
-        }
-    } finally {
-        echo "Stage result: ${stageName}: ${currentBuild.currentResult}"
-    }
-}
-
 def execChRootCommand(chRootName, chRootPackages, pBuilderBindMounts, makeCommand) {
     chroot chrootName: chRootName,
         additionalPackages: chRootPackages,
@@ -55,14 +22,14 @@ def execChRootCommand(chRootName, chRootPackages, pBuilderBindMounts, makeComman
 timestamps {
     node("${env.BotLabel}") {
         def scmVars
+        utils = load "build-tools/automation/utils.groovy"
 
-        stageWithTimeout('checkout', 60, 'MINUTES', XADir, true, 3) {    // Time ranges from seconds to minutes depending on how many changes need to be brought down
+        utils.stageWithTimeout('checkout', 60, 'MINUTES', XADir, true, 3) {    // Time ranges from seconds to minutes depending on how many changes need to be brought down
             sh "env"
             scmVars = checkout scm
         }
 
-        stageWithTimeout('init', 30, 'SECONDS', XADir, true) {    // Typically takes less than a second
-            utils = load "build-tools/automation/utils.groovy"
+        utils.stageWithTimeout('init', 30, 'SECONDS', XADir, true) {    // Typically takes less than a second
 
             // Note: PR plugin environment variable settings available here: https://wiki.jenkins.io/display/JENKINS/GitHub+pull+request+builder+plugin
             def branch = scmVars.GIT_BRANCH
@@ -79,24 +46,24 @@ timestamps {
             echo "pBuilderBindMounts: ${pBuilderBindMounts}"
         }
 
-        stageWithTimeout('build', 6, 'HOURS', XADir, true) {    // Typically takes 4 hours
+        utils.stageWithTimeout('build', 6, 'HOURS', XADir, true) {    // Typically takes 4 hours
             execChRootCommand(env.ChRootName, chRootPackages, pBuilderBindMounts,
                                 "make jenkins CONFIGURATION=${env.BuildFlavor} V=1 NO_SUDO=true MSBUILD_ARGS='/p:MonoRequiredMinimumVersion=5.12'")
         }
 
-        stageWithTimeout('package deb', 30, 'MINUTES', XADir, true) {    // Typically takes less than 5 minutes
+        utils.stageWithTimeout('package deb', 30, 'MINUTES', XADir, true) {    // Typically takes less than 5 minutes
             execChRootCommand(env.ChRootName, chRootPackages, pBuilderBindMounts,
                                 "make package-deb CONFIGURATION=${env.BuildFlavor} V=1")
         }
 
-        stageWithTimeout('build tests', 30, 'MINUTES', XADir, true) {    // Typically takes less than 10 minutes
+        utils.stageWithTimeout('build tests', 30, 'MINUTES', XADir, true) {    // Typically takes less than 10 minutes
             // Occasionally `make run-all-tests` "hangs"; we believe this might be a mono/2018-06 bug.
             // We'll install mono/2018-02 on the build machines and try using that, which requires
             execChRootCommand(env.ChRootName, chRootPackages, pBuilderBindMounts,
                                 "xvfb-run -a -- make all-tests CONFIGURATION=${env.BuildFlavor} V=1")
         }
 
-        stageWithTimeout('process build results', 10, 'MINUTES', XADir, true) {    // Typically takes less than a minute
+        utils.stageWithTimeout('process build results', 10, 'MINUTES', XADir, true) {    // Typically takes less than a minute
             try {
                 echo "processing build status"
                 execChRootCommand(env.ChRootName, chRootPackages, pBuilderBindMounts,
@@ -106,7 +73,7 @@ timestamps {
             }
         }
 
-        stageWithTimeout('publish packages to Azure', 30, 'MINUTES', '', true, 3) {    // Typically takes less than a minute, but provide ample time in situations where logs may be quite large
+        utils.stageWithTimeout('publish packages to Azure', 30, 'MINUTES', '', true, 3) {    // Typically takes less than a minute, but provide ample time in situations where logs may be quite large
         
             def publishBuildFilePaths = "${XADir}/xamarin-android/*xamarin.android*.tar*";
             publishBuildFilePaths = "${publishBuildFilePaths},${XADir}/xamarin-android/bin/${env.BuildFlavor}/bundle-*.zip"
@@ -122,7 +89,7 @@ timestamps {
             }
         }
 
-        stageWithTimeout('run all tests', 360, 'MINUTES', XADir, false) {
+        utils.stageWithTimeout('run all tests', 360, 'MINUTES', XADir, false) {
             echo "running tests"
 
             execChRootCommand(env.ChRootName, chRootPackages, pBuilderBindMounts,
@@ -132,7 +99,7 @@ timestamps {
                     """)
         }
 
-        stageWithTimeout('publish test error logs to Azure', 30, 'MINUTES', '', false, 3) {  // Typically takes less than a minute, but provide ample time in situations where logs may be quite large
+        utils.stageWithTimeout('publish test error logs to Azure', 30, 'MINUTES', '', false, 3) {  // Typically takes less than a minute, but provide ample time in situations where logs may be quite large
             echo "packaging test error logs"
 
             // UNDONE: TEST: Copied from build.groovy. Does this work for Linux build?
@@ -149,7 +116,7 @@ timestamps {
             }
         }
 
-        stageWithTimeout('Publish test results', 5, 'MINUTES', XADir, false, 3) {    // Typically takes under 1 minute to publish test results
+        utils.stageWithTimeout('Publish test results', 5, 'MINUTES', XADir, false, 3) {    // Typically takes under 1 minute to publish test results
             def initialStageResult = currentBuild.currentResult
 
             xunit thresholds: [
--- build-tools/automation/utils.groovy
+++ build-tools/automation/utils.groovy
@@ -1,3 +1,36 @@
+def stageWithTimeout(stageName, timeoutValue, timeoutUnit, directory, fatal, ctAttempts = 0, Closure body) {
+    try {
+        stage(stageName) {
+            def retryAttempt = 0
+            def waitSecondsBeforeRetry = 15
+            retry(ctAttempts) {     // Retry will always invoke the body at least once for an attempt count of 0 or 1
+                timeout(time: timeoutValue, unit: timeoutUnit) {
+                    dir(directory) {
+                        if (retryAttempt > 0) {
+                            echo "WARNING : Stage ${stageName} failed on try #${retryAttempt}. Waiting ${waitSecondsBeforeRetry} seconds"
+                            sleep(waitSecondsBeforeRetry)
+                            echo "Retrying ..."
+                            waitSecondsBeforeRetry = waitSecondsBeforeRetry * 2
+                        }
+
+                        retryAttempt++
+                        body()
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        def result = fatal ? 'ERROR' : 'UNSTABLE'
+        echo "ERROR : ${stageName}: Unexpected error: ${error}. Marking build as ${result}."
+        currentBuild.result = result
+        if (fatal) {
+            throw error
+        }
+    } finally {
+        echo "Stage result: ${stageName}: ${currentBuild.currentResult}"
+    }
+}
+
 def publishPackages(storageCredentialId, containerName, storageVirtualPath, filePaths) {
     def status = 0
     try {
