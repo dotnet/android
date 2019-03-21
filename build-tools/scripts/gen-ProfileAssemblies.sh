dotnet/android

commit 2d85fa86893770b2009538d7f4972b5df15bd0cf

Remove added xUnit test assemblies
Commit 08627bed built! yay!

Unfortunately, the BCL xUnit process crashed:

	[$xUnit] Test FAILED [monodroid_System.Core_xunit-test, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null] (TaskId:58)
	   Test name: System.IO.Pipes.Tests.AnonymousPipeTest_Write_ServerOut_ClientIn.WriteWithOutOfBoundsArray_Throws_ArgumentException (TaskId:58)
	   Exception messages: System.PlatformNotSupportedException : Operation is not supported on this platform. (TaskId:58)

In the interests of "expediency" and "sanity", *remove* all of the
unit test assemblies added in 08627bed, and let's see if we can be on
"equal" footing to a non-mono-archive build.

build-tools/scripts/gen-ProfileAssemblies.sh
@@ -1,0 +1,68 @@
+#!/bin/bash -e
+
+# Run this script as part of a "mono bunp", after a "mono archive"
+# is available for use.
+#
+# See also the `_DownloadArchive` target in `src/mono-runtimes/mono-runtimes.targets`
+
+cat <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<!-- This is a GENERATED FILE -->
+<!-- See build-tools/scripts/gen-ProfileAssemblies.sh -->
+<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+EOF
+
+function PrintItemGroup()
+{
+	local item="$1"
+	local dir="$2"
+
+	echo "  <ItemGroup>"
+
+	for f in `find "$dir" -depth 1 -name \*.dll | sed 's,.dll$,,g' | grep -v 'nunitlite' | sort -f` ; do
+		local n=`basename "$f"` ;
+		echo "    <$item Include=\"$n.dll\" />" ;
+	done
+
+	echo "  </ItemGroup>"
+}
+
+function PrintTestItemGroup()
+{
+	local dir="$1"
+
+	echo "  <ItemGroup>"
+
+	for f in `find "$dir" -depth 1 -name \*.dll | sed 's,.dll$,,g' | sort -f` ; do
+		local n=`basename "$f"` ;
+		echo "    <MonoTestAssembly Include=\"$n.dll\">" ;
+		if [[ "$n" == *xunit-test ]]; then
+			echo "      <TestType>xunit</TestType>";
+		fi
+		echo "    </MonoTestAssembly>"
+		for s in `find "$dir" -depth 2 -name "$n.resources.dll" | sed 's,.dll$,,g' | sort -f` ; do
+			local p="${s:${#dir}}"
+			p="${p:1}"
+			echo "    <MonoTestSatelliteAssembly Include=\"$p.dll\" />"
+		done
+	done
+
+	echo "  </ItemGroup>"
+}
+
+# MonoFacadeAssembly
+PrintItemGroup "MonoFacadeAssembly"   "external/mono/sdks/out/android-bcl/monodroid/Facades"
+PrintItemGroup "MonoProfileAssembly"  "external/mono/sdks/out/android-bcl/monodroid"
+PrintTestItemGroup                    "external/mono/sdks/out/android-bcl/monodroid/tests"
+
+cat <<EOF
+  <!-- Manual fixups -->
+  <ItemGroup>
+    <!-- This is referenced by monodroid_corlib_xunit-test.dll -->
+    <MonoTestAssembly Include="System.Runtime.CompilerServices.Unsafe.dll">
+      <TestType>reference</TestType>
+    </MonoTestAssembly>
+    <MonoTestRunner Include="nunitlite.dll" />
+  </ItemGroup>
+</Project>
+EOF

src/mono-runtimes/ProfileAssemblies.projitems
@@ -163,7 +163,6 @@
     <MonoProfileAssembly Include="Mono.Posix.dll" />
     <MonoProfileAssembly Include="Mono.Security.dll" />
     <MonoProfileAssembly Include="mscorlib.dll" />
-    <MonoProfileAssembly Include="nunitlite.dll" />
     <MonoProfileAssembly Include="System.dll" />
     <MonoProfileAssembly Include="System.ComponentModel.Composition.dll" />
     <MonoProfileAssembly Include="System.ComponentModel.DataAnnotations.dll" />
@@ -211,29 +210,37 @@
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_I18N.West_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_Microsoft.CSharp_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_Mono.CSharp_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_Mono.Data.Sqlite_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_Mono.Data.Tds_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_Mono.Posix_test.dll">
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_Mono.Security_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.ComponentModel.Composition_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.ComponentModel.DataAnnotations_test.dll">
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Core_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Core_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Data_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.Data_xunit-test.dll">
@@ -245,9 +252,11 @@
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.Json_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Json_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Net.Http_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.Numerics_test.dll">
@@ -255,11 +264,14 @@
     <MonoTestAssembly Include="monodroid_System.Numerics_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Runtime.CompilerServices.Unsafe_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Runtime.Serialization_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Runtime.Serialization_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
@@ -268,29 +280,40 @@
     <MonoTestAssembly Include="monodroid_System.Security_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.ServiceModel.Web_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.ServiceModel_test.dll">
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Transactions_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Web.Services_test.dll">
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Xml.Linq_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Xml.Linq_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System.Xml_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System.Xml_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
     <MonoTestAssembly Include="monodroid_System_test.dll">
     </MonoTestAssembly>
+    <!--
     <MonoTestAssembly Include="monodroid_System_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
+      -->
   </ItemGroup>
   <!-- Manual fixups -->
   <ItemGroup>

commit 1d42254c0913a8f4719db4dcad551931f96981dc

generalize a unit test exclusion list
Commit 2d85fa86 was largly successful, in that no further on-device
tests failed (instead we had `nunit-console`-based tests crash, which
isn't *too* unusual), so removing *all* of the added test assemblies
appears to have been (largely) successful.

Let's tack that back a bit: add a `$EXCLUDE_TEST_ASSEMBLIES` value to
`gen-ProfileAssemblies.sh` so that specific assemblies can be
excluded, and exclude `monodroid_System.Core_xunit-test.dll` (which
caused the crash mentioned in 2d85fa86).

build-tools/scripts/gen-ProfileAssemblies.sh
@@ -5,12 +5,18 @@
 #
 # See also the `_DownloadArchive` target in `src/mono-runtimes/mono-runtimes.targets`
 
-cat <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<!-- This is a GENERATED FILE -->
-<!-- See build-tools/scripts/gen-ProfileAssemblies.sh -->
-<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-EOF
+
+# Unit test assemblies which fail or crash the runtime, and should not be run.
+# (Ideally an empty list, but ¯\_(ツ)_/¯)
+EXCLUDE_TEST_ASSEMBLIES="
+	monodroid_System.Core_xunit-test.dll
+	"
+
+# Rationale: monodroid_System.Core_xunit-test.dll
+# [$xUnit] Test FAILED [monodroid_System.Core_xunit-test, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null] (TaskId:58)
+#    Test name: System.IO.Pipes.Tests.AnonymousPipeTest_Write_ServerOut_ClientIn.WriteWithOutOfBoundsArray_Throws_ArgumentException (TaskId:58)
+#    Exception messages: System.PlatformNotSupportedException : Operation is not supported on this platform. (TaskId:58)
+
 
 function PrintItemGroup()
 {
@@ -27,13 +33,16 @@ function PrintItemGroup()
 	echo "  </ItemGroup>"
 }
 
+EXCLUDE_TESTS=`echo "$EXCLUDE_TEST_ASSEMBLIES" | tr -d '\n' | sed -E 's/[[:space:]]+/|/g'`
+EXCLUDE_TESTS=`echo "$EXCLUDE_TESTS" | sed -E 's/^\|(.*)\|$/\1/g'`
+
 function PrintTestItemGroup()
 {
 	local dir="$1"
 
 	echo "  <ItemGroup>"
 
-	for f in `find "$dir" -depth 1 -name \*.dll | sed 's,.dll$,,g' | sort -f` ; do
+	for f in `find "$dir" -depth 1 -name \*.dll | grep -E -v "$EXCLUDE_TESTS" | sed 's,.dll$,,g' | sort -f` ; do
 		local n=`basename "$f"` ;
 		echo "    <MonoTestAssembly Include=\"$n.dll\">" ;
 		if [[ "$n" == *xunit-test ]]; then
@@ -50,7 +59,13 @@ function PrintTestItemGroup()
 	echo "  </ItemGroup>"
 }
 
-# MonoFacadeAssembly
+cat <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<!-- This is a GENERATED FILE -->
+<!-- See build-tools/scripts/gen-ProfileAssemblies.sh -->
+<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+EOF
+
 PrintItemGroup "MonoFacadeAssembly"   "external/mono/sdks/out/android-bcl/monodroid/Facades"
 PrintItemGroup "MonoProfileAssembly"  "external/mono/sdks/out/android-bcl/monodroid"
 PrintTestItemGroup                    "external/mono/sdks/out/android-bcl/monodroid/tests"

src/mono-runtimes/ProfileAssemblies.projitems
@@ -210,37 +210,26 @@
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_I18N.West_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_Microsoft.CSharp_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_Mono.CSharp_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_Mono.Data.Sqlite_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_Mono.Data.Tds_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_Mono.Posix_test.dll">
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_Mono.Security_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.ComponentModel.Composition_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.ComponentModel.DataAnnotations_test.dll">
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Core_test.dll">
     </MonoTestAssembly>
-    <!--
-    <MonoTestAssembly Include="monodroid_System.Core_xunit-test.dll">
-      <TestType>xunit</TestType>
-    </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Data_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.Data_xunit-test.dll">
@@ -252,11 +241,9 @@
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.Json_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.Json_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Net.Http_test.dll">
     </MonoTestAssembly>
     <MonoTestAssembly Include="monodroid_System.Numerics_test.dll">
@@ -264,14 +251,11 @@
     <MonoTestAssembly Include="monodroid_System.Numerics_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.Runtime.CompilerServices.Unsafe_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Runtime.Serialization_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.Runtime.Serialization_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
@@ -280,40 +264,29 @@
     <MonoTestAssembly Include="monodroid_System.Security_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.ServiceModel.Web_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.ServiceModel_test.dll">
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Transactions_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.Web.Services_test.dll">
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Xml.Linq_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.Xml.Linq_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System.Xml_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System.Xml_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
     <MonoTestAssembly Include="monodroid_System_test.dll">
     </MonoTestAssembly>
-    <!--
     <MonoTestAssembly Include="monodroid_System_xunit-test.dll">
       <TestType>xunit</TestType>
     </MonoTestAssembly>
-      -->
   </ItemGroup>
   <!-- Manual fixups -->
   <ItemGroup>
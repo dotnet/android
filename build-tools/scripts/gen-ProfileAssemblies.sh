1d42254c0913a8f4719db4dcad551931f96981dc
Jonathan Pryor <jonpryor@vt.edu>
2019-03-21 15:27:03 -0400

    generalize a unit test exclusion list
    Commit 2d85fa86 was largly successful, in that no further on-device
    tests failed (instead we had `nunit-console`-based tests crash, which
    isn't *too* unusual), so removing *all* of the added test assemblies
    appears to have been (largely) successful.
    
    Let's tack that back a bit: add a `$EXCLUDE_TEST_ASSEMBLIES` value to
    `gen-ProfileAssemblies.sh` so that specific assemblies can be
    excluded, and exclude `monodroid_System.Core_xunit-test.dll` (which
    caused the crash mentioned in 2d85fa86).

--- build-tools/scripts/gen-ProfileAssemblies.sh
+++ build-tools/scripts/gen-ProfileAssemblies.sh
@@ -5,12 +5,18 @@
 #
 # See also the `_DownloadArchive` target in `src/mono-runtimes/mono-runtimes.targets`
 
-cat <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<!-- This is a GENERATED FILE -->
-<!-- See build-tools/scripts/gen-ProfileAssemblies.sh -->
-<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-EOF
+
+# Unit test assemblies which fail or crash the runtime, and should not be run.
+# (Ideally an empty list, but ¯\_(ツ)_/¯)
+EXCLUDE_TEST_ASSEMBLIES="
+	monodroid_System.Core_xunit-test.dll
+	"
+
+# Rationale: monodroid_System.Core_xunit-test.dll
+# [$xUnit] Test FAILED [monodroid_System.Core_xunit-test, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null] (TaskId:58)
+#    Test name: System.IO.Pipes.Tests.AnonymousPipeTest_Write_ServerOut_ClientIn.WriteWithOutOfBoundsArray_Throws_ArgumentException (TaskId:58)
+#    Exception messages: System.PlatformNotSupportedException : Operation is not supported on this platform. (TaskId:58)
+
 
 function PrintItemGroup()
 {
@@ -27,13 +33,16 @@ function PrintItemGroup()
 	echo "  </ItemGroup>"
 }
 
+EXCLUDE_TESTS=`echo "$EXCLUDE_TEST_ASSEMBLIES" | tr -d '\n' | sed -E 's/[[:space:]]+/|/g'`
+EXCLUDE_TESTS=`echo "$EXCLUDE_TESTS" | sed -E 's/^\|(.*)\|$/\1/g'`
+
 function PrintTestItemGroup()
 {
 	local dir="$1"
 
 	echo "  <ItemGroup>"
 
-	for f in `find "$dir" -depth 1 -name \*.dll | sed 's,.dll$,,g' | sort -f` ; do
+	for f in `find "$dir" -depth 1 -name \*.dll | grep -E -v "$EXCLUDE_TESTS" | sed 's,.dll$,,g' | sort -f` ; do
 		local n=`basename "$f"` ;
 		echo "    <MonoTestAssembly Include=\"$n.dll\">" ;
 		if [[ "$n" == *xunit-test ]]; then
@@ -50,7 +59,13 @@ function PrintTestItemGroup()
 	echo "  </ItemGroup>"
 }
 
-# MonoFacadeAssembly
+cat <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<!-- This is a GENERATED FILE -->
+<!-- See build-tools/scripts/gen-ProfileAssemblies.sh -->
+<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+EOF
+
 PrintItemGroup "MonoFacadeAssembly"   "external/mono/sdks/out/android-bcl/monodroid/Facades"
 PrintItemGroup "MonoProfileAssembly"  "external/mono/sdks/out/android-bcl/monodroid"
 PrintTestItemGroup                    "external/mono/sdks/out/android-bcl/monodroid/tests"
