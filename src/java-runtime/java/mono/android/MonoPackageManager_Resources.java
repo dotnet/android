75023bd2305063b016d120c98f14dfb54aa3684a
Dean Ellis <dellis1972@googlemail.com>
2019-03-25 17:57:28 +0000

    [java-runtime] Remove debug files from java_runtime.jar (#2870)
    Commit 121b81aa added the new `java_runtime.jar` and
    `java_runtime_fastdev.jar` files.  The idea is that
    `java_runtime.jar` was the release version of the runtime, and would
    *not* contain files such as
    
    	mono/android/ResourcePatcher.class
    	mono/android/Seppuku.class
    	mono/android/MonkeyPatcher.class
    
    which are for fast dev only.
    
    However we were including these files!!
    
    This was because of a couple of problems.
    
     1. The `%(_RuntimeOutput.RemoveItems)` metadata was being treated
        like an ItemGroup when it is in fact a property.  Wwe need to
        semi-colon `;`-separate the items to make sure they are ALL
        removed rather then it just being the last one listed.
    
     2. Because the `Inputs` was changed to `@(_RuntimeOutput)` it means
        the target will run only ONCE.  As a result both the
        `release.txt` and `fastdev.txt` files contained the SAME file
        list.  What we should have been doing was using Target Batching:
    
            %(_RuntimeOutput.OutputJar)
    
        This makes sure the target runs for EACH output.  This will
        result in the behaviour we want, i.e the files listed above NOT
        being in the release version of the jar.
    
    Finally, change the default value for
    `MonoPackageManager_Resources.ApiPackageName` from `""` (empty string)
    to `null`.  This was done to allow startup to work; otherwise,
    [`MonoRuntimeProvider.attachInfo()` could fail][0] if it encounters a
    non-`null` value which *isn't* an installed package.
    
    We don't yet fully understand this failure scenario, but setting
    `ApiPackageName` to null allows things to work.
    
    [0]: https://github.com/xamarin/xamarin-android/blob/ae75aa627de475a5c276ccbc60e9e9f72425aab0/src/Xamarin.Android.Build.Tasks/Resources/MonoRuntimeProvider.Shared.java#L23-L39

---
+++ src/java-runtime/java/mono/android/MonoPackageManager_Resources.java
@@ -0,0 +1,14 @@
+package mono;
+
+import java.lang.String;
+import java.util.Arrays;
+
+
+public class MonoPackageManager_Resources {
+	public static final String[] Assemblies = new String[]{
+	};
+	public static final String[] Dependencies = new String[]{
+	};
+	public static final String ApiPackageName = null;
+}
+
