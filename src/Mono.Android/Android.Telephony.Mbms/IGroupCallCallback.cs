0d211ceb5c21f9f47d7894c1400596052055fb0c
Atsushi Eno <atsushieno@gmail.com>
2019-04-14 08:46:56 +0900

    [Mono.Android] API-Q Beta 2 Preview Binding (#2965)
    Context: https://web.archive.org/web/20190409091808/https://developer.android.com/preview/release-notes
    
    Android Q Beta 2 has been released.
    
    [The Android Q Developer Preview Timeline][timeline] has
    Beta 2 in April, Beta 3 in May, Beta 4 with final APIs in June, an
    final release in Q3 2019.
    
    [timeline]: https://developer.android.com/preview/overview#timeline

---
+++ src/Mono.Android/Android.Telephony.Mbms/IGroupCallCallback.cs
@@ -0,0 +1,18 @@
+using Android.Runtime;
+using Java.Interop;
+
+#if ANDROID_29
+
+namespace Android.Telephony.Mbms
+{
+	public interface IGroupCallCallback : IJavaObject
+	{
+		// This is not generated because all the methods in this
+		// interface are default interface mthods, and the interface
+		// is not generated if no method exists.
+		// This is still created because it is used by other classes.
+	}
+}
+
+#endif
+
