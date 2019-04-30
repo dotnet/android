0929d8bd3e773ef6ed2dfd5149227271627907af
Dean Ellis <dellis1972@googlemail.com>
2019-04-30 17:11:53 +0100

    [Xamarin.Android.Build.Tasks] Invalid Resources generated for DesignTime
    Fixes #3007
    
    Xamarin.Forms introduced some new `android:id` values in the
    `Xamarin.Forms.Platform.Android` assembly [1]. These include
    a period `.` in the identifier, such as
    
    	android:id="@+id/bottomtab.tabbar"
    	android:id="@+id/toolbar.one"
    
    When processing these with `aapt` or `aapt2` these identifiers
    are "fixed up" to be valid java identifiers. The same is
    NOT true for the `ManagedResourceParser`. As a result
    the very first design time build will fail with the following
    error.
    
    	obj\Debug\81\designtime\Resource.designer.cs(5048,21,5048,30): error CS0145: A const field requires a value to be provided
    	obj\Debug\81\designtime\Resource.designer.cs(5048,30,5048,31): error CS1003: Syntax error, ','
    	obj\Debug\81\designtime\Resource.designer.cs(5048,31,5048,38): error CS1002: ; expected
    
    This is because it is generating the following code in the
    `Resource.designer.cs`
    
    	public const int toolbar.one = 2131230899;
    
    which is NOT a valid C# identifier.
    
    This commit adds a new `ResourceIdentifier` class which
    is responsible for making sure the identifier we generate
    is a valid C# identifier. This logic is based on the
    C# language specifications [1]. It also does keyword
    detection and prepends an `@` if required. Just in case
    a user calls an item `base` or `class` or any other
    C# keyword.
    
    [1] https://github.com/xamarin/Xamarin.Forms/blob/master/Xamarin.Forms.Platform.Android/Resources/Layout/BottomTabLayout.axml#L13.
    [2] https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#identifiers
    [3] https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/ManagedResourceParserTests.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/ManagedResourceParserTests.cs
@@ -70,18 +70,60 @@ namespace Xamarin.Android.Build.Tests {
 <LinearLayout xmlns:android=""http://schemas.android.com/apk/res/android"">
 	<TextView android:id=""@+id/seekBar"" />
 	<TextView android:id=""@+id/seekbar"" />
+	<TextView android:id=""@+id/textview.withperiod"" />
+	<TextView android:id=""@+id/Føø-Bar"" />
 </LinearLayout>
 ";
 
-		[Test]
-		public void GenerateDesignerFileWithÜmläüts ()
+		const string AndroidManifest = @"<?xml version='1.0'?>
+<manifest xmlns:android=""http://schemas.android.com/apk/res/android"" package=""MonoAndroidApplication4.MonoAndroidApplication4"" />";
+
+		/// <summary>
+		/// When you add new resources to the code blocks above, the following
+		/// Rtxt string needs to be updated with the correct values.
+		/// You can do this manually OR use aapt2
+		///
+		/// Run the GenerateDesignerFileFromRtxt test and let it fail.
+		/// You can then go to the appropriate resource directory
+		/// and run the following commands.
+		/// 
+		/// aapt2 compile -o compile.flata --dir res/
+		/// aapt2 compile -o lp.flata --dir lp/res/
+		/// aapt2 link -o foo.apk --manifest AndroidManifest.xml -R lp.flata -R compile.flata --auto-add-overlay --output-text-symbols R.txt -I ~/android-toolchain-xa/sdk/platforms/android-Q/android.jar
+		///
+		/// Then copy the values from the R.txt file and place them below.
+		/// </summary>
+		const string Rtxt = @"int animator slide_in_bottom 0x7f010000
+int array widths_array 0x7f020000
+int dimen main_text_item_size 0x7f030000
+int drawable ic_menu_preferences 0x7f040000
+int font arial 0x7f050000
+int id Føø_Bar 0x7f060000
+int id menu_settings 0x7f060001
+int id seekBar 0x7f060002
+int id seekbar 0x7f060003
+int id textview_withperiod 0x7f060004
+int layout main 0x7f070000
+int menu Options 0x7f080000
+int mipmap icon 0x7f090000
+int plurals num_locations_reported 0x7f0a0000
+int raw foo 0x7f0b0000
+int string app_name 0x7f0c0000
+int string foo 0x7f0c0001
+int string hello 0x7f0c0002
+int string menu_settings 0x7f0c0003
+int transition transition 0x7f0d0000
+";
+
+		public void CreateResourceDirectory (string path)
 		{
-			var path = Path.Combine ("temp", TestName + " Some Space");
 			Directory.CreateDirectory (Path.Combine (Root, path, "res", "values"));
 			Directory.CreateDirectory (Path.Combine (Root, path, "res", "transition"));
 
 			Directory.CreateDirectory (Path.Combine (Root, path, "res", "raw"));
 			Directory.CreateDirectory (Path.Combine (Root, path, "res", "layout"));
+
+			File.WriteAllText (Path.Combine (Root, path, "AndroidManifest.xml"), AndroidManifest);
 
 			File.WriteAllText (Path.Combine (Root, path, "res", "values", "strings.xml"), StringsXml);
 			File.WriteAllText (Path.Combine (Root, path, "res", "transition", "transition.xml"), Transition);
@@ -105,6 +147,14 @@ namespace Xamarin.Android.Build.Tests {
 				File.WriteAllBytes (Path.Combine (Root, path, "lp", "res", "mipmap-hdpi", "icon.png"), icon_binary_mdpi);
 			}
 			File.WriteAllText (Path.Combine (Root, path, "lp", "res", "menu", "Options.xml"), Menu);
+		}
+
+
+		[Test]
+		public void GenerateDesignerFileWithÜmläüts ()
+		{
+			var path = Path.Combine ("temp", TestName + " Some Space");
+			CreateResourceDirectory (path);
 			IBuildEngine engine = new MockBuildEngine (TestContext.Out);
 			var task = new GenerateResourceDesigner {
 				BuildEngine = engine
@@ -129,6 +179,39 @@ namespace Xamarin.Android.Build.Tests {
 			var expected = Path.Combine (Root, "Expected", "GenerateDesignerFileExpected.cs");
 			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expected), 
 			 	$"{task.NetResgenOutputFile} and {expected} do not match.");
+			Directory.Delete (Path.Combine (Root, path), recursive: true);
+		}
+
+		[Test]
+		public void GenerateDesignerFileFromRtxt ()
+		{
+			var path = Path.Combine ("temp", TestName + " Some Space");
+			CreateResourceDirectory (path);
+			File.WriteAllText (Path.Combine (Root, path, "R.txt"), Rtxt);
+			IBuildEngine engine = new MockBuildEngine (TestContext.Out);
+			var task = new GenerateResourceDesigner {
+				BuildEngine = engine
+			};
+			task.UseManagedResourceGenerator = true;
+			task.DesignTimeBuild = true;
+			task.Namespace = "Foo.Foo";
+			task.NetResgenOutputFile = Path.Combine (Root, path, "Resource.designer.cs");
+			task.ProjectDir = Path.Combine (Root, path);
+			task.ResourceDirectory = Path.Combine (Root, path, "res") + Path.DirectorySeparatorChar;
+			task.Resources = new TaskItem [] {
+				new TaskItem (Path.Combine (Root, path, "res", "values", "strings.xml"), new Dictionary<string, string> () {
+					{ "LogicalName", "values\\strings.xml" },
+				}),
+			};
+			task.AdditionalResourceDirectories = new TaskItem [] {
+				new TaskItem (Path.Combine (Root, path, "lp", "res")),
+			};
+			task.IsApplication = true;
+			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
+			Assert.IsTrue (File.Exists (task.NetResgenOutputFile), $"{task.NetResgenOutputFile} should have been created.");
+			var expected = Path.Combine (Root, "Expected", "GenerateDesignerFileExpected.cs");
+			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expected),
+				 $"{task.NetResgenOutputFile} and {expected} do not match.");
 			Directory.Delete (Path.Combine (Root, path), recursive: true);
 		}
 	}
