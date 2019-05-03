05e004fa159ce291ab64a8c45365f42ced0a4d88
Daniel Cazzulino <daniel@cazzulino.com>
2019-05-03 17:12:24 -0300

    [Xamarin.Android.Build.Tasks] Add support for Deferred Builds (#2636)
    EXPERIMENTAL FEATURE: SUBJECT TO CHANGE WITHOUT NOTICE
    
    Context: https://github.com/xamarin/XamarinVS/commit/7a06170c89c07ee92f12486f252ee1a155a81a52
    Context: http://work.devdiv.io/770486
    Context: https://github.com/xamarin/xamarin-android/wiki/Design-Time-Build-System
    
    The most expensive target in Design-Time Builds -- in terms of
    execution time -- is the `UpdateAndroidResources` target, which may
    need to execute `aapt`/`aapt2` (when
    `$(AndroidUseManagedDesignTimeResourceGenerator)` is False), and
    while the `ManagedResourceParser` is faster than `aapt`, it still
    takes a noticeable amount of time to execute.
    
    Design-Time builds can be sped up by *removing*
    `UpdateAndroidResources` from the Design-Time Build entirely, by
    having the IDE monitor for Design-Time Builds and schedule a
    *Deferred Build* to run after the Design-Time Build finishes.
    
    Support for Deferred Builds is up to the IDE, and initially will only
    exist on Visual Studio (Windows).  (Visual Studio for Mac doesn't
    have Design-Time builds -- to the joy of the Xamarin.Android team! --
    and thus won't gain support for Deferred Builds at this time.)
    
    When the IDE supports Deferred Builds, the
    `$(DeferredBuildSupported)` MSBuild property will be True.
    When True, the [`DeferredBuild` target][0] will run after the
    Design-Time Build finishes, which in turn will execute all the
    targets listed within the `$(DeferredBuildDependsOn)` property:
    
    	<Target Name="DeferredBuild"
    	    Condition=" '$(DeferredBuild)' == 'true' "
    	    DependsOnTargets="$(DeferredBuildDependsOn)"
    	/>
    
    Targets can selectively disable themselves on Design-Time Builds when
    Deferred Builds are supported by checking for:
    
    	'$(DeferredBuildSupported)' == 'true' And '$(DeferredBuild)' == 'true'
    
    Both `$(DeferredBuildSupported)`=True and `$(DeferredBuild)`=True
    when Deferred Builds are executing.
    
    The deferred build queuing will be based on the following condition:
    a regular Design-Time Build was triggered and the targets run were
    either `Compile` or any target that contains `DesignTime` in its name
    (this prevents queuing a Deferred Build when a target like
    `GetFrameworkPaths` or `GetTargetPath` is run in a Design-Time Build,
    for example.)
    
    Note that `Inputs`/`Outputs` on the targets, as well as any
    `DependsOnTargets` specified remain the same as before.  The effect
    is that expensive targets that caused a slow Design-Time Build before
    can now be scheduled to run *after* the build finishes (this is when
    the deferred build is queued).
    
    This mechanism is entirely opt-in: if no targets are specified for
    `$(DeferredBuildDependsOn)`, it will be essentially a no-op run.
    
    Finally, when a regular build starts, the existing deferred build
    will be waited for completion before continuing, to ensure no
    concurrency happens.
    
    The initial `Compile` target is run by the Design-Time Builds in
    Visual Studio while skipping the compiler call, at which point the
    `UpdateAndroidResources` target should also be skipped when 
    `$(DeferredBuildSupported)`=True.
    
    Subsequently, we run the `DeferredBuild` target (or rather, the
    `UpdateAndroidResources` target which would be run via
    `$(DeferredBuildDependsOn)` which we append to it) which should also
    succeed (it's run with the same global parameters/properties as the
    initial Design-Time Build).
    
    Finally, we run the normal build, at which point everything should
    succeed like before.
    
    If an XamarinVS version that supports this feature is installed, you
    can turn it on by setting an environment variable
    `DeferredBuildSupported=true`.  An A/B experiment or remote setting
    can also set this value.
    
    [0]: https://github.com/xamarin/XamarinVS/blob/f717bd566ba957d295e1eb4875fb116a9f609897/src/MSBuild/Xamarin/Xamarin.Sdk.targets#L81

---
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/DeferredBuildTest.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System.IO;
+using Xamarin.ProjectTools;
+
+namespace Xamarin.Android.Build.Tests
+{
+	[Parallelizable (ParallelScope.Children)]
+	public class DeferredBuildTest : BaseTest
+	{
+		[Test]
+		public void SelectivelyRunUpdateAndroidResources ()
+		{
+			var path = Path.Combine ("temp", TestName);
+			var app = new XamarinAndroidApplicationProject {
+				ProjectName = "MyApp",
+			};
+
+			app.SetProperty ("AndroidUseManagedDesignTimeResourceGenerator", "True");
+			app.SetProperty ("AndroidUseIntermediateDesignerFile", "True");
+
+			using (var appBuilder = CreateApkBuilder (Path.Combine (path, app.ProjectName))) {
+				Assert.IsTrue (appBuilder.DesignTimeBuild (app, parameters: new string[]{
+					"BuildingInsideVisualStudio=true",
+					"DeferredBuildSupported=true",
+					// The DTB passes down these two properties
+					"SkipCompilerExecution=true",
+					"ProvideCommandLineArgs=true",
+				}), "first app build should have succeeded.");
+
+				Assert.IsTrue (appBuilder.Output.IsTargetSkipped ("UpdateAndroidResources"), $"`UpdateAndroidResources` should be skipped for DTB when deferred build is supported!");
+
+				// The background build would run our UpdateAndroidResources in its DeferredBuildDependsOn
+				Assert.IsTrue (appBuilder.RunTarget (app, "UpdateAndroidResources", parameters: new string[]{
+					"BuildingInsideVisualStudio=true",
+					// DeferredBuild targets set these two
+					"DeferredBuild=true",
+					"DeferredBuildSupported=true",
+					// The DTB passes down these two properties
+					"SkipCompilerExecution=true",
+					"ProvideCommandLineArgs=true",
+				}), "background build should have succeeded.");
+
+				Assert.IsFalse (appBuilder.Output.IsTargetSkipped ("UpdateAndroidResources"), $"`UpdateAndroidResources` should *not* be skipped in the deferred build!");
+
+				// Run the real build now
+				Assert.IsTrue (appBuilder.Build(app, parameters: new string[]{
+					"DesignTimeBuild=false",
+					"BuildingInsideVisualStudio=true",
+					"DeferredBuildSupported=true",
+				}), "real build should have succeeded.");
+			}
+		}
+
+		[Test]
+		public void RunUpdateAndroidResourcesIfBackgroundBuildNotSupported ()
+		{
+			var path = Path.Combine ("temp", TestName);
+			var app = new XamarinAndroidApplicationProject {
+				ProjectName = "MyApp",
+			};
+
+			app.SetProperty ("AndroidUseManagedDesignTimeResourceGenerator", "True");
+			app.SetProperty ("AndroidUseIntermediateDesignerFile", "True");
+
+			using (var appBuilder = CreateApkBuilder (Path.Combine (path, app.ProjectName))) {
+				Assert.IsTrue (appBuilder.DesignTimeBuild (app, parameters: new string[]{
+					"BuildingInsideVisualStudio=true",
+					// The DTB passes down these two properties
+					"SkipCompilerExecution=true",
+					"ProvideCommandLineArgs=true",
+				}), "first app build should have succeeded.");
+
+				Assert.IsFalse (appBuilder.Output.IsTargetSkipped ("UpdateAndroidResources"), $"`UpdateAndroidResources` should be not skipped for DTB when deferred build is not supported!");
+			}
+		}
+	}
+}
