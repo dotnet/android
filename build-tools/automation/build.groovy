375a347cd1c61568f8e376c7796debc3d433c65f
Mike Bond <mjbond-msft@outlook.com>
2019-05-10 09:05:59 -0700

    [build] Commercial pipeline build (#3004)
    Commit 3e998b21 and [monodroid@bee5f69a][0] introduced support for
    building the commercial Xamarin.Android product from the
    xamarin-android repo in addition to the monodroid repo.
    
    That support is just the beginning; now we need to integrate that
    support into some build jobs so that it's used.
    
    Update `build-tools/automation/build.groovy` so that when the
    `xamarin-android-commercial` job is executed, we:
    
      * Stage & publish commercial artifacts from a single, "flat"
        package directory
      * Generate & publish `artifacts.json` via `build-tasks.exe`;
        include `artifacts.json` as part of GitHub statuses.
      * When the `$SkipTest` environment variable is set to `1`, skip the
        building, execution, and publishing of test artifacts.
      * Sign the built installers for commercial CI builds *unless* the
        `$SkipSigning` environment variable is set to `1`.
      * Generate and upload `updateinfo` file containing setup channel
        `ProductId` for commercial builds
      * Calculate the `gitRepo` setting as a replacement for the
        `GitRepo` build configuration setting.
      * Avoid producing OSS packages for commercial builds.
    
    
    [0]: https://github.com/xamarin/monodroid/commit/bee5f69a0b41aaeb88fad86ce4d65960a959def9

--- build-tools/automation/build.groovy
+++ build-tools/automation/build.groovy
@@ -1,10 +1,23 @@
 // This file is based on the Jenkins scripted pipeline (as opposed to the declarative pipeline) syntax
 // https://jenkins.io/doc/book/pipeline/syntax/#scripted-pipeline
-def XADir = "xamarin-android"
+def XADir = 'xamarin-android'
+def packageDir = 'package'
 
 def EXTRA_MSBUILD_ARGS="/p:AutoProvision=True /p:AutoProvisionUsesSudo=True /p:IgnoreMaxMonoVersion=False"
 
+def isCommercial = false
+def commercialPath = ''
+def packagePath = ''
+def storageVirtualPath = ''
+
 def isPr = false                // Default to CI
+
+def gitRepo = ''
+def branch = ''
+def commit = ''
+
+def skipSigning = false
+def skipTest = false
 
 def hasPrLabelFullMonoIntegrationBuild = false
 
@@ -13,6 +26,21 @@ def buildTarget = 'jenkins'
 def utils = null
 
 prLabels = null  // Globally defined "static" list accessible within the hasPrLabel function
+
+@NonCPS
+def getBuildTasksRedirect() {
+    def connection = "https://dl.internalx.com/build-tools/latest/Xamarin.Build.Tasks.nupkg".toURL().openConnection()
+    connection.instanceFollowRedirects = false
+    // should be in scope at the time of this call (called within a withCredentials block)
+    connection.setRequestProperty("Authorization", "token ${env.GITHUB_AUTH_TOKEN}")
+    def response = connection.responseCode
+    connection.disconnect()
+    if (response == 302) {
+        return connection.getHeaderField("Location")
+    } else {
+        throw new Exception("DL link failed ${response}: ${content}")
+    }
+}
 
 timestamps {
     node("${env.BotLabel}") {
@@ -40,18 +68,43 @@ timestamps {
         utils = load "${XADir}/build-tools/automation/utils.groovy"
 
         utils.stageWithTimeout('init', 30, 'SECONDS', XADir, true) {    // Typically takes less than a second
+            isCommercial = env.IsCommercial == '1'
+            commercialPath = "external/${env.CommercialDirectory}"
+            packagePath =  "${env.WORKSPACE}/${packageDir}"
+            storageVirtualPath = env.StorageVirtualPath
+
+            skipSigning = env.SkipSigning == '1'
+            skipTest = env.SkipTest == '1'
+
+            gitRepo = scmVars.GIT_URL.replace("git@github.com:", "").split("/").takeRight(2).join("/").replace(".git", "")     // Example result: xamarin/xamarin-android
+
             // Note: PR plugin environment variable settings available here: https://wiki.jenkins.io/display/JENKINS/GitHub+pull+request+builder+plugin
             isPr = env.ghprbActualCommit != null
-            def branch = isPr ? env.GIT_BRANCH : scmVars.GIT_BRANCH
-            def commit = isPr ? env.ghprbActualCommit : scmVars.GIT_COMMIT
+            branch = isPr ? env.GIT_BRANCH : scmVars.GIT_BRANCH
+            commit = isPr ? env.ghprbActualCommit : scmVars.GIT_COMMIT
 
             def buildType = isPr ? 'PR' : 'CI'
 
-            echo "Git repo: ${env.GitRepo}"     // Defined as an environment variable in the jenkins build definition
+            echo "Git repo: ${gitRepo}"         // Example: xamarin/xamarin-android
             echo "Job: ${env.JOB_BASE_NAME}"
+            echo "Job name: ${env.JOB_NAME}"
+            echo "Workspace: ${env.WORKSPACE}"
             echo "Branch: ${branch}"
             echo "Commit: ${commit}"
             echo "Build type: ${buildType}"
+            echo "Build number: ${env.BUILD_NUMBER}"
+            echo "IsCommercial: ${isCommercial}"
+
+            if (isCommercial) {
+                echo "Commercial path: ${commercialPath}"
+                storageVirtualPath = "${env.JOB_NAME}-${env.BUILD_NUMBER}/${branch}/${commit}"      // This needs to be unique for commercial builds since the path determines where artifacts.json used for GitHub statuses will be published
+            }
+
+            echo "Package path: ${packagePath}"
+            echo "Storage path: ${storageVirtualPath}"
+
+            echo "SkipSigning: ${skipSigning}"
+            echo "SkipTest: ${skipTest}"
 
             if (isPr) {
                 echo "PR id: ${env.ghprbPullId}"
@@ -62,7 +115,7 @@ timestamps {
                 env.ghprbPullTitle = ''
                 env.ghprbPullLongDescription = ''
 
-                if (utils.hasPrLabel(env.GitRepo, env.ghprbPullId, 'full-mono-integration-build')) {
+                if (utils.hasPrLabel(gitRepo, env.ghprbPullId, 'full-mono-integration-build')) {
                     hasPrLabelFullMonoIntegrationBuild = true
                     buildTarget = 'jenkins'
                 } else {
@@ -87,14 +140,47 @@ timestamps {
             // This is to assure that all tests start from the same state
             sh "killall -9 qemu-system-x86_64 || true"
             sh "rm -rf \$HOME/.android/avd/XamarinAndroidTestRunner.*"
+            if (fileExists(packagePath)) {
+                sh "rm -rf ${packagePath}"
+            }
+
+            sh "mkdir -p ${packagePath}"
         }
 
-        utils.stageWithTimeout('prepare deps', 30, 'MINUTES', XADir, true) {    // Typically takes less than 2 minutes
+        utils.stageWithTimeout('prepare deps', 30, 'MINUTES', XADir, true) {    // Typically takes less than 2 minutes, but can take longer if any prereqs need to be provisioned
+            if (isCommercial) {
+                sh "make prepare-external-git-dependencies"
+
+                utils.stageWithTimeout('provisionator', 30, 'MINUTES', "${commercialPath}/build-tools/provisionator", true) {
+                    sh('./provisionator.sh profile.csx -v')
+                }
+
+                utils.stageWithTimeout('build tasks', 30, 'MINUTES', env.WORKSPACE, true) {
+                    withCredentials([string(credentialsId: "${env.GitHubAuthTokenCredentialId}", variable: 'GITHUB_AUTH_TOKEN')]) {
+                        def redirect = getBuildTasksRedirect()
+                        sh "curl -o Xamarin.Build.Tasks.nupkg \"${redirect}\""
+                        dir("BuildTasks") {
+                            deleteDir()
+                            sh "unzip ../Xamarin.Build.Tasks.nupkg"
+                        }
+                    }
+                }
+            }
+
             sh "make prepare-deps CONFIGURATION=${env.BuildFlavor} V=1 MSBUILD_ARGS='$EXTRA_MSBUILD_ARGS'"
         }
 
         utils.stageWithTimeout('build', 6, 'HOURS', XADir, true) {    // Typically takes less than one hour except a build on a new bot to populate local caches can take several hours
             sh "make prepare ${buildTarget} CONFIGURATION=${env.BuildFlavor} V=1 MSBUILD_ARGS='$EXTRA_MSBUILD_ARGS'"
+
+            if (isCommercial) {
+                sh "cp bin/${env.BuildFlavor}/bundle-*.zip ${packagePath}"
+
+                sh '''
+                    VERSION=`LANG=C; export LANG && git log --no-color --first-parent -n1 --pretty=format:%ct`
+                    echo "d1ec039f-f3db-468b-a508-896d7c382999 $VERSION" > ../package/updateinfo
+                '''
+            }
         }
 
         utils.stageWithTimeout('create installers', 30, 'MINUTES', XADir, true) {    // Typically takes less than 5 minutes
@@ -102,50 +188,120 @@ timestamps {
                 // Override _MSBUILD_ARGS to ensure we only package the `AndroidSupportedTargetJitAbis` which are built.
                 // Also ensure that we don't require mono bundle components in the installer if this is not a full mono integration build.
                 def msbuildInstallerArgs = hasPrLabelFullMonoIntegrationBuild ? '' : '/p:IncludeMonoBundleComponents=False'
-                sh "make create-installers CONFIGURATION=${env.BuildFlavor} _MSBUILD_ARGS='${msbuildInstallerArgs}'"
+                sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1 _MSBUILD_ARGS='${msbuildInstallerArgs}'"
             } else {
-                sh "make create-installers CONFIGURATION=${env.BuildFlavor}"
+                sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1"
+            }
+
+            if (isCommercial) {
+                sh "cp bin/Build*/xamarin.android*.pkg ${packagePath}"
+                sh "cp bin/Build*/Xamarin.Android*.vsix ${packagePath}"
             }
         }
 
         utils.stageWithTimeout('package oss', 30, 'MINUTES', XADir, true) {    // Typically takes less than 5 minutes
-            sh "make package-oss CONFIGURATION=${env.BuildFlavor}"
+            if (!isCommercial) {
+                sh "make package-oss CONFIGURATION=${env.BuildFlavor} V=1"
+            }
+        }
+
+        utils.stageWithTimeout('sign packages', 3, 'MINUTES', packageDir, false) {    // Typically takes less than 10 seconds
+            if (isPr || !isCommercial || skipSigning) {
+                echo "Skipping 'sign packages' stage. Packages are only signed for commercial CI builds. IsPr: ${isPr} / IsCommercial: ${isCommercial} / SkipSigning: ${skipSigning}"
+                return
+            }
+
+            def packages = findFiles(glob: '*.pkg')
+            def tmpPrefix = "/tmp/${env.JOB_NAME}"
+            def tmpdir = sh (script: "mkdir -p ${tmpPrefix} && mktemp -d ${tmpPrefix}/XXXXXXXXX", returnStdout: true).trim()
+            withCredentials([string(credentialsId: 'codesign_keychain_pw', variable: 'KEYCHAIN_PASSWORD')]) {
+                for (pkg in packages) {
+                    def tmp = "${tmpdir}/${pkg.name}"
+                    sh("mv ${pkg} ${tmpdir}")
+                    sh("security unlock-keychain -p ${env.KEYCHAIN_PASSWORD} login.keychain")
+                    sh("/usr/bin/productsign -s \"Developer ID Installer: Xamarin Inc\" \"${tmp}\" \"${pkg}\"")
+                }
+            }
         }
 
         utils.stageWithTimeout('build tests', 30, 'MINUTES', XADir, true) {    // Typically takes less than 10 minutes
+            if (skipTest) {
+                echo "Skipping 'build tests' stage. Clear the SkipTest variable setting to build and run tests"
+                return
+            }
+
             sh "make all-tests CONFIGURATION=${env.BuildFlavor} V=1"
         }
 
         utils.stageWithTimeout('process build results', 10, 'MINUTES', XADir, true) {    // Typically takes less than a minute
             try {
                 echo "processing build status"
-                sh "make package-build-status CONFIGURATION=${env.BuildFlavor}"
+                sh "make package-build-status CONFIGURATION=${env.BuildFlavor} V=1"
+
+                if (isCommercial) {
+                    sh "cp xa-build-status-*.zip ${packagePath}"
+                }
             } catch (error) {
                 echo "ERROR : NON-FATAL : processBuildStatus: Unexpected error: ${error}"
             }
         }
 
         utils.stageWithTimeout('publish packages to Azure', 30, 'MINUTES', '', true, 3) {    // Typically takes less than a minute, but provide ample time in situations where logs may be quite large
-            def publishBuildFilePaths = "${XADir}/xamarin.android-oss*.zip,${XADir}/bin/Build*/Xamarin.Android.Sdk-OSS*,${XADir}/build-status*,${XADir}/xa-build-status*";
-
-            if (!isPr) {
-                publishBuildFilePaths = "${publishBuildFilePaths},${XADir}/bin/${env.BuildFlavor}/bundle-*.zip"
+            def publishRootDir = ''
+            def publishBuildFilePaths = "${XADir}/xamarin.android-oss*.zip,${XADir}/bin/Build*/Xamarin.Android.Sdk-OSS*,${XADir}/build-status*,${XADir}/xa-build-status*"
+            if (isCommercial) {
+                publishRootDir = packageDir
+                publishBuildFilePaths = "xamarin.android*.pkg,Xamarin.Android*.vsix,build-status*,xa-build-status*,*updateinfo"
             }
 
-            echo "publishBuildFilePaths: ${publishBuildFilePaths}"
-            def commandStatus = utils.publishPackages(env.StorageCredentialId, env.ContainerName, env.StorageVirtualPath, publishBuildFilePaths)
-            if (commandStatus != 0) {
-                error "publish packages to Azure FAILED, status: ${commandStatus}"    // Ensure stage is labeled as 'failed' and red failure indicator is displayed in Jenkins pipeline steps view
+            if (!isPr) {
+                if (isCommercial) {
+                    publishBuildFilePaths = "${publishBuildFilePaths},bundle-*.zip"
+                } else {
+                    publishBuildFilePaths = "${publishBuildFilePaths},${XADir}/bin/${env.BuildFlavor}/bundle-*.zip"
+                }
+            }
+
+            dir(publishRootDir) {
+                echo "publishBuildFilePaths: ${publishBuildFilePaths}"
+                def commandStatus = utils.publishPackages(env.StorageCredentialId, env.ContainerName, storageVirtualPath, publishBuildFilePaths)
+                if (commandStatus != 0) {
+                    error "publish packages to Azure FAILED, status: ${commandStatus}"    // Ensure stage is labeled as 'failed' and red failure indicator is displayed in Jenkins pipeline steps view
+                }
+            }
+
+            if (isCommercial) {
+                utils.stageWithTimeout('report artifacts', 30, 'MINUTES', 'BuildTasks', false) {
+                    withCredentials([string(credentialsId: "${env.GitHubAuthTokenCredentialId}", variable: 'GITHUB_AUTH_TOKEN'), usernamePassword(credentialsId: "${env.UserNamePasswordCredentialId}", passwordVariable: 'STORAGE_PASSWORD', usernameVariable: 'STORAGE_ACCOUNT')]) {
+                        // Default search directory for Jenkins build artifacts is '${env.WORKSPACE}/package'
+                        sh "mono tools/BuildTasks/build-tasks.exe artifacts -s ${env.WORKSPACE}/${XADir} -a ${env.STORAGE_ACCOUNT} -c ${env.STORAGE_PASSWORD} -u ${env.ContainerName}/${storageVirtualPath} -t ${env.GITHUB_AUTH_TOKEN}"
+                    }
+                }
+
+                utils.stageWithTimeout('sign artifacts', 30, 'MINUTES', '', false) {
+                    if (isPr || skipSigning) {
+                        echo "Skipping 'sign artifacts' stage. Artifact signing is only performed for commercial CI builds. SkipSigning: ${skipSigning}"
+                        return
+                    }
+
+                    // 'jenkins-internal artifacts' is the GitHub status context (name) used by build-tasks.exe
+                    httpRequest httpMode: 'POST', ignoreSslErrors: true, responseHandle: 'NONE', url: "http://code-sign.guest.corp.microsoft.com:8080/job/sign-from-github-esrp/buildWithParameters?SIGN_TYPE=Real&REPO=${gitRepo}&COMMIT=${commit}&GITHUB_CONTEXT=jenkins-internal%20artifacts&FILES_TO_SIGN=%2E*%2Evsix"
+                }
             }
         }
 
         utils.stageWithTimeout('run all tests', 240, 'MINUTES', XADir, false) {   // Typically takes 1hr and 50 minutes (or 110 minutes)
+            if (skipTest) {
+                echo "Skipping 'run all tests' stage. Clear the SkipTest variable setting to build and run tests"
+                return
+            }
+
             echo "running tests"
 
             def skipNunitTests = false
 
             if (isPr) {
-                def hasPrLabelRunTestsRelease = utils.hasPrLabel(env.GitRepo, env.ghprbPullId, 'run-tests-release')
+                def hasPrLabelRunTestsRelease = utils.hasPrLabel(gitRepo, env.ghprbPullId, 'run-tests-release')
                 skipNunitTests = hasPrLabelFullMonoIntegrationBuild || hasPrLabelRunTestsRelease
                 echo "Run all tests: Labels on the PR: 'full-mono-integration-build' (${hasPrLabelFullMonoIntegrationBuild}) and/or 'run-tests-release' (${hasPrLabelRunTestsRelease})"
             }
@@ -157,6 +313,11 @@ timestamps {
         }
 
         utils.stageWithTimeout('publish test error logs to Azure', 30, 'MINUTES', '', false, 3) {  // Typically takes less than a minute, but provide ample time in situations where logs may be quite large
+            if (skipTest) {
+                echo "Skipping 'publish test error logs' stage. Clear the SkipTest variable setting to build and run tests"
+                return
+            }
+
             echo "packaging test error logs"
 
             publishHTML target: [
@@ -175,15 +336,15 @@ timestamps {
             def publishTestFilePaths = "${XADir}/xa-test-results*,${XADir}/test-errors.zip"
 
             echo "publishTestFilePaths: ${publishTestFilePaths}"
-            def commandStatus = utils.publishPackages(env.StorageCredentialId, env.ContainerName, env.StorageVirtualPath, publishTestFilePaths)
+            def commandStatus = utils.publishPackages(env.StorageCredentialId, env.ContainerName, storageVirtualPath, publishTestFilePaths)
             if (commandStatus != 0) {
                 error "publish test error logs to Azure FAILED, status: ${commandStatus}"    // Ensure stage is labeled as 'failed' and red failure indicator is displayed in Jenkins pipeline steps view
             }
         }
 
         utils.stageWithTimeout('Plot build & test metrics', 30, 'SECONDS', XADir, false, 3) {    // Typically takes less than a second
-            if (isPr) {
-                echo "Skipping plot metrics for PR build"
+            if (isPr || skipTest) {
+                echo "Skipping 'plot metrics' stage for PR build. IsPr: ${isPr} / SkipTest: ${skipTest}"
                 return
             }
 
