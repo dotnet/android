caa3aa47c7a9e86bacc0052c37660b8819d904c9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-06-12 20:42:12 -0500

    Bump to grendello/LibZipSharp/master@71f4a94 (#3192)
    Changes: https://github.com/grendello/LibZipSharp/compare/da94d3d...71f4a94
    
    I hit an error during a local build on Windows:
    
    	"src\aapt2\aapt2.csproj" (default target) (26:2) ->
    	(_DownloadAapt2 target) ->
    	  src\aapt2\aapt2.targets(49,5): error MSB4018: The "UnzipDirectoryChildren" task failed unexpectedly.
    	  src\aapt2\aapt2.targets(49,5): error MSB4018: System.AggregateException: One or more errors occurred.
    	  ---> System.DllNotFoundException: Unable to load DLL 'libzip.dll': The specified module could not be found. (Exception from HRESULT: 0x8007007E)
    
    The culprit appeared to be `bin\BuildDebug\libzip.dll` having a
    dependency on `zlib.dll`.  But we don't even ship `zlib.dll`?
    
    Further investigation, we found `libZipSharp` was using some outdated
    NuGet packages for these native libraries on Windows.
    
    In removing the NuGet packages for `libzip.dll` and `zlib.dll` on
    Windows, we need to copy the native libraries to `bin\BuildDebug`
    during the bootstrap process.  I added a new step in `xaprepare`
    for this.

---
+++ build-tools/xaprepare/xaprepare/Steps/Step_CopyLibZip.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Xamarin.Android.Prepare
+{
+	class Step_CopyLibZip : Step
+	{
+		const string LibZip = "libzip.dll";
+
+		public Step_CopyLibZip () : base ($"Copying {LibZip}") { }
+
+		protected override Task<bool> Execute (Context context)
+		{
+			Copy ();
+			Copy ("x64");
+			return Task.FromResult (true);
+		}
+
+		void Copy (string dir = "")
+		{
+			var src = Path.Combine (Configurables.Paths.InstallMSBuildDir, dir, LibZip);
+			var buildBin = Path.Combine (Configurables.Paths.BuildBinDir, dir, LibZip);
+			Utilities.CopyFile (src, buildBin, overwriteDestinationFile: true);
+			var testBin = Path.Combine (Configurables.Paths.TestBinDir, dir, LibZip);
+			Utilities.CopyFile (src, testBin, overwriteDestinationFile: true);
+		}
+	}
+}
