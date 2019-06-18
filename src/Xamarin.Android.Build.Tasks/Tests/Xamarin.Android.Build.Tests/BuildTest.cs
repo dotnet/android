e4bd2517983d2436a84ca5c4c745f878c5d5d862
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-06-18 09:20:23 -0500

    [Xamarin.Android.Build.Tasks] downgrade XA0106 to a message
    Fixes: http://feedback.devdiv.io/606610
    
    The latest version of Xamarin.Forms causes warnings such as:
    
        warning XA0106: Skipping UnnamedProject.Resource.Drawable.avd_hide_password_1. Please check that your Nuget Package versions are compatible.
        warning XA0106: Skipping UnnamedProject.Resource.Drawable.avd_hide_password_2. Please check that your Nuget Package versions are compatible.
        warning XA0106: Skipping UnnamedProject.Resource.Drawable.avd_hide_password_3. Please check that your Nuget Package versions are compatible.
        warning XA0106: Skipping UnnamedProject.Resource.Drawable.avd_show_password_1. Please check that your Nuget Package versions are compatible.
        warning XA0106: Skipping UnnamedProject.Resource.Drawable.avd_show_password_2. Please check that your Nuget Package versions are compatible.
        warning XA0106: Skipping UnnamedProject.Resource.Drawable.avd_show_password_3. Please check that your Nuget Package versions are compatible.
        warning XA0106: Skipping UnnamedProject.Resource.Id.fixed. Please check that your Nuget Package versions are compatible.
    
    I was able to reproduce this, just by changing a test we have to use
    `XamarinFormsAndroidApplicationProject`.
    
    It appears that the Xamarin.Forms NuGet package is shipping fields
    that aapt/aapt2 aren't finding:
    
        # Found
        public static int avd_hide_password;
        # Not found
        public static int avd_hide_password_1;
        public static int avd_hide_password_2;
        public static int avd_hide_password_3;
        # Found
        public static int avd_show_password;
        # Not Found
        public static int avd_show_password_1;
        public static int avd_show_password_2;
        public static int avd_show_password_3;
    
    Xamarin.Forms doesn't actually *use* these fields, but something has
    changed between the version of either Xamarin.Android or the Android
    SDK where Xamarin.Forms was built -- and how projects are built *now*.
    
    In either case `XA0106` is completely unactionable, we should
    downgrade it to a message and give a little more information while
    we're at it.
    
    This downgrades the warnings to:
    
        Value not found for Xamarin.Forms.Platform.Android.Resource.Drawable.avd_hide_password_1, skipping...
        Value not found for Xamarin.Forms.Platform.Android.Resource.Drawable.avd_hide_password_2, skipping...
        Value not found for Xamarin.Forms.Platform.Android.Resource.Drawable.avd_hide_password_3, skipping...
        Value not found for Xamarin.Forms.Platform.Android.Resource.Drawable.avd_show_password_1, skipping...
        Value not found for Xamarin.Forms.Platform.Android.Resource.Drawable.avd_show_password_2, skipping...
        Value not found for Xamarin.Forms.Platform.Android.Resource.Drawable.avd_show_password_3, skipping...
    
    I think it makes more sense to give the full name of the field, so it
    indicates the assembly it came from.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -40,7 +40,7 @@ namespace Xamarin.Android.Build.Tests
 		[Test]
 		public void BuildHasNoWarnings ([Values (true, false)] bool isRelease)
 		{
-			var proj = new XamarinAndroidApplicationProject {
+			var proj = new XamarinFormsAndroidApplicationProject {
 				IsRelease = isRelease,
 			};
 			using (var b = CreateApkBuilder (Path.Combine ("temp", TestName))) {
