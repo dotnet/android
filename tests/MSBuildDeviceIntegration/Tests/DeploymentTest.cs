1c01f9ec39632887e16cb3a4cd02e67d0c75e6be
Dean Ellis <dellis1972@googlemail.com>
2019-06-25 11:40:54 +0100

    Change the name of the TimeZone test project

--- tests/MSBuildDeviceIntegration/Tests/DeploymentTest.cs
+++ tests/MSBuildDeviceIntegration/Tests/DeploymentTest.cs
@@ -36,8 +36,9 @@ namespace Xamarin.Android.Build.Tests
 			builder = CreateApkBuilder (Path.Combine ("temp", "DeploymentTests"));
 			string apiLevel;
 			proj.TargetFrameworkVersion = builder.LatestTargetFrameworkVersion (out apiLevel);
+			proj.PackageName = "Xamarin.TimeZoneTest";
 			proj.AndroidManifest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
-<manifest xmlns:android=""http://schemas.android.com/apk/res/android"" android:versionCode=""1"" android:versionName=""1.0"" package=""UnnamedProject.UnnamedProject"">
+<manifest xmlns:android=""http://schemas.android.com/apk/res/android"" android:versionCode=""1"" android:versionName=""1.0"" package=""Xamarin.TimeZoneTest"">
 	<uses-sdk android:minSdkVersion=""24"" android:targetSdkVersion=""{apiLevel}"" />
 	<application android:label=""${{PROJECT_NAME}}"">
 	</application >
