5b1d77f57f273f6d8e274ce469c43094c7d47c3a
Ryan Lucia <rylucia@microsoft.com>
2019-06-26 14:29:14 -0400

    Readd _GenerateIncludeFiles target and dummy config.h

---
+++ src/monodroid/jni/config.h
@@ -0,0 +1,9 @@
+#ifndef INC_MONODROID_CONFIG_H
+#define INC_MONODROID_CONFIG_H
+
+#include <monodroid.h>
+#include <stdint.h>
+
+// This file only exists for the include in zlib.c
+
+#endif  /* !def INC_MONODROID_CONFIG_H */
