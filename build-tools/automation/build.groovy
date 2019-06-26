dotnet/android

commit 1e4d167ac45002adaf0711db99b9e68acaf087a6

[build] Don't override _MSBUILD_ARGS when creating installers
Context: https://github.com/xamarin/xamarin-android/commit/e83ba0ddc159d15abcfac1a5a4bce5a53e165d3f

The `_MSBUILD_ARGS` override was initially required because we did not
import different `rules.make` based on how our build was configured and
prepared. The `xaprepare` tool now better controls the ABIs we should be
building and packaging for each build type, so this is no longer needed.

build-tools/automation/build.groovy
@@ -188,10 +188,9 @@ timestamps {
 
         utils.stageWithTimeout('create installers', 30, 'MINUTES', XADir, true) {    // Typically takes less than 5 minutes
             if (isPr) {
-                // Override _MSBUILD_ARGS to ensure we only package the `AndroidSupportedTargetJitAbis` which are built.
-                // Also ensure that we don't require mono bundle components in the installer if this is not a full mono integration build.
+                // Exclude mono components if they aren't being built.
                 def msbuildInstallerArgs = hasPrLabelFullMonoIntegrationBuild ? '' : '/p:IncludeMonoBundleComponents=False'
-                sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1 _MSBUILD_ARGS='${msbuildInstallerArgs}'"
+                sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1 MSBUILD_ARGS='${msbuildInstallerArgs}'"
             } else {
                 sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1"
             }

commit 62b7f2f61d3432db7db88ca27b663ea866f89225

[build] Fix PR installer creation, and 'make jenkins'
Context: https://github.com/xamarin/xamarin-android/commit/a61fd98c3e1fb707329a83243d7088e7bf9ec955
Reverts: https://github.com/xamarin/xamarin-android/commit/1e4d167ac45002adaf0711db99b9e68acaf087a6

Fixes the PR build installer creation breakage[0] caused by 1e4d167 by
reverting it. Also fixes local `make jenkins` build failures, as the
recent changes in a61fd98c overlooked local build behavior. A 'full'
build was only being configured correctly when running from CI, and this
has been fixed by ensuring the `PREPARE_CI` make variable is set when
running `make jenkins` locally.

[0]
    11:03:56  "/Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/build-tools/create-pkg/create-pkg.csproj" (CreatePkg target) (1) ->
    11:03:56  (_CopyFilesToPayloadDir target) ->
    11:03:56    /Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/build-tools/create-pkg/create-pkg.targets(33,5): error MSB3030: Could not copy the file "/Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/bin/Release/lib/xamarin.android/xbuild/Xamarin/Android/lib/x86_64/libmono-android.debug.so" because it was not found. [/Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/build-tools/create-pkg/create-pkg.csproj]
    11:03:56    /Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/build-tools/create-pkg/create-pkg.targets(33,5): error MSB3030: Could not copy the file "/Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/bin/Release/lib/xamarin.android/xbuild/Xamarin/Android/lib/x86_64/libmono-android.release.so" because it was not found. [/Users/builder/jenkins/workspace/xamarin-android-pr-pipeline-release/xamarin-android/build-tools/create-pkg/create-pkg.csproj]

build-tools/automation/build.groovy
@@ -188,9 +188,10 @@ timestamps {
 
         utils.stageWithTimeout('create installers', 30, 'MINUTES', XADir, true) {    // Typically takes less than 5 minutes
             if (isPr) {
-                // Exclude mono components if they aren't being built.
+                // Override _MSBUILD_ARGS to ensure we only package the `AndroidSupportedTargetJitAbis` which are built.
+                // Also ensure that we don't require mono bundle components in the installer if this is not a full mono integration build.
                 def msbuildInstallerArgs = hasPrLabelFullMonoIntegrationBuild ? '' : '/p:IncludeMonoBundleComponents=False'
-                sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1 MSBUILD_ARGS='${msbuildInstallerArgs}'"
+                sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1 _MSBUILD_ARGS='${msbuildInstallerArgs}'"
             } else {
                 sh "make create-installers CONFIGURATION=${env.BuildFlavor} V=1"
             }

build-tools/scripts/BuildEverything.mk
@@ -10,7 +10,8 @@
 # The other targets depended upon by leeroy also require rules.mk to be present and thus they
 # are invoked in the same way framework-assemblies is
 #
-jenkins:: prepare
+jenkins::
+	$(MAKE) PREPARE_CI=1 prepare
 	$(MAKE) leeroy $(ZIP_OUTPUT)
 
 leeroy: leeroy-all framework-assemblies opentk-jcw