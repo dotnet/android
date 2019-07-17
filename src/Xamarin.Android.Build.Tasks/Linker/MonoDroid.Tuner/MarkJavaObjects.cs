dotnet/android

commit 5b945abb2e1f0fe1a9ab6589993c970869c4a752

[java.interop] improve JavaCast() for interfaces (#1059)
Currently, interfaces generate `Invoker` classes such as:

	// src/Mono.Android/obj/Debug/android-26/mcw/Java.Lang.IAppendable.cs
	partial class IAppendableInvoker {
	  public static IAppendable GetObject (IntPtr handle, JniHandleOwnership transfer)
	  {
	    return global::Java.Lang.Object.GetObject<IAppendable>(handle, transfer);
	  }
	}

`JavaObjectExtensions.JavaCast()` was previously looking up these
`Invoker` types and calling the `GetObject()` method via reflection.
We can simplify this by just calling `Java.Lang.Object.GetObject()`
directly—bypassing all the extra reflection.

One day we might be able to drop the generated `Invoker` method from
generator, but compiled bindings would break on older versions of
Xamarin.Android.

Allow apps to be smaller by improving the linker so that the
`*Invoker` types participate in linking. (Previously they were
preserved as-is, `GetObject()` method and all, as `adapter_type`
was resolved to the `*Invoker` type.) This allows the `GetObject()`
methods to be linked away normally.

We still need to preserve interface methods in the Invoker types.

src/Xamarin.Android.Build.Tasks/Linker/MonoDroid.Tuner/MarkJavaObjects.cs
@@ -149,6 +149,43 @@ namespace MonoDroid.Tuner {
 			}
 		}
 
+		string TypeNameWithoutKey (string name)
+		{
+			var idx = name.IndexOf (", PublicKeyToken=");
+			if (idx > 0)
+				name = name.Substring (0, idx);
+
+			return name;
+		}
+
+		bool CheckInvokerType (TypeDefinition type, string name)
+		{
+			return TypeNameWithoutKey (name) == TypeNameWithoutKey ($"{ type.FullName}, { type.Module.Assembly.FullName}");
+		}
+
+		void PreserveInterfaceMethods (TypeDefinition type, TypeDefinition invoker)
+		{
+			foreach (var m in type.GetMethods ()) {
+				string methodAndType;
+				if (!m.TryGetRegisterMember (out methodAndType))
+					continue;
+
+				if (!methodAndType.Contains (":"))
+					continue;
+
+				var values = methodAndType.Split (new char [] { ':' }, 2);
+				if (!CheckInvokerType (invoker, values [1]))
+					continue;
+
+				foreach (var invokerMethod in invoker.GetMethods ()) {
+					if (invokerMethod.Name == values [0]) {
+						PreserveMethod (invoker, invokerMethod);
+						break;
+					}
+				}
+			}
+		}
+
 		void PreserveInvoker (TypeDefinition type)
 		{
 			var invoker = PreserveHelperType (type, "Invoker");
@@ -156,6 +193,7 @@ namespace MonoDroid.Tuner {
 				return;
 
 			PreserveIntPtrConstructor (invoker);
+			PreserveInterfaceMethods (type, invoker);
 		}
 
 		TypeDefinition PreserveHelperType (TypeDefinition type, string suffix)

src/Mono.Android/Java.Interop/JavaObjectExtensions.cs
@@ -63,11 +63,7 @@ namespace Java.Interop {
 				return (TResult) CastClass (instance, resultType);
 			}
 			else if (resultType.IsInterface) {
-				Type invokerType = GetHelperType (resultType, "Invoker");
-				if (invokerType == null)
-					throw new ArgumentException ("Unable to get Invoker for interface '" + resultType.FullName + "'.", "TResult");
-				Func<IntPtr, JniHandleOwnership, TResult> getObject = (Func<IntPtr, JniHandleOwnership, TResult>) Delegate.CreateDelegate (typeof (Func<IntPtr, JniHandleOwnership, TResult>), invokerType, "GetObject");
-				return getObject (instance.Handle, JniHandleOwnership.DoNotTransfer);
+				return (TResult) Java.Lang.Object.GetObject (instance.Handle, JniHandleOwnership.DoNotTransfer, resultType);
 			}
 			else
 				throw new NotSupportedException (string.Format ("Unable to convert type '{0}' to '{1}'.",
@@ -113,11 +109,7 @@ namespace Java.Interop {
 				return CastClass (instance, resultType);
 			}
 			else if (resultType.IsInterface) {
-				Type invokerType = GetHelperType (resultType, "Invoker");
-				if (invokerType == null)
-					throw new ArgumentException ("Unable to get Invoker for interface '" + resultType.FullName + "'.", "resultType");
-				var getObject = invokerType.GetMethod ("GetObject", new[]{typeof (IntPtr), typeof (JniHandleOwnership)});
-				return (IJavaObject) getObject.Invoke (null, new object[]{instance.Handle, JniHandleOwnership.DoNotTransfer});
+				return Java.Lang.Object.GetObject (instance.Handle, JniHandleOwnership.DoNotTransfer, resultType);
 			}
 			else
 				throw new NotSupportedException (string.Format ("Unable to convert type '{0}' to '{1}'.",

src/Xamarin.Android.Build.Tasks/Linker/MonoDroid.Tuner/Extensions.cs
@@ -98,5 +98,60 @@ namespace MonoDroid.Tuner {
 			}
 			return false;
 		}
+
+		const string RegisterAttribute = "Android.Runtime.RegisterAttribute";
+
+		private static bool IsRegisterAttribute (CustomAttribute attribute)
+		{
+			var constructor = attribute.Constructor;
+
+			if (constructor.DeclaringType.FullName != RegisterAttribute)
+				return false;
+
+			if (!constructor.HasParameters)
+				return false;
+
+			if (constructor.Parameters.Count != 3)
+				return false;
+
+			return true;
+		}
+
+		public static bool TryGetRegisterAttribute (this MethodDefinition method, out CustomAttribute register)
+		{
+			register = null;
+
+			if (!method.HasCustomAttributes)
+				return false;
+
+			foreach (CustomAttribute attribute in method.CustomAttributes) {
+				if (!IsRegisterAttribute (attribute))
+					continue;
+
+				register = attribute;
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool TryGetRegisterMember (this MethodDefinition md, out string method)
+		{
+			CustomAttribute register;
+			method = null;
+
+			if (!md.TryGetRegisterAttribute (out register))
+				return false;
+
+			if (register.ConstructorArguments.Count != 3)
+				return false;
+
+			method = (string)register.ConstructorArguments [2].Value;
+
+			if (string.IsNullOrEmpty (method))
+				return false;
+
+			return true;
+		}
 	}
 }

src/Xamarin.Android.Build.Tasks/Linker/MonoDroid.Tuner/MonoDroidMarkStep.cs
@@ -11,7 +11,6 @@ namespace MonoDroid.Tuner
 {
 	class MonoDroidMarkStep : MarkStep
 	{
-		const string RegisterAttribute = "Android.Runtime.RegisterAttribute";
 		const string ICustomMarshalerName = "System.Runtime.InteropServices.ICustomMarshaler";
 
 		// If this is one of our infrastructure methods that has [Register], like:
@@ -21,7 +20,7 @@ namespace MonoDroid.Tuner
 		{
 			string member;
 
-			if (!TryGetRegisterMember (method, out member))
+			if (!method.TryGetRegisterMember (out member))
 				return;
 
 			PreserveRegisteredMethod (method.DeclaringType, member);
@@ -40,28 +39,6 @@ namespace MonoDroid.Tuner
 
 			foreach (MethodReference method in type.Methods)
 				MarkMethod (method);
-
-			// We also need to preserve the interface adapter
-			string adapter;
-
-			if (!TryGetRegisterMember (type, out adapter))
-				return;
-
-			// This is a fast path for finding the adapter type
-			var adapter_type = type.DeclaringType.GetNestedType (adapter);
-
-			// It wasn't in the same containing type, look through the whole assembly
-			if (adapter_type == null)
-				adapter_type = type.Module.FindType (adapter);
-
-			if (adapter_type == null)
-				return;
-
-			MarkType (adapter_type);
-
-			// We also need to preserve every member on the interface adapter
-			foreach (MethodReference m in adapter_type.Methods)
-				MarkMethod (m);
 		}
 		
 		private void PreserveRegisteredMethod (TypeDefinition type, string member)
@@ -80,59 +57,6 @@ namespace MonoDroid.Tuner
 
 			while (MarkNamedMethod (type, member) == 0 && type.BaseType != null)
 				type = type.BaseType.Resolve ();
-		}
-
-		private static bool TryGetRegisterMember (ICustomAttributeProvider provider, out string method)
-		{
-			CustomAttribute register;
-			method = null;
-
-			if (!TryGetRegisterAttribute (provider, out register))
-				return false;
-
-			if (register.ConstructorArguments.Count != 3)
-				return false;
-
-			method = (string) register.ConstructorArguments [2].Value;
-
-			if (string.IsNullOrEmpty (method))
-				return false;
-
-			return true;
-		}
-
-		private static bool TryGetRegisterAttribute (ICustomAttributeProvider provider, out CustomAttribute register)
-		{
-			register = null;
-
-			if (!provider.HasCustomAttributes)
-				return false;
-
-			foreach (CustomAttribute attribute in provider.CustomAttributes) {
-				if (!IsRegisterAttribute (attribute))
-					continue;
-
-				register = attribute;
-				return true;
-			}
-
-			return false;
-		}
-
-		private static bool IsRegisterAttribute (CustomAttribute attribute)
-		{
-			var constructor = attribute.Constructor;
-
-			if (constructor.DeclaringType.FullName != RegisterAttribute)
-				return false;
-
-			if (!constructor.HasParameters)
-				return false;
-
-			if (constructor.Parameters.Count != 3)
-				return false;
-
-			return true;
 		}
 
 		protected override TypeDefinition MarkType (TypeReference reference)

commit 643b3335d27a4c463de99f0ee904f82384e53aec

[linker] Make sure we mark *Invoker types
Fixes https://github.com/xamarin/xamarin-android/issues/3263

Looks like
https://github.com/xamarin/xamarin-android/commit/5b945abb2e1f0fe1a9ab6589993c970869c4a752
introduced a new regression, where in some cases we were not marking
the *Invoker type anymore, as [this
line](https://github.com/xamarin/xamarin-android/commit/5b945abb2e1f0fe1a9ab6589993c970869c4a752#diff-144727b152107ec306fbe284bd5902e3L60)
is gone.

This is fixed by marking the *Invoker types in MarkJavaObjects step.

src/Xamarin.Android.Build.Tasks/Linker/MonoDroid.Tuner/MarkJavaObjects.cs
@@ -192,6 +192,8 @@ namespace MonoDroid.Tuner {
 			if (invoker == null)
 				return;
 
+			Annotations.Mark (invoker);
+
 			PreserveIntPtrConstructor (invoker);
 			PreserveInterfaceMethods (type, invoker);
 		}