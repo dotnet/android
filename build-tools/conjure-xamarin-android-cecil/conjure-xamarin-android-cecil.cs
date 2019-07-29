dotnet/android

commit 0c9f83b7b15c386ff12b48a7c7295c3d35044adb

[build] Remove Mono submodule (#3371)
Context: https://github.com/xamarin/xamarin-android/projects/10

One of the goals behind the Mono SDK archive was to enable product
builds without having the entire Mono source tree present, which
means it should be possible to remove the Mono git submodule and
build the entire xamarin-android repo without any issues.

At the same time, we want to keep the ability to build Mono from
source, if necessary, for situations when a developer works on the
BCL or the runtime itself and there's no appropriate binary archive
present.

To support these two requirements:

  * Remove the Mono submodule
  * Put Mono repo commit reference into the `.external` file
  * Make distinction between "commercial" and "regular" bits in the
    `.external` file so that we don't attempt to check out everything
    mentioned in there in situations when a developer doesn't have
    access to the commercial bits.
  * Use Mono linker sources from the archive
  * Remove `zlib-helper.c` from the `libmonodroid` build - the API
    implemented in there is no longer used by Mono.
  * `Xamarin.Android.Cecil.dll` and `Xamarin.Android.Cecil.Mdb.dll`
    are no longer built from source.  Instead we use Cecil to reshape
    `Mono.Cecil` and conjure the two renamed and re-signed assemblies.
  * All the code which used `Xamarin.Android.Cecil` project reference
    now simply references the conjured assemblies (which are also
    added to the bundle)
  * Third Party Notices are generated from licenses found in the
    Mono archive.
  * Windows build downloads also the Darwin archive - in order to be
    able to use Mono Linker sources as well as licenses contained in
    this archive.

build-tools/conjure-xamarin-android-cecil/conjure-xamarin-android-cecil.cs
@@ -1,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Mono.Cecil;
+
+public class ConjureXamarinAndroidCecil
+{
+	const string BaseNameReplacement = "Xamarin.Android.Cecil";
+	const string CecilAssemblyName = BaseNameReplacement;
+	const string CecilMdbAssemblyName = BaseNameReplacement + ".Mdb";
+
+	static readonly List<string> internalsVisibleTo = new List<string> {
+		"Xamarin.Android.Cecil.Pdb, PublicKey=0024000004800000940000000602000000240000525341310004000011000000438ac2a5acfbf16cbd2b2b47a62762f273df9cb2795ceccdf77d10bf508e69e7a362ea7a45455bbf3ac955e1f2e2814f144e5d817efc4c6502cc012df310783348304e3ae38573c6d658c234025821fda87a0be8a0d504df564e2c93b2b878925f42503e9d54dfef9f9586d9e6f38a305769587b1de01f6c0410328b2c9733db",
+		"Xamarin.Android.Cecil.Mdb, PublicKey=0024000004800000940000000602000000240000525341310004000011000000438ac2a5acfbf16cbd2b2b47a62762f273df9cb2795ceccdf77d10bf508e69e7a362ea7a45455bbf3ac955e1f2e2814f144e5d817efc4c6502cc012df310783348304e3ae38573c6d658c234025821fda87a0be8a0d504df564e2c93b2b878925f42503e9d54dfef9f9586d9e6f38a305769587b1de01f6c0410328b2c9733db"
+	};
+
+	public static int Main (string[] args)
+	{
+		if (args.Length < 2) {
+			Console.WriteLine ("Usage: <input directory> <output directory>");
+			Console.WriteLine ("  <input directory> must have Mono.Cecil.dll and Mono.Cecil.Mdb.dll assemblies");
+			return 1;
+		}
+
+		string inputDir = args [0];
+		string inputFilePath = Path.Combine (inputDir, "Mono.Cecil.dll");
+		string outputDirPath = args [1];
+
+		var resolver = new DefaultAssemblyResolver ();
+		resolver.AddSearchDirectory (Path.GetDirectoryName (inputFilePath));
+		var rp = new ReaderParameters () {
+			AssemblyResolver = resolver,
+			ReadSymbols = true
+		};
+		var monoCecil = AssemblyDefinition.ReadAssembly (inputFilePath, rp);
+		monoCecil.Name.Name = CecilAssemblyName;
+
+		var ivtCtor = monoCecil.MainModule.ImportReference (typeof (System.Runtime.CompilerServices.InternalsVisibleToAttribute).GetConstructor (new []{typeof(string)}));
+		foreach (string ivtParam in internalsVisibleTo) {
+			var ca = new CustomAttribute (ivtCtor);
+			ca.ConstructorArguments.Add (new CustomAttributeArgument (monoCecil.MainModule.TypeSystem.String, ivtParam));
+			monoCecil.CustomAttributes.Add (ca);
+		}
+
+		var wp = new WriterParameters {
+			WriteSymbols = true
+		};
+
+		monoCecil.Write (Path.Combine (outputDirPath, $"{CecilAssemblyName}.dll"), wp);
+
+		inputFilePath = Path.Combine (inputDir, "Mono.Cecil.Mdb.dll");
+		var monoCecilMdb = AssemblyDefinition.ReadAssembly (inputFilePath, rp);
+		monoCecilMdb.Name.Name = CecilMdbAssemblyName;
+
+		AssemblyNameReference monoCecilRef = monoCecilMdb.MainModule.AssemblyReferences.Single (r => String.Compare ("Mono.Cecil", r.Name, StringComparison.Ordinal) == 0);
+		monoCecilRef.Name = CecilAssemblyName;
+		monoCecilMdb.Write (Path.Combine (outputDirPath, $"{CecilMdbAssemblyName}.dll"), wp);
+
+		return 0;
+	}
+}

.external
@@ -1,1 +1,2 @@
 xamarin/monodroid:master@237e0bd9f105b9842778ef82161a20c6d4497a40
+mono/mono:2019-06@afcf28a3660b069721e5c441dfe5bb7548e3204f

.gitignore
@@ -3,7 +3,6 @@
 bin
 Configuration.Override.props
 Configuration.OperatingSystem.props
-msfinal.pub
 obj
 packages
 .DS_Store

.gitmodules
@@ -22,10 +22,6 @@
     path = external/mman-win32
     url = https://github.com/witwall/mman-win32.git
     branch = master
-[submodule "external/mono"]
-    path = external/mono
-    url = https://github.com/mono/mono.git
-    branch = 2019-06
 [submodule "external/mxe"]
     path = external/mxe
     url = https://github.com/xamarin/mxe.git

Configuration.props
@@ -88,7 +88,7 @@
     <MonoRequiredMaximumVersion Condition=" '$(MonoRequiredMaximumVersion)' == '' ">6.5.0</MonoRequiredMaximumVersion>
     <IgnoreMaxMonoVersion Condition=" '$(IgnoreMaxMonoVersion)' == '' ">True</IgnoreMaxMonoVersion>
     <MonoRequiredDarwinMinimumVersion>$(MonoRequiredMinimumVersion).98</MonoRequiredDarwinMinimumVersion>
-    <LinkerSourceDirectory>$(MSBuildThisFileDirectory)external\mono\external\linker\src</LinkerSourceDirectory>
+    <LinkerSourceDirectory>$(MSBuildThisFileDirectory)external\mono\sdks\out\android-sources\external\linker\src</LinkerSourceDirectory>
     <OpenTKSourceDirectory>$(MSBuildThisFileDirectory)external\opentk</OpenTKSourceDirectory>
     <MingwZlibRootDirectory Condition=" '$(ZlibRootDirectory)' == '' And '$(HostOS)' == 'Linux' ">\usr</MingwZlibRootDirectory>
     <MingwZlibRootDirectory Condition=" '$(ZlibRootDirectory)' == '' And '$(HostOS)' == 'Darwin' ">$(HostHomebrewPrefix)\opt\mingw-zlib\usr</MingwZlibRootDirectory>

build-tools/conjure-xamarin-android-cecil/conjure-xamarin-android-cecil.csproj
@@ -5,8 +5,8 @@
     <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
     <ProjectGuid>{C876DA71-8573-4CEF-9149-716D72455ED4}</ProjectGuid>
     <OutputType>Exe</OutputType>
-    <RootNamespace>remapassemblyref</RootNamespace>
-    <AssemblyName>remap-assembly-ref</AssemblyName>
+    <RootNamespace>Xamarin.Android.Prepare</RootNamespace>
+    <AssemblyName>conjure-xamarin-android-cecil</AssemblyName>
   </PropertyGroup>
   <Import Project="..\..\Configuration.props" />
   <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
@@ -32,7 +32,7 @@
     </Reference>
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="remap-assembly-ref.cs" />
+    <Compile Include="conjure-xamarin-android-cecil.cs" />
   </ItemGroup>
   <Import Project="$(MSBuildBinPath)\Microsoft.CSharp.targets" />
 </Project>

build-tools/scripts/XAVersionInfo.targets
@@ -6,15 +6,13 @@
   <UsingTask AssemblyFile="$(MSBuildThisFileDirectory)..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"  TaskName="Xamarin.Android.BuildTools.PrepTasks.GitCommitsInRange" />
   <UsingTask AssemblyFile="$(MSBuildThisFileDirectory)..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"  TaskName="Xamarin.Android.BuildTools.PrepTasks.GitCommitTime" />
   <UsingTask AssemblyFile="$(MSBuildThisFileDirectory)..\..\bin\Build$(Configuration)\xa-prep-tasks.dll"  TaskName="Xamarin.Android.BuildTools.PrepTasks.GitCommitInfo" />
+  <Import Project="..\..\bin\Build$(Configuration)\MonoGitHash.props" />
   <Target Name="_GetSubmodulesVersionInfo">
     <ItemGroup>
       <!-- If anything changes in this list, make sure to update:
            src/Xamarin.Android.Build.Tasks/Xamarin.Android.Build.Tasks.targets (the _GenerateXACommonProps target)
            build-tools/create-vsix/create-vsix.targets (the _CreateDependencies target)
       -->
-      <_SubmoduleBranchInfo Include="external/mono">
-        <OutputPropertyName>_BuildInfo_MonoCommit</OutputPropertyName>
-      </_SubmoduleBranchInfo>
       <_SubmoduleBranchInfo Include="external/Java.Interop">
         <OutputPropertyName>_BuildInfo_JavaInteropCommit</OutputPropertyName>
       </_SubmoduleBranchInfo>

build-tools/xaprepare/xaprepare/Application/BuildInfo.cs
@@ -105,7 +105,13 @@ namespace Xamarin.Android.Prepare
 			LibZipHash = EnsureHash ("LibZip", Utilities.ShortenGitHash (FullLibZipHash));
 
 			Log.StatusLine ($"  {context.Characters.Bullet} Mono commit hash", ConsoleColor.Gray);
-			FullMonoHash = git.GetTopCommitHash (context.Properties.GetRequiredValue (KnownProperties.MonoSourceFullPath), shortHash: false);
+			List<ExternalGitDependency> externalDependencies = ExternalGitDependency.GetDependencies (context, Configurables.Paths.ExternalGitDepsFilePath, quiet: true);
+			ExternalGitDependency mono = externalDependencies?.Where (
+				eg => eg != null &&
+				      String.Compare ("mono", eg.Owner, StringComparison.Ordinal) == 0 &&
+				      String.Compare ("mono", eg.Name, StringComparison.Ordinal) == 0).FirstOrDefault ();
+
+			FullMonoHash = mono?.Commit?.Trim ();
 			MonoHash = EnsureHash ("Mono", Utilities.ShortenGitHash (FullMonoHash));
 
 			if (Configurables.Paths.BundleVersionHashFiles == null || Configurables.Paths.BundleVersionHashFiles.Count == 0) {

commit a45dc365306057cd7418fabbf9036b0ec6e11aed

[xaprepare] correct strong name for Xamarin.Android.Cecil.*
Context: http://build.azdo.io/2882865
Context: https://github.com/jbevain/cecil/blob/ab5075b3a885ffd5bee5d347b79ce71aabeb32d1/Mono.Cecil.Cil/Symbols.cs#L957-L978

Two tests on Windows have been failing on master since 0c9f83b7:

    Xamarin.Android.Build.Tests.BuildTest.BuildHasNoWarnings
        Should have zero MSBuild warnings.
        Expected: True
        But was: False

The actual warning is:

    (_GenerateJavaStubs target) ->
    warning : Failed to read 'obj\Debug\android\assets\HelloWorld.dll' with debugging symbols. Retrying to load it without it. Error detail s are logged below. [C:\src\xamarin-android\samples\HelloWorld\HelloWorld.csproj]
    warning : Mono.Cecil.Cil.SymbolsNotFoundException: No symbol found for file: obj\Debug\android\assets\HelloWorld.dll [C:\src\xamarin-an droid\samples\HelloWorld\HelloWorld.csproj]
    warning :    at Mono.Cecil.Cil.DefaultSymbolReaderProvider.GetSymbolReader(ModuleDefinition module, String fileName) [C:\src\xamarin-andr oid\samples\HelloWorld\HelloWorld.csproj]
    warning :    at Mono.Cecil.ModuleReader.ReadSymbols(ModuleDefinition module, ReaderParameters parameters) [C:\src\xamarin-android\samples \HelloWorld\HelloWorld.csproj]
    warning :    at Mono.Cecil.ModuleReader.CreateModule(Image image, ReaderParameters parameters) [C:\src\xamarin-android\samples\HelloWorld \HelloWorld.csproj]
    warning :    at Mono.Cecil.ModuleDefinition.ReadModule(String fileName, ReaderParameters parameters) [C:\src\xamarin-android\samples\Hell oWorld\HelloWorld.csproj]
    warning :    at Mono.Cecil.AssemblyDefinition.ReadAssembly(String fileName, ReaderParameters parameters) [C:\src\xamarin-android\samples\ HelloWorld\HelloWorld.csproj]

build-tools/conjure-xamarin-android-cecil/conjure-xamarin-android-cecil.cs
@@ -10,10 +10,11 @@ public class ConjureXamarinAndroidCecil
 	const string BaseNameReplacement = "Xamarin.Android.Cecil";
 	const string CecilAssemblyName = BaseNameReplacement;
 	const string CecilMdbAssemblyName = BaseNameReplacement + ".Mdb";
+	const string PublicKey = "024004800094000620002400525341310400101079159977d2d03a8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d462447c37ee68c080536fcf3c3fbe2ff9c979ce998475e56e8ce82dd5bf35dc1e93bf2eeecf874b2477c5081dbea7447fddafa277b22de47d6ffea449674a4f9fccf84d1506989380284dbdd35f46cdff12a1bd78e4ef065d016df";
 
 	static readonly List<string> internalsVisibleTo = new List<string> {
-		"Xamarin.Android.Cecil.Pdb, PublicKey=0024000004800000940000000602000000240000525341310004000011000000438ac2a5acfbf16cbd2b2b47a62762f273df9cb2795ceccdf77d10bf508e69e7a362ea7a45455bbf3ac955e1f2e2814f144e5d817efc4c6502cc012df310783348304e3ae38573c6d658c234025821fda87a0be8a0d504df564e2c93b2b878925f42503e9d54dfef9f9586d9e6f38a305769587b1de01f6c0410328b2c9733db",
-		"Xamarin.Android.Cecil.Mdb, PublicKey=0024000004800000940000000602000000240000525341310004000011000000438ac2a5acfbf16cbd2b2b47a62762f273df9cb2795ceccdf77d10bf508e69e7a362ea7a45455bbf3ac955e1f2e2814f144e5d817efc4c6502cc012df310783348304e3ae38573c6d658c234025821fda87a0be8a0d504df564e2c93b2b878925f42503e9d54dfef9f9586d9e6f38a305769587b1de01f6c0410328b2c9733db"
+		$"Xamarin.Android.Cecil.Pdb, PublicKey={PublicKey}",
+		$"Xamarin.Android.Cecil.Mdb, PublicKey={PublicKey}"
 	};
 
 	public static int Main (string[] args)