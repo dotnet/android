037572d39474b2c260641b3759e3cfa5529ea7bd
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-07-30 14:09:12 -0500

    [Xamarin.Android.Build.Tasks] fix the NuGetizer 3000 (#3379)
    Fixes: http://work.azdo.io/917139
    Context: https://github.com/NuGet/Home/wiki/NuGetizer-Core-Features
    
    The NuGetizer 3000, was failing on Xamarin.Android 9.4 with:
    
    	The "FilterAssemblies" task was not given a value for the required parameter "DesignTimeBuild".
    
    This tool runs a `GetPackageContents` MSBuild target that is defined
    in the [`NuGet.Build.Packaging` MSBuild targets][0].
    
    Since this isn't a `Build` or a `Compile`, neither of these targets
    will run:
    
      * `_SetupDesignTimeBuildForBuild`
      * `_SetupDesignTimeBuildForCompile`
    
    I don't think we can make the `_AddAndroidCustomMetaData` MSBuild
    target (that runs `<FilterAssemblies/>`) depend on either of these.
    
    The simple fix is to just remove `[Required]` from the
    `FilterAssemblies.DesignTimeBuild` property, as it will be fine for
    it to default to `false`.
    
    [0]: https://github.com/NuGet/NuGet.Build.Packaging/blob/88f9e4b5e95c0d2b7b7ac12fb721675a851fcd5d/src/Build/NuGet.Build.Packaging.Tasks/NuGet.Build.Packaging.targets#L201

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -3889,6 +3889,17 @@ public class MyWorker : Worker
 				Assert.IsTrue (b.Build (proj), "Build should have succeeded.");
 			}
 		}
+
+		[Test]
+		public void NuGetizer3000 ()
+		{
+			var proj = new XamarinAndroidApplicationProject ();
+			proj.PackageReferences.Add (KnownPackages.NuGet_Build_Packaging);
+			using (var b = CreateApkBuilder (Path.Combine ("temp", nameof (NuGetizer3000)))) {
+				b.Target = "GetPackageContents";
+				Assert.IsTrue (b.Build (proj), $"{b.Target} should have succeeded.");
+			}
+		}
 	}
 }
 
