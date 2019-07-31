dotnet/android

commit b1edd489cd5e79c8318c926f0e23ba251c72ba48

[Mono.Android] Android API-29 enumification and stabilization (#3305)
API-29/v10.0 is now stable.

Bumps to xamarin/xamarin-android-api-compatibility/d16-3@da270b88.

Bump Targets/`$(TargetSdkVersion)` to v10.0 (API-29).

Add `src/Mono.Android/Profiles/api-29.params.txt` for parameter names.

Add new enums and fix method signatures (`map.csv`, `methodmap.csv`).

Fix metadata whitespace formatting.

Update Android Emulator to use 29 image

Bump test timeouts.

Fix test issues:

  * [build-tools] Update the logcat timing patterns for API-29
  * [logcat processing] Update activity displayed detection
  * Fixing some deploy/undeploy apks to emulator
  * Ignore exit code for `adb logcat -c`
  * Allow BuildTest.BuildHasNoWarnings to have up to 1 warning due to
    be incompatible with Api level 29

Remove `#if !ANDROID_29` condition from `StringBuffer.cs` and
`StringBuilder.cs`, added in 2cace0b3, to preserve API compatibility.

build-tools/xaprepare/xaprepare/ConfigAndData/Dependencies/AndroidToolchain.cs
@@ -38,12 +38,12 @@ namespace Xamarin.Android.Prepare
 				new AndroidPlatformComponent ("platform-26_r02",   apiLevel: "26", pkgRevision: "2"),
 				new AndroidPlatformComponent ("platform-27_r03",   apiLevel: "27", pkgRevision: "3"),
 				new AndroidPlatformComponent ("platform-28_r04",   apiLevel: "28", pkgRevision: "4"),
-				new AndroidPlatformComponent ("platform-Q_r03",    apiLevel: "Q",  pkgRevision: "3"),
+				new AndroidPlatformComponent ("platform-29_r01",   apiLevel: "29", pkgRevision: "1"),
 
 				new AndroidToolchainComponent ("docs-24_r01",                                       destDir: "docs", pkgRevision: "1"),
 				new AndroidToolchainComponent ("android_m2repository_r47",                          destDir: Path.Combine ("extras", "android", "m2repository"), pkgRevision: "47.0.0"),
-				new AndroidToolchainComponent ("x86-28_r04",                                        destDir: Path.Combine ("system-images", "android-28", "default", "x86"), relativeUrl: new Uri ("sys-img/android/", UriKind.Relative), pkgRevision: "4"),
-				new AndroidToolchainComponent ($"android-ndk-r{AndroidNdkVersion}-{osTag}-x86_64",  destDir: AndroidNdkDirectory, pkgRevision: "19.2.5345600"),
+				new AndroidToolchainComponent ("x86-29_r06",                                        destDir: Path.Combine ("system-images", "android-29", "default", "x86"), relativeUrl: new Uri ("sys-img/android/", UriKind.Relative), pkgRevision: "6"),
+				new AndroidToolchainComponent ($"android-ndk-r{AndroidNdkVersion}-{osTag}-x86_64",  destDir: AndroidNdkDirectory, pkgRevision: AndroidPkgRevision),
 				new AndroidToolchainComponent ($"build-tools_r{XABuildToolsVersion}-{altOsTag}",    destDir: Path.Combine ("build-tools", XABuildToolsFolder), isMultiVersion: true),
 				new AndroidToolchainComponent ($"platform-tools_r{XAPlatformToolsVersion}-{osTag}", destDir: "platform-tools", pkgRevision: XAPlatformToolsVersion),
 				new AndroidToolchainComponent ($"sdk-tools-{osTag}-4333796",                        destDir: "tools", pkgRevision: "26.1.1"),

Configuration.Override.props.in
@@ -6,11 +6,11 @@
       kept consistent with each other, lest Bad Things Happen™
       -->
     <!-- The default Android API level to bind in src/Mono.Android -->
-    <AndroidApiLevel>28</AndroidApiLevel>
+    <AndroidApiLevel>29</AndroidApiLevel>
     <!-- The Xamarin.Android $(TargetFrameworkVersion) value that corresponds to $(AndroidApiLevel) -->
-    <AndroidFrameworkVersion>v9.0</AndroidFrameworkVersion>
+    <AndroidFrameworkVersion>v10.0</AndroidFrameworkVersion>
     <!-- The default Android API "id" that corresponds to $(AndroidApiLevel) -->
-    <AndroidPlatformId>28</AndroidPlatformId>
+    <AndroidPlatformId>29</AndroidPlatformId>
 
     <!--
       Colon-separated list of ABIs to build the mono JIT for.
@@ -24,7 +24,7 @@
     <AndroidSupportedTargetJitAbis>armeabi-v7a:arm64-v8a:x86:x86_64</AndroidSupportedTargetJitAbis>
 
     <!--
-        Colon-separated list of ABIs to build AOT cross-compilers for. 
+        Colon-separated list of ABIs to build AOT cross-compilers for.
         There doesn't need to be any parity between the AOT targets and the JIT ABIs above.
         Supported targets are:
 

Configuration.props
@@ -18,19 +18,15 @@
     <TargetFrameworkVersion Condition=" '$(TargetFrameworkVersion)' == '' And '$(_StandardLibraryPath)' != '' ">v4.7.2</TargetFrameworkVersion>
     <TargetFrameworkVersion Condition=" '$(TargetFrameworkVersion)' == '' ">v4.7.1</TargetFrameworkVersion>
     <!-- Used by the `build-tools/create-vsix` build so that `Mono.Android.Export.dll`/etc. are only included *once* -->
-    <!-- Should correspond to the first value from `$(API_LEVELS)` in `build-tools/scripts/BuildEverything.mk` -->
+    <!-- Should correspond to the first value from `$(API_LEVELS)` in `build-tools/api-xml-adjuster/Makefile` -->
     <AndroidFirstFrameworkVersion Condition="'$(AndroidFirstFrameworkVersion)' == ''">v4.4</AndroidFirstFrameworkVersion>
     <AndroidFirstApiLevel Condition="'$(AndroidFirstApiLevel)' == ''">19</AndroidFirstApiLevel>
     <AndroidFirstPlatformId Condition="'$(AndroidFirstPlatformId)' == ''">$(AndroidFirstApiLevel)</AndroidFirstPlatformId>
     <_IsRunningNuGetRestore Condition="$(RestoreTaskAssemblyFile.EndsWith('NuGet.exe', StringComparison.InvariantCultureIgnoreCase))">True</_IsRunningNuGetRestore>
     <!-- *Latest* *stable* API level binding that we support; used when building src/Xamarin.Android.Build.Tasks -->
-    <AndroidLatestStableApiLevel Condition="'$(AndroidLatestStableApiLevel)' == ''">28</AndroidLatestStableApiLevel>
+    <AndroidLatestStableApiLevel Condition="'$(AndroidLatestStableApiLevel)' == ''">29</AndroidLatestStableApiLevel>
     <AndroidLatestStablePlatformId Condition="'$(AndroidLatestStablePlatformId)' == ''">$(AndroidLatestStableApiLevel)</AndroidLatestStablePlatformId>
-    <AndroidLatestStableFrameworkVersion Condition="'$(AndroidLatestStableFrameworkVersion)'==''">v9.0</AndroidLatestStableFrameworkVersion>
-    <!-- *Latest* (possibly unstable) API level binding that we support; for informational purposes -->
-    <AndroidLatestApiLevel Condition="'$(AndroidLatestApiLevel)' == ''">29</AndroidLatestApiLevel>
-    <AndroidLatestPlatformId Condition=" '$(AndroidLatestPlatformId)' == '' ">Q</AndroidLatestPlatformId>
-    <AndroidLatestFrameworkVersion Condition="'$(AndroidLatestFrameworkVersion)' == ''">v9.0.99</AndroidLatestFrameworkVersion>
+    <AndroidLatestStableFrameworkVersion Condition="'$(AndroidLatestStableFrameworkVersion)'==''">v10.0</AndroidLatestStableFrameworkVersion>
     <!-- The API level and TargetFrameworkVersion for the default Mono.Android.dll build -->
     <AndroidApiLevel Condition=" '$(AndroidApiLevel)' == '' ">$(AndroidLatestStableApiLevel)</AndroidApiLevel>
     <AndroidPlatformId Condition=" '$(AndroidPlatformId)' == '' ">$(AndroidLatestStablePlatformId)</AndroidPlatformId>

build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/RunInstrumentationTests.cs
@@ -96,6 +96,7 @@ namespace Xamarin.Android.Tools.BootstrapTasks
 
 				new CommandInfo {
 					ArgumentsString = $"{AdbTarget} {AdbOptions} logcat -c",
+					IgnoreExitCode = true,
 				},
 
 				new CommandInfo {

build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/RunUITests.cs
@@ -41,6 +41,7 @@ namespace Xamarin.Android.Tools.BootstrapTasks
 
 				new CommandInfo {
 					ArgumentsString = $"{AdbTarget} {AdbOptions} logcat -c",
+					IgnoreExitCode = true,
 				},
 
 			};

build-tools/api-xml-adjuster/Makefile
@@ -17,7 +17,7 @@ API_XML_TOOL = $(BUILDBIN)/api-xml-adjuster.exe
 RUNTIME = mono --debug
 RUN_CLASS_PARSE = $(RUNTIME) $(CLASS_PARSE)
 RUN_API_XML_TOOL = $(RUNTIME) $(API_XML_TOOL)
-API_LEVELS = 10 15 16 17 18 19 20 21 22 23 24 25 26 27 28 Q
+API_LEVELS = 10 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
 
 XML_OUTPUT_DIR = .
 
@@ -125,4 +125,3 @@ $(DOCS_DIR)/docs-api-25: docs-25_web.zip	# Google hadn't published docs componen
 
 $(DOCS_DIR)/docs-api-26: docs-O_web.zip	# No preview docs yet
 	$(call extract-docs,$<,docs)
-

build-tools/api-xml-adjuster/api-xml-adjuster.csproj
@@ -45,13 +45,6 @@
       <ReferenceOutputAssembly>False</ReferenceOutputAssembly>
     </ProjectReference>
   </ItemGroup>
-  <ItemGroup>
-    <ProjectReference Include="..\android-toolchain\android-toolchain.csproj">
-      <Project>{8FF78EB6-6FC8-46A7-8A15-EBBA9045C5FA}</Project>
-      <Name>android-toolchain</Name>
-      <ReferenceOutputAssembly>False</ReferenceOutputAssembly>
-    </ProjectReference>
-  </ItemGroup>
   <Import Project="$(MSBuildBinPath)\Microsoft.CSharp.targets" />
   <Import Project="api-xml-adjuster.targets" />
 </Project>

build-tools/automation/azure-pipelines.yaml
@@ -431,7 +431,7 @@ stages:
         solution: tests/Xamarin.Forms-Performance-Integration/Droid/Xamarin.Forms.Performance.Integration.Droid.csproj
         configuration: $(ApkTestConfiguration)
         msbuildArguments: >
-          /t:AcquireAndroidTarget,SignAndroidPackage,UndeployTestApks,DeployTestApks,RunTestApks,ReportComponentFailures
+          /t:AcquireAndroidTarget,SignAndroidPackage,DeployTestApks,RunTestApks,UndeployTestApks,ReportComponentFailures
           /bl:$(System.DefaultWorkingDirectory)/bin/Test$(ApkTestConfiguration)/XamarinFormsPerf.binlog
       condition: succeededOrFailed()
 
@@ -441,7 +441,7 @@ stages:
         solution: tests/Xamarin.Forms-Performance-Integration/Droid/Xamarin.Forms.Performance.Integration.Droid.csproj
         configuration: $(ApkTestConfiguration)
         msbuildArguments: >
-          /t:AcquireAndroidTarget,SignAndroidPackage,UndeployTestApks,DeployTestApks,RunTestApks,ReportComponentFailures
+          /t:AcquireAndroidTarget,SignAndroidPackage,DeployTestApks,RunTestApks,UndeployTestApks,ReportComponentFailures
           /bl:$(System.DefaultWorkingDirectory)/bin/Test$(ApkTestConfiguration)/XamarinFormsPerf-Aot.binlog
           /p:AotAssemblies=true
       condition: succeededOrFailed()
@@ -452,7 +452,7 @@ stages:
         solution: tests/Xamarin.Forms-Performance-Integration/Droid/Xamarin.Forms.Performance.Integration.Droid.csproj
         configuration: $(ApkTestConfiguration)
         msbuildArguments: >
-          /t:AcquireAndroidTarget,SignAndroidPackage,UndeployTestApks,DeployTestApks,RunTestApks,ReportComponentFailures
+          /t:AcquireAndroidTarget,SignAndroidPackage,DeployTestApks,RunTestApks,UndeployTestApks,ReportComponentFailures
           /bl:$(System.DefaultWorkingDirectory)/bin/Test$(ApkTestConfiguration)/XamarinFormsPerf-Profiled.binlog
           /p:AndroidEnableProfiledAot=true
       condition: succeededOrFailed()
@@ -463,7 +463,7 @@ stages:
         solution: tests/Xamarin.Forms-Performance-Integration/Droid/Xamarin.Forms.Performance.Integration.Droid.csproj
         configuration: $(ApkTestConfiguration)
         msbuildArguments: >
-          /t:AcquireAndroidTarget,SignAndroidPackage,UndeployTestApks,DeployTestApks,RunTestApks,ReportComponentFailures
+          /t:AcquireAndroidTarget,SignAndroidPackage,DeployTestApks,RunTestApks,UndeployTestApks,ReportComponentFailures
           /bl:$(System.DefaultWorkingDirectory)/bin/Test$(ApkTestConfiguration)/XamarinFormsPerf-Bundle.binlog
           /p:BundleAssemblies=true
       condition: succeededOrFailed()

commit eb05bc6d18c0de82b77bc79dee740fd13588d9b3

[prepare] Fix build break
b1edd489 inadvertently introduced a [build break][0]:

	ConfigAndData/Dependencies/AndroidToolchain.cs(46,132): error CS0103: The name 'AndroidPkgRevision' does not exist in the current context

Oops.

Fix the build break.

[0]: https://devdiv.visualstudio.com/DevDiv/_build/results?buildId=2900128&view=logs&jobId=9342f22e-3bed-57a4-899d-de689bc4e23b

build-tools/xaprepare/xaprepare/ConfigAndData/Dependencies/AndroidToolchain.cs
@@ -13,6 +13,7 @@ namespace Xamarin.Android.Prepare
 		public AndroidToolchain ()
 		{
 			string AndroidNdkVersion       = BuildAndroidPlatforms.AndroidNdkVersion;
+			string AndroidPkgRevision      = BuildAndroidPlatforms.AndroidNdkPkgRevision;
 			string AndroidNdkDirectory     = GetRequiredProperty (KnownProperties.AndroidNdkDirectory);
 			string AndroidCmakeVersion     = GetRequiredProperty (KnownProperties.AndroidCmakeVersion);
 			string AndroidCmakeVersionPath = GetRequiredProperty (KnownProperties.AndroidCmakeVersionPath);