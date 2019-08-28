c9ed134e5d3eb94f6c09a9e3067f5c60c4a32833
Dean Ellis <dellis1972@googlemail.com>
2019-08-28 20:53:38 +0100

    [build] Remove libzip and LibZipSharp (#3375)
    Changes: https://github.com/xamarin/monodroid/compare/63bbeb076d809c74811a8001d38bf2e9e8672627...ba0a823f07a72a8e820e3eb5724a58ef3af165db
    Changes: https://github.com/xamarin/androidtools/compare/2fb054e...d78c6b2
    Changes: https://github.com/xamarin/android-sdk-installer/compare/4c4daa52814587b6f28eb634847d05102dafc8df...c092183e87e7d84fddf3b1292dfd4ddd0f18f9f9
    
    Bumps to xamarin/monodroid/master@ba0a823f:
    
      * Bumps to xamarin/android-sdk-installer/master@c092183e
      * Bumps to xamarin/androidtools/master@d78c6b2
    
    The [`LibZipSharp` repo][0] is now available as the
    [Xamarin.LibZipSharp NuGet package][1].
    
    Remove the submodules for libzip and LibZipSharp in favour of using
    the nuget package.
    
    The `Xamarin.LibZipSharp` package contains the native binaries for
    `libzip`, however by default the NuGet package does NOT copy these
    into `$(OutputPath)`.  In order for that to happen the following
    MSBuild property needs to be set:
    
    	<LibZipSharpBundleAllNativeLibraries>True</LibZipSharpBundleAllNativeLibraries>
    
    When `$(LibZipSharpBundleAllNativeLibraries)`=True, the native
    binaries will be copied to the `$(OutputPath)\@PLATFORM@` directories
    where `@PLATFORM@` will be `Darwin`, `Linux` or `Windows`.
    
    This means we need to update our build system to make sure we include
    the files from the correct locations.  We also need to make sure we
    call the `Restore` target on projects using `LibZipSharp` now as the
    package will need to be restored.
    
    Another change is that we have removed the versioning from the
    `libzip` native libraries: they used to be e.g. `libzip.5.0.dylib`
    but are now just `libzip.dylib`.  This was done upstream in the
    `Xamarin.LibZipSharp` package itself to allow for support for .NET
    Core based `DllImport` which do NOT use a `.dll.config` file, but
    instead use only the dll name (e.g `libzip`).
    
    We are also including the `Linux` version of the binaries in the
    installer.
    
    [0]: https://github.com/xamarin/LibZipSharp
    [1]: https://www.nuget.org/packages/Xamarin.LibZipSharp/

--- build-tools/xaprepare/xaprepare/ThirdPartyNotices/libzip.cs
+++ build-tools/xaprepare/xaprepare/ThirdPartyNotices/libzip.cs
@@ -7,14 +7,16 @@ namespace Xamarin.Android.Prepare
 	[TPN]
 	class libzip_nih_at_libzip_TPN : ThirdPartyNotice
 	{
-		static readonly Uri    url         = new Uri ("https://github.com/nih-at/libzip/");
-		static readonly string licenseFile = Path.Combine ("external", "libzip", "LICENSE");
+		static readonly Uri url = new Uri ("https://github.com/nih-at/libzip/");
+		static readonly string licenseFile = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.UserProfile),
+			".nuget", "packages", "xamarin.libzipsharp", LibZipSharp_grendello_LibZipSharp_TPN.LibZipSharpVersion,
+			"Licences", "libzip", "LICENSE");
 
 		public override string LicenseFile => licenseFile;
-		public override string Name        => "nih-at/libzip";
-		public override Uri    SourceUrl   => url;
+		public override string Name => "nih-at/libzip";
+		public override Uri SourceUrl => url;
 		public override string LicenseText => null;
 
-		public override bool   Include (bool includeExternalDeps, bool includeBuildDeps) => includeExternalDeps;
+		public override bool Include (bool includeExternalDeps, bool includeBuildDeps) => includeExternalDeps;
 	}
 }
