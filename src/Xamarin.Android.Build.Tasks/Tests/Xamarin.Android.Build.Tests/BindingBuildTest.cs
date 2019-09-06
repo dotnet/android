06051979/xamarin-android

[Xamarin.Android.Build.Tasks] Add MSBuild support for DIM.

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BindingBuildTest.cs
@@ -510,5 +510,63 @@ AAAA==";
 				}
 			}
 		}
+
+		[Test]
+		[TestCaseSource (nameof (ClassParseOptions))]
+		public void BindDefaultInterfaceMethods (string classParser)
+		{
+			var proj = new XamarinAndroidBindingProject {
+				IsRelease = true,
+			};
+
+			// The sources for the .jar is in the jar itself.
+			string classesJarBase64 = @"
+UEsDBBQACAgIANWk6UwAAAAAAAAAAAAAAAAJAAQATUVUQS1JTkYv/soAAAMAUEsHCAAAAAACAAAAAAA
+AAFBLAwQUAAgICADVpOlMAAAAAAAAAAAAAAAAFAAAAE1FVEEtSU5GL01BTklGRVNULk1G803My0xLLS
+7RDUstKs7Mz7NSMNQz4OVyLkpNLElN0XWqBAlY6BnEG5obKmj4FyUm56QqOOcXFeQXJZYA1WvycvFyA
+QBQSwcIFGFrLUQAAABFAAAAUEsDBAoAAAgAAK2k6UwAAAAAAAAAAAAAAAAEAAAAY29tL1BLAwQKAAAI
+AACtpOlMAAAAAAAAAAAAAAAADAAAAGNvbS94YW1hcmluL1BLAwQKAAAIAACwpOlMAAAAAAAAAAAAAAA
+AEQAAAGNvbS94YW1hcmluL3Rlc3QvUEsDBBQACAgIAJmk6UwAAAAAAAAAAAAAAAAuAAAAY29tL3hhbW
+FyaW4vdGVzdC9EZWZhdWx0SW50ZXJmYWNlTWV0aG9kcy5jbGFzc3WOvU7DMBSFjxsnKeWnXUE8QLrgh
+ScAhBSJnwHE7qQ3JVUSC8dGFc/EwsrAA/BQiOuqLKnw8Pn4+LuWv38+vwCcY5biKMVUIKqMYWbzXEBe
+mgUJTG/qju58W5B91EXDTbIkd6HtX3jj0G+DzPL5E28v3q8FJg/G25Ku6zB1ekWV9o3LO0e20iXdkns
+2i/5spV+1QFaaVq11q23dKUe9U//4ArMwoRrdLdV9saLSJQICI4QVc4ogmTGfThBugFH0zuR/MpNNE7
+x015NDL3C868VDL2XuYbL1joMTjI+BNpYC+/xcaA820uEvUEsHCIw1aijpAAAAhQEAAFBLAwQUAAgIC
+ACYpOlMAAAAAAAAAAAAAAAAHAAAAERlZmF1bHRJbnRlcmZhY2VNZXRob2RzLmphdmF1zLEOwiAQBuCd
+p7hRl0Zd2YyLgw9xwlGJFCocTWPTdxdSHarxxv///utR3bElUKFrRuwwWt8wJZZC9PnqrALrmaJBRXA
+ig9nx+RNciG9BJzEJKKeXtnowIcBmCxNE4hw97CTMP6glPmJcuf1f91y5w7cbgtWQ3rCOBnSZymJhNX
+nkPJYnUsziBVBLBwgzfz2miQAAAPUAAABQSwECFAAUAAgICADVpOlMAAAAAAIAAAAAAAAACQAEAAAAA
+AAAAAAAAAAAAAAATUVUQS1JTkYv/soAAFBLAQIUABQACAgIANWk6UwUYWstRAAAAEUAAAAUAAAAAAAA
+AAAAAAAAAD0AAABNRVRBLUlORi9NQU5JRkVTVC5NRlBLAQIKAAoAAAgAAK2k6UwAAAAAAAAAAAAAAAA
+EAAAAAAAAAAAAAAAAAMMAAABjb20vUEsBAgoACgAACAAAraTpTAAAAAAAAAAAAAAAAAwAAAAAAAAAAA
+AAAAAA5QAAAGNvbS94YW1hcmluL1BLAQIKAAoAAAgAALCk6UwAAAAAAAAAAAAAAAARAAAAAAAAAAAAA
+AAAAA8BAABjb20veGFtYXJpbi90ZXN0L1BLAQIUABQACAgIAJmk6UyMNWoo6QAAAIUBAAAuAAAAAAAA
+AAAAAAAAAD4BAABjb20veGFtYXJpbi90ZXN0L0RlZmF1bHRJbnRlcmZhY2VNZXRob2RzLmNsYXNzUEs
+BAhQAFAAICAgAmKTpTDN/PaaJAAAA9QAAABwAAAAAAAAAAAAAAAAAgwIAAERlZmF1bHRJbnRlcmZhY2
+VNZXRob2RzLmphdmFQSwUGAAAAAAcABwDOAQAAVgMAAAAA
+";
+			proj.Jars.Add (new AndroidItem.EmbeddedJar ("dim.jar") {
+				BinaryContent = () => Convert.FromBase64String (classesJarBase64)
+			});
+
+			proj.AndroidClassParser = classParser;
+
+			proj.SetProperty ("EnableInterfaceMembersPreview", "True");
+			proj.SetProperty ("LangVersion", "preview");
+
+			using (var b = CreateDllBuilder (Path.Combine ("temp", TestName), false, false)) {
+				proj.NuGetRestore (b.ProjectDirectory);
+				Assert.IsTrue (b.Build (proj), "Build should have succeeded.");
+
+				string asmpath = Path.GetFullPath (Path.Combine (Path.GetDirectoryName (new Uri (GetType ().Assembly.CodeBase).LocalPath), b.ProjectDirectory, b.Output.OutputPath, (proj.AssemblyName ?? proj.ProjectName) + ".dll"));
+				Assert.IsTrue (File.Exists (asmpath), "assembly does not exist");
+
+				var cs = b.Output.GetIntermediaryAsText (Path.Combine ("generated", "src", "Com.Xamarin.Test.IDefaultInterfaceMethods.cs"));
+				Assert.IsTrue (cs.Contains ("int Quux ();"), "Quux not generated.");
+				Assert.IsTrue (cs.Contains ("virtual unsafe int Foo ()"), "Foo not generated.");
+				Assert.IsTrue (cs.Contains ("virtual unsafe int Bar {"), "Bar not generated.");
+				Assert.IsTrue (cs.Contains ("set {"), "(Baz) setter not generated.");
+			}
+		}
 	}
 }

tests/CodeGen-Binding/Xamarin.Android.DefaultInterfaceMethods-Lib/Jars/DefaultMethodsInterface.java
@@ -1,0 +1,10 @@
+package com.xamarin.test;
+
+public interface DefaultMethodsInterface
+{
+    default int foo () { return 0; }
+    default int getBar () { return 2; }
+    default void setBar (int value) { }
+    default int toImplement () { throw new UnsupportedOperationException (); }
+    default int invokeFoo () { return foo (); }
+}

tests/CodeGen-Binding/Xamarin.Android.DefaultInterfaceMethods-Lib/Jars/EmptyOverrideClass.java
@@ -1,0 +1,5 @@
+package com.xamarin.test;
+
+public class EmptyOverrideClass implements DefaultMethodsInterface
+{
+}

tests/CodeGen-Binding/Xamarin.Android.DefaultInterfaceMethods-Lib/Jars/ImplementedOverrideClass.java
@@ -1,0 +1,18 @@
+package com.xamarin.test;
+
+public class ImplementedOverrideClass implements DefaultMethodsInterface
+{
+    @Override
+    public int foo () {
+        return 6;
+    }
+
+    @Override
+    public int getBar () {
+        return 100;
+    }
+
+    @Override
+    public void setBar (int value) {
+    }
+}

dotnet/android

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BindingBuildTest.cs
@@ -510,5 +510,63 @@ AAAA==";
 				}
 			}
 		}
+
+		[Test]
+		[TestCaseSource (nameof (ClassParseOptions))]
+		public void BindDefaultInterfaceMethods (string classParser)
+		{
+			var proj = new XamarinAndroidBindingProject {
+				IsRelease = true,
+			};
+
+			// The sources for the .jar is in the jar itself.
+			string classesJarBase64 = @"
+UEsDBBQACAgIANWk6UwAAAAAAAAAAAAAAAAJAAQATUVUQS1JTkYv/soAAAMAUEsHCAAAAAACAAAAAAA
+AAFBLAwQUAAgICADVpOlMAAAAAAAAAAAAAAAAFAAAAE1FVEEtSU5GL01BTklGRVNULk1G803My0xLLS
+7RDUstKs7Mz7NSMNQz4OVyLkpNLElN0XWqBAlY6BnEG5obKmj4FyUm56QqOOcXFeQXJZYA1WvycvFyA
+QBQSwcIFGFrLUQAAABFAAAAUEsDBAoAAAgAAK2k6UwAAAAAAAAAAAAAAAAEAAAAY29tL1BLAwQKAAAI
+AACtpOlMAAAAAAAAAAAAAAAADAAAAGNvbS94YW1hcmluL1BLAwQKAAAIAACwpOlMAAAAAAAAAAAAAAA
+AEQAAAGNvbS94YW1hcmluL3Rlc3QvUEsDBBQACAgIAJmk6UwAAAAAAAAAAAAAAAAuAAAAY29tL3hhbW
+FyaW4vdGVzdC9EZWZhdWx0SW50ZXJmYWNlTWV0aG9kcy5jbGFzc3WOvU7DMBSFjxsnKeWnXUE8QLrgh
+ScAhBSJnwHE7qQ3JVUSC8dGFc/EwsrAA/BQiOuqLKnw8Pn4+LuWv38+vwCcY5biKMVUIKqMYWbzXEBe
+mgUJTG/qju58W5B91EXDTbIkd6HtX3jj0G+DzPL5E28v3q8FJg/G25Ku6zB1ekWV9o3LO0e20iXdkns
+2i/5spV+1QFaaVq11q23dKUe9U//4ArMwoRrdLdV9saLSJQICI4QVc4ogmTGfThBugFH0zuR/MpNNE7
+x015NDL3C868VDL2XuYbL1joMTjI+BNpYC+/xcaA820uEvUEsHCIw1aijpAAAAhQEAAFBLAwQUAAgIC
+ACYpOlMAAAAAAAAAAAAAAAAHAAAAERlZmF1bHRJbnRlcmZhY2VNZXRob2RzLmphdmF1zLEOwiAQBuCd
+p7hRl0Zd2YyLgw9xwlGJFCocTWPTdxdSHarxxv///utR3bElUKFrRuwwWt8wJZZC9PnqrALrmaJBRXA
+ig9nx+RNciG9BJzEJKKeXtnowIcBmCxNE4hw97CTMP6glPmJcuf1f91y5w7cbgtWQ3rCOBnSZymJhNX
+nkPJYnUsziBVBLBwgzfz2miQAAAPUAAABQSwECFAAUAAgICADVpOlMAAAAAAIAAAAAAAAACQAEAAAAA
+AAAAAAAAAAAAAAATUVUQS1JTkYv/soAAFBLAQIUABQACAgIANWk6UwUYWstRAAAAEUAAAAUAAAAAAAA
+AAAAAAAAAD0AAABNRVRBLUlORi9NQU5JRkVTVC5NRlBLAQIKAAoAAAgAAK2k6UwAAAAAAAAAAAAAAAA
+EAAAAAAAAAAAAAAAAAMMAAABjb20vUEsBAgoACgAACAAAraTpTAAAAAAAAAAAAAAAAAwAAAAAAAAAAA
+AAAAAA5QAAAGNvbS94YW1hcmluL1BLAQIKAAoAAAgAALCk6UwAAAAAAAAAAAAAAAARAAAAAAAAAAAAA
+AAAAA8BAABjb20veGFtYXJpbi90ZXN0L1BLAQIUABQACAgIAJmk6UyMNWoo6QAAAIUBAAAuAAAAAAAA
+AAAAAAAAAD4BAABjb20veGFtYXJpbi90ZXN0L0RlZmF1bHRJbnRlcmZhY2VNZXRob2RzLmNsYXNzUEs
+BAhQAFAAICAgAmKTpTDN/PaaJAAAA9QAAABwAAAAAAAAAAAAAAAAAgwIAAERlZmF1bHRJbnRlcmZhY2
+VNZXRob2RzLmphdmFQSwUGAAAAAAcABwDOAQAAVgMAAAAA
+";
+			proj.Jars.Add (new AndroidItem.EmbeddedJar ("dim.jar") {
+				BinaryContent = () => Convert.FromBase64String (classesJarBase64)
+			});
+
+			proj.AndroidClassParser = classParser;
+
+			proj.SetProperty ("_EnableInterfaceMembers", "True");
+			proj.SetProperty ("LangVersion", "preview");
+
+			using (var b = CreateDllBuilder (Path.Combine ("temp", TestName), false, false)) {
+				proj.NuGetRestore (b.ProjectDirectory);
+				Assert.IsTrue (b.Build (proj), "Build should have succeeded.");
+
+				string asmpath = Path.GetFullPath (Path.Combine (Path.GetDirectoryName (new Uri (GetType ().Assembly.CodeBase).LocalPath), b.ProjectDirectory, b.Output.OutputPath, (proj.AssemblyName ?? proj.ProjectName) + ".dll"));
+				Assert.IsTrue (File.Exists (asmpath), "assembly does not exist");
+
+				var cs = b.Output.GetIntermediaryAsText (Path.Combine ("generated", "src", "Com.Xamarin.Test.IDefaultInterfaceMethods.cs"));
+				Assert.IsTrue (cs.Contains ("int Quux ();"), "Quux not generated.");
+				Assert.IsTrue (cs.Contains ("virtual unsafe int Foo ()"), "Foo not generated.");
+				Assert.IsTrue (cs.Contains ("virtual unsafe int Bar {"), "Bar not generated.");
+				Assert.IsTrue (cs.Contains ("set {"), "(Baz) setter not generated.");
+			}
+		}
 	}
 }

tests/CodeGen-Binding/Xamarin.Android.McwGen-Tests/java/com/xamarin/android/DefaultMethodsInterface.java
@@ -1,0 +1,10 @@
+package com.xamarin.android;
+
+public interface DefaultMethodsInterface
+{
+    default int foo () { return 0; }
+    default int getBar () { return 2; }
+    default void setBar (int value) { }
+    default int toImplement () { throw new UnsupportedOperationException (); }
+    default int invokeFoo () { return foo (); }
+}

tests/CodeGen-Binding/Xamarin.Android.McwGen-Tests/java/com/xamarin/android/EmptyOverrideClass.java
@@ -1,0 +1,5 @@
+package com.xamarin.android;
+
+public class EmptyOverrideClass implements DefaultMethodsInterface
+{
+}

tests/CodeGen-Binding/Xamarin.Android.McwGen-Tests/java/com/xamarin/android/ImplementedOverrideClass.java
@@ -1,0 +1,18 @@
+package com.xamarin.android;
+
+public class ImplementedOverrideClass implements DefaultMethodsInterface
+{
+    @Override
+    public int foo () {
+        return 6;
+    }
+
+    @Override
+    public int getBar () {
+        return 100;
+    }
+
+    @Override
+    public void setBar (int value) {
+    }
+}