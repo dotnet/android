ad08ac177e88bdea5775590ad0de9d703561dd64
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-09-19 16:23:18 -0500

    [Xamarin.Android.Build.Tasks] deprecate <GetAdditionalResourcesFromAssemblies/>
    The `<GetAdditionalResourcesFromAssemblies/>` MSBuild task is the
    precursor to Xamarin.Build.Download. It is only used by very old
    versions of the Xamarin support library components.
    
    We want to deprecate its behavior in an effort to remove complexity
    and improve build performance. We have to scan each assembly for these
    assembly-level attributes.
    
    I added a new `XA0121` warning if a library is encountered during a
    build that uses these attributes:
    
    * `IncludeAndroidResourcesFromAttribute`
    * `NativeLibraryReferenceAttribute`
    * `JavaLibraryReferenceAttribute`
    
    I added `[Obsolete]` on these as well.

--- src/Mono.Android/Android/IncludeAndroidResourcesFromAttribute.cs
+++ src/Mono.Android/Android/IncludeAndroidResourcesFromAttribute.cs
@@ -3,6 +3,7 @@ using System;
 namespace Android {
 
 	[AttributeUsage (AttributeTargets.Assembly, AllowMultiple = true)]
+	[Obsolete ("This attribute is deprecated and will be removed in a future release.")]
 	public class IncludeAndroidResourcesFromAttribute : ReferenceFilesAttribute
 	{
 		public IncludeAndroidResourcesFromAttribute (string path)
