738e8715f3e9682ba35cec5046149862e6fd6ba4
Jonathan Pryor <jonpryor@vt.edu>
2019-09-26 06:17:26 -0400

    [xaprepare] Ensure BoringSSL is in TPN (#3689)
    Commit 9302d514 didn't properly convert `external/mono.tpnitems` into
    `*_TPN` classes.  In particular, `mono/boringssl`, which should be
    included when `$(TpnIncludeExternalDependencies)`=True, was instead
    included only when `includeBuildDeps` is True.  Consequently,
    `mono/boringssl` was *not* listed in `ThirdPartyNotices.txt` file
    installed on macOS and Windows machines, even though
    `libmono-btls-shared.so` is installed onto the dev's machine.
    
    Review and verify the ThirdPartyNotice inclusion data, and remove the
    `.tpnitems` files which have been verified.
    
    With this change `bin/Debug/lib/xamarin.android/ThirdPartyNotices.txt`
    has these changes:
    
      * `android/platform/ndk` added, because of `libxamarin-app.so`
        generation and decfbccf.
      * `mono/boringssl` added, because of mono and
        `libmono-btls-shared.so`.
      * `mono/NUnitLite` added
      * `zLibDll/minizip` removed, because of 3634b0f7.
    
    After this change, `ThirdPartyNotices.txt` has these changes:
    
      * `force-net/crc32.net` added, because of decfbccf.

---
+++ build-tools/xaprepare/xaprepare/Scenarios/Scenario_ThirdPartyNotices.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Xamarin.Android.Prepare
+{
+	[Scenario (isDefault: false)]
+	class Scenario_ThirdPartyNotices : Scenario
+	{
+		public Scenario_ThirdPartyNotices ()
+			: base ("ThirdPartyNotices", "Generate the `ThirdPartyNotices.txt` files.", Context.Instance)
+		{}
+
+		protected override void AddSteps (Context context)
+		{
+			if (context == null)
+				throw new ArgumentNullException (nameof (context));
+
+			Steps.Add (new Step_ThirdPartyNotices ());
+		}
+
+		protected override void AddStartSteps (Context context)
+		{
+		}
+
+		protected override void AddEndSteps (Context context)
+		{
+		}
+	}
+}
