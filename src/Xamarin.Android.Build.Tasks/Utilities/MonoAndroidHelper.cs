dotnet/android

commit 88a1d6cd88e68bda208a6eb05d99d802c7800d2e

[Xamarin.Android.Build.Tasks] use CRC64 over SHA1 and MD5 (#3649)
Context: https://github.com/xamarin/xamarin-android/issues/1580

Bump to xamarin/java.interop/master@cd91e48

Changes: https://github.com/xamarin/java.interop/compare/be6048e...cd91e48

Use of MD5 results in an inability to use Xamarin.Android on FIPS-
enabled machines, because when the **System cryptography:
[Use FIPS compliant algorithms][0] for encryption, hashing, and
signing** setting is enabled on Windows:

!['Use Fips compliant algorithms' setting][1]

then the `System.Security.Cryptography.MD5.Create()` method will
throw `InvalidOperationException`.

In order to support FIPS-enabled machines, use a 64-bit
[Cyclic redundancy check][2] algorithm (CRC64) to instead generate
the Java package names, e.g. `crc64aae7d955a89b38db.Name` and
`crc64217c8705f00a5054.Name`.  A simple implementation using a
lookup table will suffice for our purposes, ported to C# from:

	https://github.com/gityf/crc/blob/8045f50ba6e4193d4ee5d2539025fef26e613c9f/crc/crc64.c

The use of CRC64 avoids the use of MD5, thus permitting execution on
FIPS-enabled Windows machines, and also results in *shorter*
directory names -- as the Java package name is a directory name --
which helps reduce Windows `MAX_PATH` issues.

All usage of `MD5.Create()` or `new SHA1Managed()` have been either:

  * Switched to use `new Crc64()`
  * Used `Files.HashString()` if more appropriate
  * `<GetAdditionalResourcesFromAssemblies/>` now emits warning
    `XA0120` if `new SHA1Managed()` fails.

    We will deprecate this code path.

Calls to `jarsigner` now use:

	-sigalg SHA256withRSA -digestalg SHA-256

The previous default can be restored via
`$(AndroidApkSigningAlgorithm)` and `$(AndroidApkDigestAlgorithm)`
MSBuild properties.

`$(AndroidPackageNamingPolicy)` now defaults to `LowercaseCrc64`.
The previous default can be restored by setting
`$(AndroidPackageNamingPolicy)=LowercaseMD5`.

src/Xamarin.Android.Build.Tasks/Utilities/MonoAndroidHelper.cs
@@ -470,6 +470,11 @@ namespace Xamarin.Android.Tasks
 			return Files.HashStream (stream);
 		}
 
+		public static bool HasFileChanged (string source, string destination)
+		{
+			return Files.HasFileChanged (source, destination);
+		}
+
 		/// <summary>
 		/// Open a file given its path and remove the 3 bytes UTF-8 BOM if there is one
 		/// </summary>

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Utilities/BaseTest.cs
@@ -1,19 +1,13 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
+using Xamarin.Android.Tasks;
 using Xamarin.ProjectTools;
 
 namespace Xamarin.Android.Build.Tests
@@ -206,6 +200,7 @@ namespace Xamarin.Android.Build.Tests
 
 		protected static string RunProcess (string exe, string args)
 		{
+			TestContext.Out.WriteLine ($"{nameof(RunProcess)}: {exe} {args}");
 			var info = new ProcessStartInfo (exe, args) {
 				RedirectStandardOutput = true,
 				RedirectStandardError = true,
@@ -379,29 +374,7 @@ namespace Xamarin.Android.Build.Tests
 			return BuildHelper.CreateDllBuilder (directory, cleanupAfterSuccessfulBuild, cleanupOnDispose);
 		}
 
-		protected bool FileCompare (string file1, string file2)
-		{
-			if (!File.Exists (file1) || !File.Exists (file2))
-				return false;
-			using (var stream1 = File.OpenRead (file1)) {
-				using (var stream2 = File.OpenRead (file2)) {
-					return StreamCompare (stream1, stream2);
-				}
-			}
-		}
-
-		protected bool StreamCompare (Stream stream1, Stream stream2)
-		{
-			Assert.IsNotNull (stream1, "stream1 of StreamCompare should not be null");
-			Assert.IsNotNull (stream2, "stream2 of StreamCompare should not be null");
-			byte[] f1 = ReadAllBytesIgnoringLineEndings (stream1);
-			byte[] f2 = ReadAllBytesIgnoringLineEndings (stream2);
-
-			var hash = MD5.Create ();
-			var f1hash = Convert.ToBase64String (hash.ComputeHash (f1));
-			var f2hash = Convert.ToBase64String (hash.ComputeHash (f2));
-			return f1hash.Equals (f2hash);
-		}
+		protected bool FileCompare (string file1, string file2) => !MonoAndroidHelper.HasFileChanged (file1, file2);
 
 		protected byte[] ReadAllBytesIgnoringLineEndings (Stream stream)
 		{

build-tools/xaprepare/xaprepare/ThirdPartyNotices/Java.Interop.cs
@@ -11,6 +11,7 @@ namespace Xamarin.Android.Prepare
 
 		public override List<ThirdPartyNotice> Notices => new List <ThirdPartyNotice> {
 			new JavaInterop_xamarin_Java_Interop_TPN (),
+			new JavaInterop_gityf_crc_TPN (),
 			new JavaInterop_xamarin_mono_cecil_TPN (),
 			new JavaInterop_jonpryor_mono_linq_expressions_TPN (),
 			new JavaInterop_mono_csharp_TPN (),
@@ -29,6 +30,44 @@ namespace Xamarin.Android.Prepare
 		public override string Name        => "xamarin/Java.Interop";
 		public override Uri    SourceUrl   => url;
 		public override string LicenseText => null;
+	}
+
+	class JavaInterop_gityf_crc_TPN : ThirdPartyNotice
+	{
+		static readonly Uri    url         = new Uri ("https://github.com/gityf/crc");
+
+		public override string LicenseFile => null;
+		public override string Name        => "gityf/crc";
+		public override Uri    SourceUrl   => url;
+
+		public override string LicenseText => @"
+Copyright (c) 2012, Salvatore Sanfilippo <antirez at gmail dot com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+  * Neither the name of Redis nor the names of its contributors may be used
+    to endorse or promote products derived from this software without
+    specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ""AS IS""
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+";
 	}
 
 	// git submodules of Java.Interop

src/Mono.Android/Android.Runtime/JNIEnv.cs
@@ -239,6 +239,11 @@ namespace Android.Runtime {
 					Java.Lang.Thread.DefaultUncaughtExceptionHandler = defaultUncaughtExceptionHandler;
 			}
 
+			var packageNamingPolicy = Environment.GetEnvironmentVariable ("__XA_PACKAGE_NAMING_POLICY__");
+			if (Enum.TryParse (packageNamingPolicy, out PackageNamingPolicy pnp)) {
+				JavaNativeTypeManager.PackageNamingPolicy = pnp;
+			}
+
 			if (Logger.LogTiming) {
 				totalElapsed += stopper.ElapsedMilliseconds;
 				Logger.Log (LogLevel.Info, "monodroid-timing", $"JNIEnv.Initialize end: elapsed {totalElapsed} ms");

src/Mono.Android/Mono.Android.csproj
@@ -86,6 +86,9 @@
     <Compile Include="..\..\external\Java.Interop\src\Java.Interop.Tools.TypeNameMappings\Java.Interop.Tools.TypeNameMappings\JavaNativeTypeManager.cs">
       <Link>JavaNativeTypeManager.cs</Link>
     </Compile>
+    <Compile Include="..\..\external\Java.Interop\src\Java.Interop.Tools.JavaCallableWrappers\Java.Interop.Tools.JavaCallableWrappers\Crc64.cs">
+      <Link>Crc64.cs</Link>
+    </Compile>
     <Compile Include="Android.Runtime\DynamicMethodNameCounter.cs" />
     <Compile Include="Android.Runtime\IJavaObjectValueMarshaler.cs" />
   </ItemGroup>

src/Xamarin.Android.Build.Tasks/Tasks/AndroidSignPackage.cs
@@ -53,13 +53,13 @@ namespace Xamarin.Android.Tasks
 		public string TimestampAuthorityCertificateAlias { get; set; }
 
 		/// <summary>
-		/// -sigalg switch, should be md5withRSA for APKs or SHA256withRSA for App Bundles
+		/// -sigalg switch, which is SHA256withRSA by default. Previous versions of XA was md5withRSA.
 		/// </summary>
 		[Required]
 		public string SigningAlgorithm { get; set; }
 
 		/// <summary>
-		/// -digestalg switch, should be SHA1 for APKs or SHA-256 for App Bundles
+		/// -digestalg switch, which is SHA-256 by default. Previous versions of XA was SHA1.
 		/// </summary>
 		[Required]
 		public string DigestAlgorithm { get; set; }

src/Xamarin.Android.Build.Tasks/Tasks/CalculateAdditionalResourceCacheDirectories.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-using System.Security.Cryptography;
-using System.Text.RegularExpressions;
-using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Xamarin.Android.Tools;
 
 namespace Xamarin.Android.Tasks
 {
@@ -28,15 +26,11 @@ namespace Xamarin.Android.Tasks
 			if (!AdditionalAndroidResourcePaths.Any ())
 				return true;
 
-			var md5 = MD5.Create ();
 			Directory.CreateDirectory (CacheDirectory);
 			var directories = new List<ITaskItem> ();
 
 			foreach (var path in AdditionalAndroidResourcePaths) {
-				var cacheSubDirectory = string.Concat (md5.ComputeHash (
-									Encoding.UTF8.GetBytes (path)).Select (b => b.ToString ("X02"))
-								);
-				var targetDir = Path.Combine (CacheDirectory, cacheSubDirectory);
+				var targetDir = Path.Combine (CacheDirectory, Files.HashString (path));
 				directories.Add (new TaskItem (Path.GetFullPath (targetDir).TrimEnd (Path.DirectorySeparatorChar)));
 			}
 			AdditionalResourceCachePaths = directories.ToArray ();

src/Xamarin.Android.Build.Tasks/Tasks/ComputeHash.cs
@@ -1,16 +1,13 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Security.Cryptography;
 using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
+using Xamarin.Android.Tools;
 
-namespace Xamarin.Android.Tasks {
-	public class ComputeHash : AndroidTask {
+namespace Xamarin.Android.Tasks
+{
+	public class ComputeHash : AndroidTask
+	{
 		public override string TaskPrefix => "CPT";
-
-		List<ITaskItem> output = new List<ITaskItem> ();
 
 		[Required]
 		public ITaskItem [] Source { get; set; }
@@ -18,34 +15,22 @@ namespace Xamarin.Android.Tasks {
 		public bool CopyMetaData { get; set; } = true;
 
 		[Output]
-		public ITaskItem [] Output { get => output.ToArray (); }
+		public ITaskItem [] Output { get; set; }
 
 		public override bool RunTask ()
 		{
-			using (var sha1 = SHA1.Create ()) {
-
-				foreach (var item in Source) {
-					var newItem = new TaskItem(item.ItemSpec, new Dictionary<string, string>() {
-						{ "Hash", HashItemSpec (sha1, item.ItemSpec) }
-					});
-					if (CopyMetaData)
-						item.CopyMetadataTo (newItem);
-					output.Add (newItem);
-				}
-				Log.LogDebugTaskItems ("Output : ", Output);
-				return !Log.HasLoggedErrors;
+			var output = new List<ITaskItem> (Source.Length);
+			foreach (var item in Source) {
+				var newItem = new TaskItem(item.ItemSpec, new Dictionary<string, string>() {
+					{ "Hash", Files.HashString (item.ItemSpec) }
+				});
+				if (CopyMetaData)
+					item.CopyMetadataTo (newItem);
+				output.Add (newItem);
 			}
-		}
-
-		string HashItemSpec (SHA1 sha1, string hashInput)
-		{
-			var hash = sha1.ComputeHash (Encoding.UTF8.GetBytes (hashInput));
-			var hashResult = new StringBuilder (hash.Length * 2);
-
-			foreach (byte b in hash) {
-				hashResult.Append (b.ToString ("x2"));
-			}
-			return hashResult.ToString ();
+			Output = output.ToArray ();
+			Log.LogDebugTaskItems ("Output : ", Output);
+			return !Log.HasLoggedErrors;
 		}
 	}
 }

commit d76da93d5ceb498750dc19a345f5d0d3df14d00e

[tests] Replace FileCompare with AssertFileContentsMatch (#3708)
Context: https://devdiv.visualstudio.com/DevDiv/_build/results?buildId=3096255&view=ms.vss-test-web.build-test-results-tab&runId=8756400&resultId=100484&paneView=debug

Commit 88a1d6cd introduced multiple unit test regressions in e.g.
`ManagedResourceParserTests.GenerateDesignerFileFromRtxt()`:

	…\GenerateDesignerFileFromRtxtTrue Some Space\Resource.designer.cs and …\bin\TestRelease\Expected\GenerateDesignerFileWithLibraryReferenceExpected.cs do not match.
	Expected: True
	 But was: False 

The cause of the breakage is that commit 88a1d6cd removed the use of
`BaseTest.ReadAllBytesIgnoringLineEndings()` for file comparisons.
On Windows, the generated `Resource.designer.cs` file contains
Windows line endings (CRLF), while the in-repro copy has Unix (LF)
line endings.  This causes the file comparisons to fail.

The test failures weren't seen on PR #3649 because the test failures
occur on the **MSBuild - Windows** test job added in f91cd2d3, which
hadn't yet been merged when PR #3649 was being written.

In exploring these test failures, a usability problem arose: we knew
that the file contents differed, because of the assert message, but
we didn't know *how* the file contents differed, nor *could* we
obtain the files until 936459c0, because they weren't uploaded.

Add a `BaseTest.AssertFileContentsMatch()` method, which:

 1. Uses `ReadAllBytesIgnoringLineEndings()`, which fixes the
    regressions introduced in 88a1d6cd.

 2. Adds the files as Azure test attachments, so that we can easily
    see their contents from the unit test failure screen.

src/Xamarin.Android.Build.Tasks/Utilities/MonoAndroidHelper.cs
@@ -470,6 +470,11 @@ namespace Xamarin.Android.Tasks
 			return Files.HashStream (stream);
 		}
 
+		public static string HashBytes (byte[] bytes)
+		{
+			return Files.HashBytes (bytes);
+		}
+
 		public static bool HasFileChanged (string source, string destination)
 		{
 			return Files.HasFileChanged (source, destination);

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/ManagedResourceParserTests.cs
@@ -348,8 +348,7 @@ int xml myxml 0x7f140000
 			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
 			Assert.IsTrue (File.Exists (task.NetResgenOutputFile), $"{task.NetResgenOutputFile} should have been created.");
 			var expected = Path.Combine (Root, "Expected", "GenerateDesignerFileExpected.cs");
-			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expected), 
-			 	$"{task.NetResgenOutputFile} and {expected} do not match.");
+			AssertFileContentsMatch (task.NetResgenOutputFile, expected);
 			Directory.Delete (Path.Combine (Root, path), recursive: true);
 		}
 
@@ -389,8 +388,7 @@ int xml myxml 0x7f140000
 			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
 			Assert.IsTrue (File.Exists (task.NetResgenOutputFile), $"{task.NetResgenOutputFile} should have been created.");
 			var expected = Path.Combine (Root, "Expected", withLibraryReference ? "GenerateDesignerFileWithLibraryReferenceExpected.cs" : "GenerateDesignerFileExpected.cs");
-			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expected),
-				 $"{task.NetResgenOutputFile} and {expected} do not match.");
+			AssertFileContentsMatch (task.NetResgenOutputFile, expected);
 			Directory.Delete (Path.Combine (Root, path), recursive: true);
 		}
 
@@ -426,8 +424,7 @@ int xml myxml 0x7f140000
 			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
 			Assert.IsTrue (File.Exists (task.NetResgenOutputFile), $"{task.NetResgenOutputFile} should have been created.");
 			var expected = Path.Combine (Root, "Expected", "GenerateDesignerFileExpected.cs");
-			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expected),
-				 $"{task.NetResgenOutputFile} and {expected} do not match.");
+			AssertFileContentsMatch (task.NetResgenOutputFile, expected);
 			// Update the id, and force the managed parser to re-parse the output
 			File.WriteAllText (Path.Combine (Root, path, "res", "layout", "main.xml"), Main.Replace ("@+id/textview.withperiod", "@+id/textview.withperiod2"));
 			File.SetLastWriteTimeUtc (task.ResourceFlagFile, DateTime.UtcNow);
@@ -436,8 +433,7 @@ int xml myxml 0x7f140000
 			var data = File.ReadAllText (expected);
 			var expectedWithNewId = Path.Combine (Root, path, "GenerateDesignerFileExpectedWithNewId.cs");
 			File.WriteAllText (expectedWithNewId, data.Replace ("withperiod", "withperiod2"));
-			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expectedWithNewId),
-				 $"{task.NetResgenOutputFile} and {expectedWithNewId} do not match.");
+			AssertFileContentsMatch (task.NetResgenOutputFile, expectedWithNewId);
 			Directory.Delete (Path.Combine (Root, path), recursive: true);
 		}
 
@@ -520,8 +516,7 @@ int xml myxml 0x7f140000
 			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
 			string aapt2Designer = Path.Combine (Root, path, "Resource.designer.aapt2.cs");
 			string managedDesigner = Path.Combine (Root, path, "Resource.designer.managed.cs");
-			Assert.IsTrue (FileCompare (managedDesigner, aapt2Designer),
-				 $"{managedDesigner} and {aapt2Designer} do not match.");
+			AssertFileContentsMatch (managedDesigner, aapt2Designer);
 			Directory.Delete (Path.Combine (Root, path), recursive: true);
 
 		}
@@ -594,9 +589,7 @@ int xml myxml 0x7f140000
 			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
 
 			string managedDesignerRtxt = Path.Combine (Root, path, "Resource.designer.managedrtxt.cs");
-
-			Assert.IsTrue (FileCompare (managedDesignerRtxt, aaptDesigner),
-				 $"{managedDesignerRtxt} and {aaptDesigner} do not match.");
+			AssertFileContentsMatch (managedDesignerRtxt, aaptDesigner);
 
 			File.WriteAllText (task.ResourceFlagFile, string.Empty);
 			File.Delete (Path.Combine (Root, path, "R.txt.bak"));
@@ -717,8 +710,7 @@ int styleable ElevenAttributes_attr10 10";
 			Assert.IsTrue (task.Execute (), "Task should have executed successfully.");
 			Assert.IsTrue (File.Exists (task.NetResgenOutputFile), $"{task.NetResgenOutputFile} should have been created.");
 			var expected = Path.Combine (Root, "Expected", "GenerateDesignerFileWithElevenStyleableAttributesExpected.cs");
-			Assert.IsTrue (FileCompare (task.NetResgenOutputFile, expected),
-				 $"{task.NetResgenOutputFile} and {expected} do not match.");
+			AssertFileContentsMatch (task.NetResgenOutputFile, expected);
 			Directory.Delete (Path.Combine (Root, path), recursive: true);
 		}
 	}

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Utilities/BaseTest.cs
@@ -374,9 +374,35 @@ namespace Xamarin.Android.Build.Tests
 			return BuildHelper.CreateDllBuilder (directory, cleanupAfterSuccessfulBuild, cleanupOnDispose);
 		}
 
-		protected bool FileCompare (string file1, string file2) => !MonoAndroidHelper.HasFileChanged (file1, file2);
+		protected void AssertFileContentsMatch (string file1, string file2)
+		{
+			if (!FileCompare (file1, file2)) {
+				TestContext.AddTestAttachment (file1, Path.GetFileName (file1));
+				TestContext.AddTestAttachment (file2, Path.GetFileName (file2));
+				Assert.Fail ($"{file1} and {file2} do not match.");
+			}
+		}
 
-		protected byte[] ReadAllBytesIgnoringLineEndings (Stream stream)
+		protected bool FileCompare (string file1, string file2)
+		{
+			FileAssert.Exists (file1);
+			FileAssert.Exists (file2);
+			using (var stream1 = File.OpenRead (file1))
+			using (var stream2 = File.OpenRead (file2)) {
+				return StreamCompare (stream1, stream2);
+			}
+		}
+
+		protected bool StreamCompare (Stream stream1, Stream stream2)
+		{
+			Assert.IsNotNull (stream1, "stream1 of StreamCompare should not be null");
+			Assert.IsNotNull (stream2, "stream2 of StreamCompare should not be null");
+			string hash1 = MonoAndroidHelper.HashBytes (ReadAllBytesIgnoringLineEndings (stream1));
+			string hash2 = MonoAndroidHelper.HashBytes (ReadAllBytesIgnoringLineEndings (stream2));
+			return hash1 == hash2;
+		}
+
+		protected byte [] ReadAllBytesIgnoringLineEndings (Stream stream)
 		{
 			using (var memoryStream = new MemoryStream ()) {
 				int readByte;