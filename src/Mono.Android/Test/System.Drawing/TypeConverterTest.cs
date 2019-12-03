41e90b2303404cdf0c30fd380458ab0c7dc61fc0
Alexander Köplinger <alex.koeplinger@outlook.com>
2019-12-03 21:33:18 +0100

    Add simple Drawing TypeConverter tests
    Regression test for https://github.com/xamarin/xamarin-android/issues/3726

---
+++ src/Mono.Android/Test/System.Drawing/TypeConverterTest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.ComponentModel;
+using System.Drawing;
+
+using NUnit.Framework;
+
+namespace System.Drawing {
+
+	[TestFixture]
+	public class TypeConverterTest {
+
+		[Test]
+		public void ColorConverter ()
+		{
+			var typeConverter = TypeDescriptor.GetConverter (typeof (Color));
+			var color = (Color)typeConverter.ConvertFromString ("#FFAAEE");
+
+			Assert.AreEqual (0xFF, color.A, "A");
+			Assert.AreEqual (0xFF, color.R, "R");
+			Assert.AreEqual (0xAA, color.G, "G");
+			Assert.AreEqual (0xEE, color.B, "B");
+		}
+
+		[Test]
+		public void RectangleConverter ()
+		{
+			var typeConverter = TypeDescriptor.GetConverter (typeof (Rectangle));
+			var rect = (Rectangle)typeConverter.ConvertFromString ("10, 20, 30, 40");
+
+			Assert.AreEqual (10, rect.X, "X");
+			Assert.AreEqual (20, rect.Y, "Y");
+			Assert.AreEqual (30, rect.Width, "Width");
+			Assert.AreEqual (40, rect.Height, "Height");
+		}
+
+		[Test]
+		public void PointConverter ()
+		{
+			var typeConverter = TypeDescriptor.GetConverter (typeof (Point));
+			var point = (Point)typeConverter.ConvertFromString ("10, 20");
+
+			Assert.AreEqual (10, point.X, "X");
+			Assert.AreEqual (20, point.Y, "Y");
+		}
+
+		[Test]
+		public void SizeConverter ()
+		{
+			var typeConverter = TypeDescriptor.GetConverter (typeof (Size));
+			var size = (Size)typeConverter.ConvertFromString ("10, 20");
+
+			Assert.AreEqual (10, size.Width, "Width");
+			Assert.AreEqual (20, size.Height, "Height");
+		}
+
+		[Test]
+		public void SizeFConverter ()
+		{
+			var typeConverter = TypeDescriptor.GetConverter (typeof (SizeF));
+			var sizeF = (SizeF)typeConverter.ConvertFromString ("10.5, 20.5");
+
+			Assert.AreEqual (10.5, sizeF.Width, "Width");
+			Assert.AreEqual (20.5, sizeF.Height, "Height");
+		}
+	}
+}
