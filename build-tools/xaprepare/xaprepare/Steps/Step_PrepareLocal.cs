95d3750d4cd8621dd0af12779d69085cc8de08f2
Peter Collins <peter.collins@xamarin.com>
2019-12-04 17:57:08 -0500

    Restore prep and bootstrap task NuGets

--- build-tools/xaprepare/xaprepare/Steps/Step_PrepareLocal.cs
+++ build-tools/xaprepare/xaprepare/Steps/Step_PrepareLocal.cs
@@ -14,14 +14,34 @@ namespace Xamarin.Android.Prepare
 		{
 			var msbuild = new MSBuildRunner (context);
 
+			string prepTasksPath = Path.Combine (BuildPaths.XamarinAndroidSourceRoot, "build-tools", "xa-prep-tasks", "xa-prep-tasks.csproj");
+			string bootstrapTasksPath = Path.Combine (BuildPaths.XamarinAndroidSourceRoot, "build-tools", "Xamarin.Android.Tools.BootstrapTasks", "Xamarin.Android.Tools.BootstrapTasks.csproj");
 			string xfTestPath = Path.Combine (BuildPaths.XamarinAndroidSourceRoot, "tests", "Xamarin.Forms-Performance-Integration", "Xamarin.Forms.Performance.Integration.csproj");
+
+			if (!await RestorePackageRefProjects (msbuild, prepTasksPath, "prep-tasks-restore")) {
+				return false;
+			}
+
+			if (!await RestorePackageRefProjects (msbuild, bootstrapTasksPath, "bootstrap-tasks-restore")) {
+				return false;
+			}
+
+			if (!await RestorePackageRefProjects (msbuild, xfTestPath, "xfperf-restore")) {
+				return false;
+			}
+
+			return true;
+		}
+
+		async Task<bool> RestorePackageRefProjects (MSBuildRunner msbuild, string projectPath, string tag)
+		{
 			return await msbuild.Run (
-				projectPath: xfTestPath,
-				logTag: "xfperf",
-				arguments: new List <string> {
+				projectPath: projectPath,
+				logTag: tag,
+				arguments: new List<string> {
 					"/t:Restore"
 				},
-				binlogName: "prepare-restore"
+				binlogName: tag
 			);
 		}
 	}
