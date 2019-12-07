2c7cbd4e8bbe8df05c95b4b3e6e9481727291962
Bruno Garcia <bruno@brunogarcia.com>
2019-12-07 17:38:43 -0500

    Remove libzip from build tools
    #3375 removed libzip in favor of a nuget dependency

--- build-tools/xaprepare/xaprepare/ConfigAndData/Dependencies/MacOS.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Dependencies/MacOS.cs
@@ -14,11 +14,6 @@ namespace Xamarin.Android.Prepare
 				MinimumVersion = "2.20.0",
 			},
 
-			new HomebrewProgram ("libzip", new Uri ("https://raw.githubusercontent.com/Homebrew/homebrew-core/3d9a3eb3a62ed9586cd8f6b3bf506845159f39a4/Formula/libzip.rb")) {
-				MinimumVersion = "1.5.2",
-				Pin = true,
-			},
-
 			new HomebrewProgram ("make"),
 
 			new HomebrewProgram ("mingw-w64", new Uri ("https://raw.githubusercontent.com/Homebrew/homebrew-core/a6542037a48a55061a4c319e6bb174b3715f7cbe/Formula/mingw-w64.rb")) {
