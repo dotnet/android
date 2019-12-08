0e22e7e73a6902d5a7f5bfb7971b3f4a9ea5453e
Dean Ellis <dellis1972@googlemail.com>
2019-12-08 16:14:03 +0000

    Add an actual build as well

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -4209,9 +4209,11 @@ namespace UnnamedProject
 				Environment.SetEnvironmentVariable ("ANDROID_SDK_PATH", sdkPath);
 				var proj = new XamarinAndroidApplicationProject ();
 				using (var b = CreateApkBuilder ()) {
+					string defaultTarget = b.Target;
 					b.Target = "InstallAndroidDependencies";
-
-					Assert.IsTrue (b.Build (proj, parameters: new string [] { "AcceptAndroidSDKLicenses=true" }), "build should have succeeded.");
+					Assert.IsTrue (b.Build (proj, parameters: new string [] { "AcceptAndroidSDKLicenses=true" }), "InstallAndroidDependencies should have succeeded.");
+					b.Target = defaultTarget;
+					Assert.IsTrue (b.Build (proj), "build should have succeeded.");
 				}
 			} finally {
 				Environment.SetEnvironmentVariable ("ANDROID_SDK_PATH", old);
