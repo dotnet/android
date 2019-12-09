a963b977f4c6f9c0fe0613b651bbd5204d9203c1
Dean Ellis <dellis1972@googlemail.com>
2019-12-09 14:54:55 +0000

    [Xamarin.Android.Build.Tasks] fix installing the SDK if one doesn't exist (#4003)
    Fixes #3996
    
    Commit 008ba98 did not go far enough. Both `ValidateJavaVersion` and `ResolveSdks` also needed the same `ContinueOnError` setting. While the commit did allow users to install components which were missing, it did NOT allow users to install the entire SDK. This is because even when calling's the `InstallAndroidDependencies` target the build would still fail with 
    
    ```
    error XA5300: The Android SDK Directory could not be found. Please set via /p:AndroidSdkDirectory.
    ```
    
    What we need to do in this case was include the `ContinueOnError` property on `ValidateJavaVersion` and `ResolveSdks`. This allows the build to continue and call the targets to install the required SDK. This is very handy for CI environments as it means we can install the Android SDK from scratch on a clean machine.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -4186,5 +4186,27 @@ namespace UnnamedProject
 				}
 			}
 		}
+
+		[Test]
+		public void InstallAndroidDependenciesTest ()
+		{
+			if (!CommercialBuildAvailable)
+				Assert.Ignore ("Not required on Open Source Builds");
+			var old = Environment.GetEnvironmentVariable ("ANDROID_SDK_PATH");
+			try {
+				string sdkPath = Path.Combine (Root, "temp", TestName, "android-sdk");
+				Environment.SetEnvironmentVariable ("ANDROID_SDK_PATH", sdkPath);
+				var proj = new XamarinAndroidApplicationProject ();
+				using (var b = CreateApkBuilder ()) {
+					string defaultTarget = b.Target;
+					b.Target = "InstallAndroidDependencies";
+					Assert.IsTrue (b.Build (proj, parameters: new string [] { "AcceptAndroidSDKLicenses=true" }), "InstallAndroidDependencies should have succeeded.");
+					b.Target = defaultTarget;
+					Assert.IsTrue (b.Build (proj), "build should have succeeded.");
+				}
+			} finally {
+				Environment.SetEnvironmentVariable ("ANDROID_SDK_PATH", old);
+			}
+		}
 	}
 }
