40946ef979bcf4e088f6811985be9a90c6c9ed2f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-12-11 13:58:10 -0600

    [Xamarin.Android.Build.Tasks] add feature flag for Google Manifest Merger
    Context: https://github.com/xamarin/xamarin-android/pull/3918
    
    In 2c6f5cd1, we added support for the official Google/Android Manifest
    Merger. After further QA testing, it seems that a reasonable number of
    existing apps require code changes to *appease* the new manifest
    merger.
    
    This is a similar situation we were in compared to `aapt2`, where the
    suggested changes are good (and usually problems). I think we should
    add a feature flag, so we can gradually turn this on. We could add the
    feature off by default for one release, and then turn it on by default
    for the next.
    
    This PR adds:
    
    * `$(AndroidManifestMerger)` with enum-style values of `legacy` and
      `manifestmerger.jar`.
    * Updated a few tests to leverage both code paths.

--- src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaStubs.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaStubs.cs
@@ -43,6 +43,7 @@ namespace Xamarin.Android.Tasks
 		public string [] SupportedAbis { get; set; }
 
 		public string ManifestTemplate { get; set; }
+		public string[] MergedManifestDocuments { get; set; }
 
 		public bool Debug { get; set; }
 		public bool MultiDex { get; set; }
@@ -249,7 +250,7 @@ namespace Xamarin.Android.Tasks
 			manifest.NeedsInternet = NeedsInternet;
 			manifest.InstantRunEnabled = InstantRunEnabled;
 
-			var additionalProviders = manifest.Merge (all_java_types, ApplicationJavaClass, EmbedAssemblies, BundledWearApplicationName);
+			var additionalProviders = manifest.Merge (all_java_types, ApplicationJavaClass, EmbedAssemblies, BundledWearApplicationName, MergedManifestDocuments);
 
 			using (var stream = new MemoryStream ()) {
 				manifest.Save (stream);
