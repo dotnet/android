217c0ee51a56e8d74d492d03a5f64f358769a76a
Dean Ellis <dellis1972@googlemail.com>
2020-01-20 14:25:55 +0000

    Move the LibZipSharp version number to a central location

--- build-tools/xaprepare/xaprepare/ThirdPartyNotices/LibZipSharp.cs
+++ build-tools/xaprepare/xaprepare/ThirdPartyNotices/LibZipSharp.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Xamarin.Android.Prepare
 {
@@ -8,9 +10,12 @@ namespace Xamarin.Android.Prepare
 	class LibZipSharp_grendello_LibZipSharp_TPN : ThirdPartyNotice
 	{
 		static readonly Uri url = new Uri ("https://github.com/xamarin/LibZipSharp/");
-		internal static readonly string LibZipSharpVersion = "1.0.7";
+		internal static readonly string LibZipSharpVersion => {
+			var doc = XDocumment.Load (Path.Combine (BuildPaths.XamarinAndroidSourceRoot, "Configuration.props"));
+			return doc.Descendants(XName.Get("LibZipSharpVersion", @"http://schemas.microsoft.com/developer/msbuild/2003")).First().Value;
+		};
 		static readonly string licenseFile = Path.Combine (BuildPaths.XamarinAndroidSourceRoot,
-			"packages", "xamarin.libzipsharp", LibZipSharpVersion,
+			"packages", "xamarin.libzipsharp", LibZipSharpVersion (),
 			"Licences", "LICENSE");
 
 		public override string LicenseFile => licenseFile;
