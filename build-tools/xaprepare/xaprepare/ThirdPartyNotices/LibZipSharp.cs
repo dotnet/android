3ae6f256c902fb65a103586045abce2ed09c7f30
Dean Ellis <dellis1972@googlemail.com>
2020-01-21 12:12:23 +0000

    [Build] Bump Xamarin.LibZipSharp to 1.0.7 (#4147)
    * [Build] Bump Xamarin.LibZipSharp to 1.0.7
    
    Fixes #4100
    
    The version of libzip.so we shipped for linux in
    1.0.6 had a dependency on libcrypto.so.1.1. Some
    linux based systems only ship with libcrypto.so.1.0.
    As a result Xamarin.Android would not build and we give
    the following error.
    
    	error MSB4018: System.DllNotFoundException: lib64/libzip.so assembly
    
    Verison 1.0.7 if Xamarin.LibZipSharp fixes this issue by
    statically linking libcrypto (and other dependencies) into the
    dynamic library. This should allow Xamarin.Android to build and
    run on those systems which does not include libcrypto or have
    an older version.
    
    * Fix Third Party Notices
    
    * Move the LibZipSharp version number to a central location
    
    * Fix Typo
    
    * Fix typo's

--- build-tools/xaprepare/xaprepare/ThirdPartyNotices/LibZipSharp.cs
+++ build-tools/xaprepare/xaprepare/ThirdPartyNotices/LibZipSharp.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Xamarin.Android.Prepare
 {
 	[TPN]
-	class LibZipSharp_grendello_LibZipSharp_TPN : ThirdPartyNotice
+	class LibZipSharp_xamarin_LibZipSharp_TPN : ThirdPartyNotice
 	{
 		static readonly Uri url = new Uri ("https://github.com/xamarin/LibZipSharp/");
-		internal static readonly string LibZipSharpVersion = "1.0.6";
+		internal static string LibZipSharpVersion ()
+		{
+			var doc = XDocument.Load (Path.Combine (BuildPaths.XamarinAndroidSourceRoot, "Configuration.props"));
+			return doc.Descendants(XName.Get("LibZipSharpVersion", @"http://schemas.microsoft.com/developer/msbuild/2003")).First().Value;
+		}
 		static readonly string licenseFile = Path.Combine (BuildPaths.XamarinAndroidSourceRoot,
-			"packages", "xamarin.libzipsharp", LibZipSharpVersion,
+			"packages", "xamarin.libzipsharp", LibZipSharpVersion (),
 			"Licences", "LICENSE");
 
 		public override string LicenseFile => licenseFile;
