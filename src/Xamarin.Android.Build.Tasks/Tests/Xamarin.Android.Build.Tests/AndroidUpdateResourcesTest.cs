656085a9f7c7b80f339600b6f24465727569c449
Dean Ellis <dellis1972@googlemail.com>
2020-01-27 16:08:59 +0000

    [Xamarin.Android.Build.Task] use global:: for GeneratedCodeAttribute (#4170)
    Fixes http://work.azdo.io/1057222
    
    We had the global:: namespace prefix on ALL namespaces
    in the Resource.Designer.cs file EXCEPT GeneratedCodeAttribute :facepalm.
    So users can get errors like
    
    	The type or namespace name 'CodeDom' does not exist in the namespace Foo.Bar.System
    
    If they use `System` in their defualt namespace for the project.
    This commit adds it, so users who use `System` in their
    assembly namespaces will not be hit by this in the future.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/AndroidUpdateResourcesTest.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/AndroidUpdateResourcesTest.cs
@@ -41,6 +41,29 @@ namespace Xamarin.Android.Build.Tests
 		}
 
 		[Test]
+		public void BuildAppWithSystemNamespace ()
+		{
+			var path = Path.Combine (Root, "temp", TestName);
+			var library = new XamarinAndroidLibraryProject () {
+				ProjectName = "Library1.System",
+			};
+			var proj = new XamarinAndroidApplicationProject () {
+				References = {
+					new BuildItem.ProjectReference (Path.Combine("..", library.ProjectName, Path.GetFileName (library.ProjectFilePath)), "Library1.System") {
+					},
+				},
+			};
+			using (var builder = CreateDllBuilder (Path.Combine (path, library.ProjectName), cleanupAfterSuccessfulBuild: false, cleanupOnDispose: false)) {
+				builder.ThrowOnBuildFailure = false;
+				Assert.IsTrue (builder.Build (library), "Library should have built.");
+				using (var b = CreateApkBuilder (Path.Combine (path, proj.ProjectName), cleanupAfterSuccessfulBuild: false, cleanupOnDispose: false)) {
+					b.ThrowOnBuildFailure = false;
+					Assert.IsTrue (b.Build (proj), "Project should have built.");
+				}
+			}
+		}
+
+		[Test]
 		public void RepetitiveBuild ()
 		{
 			if (Directory.Exists ("temp/RepetitiveBuild"))
