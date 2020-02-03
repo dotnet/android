30cc27698e1aa79354ea66aca74edc9921cad06c
Gustavo Varo <gugavaro@users.noreply.github.com>
2020-02-03 16:04:20 -0500

    [check-boot-times] Add check-boot-times (#4118)
    `check-boot-times` is a tool to be used by the Hyper-V team to be
    able to compare performance improvement progress.
    
    Example of a run:
    
    	Testing emulator startup times for 1 execution(s). This may take several minutes.
    	Acceleration type: 0, WHPX (10.0.19041) is installed and usable.
    	Android emulator version 29.3.5.0 (build_id 6120838) (CL:N/A)
    	XamarinPerfTest Average Hot Boot Time for 1 run(s) out of 1 request(s): 4224 ms
    	XamarinPerfTestPixelWithSkin Average Cold Boot Time for 1 run(s) out of 1 request(s): 159600 ms
    	XamarinPerfTestPixelWithSkin Average Hot Boot Time for 1 run(s) out of 1 request(s): 2695 ms
    
    	Testing emulator startup times for 1 execution(s). This may take several minutes.
    	Acceleration type: 0, HAXM version 7.5.4 (4) is installed and usable.
    	Android emulator version 29.3.5.0 (build_id 6120838) (CL:N/A)
    	XamarinPerfTest Average Cold Boot Time for 1 run(s) out of 1 request(s): 23834 ms
    	XamarinPerfTest Average Hot Boot Time for 1 run(s) out of 1 request(s): 4021 ms
    	XamarinPerfTestPixelWithSkin Average Cold Boot Time for 1 run(s) out of 1 request(s): 41912 ms
    	XamarinPerfTestPixelWithSkin Average Hot Boot Time for 1 run(s) out of 1 request(s): 4584 ms
    
    It also helps us track percentage of times emulator failed to boot.

---
+++ build-tools/check-boot-times/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("check-boot-times")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Microsoft")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("Microsoft.")]
+[assembly: AssemblyTrademark ("Microsoft")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.0.0")]
