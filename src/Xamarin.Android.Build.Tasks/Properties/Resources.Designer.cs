1673f6529ded2db1624c84586c12a983eacf3683
Brendan Zagaeski <brzaga@microsoft.com>
2020-02-09 11:34:04 -0800

    [Xamarin.Android.Build.Tasks] Make XA5101 errors localizable (#4228)
    Context: 0342fe5698b86e21e36c924732ff135b9a87e4af
    Context: https://dev.azure.com/devdiv/DevDiv/_workitems/edit/1009374/
    
    Give the XA5101 errors in `NdkUtil` their own new error codes so they
    can be distinguished from the `NdkUtilOld` errors in telemetry.
    
    Move the message strings for XA5101 and the other new error codes to
    the `.resx` file so that they are localizable.

--- src/Xamarin.Android.Build.Tasks/Properties/Resources.Designer.cs
+++ src/Xamarin.Android.Build.Tasks/Properties/Resources.Designer.cs
@@ -304,6 +304,51 @@ namespace Xamarin.Android.Tasks.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Missing Android NDK toolchains directory &apos;{0}&apos;. Please install the Android NDK..
+        /// </summary>
+        internal static string XA5101 {
+            get {
+                return ResourceManager.GetString("XA5101", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to C compiler for target {0} was not found. Tried paths: &quot;{1}&quot;.
+        /// </summary>
+        internal static string XA5101_C_Compiler {
+            get {
+                return ResourceManager.GetString("XA5101_C_Compiler", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toolchain directory for target {0} was not found..
+        /// </summary>
+        internal static string XA5101_Toolchain {
+            get {
+                return ResourceManager.GetString("XA5101_Toolchain", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Could not locate the Android NDK. Please make sure the Android NDK is installed in the Android SDK Manager, or if using a custom NDK path, please ensure the $(AndroidNdkDirectory) MSBuild property is set to the custom path..
+        /// </summary>
+        internal static string XA5102 {
+            get {
+                return ResourceManager.GetString("XA5102", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toolchain utility &apos;{0}&apos; for target {1} was not found. Tried in path: &quot;{2}&quot;.
+        /// </summary>
+        internal static string XA5103 {
+            get {
+                return ResourceManager.GetString("XA5103", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Failed to generate Java type for class: {0} due to MAX_PATH: {1}.
         /// </summary>
         internal static string XA5301 {
