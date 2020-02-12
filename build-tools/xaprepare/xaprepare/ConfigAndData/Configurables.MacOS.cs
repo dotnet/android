7d98f5cd6fb284cfeac0456dadb2cde22b414300
Fan Yang <yangfan@microsoft.com>
2020-02-12 15:01:35 -0500

    Fix mono version

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
@@ -7,7 +7,7 @@ namespace Xamarin.Android.Prepare
 		partial class Urls
 		{
 			public static readonly Uri Corretto = new Uri ($"{Corretto_BaseUri}{CorrettoUrlPathVersion}/amazon-corretto-{CorrettoDistVersion}-macosx-x64.tar.gz");
-			public static readonly Uri MonoPackage = new Uri ("https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2020-02/9/879f6699bb5e8bd684bcd0f0acc55401ad64991c/MonoFramework-MDK-6.12.0.7.macos10.xamarin.universal.pkg");
+			public static readonly Uri MonoPackage = new Uri ("https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2020-02/9/879f6699bb5e8bd684bcd0f0acc55401ad64991c/MonoFramework-MDK-6.12.0.8.macos10.xamarin.universal.pkg");
 		}
 
 		partial class Defaults
