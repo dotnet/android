7182ac116a7658cb6020d9dffe1d374c2c79c991
Fan Yang <yangfan@microsoft.com>
2020-02-14 10:53:47 -0500

    Bump mono

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
@@ -7,7 +7,7 @@ namespace Xamarin.Android.Prepare
 		partial class Urls
 		{
 			public static readonly Uri Corretto = new Uri ($"{Corretto_BaseUri}{CorrettoUrlPathVersion}/amazon-corretto-{CorrettoDistVersion}-macosx-x64.tar.gz");
-			public static readonly Uri MonoPackage = new Uri ("https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2020-02/11/778878e3364d50984987e49b02b63ab3b8a7d87e/MonoFramework-MDK-6.12.0.10.macos10.xamarin.universal.pkg");
+			public static readonly Uri MonoPackage = new Uri ("https://xamjenkinsartifact.azureedge.net/build-package-osx-mono/2020-02/12/f460694bacf73923809456071491ed6b9bf0a9f1/MonoFramework-MDK-6.12.0.11.macos10.xamarin.universal.pkg");
 		}
 
 		partial class Defaults
