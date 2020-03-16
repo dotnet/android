c443b387ab271efb9de2808fa4b910409fa89509
Marek Habersack <grendel@twistedcode.net>
2020-03-16 08:30:14 +0000

    [build] Preserve CONFIGURATION for subsequent make invocations (#4401)
    * [build] Preserve CONFIGURATION for subsequent make invocations
    
    When the tree is configured with `make CONFIGURATION=Release`, every
    subsequent `make` invocation will require `CONFIGURATION=Release` to be
    present on the command line or otherwise the build tree will be messed
    up and will require a re-run of `make prepare`.
    Prevent the above from happening by storing the configuration in a
    `bin/configuration.mk` file which is included by the `Makefile` as the
    very first thing.
    
    * Fix indentation
    
    Co-authored-by: Jonathan Pryor <jonpryor@vt.edu>

--- build-tools/xaprepare/xaprepare/Steps/Step_GenerateFiles.Unix.cs
+++ build-tools/xaprepare/xaprepare/Steps/Step_GenerateFiles.Unix.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -5,9 +6,28 @@ namespace Xamarin.Android.Prepare
 {
 	partial class Step_GenerateFiles
 	{
+		sealed class GeneratedConfigurationFile : GeneratedFile
+		{
+			public GeneratedConfigurationFile (string outputPath) :
+				base(outputPath)
+			{}
+
+			public override void Generate (Context context)
+			{
+				if (context == null)
+					throw new ArgumentNullException (nameof (context));
+
+				using (StreamWriter sw = Utilities.OpenStreamWriter (OutputPath)) {
+					sw.WriteLine ($"CONFIGURATION = {context.Configuration}");
+					sw.Flush ();
+				}
+			}
+		}
+
 		partial void AddUnixPostBuildSteps (Context context, List<GeneratedFile> steps)
 		{
 			steps.Add (new GeneratedMakeRulesFile (Path.Combine (Configurables.Paths.BuildBinDir, "rules.mk")));
+			steps.Add (new GeneratedConfigurationFile (Path.Combine (Configurables.Paths.BinDirRoot, "configuration.mk")));
 		}
 	}
 }
