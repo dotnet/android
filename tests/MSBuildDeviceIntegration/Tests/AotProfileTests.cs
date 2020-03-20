318f3b126e5412122644289ce590ba504a039f2e
Dean Ellis <dellis1972@googlemail.com>
2020-03-20 15:50:46 +0000

    [MSBuildDeviceIntegration] Fix AotProfileTest
    The app for the AotProfileTest was crashing on
    startup with
    
    	Could not create log profiler server socket: Operation not permitted
    
    This was because the `INTERNET` permission was not being
    included in the manifst. This PR adds that permission.
    
    Why this was working in the first place is a question that
    is still unanswered.

--- tests/MSBuildDeviceIntegration/Tests/AotProfileTests.cs
+++ tests/MSBuildDeviceIntegration/Tests/AotProfileTests.cs
@@ -8,6 +8,14 @@ namespace Xamarin.Android.Build.Tests
 	public class AotProfileTests : DeviceTest
 	{
 
+		readonly string PermissionManifest = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<manifest xmlns:android=""http://schemas.android.com/apk/res/android"" android:versionCode=""1"" android:versionName=""1.0"" package=""{0}"">
+	<uses-sdk />
+	<application android:label=""{0}"">
+	</application>
+	<uses-permission android:name=""android.permission.INTERNET"" />
+</manifest>";
+
 		[Test]
 		public void BuildBasicApplicationAndAotProfileIt ()
 		{
@@ -18,6 +26,7 @@ namespace Xamarin.Android.Build.Tests
 			proj.SetProperty (KnownProperties.AndroidSupportedAbis, "armeabi-v7a;x86");
 			var port = 9000 + new Random ().Next (1000);
 			proj.SetProperty ("AndroidAotProfilerPort", port.ToString ());
+			proj.AndroidManifest = string.Format (PermissionManifest, proj.PackageName);
 			var projDirectory = Path.Combine ("temp", TestName);
 			using (var b = CreateApkBuilder (projDirectory)) {
 				Assert.IsTrue (b.RunTarget (proj, "BuildAndStartAotProfiling"), "Run of BuildAndStartAotProfiling should have succeeded.");
