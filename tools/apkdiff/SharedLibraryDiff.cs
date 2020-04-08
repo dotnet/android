3c957ae45b75216062d99ee54e05cb4072538b91
Radek Doulik <radekdoulik@gmail.com>
2020-04-08 17:19:59 +0200

    Merge apkdiff from upstream
    Besides the other changes we want the regression testing.
    
    Example output:
    
        apkdiff.exe --test-apk-size-regression=51200 --test-assembly-size-regression=51200 Xamarin.Forms_Performance_Integration-Signed-Release.apkdesc bin\TestDebug\Xamarin.Forms_Performance_Integration-Signed.apk
        Size difference in bytes ([*1] apk1 only, [*2] apk2 only):
          +  26,749,952 assemblies/Mono.Android.dll
        Error: apkdiff: Assembly size differs more than 51,200 bytes.
          +  14,655,424 assemblies/Mono.Android.pdb *2
          +   2,423,388 lib/x86/libmonosgen-2.0.so
          +   2,413,568 assemblies/mscorlib.dll
        Error: apkdiff: Assembly size differs more than 51,200 bytes.
          +   2,025,076 classes.dex
          +   1,755,580 lib/armeabi-v7a/libmonosgen-2.0.so
          +   1,683,644 assemblies/mscorlib.pdb *2
          +   1,511,544 assemblies/Xamarin.Android.Support.v7.AppCompat.dll
        Error: apkdiff: Assembly size differs more than 51,200 bytes.
        ...
        Summary:
          +  43,001,792 Assemblies 303.25% (of 14,180,480)
          +  10,653,224 Shared libraries 89.86% (of 11,855,444)
          +  66,973,060 Package size difference 318.80% (of 21,007,581)
        Error: apkdiff: PackageSize differ more than 51,200 bytes. apk1 size: 21,007,581 bytes, apk2 size: 87,980,641 bytes.
        Error: apkdiff: Size regression occured, 39 test(s) failed.
    
    Changes:
    
        dc6cfc Update the handling of an unknown option
        3b825e Handle unknown option
        897547 Support aab packages
        812215 Add new regression options
        fbac82 Use thousand separator in regression prints
        e5aef1 Count regression failures and report the count
        11ba62 Added assembly size regression testing
        e7ee8e Added apk size regression testing implementation
        b95995 Set platform target
        a74d02 Fix white space
        3afd5a Print also percentage in package size difference
        fd8d3e Show assemblies and shared libraries total differences in summary
        130598 Add section sizes example
        b1ee9a Update LibZipSharp to newer version
        d84b35 Compare shared libraries section sizes
        3a3ceb Added another example output
        4e094f Add initial assembly comparison
        7efe90 Compare shared libraries symbols and their sizes
        fcac80 Improve csproj file
        3a479d Update solution/project files to load in VS/Mac without issues

---
+++ tools/apkdiff/SharedLibraryDiff.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace apkdiff {
+	public class SharedLibraryDiff : EntryDiff {
+		public SharedLibraryDiff ()
+		{
+		}
+
+		public override string Name { get { return "Shared libraries"; } }
+
+		string RunCommand (string cmd, string arguments)
+		{
+			String output;
+
+			using (var p = new System.Diagnostics.Process ()) {
+
+				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.RedirectStandardError = true;
+				p.StartInfo.RedirectStandardOutput = true;
+				p.StartInfo.FileName = cmd;
+				p.StartInfo.Arguments = arguments;
+
+				try {
+					p.Start ();
+				} catch {
+					Program.Warning ($"Unable to run '{p.StartInfo.FileName}' command");
+
+					return null;
+				}
+
+				output = p.StandardOutput.ReadToEnd ();
+				var error = p.StandardError.ReadToEnd ();
+
+				if (error.Length > 0)
+					Program.Error ($"nm error output:\n{error}");
+
+				p.WaitForExit ();
+			}
+
+			return output;
+		}
+
+		string HomeDir {
+			get {
+				return Environment.GetEnvironmentVariable ("HOME");
+			}
+		}
+
+		string AndroidToolsDir {
+			get {
+				return HomeDir + "/android-toolchain/toolchains/x86-clang/bin";
+			}
+		}
+
+		string RunNMCmd (string file)
+		{
+			return RunCommand ($"{AndroidToolsDir}/x86_64-linux-android-nm", $"-S --size-sort -D {file}");
+		}
+
+		string RunSizeCmd (string file)
+		{
+			return RunCommand ($"{AndroidToolsDir}/x86_64-linux-android-size", $"-A {file}");
+		}
+
+		struct SymbolInfo : ISizeProvider {
+			public long Size { get; set; }
+		}
+
+		Dictionary<string, SymbolInfo> ParseNMOutput (string output)
+		{
+			var symbols = new Dictionary<string, SymbolInfo> ();
+
+			foreach (var line in output.Split (new char [] { '\n' })) {
+				var cols = line.Split (new char [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+				if (cols.Length != 4)
+					continue;
+
+				symbols [cols [3]] = new SymbolInfo () { Size = int.Parse (cols [1], System.Globalization.NumberStyles.HexNumber) };
+			}
+
+			return symbols;
+		}
+
+		struct SectionInfo : ISizeProvider {
+			public long Size { get; set; }
+		}
+
+		Dictionary<string, SectionInfo> ParseSizeOutput (string output)
+		{
+			var sections = new Dictionary<string, SectionInfo> ();
+
+			int skipLines = 2;
+
+			foreach (var line in output.Split (new char [] { '\n' })) {
+				if (skipLines > 0) {
+					skipLines--;
+					continue;
+				}
+
+				var cols = line.Split (new char [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+				if (cols.Length != 3)
+					continue;
+
+				sections [cols [0]] = new SectionInfo () { Size = int.Parse (cols [1]) };
+			}
+
+			return sections;
+		}
+
+		void CompareSections (string file, string other, string padding)
+		{
+			var scs1 = ParseSizeOutput (RunSizeCmd (file));
+			var scs2 = ParseSizeOutput (RunSizeCmd (other));
+
+			Program.ColorWriteLine ($"{padding}                Section size difference", ConsoleColor.Yellow);
+
+			var differences = new Dictionary<string, long> ();
+			var singles = new HashSet<string> ();
+
+			foreach (var entry in scs1) {
+				var key = entry.Key;
+
+				if (scs2.ContainsKey (key)) {
+					var otherEntry = scs2 [key];
+					differences [key] = otherEntry.Size - scs1 [key].Size;
+				} else {
+					differences [key] = -scs1 [key].Size;
+					singles.Add (key);
+				}
+			}
+
+			foreach (var key in scs2.Keys) {
+				if (scs1.ContainsKey (key))
+					continue;
+
+				differences [key] = scs2 [key].Size;
+				singles.Add (key);
+			}
+
+			foreach (var diff in differences.OrderByDescending (v => v.Value)) {
+				if (diff.Value == 0)
+					continue;
+
+				var single = singles.Contains (diff.Key);
+
+				ApkDescription.PrintDifference (diff.Key, diff.Value, single ? $" *{(diff.Value > 0 ? 2 : 1)}" : null, padding);
+			}
+		}
+
+		void CompareSymbols (string file, string other, string padding)
+		{
+			var sym1 = ParseNMOutput (RunNMCmd (file));
+			var sym2 = ParseNMOutput (RunNMCmd (other));
+
+			Program.ColorWriteLine ($"{padding}                Symbol size difference", ConsoleColor.Yellow);
+
+			var differences = new Dictionary<string, long> ();
+			var singles = new HashSet<string> ();
+
+			foreach (var entry in sym1) {
+				var key = entry.Key;
+
+				if (sym2.ContainsKey (key)) {
+					var otherEntry = sym2 [key];
+					differences [key] = otherEntry.Size - sym1 [key].Size;
+				} else {
+					differences [key] = -sym1 [key].Size;
+					singles.Add (key);
+				}
+			}
+
+			foreach (var key in sym2.Keys) {
+				if (sym1.ContainsKey (key))
+					continue;
+
+				differences [key] = sym2 [key].Size;
+				singles.Add (key);
+			}
+
+			foreach (var diff in differences.OrderByDescending (v => v.Value)) {
+				if (diff.Value == 0)
+					continue;
+
+				var single = singles.Contains (diff.Key);
+
+				ApkDescription.PrintDifference (diff.Key, diff.Value, single ? $" *{(diff.Value > 0 ? 2 : 1)}" : null, padding);
+			}
+		}
+
+		public override void Compare (string file, string other, string padding)
+		{
+			CompareSections (file, other, padding);
+			CompareSymbols (file, other, padding);
+		}
+	}
+}
