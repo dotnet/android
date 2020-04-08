53c18020bbbb4c94aaa97edb3ec609ee9ddfd1d1
Jonathan Pobst <jonathan.pobst@microsoft.com>
2020-04-08 14:30:48 -0500

    More fixes.

--- src/Mono.Android/Android.Text/ITextWatcher.cs
+++ src/Mono.Android/Android.Text/ITextWatcher.cs
@@ -6,20 +6,20 @@ namespace Android.Text {
 
 	public class AfterTextChangedEventArgs : EventArgs {
 
-		public AfterTextChangedEventArgs (IEditable editable)
+		public AfterTextChangedEventArgs (IEditable? editable)
 		{
 			this.editable = editable;
 		}
 
-		IEditable editable;
-		public IEditable Editable {
+		IEditable? editable;
+		public IEditable? Editable {
 			get { return editable; }
 		}
 	}
 
 	public class TextChangedEventArgs : EventArgs {
 
-		public TextChangedEventArgs (IEnumerable<char> text, int start, int before, int after)
+		public TextChangedEventArgs (IEnumerable<char>? text, int start, int before, int after)
 		{
 			this.text = text;
 			this.start = start;
@@ -27,8 +27,8 @@ namespace Android.Text {
 			this.after = after;
 		}
 
-		IEnumerable<char> text;
-		public IEnumerable<char> Text {
+		IEnumerable<char>? text;
+		public IEnumerable<char>? Text {
 			get { return text; }
 		}
 
@@ -70,21 +70,21 @@ namespace Android.Text {
 			TextChanged = changed_handler;
 		}
 
-		void ITextWatcher.AfterTextChanged (Android.Text.IEditable s)
+		void ITextWatcher.AfterTextChanged (Android.Text.IEditable? s)
 		{
 			var h = AfterTextChanged;
 			if (h != null)
 				h (inst, new AfterTextChangedEventArgs (s));
 		}
 
-		void ITextWatcher.BeforeTextChanged (Java.Lang.ICharSequence s, int start, int before, int after)
+		void ITextWatcher.BeforeTextChanged (Java.Lang.ICharSequence? s, int start, int before, int after)
 		{
 			var h = BeforeTextChanged;
 			if (h != null)
 				h (inst, new TextChangedEventArgs (s, start, before, after));
 		}
 
-		void ITextWatcher.OnTextChanged (Java.Lang.ICharSequence s, int start, int before, int count)
+		void ITextWatcher.OnTextChanged (Java.Lang.ICharSequence? s, int start, int before, int count)
 		{
 			var h = TextChanged;
 			if (h != null)
