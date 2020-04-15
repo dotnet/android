305ff973fb106a11241b45c98996359ed1aba4bd
Brendan Zagaeski <brzaga@microsoft.com>
2020-04-15 12:25:02 -0700

    Simplify test

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/CheckDuplicateJavaLibrariesTests.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/CheckDuplicateJavaLibrariesTests.cs
@@ -1,11 +1,9 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using Xamarin.Android.Tasks;
-using Xamarin.ProjectTools;
 
 namespace Xamarin.Android.Build.Tests {
 
@@ -36,25 +34,11 @@ namespace Xamarin.Android.Build.Tests {
 		{
 			Directory.CreateDirectory (path);
 			string jar1 = Path.Combine (path, "library.jar");
-			using (Stream resourceStream = typeof (XamarinAndroidCommonProject).Assembly.GetManifestResourceStream ("Xamarin.ProjectTools.Resources.Base.classes.jar"))
-			using (FileStream fileStream = File.Create (jar1))
-				resourceStream.CopyTo (fileStream);
+			File.WriteAllText (jar1, "jar1");
 
-			Directory.CreateDirectory (Path.Combine (path, "Hello (World)"));
-			string jar2 = Path.Combine (path, "Hello (World)", "library.jar");
-			File.WriteAllBytes (jar2, Convert.FromBase64String (@"
-UEsDBBQACAgIAMl8lUsAAAAAAAAAAAAAAAAJAAQATUVUQS1JTkYv/soAAAMAUEsHCAAAAAACAAAAA
-AAAAFBLAwQUAAgICADJfJVLAAAAAAAAAAAAAAAAFAAAAE1FVEEtSU5GL01BTklGRVNULk1G803My0
-xLLS7RDUstKs7Mz7NSMNQz4OVyLkpNLElN0XWqBAlY6BnEG5oaKWj4FyUm56QqOOcXFeQXJZYA1Wv
-ycvFyAQBQSwcIbrokAkQAAABFAAAAUEsDBBQACAgIAIJ8lUsAAAAAAAAAAAAAAAASAAAAc2FtcGxl
-L0hlbGxvLmNsYXNzO/Vv1z4GBgYTBkEuBhYGXg4GPnYGfnYGAUYGNpvMvMwSO0YGZg3NMEYGFuf8l
-FRGBn6fzLxUv9LcpNSikMSkHKAIa3l+UU4KI4OIhqZPVmJZon5OYl66fnBJUWZeujUjA1dwfmlRcq
-pbJkgtl0dqTk6+HkgZDwMrAxvQFrCIIiMDT3FibkFOqj6Yz8gggDDKPykrNbmEQZGBGehCEGBiYAR
-pBpLsQJ4skGYE0qxa2xkYNwIZjAwcQJINIggkORm4oEqloUqZhZg2oClkB5LcYLN5AFBLBwjQMrpO
-0wAAABMBAABQSwECFAAUAAgICADJfJVLAAAAAAIAAAAAAAAACQAEAAAAAAAAAAAAAAAAAAAATUVUQ
-S1JTkYv/soAAFBLAQIUABQACAgIAMl8lUtuuiQCRAAAAEUAAAAUAAAAAAAAAAAAAAAAAD0AAABNRV
-RBLUlORi9NQU5JRkVTVC5NRlBLAQIUABQACAgIAIJ8lUvQMrpO0wAAABMBAAASAAAAAAAAAAAAAAA
-AAMMAAABzYW1wbGUvSGVsbG8uY2xhc3NQSwUGAAAAAAMAAwC9AAAA1gEAAAAA"));
+			Directory.CreateDirectory (Path.Combine (path, "jar2"));
+			string jar2 = Path.Combine (path, "jar2", "library.jar");
+			File.WriteAllText (jar2, "jar2");
 
 			var task = new CheckDuplicateJavaLibraries {
 				BuildEngine = engine,
