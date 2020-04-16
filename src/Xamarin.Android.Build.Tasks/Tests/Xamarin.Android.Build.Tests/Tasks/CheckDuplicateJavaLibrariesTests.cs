30b391b7ecfa55a9f77fdd2cd29fd8182e833605
Brendan Zagaeski <brzaga@microsoft.com>
2020-04-16 16:56:48 -0700

    [Xamarin.Android.Build.Tasks] CheckDuplicateJavaLibraries l10n support (#4558)
    Context: 0342fe5698b86e21e36c924732ff135b9a87e4af
    Context: https://dev.azure.com/devdiv/DevDiv/_workitems/edit/1009374/
    
    Add a code to the existing error in `<CheckDuplicateJavaLibraries/>`,
    and move the message string into the `.resx` file so that it is
    localizable.
    
    Other changes:
    
    Remove "you" from the message.

---
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/CheckDuplicateJavaLibrariesTests.cs
@@ -0,0 +1,54 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Android.Tasks;
+
+namespace Xamarin.Android.Build.Tests {
+
+	[TestFixture]
+	[Category ("Node-2")]
+	[Parallelizable (ParallelScope.Self)]
+	public class CheckDuplicateJavaLibrariesTests : BaseTest {
+		List<BuildErrorEventArgs> errors;
+		List<BuildWarningEventArgs> warnings;
+		List<BuildMessageEventArgs> messages;
+		MockBuildEngine engine;
+		string path;
+
+		[SetUp]
+		public void Setup ()
+		{
+			engine = new MockBuildEngine (TestContext.Out,
+				errors: errors = new List<BuildErrorEventArgs> (),
+				warnings: warnings = new List<BuildWarningEventArgs> (),
+				messages: messages = new List<BuildMessageEventArgs> ());
+
+			path = Path.Combine (Root, "temp", TestName);
+			TestOutputDirectories [TestContext.CurrentContext.Test.ID] = path;
+		}
+
+		[Test]
+		public void XA1014ConflictingJarNames ()
+		{
+			Directory.CreateDirectory (path);
+			string jar1 = Path.Combine (path, "library.jar");
+			File.WriteAllText (jar1, "jar1");
+
+			Directory.CreateDirectory (Path.Combine (path, "jar2"));
+			string jar2 = Path.Combine (path, "jar2", "library.jar");
+			File.WriteAllText (jar2, "jar2");
+
+			var task = new CheckDuplicateJavaLibraries {
+				BuildEngine = engine,
+				JavaSourceFiles = new ITaskItem [] { new TaskItem (jar1), new TaskItem (jar2) }
+			};
+
+			Assert.IsFalse (task.Execute (), "Task should fail!");
+			BuildErrorEventArgs error = errors [0];
+			Assert.AreEqual ("XA1014", error.Code);
+			StringAssert.Contains ("library.jar", error.Message);
+		}
+	}
+}
