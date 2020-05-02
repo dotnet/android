dotnet/android

commit 5016dc40ade0a74c56d956e04ec63a8059cf78bb

[monodroid] Return first entry in binary_search
Fixes: https://github.com/xamarin/xamarin-android/issues/4596

Context: https://github.com/xamarin/monodroid/commit/192b1f1b71c98650d04bf0a4e52ee337a054ff4c
Context: https://github.com/xamarin/java.interop/blob/fc18c54b2ccf14f444fadac0a1e7e81f837cc470/src/Java.Interop.Tools.JavaCallableWrappers/Java.Interop.Tools.JavaCallableWrappers/TypeNameMapGenerator.cs#L149-L186

In some environments, `BundleTest.TestBundleIntegerArrayList2()` fails
when  using the commercial shared runtime:

	Test 'Xamarin.Android.RuntimeTests.BundleTest.TestBundleIntegerArrayList2' failed: System.MemberAccessException : Cannot create an instance of Android.Runtime.JavaList`1[T] because Type.ContainsGenericParameters is true.
	  at System.Reflection.RuntimeConstructorInfo.DoInvoke (System.Object obj, System.Reflection.BindingFlags invokeAttr, System.Reflection.Binder binder, System.Object[] parameters, System.Globalization.CultureInfo culture)
	  at System.Reflection.RuntimeConstructorInfo.Invoke (System.Reflection.BindingFlags invokeAttr, System.Reflection.Binder binder, System.Object[] parameters, System.Globalization.CultureInfo culture)
	  at System.Reflection.ConstructorInfo.Invoke (System.Object[] parameters)
	  at Java.Interop.TypeManager.CreateProxy (System.Type type, System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
	  at Java.Interop.TypeManager.CreateInstance (System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer, System.Type targetType)
	  at Java.Lang.Object.GetObject (System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer, System.Type type)
	  at Java.Lang.Object._GetObject[T] (System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
	  at Java.Lang.Object.GetObject[T] (System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
	  at Android.OS.Bundle.Get (System.String key)
	  at Xamarin.Android.RuntimeTests.BundleTest.TestBundleIntegerArrayList2 ()

src/monodroid/jni/embedded-assemblies.cc
@@ -149,8 +149,8 @@ EmbeddedAssemblies::binary_search (const Key *key, const Entry *base, size_t nme
 	}
 
 	constexpr size_t size = sizeof(Entry);
+	const Entry *ret = nullptr;
 	while (nmemb > 0) {
-		const Entry *ret;
 		if constexpr (use_extra_size) {
 			ret = reinterpret_cast<const Entry*>(reinterpret_cast<const uint8_t*>(base) + ((size + extra_size) * (nmemb / 2)));
 		} else {
@@ -168,11 +168,29 @@ EmbeddedAssemblies::binary_search (const Key *key, const Entry *base, size_t nme
 			}
 			nmemb -= nmemb / 2 + 1;
 		} else {
-			return ret;
+			break;
 		}
 	}
 
-	return nullptr;
+	if (ret == nullptr) {
+		return ret;
+	}
+
+	// `base` may contain duplicate keys.  Return the *first* entry for a given key.
+	while (ret > base) {
+		const Entry *prev_ret;
+		if constexpr (use_extra_size) {
+			prev_ret = reinterpret_cast<const Entry*>(reinterpret_cast<const uint8_t*>(ret) - (size + extra_size));
+		} else {
+			prev_ret = reinterpret_cast<const Entry*>(reinterpret_cast<const uint8_t*>(ret) - size);
+		}
+		int result = compare (key, prev_ret);
+		if (result != 0) {
+			break;
+		}
+		ret = prev_ret;
+	}
+	return ret;
 }
 
 #if defined (DEBUG) || !defined (ANDROID)

src/Mono.Android/Test/Android.OS/BundleTest.cs
@@ -34,6 +34,7 @@ namespace Xamarin.Android.RuntimeTests {
 			b.PutIntegerArrayList ("key", new List<Java.Lang.Integer> () { Java.Lang.Integer.ValueOf (1) });
 			var obj = b.Get ("key");
 			Assert.NotNull (obj, "Missing 'key' in bundle");
+			Assert.IsTrue (obj is global::Android.Runtime.JavaList, "`obj` should be a JavaList!");
 			try {
 				var list = b.GetIntegerArrayList ("key");
 				Assert.NotNull (list, "'key' doesn't refer to a list of integers");

commit 7e9362a57efaf1628f92b6fab12caba59d5f6724

Fix binary_search
Commit af13997b asked, "is binary_search broken?"

Answer: Yes, because 5016dc40 broke it:

@@ -149,8 +149,8 @@ EmbeddedAssemblies::binary_search (const Key *key, const Entry *base, size_t nme
 	}

 	constexpr size_t size = sizeof(Entry);
+	const Entry *ret = nullptr;
 	while (nmemb > 0) {
-		const Entry *ret;
 		if constexpr (use_extra_size) {
 			ret = reinterpret_cast<const Entry*>(reinterpret_cast<const uint8_t*>(base) + ((size + extra_size) * (nmemb / 2)));
 		} else {
@@ -168,11 +168,29 @@ EmbeddedAssemblies::binary_search (const Key *key, const Entry *base, size_t nme
 			}
 			nmemb -= nmemb / 2 + 1;
 		} else {
-			return ret;
+			break;
 		}
 	}

The bug is that in moving `ret` up a scope, nothing *clears out* `ret`
on error!  Thus, it's "garbage" when the `while(nmemb>0)` loop
terminates and no key has been found, which is why it *always*
returned a non-null value.

(Rocket science!  And bad code refactoring.  And PEBKAC.)

The fixis to `ret=nullptr` on every loop iteration unless we have a
value value.

src/monodroid/jni/embedded-assemblies.cc
@@ -170,14 +170,12 @@ EmbeddedAssemblies::binary_search (const Key *key, const Entry *base, size_t nme
 		} else {
 			break;
 		}
+		ret = nullptr;
 	}
 
 	if (ret == nullptr) {
 		return ret;
 	}
-
-	const Entry *orig_ret = ret;
-	log_info (LOG_DEFAULT, "# jonp: binary_search: found ret=%p; trying previous entries!", orig_ret);
 
 	// `base` may contain duplicate keys.  Return the *first* entry for a given key.
 	while (ret > base) {
@@ -193,7 +191,6 @@ EmbeddedAssemblies::binary_search (const Key *key, const Entry *base, size_t nme
 		}
 		ret = prev_ret;
 	}
-	log_info (LOG_DEFAULT, "# jonp: binary_search: new ret=%p; distance=%ul", ret, ((unsigned long) (orig_ret - ret))/sizeof(Entry));
 	return ret;
 }
 
@@ -245,15 +242,15 @@ EmbeddedAssemblies::typemap_java_to_managed (const char *java_type_name)
 		TypeMap *module;
 		for (size_t i = 0; i < type_map_count; i++) {
 			module = &type_maps[i];
-			entry = linear_search<const char, TypeMapEntry, compare_type_name, false> (java_type_name, module->java_to_managed, module->entry_count);
+			entry = binary_search<const char, TypeMapEntry, compare_type_name, false> (java_type_name, module->java_to_managed, module->entry_count);
 			if (entry != nullptr) {
-				entry2 = binary_search<const char, TypeMapEntry, compare_type_name, false>(java_type_name, module->java_to_managed, module->entry_count);
+				entry2 = linear_search<const char, TypeMapEntry, compare_type_name, false>(java_type_name, module->java_to_managed, module->entry_count);
 				break;
 			}
 		}
 	} else {
-		entry = linear_search<const char, TypeMapEntry, compare_type_name, false> (java_type_name, type_map.java_to_managed, type_map.entry_count);
-		entry2 = binary_search<const char, TypeMapEntry, compare_type_name, false>(java_type_name, type_map.java_to_managed, type_map.entry_count);
+		entry = binary_search<const char, TypeMapEntry, compare_type_name, false> (java_type_name, type_map.java_to_managed, type_map.entry_count);
+		entry2 = linear_search<const char, TypeMapEntry, compare_type_name, false>(java_type_name, type_map.java_to_managed, type_map.entry_count);
 	}
 
 	if (entry != entry2) {
@@ -395,15 +392,15 @@ EmbeddedAssemblies::typemap_managed_to_java (const char *managed_type_name)
 		TypeMap *module;
 		for (size_t i = 0; i < type_map_count; i++) {
 			module = &type_maps[i];
-			entry = linear_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, module->managed_to_java, module->entry_count);
+			entry = binary_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, module->managed_to_java, module->entry_count);
 			if (entry != nullptr) {
-				entry2 = binary_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, module->managed_to_java, module->entry_count);
+				entry2 = linear_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, module->managed_to_java, module->entry_count);
 				break;
 			}
 		}
 	} else {
-		entry = linear_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, type_map.managed_to_java, type_map.entry_count);
-		entry2 = binary_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, type_map.managed_to_java, type_map.entry_count);
+		entry = binary_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, type_map.managed_to_java, type_map.entry_count);
+		entry2 = linear_search<const char, TypeMapEntry, compare_type_name, false> (managed_type_name, type_map.managed_to_java, type_map.entry_count);
 	}
 
 	if (entry != entry2) {