9ee06d16d57d11b9ef29e68c0b5bd0d9aa624487
Radek Doulik <radekdoulik@gmail.com>
2020-05-06 17:15:04 +0200

    Run ILLink with our custom SetupStep
    The step so far only installs `FixAbstractMethodsStep`.
    
    Parts of the code come from xamarin-macios repo. We might share these
    and move them to linker. Or enable more linker API to avoid getting
    the pipeline steps with reflection.

---
+++ src/Xamarin.Android.Linker/SetupStep.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Java.Interop.Tools.Cecil;
+using Mono.Linker;
+using Mono.Linker.Steps;
+using MonoDroid.Tuner;
+
+namespace Xamarin.Android.Linker
+{
+	class SetupStep : BaseStep
+	{
+		List<IStep> _steps;
+
+		List<IStep> Steps {
+			get {
+				if (_steps == null) {
+					var pipeline = typeof (LinkContext).GetProperty ("Pipeline").GetGetMethod ().Invoke (Context, null);
+					_steps = (List<IStep>) pipeline.GetType ().GetField ("_steps", BindingFlags.Instance | BindingFlags.NonPublic).GetValue (pipeline);
+					//foreach (var step in _steps)
+					//	Console.WriteLine ($"step: {step.GetType ().Name}");
+				}
+				return _steps;
+			}
+		}
+
+		protected override void Process ()
+		{
+			var cache = new TypeDefinitionCache ();
+			InsertAfter (new FixAbstractMethodsStep (cache), "RemoveUnreachableBlocksStep");
+		}
+
+		void InsertAfter (IStep step, string stepName)
+		{
+			for (int i = 0; i < Steps.Count;) {
+				if (Steps [i++].GetType ().Name == stepName) {
+					Steps.Insert (i, step);
+					return;
+				}
+			}
+
+			throw new InvalidOperationException ($"Could not insert {step} after {stepName}.");
+		}
+	}
+}
