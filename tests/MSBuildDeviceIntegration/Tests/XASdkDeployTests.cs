dotnet/android

commit fb637e082b07872bd8196eeb6f099c8536441620

[.NET 5] support for binding projects (#4646)
The current idea for binding projects in .NET 5 is they won't be a
distinct project type at all anymore.  It would be completely valid
to put this in a .NET 5 Xamarin.Android app with `$(OutputType)` of
`Exe`:

	<InputJar Include="foo.jar" />

A project with an `$(OutputType)` of `Library` would likely do:

	<EmbeddedJar Include="foo.jar" />

To make this work, I've split up `Xamarin.Android.Bindings.targets`
into a few different files:

  * `Xamarin.Android.Bindings.Core.targets`: the targets shared
    between .NET 5 and "legacy" binding projects. It imports the
    other new files.

  * `Xamarin.Android.Bindings.ClassParse.targets`: is only imported
    when `$(AndroidClassParser)` is `class-parse`.

  * `Xamarin.Android.Bindings.JarToXml.targets`: is only imported
    when `$(AndroidClassParser)` is `jar2xml`.

  * `Xamarin.Android.Bindings.Documentation.targets`: is imported by
    "legacy" binding projects, contains targets related to
    documentation extraction/generation.  We will eventually use this
    in .NET 5 when `mdoc.exe` is available or we have an alternative.

Once these were split up, to get bindings working in .NET 5 projects
I had to:

 1. Import `Xamarin.Android.Bindings.Core.targets`.
 2. Update `Microsoft.Android.Sdk.BuildOrder.targets` to include the
    binding-specific targets.
 3. Make the binding targets only run if there are any `@(InputJar)`
    or `@(EmbeddedJar)`.

One hangup is that bindings require:

	<AllowUnsafeBlocks>true</AllowUnsafeBlocks>

We probably don't want this set for all projects by default.  I set
this if any of the binding targets run, so that once you add a
`@(InputJar)` it gets turned on.

I updated `XASdkTests` to verify binding `.jar` files works in a
library project and an app project.

Other changes:

  * Added a new `AndroidDotnetToolTask` for swapping between

src/Xamarin.Android.Build.Tasks/MSBuild/Xamarin/Android/Xamarin.Android.Bindings.Core.targets
@@ -1,0 +1,154 @@
+<!--
+***********************************************************************************************
+Xamarin.Android.Bindings.Core.targets
+
+This file contains the core MSBuild logic for Xamarin.Android bindings.
+
+It is shared between "legacy" binding projects and .NET 5 projects.
+
+***********************************************************************************************
+-->
+
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+
+  <UsingTask TaskName="Xamarin.Android.Tasks.BindingsGenerator" AssemblyFile="Xamarin.Android.Build.Tasks.dll" />
+  <UsingTask TaskName="Xamarin.Android.Tasks.RemoveDirFixed"    AssemblyFile="Xamarin.Android.Build.Tasks.dll" />
+
+  <PropertyGroup>
+    <UseShortGeneratorFileNames Condition=" '$(UseShortGeneratorFileNames)' == '' ">false</UseShortGeneratorFileNames>
+    <GeneratedOutputPath        Condition=" '$(GeneratedOutputPath)' == '' ">$(IntermediateOutputPath)generated\</GeneratedOutputPath>
+    <ApiOutputFile              Condition=" '$(ApiOutputFile)' == '' ">$(IntermediateOutputPath)api.xml</ApiOutputFile>
+    <_GeneratorStampFile>$(IntermediateOutputPath)generator.stamp</_GeneratorStampFile>
+  </PropertyGroup>
+
+  <!-- Get our Build Actions to show up in VS -->
+  <ItemGroup>
+    <AvailableItemName Include="EmbeddedJar" />
+    <AvailableItemName Include="EmbeddedNativeLibrary" />
+    <AvailableItemName Include="EmbeddedReferenceJar" />
+    <AvailableItemName Include="InputJar" />
+    <AvailableItemName Include="ReferenceJar" />
+    <AvailableItemName Include="TransformFile" />
+    <AvailableItemName Include="LibraryProjectProperties" />
+    <AvailableItemName Include="LibraryProjectZip" />
+    <AvailableItemName Include="JavaDocIndex" />
+    <AvailableItemName Include="JavaDocJar" />
+    <AvailableItemName Include="JavaSourceJar" />
+  </ItemGroup>
+
+  <Target Name="ExportJarToXml"
+      DependsOnTargets="$(ExportJarToXmlDependsOnTargets)"
+      Condition=" '$(UsingAndroidNETSdk)' != 'true' Or '@(InputJar)' != '' Or '@(EmbeddedJar)' != '' ">
+    <PropertyGroup>
+      <AllowUnsafeBlocks Condition=" '$(AllowUnsafeBlocks)' != 'true' ">true</AllowUnsafeBlocks>
+    </PropertyGroup>
+  </Target>
+
+  <Target Name="AndroidPrepareForBuild" DependsOnTargets="GetReferenceAssemblyPaths" />
+
+  <Target Name="GenerateBindings"
+      Condition=" '$(UsingAndroidNETSdk)' != 'true' Or '@(InputJar)' != '' Or '@(EmbeddedJar)' != '' "
+      DependsOnTargets="ExportJarToXml;_ResolveMonoAndroidSdks"
+      Inputs="$(ApiOutputFile);@(TransformFile);@(ReferencePath);@(ReferenceDependencyPaths);$(MSBuildAllProjects)"
+      Outputs="$(_GeneratorStampFile)">
+
+    <!-- Delete previous generated files if they still exist -->
+    <RemoveDirFixed Directories="$(GeneratedOutputPath)" Condition="Exists ('$(GeneratedOutputPath)')" />
+
+    <ItemGroup>
+      <AnnotationsZip Include="$(IntermediateOutputPath)library_project_annotations\*.zip" />
+    </ItemGroup>
+
+    <!-- Create the .cs binding source files -->
+    <BindingsGenerator
+        OutputDirectory="$(GeneratedOutputPath)src"
+        EnumDirectory="$(GeneratedOutputPath)enums"
+        EnumMetadataDirectory="$(GeneratedOutputPath)metadata"
+        CodegenTarget="$(AndroidCodegenTarget)"
+        AndroidApiLevel="$(_AndroidApiLevel)"
+        ApiXmlInput="$(ApiOutputFile)"
+        AnnotationsZipFiles="@(AnnotationsZip)"
+        AssemblyName="$(AssemblyName)"
+        TransformFiles="@(TransformFile)"
+        ReferencedManagedLibraries="@(ReferencePath);@(ReferenceDependencyPaths)"
+        MonoAndroidFrameworkDirectories="$(_XATargetFrameworkDirectories)"
+        TypeMappingReportFile="$(GeneratedOutputPath)type-mapping.txt"
+        ToolPath="$(MonoAndroidToolsDirectory)"
+        ToolExe="$(BindingsGeneratorToolExe)"
+        UseShortFileNames="$(UseShortGeneratorFileNames)"
+        LangVersion="$(LangVersion)"
+        EnableInterfaceMembersPreview="$(_EnableInterfaceMembers)"
+    />
+
+    <!-- Write a flag so we won't redo this target if nothing changed -->
+    <Touch Files="$(_GeneratorStampFile)" AlwaysCreate="true" />
+
+  </Target>
+
+  <Target Name="AddBindingsToCompile" DependsOnTargets="GenerateBindings">
+    <!-- Add the files to list of things to be compiled -->
+    <ItemGroup>
+      <Compile Include="$(IntermediateOutputPath)generated\*\*.cs" />
+    </ItemGroup>
+  </Target>
+
+  <Target Name="AddEmbeddedJarsAsResources" DependsOnTargets="GenerateBindings">
+    <ItemGroup>
+      <EmbeddedResource Include="@(EmbeddedJar)">
+        <LogicalName>%(Filename)%(Extension)</LogicalName>
+      </EmbeddedResource>
+    </ItemGroup>
+  </Target>
+
+  <Target Name="AddEmbeddedReferenceJarsAsResources" DependsOnTargets="GenerateBindings">
+    <ItemGroup>
+      <EmbeddedResource Include="@(EmbeddedReferenceJar)">
+        <LogicalName>__reference__%(Filename)%(Extension)</LogicalName>
+      </EmbeddedResource>
+    </ItemGroup>
+  </Target>
+
+  <Target Name="ResolveLibraryProjects" DependsOnTargets="_CreateBindingResourceArchive" />
+
+  <Target Name="AddLibraryJarsToBind" DependsOnTargets="ResolveLibraryProjects">
+    <ItemGroup>
+      <InputJar Include="$(IntermediateOutputPath)library_project_jars\*.jar" />
+    </ItemGroup>
+  </Target>
+
+  <Target Name="CleanBindingsOutput">
+    <Delete Files="$(ApiOutputFile)" />
+    <Delete Files="$(ApiOutputFile).adjusted" />
+    <Delete Files="$(ApiOutputFile).class-parse" />
+    <Delete Files="$(ApiOutputFile).fixed" />
+    <Delete Files="$(ApiOutputFile).adjusted.fixed" />
+    <Delete Files="$(_GeneratorStampFile)" />
+    <Delete Files="@(IntermediateAssembly->'$(OutputPath)%(filename).xml')" />
+    <Delete Files="@(IntermediateAssembly->'$(IntermediateOutputPath)%(filename).xml')" />
+    <RemoveDirFixed Directories="$(GeneratedOutputPath)" />
+    <RemoveDirFixed Directories="$(_AndroidStampDirectory)" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)docs" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)javadocs" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)javasources" />
+  </Target>
+  
+  <Target Name="CleanLibraryProjectIntermediaries">
+    <Delete Files="$(IntermediateOutputPath)__AndroidLibraryProjects__.zip" />
+    <Delete Files="$(_AndroidLibraryImportsCache)" />
+    <Delete Files="$(_AndroidLibraryProjectImportsCache)" />
+    <Delete Files="$(_AndroidLibrayProjectAssemblyMapFile)" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)library_project_jars" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)library_project_annotations" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)$(_LibraryProjectImportsDirectoryName)" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)__library_projects__" />
+    <RemoveDirFixed Directories="$(_AndroidLibrayProjectIntermediatePath)" />
+  </Target>
+
+  <Target Name="CleanNativeLibraryIntermediaries">
+    <Delete Files="$(IntermediateOutputPath)__AndroidNativeLibraries__.zip" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)native_library_imports" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)libs" />
+    <RemoveDirFixed Directories="$(IntermediateOutputPath)$(_NativeLibraryImportsDirectoryName)" />
+  </Target>
+
+</Project>

commit 90a4612e17da02d0b1889ce867f9db55ce29c3e9

[One .NET] fix debugging
If you tried to debug an app with:

    dotnet build HelloAndroid/HelloAndroid.csproj -t:Install,_Run -p:AndroidAttachDebugger=true

The presence of `$(AndroidAttachDebugger)` causes a crash on startup:

    06-24 11:57:42.538 14948 14948 E mono    : Unhandled Exception:
    06-24 11:57:42.538 14948 14948 E mono    : System.ArgumentNullException: Value cannot be null. (Parameter 'meth')
    06-24 11:57:42.538 14948 14948 E mono    :    at System.Reflection.Emit.ILGenerator.Emit(OpCode opcode, MethodInfo meth)
    06-24 11:57:42.538 14948 14948 E mono    :    at Android.Runtime.JNINativeWrapper.CreateDelegate(Delegate dlg)
    06-24 11:57:42.538 14948 14948 E mono    :    at Android.App.Activity.GetOnCreate_Landroid_os_Bundle_Handler()
    06-24 11:57:42.538 14948 14948 E mono    :    at Android.Runtime.AndroidTypeManager.RegisterNativeMembers(JniType nativeClass, Type type, String methods)
    06-24 11:57:42.538 14948 14948 E mono    :    at Android.Runtime.JNIEnv.RegisterJniNatives(IntPtr typeName_ptr, Int32 typeName_len, IntPtr jniClass, IntPtr methods_ptr, Int32 methods_len)
    06-24 11:57:42.538 14948 14948 E mono-rt : [ERROR] FATAL UNHANDLED EXCEPTION: System.ArgumentNullException: Value cannot be null. (Parameter 'meth')
    06-24 11:57:42.538 14948 14948 E mono-rt :    at System.Reflection.Emit.ILGenerator.Emit(OpCode opcode, MethodInfo meth)
    06-24 11:57:42.538 14948 14948 E mono-rt :    at Android.Runtime.JNINativeWrapper.CreateDelegate(Delegate dlg)
    06-24 11:57:42.538 14948 14948 E mono-rt :    at Android.App.Activity.GetOnCreate_Landroid_os_Bundle_Handler()
    06-24 11:57:42.538 14948 14948 E mono-rt :    at Android.Runtime.AndroidTypeManager.RegisterNativeMembers(JniType nativeClass, Type type, String methods)
    06-24 11:57:42.538 14948 14948 E mono-rt :    at Android.Runtime.JNIEnv.RegisterJniNatives(IntPtr typeName_ptr, Int32 typeName_len, IntPtr jniClass, IntPtr methods_ptr, Int32 methods_len)

We need to check if `mono_unhandled_exception_method` is `null` and
respond appropriately. In `get_runtime_types()`, we will use a
different null check for `exception_handler_method` to return early.

This surfaced another problem when using the `_Run` target by itself:

    Xamarin.Android.Common.Debugging.targets(518,2): error MSB3073: The command ""adb"  forward tcp:10000 tcp:10000" exited with code 9009.

The `$(_AndroidPlatformToolsDirectory)` was blank when running `_Run`
by itself, but things work fine if you run `Install,_Run` at the same
time.

This broke in fb637e08. I accidentally replaced the

src/Mono.Android/Android.Runtime/JNINativeWrapper.cs
@@ -13,7 +13,7 @@ namespace Android.Runtime {
 
 		static void get_runtime_types ()
 		{
-			if (mono_unhandled_exception_method != null)
+			if (exception_handler_method != null)
 				return;
 #if MONOANDROID1_0
 			mono_unhandled_exception_method = typeof (System.Diagnostics.Debugger).GetMethod (
@@ -70,10 +70,10 @@ namespace Android.Runtime {
 			ig.Emit (OpCodes.Leave, label);
 
 			bool  filter = Debugger.IsAttached || !JNIEnv.PropagateExceptions;
-			if (filter) {
+			if (filter && mono_unhandled_exception_method != null) {
 				ig.BeginExceptFilterBlock ();
 
-				ig.Emit (OpCodes.Call, mono_unhandled_exception_method!);
+				ig.Emit (OpCodes.Call, mono_unhandled_exception_method);
 				ig.Emit (OpCodes.Ldc_I4_1);
 				ig.BeginCatchBlock (null!);
 			} else {

src/Xamarin.Android.Build.Tasks/MSBuild/Xamarin/Android/Xamarin.Android.Bindings.Core.targets
@@ -44,8 +44,6 @@ It is shared between "legacy" binding projects and .NET 5 projects.
     </PropertyGroup>
   </Target>
 
-  <Target Name="AndroidPrepareForBuild" DependsOnTargets="GetReferenceAssemblyPaths" />
-
   <Target Name="GenerateBindings"
       Condition=" '$(UsingAndroidNETSdk)' != 'true' Or '@(InputJar)' != '' Or '@(EmbeddedJar)' != '' "
       DependsOnTargets="ExportJarToXml;_ResolveMonoAndroidSdks"

src/Xamarin.Android.Build.Tasks/Xamarin.Android.Bindings.targets
@@ -75,6 +75,8 @@ Copyright (C) 2012 Xamarin Inc. All rights reserved.
 	<GenerateSerializationAssemblies>Off</GenerateSerializationAssemblies>
   </PropertyGroup>
 
+  <Target Name="AndroidPrepareForBuild" DependsOnTargets="GetReferenceAssemblyPaths" />
+
   <!-- Warn about deprecated configurations.
        Do it here because we want them to appear on every build,
        even if we aren't rerunning generator -->

tests/MSBuildDeviceIntegration/Tests/XASdkDeployTests.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
+using System.Threading;
+using Mono.Debugging.Client;
+using Mono.Debugging.Soft;
 using NUnit.Framework;
 using Xamarin.ProjectTools;
 
@@ -26,7 +31,6 @@ namespace Xamarin.Android.Build.Tests
 					IsRelease = isRelease
 				};
 			}
-			proj.SetProperty (KnownProperties.AndroidSupportedAbis, DeviceAbi);
 			proj.SetRuntimeIdentifier (DeviceAbi);
 
 			var relativeProjDir = Path.Combine ("temp", TestName);
@@ -42,6 +46,78 @@ namespace Xamarin.Android.Build.Tests
 				Path.Combine (fullProjDir, "logcat.log"), 30);
 			RunAdbCommand ($"uninstall {proj.PackageName}");
 			Assert.IsTrue(didLaunch, "Activity should have started.");
+		}
+
+		[Test]
+		public void DotNetDebug ()
+		{
+			if (!HasDevices)
+				Assert.Ignore ("Skipping Test. No devices available.");
+
+			XASdkProject proj;
+			proj = new XASdkProject ();
+			proj.SetRuntimeIdentifier (DeviceAbi);
+
+			var relativeProjDir = Path.Combine ("temp", TestName);
+			var fullProjDir = Path.Combine (Root, relativeProjDir);
+			TestOutputDirectories [TestContext.CurrentContext.Test.ID] = fullProjDir;
+			var files = proj.Save ();
+			proj.Populate (relativeProjDir, files);
+			proj.CopyNuGetConfig (relativeProjDir);
+			var dotnet = new DotNetCLI (proj, Path.Combine (fullProjDir, proj.ProjectFilePath));
+			Assert.IsTrue (dotnet.Build ("Install"), "`dotnet build` should succeed");
+
+			bool breakpointHit = false;
+			ManualResetEvent resetEvent = new ManualResetEvent (false);
+			var sw = new Stopwatch ();
+			// setup the debugger
+			var session = new SoftDebuggerSession ();
+			session.Breakpoints = new BreakpointStore {
+				{ Path.Combine (Root, dotnet.ProjectDirectory, "MainActivity.cs"),  19 },
+			};
+			session.TargetHitBreakpoint += (sender, e) => {
+				Console.WriteLine ($"BREAK {e.Type}");
+				breakpointHit = true;
+				session.Continue ();
+			};
+			var rnd = new Random ();
+			int port = rnd.Next (10000, 20000);
+			TestContext.Out.WriteLine ($"{port}");
+			var args = new SoftDebuggerConnectArgs ("", IPAddress.Loopback, port) {
+				MaxConnectionAttempts = 10,
+			};
+			var startInfo = new SoftDebuggerStartInfo (args) {
+				WorkingDirectory = Path.Combine (dotnet.ProjectDirectory, proj.IntermediateOutputPath, "android", "assets"),
+			};
+			var options = new DebuggerSessionOptions () {
+				EvaluationOptions = EvaluationOptions.DefaultOptions,
+			};
+			options.EvaluationOptions.UseExternalTypeResolver = true;
+			ClearAdbLogcat ();
+			Assert.True (dotnet.Build ("_Run", new string [] {
+				$"AndroidSdbTargetPort={port}",
+				$"AndroidSdbHostPort={port}",
+				"AndroidAttachDebugger=True",
+			}), "Project should have run.");
+
+			Assert.IsTrue (WaitForDebuggerToStart (Path.Combine (Root, dotnet.ProjectDirectory, "logcat.log")), "Activity should have started");
+			// we need to give a bit of time for the debug server to start up.
+			WaitFor (2000);
+			session.LogWriter += (isStderr, text) => { Console.WriteLine (text); };
+			session.OutputWriter += (isStderr, text) => { Console.WriteLine (text); };
+			session.DebugWriter += (level, category, message) => { Console.WriteLine (message); };
+			session.Run (startInfo, options);
+			WaitFor (TimeSpan.FromSeconds (30), () => session.IsConnected);
+			Assert.True (session.IsConnected, "Debugger should have connected but it did not.");
+			// we need to wait here for a while to allow the breakpoints to hit
+			// but we need to timeout
+			TimeSpan timeout = TimeSpan.FromSeconds (60);
+			while (session.IsConnected && !breakpointHit && timeout >= TimeSpan.Zero) {
+				Thread.Sleep (10);
+				timeout = timeout.Subtract (TimeSpan.FromMilliseconds (10));
+			}
+			WaitFor (2000);
+			Assert.IsTrue (breakpointHit, "Should have a breakpoint");
 		}
 	}
 }