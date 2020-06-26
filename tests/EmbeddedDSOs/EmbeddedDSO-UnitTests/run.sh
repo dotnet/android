f6f31aa9008c19609bc896a3c2d3580b7e6a4298
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-06-26 10:02:49 -0500

    [tests] update and consolidate NUnit packages (#4838)
    Update to:
    
    * NUnit 3.12.0
    * NUnit.ConsoleRunner 3.11.1
    * NUnit3TestAdapter 3.16.1
    
    These are all different numbers, but these are the latest stable
    versions on nuget.org.
    
    I also went ahead and consolidated all NUnit `@(PackageReference)` to
    a `NUnitReferences.projitems` file. This way we can list the versions
    for all NUnit packages in one MSBuild file.
    
    NUnit.ConsoleRunner was still listed in a few other files we will have
    to manually update.
    
    Bump to xamarin/monodroid/master@82bea9c6
    
    Changes: https://github.com/xamarin/monodroid/compare/6e43a20e...82bea9c6

--- tests/EmbeddedDSOs/EmbeddedDSO-UnitTests/run.sh
+++ tests/EmbeddedDSOs/EmbeddedDSO-UnitTests/run.sh
@@ -5,4 +5,4 @@ export MSBUILD=msbuild
 CONFIGURATION=${1:-Debug}
 msbuild /p:Configuration=${CONFIGURATION} EmbeddedDSO-UnitTests.csproj
 cd ../../../
-exec mono --debug packages/nunit.consolerunner/3.9.0/tools/nunit3-console.exe bin/Test${CONFIGURATION}/EmbeddedDSOUnitTests.dll
+exec mono --debug packages/nunit.consolerunner/3.11.1/tools/nunit3-console.exe bin/Test${CONFIGURATION}/EmbeddedDSOUnitTests.dll
