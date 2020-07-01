e564f258df95fd07181fb8d52b2ec3c07f866fe8
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-07-01 09:12:20 -0500

    [Microsoft.Android.Sdk] set $(PublishTrimmed) after .csproj evaluated
    I noticed that if you define `<AndroidLinkMode>Full</AndroidLinkMode>`
    in a `.csproj` file, the `ILLink` MSBuild target did not run.
    
    It turns out that we were setting `$(PublishTrimmed)` by default too
    early. If set in `Microsoft.Android.Sdk.props`, this is evaluated
    before the user's `.csproj` file. If we set this in
    `Microsoft.Android.Sdk.targets`, then we can rely on values set in the
    `.csproj` file.
    
    I found a test that was failing due to this issue and added it to the
    `dotnet` category. I made a couple other changes to the test so it
    passed under .NET 5+:
    
    * Test against `FormsViewGroup.dll` instead of a support library, so
      the test still passes if using AndroidX libraries.
    * Use `$(RuntimeIdentifiers)` over `$(AndroidSupportedAbis)`. I added
      a new helper method for multiple RIDs.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Utilities/ProjectExtensions.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Utilities/ProjectExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 
 namespace Xamarin.ProjectTools
 {
@@ -15,6 +15,23 @@ namespace Xamarin.ProjectTools
 			} else if (androidAbi == "x86_64") {
 				project.SetProperty (KnownProperties.RuntimeIdentifier, "android.21-x64");
 			}
+		}
+
+		public static void SetRuntimeIdentifiers (this IShortFormProject project, string [] androidAbis)
+		{
+			var abis = new List<string> ();
+			foreach (var androidAbi in androidAbis) {
+				if (androidAbi == "armeabi-v7a") {
+					abis.Add ("android.21-arm");
+				} else if (androidAbi == "arm64-v8a") {
+					abis.Add ("android.21-arm64");
+				} else if (androidAbi == "x86") {
+					abis.Add ("android.21-x86");
+				} else if (androidAbi == "x86_64") {
+					abis.Add ("android.21-x64");
+				}
+			}
+			project.SetProperty (KnownProperties.RuntimeIdentifiers, string.Join (";", abis));
 		}
 	}
 }
