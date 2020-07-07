b1aac82c388090de42d913a44d3fdeecf300d4c6
Jonathan Pryor <jonpryor@vt.edu>
2020-07-07 15:37:17 -0400

    Explicitly separate 3rd party IP from Microsoft IP
    Fixes: https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1139604
    
    > Keeping Third-Party IP in a source control system separate from
    > Microsoft source code and other assets is the preferred engineering
    > practice…
    
    Begin following this preferred engineering practice: move source code
    which was copied into this repo into a separate `src-ThirdParty`
    directory, so that it is easier to track and manage them.
    
    Move the NUnitLite sources into `src-ThirdParty\NUnitLite`.
    
    Move the force-net/crc32.net sources into
    `src-ThirdParty\crc32.net`.
    
    Move the bazelbuild/bazel sources into
    `src-ThirdParty\bazel`.
    
    Move the *C# ported* android/platform/tools/base sources into
    `src-ThirdParty\android-platform-tools-base`.

--- build-tools/xaprepare/xaprepare/Steps/Step_Get_Windows_Binutils.cs
+++ build-tools/xaprepare/xaprepare/Steps/Step_Get_Windows_Binutils.cs
@@ -7,6 +7,8 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
+using Force.Crc32;
+
 namespace Xamarin.Android.Prepare
 {
 	class Step_Get_Windows_Binutils : Step
@@ -677,74 +679,6 @@ namespace Xamarin.Android.Prepare
 				return (false, 0);
 
 			return (true, resp.Content.Headers.ContentLength.Value);
-		}
-	}
-
-	//
-	// Taken from:
-	//   https://github.com/force-net/Crc32.NET/blob/fbc1061b0cb53df2322d5aed33167a2e6335970b/Crc32.NET/SafeProxy.cs
-	//
-	// License: MIT
-	//   https://github.com/force-net/Crc32.NET/blob/fbc1061b0cb53df2322d5aed33167a2e6335970b/LICENSE
-	//
-	class CRC32
-	{
-		const uint Poly = 0xedb88320u;
-
-		readonly uint[] _table = new uint[16 * 256];
-
-		internal CRC32 ()
-		{
-			Init (Poly);
-		}
-
-		protected void Init (uint poly)
-		{
-			var table = _table;
-			for (uint i = 0; i < 256; i++) {
-				uint res = i;
-				for (int t = 0; t < 16; t++) {
-					for (int k = 0; k < 8; k++) res = (res & 1) == 1 ? poly ^ (res >> 1) : (res >> 1);
-					table[(t * 256) + i] = res;
-				}
-			}
-		}
-
-		public uint Append (uint crc, byte[] input, int offset, int length)
-		{
-			uint crcLocal = uint.MaxValue ^ crc;
-
-			uint[] table = _table;
-			while (length >= 16) {
-				var a = table[(3 * 256) + input[offset + 12]]
-					^ table[(2 * 256) + input[offset + 13]]
-					^ table[(1 * 256) + input[offset + 14]]
-					^ table[(0 * 256) + input[offset + 15]];
-
-				var b = table[(7 * 256) + input[offset + 8]]
-					^ table[(6 * 256) + input[offset + 9]]
-					^ table[(5 * 256) + input[offset + 10]]
-					^ table[(4 * 256) + input[offset + 11]];
-
-				var c = table[(11 * 256) + input[offset + 4]]
-					^ table[(10 * 256) + input[offset + 5]]
-					^ table[(9 * 256) + input[offset + 6]]
-					^ table[(8 * 256) + input[offset + 7]];
-
-				var d = table[(15 * 256) + ((byte)crcLocal ^ input[offset])]
-					^ table[(14 * 256) + ((byte)(crcLocal >> 8) ^ input[offset + 1])]
-					^ table[(13 * 256) + ((byte)(crcLocal >> 16) ^ input[offset + 2])]
-					^ table[(12 * 256) + ((crcLocal >> 24) ^ input[offset + 3])];
-
-				crcLocal = d ^ c ^ b ^ a;
-				offset += 16;
-				length -= 16;
-			}
-
-			while (--length >= 0)
-				crcLocal = table[(byte)(crcLocal ^ input[offset++])] ^ crcLocal >> 8;
-
-			return crcLocal ^ uint.MaxValue;
 		}
 	}
 }
