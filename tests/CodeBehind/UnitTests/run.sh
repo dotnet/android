d447aa66da79993bdf30e7f9a6a7f03f11a40f19
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-07-30 09:54:06 -0500

    [tests] combine 3 test assemblies into Xamarin.Android.Build.Tests (#4961)
    Moving the following assemblies into `Xamarin.Android.Build.Tasks.dll`:
    
    * `CodeBehindUnitTests.dll`
    * `EmbeddedDSOTests.dll`
    * `Xamarin.Android.MakeBundle-UnitTests.dll`
    
    This will simplify the `.yaml` definitions for running these tests and
    makes it possible to parameterize them for a future `One .NET` test
    phase.
    
    The only code changes are:
    
    * Move any classes in the top-level namespace to nested classes.
    * Add a `BaseTest.GetPathToZipAlign()` method instead of generating
      `Config.cs` at build time.
    
    These tests are still passing.

--- tests/CodeBehind/UnitTests/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-export MONO_ENV_OPTIONS="--debug"
-export USE_MSBUILD=1
-export MSBUILD=msbuild
-msbuild CodeBehindUnitTests.csproj
-cd ../../../
-exec mono --debug packages/nunit.consolerunner/3.11.1/tools/nunit3-console.exe bin/TestDebug/CodeBehind/CodeBehindUnitTests.dll
--- tests/EmbeddedDSOs/EmbeddedDSO-UnitTests/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-export MONO_ENV_OPTIONS="--debug"
-export USE_MSBUILD=1
-export MSBUILD=msbuild
-CONFIGURATION=${1:-Debug}
-msbuild /p:Configuration=${CONFIGURATION} EmbeddedDSO-UnitTests.csproj
-cd ../../../
-exec mono --debug packages/nunit.consolerunner/3.11.1/tools/nunit3-console.exe bin/Test${CONFIGURATION}/EmbeddedDSOUnitTests.dll
