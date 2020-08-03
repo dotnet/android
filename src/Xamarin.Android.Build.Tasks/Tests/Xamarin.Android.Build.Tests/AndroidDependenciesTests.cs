936f8dc6227c5e6d0d44b404f692729ced476eb6
Peter Collins <pecolli@microsoft.com>
2020-08-03 13:42:03 -0400

    Bump to xamarin/monodroid/master@b12a3e8d (#4969)
    Context: https://github.com/xamarin/android-sdk-installer/commit/2b4d1a6a4c49c3bc3fc9751726d854d75c617434
    
    Changes: https://github.com/xamarin/monodroid/compare/5784a746...b12a3e8d
    
      * xamarin/monodroid@b12a3e8d5: Bump to xamarin/android-sdk-installer/master@2b4d1a6 & xamarin/xamarin-android/master@d583b7c (#1107)
      * xamarin/monodroid@3d84feddd: Bump to xamarin/xamarin-android/master@f0d565fe (#1106)
      * xamarin/monodroid@ec2931825: Bump to xamarin/android-sdk-installer/master@c6c12db0 (#1105)
    
    Sometimes, `<InstallAndroidDependencies/>` would fail when running on
    Windows, a'la:
    
    	…\Xamarin\Android\Xamarin.Installer.Common.targets(12,3): Cannot access a disposed object.
    	Object name: 'SslStream'.
    
    A fix for the `ObjectDisposedException` has been brought in via the
    `android-sdk-installer` bump in xamarin/monodroid@b12a3e8d.
    
    The `<InstallAndroidDependencies/>` test previously had issues due to
    clean up logic in the test framework that partially deleted the newly
    provisioned Android SDK on a subsequent build.  I believe that we've
    been seeing successful results for this test because the subsequent
    build in the test would fall back to a different Android SDK that was
    already on disk.  The test has been improved by asserting that the
    newly installed Android SDK is used during the subsequent build.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/AndroidDependenciesTests.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/AndroidDependenciesTests.cs
@@ -18,21 +18,23 @@ namespace Xamarin.Android.Build.Tests
 		public void InstallAndroidDependenciesTest ()
 		{
 			AssertCommercialBuild ();
-			if (IsWindows) {
-				// TODO: we need: https://github.com/xamarin/android-sdk-installer/pull/450
-				Assert.Ignore ("InstallAndroidDependencies currently fails on Windows");
-			}
 			var old = Environment.GetEnvironmentVariable ("ANDROID_SDK_PATH");
 			try {
 				string sdkPath = Path.Combine (Root, "temp", TestName, "android-sdk");
 				Environment.SetEnvironmentVariable ("ANDROID_SDK_PATH", sdkPath);
+				if (Directory.Exists (sdkPath))
+					Directory.Delete (sdkPath, true);
+				Directory.CreateDirectory (sdkPath);
 				var proj = new XamarinAndroidApplicationProject ();
 				using (var b = CreateApkBuilder ()) {
+					b.CleanupAfterSuccessfulBuild = false;
 					string defaultTarget = b.Target;
 					b.Target = "InstallAndroidDependencies";
 					Assert.IsTrue (b.Build (proj, parameters: new string [] { "AcceptAndroidSDKLicenses=true" }), "InstallAndroidDependencies should have succeeded.");
 					b.Target = defaultTarget;
-					Assert.IsTrue (b.Build (proj), "build should have succeeded.");
+					Assert.IsTrue (b.Build (proj, true), "build should have succeeded.");
+					Assert.IsTrue (b.LastBuildOutput.ContainsText ($"Output Property: _AndroidSdkDirectory={sdkPath}"), "_AndroidSdkDirectory was not set to new SDK path.");
+					Assert.IsTrue (b.LastBuildOutput.ContainsText ($"JavaPlatformJarPath={sdkPath}"), "JavaPlatformJarPath did not contain new SDK path.");
 				}
 			} finally {
 				Environment.SetEnvironmentVariable ("ANDROID_SDK_PATH", old);
