dotnet/android

commit 323be40a2a98daad5ba61671579ab62b44de0fd8

[Java.Interop] Avoid double-disposing PeerReferences (#690)
Context: https://github.com/xamarin/xamarin-android/issues/4989

What happens if you dispose an instance *while disposing the instance*?

	// C#
	public class MyDisposableObject : JavaObject {
	    bool _isDisposed;

	    protected override void Dispose (bool disposing)
	    {
	        if (_isDisposed) {
	            return;
	        }
	        _isDisposed = true;
	        if (this.PeerReference.IsValid)
	            this.Dispose ();
	        base.Dispose (disposing);
	    }
	}

	// …
	void MyTestMethod ()
	{
	    var value = new MyDisposableObject ();
	    value.Dispose ();
	    value.Dispose ();
	}

Here, `MyDisposableObject.Dispose(bool)` calls `JavaObject.Dispose()`
when `PeerReference` is valid.  This "feels" admittedly unusual, but
`IDisposable.Dispose()` is *supposed to be* Idempotent: it can be
called multiple times with no ill effects.  Shouldn't this be the same?

Unfortunately, it *isn't* the same; it crashes, hard:

	  =================================================================
	  	Native stacktrace:
	  =================================================================
	  	0x10245bc49 - /Library/Frameworks/Mono.framework/Versions/Current/Commands/mono : mono_dump_native_crash_info
	  	0x1023f3d35 - /Library/Frameworks/Mono.framework/Versions/Current/Commands/mono : mono_handle_native_crash
	  	0x10245b20f - /Library/Frameworks/Mono.framework/Versions/Current/Commands/mono : sigabrt_signal_handler
	  	0x7fff6983d5fd - /usr/lib/system/libsystem_platform.dylib : _sigtramp
	  	0x700009b716b0 - Unknown
	  	0x7fff69713808 - /usr/lib/system/libsystem_c.dylib : abort
	  	0x10658377a - …/lib/server/libjvm.dylib : _ZN2os5abortEbPvPKv
	  	0x10637ea5f - …/lib/server/libjvm.dylib : _ZN8jniCheck15validate_handleEP10JavaThreadP8_jobject
	  	0x10637eb08 - …/lib/server/libjvm.dylib : _ZN8jniCheck15validate_objectEP10JavaThreadP8_jobject

external/Java.Interop/src/Java.Interop/Java.Interop/JniRuntime.JniValueManager.cs
@@ -143,6 +143,13 @@ namespace Java.Interop
 				if (value == null)
 					throw new ArgumentNullException (nameof (value));
 
+				if (!value.PeerReference.IsValid)
+					return;
+
+				RemovePeer (value);
+
+				value.Disposed ();
+
 				var h = value.PeerReference;
 				if (!h.IsValid)
 					return;
@@ -155,8 +162,6 @@ namespace Java.Interop
 				if (disposed)
 					throw new ObjectDisposedException (GetType ().Name);
 
-				value.Disposed ();
-				RemovePeer (value);
 				var o = Runtime.ObjectReferenceManager;
 				if (o.LogGlobalReferenceMessages) {
 					o.WriteGlobalReferenceLine ("Disposing PeerReference={0} IdentityHashCode=0x{1} Instance=0x{2} Instance.Type={3} Java.Type={4}",

external/Java.Interop/tests/Java.Interop-Tests/Java.Interop/JavaObjectTest.cs
@@ -185,6 +185,14 @@ namespace Java.InteropTests
 			o.ToString ();
 			o.Dispose ();
 		}
+
+		[Test]
+		public void NestedDisposeInvocations ()
+		{
+			var value = new MyDisposableObject ();
+			value.Dispose ();
+			value.Dispose ();
+		}
 	}
 
 	class JavaObjectWithNoJavaPeer : JavaObject {
@@ -213,6 +221,26 @@ namespace Java.InteropTests
 				OnDisposed ();
 			else
 				OnFinalized ();
+		}
+	}
+
+	[JniTypeSignature ("java/lang/Object")]
+	class MyDisposableObject : JavaObject {
+		bool _isDisposed;
+
+		public MyDisposableObject ()
+		{
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			if (_isDisposed) {
+				return;
+			}
+			_isDisposed = true;
+			if (this.PeerReference.IsValid)
+				this.Dispose ();
+			base.Dispose (disposing);
 		}
 	}
 }

commit 0a998f8118c05ccd3b96d341e5af72ad7373e327

[Java.Interop] Dispose, *then* Remove (#708)
Fixes: https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1193891

A bug was found in Xamarin.Android 11.1.0.2, candidate for
Visual Studio 16.8 Preview 3:

 1. Create a new "Shell Forms App"
 2. Build & Run the app
 3. Tap the "Browse" button.

Expected results: it works!

Actual results: A rather abrupt crash:

	Unhandled Exception:
	System.NotSupportedException: Unable to activate instance of type Xamarin.Forms.Platform.Android.PageRenderer from native handle 0x69 (key_handle 0x33efaa1). ---> System.MissingMethodException: No constructor found for Xamarin.Forms.Platform.Android.PageRenderer::.ctor(System.IntPtr, Android.Runtime.JniHandleOwnership) ---> Java.Interop.JavaLocationException: Exception of type 'Java.Interop.JavaLocationException' was thrown.
	   --- End of inner exception stack trace ---
	  at Java.Interop.TypeManager.CreateProxy (System.Type type, System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
	  at Java.Interop.TypeManager.CreateInstance (System.IntPtr handle, Android.Runtime.JniHandleOwnership transfer, System.Type targetType)
	   --- End of inner exception stack trace ---
	  at (wrapper dynamic-method) Android.Runtime.DynamicMethodNameCounter.64(intptr,intptr)
	  at (wrapper native-to-managed) Android.Runtime.DynamicMethodNameCounter.64(intptr,intptr)

The crash was caused by commit 323be40a, which changed
`JniRuntime.JniValueManager.DisposePeer()` semantics from:

	value.Disposed ();
	RemovePeer (value);

and reversed this to:

	RemovePeer (value);
	value.Disposed ();

(Why this reversal?  Unmentioned in 323be40a -- doh! -- but as
@jonpryor is the one who did this, "it seemed like a good idea
at the time!")

(Very clearly, this wasn't a good idea.)

The crash was caused by commit 323be40a interacting with Xamarin.Forms'
[`VisualElementPackager.Dispose(bool)`][0], which can call back into

external/Java.Interop/src/Java.Interop/Java.Interop/JniRuntime.JniValueManager.cs
@@ -146,9 +146,8 @@ namespace Java.Interop
 				if (!value.PeerReference.IsValid)
 					return;
 
-				RemovePeer (value);
-
 				value.Disposed ();
+				RemovePeer (value);
 
 				var h = value.PeerReference;
 				if (!h.IsValid)

external/Java.Interop/tests/Java.Interop-Tests/Java.Interop-Tests.csproj
@@ -40,6 +40,7 @@
     <JavaInteropTestJar Include="$(MSBuildThisFileDirectory)java\com\xamarin\interop\CallNonvirtualDerived2.java" />
     <JavaInteropTestJar Include="$(MSBuildThisFileDirectory)java\com\xamarin\interop\CallVirtualFromConstructorBase.java" />
     <JavaInteropTestJar Include="$(MSBuildThisFileDirectory)java\com\xamarin\interop\CallVirtualFromConstructorDerived.java" />
+    <JavaInteropTestJar Include="$(MSBuildThisFileDirectory)java\com\xamarin\interop\GetThis.java" />
     <JavaInteropTestJar Include="$(MSBuildThisFileDirectory)java\com\xamarin\interop\SelfRegistration.java" />
     <JavaInteropTestJar Include="$(MSBuildThisFileDirectory)java\com\xamarin\interop\TestType.java" />
   </ItemGroup>

external/Java.Interop/tests/Java.Interop-Tests/Java.Interop/GetThis.cs
@@ -1,0 +1,47 @@
+using System;
+
+using Java.Interop;
+
+namespace Java.InteropTests
+{
+	[JniTypeSignature (GetThis.JniTypeName)]
+	public class GetThis : JavaObject
+	{
+		internal const string JniTypeName = "com/xamarin/interop/GetThis";
+
+		bool _isDisposed;
+
+		readonly static JniPeerMembers _members = new JniPeerMembers (JniTypeName, typeof (GetThis));
+
+		public override JniPeerMembers JniPeerMembers {
+			get {return _members;}
+		}
+
+		public GetThis ()
+		{
+		}
+
+		public unsafe GetThis This {
+			get {
+				var o   = _members.InstanceMethods.InvokeNonvirtualObjectMethod ("getThis.()Lcom/xamarin/interop/GetThis;", this, null);
+				return JniEnvironment.Runtime.ValueManager.GetValue<GetThis> (ref o, JniObjectReferenceOptions.CopyAndDispose);
+			}
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			if (_isDisposed) {
+				return;
+			}
+			_isDisposed = true;
+			if (disposing) {
+				var t = This;
+				if (t != this) {
+					throw new InvalidOperationException ("SHOULD NOT BE REACHED");
+				}
+			}
+			base.Dispose (disposing);
+		}
+	}
+}
+

external/Java.Interop/tests/Java.Interop-Tests/Java.Interop/JavaObjectTest.cs
@@ -193,6 +193,14 @@ namespace Java.InteropTests
 			value.Dispose ();
 			value.Dispose ();
 		}
+
+		[Test]
+		public void DisposeAccessesThis ()
+		{
+			var value = new GetThis ();
+			value.Dispose ();
+			value.Dispose ();
+		}
 	}
 
 	class JavaObjectWithNoJavaPeer : JavaObject {

external/Java.Interop/tests/Java.Interop-Tests/java/com/xamarin/interop/GetThis.java
@@ -1,0 +1,42 @@
+package com.xamarin.interop;
+
+import java.util.ArrayList;
+
+import com.xamarin.java_interop.GCUserPeerable;
+
+public class GetThis implements GCUserPeerable {
+
+	static  final   String  assemblyQualifiedName   = "Java.InteropTests.GetThis, Java.Interop-Tests, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+	static {
+		com.xamarin.java_interop.ManagedPeer.registerNativeMembers (
+				GetThis.class,
+				assemblyQualifiedName,
+				"");
+	}
+
+	ArrayList<Object>       managedReferences     = new ArrayList<Object>();
+
+	public GetThis () {
+		if (GetThis.class == getClass ()) {
+			com.xamarin.java_interop.ManagedPeer.construct (
+					this,
+					"Java.InteropTests.GetThis, Java.Interop-Tests, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null",
+					""
+			);
+		}
+	}
+    
+	public final GetThis getThis() {
+		return this;
+	}
+
+	public void jiAddManagedReference (java.lang.Object obj)
+	{
+		managedReferences.add (obj);
+	}
+
+	public void jiClearManagedReferences ()
+	{
+		managedReferences.clear ();
+	}
+}