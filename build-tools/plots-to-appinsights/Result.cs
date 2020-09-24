e520a84eff897cd1e0fb219e96fff47136eeebe6
Mike Bond <mjbond-msft@outlook.com>
2020-09-24 16:29:44 -0700

    Since .HasFlag is being called on the Status enum, ensure each flag occupies a unique bit position

--- build-tools/plots-to-appinsights/Result.cs
+++ build-tools/plots-to-appinsights/Result.cs
@@ -1,12 +1,15 @@
+using System;
+
 namespace Xamarin.Android.Tools.Plots
 {
+	[Flags]
 	internal enum Status
 	{
 		OK = 0,
 		ShowHelp = 1,
 		MissingArgument = 2,
-		CsvFileDoesNotExist = 3,
-		Error = 4
+		CsvFileDoesNotExist = 4,
+		Error = 8
 	}
 
 	internal class Result
