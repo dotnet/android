8e902e252f7920f90346e5ea1e022575fd5e41ac
Jonathan Pryor <jonpryor@vt.edu>
2020-09-29 16:18:26 -0400

    Revert [Flags] use, too

--- build-tools/plots-to-appinsights/Result.cs
+++ build-tools/plots-to-appinsights/Result.cs
@@ -1,15 +1,12 @@
-using System;
-
 namespace Xamarin.Android.Tools.Plots
 {
-	[Flags]
 	internal enum Status
 	{
 		OK = 0,
 		ShowHelp = 1,
 		MissingArgument = 2,
-		CsvFileDoesNotExist = 4,
-		Error = 8
+		CsvFileDoesNotExist = 3,
+		Error = 4
 	}
 
 	internal class Result
