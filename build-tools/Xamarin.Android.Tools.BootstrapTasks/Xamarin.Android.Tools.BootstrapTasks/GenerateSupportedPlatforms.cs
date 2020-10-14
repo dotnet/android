6f1a0e4eb279d74265ce71da568d28c9a618ef43
Peter Collins <pecolli@microsoft.com>
2020-10-14 16:55:42 -0400

    Move $(TargetPlatformSupported) to Microsoft.Android.Sdk.SupportedPlatforms.targets

--- build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/GenerateSupportedPlatforms.cs
+++ build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/GenerateSupportedPlatforms.cs
@@ -56,6 +56,9 @@ Specifies the supported Android platform versions for this SDK.
 				writer.WriteStartElement ("Project");
 
 				writer.WriteStartElement ("PropertyGroup");
+				writer.WriteStartElement ("TargetPlatformSupported");
+				writer.WriteString ("true");
+				writer.WriteEndElement (); // </TargetPlatformSupported>
 				writer.WriteStartElement ("TargetPlatformVersion");
 				writer.WriteAttributeString ("Condition", " '$(TargetPlatformVersion)' == '' ");
 				writer.WriteString (versions.MaxStableVersion.ApiLevel.ToString ());
