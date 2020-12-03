dotnet/android

commit 6e3e3831afa958ab8126e268f7a128321f75dbd6

[tests] multi-target MSBuildDeviceIntegration (#5035)
Build `MSBuildDeviceIntegration.csproj` for `netcoreapp3.1`, and add
a new test job for the `MSBuild Emulator Tests` stage.

I added `[Category]` as needed for tests when running under a
`dotnet` context.

Other fixes:

  * `PerformanceTest` now builds all projects for a single ABI.  This
    reduces the time spent in `<ProcessAssemblies/>`.  I also added a
    500ms buffer for a `dotnet` context and an additional 500ms if
    the test would now sign the `.apk` during `dotnet build`.
  * If `$(AutomaticNuGetRestore)`=False, `dotnet build` needs to pass
    `--no-restore`.  This improves `PerformanceTest` times.
  * `BundleToolTests` needed many file paths updated.
  * Ensure that `AssertDexToolSupported(dexTool)` is called to ignore
    tests using `$(AndroidDexTool)`=`dx`.
  * The MSBuild tests using `mono/debugger-libs` do not appear to work
    in .NET 6; added a new `Debugger` category to ignore them for now.
  * A few specific tests are marked `[Category("DotNetIgnore")]` with
    an explanation for each.
  * The `CustomLinkDescriptionPreserve` test needed to exclude code
    relying on `sqlite-net-pcl` as using PCLs do not appear to work
    in .NET 6.

tests/MSBuildDeviceIntegration/Resources/LinkDescTest/MainActivityReplacement.cs
@@ -36,8 +36,13 @@ namespace UnnamedProject
 			try
 			{
 				var asm = typeof(Library1.SomeClass).Assembly;
+				// TODO: ILLink removing .ctor: https://github.com/mono/linker/issues/1633
+#if NET
+				Android.Util.Log.Info(TAG, $"[PASS] Able to use 'typeof({typeof(Library1.SomeClass)})'.");
+#else
 				var o = Activator.CreateInstance(asm.GetType("Library1.SomeClass"));
 				Android.Util.Log.Info(TAG, $"[PASS] Able to create instance of '{o.GetType().Name}'.");
+#endif
 			}
 			catch (Exception ex)
 			{
@@ -111,7 +116,9 @@ namespace UnnamedProject
 			Android.Util.Log.Info(TAG, cldt.TryAccessNonXmlPreservedMethodOfLinkerModeFullClass());
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug21578.MulticastOption_ShouldNotBeStripped());
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug21578.MulticastOption_ShouldNotBeStripped2());
+#if !NET
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug35195.AttemptCreateTable());
+#endif
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug36250.SerializeSearchRequestWithDictionary());
 
 			Android.Util.Log.Info(TAG, "All regression tests completed.");

tests/MSBuildDeviceIntegration/Tests/InstallAndRunTests.cs
@@ -37,12 +37,11 @@ namespace Xamarin.Android.Build.Tests
 
 			proj = new XamarinAndroidApplicationProject () {
 				IsRelease = isRelease,
+				MinSdkVersion = "23",
+				TargetSdkVersion = null,
 			};
 			if (isRelease || !CommercialBuildAvailable) {
 				proj.SetAndroidSupportedAbis ("armeabi-v7a", "arm64-v8a", "x86");
-			} else {
-				proj.MinSdkVersion = "23";
-				proj.TargetSdkVersion = null;
 			}
 			proj.MainActivity = proj.DefaultMainActivity.Replace ("//${AFTER_ONCREATE}",
 $@"button.ViewTreeObserver.GlobalLayout += Button_ViewTreeObserver_GlobalLayout;
@@ -61,6 +60,7 @@ $@"button.ViewTreeObserver.GlobalLayout += Button_ViewTreeObserver_GlobalLayout;
 		}
 
 		[Test]
+		[Category ("DotNetIgnore")] // TODO: UnhandledException not firing: https://github.com/dotnet/runtime/issues/44526
 		public void SubscribeToAppDomainUnhandledException ()
 		{
 			AssertHasDevices ();
@@ -231,6 +231,7 @@ namespace Library1 {
 		};
 
 		[Test]
+		[Category ("DotNetIgnore")] // TODO: libmono-profiler-log.so is missing in .NET 6
 		public void ProfilerLogOptions_ShouldCreateMlpdFiles ([ValueSource (nameof (ProfilerOptions))] string profilerOption)
 		{
 			AssertHasDevices ();
@@ -323,12 +324,6 @@ namespace Library1 {
 								return sr.ReadToEnd ();
 						},
 					},
-					new BuildItem.Source ("Bug35195.cs") {
-						TextContent = () => {
-							using (var sr = new StreamReader (typeof (InstallAndRunTests).Assembly.GetManifestResourceStream ("Xamarin.Android.Build.Tests.Resources.LinkDescTest.Bug35195.cs")))
-								return sr.ReadToEnd ();
-						},
-					},
 					new BuildItem.Source ("Bug36250.cs") {
 						TextContent = () => {
 							using (var sr = new StreamReader (typeof (InstallAndRunTests).Assembly.GetManifestResourceStream ("Xamarin.Android.Build.Tests.Resources.LinkDescTest.Bug36250.cs")))
@@ -336,13 +331,21 @@ namespace Library1 {
 						},
 					},
 				},
-				PackageReferences = {
-					new Package {
-						Id = "sqlite-net-pcl",
-						Version = "1.7.335",
-					},
-				},
 			};
+
+			// sqlite-net-pcl does not work in .NET 6
+			if (!Builder.UseDotNet) {
+				lib2.PackageReferences.Add (new Package {
+					Id = "sqlite-net-pcl",
+					Version = "1.7.335",
+				});
+				lib2.Sources.Add (new BuildItem.Source ("Bug35195.cs") {
+					TextContent = () => {
+						using (var sr = new StreamReader (typeof (InstallAndRunTests).Assembly.GetManifestResourceStream ("Xamarin.Android.Build.Tests.Resources.LinkDescTest.Bug35195.cs")))
+							return sr.ReadToEnd ();
+					},
+				});
+			}
 
 			proj = new XamarinFormsAndroidApplicationProject () {
 				IsRelease = true,

build-tools/automation/azure-pipelines.yaml
@@ -62,7 +62,7 @@ variables:
   # - This is a non-fork branch with name containing "mono-" (for Mono bumps)
   IsMonoBranch: $[and(eq(variables['XA.Commercial.Build'], 'true'), ne(variables['System.PullRequest.IsFork'], 'True'), or(contains(variables['Build.SourceBranchName'], 'mono-'), contains(variables['System.PullRequest.SourceBranch'], 'mono-')))]
   RunAllTests: $[or(eq(variables['XA.RunAllTests'], true), eq(variables['IsMonoBranch'], true))]
-  DotNetNUnitCategories: '& TestCategory != DotNetIgnore & TestCategory != AOT & TestCategory != MkBundle & TestCategory != MonoSymbolicate & TestCategory != PackagesConfig & TestCategory != StaticProject'
+  DotNetNUnitCategories: '& TestCategory != DotNetIgnore & TestCategory != AOT & TestCategory != MkBundle & TestCategory != MonoSymbolicate & TestCategory != PackagesConfig & TestCategory != StaticProject & TestCategory != Debugger'
   NUnit.NumberOfTestWorkers: 4
   GitHub.Token: $(github--pat--vs-mobiletools-engineering-service2)
 
@@ -704,7 +704,7 @@ stages:
     - template: yaml-templates/run-nunit-tests.yaml
       parameters:
         testRunTitle: MSBuildDeviceIntegration Smoke - macOS
-        testAssembly: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/MSBuildDeviceIntegration/MSBuildDeviceIntegration.dll
+        testAssembly: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/MSBuildDeviceIntegration/net472/MSBuildDeviceIntegration.dll
         nunitConsoleExtraArgs: --where "cat == SmokeTests"
         testResultsFile: TestResult-MSBuildDeviceIntegrationSmoke-$(XA.Build.Configuration).xml
 
@@ -835,70 +835,19 @@ stages:
   dependsOn: mac_build
   condition: and(succeeded(), or(eq(variables['RunAllTests'], true), contains(dependencies.mac_build.outputs['mac_build_create_installers.TestConditions.TestAreas'], 'MSBuildDevice')))
   jobs:
-  # Check - "Xamarin.Android (MSBuild Emulator Tests macOS)"
-  - job: mac_msbuilddevice_tests
-    displayName: macOS
-    pool: $(HostedMac)
-    timeoutInMinutes: 150
-    cancelTimeoutInMinutes: 5
-    workspace:
-      clean: all
-    steps:
-    - template: yaml-templates/setup-test-environment.yaml
+  # Check - "Xamarin.Android (MSBuild Emulator Tests macOS - Legacy)"
+  - template: yaml-templates/run-msbuild-device-tests.yaml
+    parameters:
+      job_name: mac_msbuilddevice_tests
+      job_suffix: Legacy
 
-    - script: mono $(System.DefaultWorkingDirectory)/build-tools/xaprepare/xaprepare/bin/$(XA.Build.Configuration)/xaprepare.exe --s=EmulatorTestDependencies --no-emoji --run-mode=CI
-      displayName: install emulator
-
-    - task: DownloadPipelineArtifact@2
-      inputs:
-        artifactName: $(TestAssembliesArtifactName)
-        downloadPath: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)
-
-    - task: MSBuild@1
-      displayName: build check-boot-times.csproj
-      inputs:
-        solution: build-tools/check-boot-times/check-boot-times.csproj
-        configuration: $(XA.Build.Configuration)
-        msbuildArguments: /restore /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/build-check-boot-times.binlog
-      continueOnError: true
-
-    - task: MSBuild@1
-      displayName: Run check-boot-times
-      inputs:
-        solution: tests/Mono.Android-Tests/Mono.Android-Tests.csproj
-        configuration: $(XA.Build.Configuration)
-        msbuildArguments: /t:CheckBootTimes /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/run-check-boot-times.binlog
-      continueOnError: true
-
-    - task: MSBuild@1
-      displayName: start emulator
-      inputs:
-        solution: tests/Mono.Android-Tests/Mono.Android-Tests.csproj
-        configuration: $(XA.Build.Configuration)
-        msbuildArguments: /t:AcquireAndroidTarget /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/start-emulator.binlog
-
-    - template: yaml-templates/run-nunit-tests.yaml
-      parameters:
-        testRunTitle: MSBuildDeviceIntegration On Device - macOS
-        testAssembly: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/MSBuildDeviceIntegration/MSBuildDeviceIntegration.dll
-        nunitConsoleExtraArgs: --where "cat != TimeZoneInfo && cat != SmokeTests"
-        testResultsFile: TestResult-MSBuildDeviceIntegration-$(XA.Build.Configuration).xml
-
-    - task: MSBuild@1
-      displayName: shut down emulator
-      inputs:
-        solution: tests/Mono.Android-Tests/Mono.Android-Tests.csproj
-        configuration: $(XA.Build.Configuration)
-        msbuildArguments: >-
-          /t:AcquireAndroidTarget,ReleaseAndroidTarget
-          /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/shutdown-emulator.binlog
-      condition: always()
-
-    - template: yaml-templates/upload-results.yaml
-      parameters:
-        artifactName: Test Results - MSBuild With Emulator - macOS
-
-    - template: yaml-templates/fail-on-issue.yaml
+  # Check - "Xamarin.Android (MSBuild Emulator Tests macOS - One .NET)"
+  - template: yaml-templates/run-msbuild-device-tests.yaml
+    parameters:
+      job_name: mac_dotnetdevice_tests
+      job_suffix: One .NET
+      nunit_categories: $(DotNetNUnitCategories)
+      target_framework: 'netcoreapp3.1'
 
 - stage: designer_tests
   displayName: Designer Tests

build-tools/automation/yaml-templates/run-msbuild-device-tests.yaml
@@ -1,0 +1,77 @@
+# Runs MSBuild tests against a device running on macOS
+
+parameters:
+  job_name: ''
+  job_suffix: ''
+  nunit_categories: ''
+  target_framework: 'net472'
+
+jobs:
+  - job: ${{ parameters.job_name }}
+    displayName: MSBuild With Emulator - macOS - ${{ parameters.job_suffix }}
+    pool: $(HostedMac)
+    timeoutInMinutes: 150
+    cancelTimeoutInMinutes: 5
+    workspace:
+      clean: all
+    variables:
+      UseDotNet: ${{ eq(parameters.target_framework, 'netcoreapp3.1') }}
+    steps:
+    - template: setup-test-environment.yaml
+
+    - script: mono $(System.DefaultWorkingDirectory)/build-tools/xaprepare/xaprepare/bin/$(XA.Build.Configuration)/xaprepare.exe --s=EmulatorTestDependencies --no-emoji --run-mode=CI
+      displayName: install emulator
+
+    - task: DownloadPipelineArtifact@2
+      inputs:
+        artifactName: $(TestAssembliesArtifactName)
+        downloadPath: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)
+
+    - ${{ if eq(parameters.job_suffix, 'Legacy') }}:
+      - task: MSBuild@1
+        displayName: build check-boot-times.csproj
+        inputs:
+          solution: build-tools/check-boot-times/check-boot-times.csproj
+          configuration: $(XA.Build.Configuration)
+          msbuildArguments: /restore /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/build-check-boot-times.binlog
+        continueOnError: true
+
+      - task: MSBuild@1
+        displayName: Run check-boot-times
+        inputs:
+          solution: tests/Mono.Android-Tests/Mono.Android-Tests.csproj
+          configuration: $(XA.Build.Configuration)
+          msbuildArguments: /t:CheckBootTimes /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/run-check-boot-times.binlog
+        continueOnError: true
+
+    - task: MSBuild@1
+      displayName: start emulator
+      inputs:
+        solution: tests/Mono.Android-Tests/Mono.Android-Tests.csproj
+        configuration: $(XA.Build.Configuration)
+        msbuildArguments: /t:AcquireAndroidTarget /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/start-emulator.binlog
+
+    - template: run-nunit-tests.yaml
+      parameters:
+        useDotNet: $(UseDotNet)
+        testRunTitle: MSBuildDeviceIntegration On Device - macOS - ${{ parameters.job_suffix }}
+        testAssembly: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/MSBuildDeviceIntegration/${{ parameters.target_framework }}/MSBuildDeviceIntegration.dll
+        nunitConsoleExtraArgs: --where "cat != TimeZoneInfo && cat != SmokeTests ${{ parameters.nunit_categories }}"
+        dotNetTestExtraArgs: --filter "TestCategory != TimeZoneInfo & TestCategory != SmokeTests ${{ parameters.nunit_categories }}"
+        testResultsFile: TestResult-MSBuildDeviceIntegration-${{ parameters.job_name }}-$(XA.Build.Configuration).xml
+
+    - task: MSBuild@1
+      displayName: shut down emulator
+      inputs:
+        solution: tests/Mono.Android-Tests/Mono.Android-Tests.csproj
+        configuration: $(XA.Build.Configuration)
+        msbuildArguments: >-
+          /t:AcquireAndroidTarget,ReleaseAndroidTarget
+          /bl:$(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/shutdown-emulator.binlog
+      condition: always()
+
+    - template: upload-results.yaml
+      parameters:
+        artifactName: Test Results - MSBuild With Emulator - macOS - ${{ parameters.job_suffix }}
+
+    - template: fail-on-issue.yaml

build-tools/automation/yaml-templates/run-timezoneinfo-tests.yaml
@@ -33,7 +33,7 @@ jobs:
     - template: run-nunit-tests.yaml
       parameters:
         testRunTitle: TimeZoneInfoTests On Device - macOS
-        testAssembly: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/MSBuildDeviceIntegration/MSBuildDeviceIntegration.dll
+        testAssembly: $(System.DefaultWorkingDirectory)/bin/Test$(XA.Build.Configuration)/MSBuildDeviceIntegration/net472/MSBuildDeviceIntegration.dll
         nunitConsoleExtraArgs: --where "test == Xamarin.Android.Build.Tests.DeploymentTest.CheckTimeZoneInfoIsCorrectNode${{ parameters.node_id }}"
         testResultsFile: TestResult-TimeZoneInfoTests-Node${{ parameters.node_id }}-$(XA.Build.Configuration).xml
 

build-tools/scripts/NUnitReferences.projitems
@@ -5,4 +5,9 @@
     <PackageReference Include="NUnit.ConsoleRunner" Version="$(NUnitConsoleVersion)" />
     <PackageReference Include="NUnit3TestAdapter"   Version="3.16.1" />
   </ItemGroup>
+  <!-- Required packages for .NET Core -->
+  <ItemGroup Condition=" '$(TargetFramework)' == 'netcoreapp3.1' ">
+    <PackageReference Include="Microsoft.NET.Test.Sdk" Version="16.6.1" />
+    <PackageReference Include="System.Configuration.ConfigurationManager" Version="4.7.0" />
+  </ItemGroup>
 </Project>

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Xamarin.Android.Build.Tests.csproj
@@ -16,12 +16,6 @@
     </Reference>
   </ItemGroup>
 
-  <!-- Required packages for .NET Core -->
-  <ItemGroup Condition=" '$(TargetFramework)' == 'netcoreapp3.1' ">
-    <PackageReference Include="Microsoft.NET.Test.Sdk" Version="16.6.1" />
-    <PackageReference Include="System.Configuration.ConfigurationManager" Version="4.7.0" />
-  </ItemGroup>
-
   <ItemGroup>
     <ProjectReference Include="..\Xamarin.ProjectTools\Xamarin.ProjectTools.csproj" />
     <ProjectReference Include="..\..\Xamarin.Android.Build.Tasks.csproj" />

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/Builder.cs
@@ -282,7 +282,11 @@ namespace Xamarin.ProjectTools
 				args.Append ("build ");
 			args.AppendFormat ("{0} /t:{1} {2}",
 					QuoteFileName (Path.Combine (XABuildPaths.TestOutputDirectory, projectOrSolution)), target, logger);
-			if (AutomaticNuGetRestore && restore && !UseDotNet) {
+			if (UseDotNet) {
+				if (!AutomaticNuGetRestore) {
+					args.Append (" --no-restore");
+				}
+			} else if (AutomaticNuGetRestore && restore) {
 				args.Append (" /restore");
 			}
 			if (MaxCpuCount != null) {

commit 25b375c16346cc57725cabb3fe05f4bb4d47d563

[tests] enable sqlite-net-pcl test in .NET 6
Fixes: https://github.com/xamarin/xamarin-android/issues/5319

This test was originally failing in 6e3e3831 with:

    android.runtime.JavaProxyThrowable: System.TypeInitializationException: The type initializer for 'SQLite.SQLiteConnection' threw an exception.
    ---> System.DllNotFoundException: e_sqlite3
        at System.Runtime.InteropServices.NativeLibrary.LoadLibraryByName(String libraryName, Assembly assembly, Nullable`1 searchPath, Boolean throwOnError)
        at System.Runtime.InteropServices.NativeLibrary.Load(String libraryName, Assembly assembly, Nullable`1 searchPath)
        at SQLitePCL.NativeLibrary.Load(String libraryName, Assembly assy, Int32 flags)
        at SQLitePCL.Batteries_V2.MakeDynamic(String name, Int32 flags)
        at SQLitePCL.Batteries_V2.DoDynamic_cdecl(String name, Int32 flags)
        at SQLitePCL.Batteries_V2.Init()
        at SQLite.SQLiteConnection..cctor()

The problem being one of the transitive dependencies of `sqlite-net-pcl`:

https://www.nuget.org/packages/SQLitePCLRaw.bundle_green/

This package pulls in even more packages:

    .NETCoreApp 3.1
        SQLitePCLRaw.core (>= 2.0.4)
        SQLitePCLRaw.lib.e_sqlite3 (>= 2.0.4)
        SQLitePCLRaw.provider.dynamic_cdecl (>= 2.0.4)
    MonoAndroid 8.0
        SQLitePCLRaw.core (>= 2.0.4)
        SQLitePCLRaw.lib.e_sqlite3.android (>= 2.0.4)
        SQLitePCLRaw.provider.e_sqlite3 (>= 2.0.4)

`SQLitePCLRaw.lib.e_sqlite3.android` contains the `libe_sqlite3.so`
native library for Android. In .NET 6, NuGet is preferring the
`.NETCoreApp 3.1` packages over the `MonoAndroid 8.0` ones.

What we can do to workaround the problem is manually add an additional
`@(PackageReference)` to the Android head project:

    <PackageReference Include="SQLitePCLRaw.lib.e_sqlite3.android" Version="2.0.4" />

Then the test works as expected.

Eventually, `SQLitePCLRaw.bundle_green` will need to include
dependencies for .NET 6, such as:

tests/MSBuildDeviceIntegration/Resources/LinkDescTest/MainActivityReplacement.cs
@@ -116,9 +116,7 @@ namespace UnnamedProject
 			Android.Util.Log.Info(TAG, cldt.TryAccessNonXmlPreservedMethodOfLinkerModeFullClass());
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug21578.MulticastOption_ShouldNotBeStripped());
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug21578.MulticastOption_ShouldNotBeStripped2());
-#if !NET
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug35195.AttemptCreateTable());
-#endif
 			Android.Util.Log.Info(TAG, LinkTestLib.Bug36250.SerializeSearchRequestWithDictionary());
 
 			Android.Util.Log.Info(TAG, "All regression tests completed.");

tests/MSBuildDeviceIntegration/Tests/InstallAndRunTests.cs
@@ -317,6 +317,12 @@ namespace Library1 {
 				ProjectName = "LinkTestLib",
 				Sdk = "Microsoft.NET.Sdk",
 				TargetFramework = "netstandard2.0",
+				PackageReferences = {
+					new Package {
+						Id = "sqlite-net-pcl",
+						Version = "1.7.335",
+					}
+				},
 				Sources = {
 					new BuildItem.Source ("Bug21578.cs") {
 						TextContent = () => {
@@ -330,22 +336,14 @@ namespace Library1 {
 								return sr.ReadToEnd ();
 						},
 					},
+					new BuildItem.Source ("Bug35195.cs") {
+						TextContent = () => {
+							using (var sr = new StreamReader (typeof (InstallAndRunTests).Assembly.GetManifestResourceStream ("Xamarin.Android.Build.Tests.Resources.LinkDescTest.Bug35195.cs")))
+								return sr.ReadToEnd ();
+						},
+					},
 				},
 			};
-
-			// sqlite-net-pcl does not work in .NET 6
-			if (!Builder.UseDotNet) {
-				lib2.PackageReferences.Add (new Package {
-					Id = "sqlite-net-pcl",
-					Version = "1.7.335",
-				});
-				lib2.Sources.Add (new BuildItem.Source ("Bug35195.cs") {
-					TextContent = () => {
-						using (var sr = new StreamReader (typeof (InstallAndRunTests).Assembly.GetManifestResourceStream ("Xamarin.Android.Build.Tests.Resources.LinkDescTest.Bug35195.cs")))
-							return sr.ReadToEnd ();
-					},
-				});
-			}
 
 			proj = new XamarinFormsAndroidApplicationProject () {
 				IsRelease = true,
@@ -373,6 +371,14 @@ namespace Library1 {
 					},
 				},
 			};
+			if (Builder.UseDotNet) {
+				// NOTE: workaround for netcoreapp3.1 dependency preferred over monoandroid8.0
+				proj.PackageReferences.Add (new Package {
+					Id = "SQLitePCLRaw.lib.e_sqlite3.android",
+					Version = "2.0.4",
+				});
+			}
+
 			proj.AndroidManifest = proj.AndroidManifest.Replace ("</manifest>", "<uses-permission android:name=\"android.permission.INTERNET\" /></manifest>");
 			proj.SetAndroidSupportedAbis ("armeabi-v7a", "arm64-v8a", "x86", "x86_64");
 			using (var sr = new StreamReader (typeof (InstallAndRunTests).Assembly.GetManifestResourceStream ("Xamarin.Android.Build.Tests.Resources.LinkDescTest.MainActivityReplacement.cs")))