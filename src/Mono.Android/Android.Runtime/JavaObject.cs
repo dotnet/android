a4297e8f7fb29e403c4ab7f3335d4013f62587b3
Radek Doulik <radekdoulik@gmail.com>
2020-12-04 16:52:24 +0100

    Remove Clone method

--- src/Mono.Android/Android.Runtime/JavaObject.cs
+++ src/Mono.Android/Android.Runtime/JavaObject.cs
@@ -90,14 +90,6 @@ namespace Android.Runtime {
 			get { return inst; }
 		}
 
-		protected override Java.Lang.Object Clone ()
-		{
-			if (inst is ICloneable)
-				return new JavaObject (((ICloneable)inst).Clone ());
-			else
-				return new JavaObject (inst);
-		}
-
 		public override bool Equals (Java.Lang.Object? obj)
 		{
 			if (obj is JavaObject jobj) {
