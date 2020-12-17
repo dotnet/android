6034f6f35552f67dd872f00857b7a4edacda66e8
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-12-17 17:04:34 -0600

    [One .NET] simplify native library inclusion (#5386)
    I've noticed that the `_IncludeNativeSystemLibraries` MSBuild target
    can take 40ms because it is running this workaround:
    
    	<!-- HACK remove any unexpected runtime components -->
    	<FrameworkNativeLibrary
    	    Remove="@(ResolvedFileToPublish)"
    	    Condition="  $([System.Text.RegularExpressions.Regex]::IsMatch('%(FullPath)', '.*linux-.*'))  "
    	/>
    
    The `_IncludeNativeSystemLibraries` also can be split up between
    "legacy" Xamarin.Android and .NET 6. The .NET 6 version can simply
    check the `@(ResolvedFileToPublish)` item group, as the other parts
    support the interpreter, profiler, etc., which are not available in
    .NET 6 yet.
    
    I created a new `<ProcessNativeLibraries/>` MSBuild task to simplify
    the work in C#:
    
      * We can iterate over all `.so` files in `@(ResolvedFileToPublish)`.
      * We can fix up `libmonodroid.so` as needed.
      * We can emit a warning if the ABI is unknown without using a `Regex`.
        It is possible for Linux `.so` files to be brought in from NuGet
        packages as discussed in: https://github.com/dotnet/runtime/pull/45927
    
    The warning is a new alternate message for XA4301, such as:
    
    	warning XA4301: Cannot determine ABI of native library 'C:\src\xamarin-android\packages\sqlitepclraw.lib.e_sqlite3.linux\1.1.11\runtimes\linux-arm64\native\libe_sqlite3.so'.
    	Remove the 'SQLitePCLRaw.lib.e_sqlite3.linux' NuGet package, or notify the library author.
    
    As part of this change, .NET 6 will no longer need to do:
    
    	<PropertyGroup>
    	  <_Assemblies>@(_ResolvedFrameworkAssemblies)</_Assemblies>
    
    So we can also avoid creating a ~14K character string.
    
    ~~ Results ~~
    
    Timing the project fromm the `DotNetBuild(android.21-arm,false)`
    test, in a build with no changes:
    
      * Before:
    
            40 ms  _IncludeNativeSystemLibraries              1 calls
    
      * After:
    
             4 ms  _IncludeNativeSystemLibraries              1 calls
    
    This should save ~36ms from all .NET 6 builds.

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/ProcessNativeLibraries.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+
+namespace Xamarin.Android.Tasks
+{
+	/// <summary>
+	/// Processes .so files coming from @(ResolvedFileToPublish).
+	/// * Checks if ABI is valid
+	/// * Fixes up libmonodroid.so based on $(AndroidIncludeDebugSymbols)
+	/// </summary>
+	public class ProcessNativeLibraries : AndroidTask
+	{
+		public override string TaskPrefix => "PRNL";
+
+		/// <summary>
+		/// Assumed to be .so files only
+		/// </summary>
+		public ITaskItem [] InputLibraries { get; set; }
+
+		public bool IncludeDebugSymbols { get; set; }
+
+		[Output]
+		public ITaskItem [] OutputLibraries { get; set; }
+
+		public override bool RunTask ()
+		{
+			if (InputLibraries == null || InputLibraries.Length == 0)
+				return true;
+
+			var output = new List<ITaskItem> (InputLibraries.Length);
+
+			foreach (var library in InputLibraries) {
+				var abi = MonoAndroidHelper.GetNativeLibraryAbi (library);
+				if (string.IsNullOrEmpty (abi)) {
+					var packageId = library.GetMetadata ("NuGetPackageId");
+					if (!string.IsNullOrEmpty (packageId)) {
+						Log.LogCodedWarning ("XA4301", library.ItemSpec, 0, Properties.Resources.XA4301_ABI_NuGet, library.ItemSpec, packageId);
+					} else {
+						Log.LogCodedWarning ("XA4301", library.ItemSpec, 0, Properties.Resources.XA4301_ABI, library.ItemSpec);
+					}
+					continue;
+				}
+				// Both libmono-android.debug.so and libmono-android.release.so are in InputLibraries.
+				// Use IncludeDebugSymbols to determine which one to include.
+				// We may eventually have files such as `libmono-android-checked+asan.release.so` as well.
+				var fileName = Path.GetFileNameWithoutExtension (library.ItemSpec);
+				if (fileName.StartsWith ("libmono-android", StringComparison.Ordinal)) {
+					if (fileName.EndsWith (".debug", StringComparison.Ordinal)) {
+						if (!IncludeDebugSymbols)
+							continue;
+						library.SetMetadata ("ArchiveFileName", "libmonodroid.so");
+					} else if (fileName.EndsWith (".release", StringComparison.Ordinal)) {
+						if (IncludeDebugSymbols)
+							continue;
+						library.SetMetadata ("ArchiveFileName", "libmonodroid.so");
+					}
+				}
+				output.Add (library);
+			}
+
+			OutputLibraries = output.ToArray ();
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
