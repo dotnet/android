eef95d9c5df0e3b08043f8b4ab5c1da780bfd3cf
Radek Doulik <radekdoulik@gmail.com>
2020-12-18 17:03:55 +0100

    [illink] Don't initialize managed peer
    Context: https://github.com/xamarin/xamarin-android/issues/5400
    Context: https://github.com/xamarin/java.interop/pull/761
    
    Generate `XAConfig.props` file in Java.Interop to define `XA_JI_EXCLUDE`
    compilation constant.
    
    The generated file contains properties set for Java.Interop by XA. So
    far it is only setting one property, and thus doesn't necessarily be
    generated. I still wanted to be written by `xaprep`, plus it might be
    handy in future for other configuration.
    
    The currently set `JavaInteropDefineConstants` property is going to be
    used to disable `ManagedPeer` initialization.
    
    We don't really use `ManagedPeer` today and it is duplicating
    functionality of `ConstructorBuilder` class. The unwanted effect of
    having it in JI is that it is pulling in System.Linq.Expressions, which
    greatly increase assemblies size after linking.
    
    The size reduction for simple XA app is cca 135kbytes of compressed
    size.
    
        Size difference in bytes ([*1] apk1 only, [*2] apk2 only):
          -          27 assemblies/Mono.Android.dll
          -          30 assemblies/System.Collections.Concurrent.dll
          -         989 assemblies/System.Linq.dll
          -       2,930 assemblies/Java.Interop.dll
          -       4,514 assemblies/System.Collections.dll *1
          -       4,874 assemblies/System.ObjectModel.dll *1
          -       7,987 assemblies/System.Private.CoreLib.dll
          -     115,284 assemblies/System.Linq.Expressions.dll *1
        Summary:
          -     136,635 Assemblies -14.87% (of 918,689)
          -     139,506 Package size difference -1.79% (of 7,780,819)

---
+++ build-tools/xaprepare/xaprepare/Application/GeneratedJavaInteropConfigPropsFile.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Xamarin.Android.Prepare
+{
+	class GeneratedJavaInteropConfigPropsFile : GeneratedFile
+	{
+		const string Content = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<!-- This is a GENERATED FILE -->
+<!-- See build-tools/xaprepare/xaprepare/Application/GeneratedJavaInteropConfigPropsFile.cs -->
+<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <JavaInteropDefineConstants>XA_JI_EXCLUDE</JavaInteropDefineConstants>
+  </PropertyGroup>
+</Project>
+";
+
+		public GeneratedJavaInteropConfigPropsFile (Context context)
+			: base (Path.Combine (Configurables.Paths.ExternalJavaInteropDir, "bin", $"Build{context.Configuration}", "XAConfig.props"))
+		{}
+
+		public override void Generate (Context context)
+		{
+			using (var fs = File.Open (OutputPath, FileMode.Create)) {
+				using (var sw = new StreamWriter (fs)) {
+					sw.Write (Content);
+				}
+			}
+		}
+	}
+}
