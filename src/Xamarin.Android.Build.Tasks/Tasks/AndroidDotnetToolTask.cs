a9bd44bf5bd487c6e66fa4074c23058e2721aa5e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-01-20 11:52:33 -0600

    Changes based on feedback

--- src/Xamarin.Android.Build.Tasks/Tasks/AndroidDotnetToolTask.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/AndroidDotnetToolTask.cs
@@ -15,16 +15,16 @@ namespace Xamarin.Android.Tasks
 		/// <summary>
 		/// If `true`, this task should run `dotnet foo.dll` and `foo.exe` otherwise.
 		/// </summary>
-		protected bool UsingDotnet { get; private set; }
+		protected bool NeedsDotnet { get; private set; }
 
 		/// <summary>
 		/// If `true`, this task should run `mono foo.exe`.
 		/// NOTE: this would only occur when building "legacy" Xamarin.Android projects under `dotnet build`.
 		/// </summary>
-		protected bool UsingMono { get; private set; }
+		protected bool NeedsMono { get; private set; }
 
 		/// <summary>
-		/// The path to the assembly `foo.dll`. Will be `null` when UsingDotnet and UsingMono are `false`.
+		/// The path to the assembly `foo.dll`. Will be `null` when NeedsDotnet and NeedsMono are `false`.
 		/// </summary>
 		protected string AssemblyPath { get; private set; }
 
@@ -36,7 +36,7 @@ namespace Xamarin.Android.Tasks
 
 			var assemblyPath = Path.Combine (ToolPath, $"{BaseToolName}.dll");
 			if (File.Exists (assemblyPath)) {
-				UsingDotnet = true;
+				NeedsDotnet = true;
 				AssemblyPath = assemblyPath;
 				ToolPath = null;
 
@@ -44,12 +44,14 @@ namespace Xamarin.Android.Tasks
 			} else {
 				if (!RuntimeInformation.IsOSPlatform (OSPlatform.Windows) &&
 						!RuntimeInformation.FrameworkDescription.StartsWith ("Mono", StringComparison.OrdinalIgnoreCase)) {
-					UsingMono = true;
-					AssemblyPath = Path.ChangeExtension (assemblyPath, ".exe");
+					// If not Windows and not running under Mono
+					NeedsMono = true;
+					AssemblyPath = Path.Combine (ToolPath, $"{BaseToolName}.exe");
 					ToolPath = null;
 
 					Log.LogDebugMessage ($"Using: mono {AssemblyPath}");
 				} else {
+					// Otherwise running the .exe directly should work
 					Log.LogDebugMessage ($"Using: {GenerateFullPathToTool ()}");
 				}
 			}
@@ -64,35 +66,62 @@ namespace Xamarin.Android.Tasks
 			get;
 		}
 
-		protected override string ToolName =>
-			UsingDotnet ? "dotnet" : UsingMono ? "mono" : $"{BaseToolName}.exe";
+		protected override string ToolName {
+			get {
+				if (NeedsDotnet)
+					return "dotnet";
+				if (NeedsMono)
+					return "mono";
+				return $"{BaseToolName}.exe";
+			}
+		}
 
-		protected override string GenerateFullPathToTool () =>
-			UsingDotnet ? "dotnet" : UsingMono ? FindMono () : Path.Combine (ToolPath, ToolExe);
+		protected override string GenerateFullPathToTool ()
+		{
+			if (NeedsDotnet)
+				return "dotnet";
+			if (NeedsMono)
+				return FindMono ();
+			return Path.Combine (ToolPath, ToolExe);
+		}
 
 
 		const RegisteredTaskObjectLifetime Lifetime = RegisteredTaskObjectLifetime.Build;
 		const string MonoKey = nameof (AndroidDotnetToolTask) + "_Mono";
+		static readonly string [] KnownMonoPaths = new [] {
+			"/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono",
+			"/usr/bin/mono",
+		};
 
 		string FindMono ()
 		{
 			string mono = BuildEngine4.GetRegisteredTaskObjectAssemblyLocal<string> (MonoKey, Lifetime);
-			if (!string.IsNullOrEmpty (mono))
+			if (!string.IsNullOrEmpty (mono)) {
+				Log.LogDebugMessage ($"Found cached mono via {nameof (BuildEngine4.RegisterTaskObject)}");
 				return mono;
+			}
 
 			var env = Environment.GetEnvironmentVariable ("PATH");
 			if (string.IsNullOrEmpty (env)) {
 				foreach (var path in env.Split (Path.PathSeparator)) {
-					mono = Path.Combine (path, "mono");
-					if (File.Exists (mono)) {
+					if (File.Exists (mono = Path.Combine (path, "mono"))) {
+						Log.LogDebugMessage ("Found mono in $PATH");
 						BuildEngine4.RegisterTaskObjectAssemblyLocal (MonoKey, mono, Lifetime);
 						return mono;
 					}
 				}
 			}
 
-			mono = "/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono";
-			BuildEngine4.RegisterTaskObjectAssemblyLocal (MonoKey, mono, Lifetime);
+			foreach (var path in KnownMonoPaths) {
+				if (File.Exists (mono = path)) {
+					Log.LogDebugMessage ($"Found mono in {nameof (KnownMonoPaths)}");
+					BuildEngine4.RegisterTaskObjectAssemblyLocal (MonoKey, mono, Lifetime);
+					return mono;
+				}
+			}
+
+			// Last resort
+			BuildEngine4.RegisterTaskObjectAssemblyLocal (MonoKey, mono = "mono", Lifetime);
 			return mono;
 		}
 
