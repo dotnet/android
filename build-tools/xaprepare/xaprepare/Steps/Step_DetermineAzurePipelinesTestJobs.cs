dotnet/android

commit 49e570d8c93398643a84f5da251752e2bd8ccb0f

[build] migrate remap-assembly-ref and create-pkg to short-form MSBuild project (#5505)
Convert to SDK style projects.

Additionally, update `MSBuildRunner` so that `msbuild /restore` is
used by default when the `binLogName` parameter is provided.

build-tools/remap-assembly-ref/remap-assembly-ref.csproj
@@ -1,38 +1,20 @@
 <?xml version="1.0" encoding="utf-8"?>
-<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+<Project Sdk="Microsoft.NET.Sdk">
   <PropertyGroup>
-    <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
-    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
-    <ProjectGuid>{C876DA71-8573-4CEF-9149-716D72455ED4}</ProjectGuid>
+    <TargetFramework>net472</TargetFramework>
+    <AppendTargetFrameworkToOutputPath>false</AppendTargetFrameworkToOutputPath>
     <OutputType>Exe</OutputType>
     <RootNamespace>remapassemblyref</RootNamespace>
     <AssemblyName>remap-assembly-ref</AssemblyName>
+    <OutputPath>..\..\bin\Build$(Configuration)</OutputPath>
   </PropertyGroup>
+
   <Import Project="..\..\Configuration.props" />
-  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
-    <DebugSymbols>true</DebugSymbols>
-    <Optimize>false</Optimize>
-    <OutputPath>..\..\bin\BuildDebug</OutputPath>
-    <DefineConstants>DEBUG;</DefineConstants>
-    <ErrorReport>prompt</ErrorReport>
-    <WarningLevel>4</WarningLevel>
-    <ExternalConsole>true</ExternalConsole>
-  </PropertyGroup>
-  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
-    <Optimize>true</Optimize>
-    <OutputPath>..\..\bin\BuildRelease</OutputPath>
-    <ErrorReport>prompt</ErrorReport>
-    <WarningLevel>4</WarningLevel>
-    <ExternalConsole>true</ExternalConsole>
-  </PropertyGroup>
+
   <ItemGroup>
-    <Reference Include="System" />
     <Reference Include="Mono.Cecil">
       <HintPath>$(XamarinAndroidSourcePath)external\mono\sdks\out\android-bcl\monodroid_tools\Mono.Cecil.dll</HintPath>
     </Reference>
   </ItemGroup>
-  <ItemGroup>
-    <Compile Include="remap-assembly-ref.cs" />
-  </ItemGroup>
-  <Import Project="$(MSBuildBinPath)\Microsoft.CSharp.targets" />
+
 </Project>

build-tools/xaprepare/xaprepare/Application/Utilities.cs
@@ -99,10 +99,7 @@ namespace Xamarin.Android.Prepare
 			bool result = await msbuild.Run (
 				projectPath: projectPath,
 				logTag: "remap-assembly-ref",
-				arguments: new List<string> {
-					"/restore",
-				},
-				binlogName: "build-remap-assembly-ref"	
+				binlogName: "build-remap-assembly-ref"
 			);
 
 			if (!result) {

build-tools/xaprepare/xaprepare/Steps/Step_BuildMonoRuntimes.Unix.cs
@@ -88,9 +88,6 @@ namespace Xamarin.Android.Prepare
 			string projectPath = Path.Combine (Configurables.Paths.BuildToolsDir, "conjure-xamarin-android-cecil", "conjure-xamarin-android-cecil.csproj");
 			result = await msbuild.Run (
 				projectPath: projectPath,
-				arguments: new List<string> {
-					"/restore",
-				},
 				logTag: "conjure-xamarin-android-cecil",
 				binlogName: "build-conjure-xamarin-android-cecil"
 			);

build-tools/xaprepare/xaprepare/Steps/Step_InstallMonoRuntimes.cs
@@ -97,9 +97,6 @@ namespace Xamarin.Android.Prepare
 			result = await msbuild.Run (
 				projectPath: projectPath,
 				logTag: "conjure-xamarin-android-cecil",
-				arguments: new List<string> {
-					"/restore",
-				},
 				binlogName: "build-conjure-xamarin-android-cecil"
 			);
 

build-tools/xaprepare/xaprepare/ToolRunners/MSBuildRunner.cs
@@ -19,6 +19,7 @@ namespace Xamarin.Android.Prepare
 
 			StandardArguments = new List<string> {
 				$"/p:Configuration={context.Configuration}",
+				"/restore",
 			};
 		}
 
@@ -34,7 +35,6 @@ namespace Xamarin.Android.Prepare
 			AddArguments (runner, StandardArguments);
 			if (!String.IsNullOrEmpty (binlogName)) {
 				string logPath = Utilities.GetRelativePath (workingDirectory!, Path.Combine (Configurables.Paths.BuildBinDir, $"msbuild-{Context.BuildTimeStamp}-{binlogName}.binlog"));
-				runner.AddQuotedArgument ("/restore");
 				runner.AddArgument ("/v:normal");
 				runner.AddQuotedArgument ($"/bl:{logPath}");
 			}

commit b71434ee78735f3ce0a3b87905cd1145f13f3e3f

[remap-assembly-ref] use Mono.Cecil from NuGet
BCL Test phases are failing with:

    remap-assembly-ref.cs(3,12): error CS0234: The type or namespace name 'Cecil' does not exist in the namespace 'Mono' (are you missing an assembly reference?)

The problem being `Mono.Cecil.dll` does not actually exist:

    warning MSB3245: Could not resolve this reference. Could not locate the assembly "Mono.Cecil". Check to make sure the assembly exists on disk. If this reference is required by your code, you may get compilation errors.

This appears to have started failing since 49e570d8.

19c3b731 fixed the issue with needing `/restore`, but this is the next
issue...

Reviewing a build log from before 49e570d8, it *only* worked because
the build was pulling `Mono.Cecil.dll` from Mono's GAC:

    Task ResolveAssemblyReference
        Reference found at search path location "{GAC}".
        ...
        Considered "/Users/runner/work/1/s/external/mono/sdks/out/android-bcl/monodroid_tools/Mono.Cecil.dll", but it didn't exist.
        ...
    OutputItems
        ReferencePath
            /Library/Frameworks/Mono.framework/Versions/6.12.0/lib/mono/gac/Mono.Cecil/0.11.1.0__0738eb9f132ed756/Mono.Cecil.dll

Switching to `Microsoft.NET.Sdk` prevented the probing of the GAC,
because `$(AssemblySearchPaths)` does not contain `{GAC}` in the
dotnet/sdk:

https://github.com/dotnet/sdk/blob/ee455863aa6ddf13108bb54b37a5a47fb12fe39e/src/Tasks/Microsoft.NET.Build.Tasks/targets/Microsoft.NET.Sdk.props#L90-L91

To fix the problem, I think we should just use a `<PackageReference/>`
for Mono.Cecil in this tool.

I also updated `Step_DetermineAzurePipelinesTestJobs`, so changes to
`build-tools` will run all tests.

build-tools/remap-assembly-ref/remap-assembly-ref.csproj
@@ -12,9 +12,7 @@
   <Import Project="..\..\Configuration.props" />
 
   <ItemGroup>
-    <Reference Include="Mono.Cecil">
-      <HintPath>$(XamarinAndroidSourcePath)external\mono\sdks\out\android-bcl\monodroid_tools\Mono.Cecil.dll</HintPath>
-    </Reference>
+    <PackageReference Include="Mono.Cecil" Version="0.11.2" />
   </ItemGroup>
 
 </Project>

build-tools/xaprepare/xaprepare/Steps/Step_DetermineAzurePipelinesTestJobs.cs
@@ -48,17 +48,11 @@ namespace Xamarin.Android.Prepare
 				// BCL: Runs BCL tests on emulator
 				// TimeZone: Runs timezone unit tests on emulator
 				// Designer: Runs designer integration tests
-				if (file == ".external" || file == "Configuration.props") {
+				if (file == ".external" || file == "Configuration.props" || file.Contains ("build-tools/")) {
 					testAreas.Add ("MSBuild");
 					testAreas.Add ("MSBuildDevice");
 					testAreas.Add ("BCL");
 					testAreas.Add ("TimeZone");
-					testAreas.Add ("Designer");
-				}
-
-				if (file.Contains ("build-tools/installers")) {
-					testAreas.Add ("MSBuild");
-					testAreas.Add ("MSBuildDevice");
 					testAreas.Add ("Designer");
 				}
 