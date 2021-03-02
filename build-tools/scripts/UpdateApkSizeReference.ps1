9f02a11e31458de8b6ae944ff3da21754a1c792a
Radek Doulik <radekdoulik@gmail.com>
2021-03-02 12:44:54 +0100

    Update the script
    To run the BuildReleaseArm64 test with legacy and dotnet. Also
    copy the apkdesc files from new location to reference location.

--- build-tools/scripts/UpdateApkSizeReference.ps1
+++ build-tools/scripts/UpdateApkSizeReference.ps1
@@ -1,1 +1,8 @@
-Get-ChildItem -r -Filter Build*apkdesc .\bin\TestDebug\temp\BuildReleaseArm64* | Copy-Item -Destination src\Xamarin.Android.Build.Tasks\Tests\Xamarin.ProjectTools\Resources\Base\
+Write-Output "Building xabuild"
+msbuild /restore .\tools\xabuild\xabuild.csproj
+Write-Output "Building legacy BuildReleaseArm64 tests"
+msbuild Xamarin.Android.sln /t:RunNunitTests /p:TEST="Xamarin.Android.Build.Tests.BuildTest.BuildReleaseArm64"
+Write-Output "Building DotNet BuildReleaseArm64 tests"
+~\android-toolchain\dotnet\dotnet test -v diag --filter BuildTest.BuildReleaseArm64 .\bin\TestDebug\netcoreapp3.1\Xamarin.Android.Build.Tests.dll
+Write-Output "Updating reference files"
+Copy-Item -Verbose bin\TestDebug\BuildReleaseArm64*.apkdesc -Destination src\Xamarin.Android.Build.Tasks\Tests\Xamarin.ProjectTools\Resources\Base\
