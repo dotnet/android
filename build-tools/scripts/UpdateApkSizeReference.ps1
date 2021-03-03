2143aae462bf8f3dc2974db8541f82ec08a15556
Radek Doulik <radekdoulik@gmail.com>
2021-03-03 13:31:32 +0100

    Update the script
    And let it check for release XA build

--- build-tools/scripts/UpdateApkSizeReference.ps1
+++ build-tools/scripts/UpdateApkSizeReference.ps1
@@ -1,8 +1,15 @@
+# Run BuildReleaseArm64 tests and update the apkdesc reference files
+
+if (-not (Test-Path bin/Release)) {
+    Write-Output "bin/Release doesn't exists, please build XA in release configuration, before running this script"
+    exit 1
+}
+
 Write-Output "Building xabuild"
-msbuild /restore .\tools\xabuild\xabuild.csproj
+msbuild /p:Configuration=Release /restore .\tools\xabuild\xabuild.csproj
 Write-Output "Building legacy BuildReleaseArm64 tests"
-msbuild Xamarin.Android.sln /t:RunNunitTests /p:TEST="Xamarin.Android.Build.Tests.BuildTest.BuildReleaseArm64"
+msbuild /p:Configuration=Release Xamarin.Android.sln /t:RunNunitTests /p:TEST="Xamarin.Android.Build.Tests.BuildTest.BuildReleaseArm64"
 Write-Output "Building DotNet BuildReleaseArm64 tests"
-~\android-toolchain\dotnet\dotnet test -v diag --filter BuildTest.BuildReleaseArm64 .\bin\TestDebug\netcoreapp3.1\Xamarin.Android.Build.Tests.dll
+~\android-toolchain\dotnet\dotnet test -p:Configuration=Release --filter BuildTest.BuildReleaseArm64 .\bin\TestRelease\netcoreapp3.1\Xamarin.Android.Build.Tests.dll
 Write-Output "Updating reference files"
-Copy-Item -Verbose bin\TestDebug\BuildReleaseArm64*.apkdesc -Destination src\Xamarin.Android.Build.Tasks\Tests\Xamarin.ProjectTools\Resources\Base\
+Copy-Item -Verbose bin\TestRelease\BuildReleaseArm64*.apkdesc -Destination src\Xamarin.Android.Build.Tasks\Tests\Xamarin.ProjectTools\Resources\Base\
