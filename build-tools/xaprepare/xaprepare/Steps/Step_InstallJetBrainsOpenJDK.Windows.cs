dotnet/android

commit 60b54d5ae8f63b9dce0ea7e7db2e9a3b09e041fd

[xaprepare] Provision Microsoft OpenJDK 11 (#5746)
Context: https://aka.ms/getopenjdk
Context: https://devblogs.microsoft.com/java/

Begin provisioning and using Microsoft OpenJDK 11 for
integration test purposes.

In the process of testing Microsoft OpenJDK 11, we noticed a
behavioral change vs. JetBrains OpenJDK 11.0.7 (f8eb265c):
`jarsigner` would emit a warning on Windows about POSIX file
permissions on entries within `.apk` files:

	jarsigner.exe : warning ANDJS0000: POSIX file permission and/or symlink attributes detected. These attributes are ignored when signing and are not protected by the signature.

These POSIX file permissions can be viewed with `7z l -slt`:

	$ 7z l -slt bin/Debug/com.xamarin.android.helloworld.apk
	…
	Path = lib/arm64-v8a/libxamarin-debug-app-helper.so
	Folder = -
	Size = 195880
	Packed Size = 195880
	Modified = 2021-03-16 21:37:12
	Created = 
	Accessed = 
	Attributes = _ -rwxr-xr-x
	Encrypted = -
	Comment = 
	CRC = 33A81133
	Method = Store
	Host OS = Unix
	Version = 10
	Volume Index = 0
	…

Note the `Attributes = _ -rwxr-xr-x` line; this is what
Microsoft OpenJDK 11 is warning about.

We believe the POSIX file permissions are added by libZipSharp,
[setting a default Unix permission on Windows][0].

Since, we're unsure why this was done, let's downgrade the warning to
a message instead.  We already had an
`AndroidSignPackage.IgnoredWarnings` list we could add the message to
in the `<AndroidSignPackage/>` MSBuild task.

This allows all unit tests to pass.  (Unit tests were failing because
some asserted that no warnings were produced.)

Co-authored-by: Jonathan Peppers <jonathan.peppers@microsoft.com>

[0]: https://github.com/xamarin/LibZipSharp/blob/05f94a482fe9a791691d22951c3345ee67827b54/WindowsPlatformServices.cs#L199

build-tools/xaprepare/xaprepare/Steps/Step_InstallJetBrainsOpenJDK.Windows.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Xamarin.Android.Prepare
 {
-	partial class Step_InstallJetBrainsOpenJDK
+	partial class Step_InstallOpenJDK
 	{
 		async Task<bool> Unpack (string fullArchivePath, string destinationDirectory, bool cleanDestinationBeforeUnpacking = false)
 		{
@@ -17,10 +17,6 @@ namespace Xamarin.Android.Prepare
 				destinationDirectory = Path.Combine (destinationDirectory, RootDirName);
 			}
 
-			// On Windows we don't have Tar available and the Windows package is a .tar.gz
-			// 7zip can unpack tar.gz but it's a two-stage process - first it decompresses the package, then it can be
-			// invoked again to extract the actual tar contents.
-
 			if (cleanDestinationBeforeUnpacking)
 				Utilities.DeleteDirectorySilent (destinationDirectory);
 			Utilities.CreateDirectory (destinationDirectory);
@@ -29,15 +25,6 @@ namespace Xamarin.Android.Prepare
 			Log.DebugLine ($"Uncompressing {fullArchivePath} to {destinationDirectory}");
 			if (!await sevenZip.Extract (fullArchivePath, destinationDirectory)) {
 				Log.DebugLine ($"Failed to decompress {fullArchivePath}");
-				return false;
-			}
-
-			string tarPath = Path.Combine (destinationDirectory, Path.GetFileNameWithoutExtension (fullArchivePath));
-			bool ret = await sevenZip.Extract (tarPath, destinationDirectory);
-			Utilities.DeleteFileSilent (tarPath);
-
-			if (!ret) {
-				Log.DebugLine ($"Failed to extract TAR contents from {tarPath}");
 				return false;
 			}
 

build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.Linux.cs
@@ -5,6 +5,7 @@ namespace Xamarin.Android.Prepare
 	partial class Configurables
 	{
 		const string JetBrainsOpenJDKOperatingSystem = "linux-x64";
+		const string MicrosoftOpenJDKOperatingSystem = "linux/x64";
 
 		partial class Defaults
 		{

build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
@@ -5,6 +5,7 @@ namespace Xamarin.Android.Prepare
 	partial class Configurables
 	{
 		const string JetBrainsOpenJDKOperatingSystem = "osx-x64";
+		const string MicrosoftOpenJDKOperatingSystem = "macOS/x64";
 
 		partial class Defaults
 		{

build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.Unix.cs
@@ -5,6 +5,8 @@ namespace Xamarin.Android.Prepare
 {
 	partial class Configurables
 	{
+		const string MicrosoftOpenJDKFileExtension   = "tar.gz";
+
 		partial class Defaults
 		{
 			public const string DefaultCompiler = "cc";

build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.Windows.cs
@@ -6,6 +6,8 @@ namespace Xamarin.Android.Prepare
 	partial class Configurables
 	{
 		const string JetBrainsOpenJDKOperatingSystem = "windows-x64";
+		const string MicrosoftOpenJDKOperatingSystem = "windows/x64";
+		const string MicrosoftOpenJDKFileExtension   = "zip";
 
 		partial class Defaults
 		{

build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
@@ -15,9 +15,11 @@ namespace Xamarin.Android.Prepare
 	//
 	partial class Configurables
 	{
-		const string JetBrainsOpenJDK11Version = "11.0.7";
-		const string JetBrainsOpenJDK11Release = "944.14";
-		static readonly string JetBrainsOpenJDK11DownloadVersion = JetBrainsOpenJDK11Version.Replace ('.', '_');
+		const string MicrosoftOpenJDK11Version      = "11.0.10";
+		const string MicrosoftOpenJDK11Release      = "9.1";
+		const string MicrosoftOpenJDK11RootDirName  = "jdk-11.0.10+9";
+		const string MicrosoftOpenJDK11DirVersion   = MicrosoftOpenJDK11RootDirName + ".1";
+		const string MicrosoftOpenJDK11FileVersion  = "11.0.10.9";
 
 		const string JetBrainsOpenJDK8VersionMinor = "8";
 		const string JetBrainsOpenJDK8VersionRevision = "202";
@@ -37,10 +39,10 @@ namespace Xamarin.Android.Prepare
 			// https://bintray.com/jetbrains/intellij-jdk/download_file?file_path=jbrsdk-8u202-windows-x64-b1483.37.tar.gz
 			public static readonly Uri JetBrainsOpenJDK8 = new Uri ($"https://bintray.com/jetbrains/intellij-jdk/download_file?file_path=jbrsdk-{JetBrainsOpenJDK8DownloadVersion}-{JetBrainsOpenJDKOperatingSystem}-b{JetBrainsOpenJDK8Release}.tar.gz");
 
-			// https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-11_0_7-linux-x64-b944.14.tar.gz
-			// https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-11_0_7-osx-x64-b944.14.tar.gz
-			// https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-11_0_7-windows-x64-b944.14.tar.gz
-			public static readonly Uri JetBrainsOpenJDK11 = new Uri ($"https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbrsdk-{JetBrainsOpenJDK11DownloadVersion}-{JetBrainsOpenJDKOperatingSystem}-b{JetBrainsOpenJDK11Release}.tar.gz");
+			// https://msopenjdk.azureedge.net/jdk11u/jdk-11.0.10+9.1/linux/x64/microsoft-jdk-11.0.10.9-linux-x64.tar.gz
+			// https://msopenjdk.azureedge.net/jdk11u/jdk-11.0.10+9.1/macOS/x64/microsoft-jdk-11.0.10.9-macOS-x64.tar.gz
+			// https://msopenjdk.azureedge.net/jdk11u/jdk-11.0.10+9.1/windows/x64/microsoft-jdk-11.0.10.9-windows-x64.zip
+			public static readonly Uri MicrosoftOpenJDK11 = new Uri ($"https://msopenjdk.azureedge.net/jdk11u/{MicrosoftOpenJDK11DirVersion}/{MicrosoftOpenJDKOperatingSystem}/microsoft-jdk-{MicrosoftOpenJDK11FileVersion}-{MicrosoftOpenJDKOperatingSystem.Replace ('/', '-')}.{MicrosoftOpenJDKFileExtension}");
 
 			/// <summary>
 			///   Base URL for all Android SDK and NDK downloads. Used in <see cref="AndroidToolchain"/>
@@ -58,8 +60,9 @@ namespace Xamarin.Android.Prepare
 
 			public static readonly string JdkFolder                        = "jdk-11";
 
-			public static readonly Version JetBrainsOpenJDK11Version = new Version (Configurables.JetBrainsOpenJDK11Version);
-			public static readonly Version JetBrainsOpenJDK11Release = new Version (Configurables.JetBrainsOpenJDK11Release);
+			public static readonly Version MicrosoftOpenJDK11Version = new Version (Configurables.MicrosoftOpenJDK11Version);
+			public static readonly Version MicrosoftOpenJDK11Release = new Version (Configurables.MicrosoftOpenJDK11Release);
+			public static readonly string  MicrosoftOpenJDK11RootDirName   = Configurables.MicrosoftOpenJDK11RootDirName;
 
 			public static readonly Version JetBrainsOpenJDK8Version = new Version (Configurables.JetBrainsOpenJDK8VersionFull);
 			public static readonly Version JetBrainsOpenJDK8Release = new Version (Configurables.JetBrainsOpenJDK8Release);

build-tools/xaprepare/xaprepare/Scenarios/Scenario_AndroidTestDependencies.cs
@@ -13,7 +13,7 @@ namespace Xamarin.Android.Prepare
 		{
 			Steps.Add (new Step_InstallDotNetPreview ());
 			Steps.Add (new Step_InstallJetBrainsOpenJDK8 ());
-			Steps.Add (new Step_InstallJetBrainsOpenJDK11 ());
+			Steps.Add (new Step_InstallMicrosoftOpenJDK11 ());
 			Steps.Add (new Step_Android_SDK_NDK (AndroidToolchainComponentType.CoreDependency));
 
 			// disable installation of missing programs...

build-tools/xaprepare/xaprepare/Scenarios/Scenario_Standard.cs
@@ -21,7 +21,7 @@ namespace Xamarin.Android.Prepare
 			Steps.Add (new Step_ShowEnabledRuntimes ());
 			Steps.Add (new Step_InstallDotNetPreview ());
 			Steps.Add (new Step_InstallJetBrainsOpenJDK8 ());
-			Steps.Add (new Step_InstallJetBrainsOpenJDK11 ());
+			Steps.Add (new Step_InstallMicrosoftOpenJDK11 ());
 			Steps.Add (new Step_Android_SDK_NDK ());
 			Steps.Add (new Step_GenerateFiles (atBuildStart: true));
 			Steps.Add (new Step_PrepareProps ());

commit 6e55770efba4b1d00773717e355f21a602df0275

[xaprepare] Fix JetBrains JDK 1.8 provisioning on Windows
Context: https://devdiv.visualstudio.com/DevDiv/_build/results?buildId=4605864&view=logs&jobId=fb3518d4-6415-56d8-56e7-3e01113bd696&j=fb3518d4-6415-56d8-56e7-3e01113bd696&t=cdc044ef-b869-5235-56df-ac4b6018a01e
Context: https://discord.com/channels/732297728826277939/732297837953679412/826969920184778782
Context: https://github.com/xamarin/xamarin-android/commit/60b54d5ae8f63b9dce0ea7e7db2e9a3b09e041fd#diff-57d44829988dde5ea93a4c3f65fa77d5b04bd82c1ac87c4b300c0316f94f85a3L20

After 60b54d5a was merged, occasionally we would see the
**Legacy Tests** > **MSBuild Legacy - Windows** jobs fail in the
`nuget restore Xamarin.Android-Tests.sln` task:

	##[error]The nuget command failed with exit code(1) and error(C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Xamarin\Android\Xamarin.Android.Tooling.targets(64,5):
	warning XA5300: An exception occurred while validating the Java SDK installation in 'C:\Users\AzDevOps\android-toolchain\jdk-1.8' that was found while searching the paths from '$JI_JAVA_HOME'.
	  Ensure that the Android section of the Visual Studio options has a valid Java SDK directory configured.
	  To use a custom SDK path for a command line build, set the 'JavaSdkDirectory' MSBuild property to the custom path.
	  Exception: Could not find required file `jar` within `C:\Users\AzDevOps\android-toolchain\jdk-1.8`; is this a valid JDK?
	C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Xamarin\Android\Xamarin.Android.Tooling.targets(64,5):
	error XA5300: The Java SDK directory could not be found.
	  Ensure that the Android section of the Visual Studio options has a valid Java SDK directory configured.
	  To use a custom SDK path for a command line build, set the 'JavaSdkDirectory' MSBuild property to the custom path.
	  [C:\a\1\s\tests\TestRunner.Core\TestRunner.Core.csproj]

Subsequent investigation in PR #5805 found the culprit:

	    Directory: C:\Users\AzDevOps\android-toolchain\jdk-1.8
	Mode                LastWriteTime         Length Name
	----                -------------         ------ ----
	-a----         3/6/2019  10:40 AM      268267520 jbrsdk-8u202-windows-x64-b1483.37.tar
	-a----        3/31/2021  10:12 PM              9 xa_jdk_version.txt

We provisioned JetBrains JDK 1.8, but we didn't *finish* provisioning
JDK 1.8!

The problem was introduced in commit 60b54d5a: Microsoft OpenJDK 11

build-tools/xaprepare/xaprepare/Steps/Step_InstallJetBrainsOpenJDK.Windows.cs
@@ -28,6 +28,20 @@ namespace Xamarin.Android.Prepare
 				return false;
 			}
 
+			if (fullArchivePath.EndsWith ("tar.gz", StringComparision.OrdinalIgnoreCase)) {
+				// On Windows we don't have Tar available and the Windows package is a .tar.gz
+				// 7zip can unpack tar.gz but it's a two-stage process - first it decompresses the package, then it can be
+				// invoked again to extract the actual tar contents.
+				string tarPath = Path.Combine (destinationDirectory, Path.GetFileNameWithoutExtension (fullArchivePath));
+				bool ret = await sevenZip.Extract (tarPath, destinationDirectory);
+				Utilities.DeleteFileSilent (tarPath);
+
+				if (!ret) {
+					Log.DebugLine ($"Failed to extract TAR contents from {tarPath}");
+					return false;
+				}
+			}
+
 			return true;
 		}
 