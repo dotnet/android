dotnet/android

commit ae1b72f7e78277f32b4b9d7e8be82f44fbaa5bec

[Java.Interop.Tools.JavaCallableWrappers] Include interfaces in type maps (#824)
Context: a87a90df2ef276eaad55ca8e9048112ca873a2fa
Context: https://github.com/xamarin/xamarin-android/pull/5859

Java typemaps (a833ac42) have never included interfaces because,
until very recently (a87a90df), they couldn't contain methods, and
thus they couldn't have been used as method invocation targets.  With
the addition of Default Interface Methods, this is no longer true.

The setup:

	// 62e113ff, afb3604bf, …
	namespace Java.Interop {
	  partial class JniPeerMembers {
	    JniPeerMembers (string jniPeerType, Type managedPeerType, bool checkManagedPeerType)
	    {
	      …
	      Debug.Assert (
	          JniEnvironment.Runtime.TypeManager.GetTypeSignature (managedPeerType).JniTypeName == jniPeerType
	          $"ManagedPeerType <=> JniTypeName Mismatch! javaVM.GetJniTypeInfoForType(typeof({managedPeerType.FullName})).JniTypeName=\"{JniEnvironment.Current.JavaVM.GetJniTypeInfoForType (managedPeerType).JniTypeName}\" != \"{jniPeerType}\"
	    }
	  }
	}

	// generated in xamarin-android
	namespace Android.App {
	  public partial class Application {
	    public partial interface IActivityLifecycleCallbacks : IJavaObject, IJavaPeerable {
	      private static readonly JniPeerMembers _members = new JniPeerMembers ("android/app/Application$ActivityLifecycleCallbacks", typeof (IActivityLifecycleCallbacks), isInterface: true);
	    }
	  }
	}

	// https://github.com/dotnet/maui/blob/18e0f4ebbcca7c904ccdb67e6439cb72382e2a40/src/Core/src/Platform/Android/MauiApplication.cs#L56-L79
	namespace Microsoft.Maui {
	  public partial class MauiApplication : Application {
	    public class ActivityLifecycleCallbacks : Java.Lang.Object, IActivityLifecycleCallbacks {
	      // …
	    }
	  }
	}

The punch; at runtime, with .NET 6, when using a *Debug* build of
`Java.Interop.dll`, the app crashes with an assertion failure when

external/Java.Interop/src/Java.Interop.Tools.JavaCallableWrappers/Java.Interop.Tools.JavaCallableWrappers/JavaTypeScanner.cs
@@ -50,11 +50,12 @@ namespace Java.Interop.Tools.JavaCallableWrappers
 
 		void AddJavaTypes (List<TypeDefinition> javaTypes, TypeDefinition type)
 		{
-			if (type.IsSubclassOf ("Java.Lang.Object", cache) || type.IsSubclassOf ("Java.Lang.Throwable", cache)) {
-
+			if (type.IsSubclassOf ("Java.Lang.Object", cache) ||
+			    		type.IsSubclassOf ("Java.Lang.Throwable", cache) ||
+					(type.IsInterface && type.ImplementsInterface ("Java.Interop.IJavaPeerable", cache))) {
 				// For subclasses of e.g. Android.App.Activity.
 				javaTypes.Add (type);
-			} else if (type.IsClass && !type.IsSubclassOf ("System.Exception", cache) && type.ImplementsInterface ("Android.Runtime.IJavaObject", cache)) {
+			} else if (type.IsClass && !type.IsSubclassOf ("System.Exception", cache) && type.ImplementsInterface ("Android.Runtime.IJa", cache)) {
 				var level   = ErrorOnCustomJavaObject ? TraceLevel.Error : TraceLevel.Warning;
 				var prefix  = ErrorOnCustomJavaObject ? "error" : "warning";
 				Logger (

commit 3e8213f8f5e16842f97d438be8b7777157b26bd5

[Java.Interop.Tools.JavaCallableWrappers] Fix typo.
Fix typo introduced in commit ae1b72f7.

Oops.

external/Java.Interop/src/Java.Interop.Tools.JavaCallableWrappers/Java.Interop.Tools.JavaCallableWrappers/JavaTypeScanner.cs
@@ -51,11 +51,11 @@ namespace Java.Interop.Tools.JavaCallableWrappers
 		void AddJavaTypes (List<TypeDefinition> javaTypes, TypeDefinition type)
 		{
 			if (type.IsSubclassOf ("Java.Lang.Object", cache) ||
-			    		type.IsSubclassOf ("Java.Lang.Throwable", cache) ||
+					type.IsSubclassOf ("Java.Lang.Throwable", cache) ||
 					(type.IsInterface && type.ImplementsInterface ("Java.Interop.IJavaPeerable", cache))) {
 				// For subclasses of e.g. Android.App.Activity.
 				javaTypes.Add (type);
-			} else if (type.IsClass && !type.IsSubclassOf ("System.Exception", cache) && type.ImplementsInterface ("Android.Runtime.IJa", cache)) {
+			} else if (type.IsClass && !type.IsSubclassOf ("System.Exception", cache) && type.ImplementsInterface ("Android.Runtime.IJavaObject", cache)) {
 				var level   = ErrorOnCustomJavaObject ? TraceLevel.Error : TraceLevel.Warning;
 				var prefix  = ErrorOnCustomJavaObject ? "error" : "warning";
 				Logger (