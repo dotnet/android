070f4202a621af41875543f37e38d9c06239b7ff
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-05-28 12:25:50 -0500

    [tests] fix CustomLinkDescriptionPreserve test
    Context: https://www.nuget.org/packages/SQLitePCLRaw.bundle_green/2.0.3
    
    The `CustomLinkDescriptionPreserve` test builds an Android application
    that references a `netstandard2.0` library. The `netstandard2.0`
    library has a dependency on `sqlite-net-pcl`.
    
    One improvement here is now `SQLitePCLRaw.lib.e_sqlite3.android` is
    brought in correctly. Unfortunately we get a crash at runtime:
    
        System.TypeInitializationException: TypeInitialization_Type, SQLite.SQLiteConnection
        ---> System.IO.FileNotFoundException: Could not load file or assembly 'SQLitePCLRaw.provider.dynamic_cdecl, Version=2.0.3.851, Culture=neutral, PublicKeyToken=b68184102cba0b3b' or one of its dependencies.
        IO_FileName_Name, SQLitePCLRaw.provider.dynamic_cdecl, Version=2.0.3.851, Culture=neutral, PublicKeyToken=b68184102cba0b3b
        at SQLitePCL.Batteries_V2.Init() in SQLitePCLRaw.batteries_v2.dll:token 0x6000004+0x0
        at SQLite.SQLiteConnection..cctor() in SQLite-net.dll:token 0x6000020+0x16
    
    This causes some *odd* NuGet behavior in that `netstandard2.0`,
    `netcoreapp3.0`, and `monoandroid8.0` dependencies are brought in:
    
        Task ResolvePackageAssets
        ...
        RuntimeCopyLocalItems
            ~\.nuget\packages\sqlite-net-pcl\1.7.335\lib\netstandard2.0\SQLite-net.dll
            ~\.nuget\packages\sqlitepclraw.bundle_green\2.0.3\lib\netcoreapp3.0\SQLitePCLRaw.batteries_v2.dll
            ~\.nuget\packages\sqlitepclraw.bundle_green\2.0.3\lib\netcoreapp3.0\SQLitePCLRaw.nativelibrary.dll
            ~\.nuget\packages\sqlitepclraw.core\2.0.3\lib\netstandard2.0\SQLitePCLRaw.core.dll
            ~\.nuget\packages\sqlitepclraw.lib.e_sqlite3.android\2.0.3\lib\monoandroid80\SQLitePCLRaw.lib.e_sqlite3.android.dll
            ~\.nuget\packages\sqlitepclraw.provider.e_sqlite3\2.0.3\lib\netstandard2.0\SQLitePCLRaw.provider.e_sqlite3.dll
    
    And yet we're missing `SQLitePCLRaw.provider.dynamic_cdecl`?
    
    I don't quite understand what's happening here, due to how complicated
    the dependency graph is here. For now changing the workaround to
    include `SQLitePCLRaw.provider.dynamic_cdecl` instead of
    `SQLitePCLRaw.lib.e_sqlite3.android` is sufficient to fix this test.

--- tests/MSBuildDeviceIntegration/Tests/InstallAndRunTests.cs
+++ tests/MSBuildDeviceIntegration/Tests/InstallAndRunTests.cs
@@ -378,9 +378,10 @@ namespace Library1 {
 				},
 			};
 			if (Builder.UseDotNet) {
-				// NOTE: workaround for netcoreapp3.1 dependency preferred over monoandroid8.0
+				// NOTE: workaround for netcoreapp3.0 dependency being included along with monoandroid8.0
+				// See: https://www.nuget.org/packages/SQLitePCLRaw.bundle_green/2.0.3
 				proj.PackageReferences.Add (new Package {
-					Id = "SQLitePCLRaw.lib.e_sqlite3.android",
+					Id = "SQLitePCLRaw.provider.dynamic_cdecl",
 					Version = "2.0.4",
 				});
 			}
