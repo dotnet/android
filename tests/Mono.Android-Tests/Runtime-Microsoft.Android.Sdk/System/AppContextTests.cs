ab244d54aa5b0f30cd52a7a0614b3715a9ffe347
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-06-11 08:16:05 -0500

    Add runtime tests for AppContext

---
+++ tests/Mono.Android-Tests/Runtime-Microsoft.Android.Sdk/System/AppContextTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using System;
+
+namespace SystemTests
+{
+	[TestFixture]
+	public class AppContextTests
+	{
+		static readonly object [] GetDataSource = new object [] {
+			new object [] {
+				/* name */     "test_bool",
+				/* expected */ true,
+			},
+			new object [] {
+				/* name */     "test_integer",
+				/* expected */ 42,
+			},
+			new object [] {
+				/* name */     "test_string",
+				/* expected */ "foo",
+			},
+		};
+
+		[Test]
+		[TestCaseSource (nameof (GetDataSource))]
+		public void GetData (string name, object expected)
+		{
+			Assert.AreEqual (expected, AppContext.GetData (name));
+		}
+	}
+}
