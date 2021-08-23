9d58fad166391683d8c49407833d20ea4479361f
Jonathan Pobst <jonathan.pobst@microsoft.com>
2021-08-23 16:03:16 -0500

    [Mono.Android] API-31 stabilization (#6174)
    Context: https://developer.android.com/about/versions/12
    
    API-31 has been enumified (88438081, cf2a39b2, cf2a39b2).
    Time to declare it stable!
    
    Set API-S as API-31, `$(TargetFrameworkVersion)`=v12.0,
    `$(AndroidPackVersion)`=31.0.100, and declare as stable.
    
    Update unit test files so `$(TargetFrameworkVersion)`= v12.0 (API-31)
    *or* `$(TargetFrameworkVersion)` isn't needed, a'la 8140991e.
    
    Rename `src/Mono.Android/Profiles/api-S.params.txt` to
    `api-31.params.txt`, to match the stable API-level ID.
    
    Updated `tests/api-compatibility/acceptable-breakages-vReference.txt`
    because the default API compare will now be against `API-31`, not
    `API-30`, and we deliberately removed the `[Category]` custom attribute
    (e5a4beef), remove `[DataContractAttribute]` (101fea2c), and fix the
    value of `Android.OS.BuildVersionCodes.R` (1b1ec097).
    
    Support `$(AndroidUseLatestPlatformSdk)` in binding projects: once
    API-31 is stable, binding projects with
    `$(TargetFrameworkVersion)`=v11.0 and
    `$(AndroidUseLatestPlatformSdk)`=true fail to build with:
    
    	Task ResolveNuGetPackageAssets
    	...
    	Microsoft.NuGet.targets(198,5): Your project does not reference "MonoAndroid,Version=v12.0" framework. Add a reference to "MonoAndroid,Version=v12.0" in the "TargetFrameworks" property of your project file and then re-run NuGet restore.
    
    What happened is that `$(TargetFrameworkVersion)` was changed to 12.0
    during `Build`, but not during `Restore`. Thus NuGet restored with
    `MonoAndroid,Version=v11.0` instead of `MonoAndroid,Version=v12.0`.
    
    I found that we have this setup for other project types, but not
    binding projects:
    
    	<!--
    	  NOTE:
    	  This target runs during Restore, and there is no $(RestoreDependsOn) property.
    	  There appears to be no other way to do this other than use BeforeTargets.
    	-->
    	<Target Name="_SetLatestTargetFrameworkVersionForPackageReference"
    	    Condition=" '$(AndroidUseLatestPlatformSdk)' == 'True' "
    	    BeforeTargets="_GetRestoreTargetFrameworksOutput"
    	    DependsOnTargets="_SetLatestTargetFrameworkVersion">
    	</Target>
    
    If we move this target to `Xamarin.Android.Legacy.targets`, it will be
    imported by binding projects.
    
    We also get the added benefit of it not being in .NET 6.
    `$(AndroidUseLatestPlatformSdk)` is not supported in .NET 6, so we
    should not have this MSBuild target there.
    
    Co-authored-by: Jonathan Peppers <jonathan.peppers@microsoft.com>

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/XASdkTests.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/XASdkTests.cs
@@ -9,11 +9,12 @@ using Xamarin.ProjectTools;
 using Xamarin.Tools.Zip;
 using Microsoft.Android.Build.Tasks;
 
+#if !NET472
 namespace Xamarin.Android.Build.Tests
 {
 	[TestFixture]
 	[NonParallelizable] // On MacOS, parallel /restore causes issues
-	[Category ("Node-2"), Category ("DotNetIgnore")] // These don't need to run under `dotnet test`
+	[Category ("Node-2")]
 	public class XASdkTests : BaseTest
 	{
 		/// <summary>
@@ -207,7 +208,7 @@ namespace Xamarin.Android.Build.Tests
 		}
 
 		[Test]
-		public void DotNetPack ([Values ("net6.0-android", "net6.0-android30")] string targetFramework)
+		public void DotNetPack ([Values ("net6.0-android", "net6.0-android31")] string targetFramework)
 		{
 			var proj = new XASdkProject (outputType: "Library") {
 				TargetFramework = targetFramework,
@@ -239,8 +240,8 @@ namespace Xamarin.Android.Build.Tests
 			var nupkgPath = Path.Combine (FullProjectDirectory, proj.OutputPath, "..", $"{proj.ProjectName}.1.0.0.nupkg");
 			FileAssert.Exists (nupkgPath);
 			using (var nupkg = ZipHelper.OpenZip (nupkgPath)) {
-				nupkg.AssertContainsEntry (nupkgPath, $"lib/net6.0-android30.0/{proj.ProjectName}.dll");
-				nupkg.AssertContainsEntry (nupkgPath, $"lib/net6.0-android30.0/{proj.ProjectName}.aar");
+				nupkg.AssertContainsEntry (nupkgPath, $"lib/net6.0-android31.0/{proj.ProjectName}.dll");
+				nupkg.AssertContainsEntry (nupkgPath, $"lib/net6.0-android31.0/{proj.ProjectName}.aar");
 			}
 		}
 
@@ -423,7 +424,7 @@ namespace Xamarin.Android.Build.Tests
 				$"{proj.ProjectName}.pdb",
 				$"{proj.ProjectName}.xml",
 			};
-			CollectionAssert.AreEqual (expectedFiles, files, $"Expected: {string.Join (";", expectedFiles)}\n   Found: {string.Join (";", files)}");
+			CollectionAssert.AreEquivalent (expectedFiles, files, $"Expected: {string.Join (";", expectedFiles)}\n   Found: {string.Join (";", files)}");
 
 			var assemblyPath = Path.Combine (outputPath, $"{proj.ProjectName}.dll");
 			FileAssert.Exists (assemblyPath);
@@ -450,7 +451,7 @@ namespace Xamarin.Android.Build.Tests
 			XNamespace ns = "http://schemas.android.com/apk/res/android";
 			var uses_sdk = manifest.Root.Element ("uses-sdk");
 			Assert.AreEqual ("21", uses_sdk.Attribute (ns + "minSdkVersion").Value);
-			Assert.AreEqual ("30", uses_sdk.Attribute (ns + "targetSdkVersion").Value);
+			Assert.AreEqual ("31", uses_sdk.Attribute (ns + "targetSdkVersion").Value);
 
 			bool expectEmbeddedAssembies = !(CommercialBuildAvailable && !isRelease);
 			var apkPath = Path.Combine (outputPath, $"{proj.PackageName}.apk");
@@ -472,6 +473,11 @@ namespace Xamarin.Android.Build.Tests
 			}
 		}
 
+
+		/// <summary>
+		/// NOTE: we cannot use SupportedOSPlatformVersion=31 yet, due to d8 2.2.64 emitting a warning for `--min-api 31`:
+		///       D8 An API level of 31 is not supported by this compiler. Please use an API level of 30 or earlier
+		/// </summary>
 		[Test]
 		public void SupportedOSPlatformVersion ([Values (21, 30)] int minSdkVersion)
 		{
@@ -584,7 +590,7 @@ namespace Xamarin.Android.Build.Tests
 			};
 
 			using var b = new Builder ();
-			var dotnetTargetFramework = "net6.0-android30.0";
+			var dotnetTargetFramework = "net6.0-android31.0";
 			var legacyTargetFrameworkVersion = "11.0";
 			var legacyTargetFramework = $"monoandroid{legacyTargetFrameworkVersion}";
 			proj.SetProperty ("TargetFramework",  value: "");
@@ -601,7 +607,7 @@ namespace Xamarin.Android.Build.Tests
 		}
 
 		[Test]
-		public void MauiTargetFramework ([Values ("net6.0-android", "net6.0-android30", "net6.0-android30.0")] string targetFramework)
+		public void MauiTargetFramework ([Values ("net6.0-android", "net6.0-android31", "net6.0-android31.0")] string targetFramework)
 		{
 			var library = new XASdkProject (outputType: "Library") {
 				TargetFramework = targetFramework,
@@ -705,3 +711,4 @@ public abstract class Foo<TVirtualView, TNativeView> : AbstractViewHandler<TVirt
 		}
 	}
 }
+#endif
