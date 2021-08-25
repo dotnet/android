31704e3b0d05b2b7fa5814c3ae3c209c8d5d5a1a
Dean Ellis <dellis1972@googlemail.com>
2021-08-25 20:16:53 +0100

    Add verbose output

--- build-tools/xaprepare/xaprepare/Steps/Step_PrepareExternalJavaInterop.Windows.cs
+++ build-tools/xaprepare/xaprepare/Steps/Step_PrepareExternalJavaInterop.Windows.cs
@@ -9,6 +9,19 @@ namespace Xamarin.Android.Prepare
 #pragma warning disable CS1998
 		async Task<bool> ExecuteOSSpecific (Context context)
 		{
+			string javaInteropDir = context.Properties.GetRequiredValue (KnownProperties.JavaInteropFullPath);
+			string projectPath = Path.Combine (javaInteropDir, "build-tools", "Java.Interop.BootstrapTasks", "Java.Interop.BootstrapTasks.csproj");
+			var msbuild = new MSBuildRunner (context);
+			bool result = await msbuild.Run (
+				projectPath: projectPath,
+				logTag: "java-interop-prepare",
+				binlogName: "build-java-interop-prepare"
+			);
+
+			if (!result) {
+				Log.ErrorLine ("Failed to build java-interop-prepare");
+				return false;
+			}
 			return true;
 		}
 #pragma warning restore CS1998
