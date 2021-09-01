dotnet/android

commit 658db25e44fec14c93568dee2cef15f1e5f9ec33

[Xamarin.Android.Build.Tasks] Improve XA0031 error message (#6252)
Context: https://aka.ms/vs2019-and-jdk-11
Context: https://github.com/xamarin/xamarin-android/wiki/JDK-11-Warning

The XA0031 error will be generated when
`$(TargetFrameworkVersion)`=v12.0 and JDK 1.8 is detected, as JDK-11
is required for API-31; see also 8140991e, 26770284.

The problem is the XA0031 error message:

	error XA0031: Java SDK 11.0 or above is required when using $(TargetFrameworkVersion) v12.0.

The error message implies that error can be fixed by installing JDK-11.

Which is correct: the XA0031 error *can* be fixed by installing JDK-11.

The problem is that *once JDK-11 is installed*, the Android Designer
breaks, as the Android Designer, as shipped in Visual Studio 16.11 and
Visual Studio for Mac 8.10, only works with JDK 1.8.

This can easily result in a scenario where a customer wants to use
API-31, installs JDK-11, and then can no longer use the Designer.
(This could be mentioned in the Release Notes, but historically nobody
reads the release notes.)

Update the XA0031 error message so that this implicit dependency is
made explicit:

	error XA0031: Java SDK 11.0 or above is required when using $(TargetFrameworkVersion) v12.0.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11

<https://aka.ms/vs2019-and-jdk-11> provides more details.

src/Xamarin.Android.Build.Tasks/Properties/Resources.resx
@@ -178,7 +178,7 @@ In this message, the phrase "should not be reached" means that this error messag
     <comment>The abbreviation "JDK" should not be translated.</comment>
   </data>
   <data name="XA0031" xml:space="preserve">
-    <value>Java SDK {0} or above is required when using {1}.</value>
+    <value>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</value>
     <comment>
 {0} - The Java SDK version number
 {1} - The feature being used: `$(TargetFrameworkVersion) v10.0`</comment>

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.cs.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">Když se používá {1}, vyžaduje se sada Java SDK {0} nebo novější.</target>
         <note>
 {0} - The Java SDK version number

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.de.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">Bei Verwendung von {1} ist Java SDK {0} oder höher erforderlich.</target>
         <note>
 {0} - The Java SDK version number

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.es.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">Se requiere el SDK de Java {0} o posterior cuando se use {1}.</target>
         <note>
 {0} - The Java SDK version number

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.fr.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">Le SDK Java {0} ou supérieur est nécessaire pour utiliser {1}.</target>
         <note>
 {0} - The Java SDK version number

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.it.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">Quando si usa {1}, è richiesto Java SDK {0} o versione successiva.</target>
         <note>
 {0} - The Java SDK version number

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.ja.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">{1} を使用するときには、Java SDK {0} 以上が必要です。</target>
         <note>
 {0} - The Java SDK version number

src/Xamarin.Android.Build.Tasks/Properties/xlf/Resources.ko.xlf
@@ -162,7 +162,7 @@ In this message, the phrase "should not be reached" means that this error messag
         <note>The abbreviation "JDK" should not be translated.</note>
       </trans-unit>
       <trans-unit id="XA0031">
-        <source>Java SDK {0} or above is required when using {1}.</source>
+        <source>Java SDK {0} or above is required when using {1}.  Note: the Android Designer is incompatible with Java SDK 11.0: https://aka.ms/vs2019-and-jdk-11</source>
         <target state="needs-review-translation">{1}을(를) 사용하는 경우 Java SDK {0} 이상이 필요합니다.</target>
         <note>
 {0} - The Java SDK version number

commit 226c231eeaf8619863ffe7414b7cf0f077839b52

[tests] Fix TargetFramework8_1_Requires_1_8_0
Commit 658db25e inadvertently broke the
`ValidateJavaVersionTests.TargetFramework8_1_Requires_1_8_0()`
and `ValidateJavaVersionTests.BuildTools27_Requires_1_8_0()` tests,
by updating the XA0031 error message text.  Because these tests use
string.operator==() to compare the string contents, updating the
XA0031 message text broke the tests.

(This also means that the tests will fail if/when localizations are
ever actually used… 🤔)

Update these tests to instead use `string.StartsWith()`, as the *end*
of the XA0031 message was changed, not the beginning.

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/ValidateJavaVersionTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Build.Framework;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -54,7 +55,9 @@ namespace Xamarin.Android.Build.Tests
 				MinimumSupportedJavaVersion = "1.7.0",
 			};
 			Assert.False (validateJavaVersion.Execute (), "Execute should *not* succeed!");
-			Assert.IsTrue (errors.Any (e => e.Message == $"Java SDK 1.8 or above is required when using $(TargetFrameworkVersion) {validateJavaVersion.TargetFrameworkVersion}."), "Should get error about TargetFrameworkVersion=v8.1");
+			Assert.IsTrue (
+					errors.Any (e => e.Message.StartsWith ($"Java SDK 1.8 or above is required when using $(TargetFrameworkVersion) {validateJavaVersion.TargetFrameworkVersion}.", StringComparison.OrdinalIgnoreCase)),
+					"Should get error about TargetFrameworkVersion=v8.1");
 		}
 
 		[Test]
@@ -71,7 +74,9 @@ namespace Xamarin.Android.Build.Tests
 				MinimumSupportedJavaVersion = "1.7.0",
 			};
 			Assert.False (validateJavaVersion.Execute (), "Execute should *not* succeed!");
-			Assert.IsTrue (errors.Any (e => e.Message == $"Java SDK 1.8 or above is required when using Android SDK Build-Tools {validateJavaVersion.AndroidSdkBuildToolsVersion}."), "Should get error about build-tools=27.0.0");
+			Assert.IsTrue (
+					errors.Any (e => e.Message.StartsWith ($"Java SDK 1.8 or above is required when using Android SDK Build-Tools {validateJavaVersion.AndroidSdkBuildToolsVersion}.", StringComparison.OrdinalIgnoreCase)),
+					"Should get error about build-tools=27.0.0");
 		}
 
 		[Test]