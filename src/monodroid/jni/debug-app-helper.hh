9030cf32e8e440e14564397e5146e7000172028b
Marek Habersack <grendel@twistedcode.net>
2021-09-20 22:20:09 +0200

    [WIP] Fix DebugRuntime.init signature

--- src/monodroid/jni/debug-app-helper.hh
+++ src/monodroid/jni/debug-app-helper.hh
@@ -12,6 +12,6 @@ extern "C" {
 	 * Signature: ([Ljava/lang/String;Ljava/lang/String;[Ljava/lang/String);[Ljava/lang/String);Ljava/lang/String;IZ)V
 	 */
 	JNIEXPORT void JNICALL Java_mono_android_DebugRuntime_init
-	(JNIEnv *, jclass, jobjectArray, jstring, jobjectArray);
+	(JNIEnv *, jclass, jobjectArray, jstring, jobjectArray, jboolean);
 }
 #endif // _Included_mono_android_DebugRuntime
