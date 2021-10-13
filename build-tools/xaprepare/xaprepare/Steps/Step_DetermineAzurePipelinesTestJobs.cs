777990de49e7638a0b320133d8d74c883bc892b4
Peter Collins <pecolli@microsoft.com>
2021-10-13 12:28:38 -0400

    Revert "Add extra TestAreas printing"
    This reverts commit 682d577ac397e39c6f0cdf9c5ad5ff230951e1d4.

--- build-tools/xaprepare/xaprepare/Steps/Step_DetermineAzurePipelinesTestJobs.cs
+++ build-tools/xaprepare/xaprepare/Steps/Step_DetermineAzurePipelinesTestJobs.cs
@@ -160,7 +160,6 @@ namespace Xamarin.Android.Prepare
 
 			}
 
-			Log.InfoLine ($"Setting output var `TestAreas` with value: {string.Join (",", testAreas)}");
 			Log.MessageLine ($"##vso[task.setvariable variable=TestAreas;isOutput=true]{string.Join (",", testAreas)}");
 			return true;
 		}
