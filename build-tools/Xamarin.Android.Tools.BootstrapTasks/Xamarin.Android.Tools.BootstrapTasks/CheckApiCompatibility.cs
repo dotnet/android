e94097f301abd15e81d477d334c1ec31d3b57d8e
Jonathan Pobst <jonathan.pobst@microsoft.com>
2021-10-15 08:58:36 -0500

    Apply suggestions from code review
    Co-authored-by: Jonathan Peppers <jonathan.peppers@microsoft.com>

--- build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/CheckApiCompatibility.cs
+++ build-tools/Xamarin.Android.Tools.BootstrapTasks/Xamarin.Android.Tools.BootstrapTasks/CheckApiCompatibility.cs
@@ -159,7 +159,7 @@ namespace Xamarin.Android.Tools.BootstrapTasks
 			for (int i = 0; i < 3; i++) {
 				using (var genApiProcess = new Process ()) {
 
-					if (Environment.Version.Major > 5) {
+					if (Environment.Version.Major >= 5) {
 						var apiCompat = new FileInfo (Path.Combine (ApiCompatPath, "..", "netcoreapp2.1", "Microsoft.DotNet.ApiCompat.dll"));
 						genApiProcess.StartInfo.FileName = "dotnet";
 						genApiProcess.StartInfo.Arguments = $"\"{apiCompat}\" ";
