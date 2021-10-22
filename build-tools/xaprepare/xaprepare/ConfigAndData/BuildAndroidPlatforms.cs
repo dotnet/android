0ca520a8fb70aab409e16cdf17cfce6f82d1bb16
Marek Habersack <grendel@twistedcode.net>
2021-10-22 11:46:31 +0200

    Bump to NDK r23b
    Changes: https://github.com/android/ndk/wiki/Changelog-r23#r23b
    
    The most important changes for us:
    
      * Bumped compiler toolchain to LLVM 12
        * Uses universal binaries on M1 Macs
      * Assorted cmake fixes

--- build-tools/xaprepare/xaprepare/ConfigAndData/BuildAndroidPlatforms.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/BuildAndroidPlatforms.cs
@@ -5,8 +5,8 @@ namespace Xamarin.Android.Prepare
 {
 	class BuildAndroidPlatforms
 	{
-		public const string AndroidNdkVersion = "23";
-		public const string AndroidNdkPkgRevision = "23.0.7599858";
+		public const string AndroidNdkVersion = "23b";
+		public const string AndroidNdkPkgRevision = "23.1.7779620";
 
 		public static readonly List<AndroidPlatform> AllPlatforms = new List<AndroidPlatform> {
 			new AndroidPlatform (apiName: "",                       apiLevel: 1,  platformID: "1"),
