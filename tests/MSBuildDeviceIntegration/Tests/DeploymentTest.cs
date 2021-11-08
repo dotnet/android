ce21ecd02a258c1aaecfe479f334a8e63f3e53f6
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-11-08 14:54:56 -0600

    Update TODO for LatestTargetFrameworkVersion() and API 32

--- tests/MSBuildDeviceIntegration/Tests/DeploymentTest.cs
+++ tests/MSBuildDeviceIntegration/Tests/DeploymentTest.cs
@@ -52,12 +52,6 @@ namespace Xamarin.Android.Build.Tests
 			string apiLevel;
 			proj.TargetFrameworkVersion = builder.LatestTargetFrameworkVersion (out apiLevel);
 
-			// TODO: We aren't sure how to support preview bindings in .NET6 yet.
-			if (Builder.UseDotNet && apiLevel == "31") {
-				apiLevel = "30";
-				proj.TargetFrameworkVersion = "v11.0";
-			}
-
 			proj.PackageName = "Xamarin.TimeZoneTest";
 			proj.AndroidManifest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <manifest xmlns:android=""http://schemas.android.com/apk/res/android"" android:versionCode=""1"" android:versionName=""1.0"" package=""Xamarin.TimeZoneTest"">
@@ -125,12 +119,6 @@ namespace Xamarin.Android.Build.Tests
 				string apiLevel;
 				app.TargetFrameworkVersion = b.LatestTargetFrameworkVersion (out apiLevel);
 
-				// TODO: We aren't sure how to support preview bindings in .NET6 yet.
-				if (Builder.UseDotNet && apiLevel == "31") {
-					apiLevel = "30";
-					app.TargetFrameworkVersion = "v11.0";
-				}
-
 				app.AndroidManifest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <manifest xmlns:android=""http://schemas.android.com/apk/res/android"" android:versionCode=""1"" android:versionName=""1.0"" package=""{app.PackageName}"">
 	<uses-sdk android:minSdkVersion=""24"" android:targetSdkVersion=""{apiLevel}"" />
@@ -165,12 +153,6 @@ namespace Xamarin.Android.Build.Tests
 				app.AndroidUseAapt2 = useAapt2;
 				app.LayoutMain = app.LayoutMain.Replace ("@string/hello", "@string/hello_me");
 				app.TargetFrameworkVersion = b.LatestTargetFrameworkVersion (out apiLevel);
-
-				// TODO: We aren't sure how to support preview bindings in .NET6 yet.
-				if (Builder.UseDotNet && apiLevel == "31") {
-					apiLevel = "30";
-					app.TargetFrameworkVersion = "v11.0";
-				}
 
 				app.AndroidManifest = $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <manifest xmlns:android=""http://schemas.android.com/apk/res/android"" android:versionCode=""1"" android:versionName=""1.0"" package=""{app.PackageName}"">
