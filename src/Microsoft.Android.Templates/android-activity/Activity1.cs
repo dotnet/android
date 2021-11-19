b261de527b8edbbab9fbb87cfa9c753346149147
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-11-19 04:56:00 -0600

    [One .NET] use file scoped namespaces in templates
    Context: https://docs.microsoft.com/dotnet/csharp/whats-new/csharp-10#file-scoped-namespace-declaration
    
    In 7fb7c814, I implemented most C# 10 features in templates, but I forgot about file scoped namespaces.
    
    To match the other .NET 6 templates, we should have:
    
        namespace AndroidApp1;
    
    And remove one level of indentation from these files.

--- src/Microsoft.Android.Templates/android-activity/Activity1.cs
+++ src/Microsoft.Android.Templates/android-activity/Activity1.cs
@@ -1,13 +1,12 @@
-namespace AndroidApp1
-{
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class Activity1 : Activity
-    {
-        protected override void OnCreate(Bundle? savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+namespace AndroidApp1;
 
-            // Create your application here
-        }
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class Activity1 : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Create your application here
     }
 }
