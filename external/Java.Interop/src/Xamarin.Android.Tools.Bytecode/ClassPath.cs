dotnet/android

commit 49e1e966fb8fa8fc8734071e03fdd2a7142ff679

[class-parse] Import parameter names for unresolvable types (#921)
Fixes: https://github.com/xamarin/java.interop/issues/920

Context: 15003075e5973017bed8ae86096ef9c56ae36001

`java-source-utils.jar` isn't always able to fully resolve types.
When it is unable to do so, it uses an "alternate encoding":

> In some scenarios, types won't be resolvable.  What should output be?
>
> We don't want to *require* that everything be resolvable -- it's painful, and
> possibly impossible, e.g. w/ internal types -- so instead we should "demark"
> the unresolvable types.
>
> `.params.txt` output will use `.*` as a type prefix, e.g.
>
>         method(.*UnresolvableType foo, int bar);
>
> `docs.xml` will output `L.*UnresolvableType;`.

The problem is that `class-parse --parameter-names=PATH` didn't check
for this "unresolvable type" pattern, so if you had a method which
contained them, e.g. the `target` parameter in:

	<interface jni-signature="Landroidx/core/view/NestedScrollingParent3;" name="NestedScrollingParent3">
	  <method jni-return="V" jni-signature="(L.*View;IIIII[I)V" name="onNestedScroll" return="void">
	    <parameter jni-type="L.*View;" name="target" type=".*View"/>
	    <parameter jni-type="I" name="dxConsumed" type="int"/>
	    <parameter jni-type="I" name="dyConsumed" type="int"/>
	    <parameter jni-type="I" name="dxUnconsumed" type="int"/>
	    <parameter jni-type="I" name="dyUnconsumed" type="int"/>
	    <parameter jni-type="I" name="type" type="int"/>
	    <parameter jni-type="[I" name="consumed" type="int[]"/>

then `class-parse --parameter-names=params.xml lib.jar` wouldn't
try to "loosely match" these parameter types.  Consequently,
parameter names were not imported.

Update the `IJavaMethodParameterNameProvider.GetParameterNames()`
method to take a new `JavaMethodParameterNameInfo` type, instead of
five (5) parameters, as I wanted to increase the amount of data
available to `GetParameterNames()`.

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/ClassPath.cs
@@ -304,14 +304,23 @@ namespace Xamarin.Android.Tools.Bytecode {
 					if (!parameterElements.Select (x => x.Attribute ("name")?.Value).Any (p => p != null && IsGeneratedName (p)))
 						continue;
 
-					var parameters = parameterElements
-						.Select (p => p.Attribute ("type")?.Value!)
-						.Where (p => p != null);
+					var parameterTypes = parameterElements
+						.Select (p => new JavaMethodParameterTypeInfo ((string) p.Attribute ("jni-type"), (string) p.Attribute ("type")))
+						.ToArray ();
 
-					if (!parameters.Any ())
+					if (!parameterTypes.Any ())
 						continue;
 
-					var pnames = jdoc.GetParameterNames (currentpackage, className, currentMethod, parameters.ToArray (), isVarArgs: false);
+					var nameInfo = new JavaMethodParameterNameInfo (
+							currentpackage,
+							className,
+							currentMethod,
+							(string) method.Attribute ("jni-signature"),
+							parameterTypes,
+							isVarArgs: false
+					);
+
+					var pnames = jdoc.GetParameterNames (nameInfo);
 					if (pnames == null || pnames.Length != parameterElements.Count)
 						continue;
 					for (int i = 0; i < parameterElements.Count; i++) {

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/JavaDocumentScraper.cs
@@ -193,9 +193,14 @@ namespace Xamarin.Android.Tools.Bytecode
 		{
 			return value;
 		}
-	
-		public virtual String[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs)
+
+		public virtual string[]? GetParameterNames (JavaMethodParameterNameInfo info)
 		{
+			var package = info.PackageName;
+			var type    = info.TypeName;
+			var method  = info.MethodName;
+			var ptypes  = info.ParameterTypes.Select (p => p.JavaType).ToArray ();
+
 			string path = package.Replace ('.', '/') + '/' + type.Replace ('$', '.') + ".html";
 			string file = Path.Combine (root, path);
 			if (!File.Exists (file)) {
@@ -301,9 +306,39 @@ namespace Xamarin.Android.Tools.Bytecode
 		}
 	}
 
+	public readonly struct JavaMethodParameterTypeInfo {
+		public  string  JniType     {get;}
+		public  string  JavaType    {get;}
+
+		public JavaMethodParameterTypeInfo (string jniType, string javaType)
+		{
+			JniType     = jniType;
+			JavaType    = javaType;
+		}
+	}
+
+	public readonly struct JavaMethodParameterNameInfo {
+		public  string                          PackageName     {get;}
+		public  string                          TypeName        {get;}
+		public  string                          MethodName      {get;}
+		public  string?                         MethodSignature {get;}
+		public  bool                            IsVarArgs       {get;}
+		public  JavaMethodParameterTypeInfo[]   ParameterTypes  {get;}
+
+		public JavaMethodParameterNameInfo (string packageName, string typeName, string methodName, string? methodSignature, JavaMethodParameterTypeInfo[] parameterTypes, bool isVarArgs)
+		{
+			PackageName     = packageName;
+			TypeName        = typeName;
+			MethodName      = methodName;
+			MethodSignature = methodSignature;
+			ParameterTypes  = parameterTypes;
+			IsVarArgs       = isVarArgs;
+		}
+	}
+
 	public interface IJavaMethodParameterNameProvider
 	{
-		String[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs);
+		string[]? GetParameterNames (JavaMethodParameterNameInfo info);
 	}
 
 	public static class JavaMethodParameterNameProvider {
@@ -360,39 +395,59 @@ namespace Xamarin.Android.Tools.Bytecode
 
 		XDocument xdoc;
 
-		public string[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs)
+		public string[]? GetParameterNames (JavaMethodParameterNameInfo info)
 		{
-			var methodOrCtor = method == "constructor" ?
-				"constructor[" : $"method[@name='{method}'";
-
-			var pcount = ptypes.Length;
-
-			var xpath = new StringBuilder ();
-
-			xpath.Append ($"/api/package[@name='{package}']/*[self::class or self::interface]/");
-
-			if (method == "constructor")
-				xpath.Append ("constructor[");
-			else
-				xpath.Append ($"method[@name='{method}'");
-
-			xpath.Append ($" and count(parameter)={pcount}");
-
-			if (pcount > 0) {
-				xpath.Append (" and ");
-				xpath.Append (string.Join (" and ", ptypes.Select ((pt, pindex) => $"parameter[{pindex + 1}][@type='{pt}']")));
+			var xtype = xdoc
+				.Elements ("api")
+				.Elements ("package")
+				.Where (p => ((string) p.Attribute ("name")) == info.PackageName)
+				.Elements ()
+				.Where (t => ((string) t.Attribute ("name")) == info.TypeName)
+				.FirstOrDefault ();
+			if (xtype == null) {
+				return null;
 			}
 
-			xpath.Append ("]");
+			var members = info.MethodName == "constructor"
+				? xtype.Elements ("constructor")
+				: xtype.Elements ("method").Where (m => ((string) m.Attribute ("name")) == info.MethodName);
+			var pcount  = info.ParameterTypes.Length;
+			members     = members
+				.Where (m => m.Elements ("parameter").Count () == pcount);
 
-			var methodElem = xdoc.XPathSelectElement (xpath.ToString ());
+			XElement? member =
+				members.FirstOrDefault (m => info.MethodSignature == (string) m.Attribute ("jni-signature"));
+			if (member == null) {
+				foreach (var m in members) {
+					var found   = true;
+					int i       = 0;
+					foreach (var p in m.Elements ("parameter")) {
+						if (!ParameterTypesMatch (p, info.ParameterTypes [i++].JavaType)) {
+							found = false;
+							break;
+						}
+					}
+					if (found) {
+						member = m;
+						break;
+					}
+				}
+			}
+			if (member == null)
+				return null;
+			return member.Elements ("parameter")
+				.Select (p => (string) p.Attribute ("name"))
+				.ToArray ();
 
-			if (methodElem != null)
-				return methodElem.Elements ("parameter")
-					.Select (pe => pe.Attribute ("name")?.Value ?? "")
-					.ToArray ();
-
-			return null;
+			bool ParameterTypesMatch (XElement parameter, string ptype)
+			{
+				var jtype = (string) parameter.Attribute ("type");
+				if (!jtype.StartsWith (".*", StringComparison.Ordinal)) {
+					return jtype == ptype;
+				}
+				jtype = "." + jtype.Substring (".*".Length);
+				return ptype.EndsWith (jtype, StringComparison.Ordinal);
+			}
 		}
 	}
 }

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/JavaParameterNamesLoader.cs
@@ -114,8 +114,13 @@ namespace Xamarin.Android.Tools.Bytecode
 			return fixup;
 		}
 
-		public string[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs)
+		public string[]? GetParameterNames (JavaMethodParameterNameInfo info)
 		{
+			var package = info.PackageName;
+			var type    = info.TypeName;
+			var method  = info.MethodName;
+			var ptypes  = info.ParameterTypes.Select (p => p.JavaType).ToArray ();
+
 			var methods = this.packages
 				.Where(p => p.Name == package && p.Types != null)
 				.SelectMany(p => p.Types!)

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/ParameterFixupTests.cs
@@ -46,6 +46,21 @@ namespace Xamarin.Android.Tools.BytecodeTests
 		}
 
 		[Test]
+		public void XmlDeclaration_FixedUpFromUnresolvedApiXmlDocumentation ()
+		{
+			string docsPath = null;
+
+			try {
+				docsPath = LoadToTempFile ("ParameterFixup_JavaInterfaceNoParameters_JavaSourceUtils.xml");
+
+				AssertXmlDeclaration ("JavaInterfaceNoParameters.class", "ParameterFixup_JavaInterfaceNoParameters.xml", docsPath);
+			} finally {
+				if (File.Exists (docsPath))
+					File.Delete (docsPath);
+			}
+		}
+
+		[Test]
 		public void XmlDeclaration_DoesNotThrowAnExceptionIfDocumentationNotFound ()
 		{
 			try {

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/Resources/ParameterFixup_JavaInterfaceNoParameters.xml
@@ -1,0 +1,75 @@
+<api
+  api-source="class-parse">
+  <package
+    name="com.xamarin"
+    jni-name="com/xamarin">
+    <interface
+      abstract="true"
+      deprecated="not deprecated"
+      final="false"
+      name="JavaInterfaceNoParameters"
+      jni-signature="Lcom/xamarin/JavaInterfaceNoParameters;"
+      source-file-name="JavaInterfaceNoParameters.java"
+      static="false"
+      visibility="public">
+      <method
+        abstract="true"
+        deprecated="not deprecated"
+        final="false"
+        name="asList"
+        native="false"
+        return="java.util.List&lt;T&gt;"
+        jni-return="Ljava/util/List&lt;TT;&gt;;"
+        static="false"
+        synchronized="false"
+        visibility="public"
+        bridge="false"
+        synthetic="false"
+        jni-signature="([Ljava/lang/Object;)Ljava/util/List;">
+        <typeParameters>
+          <typeParameter
+            name="T"
+            jni-classBound="Ljava/lang/Object;"
+            classBound="java.lang.Object"
+            interfaceBounds=""
+            jni-interfaceBounds="" />
+        </typeParameters>
+        <parameter
+          name="a"
+          type="T..."
+          jni-type="[TT;" />
+      </method>
+      <method
+        abstract="true"
+        deprecated="not deprecated"
+        final="false"
+        name="binarySearch"
+        native="false"
+        return="int"
+        jni-return="I"
+        static="false"
+        synchronized="false"
+        visibility="public"
+        bridge="false"
+        synthetic="false"
+        jni-signature="([Ljava/lang/Object;IILjava/lang/Object;)I">
+        <parameter
+          name="a"
+          type="java.lang.Object[]"
+          jni-type="[Ljava/lang/Object;" />
+        <parameter
+          name="fromIndex"
+          type="int"
+          jni-type="I" />
+        <parameter
+          name="toIndex"
+          type="int"
+          jni-type="I" />
+        <parameter
+          name="key"
+          type="java.lang.Object"
+          jni-type="Ljava/lang/Object;" />
+      </method>
+    </interface>
+  </package>
+</api>

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/Resources/ParameterFixup_JavaInterfaceNoParameters_JavaSourceUtils.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<api api-source="java-source-utils">
+  <package jni-name="com/xamarin" name="com.xamarin">
+    <interface jni-signature="Lcom/xamarin/JavaInterfaceNoParameters;" name="JavaInterfaceNoParameters">
+      <method jni-return="L.*List;" jni-signature="([L.*Object;)L.*List;" name="asList" return=".*List&lt;T&gt;">
+        <parameter jni-type="[L.*Object;" name="a" type="T..."/>
+        <javadoc>
+          <![CDATA[JNI sig: (Lcom/xamarin/JavaTypeNoParameters;)V]]>
+        </javadoc>
+      </method>
+      <method jni-return="I" jni-signature="([L.*Object;IIL.*Object;)I" name="binarySearch" return="int">
+        <parameter jni-type="[L.*Object;" name="a" type=".*Object[]"/>
+        <parameter jni-type="I" name="fromIndex" type="int"/>
+        <parameter jni-type="I" name="toIndex" type="int"/>
+        <parameter jni-type="L.*Object;" name="key" type=".*Object"/>
+      </method>
+    </interface>
+  </package>
+</api>

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/Xamarin.Android.Tools.Bytecode-Tests.csproj
@@ -32,6 +32,7 @@
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\IParameterInterface.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaAnnotation.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaEnum.class" />
+    <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaInterfaceNoParameters.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaType%241.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaType%241MyStringList.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaType%24ASC.class" />

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/java/com/xamarin/JavaInterfaceNoParameters.java
@@ -1,0 +1,19 @@
+package com.xamarin;
+
+public interface JavaInterfaceNoParameters {
+	/**
+	 * JNI sig: ([Ljava/lang/Object;)Ljava/util/List;
+	 */
+	<T> java.util.List<T> asList(T... a);
+
+	/**
+	 * JNI sig: ([Ljava/lang/Object;IILjava/lang/Object;)I
+	 *
+	 * @param a [Ljava/lang/Object;
+	 * @param fromIndex int
+	 * @param toIndex int
+	 * @param key Ljava/lang/Object
+	 * @return int
+	 */
+	int binarySearch(Object[] a, int fromIndex, int toIndex, Object key);
+}

commit 7f7c9835e0382cc21e18bd3eb2ab09926d7f09b1

[Xamarin.Android.Tools.Bytecode] Fix Nullability warnings (#922)
Context: 49e1e966fb8fa8fc8734071e03fdd2a7142ff679

Commit 49e1e966 inadvertently introduced nullability warnings:

	…\Xamarin.Android.Tools.Bytecode\ClassPath.cs(308,89): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\ClassPath.cs(308,54): warning CS8604: Possible null reference argument for parameter 'jniType' in 'JavaMethodParameterTypeInfo.JavaMethodParameterTypeInfo(string jniType, string javaType)'.
	…\Xamarin.Android.Tools.Bytecode\ClassPath.cs(308,89): warning CS8604: Possible null reference argument for parameter 'javaType' in 'JavaMethodParameterTypeInfo.JavaMethodParameterTypeInfo(string jniType, string javaType)'.
	…\Xamarin.Android.Tools.Bytecode\ClassPath.cs(318,8): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\JavaDocumentScraper.cs(403,19): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\JavaDocumentScraper.cs(405,19): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\JavaDocumentScraper.cs(413,46): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\JavaDocumentScraper.cs(419,58): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\JavaDocumentScraper.cs(439,19): warning CS8600: Converting null literal or possible null value to non-nullable type.
	…\Xamarin.Android.Tools.Bytecode\JavaDocumentScraper.cs(438,11): warning CS8619: Nullability of reference types in value of type 'string?[]' doesn't match target type 'string[]'.

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/ClassPath.cs
@@ -305,7 +305,9 @@ namespace Xamarin.Android.Tools.Bytecode {
 						continue;
 
 					var parameterTypes = parameterElements
-						.Select (p => new JavaMethodParameterTypeInfo ((string) p.Attribute ("jni-type"), (string) p.Attribute ("type")))
+						.Select (p => new JavaMethodParameterTypeInfo (
+								jniType: (string?) p.Attribute ("jni-type") ?? "",
+								javaType: (string?) p.Attribute ("type") ?? ""))
 						.ToArray ();
 
 					if (!parameterTypes.Any ())
@@ -315,7 +317,7 @@ namespace Xamarin.Android.Tools.Bytecode {
 							currentpackage,
 							className,
 							currentMethod,
-							(string) method.Attribute ("jni-signature"),
+							(string?) method.Attribute ("jni-signature"),
 							parameterTypes,
 							isVarArgs: false
 					);

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/JavaDocumentScraper.cs
@@ -400,9 +400,9 @@ namespace Xamarin.Android.Tools.Bytecode
 			var xtype = xdoc
 				.Elements ("api")
 				.Elements ("package")
-				.Where (p => ((string) p.Attribute ("name")) == info.PackageName)
+				.Where (p => ((string?) p.Attribute ("name")) == info.PackageName)
 				.Elements ()
-				.Where (t => ((string) t.Attribute ("name")) == info.TypeName)
+				.Where (t => ((string?) t.Attribute ("name")) == info.TypeName)
 				.FirstOrDefault ();
 			if (xtype == null) {
 				return null;
@@ -410,13 +410,13 @@ namespace Xamarin.Android.Tools.Bytecode
 
 			var members = info.MethodName == "constructor"
 				? xtype.Elements ("constructor")
-				: xtype.Elements ("method").Where (m => ((string) m.Attribute ("name")) == info.MethodName);
+				: xtype.Elements ("method").Where (m => ((string?) m.Attribute ("name")) == info.MethodName);
 			var pcount  = info.ParameterTypes.Length;
 			members     = members
 				.Where (m => m.Elements ("parameter").Count () == pcount);
 
 			XElement? member =
-				members.FirstOrDefault (m => info.MethodSignature == (string) m.Attribute ("jni-signature"));
+				members.FirstOrDefault (m => info.MethodSignature == (string?) m.Attribute ("jni-signature"));
 			if (member == null) {
 				foreach (var m in members) {
 					var found   = true;
@@ -436,12 +436,15 @@ namespace Xamarin.Android.Tools.Bytecode
 			if (member == null)
 				return null;
 			return member.Elements ("parameter")
-				.Select (p => (string) p.Attribute ("name"))
+				.Select (p => ((string?) p.Attribute ("name")) ?? "")
 				.ToArray ();
 
 			bool ParameterTypesMatch (XElement parameter, string ptype)
 			{
-				var jtype = (string) parameter.Attribute ("type");
+				var jtype = (string?) parameter.Attribute ("type");
+				if (jtype == null) {
+					return false;
+				}
 				if (!jtype.StartsWith (".*", StringComparison.Ordinal)) {
 					return jtype == ptype;
 				}