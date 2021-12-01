dotnet/android

commit 49e1e966fb8fa8fc8734071e03fdd2a7142ff679

[class-parse] Import parameter names for unresolvable types (#921)
Fixes: https://github.com/xamarin/java.interop/issues/920

Context: 15003075e5973017bed8ae86096ef9c56ae36001

`java-source-utils.jar` isn't always able to fully resolve types.
When it is unable to do so, it uses an "alternate encoding":

> In some scenarios, types won't be resolvable.  What should output be?
>
> We don't want to *require* that everything be resolvable -- it's painful, and
> possibly impossible, e.g. w/ internal types -- so instead we should "demark"
> the unresolvable types.
>
> `.params.txt` output will use `.*` as a type prefix, e.g.
>
>         method(.*UnresolvableType foo, int bar);
>
> `docs.xml` will output `L.*UnresolvableType;`.

The problem is that `class-parse --parameter-names=PATH` didn't check
for this "unresolvable type" pattern, so if you had a method which
contained them, e.g. the `target` parameter in:

	<interface jni-signature="Landroidx/core/view/NestedScrollingParent3;" name="NestedScrollingParent3">
	  <method jni-return="V" jni-signature="(L.*View;IIIII[I)V" name="onNestedScroll" return="void">
	    <parameter jni-type="L.*View;" name="target" type=".*View"/>
	    <parameter jni-type="I" name="dxConsumed" type="int"/>
	    <parameter jni-type="I" name="dyConsumed" type="int"/>
	    <parameter jni-type="I" name="dxUnconsumed" type="int"/>
	    <parameter jni-type="I" name="dyUnconsumed" type="int"/>
	    <parameter jni-type="I" name="type" type="int"/>
	    <parameter jni-type="[I" name="consumed" type="int[]"/>

then `class-parse --parameter-names=params.xml lib.jar` wouldn't
try to "loosely match" these parameter types.  Consequently,
parameter names were not imported.

Update the `IJavaMethodParameterNameProvider.GetParameterNames()`
method to take a new `JavaMethodParameterNameInfo` type, instead of
five (5) parameters, as I wanted to increase the amount of data
available to `GetParameterNames()`.

external/Java.Interop/tools/java-source-utils/src/test/resources/UnresolvedTypes.xml
@@ -3,7 +3,7 @@
   <package jni-name="example" name="example">
     <class jni-signature="Lexample/UnresolvedTypes;" name="UnresolvedTypes">
       <method jni-return="L.*UnresolvedReturnType;" jni-signature="(L.*example.name.UnresolvedParameterType;)L.*UnresolvedReturnType;" name="method" return=".*UnresolvedReturnType">
-        <parameter jni-type="L.*example.name.UnresolvedParameterType;" name="parameter" type=".*example.name.UnresolvedParameterType"/>
+        <parameter jni-type="[L.*example.name.UnresolvedParameterType;" name="parameter" type=".*example.name.UnresolvedParameterType..."/>
         <javadoc><![CDATA[Method using unresolvable types.  As such, we make do.
 
 JNI Sig: method.(L.*example.name.UnresolvedParameterType;)L.*UnresolvedReturnType;]]></javadoc>

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/ClassPath.cs
@@ -304,14 +304,23 @@ namespace Xamarin.Android.Tools.Bytecode {
 					if (!parameterElements.Select (x => x.Attribute ("name")?.Value).Any (p => p != null && IsGeneratedName (p)))
 						continue;
 
-					var parameters = parameterElements
-						.Select (p => p.Attribute ("type")?.Value!)
-						.Where (p => p != null);
+					var parameterTypes = parameterElements
+						.Select (p => new JavaMethodParameterTypeInfo ((string) p.Attribute ("jni-type"), (string) p.Attribute ("type")))
+						.ToArray ();
 
-					if (!parameters.Any ())
+					if (!parameterTypes.Any ())
 						continue;
 
-					var pnames = jdoc.GetParameterNames (currentpackage, className, currentMethod, parameters.ToArray (), isVarArgs: false);
+					var nameInfo = new JavaMethodParameterNameInfo (
+							currentpackage,
+							className,
+							currentMethod,
+							(string) method.Attribute ("jni-signature"),
+							parameterTypes,
+							isVarArgs: false
+					);
+
+					var pnames = jdoc.GetParameterNames (nameInfo);
 					if (pnames == null || pnames.Length != parameterElements.Count)
 						continue;
 					for (int i = 0; i < parameterElements.Count; i++) {

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/JavaDocumentScraper.cs
@@ -193,9 +193,14 @@ namespace Xamarin.Android.Tools.Bytecode
 		{
 			return value;
 		}
-	
-		public virtual String[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs)
+
+		public virtual string[]? GetParameterNames (JavaMethodParameterNameInfo info)
 		{
+			var package = info.PackageName;
+			var type    = info.TypeName;
+			var method  = info.MethodName;
+			var ptypes  = info.ParameterTypes.Select (p => p.JavaType).ToArray ();
+
 			string path = package.Replace ('.', '/') + '/' + type.Replace ('$', '.') + ".html";
 			string file = Path.Combine (root, path);
 			if (!File.Exists (file)) {
@@ -301,9 +306,39 @@ namespace Xamarin.Android.Tools.Bytecode
 		}
 	}
 
+	public readonly struct JavaMethodParameterTypeInfo {
+		public  string  JniType     {get;}
+		public  string  JavaType    {get;}
+
+		public JavaMethodParameterTypeInfo (string jniType, string javaType)
+		{
+			JniType     = jniType;
+			JavaType    = javaType;
+		}
+	}
+
+	public readonly struct JavaMethodParameterNameInfo {
+		public  string                          PackageName     {get;}
+		public  string                          TypeName        {get;}
+		public  string                          MethodName      {get;}
+		public  string?                         MethodSignature {get;}
+		public  bool                            IsVarArgs       {get;}
+		public  JavaMethodParameterTypeInfo[]   ParameterTypes  {get;}
+
+		public JavaMethodParameterNameInfo (string packageName, string typeName, string methodName, string? methodSignature, JavaMethodParameterTypeInfo[] parameterTypes, bool isVarArgs)
+		{
+			PackageName     = packageName;
+			TypeName        = typeName;
+			MethodName      = methodName;
+			MethodSignature = methodSignature;
+			ParameterTypes  = parameterTypes;
+			IsVarArgs       = isVarArgs;
+		}
+	}
+
 	public interface IJavaMethodParameterNameProvider
 	{
-		String[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs);
+		string[]? GetParameterNames (JavaMethodParameterNameInfo info);
 	}
 
 	public static class JavaMethodParameterNameProvider {
@@ -360,39 +395,59 @@ namespace Xamarin.Android.Tools.Bytecode
 
 		XDocument xdoc;
 
-		public string[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs)
+		public string[]? GetParameterNames (JavaMethodParameterNameInfo info)
 		{
-			var methodOrCtor = method == "constructor" ?
-				"constructor[" : $"method[@name='{method}'";
-
-			var pcount = ptypes.Length;
-
-			var xpath = new StringBuilder ();
-
-			xpath.Append ($"/api/package[@name='{package}']/*[self::class or self::interface]/");
-
-			if (method == "constructor")
-				xpath.Append ("constructor[");
-			else
-				xpath.Append ($"method[@name='{method}'");
-
-			xpath.Append ($" and count(parameter)={pcount}");
-
-			if (pcount > 0) {
-				xpath.Append (" and ");
-				xpath.Append (string.Join (" and ", ptypes.Select ((pt, pindex) => $"parameter[{pindex + 1}][@type='{pt}']")));
+			var xtype = xdoc
+				.Elements ("api")
+				.Elements ("package")
+				.Where (p => ((string) p.Attribute ("name")) == info.PackageName)
+				.Elements ()
+				.Where (t => ((string) t.Attribute ("name")) == info.TypeName)
+				.FirstOrDefault ();
+			if (xtype == null) {
+				return null;
 			}
 
-			xpath.Append ("]");
+			var members = info.MethodName == "constructor"
+				? xtype.Elements ("constructor")
+				: xtype.Elements ("method").Where (m => ((string) m.Attribute ("name")) == info.MethodName);
+			var pcount  = info.ParameterTypes.Length;
+			members     = members
+				.Where (m => m.Elements ("parameter").Count () == pcount);
 
-			var methodElem = xdoc.XPathSelectElement (xpath.ToString ());
+			XElement? member =
+				members.FirstOrDefault (m => info.MethodSignature == (string) m.Attribute ("jni-signature"));
+			if (member == null) {
+				foreach (var m in members) {
+					var found   = true;
+					int i       = 0;
+					foreach (var p in m.Elements ("parameter")) {
+						if (!ParameterTypesMatch (p, info.ParameterTypes [i++].JavaType)) {
+							found = false;
+							break;
+						}
+					}
+					if (found) {
+						member = m;
+						break;
+					}
+				}
+			}
+			if (member == null)
+				return null;
+			return member.Elements ("parameter")
+				.Select (p => (string) p.Attribute ("name"))
+				.ToArray ();
 
-			if (methodElem != null)
-				return methodElem.Elements ("parameter")
-					.Select (pe => pe.Attribute ("name")?.Value ?? "")
-					.ToArray ();
-
-			return null;
+			bool ParameterTypesMatch (XElement parameter, string ptype)
+			{
+				var jtype = (string) parameter.Attribute ("type");
+				if (!jtype.StartsWith (".*", StringComparison.Ordinal)) {
+					return jtype == ptype;
+				}
+				jtype = "." + jtype.Substring (".*".Length);
+				return ptype.EndsWith (jtype, StringComparison.Ordinal);
+			}
 		}
 	}
 }

external/Java.Interop/src/Xamarin.Android.Tools.Bytecode/JavaParameterNamesLoader.cs
@@ -114,8 +114,13 @@ namespace Xamarin.Android.Tools.Bytecode
 			return fixup;
 		}
 
-		public string[]? GetParameterNames (string package, string type, string method, string[] ptypes, bool isVarArgs)
+		public string[]? GetParameterNames (JavaMethodParameterNameInfo info)
 		{
+			var package = info.PackageName;
+			var type    = info.TypeName;
+			var method  = info.MethodName;
+			var ptypes  = info.ParameterTypes.Select (p => p.JavaType).ToArray ();
+
 			var methods = this.packages
 				.Where(p => p.Name == package && p.Types != null)
 				.SelectMany(p => p.Types!)

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/ParameterFixupTests.cs
@@ -46,6 +46,21 @@ namespace Xamarin.Android.Tools.BytecodeTests
 		}
 
 		[Test]
+		public void XmlDeclaration_FixedUpFromUnresolvedApiXmlDocumentation ()
+		{
+			string docsPath = null;
+
+			try {
+				docsPath = LoadToTempFile ("ParameterFixup_JavaInterfaceNoParameters_JavaSourceUtils.xml");
+
+				AssertXmlDeclaration ("JavaInterfaceNoParameters.class", "ParameterFixup_JavaInterfaceNoParameters.xml", docsPath);
+			} finally {
+				if (File.Exists (docsPath))
+					File.Delete (docsPath);
+			}
+		}
+
+		[Test]
 		public void XmlDeclaration_DoesNotThrowAnExceptionIfDocumentationNotFound ()
 		{
 			try {

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/Resources/ParameterFixup_JavaInterfaceNoParameters.xml
@@ -1,0 +1,75 @@
+<api
+  api-source="class-parse">
+  <package
+    name="com.xamarin"
+    jni-name="com/xamarin">
+    <interface
+      abstract="true"
+      deprecated="not deprecated"
+      final="false"
+      name="JavaInterfaceNoParameters"
+      jni-signature="Lcom/xamarin/JavaInterfaceNoParameters;"
+      source-file-name="JavaInterfaceNoParameters.java"
+      static="false"
+      visibility="public">
+      <method
+        abstract="true"
+        deprecated="not deprecated"
+        final="false"
+        name="asList"
+        native="false"
+        return="java.util.List&lt;T&gt;"
+        jni-return="Ljava/util/List&lt;TT;&gt;;"
+        static="false"
+        synchronized="false"
+        visibility="public"
+        bridge="false"
+        synthetic="false"
+        jni-signature="([Ljava/lang/Object;)Ljava/util/List;">
+        <typeParameters>
+          <typeParameter
+            name="T"
+            jni-classBound="Ljava/lang/Object;"
+            classBound="java.lang.Object"
+            interfaceBounds=""
+            jni-interfaceBounds="" />
+        </typeParameters>
+        <parameter
+          name="a"
+          type="T..."
+          jni-type="[TT;" />
+      </method>
+      <method
+        abstract="true"
+        deprecated="not deprecated"
+        final="false"
+        name="binarySearch"
+        native="false"
+        return="int"
+        jni-return="I"
+        static="false"
+        synchronized="false"
+        visibility="public"
+        bridge="false"
+        synthetic="false"
+        jni-signature="([Ljava/lang/Object;IILjava/lang/Object;)I">
+        <parameter
+          name="a"
+          type="java.lang.Object[]"
+          jni-type="[Ljava/lang/Object;" />
+        <parameter
+          name="fromIndex"
+          type="int"
+          jni-type="I" />
+        <parameter
+          name="toIndex"
+          type="int"
+          jni-type="I" />
+        <parameter
+          name="key"
+          type="java.lang.Object"
+          jni-type="Ljava/lang/Object;" />
+      </method>
+    </interface>
+  </package>
+</api>

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/Resources/ParameterFixup_JavaInterfaceNoParameters_JavaSourceUtils.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<api api-source="java-source-utils">
+  <package jni-name="com/xamarin" name="com.xamarin">
+    <interface jni-signature="Lcom/xamarin/JavaInterfaceNoParameters;" name="JavaInterfaceNoParameters">
+      <method jni-return="L.*List;" jni-signature="([L.*Object;)L.*List;" name="asList" return=".*List&lt;T&gt;">
+        <parameter jni-type="[L.*Object;" name="a" type="T..."/>
+        <javadoc>
+          <![CDATA[JNI sig: (Lcom/xamarin/JavaTypeNoParameters;)V]]>
+        </javadoc>
+      </method>
+      <method jni-return="I" jni-signature="([L.*Object;IIL.*Object;)I" name="binarySearch" return="int">
+        <parameter jni-type="[L.*Object;" name="a" type=".*Object[]"/>
+        <parameter jni-type="I" name="fromIndex" type="int"/>
+        <parameter jni-type="I" name="toIndex" type="int"/>
+        <parameter jni-type="L.*Object;" name="key" type=".*Object"/>
+      </method>
+    </interface>
+  </package>
+</api>

external/Java.Interop/tests/Xamarin.Android.Tools.Bytecode-Tests/Xamarin.Android.Tools.Bytecode-Tests.csproj
@@ -32,6 +32,7 @@
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\IParameterInterface.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaAnnotation.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaEnum.class" />
+    <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaInterfaceNoParameters.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaType%241.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaType%241MyStringList.class" />
     <EmbeddedResource Include="$(IntermediateOutputPath)classes\com\xamarin\JavaType%24ASC.class" />

commit 81d1a0d44384e330f388b0753e210a887336191b

[java-source-utils] Fix regresion from 49e1e966 (#923)
Context: 49e1e966fb8fa8fc8734071e03fdd2a7142ff679

Commit 49e1e966 introduced a regression in `tools/java-source-utils`,
wherein the unit tests would fail, e.g.

	org.junit.ComparisonFailure: ../../../com/xamarin/JavaType.java Javadoc XML expected:<...um">
	      <javadoc>[<![CDATA[JNI sig: Lcom/xamarin/JavaEnum;]]></javadoc>
	…
	      </m...> but was:<...um">
	      <javadoc>[
	        <![CDATA[JNI sig: Lcom/xamarin/JavaEnum;]]>

Part of the problem is that this failure was *overlooked*;
it's very common for PR builds to fail because mono SIGSEGVs, and we
don't always take the time to restart failed PR builds to ensure that
they're green (oops).  The Windows builds, meanwhile, were green.
Consequently, when commit 49e1e966 introduced a test failure in the
`java-source-utils` unit tests, but we didn't see the failure.
This "lack of seeing" is in part because we've become inured to test
failures (mono SIGSEGVs), but also because the `java-source-utils`
unit tests:

 1. Are *only* run on the `mac_build` job, and

 2. The results of the `java-source-utils` unit tests are not
    shown in DevOps' **Tests** tab.

    The only way to see the failure is to read the full
    **Mac - Mono** > **Run Tests** output, and know what you're
    looking for.

Fix these two more fundamental issues:

 1. Update `tools/java-source-utils/build.gradle` so that when
    `gradle test` is run, JUnit XML-formatted test result files
    are created.

 2. Update `core-tests.yaml` and `RunNUnitTests.targets` so that the
    `java-source-utils` unit tests are executed.

 3. Publish the JUnit XML files created between (1) and (2).

This should ensure that future `java-source-utils` failures are
appropriately seen and not ignored.

I then ran a PR build without fixing anything, verifying that the
**Tests** tab now shows the expected unit tests failures.

external/Java.Interop/build-tools/automation/azure-pipelines.yaml
@@ -70,12 +70,7 @@ jobs:
       msbuildArguments: /p:TestAssembly="bin\Test$(Build.Configuration)\generator-Tests.dll;bin\Test$(Build.Configuration)\Java.Interop.Tools.JavaCallableWrappers-Tests.dll;bin\Test$(Build.Configuration)\logcat-parse-Tests.dll;bin\Test$(Build.Configuration)\Xamarin.Android.Tools.ApiXmlAdjuster-Tests.dll;bin\Test$(Build.Configuration)\Xamarin.Android.Tools.Bytecode-Tests.dll;bin\Test$(Build.Configuration)\Java.Interop.Tools.Generator-Tests.dll;bin\Test$(Build.Configuration)\Xamarin.SourceWriter-Tests.dll"
     condition: succeededOrFailed()
 
-  - task: PublishTestResults@2
-    displayName: Publish Test Results
-    inputs:
-      testResultsFormat: NUnit
-      testResultsFiles: TestResult-*.xml
-    condition: succeededOrFailed()
+  - template: templates\publish-test-results.yaml
 
 - job: windows_dotnet_build
   displayName: Windows - .NET Core
@@ -145,12 +140,7 @@ jobs:
       exit $r
     displayName: Run Tests
 
-  - task: PublishTestResults@2
-    displayName: Publish Test Results
-    inputs:
-      testResultsFormat: NUnit
-      testResultsFiles: TestResult-*.xml
-    condition: succeededOrFailed()
+  - template: templates\publish-test-results.yaml
 
   - task: CopyFiles@2
     displayName: 'Copy Files to: Artifact Staging Directory'
@@ -189,5 +179,6 @@ jobs:
   - template: templates\core-tests.yaml
     parameters:
       runNativeTests: true
+      runJavaTests: true
 
   - template: templates\fail-on-issue.yaml

external/Java.Interop/build-tools/automation/templates/publish-test-results.yaml
@@ -1,0 +1,16 @@
+steps:
+- task: PublishTestResults@2
+  displayName: Publish NUnit Test Results
+  condition: succeededOrFailed()
+  inputs:
+    testResultsFormat: NUnit
+    testResultsFiles: TestResult-*.xml
+  continueOnError: true
+
+- task: PublishTestResults@2
+  displayName: Publish JUnit Test Results
+  condition: ne('$(Agent.OS)', 'Windows')
+  inputs:
+    testResultsFormat: JUnit
+    testResultsFiles: '**/TEST-*.xml'
+  continueOnError: true

external/Java.Interop/tools/java-source-utils/build.gradle
@@ -54,3 +54,9 @@ jar {
     }
     archiveName 'java-source-utils.jar'
 }
+
+test {
+    reports {
+        junitXml.enabled = true
+    }
+}

external/Java.Interop/build-tools/automation/templates/core-tests.yaml
@@ -1,6 +1,7 @@
 parameters:
   condition: succeeded()
   runNativeTests: false
+  runJavaTests: false
 
 steps:
 - task: DotNetCoreCLI@2
@@ -105,4 +106,20 @@ steps:
   inputs:
     command: test
     arguments: bin/Test$(Build.Configuration)$(NetCoreTargetFrameworkPathSuffix)/Java.Interop-PerformanceTests.dll
+  continueOnError: true
+
+- task: DotNetCoreCLI@2
+  displayName: 'Tests: java-source-utils'
+  condition: eq('${{ parameters.runJavaTests }}', 'true')
+  inputs:
+    command: build
+    arguments: -c $(Build.Configuration) tools/java-source-utils/java-source-utils.csproj -t:RunTests
+  continueOnError: true
+
+- task: PublishTestResults@2
+  displayName: Publish JUnit Test Results
+  condition: eq('${{ parameters.runJavaTests }}', 'true')
+  inputs:
+    testResultsFormat: JUnit
+    testResultsFiles: 'tools/java-source-utils/build/test-results/**/TEST-*.xml'
   continueOnError: true

external/Java.Interop/build-tools/scripts/RunNUnitTests.targets
@@ -29,5 +29,10 @@
         WorkingDirectory="$(_TopDir)"
         ContinueOnError="ErrorAndContinue"
     />
+    <MSBuild
+        Condition=" !$([MSBuild]::IsOSPlatform ('windows')) "
+        Projects="$(MSBuildThisFileDirectory)..\..\tools\java-source-utils\java-source-utils.csproj"
+        Targets="RunTests"
+    />
   </Target>
 </Project>

external/Java.Interop/tools/java-source-utils/src/test/resources/UnresolvedTypes.xml
@@ -2,11 +2,11 @@
 <api api-source="java-source-utils">
   <package jni-name="example" name="example">
     <class jni-signature="Lexample/UnresolvedTypes;" name="UnresolvedTypes">
-      <method jni-return="L.*UnresolvedReturnType;" jni-signature="(L.*example.name.UnresolvedParameterType;)L.*UnresolvedReturnType;" name="method" return=".*UnresolvedReturnType">
+      <method jni-return="L.*UnresolvedReturnType;" jni-signature="([L.*example.name.UnresolvedParameterType;)L.*UnresolvedReturnType;" name="method" return=".*UnresolvedReturnType">
         <parameter jni-type="[L.*example.name.UnresolvedParameterType;" name="parameter" type=".*example.name.UnresolvedParameterType..."/>
         <javadoc><![CDATA[Method using unresolvable types.  As such, we make do.
 
-JNI Sig: method.(L.*example.name.UnresolvedParameterType;)L.*UnresolvedReturnType;]]></javadoc>
+JNI Sig: method.([L.*example.name.UnresolvedParameterType;)L.*UnresolvedReturnType;]]></javadoc>
       </method>
     </class>
   </package>

external/Java.Interop/tools/java-source-utils/src/test/resources/com/microsoft/android/JavaType.xml
@@ -127,9 +127,9 @@
         <parameter jni-type="Ljava/util/List;" name="superList" type="java.util.List&lt;? super java.lang.Throwable&gt;"/>
         <javadoc><![CDATA[JNI sig: staticActionWithGenerics.(Ljava/lang/Object;Ljava/lang/Number;Ljava/util/List;Ljava/util/List;Ljava/util/List;)V]]></javadoc>
       </method>
-      <method jni-return="I" jni-signature="(II)I" name="sum" return="int">
+      <method jni-return="I" jni-signature="(I[I)I" name="sum" return="int">
         <parameter jni-type="I" name="first" type="int"/>
-        <parameter jni-type="I" name="remaining" type="int"/>
+        <parameter jni-type="[I" name="remaining" type="int..."/>
         <javadoc><![CDATA[JNI sig: sum.(I[I)I]]></javadoc>
       </method>
     </class>