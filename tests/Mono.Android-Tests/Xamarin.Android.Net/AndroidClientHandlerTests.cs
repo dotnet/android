b49fd00cd1529828ba355f18d50e5c2f52029dd0
Simon Rozsival <simon@rozsival.com>
2021-12-23 09:22:37 +0100

    Revert all recent changes to tests

--- tests/Mono.Android-Tests/Xamarin.Android.Net/AndroidClientHandlerTests.cs
+++ tests/Mono.Android-Tests/Xamarin.Android.Net/AndroidClientHandlerTests.cs
@@ -70,32 +70,32 @@ namespace Xamarin.Android.NetTests {
 			}
 		}
 
-		// [Test]
-		// public void Properties_Defaults ()
-		// {
-		// 	var obj = CreateHandler ();
-		// 	if (obj is not HttpClientHandler h) {
-		// 		Assert.Ignore ($"{obj.GetType()} is not a HttpClientHandler.");
-		// 		return;
-		// 	}
+		[Test]
+		public void Properties_Defaults ()
+		{
+			var obj = CreateHandler ();
+			if (obj is not HttpClientHandler h) {
+				Assert.Ignore ($"{obj.GetType()} is not a HttpClientHandler.");
+				return;
+			}
 
-		// 	Assert.IsTrue (h.AllowAutoRedirect, "#1");
-		// 	Assert.AreEqual (DecompressionMethods.None, h.AutomaticDecompression, "#2");
-		// 	Assert.AreEqual (0, h.CookieContainer.Count, "#3");
-		// 	Assert.AreEqual (4096, h.CookieContainer.MaxCookieSize, "#3b");
-		// 	Assert.AreEqual (null, h.Credentials, "#4");
-		// 	Assert.AreEqual (50, h.MaxAutomaticRedirections, "#5");
-		// 	Assert.IsFalse (h.PreAuthenticate, "#7");
-		// 	Assert.IsNull (h.Proxy, "#8");
-		// 	Assert.IsTrue (h.SupportsAutomaticDecompression, "#9");
-		// 	Assert.IsTrue (h.SupportsProxy, "#10");
-		// 	Assert.IsTrue (h.SupportsRedirectConfiguration, "#11");
-		// 	Assert.IsTrue (h.UseCookies, "#12");
-		// 	Assert.IsFalse (h.UseDefaultCredentials, "#13");
-		// 	Assert.IsTrue (h.UseProxy, "#14");
-		// 	Assert.AreEqual (ClientCertificateOption.Manual, h.ClientCertificateOptions, "#15");
-		// 	Assert.IsNull (h.ServerCertificateCustomValidationCallback, "#16");
-		// }
+			Assert.IsTrue (h.AllowAutoRedirect, "#1");
+			Assert.AreEqual (DecompressionMethods.None, h.AutomaticDecompression, "#2");
+			Assert.AreEqual (0, h.CookieContainer.Count, "#3");
+			Assert.AreEqual (4096, h.CookieContainer.MaxCookieSize, "#3b");
+			Assert.AreEqual (null, h.Credentials, "#4");
+			Assert.AreEqual (50, h.MaxAutomaticRedirections, "#5");
+			Assert.IsFalse (h.PreAuthenticate, "#7");
+			Assert.IsNull (h.Proxy, "#8");
+			Assert.IsTrue (h.SupportsAutomaticDecompression, "#9");
+			Assert.IsTrue (h.SupportsProxy, "#10");
+			Assert.IsTrue (h.SupportsRedirectConfiguration, "#11");
+			Assert.IsTrue (h.UseCookies, "#12");
+			Assert.IsFalse (h.UseDefaultCredentials, "#13");
+			Assert.IsTrue (h.UseProxy, "#14");
+			Assert.AreEqual (ClientCertificateOption.Manual, h.ClientCertificateOptions, "#15");
+			Assert.IsNull (h.ServerCertificateCustomValidationCallback, "#16");
+		}
 
 		[Test]
 		public void Properties_Invalid ()
@@ -136,23 +136,23 @@ namespace Xamarin.Android.NetTests {
 			}
 		}
 
-		// [Test]
-		// public void Disposed ()
-		// {
-		// 	var h = CreateHandler ();
-		// 	h.Dispose ();
-		// 	var c = new HttpClient (h);
-		// 	try {
-		// 		var t = ConnectIgnoreFailure (() => c.GetAsync ("http://google.com"), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Disposed ()
+		{
+			var h = CreateHandler ();
+			h.Dispose ();
+			var c = new HttpClient (h);
+			try {
+				var t = ConnectIgnoreFailure (() => c.GetAsync ("http://google.com"), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		t.Wait ();
-		// 		Assert.Fail ("#1");
-		// 	} catch (AggregateException e) {
-		// 		Assert.IsTrue (e.InnerException is ObjectDisposedException, "#2");
-		// 	}
-		// }
+				t.Wait ();
+				Assert.Fail ("#1");
+			} catch (AggregateException e) {
+				Assert.IsTrue (e.InnerException is ObjectDisposedException, "#2");
+			}
+		}
 
 		protected Task<HttpResponseMessage> ConnectIgnoreFailure (Func<Task<HttpResponseMessage>> connector, out bool connectionFailed)
 		{
@@ -214,25 +214,25 @@ namespace Xamarin.Android.NetTests {
 	{
 		const string Tls_1_2_Url = "https://tls-test.internalx.com";
 
-		// [Test]
-		// public void Tls_1_2_Url_Works ()
-		// {
-		// 	if (((int) Build.VERSION.SdkInt) < 16) {
-		// 		Assert.Ignore ("Host platform doesn't support TLS 1.2.");
-		// 		return;
-		// 	}
-		// 	using (var c = new HttpClient (CreateHandler ())) {
-		// 		var tr = ConnectIgnoreFailure (() => c.GetAsync (Tls_1_2_Url), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Tls_1_2_Url_Works ()
+		{
+			if (((int) Build.VERSION.SdkInt) < 16) {
+				Assert.Ignore ("Host platform doesn't support TLS 1.2.");
+				return;
+			}
+			using (var c = new HttpClient (CreateHandler ())) {
+				var tr = ConnectIgnoreFailure (() => c.GetAsync (Tls_1_2_Url), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+				RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		tr.Result.EnsureSuccessStatusCode ();
-		// 	}
-		// }
+				tr.Result.EnsureSuccessStatusCode ();
+			}
+		}
 
 		static IEnumerable<Exception> Exceptions (Exception e)
 		{
@@ -259,154 +259,154 @@ namespace Xamarin.Android.NetTests {
 			return innerHandler.GetType ();
 		}
 
-		// [Test, Category ("DotNetIgnore")]
-		// public void Sanity_Tls_1_2_Url_WithMonoClientHandlerFails ()
-		// {
-		// 	var tlsProvider   = global::System.Environment.GetEnvironmentVariable ("XA_TLS_PROVIDER");
-		// 	var supportTls1_2 = tlsProvider.Equals ("btls", StringComparison.OrdinalIgnoreCase);
-		// 	using (var c = new HttpClient (new HttpClientHandler ())) {
-		// 		try {
-		// 			Assert.AreEqual ("SocketsHttpHandler", GetInnerHandlerType (c).Name, 
-		// 				"Underlying HttpClientHandler is expected to use SocketsHttpHandler by default. " + 
-		// 				"XA_HTTP_CLIENT_HANDLER_TYPE=" + global::System.Environment.GetEnvironmentVariable ("XA_HTTP_CLIENT_HANDLER_TYPE"));
+		[Test, Category ("DotNetIgnore")]
+		public void Sanity_Tls_1_2_Url_WithMonoClientHandlerFails ()
+		{
+			var tlsProvider   = global::System.Environment.GetEnvironmentVariable ("XA_TLS_PROVIDER");
+			var supportTls1_2 = tlsProvider.Equals ("btls", StringComparison.OrdinalIgnoreCase);
+			using (var c = new HttpClient (new HttpClientHandler ())) {
+				try {
+					Assert.AreEqual ("SocketsHttpHandler", GetInnerHandlerType (c).Name, 
+						"Underlying HttpClientHandler is expected to use SocketsHttpHandler by default. " + 
+						"XA_HTTP_CLIENT_HANDLER_TYPE=" + global::System.Environment.GetEnvironmentVariable ("XA_HTTP_CLIENT_HANDLER_TYPE"));
 
-		// 			var tr = ConnectIgnoreFailure (() => c.GetAsync (Tls_1_2_Url), out bool connectionFailed);
-		// 			if (connectionFailed)
-		// 				return;
+					var tr = ConnectIgnoreFailure (() => c.GetAsync (Tls_1_2_Url), out bool connectionFailed);
+					if (connectionFailed)
+						return;
 
-		// 			RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
-		// 			if (connectionFailed)
-		// 				return;
+					RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
+					if (connectionFailed)
+						return;
 
-		// 			tr.Result.EnsureSuccessStatusCode ();
-		// 			if (!supportTls1_2) {
-		// 				Assert.Fail ("SHOULD NOT BE REACHED: Mono's HttpClientHandler doesn't support TLS 1.2.");
-		// 			}
-		// 		}
-		// 		catch (AggregateException e) {
-		// 			if (supportTls1_2) {
-		// 				Assert.Fail ("SHOULD NOT BE REACHED: BTLS is present, TLS 1.2 should work. Network error? {0}", e.ToString ());
-		// 			}
-		// 			if (!supportTls1_2) {
-		// 				Assert.IsTrue (IsSecureChannelFailure (e),
-		// 					       "Nested exception and/or corresponding status code did not match expected results for TLS 1.2 incompatibility {0}",
-		// 					       e);
-		// 			}
-		// 		}
-		// 	}
-		// }
+					tr.Result.EnsureSuccessStatusCode ();
+					if (!supportTls1_2) {
+						Assert.Fail ("SHOULD NOT BE REACHED: Mono's HttpClientHandler doesn't support TLS 1.2.");
+					}
+				}
+				catch (AggregateException e) {
+					if (supportTls1_2) {
+						Assert.Fail ("SHOULD NOT BE REACHED: BTLS is present, TLS 1.2 should work. Network error? {0}", e.ToString ());
+					}
+					if (!supportTls1_2) {
+						Assert.IsTrue (IsSecureChannelFailure (e),
+							       "Nested exception and/or corresponding status code did not match expected results for TLS 1.2 incompatibility {0}",
+							       e);
+					}
+				}
+			}
+		}
 
-		// [Test]
-		// public void Cancel_Client_Works()
-		// {
-		// 	var cts = new CancellationTokenSource ();
-		// 	cts.Cancel (); //Cancel immediately
-		// 	using (var c = new HttpClient (CreateHandler())) {
-		// 		var tr = ConnectIgnoreFailure (() => c.GetAsync ("http://10.255.255.1", cts.Token), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Cancel_Client_Works()
+		{
+			var cts = new CancellationTokenSource ();
+			cts.Cancel (); //Cancel immediately
+			using (var c = new HttpClient (CreateHandler())) {
+				var tr = ConnectIgnoreFailure (() => c.GetAsync ("http://10.255.255.1", cts.Token), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		try {
-		// 			RunIgnoringNetworkIssues (() => tr.Wait(), out connectionFailed);
-		// 			if (connectionFailed)
-		// 				return;
+				try {
+					RunIgnoringNetworkIssues (() => tr.Wait(), out connectionFailed);
+					if (connectionFailed)
+						return;
 
-		// 			Assert.Fail ("SHOULD NOT HAPPEN: Request is expected to cancel");
-		// 		}
-		// 		catch (AggregateException ex) {
-		// 			Assert.IsTrue (ex.InnerExceptions.Any (ie => ie is System.OperationCanceledException), "Request did not throw cancellation exception; threw: {0}", ex);
-		// 			Assert.IsTrue (cts.IsCancellationRequested, "The request was canceled before cancellation was requested");
-		// 		}
-		// 	}
-		// }
+					Assert.Fail ("SHOULD NOT HAPPEN: Request is expected to cancel");
+				}
+				catch (AggregateException ex) {
+					Assert.IsTrue (ex.InnerExceptions.Any (ie => ie is System.OperationCanceledException), "Request did not throw cancellation exception; threw: {0}", ex);
+					Assert.IsTrue (cts.IsCancellationRequested, "The request was canceled before cancellation was requested");
+				}
+			}
+		}
 
-		// [Test]
-		// public void Token_Timeout_Works()
-		// {
-		// 	var cts = new CancellationTokenSource (2000); //Cancel after 2000ms through token
-		// 	using (var c = new HttpClient (CreateHandler())){
-		// 		var tr = ConnectIgnoreFailure (() => c.GetAsync ("http://10.255.255.1", cts.Token), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Token_Timeout_Works()
+		{
+			var cts = new CancellationTokenSource (2000); //Cancel after 2000ms through token
+			using (var c = new HttpClient (CreateHandler())){
+				var tr = ConnectIgnoreFailure (() => c.GetAsync ("http://10.255.255.1", cts.Token), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		try {
-		// 			RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
-		// 			if (connectionFailed)
-		// 				return;
+				try {
+					RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
+					if (connectionFailed)
+						return;
 
-		// 			Assert.Fail ("SHOULD NOT HAPPEN: Request is expected to cancel");
-		// 		}
-		// 		catch (AggregateException ex) {
-		// 			Assert.IsTrue (ex.InnerExceptions.Any(ie => ie is System.OperationCanceledException), "Request did not throw cancellation exception; threw: {0}", ex);
-		// 			Assert.IsTrue (cts.IsCancellationRequested, "The request was canceled before cancellation was requested");
-		// 		}
-		// 	}
-		// }
+					Assert.Fail ("SHOULD NOT HAPPEN: Request is expected to cancel");
+				}
+				catch (AggregateException ex) {
+					Assert.IsTrue (ex.InnerExceptions.Any(ie => ie is System.OperationCanceledException), "Request did not throw cancellation exception; threw: {0}", ex);
+					Assert.IsTrue (cts.IsCancellationRequested, "The request was canceled before cancellation was requested");
+				}
+			}
+		}
 
-		// [Test]
-		// public void Property_Timeout_Works()
-		// {
-		// 	using (var c = new HttpClient (CreateHandler ()))
-		// 	{
-		// 		c.Timeout = TimeSpan.FromMilliseconds (2000); //Cancel after 2000ms through Timeout property
-		// 		var tr = ConnectIgnoreFailure (() => c.GetAsync ("http://10.255.255.1"), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Property_Timeout_Works()
+		{
+			using (var c = new HttpClient (CreateHandler ()))
+			{
+				c.Timeout = TimeSpan.FromMilliseconds (2000); //Cancel after 2000ms through Timeout property
+				var tr = ConnectIgnoreFailure (() => c.GetAsync ("http://10.255.255.1"), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		try {
-		// 			RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
-		// 			if (connectionFailed)
-		// 				return;
+				try {
+					RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
+					if (connectionFailed)
+						return;
 
-		// 			Assert.Fail ("SHOULD NOT HAPPEN: Request is expected to cancel");
-		// 		}
-		// 		catch (AggregateException ex)
-		// 		{
-		// 			Assert.IsTrue (ex.InnerExceptions.Any (ie => ie is System.OperationCanceledException), "Request did not throw cancellation exception; threw: {0}", ex);
-		// 		}
-		// 	}
-		// }
+					Assert.Fail ("SHOULD NOT HAPPEN: Request is expected to cancel");
+				}
+				catch (AggregateException ex)
+				{
+					Assert.IsTrue (ex.InnerExceptions.Any (ie => ie is System.OperationCanceledException), "Request did not throw cancellation exception; threw: {0}", ex);
+				}
+			}
+		}
 
-		// [Test]
-		// public void Redirect_Without_Protocol_Works()
-		// {
-		// 	var requestURI = new Uri ("http://tls-test.internalx.com/redirect.php");
-		// 	var redirectedURI = new Uri ("http://tls-test.internalx.com/redirect-301.html");
-		// 	using (var c = new HttpClient (CreateHandler ())) {
-		// 		var tr = ConnectIgnoreFailure (() => c.GetAsync (requestURI), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Redirect_Without_Protocol_Works()
+		{
+			var requestURI = new Uri ("http://tls-test.internalx.com/redirect.php");
+			var redirectedURI = new Uri ("http://tls-test.internalx.com/redirect-301.html");
+			using (var c = new HttpClient (CreateHandler ())) {
+				var tr = ConnectIgnoreFailure (() => c.GetAsync (requestURI), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+				RunIgnoringNetworkIssues (() => tr.Wait (), out connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		tr.Result.EnsureSuccessStatusCode ();
-		// 		Assert.AreEqual (redirectedURI, tr.Result.RequestMessage.RequestUri, "Invalid redirected URI");
-		// 	}
-		// }
+				tr.Result.EnsureSuccessStatusCode ();
+				Assert.AreEqual (redirectedURI, tr.Result.RequestMessage.RequestUri, "Invalid redirected URI");
+			}
+		}
 
-		// [Test]
-		// public void Redirect_POST_With_Content_Works ()
-		// {
-		// 	var requestURI = new Uri ("http://tls-test.internalx.com/redirect.php");
-		// 	var redirectedURI = new Uri ("http://tls-test.internalx.com/redirect-301.html");
-		// 	using (var c = new HttpClient (CreateHandler ())) {
-		// 		var request = new HttpRequestMessage (HttpMethod.Post, requestURI);
-		// 		request.Content = new StringContent("{}", Encoding.UTF8, "application/json");
-		// 		var t = ConnectIgnoreFailure (() => c.SendAsync(request), out bool connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+		[Test]
+		public void Redirect_POST_With_Content_Works ()
+		{
+			var requestURI = new Uri ("http://tls-test.internalx.com/redirect.php");
+			var redirectedURI = new Uri ("http://tls-test.internalx.com/redirect-301.html");
+			using (var c = new HttpClient (CreateHandler ())) {
+				var request = new HttpRequestMessage (HttpMethod.Post, requestURI);
+				request.Content = new StringContent("{}", Encoding.UTF8, "application/json");
+				var t = ConnectIgnoreFailure (() => c.SendAsync(request), out bool connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		HttpResponseMessage response = null;
-		// 		RunIgnoringNetworkIssues (() => response = t.Result, out connectionFailed);
-		// 		if (connectionFailed)
-		// 			return;
+				HttpResponseMessage response = null;
+				RunIgnoringNetworkIssues (() => response = t.Result, out connectionFailed);
+				if (connectionFailed)
+					return;
 
-		// 		response.EnsureSuccessStatusCode ();
-		// 		Assert.AreEqual (redirectedURI, response.RequestMessage.RequestUri, "Invalid redirected URI");
-		// 	}
-		// }
+				response.EnsureSuccessStatusCode ();
+				Assert.AreEqual (redirectedURI, response.RequestMessage.RequestUri, "Invalid redirected URI");
+			}
+		}
 
 		[TestFixture]
 		public class AndroidClientHandlerTests : AndroidHandlerTestBase
