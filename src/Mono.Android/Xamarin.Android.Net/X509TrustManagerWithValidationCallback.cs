f0deddac10cba93618f23a0dbca433983bfcc3c7
Simon Rozsival <simon@rozsival.com>
2022-01-27 11:37:49 +0100

    Change spaces to tabs

--- src/Mono.Android/Xamarin.Android.Net/X509TrustManagerWithValidationCallback.cs
+++ src/Mono.Android/Xamarin.Android.Net/X509TrustManagerWithValidationCallback.cs
@@ -14,84 +14,84 @@ using JavaX509Certificate = Java.Security.Cert.X509Certificate;
 
 namespace Xamarin.Android.Net
 {
-    [Register ("xamarin/android/net/X509TrustManagerWithValidationCallback")]
-    internal sealed class X509TrustManagerWithValidationCallback : Java.Lang.Object, IX509TrustManager
-    {
-        private readonly IX509TrustManager? _internalTrustManager;
-        private readonly HttpRequestMessage _request;
-        private readonly Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> _serverCertificateCustomValidationCallback;
+	[Register ("xamarin/android/net/X509TrustManagerWithValidationCallback")]
+	internal sealed class X509TrustManagerWithValidationCallback : Java.Lang.Object, IX509TrustManager
+	{
+		private readonly IX509TrustManager? _internalTrustManager;
+		private readonly HttpRequestMessage _request;
+		private readonly Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> _serverCertificateCustomValidationCallback;
 
-        public static ITrustManager[] Inject(
-            ITrustManager[]? trustManagers,
-            HttpRequestMessage requestMessage,
-            Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> serverCertificateCustomValidationCallback)
-        {
-            IX509TrustManager? x509TrustManager = trustManagers?.OfType<IX509TrustManager> ().FirstOrDefault ();
-            IEnumerable<ITrustManager> otherTrustManagers = trustManagers?.Where (manager => manager != x509TrustManager) ?? Enumerable.Empty<ITrustManager> ();
-            var trustManagerWithCallback = new X509TrustManagerWithValidationCallback (x509TrustManager, requestMessage, serverCertificateCustomValidationCallback);
-            return otherTrustManagers.Append (trustManagerWithCallback).ToArray ();
-        }
+		public static ITrustManager[] Inject(
+			ITrustManager[]? trustManagers,
+			HttpRequestMessage requestMessage,
+			Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> serverCertificateCustomValidationCallback)
+		{
+			IX509TrustManager? x509TrustManager = trustManagers?.OfType<IX509TrustManager> ().FirstOrDefault ();
+			IEnumerable<ITrustManager> otherTrustManagers = trustManagers?.Where (manager => manager != x509TrustManager) ?? Enumerable.Empty<ITrustManager> ();
+			var trustManagerWithCallback = new X509TrustManagerWithValidationCallback (x509TrustManager, requestMessage, serverCertificateCustomValidationCallback);
+			return otherTrustManagers.Append (trustManagerWithCallback).ToArray ();
+		}
 
-        public X509TrustManagerWithValidationCallback(
-            IX509TrustManager? internalTrustManager,
-            HttpRequestMessage request,
-            Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> serverCertificateCustomValidationCallback)
-            : base (
-                JNIEnv.StartCreateInstance ("xamarin/android/net/X509TrustManagerWithValidationCallback", "()V"),
-                JniHandleOwnership.TransferLocalRef)
-        {
-            JNIEnv.FinishCreateInstance (Handle, "()V");
+		public X509TrustManagerWithValidationCallback(
+			IX509TrustManager? internalTrustManager,
+			HttpRequestMessage request,
+			Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> serverCertificateCustomValidationCallback)
+			: base (
+				JNIEnv.StartCreateInstance ("xamarin/android/net/X509TrustManagerWithValidationCallback", "()V"),
+				JniHandleOwnership.TransferLocalRef)
+		{
+			JNIEnv.FinishCreateInstance (Handle, "()V");
 
-            _request = request;
-            _internalTrustManager = internalTrustManager;
-            _serverCertificateCustomValidationCallback = serverCertificateCustomValidationCallback;
-        }
+			_request = request;
+			_internalTrustManager = internalTrustManager;
+			_serverCertificateCustomValidationCallback = serverCertificateCustomValidationCallback;
+		}
 
-        public void CheckServerTrusted (JavaX509Certificate[] javaChain, string authType)
-        {
-            var sslPolicyErrors = SslPolicyErrors.None;
-            var certificates = ConvertCertificates (javaChain);
+		public void CheckServerTrusted (JavaX509Certificate[] javaChain, string authType)
+		{
+			var sslPolicyErrors = SslPolicyErrors.None;
+			var certificates = ConvertCertificates (javaChain);
 
-            try
-            {
-                _internalTrustManager?.CheckServerTrusted (javaChain, authType);
-            }
-            catch (JavaCertificateException)
-            {
-                sslPolicyErrors |= SslPolicyErrors.RemoteCertificateChainErrors;
-            }
+			try
+			{
+				_internalTrustManager?.CheckServerTrusted (javaChain, authType);
+			}
+			catch (JavaCertificateException)
+			{
+				sslPolicyErrors |= SslPolicyErrors.RemoteCertificateChainErrors;
+			}
 
-            X509Certificate2? certificate = certificates.FirstOrDefault ();
-            using X509Chain chain = CreateChain (certificates);
+			X509Certificate2? certificate = certificates.FirstOrDefault ();
+			using X509Chain chain = CreateChain (certificates);
 
-            if (certificate == null)
-            {
-                sslPolicyErrors |= SslPolicyErrors.RemoteCertificateNotAvailable;
-            }
+			if (certificate == null)
+			{
+				sslPolicyErrors |= SslPolicyErrors.RemoteCertificateNotAvailable;
+			}
 
-            if (!_serverCertificateCustomValidationCallback (_request, certificate, chain, sslPolicyErrors))
-            {
-                throw new JavaCertificateException("The remote certificate was rejected by the provided RemoteCertificateValidationCallback.");
-            }
-        }
+			if (!_serverCertificateCustomValidationCallback (_request, certificate, chain, sslPolicyErrors))
+			{
+				throw new JavaCertificateException("The remote certificate was rejected by the provided RemoteCertificateValidationCallback.");
+			}
+		}
 
-        public void CheckClientTrusted (JavaX509Certificate[] chain, string authType)
-            => _internalTrustManager?.CheckClientTrusted (chain, authType);
+		public void CheckClientTrusted (JavaX509Certificate[] chain, string authType)
+			=> _internalTrustManager?.CheckClientTrusted (chain, authType);
 
-        public JavaX509Certificate[] GetAcceptedIssuers ()
-            => _internalTrustManager?.GetAcceptedIssuers () ?? Array.Empty<JavaX509Certificate> ();
+		public JavaX509Certificate[] GetAcceptedIssuers ()
+			=> _internalTrustManager?.GetAcceptedIssuers () ?? Array.Empty<JavaX509Certificate> ();
 
-        private static X509Chain CreateChain (X509Certificate2[] certificates)
-        {
-            // TODO I doubt this is the correct way to implement this and this whole method needs revisiting
-            var chain = new X509Chain ();
-            chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
-            chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
-            chain.ChainPolicy.ExtraStore.AddRange (certificates);
-            return chain;
-        }
+		private static X509Chain CreateChain (X509Certificate2[] certificates)
+		{
+			// TODO I doubt this is the correct way to implement this and this whole method needs revisiting
+			var chain = new X509Chain ();
+			chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+			chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+			chain.ChainPolicy.ExtraStore.AddRange (certificates);
+			return chain;
+		}
 
-        private static X509Certificate2[] ConvertCertificates (JavaX509Certificate[] certificates)
-            => certificates.Select (cert => new X509Certificate2 (cert.GetEncoded ()!)).ToArray ();
-    }
+		private static X509Certificate2[] ConvertCertificates (JavaX509Certificate[] certificates)
+			=> certificates.Select (cert => new X509Certificate2 (cert.GetEncoded ()!)).ToArray ();
+	}
 }
