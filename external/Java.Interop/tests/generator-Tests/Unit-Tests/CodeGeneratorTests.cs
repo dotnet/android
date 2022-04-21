dotnet/android

commit e48c48c802d24cf7abffc7db0e9f82d9e1804036

[Java.Base, generator] Bind all of package java.lang (#966)
Context: d0996b0e73b99aaccff566e60dfa8c215174ac4e

Bind all classes and interfaces in the `java.lang` package.

Alter Java array binding, so that instead of `IList<T>`, we get
"direct" Java arrays, e.g.

	namespace Java.Lang {
	    partial class Character {
	        // Previous/Xamarin.Android-like
	        public static int CodePointAt (IList<char>? a, int index);

	        // New/Desktop
	        public static int CodePointAt (JavaCharArray? a, int index);
	    }
	}

Rationale: it *allows* for more efficient JVM :: .NET array copying,
by making copies explicit (to the dev), not implicit.  We can
add an implicit conversion from e.g. `IEnumerable<char>` to
`JavaCharArray` in the future, if deemed useful.

This also impacts method return types, properties, and fields.

Bind the `java.lang.module` package in the namespace
`Java.Lang.Modules`.  This is to avoid a type/namespace conflict
with `java.lang.Module`, bound as `Java.Lang.Module`.

Continue updating `generator` to remove "Android-isms".

Update `Java.Base.csproj` to ignore [warning CS0108][0]:

	Java.Lang.Reflect.IAnnotatedArrayType.cs(15,45):
	warning CS0108: 'IAnnotatedArrayType.AnnotatedOwnerType' hides inherited member
	'IAnnotatedType.AnnotatedOwnerType'. Use the new keyword if hiding was intended.

The problem here is that we have:

	public partial interface IAnnotatedType {
	    // Contains default interface method
	    virtual unsafe IAnnotatedType? AnnotatedOwnerType => …;
	}
	public partial interface IAnnotatedArrayType : IAnnotatedType {
	    // Contains *no* method body; re-abstracted
	    IAnnotatedType? AnnotatedOwnerType {get;}
	}

TODO: figure out how to properly fix this.  `managedOverride`
metadata (97f1144d) doesn't seem useful to "re-abstract" a default
interface member.

Update `Java.Base.targets` to use `generator --global`.  This is so

external/Java.Interop/tools/generator/SourceWriters/BoundFieldAsProperty.cs
@@ -24,7 +24,15 @@ namespace generator.SourceWriters
 
 			Name = field.Name;
 
-			var fieldType = field.Symbol.IsArray ? "IList<" + field.Symbol.ElementType + ">" + opt.NullableOperator : opt.GetTypeReferenceName (field);
+			string fieldType;
+			if (opt.CodeGenerationTarget == CodeGenerationTarget.JavaInterop1) {
+				fieldType = opt.GetTypeReferenceName (field);
+			} else {
+				fieldType = field.Symbol.IsArray
+					? "IList<" + field.Symbol.ElementType + ">" + opt.NullableOperator
+					: opt.GetTypeReferenceName (field);
+			}
+
 			PropertyType = new TypeReferenceWriter (fieldType);
 
 			field.JavadocInfo?.AddJavadocs (Comments);
@@ -85,6 +93,18 @@ namespace generator.SourceWriters
 
 			writer.WriteLine ($"var __v = {field.Symbol.ReturnCast}_members.{indirect}.{invoke} (__id{(field.IsStatic ? "" : ", this")});");
 
+			if (opt.CodeGenerationTarget == CodeGenerationTarget.JavaInterop1) {
+				if (field.Symbol.NativeType == field.Symbol.FullName) {
+					writer.WriteLine ("return __v;");
+					return;
+				}
+				writer.Write ("return global::Java.Interop.JniEnvironment.Runtime.ValueManager.GetValue<");
+				PropertyType.WriteTypeReference (writer);
+				writer.Write (">(ref __v, JniObjectReferenceOptions.Copy)");
+				writer.WriteLine (";");
+				return;
+			}
+
 			if (field.Symbol.IsArray) {
 				writer.WriteLine ($"return global::Android.Runtime.JavaArray<{opt.GetOutputName (field.Symbol.ElementType)}>.FromJniHandle (__v.Handle, JniHandleOwnership.TransferLocalRef);");
 			} else if (field.Symbol.NativeType != field.Symbol.FullName) {
@@ -102,12 +122,17 @@ namespace generator.SourceWriters
 			var invokeType = SourceWriterExtensions.GetInvokeType (field.GetMethodPrefix);
 			var indirect = field.IsStatic ? "StaticFields" : "InstanceFields";
 
+			string native_arg = opt.GetSafeIdentifier (TypeNameUtilities.GetNativeName ("value"));
 			string arg;
 			bool have_prep = false;
 
 			if (field.Symbol.IsArray) {
-				arg = opt.GetSafeIdentifier (TypeNameUtilities.GetNativeName ("value"));
-				writer.WriteLine ($"IntPtr {arg} = global::Android.Runtime.JavaArray<{opt.GetOutputName (field.Symbol.ElementType)}>.ToLocalJniHandle (value);");
+				if (opt.CodeGenerationTarget == CodeGenerationTarget.JavaInterop1) {
+					arg = "value";
+				} else {
+					arg = native_arg;
+					writer.WriteLine ($"IntPtr {native_arg} = global::Android.Runtime.JavaArray<{opt.GetOutputName (field.Symbol.ElementType)}>.ToLocalJniHandle (value);");
+				}
 			} else {
 				foreach (var prep in field.SetParameters.GetCallPrep (opt)) {
 					have_prep = true;
@@ -116,27 +141,45 @@ namespace generator.SourceWriters
 
 				arg = field.SetParameters [0].ToNative (opt);
 
-				if (field.SetParameters.HasCleanup && !have_prep) {
-					arg = opt.GetSafeIdentifier (TypeNameUtilities.GetNativeName ("value"));
-					writer.WriteLine ($"IntPtr {arg} = global::Android.Runtime.JNIEnv.ToLocalJniHandle (value);");
+				if (opt.CodeGenerationTarget != CodeGenerationTarget.JavaInterop1 &&
+						field.SetParameters.HasCleanup &&
+						!have_prep) {
+					writer.WriteLine ($"IntPtr {native_arg} = global::Android.Runtime.JNIEnv.ToLocalJniHandle (value);");
 				}
 			}
 
 			writer.WriteLine ("try {");
+			writer.Write ($"\t_members.{indirect}.SetValue (__id{(field.IsStatic ? "" : ", this")}, ");
 
-			writer.WriteLine ($"\t_members.{indirect}.SetValue (__id{(field.IsStatic ? "" : ", this")}, {(invokeType != "Object" ? arg : "new JniObjectReference (" + arg + ")")});");
+			if (opt.CodeGenerationTarget == CodeGenerationTarget.JavaInterop1) {
+				writer.WriteLine ($"{(invokeType != "Object" ? arg : $"{arg}?.PeerReference ?? default")});");
+			} else {
+				writer.WriteLine ($"{(invokeType != "Object" ? arg : "new JniObjectReference (" + arg + ")")});");
+			}
+			writer.WriteLine ();
 
 			writer.WriteLine ("} finally {");
 			writer.Indent ();
 
 			if (field.Symbol.IsArray) {
-				writer.WriteLine ($"global::Android.Runtime.JNIEnv.DeleteLocalRef ({arg});");
+				if (opt.CodeGenerationTarget != CodeGenerationTarget.JavaInterop1) {
+					writer.WriteLine ($"global::Android.Runtime.JNIEnv.DeleteLocalRef ({arg});");
+				}
 			} else {
 				foreach (var cleanup in field.SetParameters.GetCallCleanup (opt))
 					writer.WriteLine (cleanup);
 				if (field.SetParameters.HasCleanup && !have_prep) {
-					writer.WriteLine ($"global::Android.Runtime.JNIEnv.DeleteLocalRef ({arg});");
+					if (opt.CodeGenerationTarget != CodeGenerationTarget.JavaInterop1) {
+						writer.WriteLine ($"global::Android.Runtime.JNIEnv.DeleteLocalRef ({arg});");
+					}
 				}
+			}
+
+			if (opt.CodeGenerationTarget == CodeGenerationTarget.JavaInterop1 &&
+					field.Symbol.JniName != null &&
+					field.Symbol.JniName.Length > 1 &&
+					(field.Symbol.JniName[0] == 'L' || field.Symbol.JniName[0] == '[')) {
+				writer.WriteLine ($"GC.KeepAlive (value);");
 			}
 
 			writer.Unindent ();

external/Java.Interop/tools/generator/SourceWriters/Extensions/SourceWriterExtensions.cs
@@ -352,7 +352,7 @@ namespace generator.SourceWriters
 					writer.WriteLine ($"var {pname} = {p.Name} == null ? null : new global::Java.Lang.String ({p.Name});");
 				} else if (p.Type == "Java.Lang.ICharSequence[]" || p.Type == "params Java.Lang.ICharSequence[]") {
 					pname = p.GetName ("jlca_");
-					writer.WriteLine ($"var {pname} = CharSequence.ArrayFromStringArray({p.Name});");
+					writer.WriteLine ($"var {pname} = {opt.GetStringArrayToCharSequenceArrayMethodName ()} ({p.Name});");
 				}
 
 				if (call.Length > 0)
@@ -367,7 +367,7 @@ namespace generator.SourceWriters
 				case "void":
 					break;
 				case "Java.Lang.ICharSequence[]":
-					writer.WriteLine ("var __rsval = CharSequence.ArrayToStringArray (__result);");
+					writer.WriteLine ($"var __rsval = {opt.GetStringArrayToCharSequenceArrayMethodName ()} (__result);");
 					break;
 				case "Java.Lang.ICharSequence":
 					writer.WriteLine ("var __rsval = __result?.ToString ();");

external/Java.Interop/tests/generator-Tests/Unit-Tests/SupportTypes.cs
@@ -221,14 +221,16 @@ namespace generatortests
 
 			var ctor_name = className.Contains ('.') ? className.Substring (className.LastIndexOf ('.') + 1) : className;
 			@class.Ctors.Add (CreateConstructor (@class, ctor_name, options));
-			@class.Ctors.Add (CreateConstructor (@class, ctor_name, options, new Parameter ("p0", "java.lang.String", "string", false)));
+			@class.Ctors.Add (CreateConstructor (@class, ctor_name, options,
+				new Parameter ("p0", "java.lang.String", options.SymbolTable.Lookup ("java.lang.String", null).FullName, false)));
 
 			@class.Properties.Add (CreateProperty (@class, "Count", "int", options));
 			@class.Properties.Add (CreateProperty (@class, "Key", "java.lang.String", options));
 			@class.Properties.Add (CreateProperty (@class, "StaticCount", "int", options, true));
 			@class.Properties.Add (CreateProperty (@class, "AbstractCount", "int", options, false, true));
 
-			@class.Methods.Add (CreateMethod (@class, "GetCountForKey", options, "int", false, parameters: new Parameter ("key", "java.lang.String", "string", false)));
+			@class.Methods.Add (CreateMethod (@class, "GetCountForKey", options, "int", false,
+				parameters: new Parameter ("key", "java.lang.String", options.SymbolTable.Lookup ("java.lang.String", null).FullName, false)));
 			@class.Methods.Add (CreateMethod (@class, "Key", options, "java.lang.String"));
 			@class.Methods.Add (CreateMethod (@class, "StaticMethod", options, "void", true));
 			@class.Methods.Add (CreateMethod (@class, "AbstractMethod", options, "void", false, true));
@@ -273,7 +275,8 @@ namespace generatortests
 			iface.Properties.Add (CreateProperty (iface, "StaticCount", "int", options, true));
 			iface.Properties.Add (CreateProperty (iface, "AbstractCount", "int", options, false, true));
 
-			iface.Methods.Add (CreateMethod (iface, "GetCountForKey", options, "int", false, parameters: new Parameter ("key", "java.lang.String", "string", false)));
+			iface.Methods.Add (CreateMethod (iface, "GetCountForKey", options, "int", false,
+				parameters: new Parameter ("key", "java.lang.String", options.SymbolTable.Lookup ("java.lang.String", null).FullName, false)));
 			iface.Methods.Add (CreateMethod (iface, "Key", options, "java.lang.String"));
 			iface.Methods.Add (CreateMethod (iface, "StaticMethod", options, "void", true));
 			iface.Methods.Add (CreateMethod (iface, "AbstractMethod", options, "void", false, true));
@@ -313,7 +316,7 @@ namespace generatortests
 		{
 			var list = new ParameterList {
 				new Parameter ("value", "int", "int", false),
-				new Parameter ("str", "java.lang.String", "string", false),
+				new Parameter ("str", "java.lang.String", options.SymbolTable.Lookup ("java.lang.String", null).FullName, false),
 				new Parameter ("flag", "int", "OptionTypes", true)
 			};
 

commit 98d4d4d83aa48a061ca0be83f012354f395192e4

[generator] Fix xamarin-android/src/Mono.Android build (#972)
Context: e48c48c802d24cf7abffc7db0e9f82d9e1804036
Context: https://github.com/xamarin/xamarin-android/pull/6939

xamarin/xamarin-android#6939 attempted to bump to 0575eb81, which
promptly broke the build of src/Mono.Android, e.g.
`src/Mono.Android/obj/Debug/net6.0/android-32/mcw/Android.Widget.GridLayout.cs`:

	/*       */ partial class GridLayout {
	/*       */     partial class partial class LayoutParams {
	/*       */         [Register ("rowSpec")]
	/*       */         public Android.Widget.GridLayout.Spec? RowSpec {
	/*       */             get {
	/*       */                 const string __id = "rowSpec.Landroid/widget/GridLayout$Spec;";
	/*       */                 var __v = _members.InstanceFields.GetObjectValue (__id, this);
	/*       */                 return global::Java.Lang.Object.GetObject<Android.Widget.GridLayout.Spec> (__v.Handle, JniHandleOwnership.TransferLocalRef);
	/*       */             }
	/*       */             set {
	/*       */                 const string __id = "rowSpec.Landroid/widget/GridLayout$Spec;";
	/*       */                 IntPtr native_value = global::Android.Runtime.JNIEnv.ToLocalJniHandle (value);
	/*       */                 try {
	/* L 239 */                     _members.InstanceFields.SetValue (__id, this, new JniObjectReference (value));
	/*       */                 } finally {
	/* L 242 */                     global::Android.Runtime.JNIEnv.DeleteLocalRef (value);
	/*       */                 }
	/*       */             }
	/*       */         }
	/*       */     }
	/*       */ }

due to compilation errors:

	src/Mono.Android/obj/Debug/net6.0/android-32/mcw/Android.Widget.GridLayout.cs(239,77):
	  error CS1503: Argument 1: cannot convert from 'Android.Widget.GridLayout.Spec' to 'System.IntPtr'
	src/Mono.Android/obj/Debug/net6.0/android-32/mcw/Android.Widget.GridLayout.cs(242,54):
	  error CS1503: Argument 1: cannot convert from 'Android.Widget.GridLayout.Spec' to 'System.IntPtr'

This was caused by `BoundFieldAsProperty.cs` not appropriately
setting `arg` to `native_arg`, so that the correct variable would be
cleaned up in the `finally` block.

There was another set of errors:

	src/Mono.Android/obj/Release/net6.0/android-32/mcw/Android.Widget.ArrayAdapter.cs(525,53):
	  error CS1503: Argument 1: cannot convert from 'Java.Lang.ICharSequence[]' to 'string[]?'

which was also caused by e48c48c8 mis-refactoring

external/Java.Interop/tools/generator/SourceWriters/BoundFieldAsProperty.cs
@@ -144,11 +144,13 @@ namespace generator.SourceWriters
 				if (opt.CodeGenerationTarget != CodeGenerationTarget.JavaInterop1 &&
 						field.SetParameters.HasCleanup &&
 						!have_prep) {
+					arg = native_arg;
 					writer.WriteLine ($"IntPtr {native_arg} = global::Android.Runtime.JNIEnv.ToLocalJniHandle (value);");
 				}
 			}
 
 			writer.WriteLine ("try {");
+
 			writer.Write ($"\t_members.{indirect}.SetValue (__id{(field.IsStatic ? "" : ", this")}, ");
 
 			if (opt.CodeGenerationTarget == CodeGenerationTarget.JavaInterop1) {
@@ -161,7 +163,6 @@ namespace generator.SourceWriters
 			} else {
 				writer.WriteLine ($"{(invokeType != "Object" ? arg : "new JniObjectReference (" + arg + ")")});");
 			}
-			writer.WriteLine ();
 
 			writer.WriteLine ("} finally {");
 			writer.Indent ();

external/Java.Interop/tools/generator/SourceWriters/Extensions/SourceWriterExtensions.cs
@@ -367,7 +367,7 @@ namespace generator.SourceWriters
 				case "void":
 					break;
 				case "Java.Lang.ICharSequence[]":
-					writer.WriteLine ($"var __rsval = {opt.GetStringArrayToCharSequenceArrayMethodName ()} (__result);");
+					writer.WriteLine ("var __rsval = CharSequence.ArrayToStringArray (__result);");
 					break;
 				case "Java.Lang.ICharSequence":
 					writer.WriteLine ("var __rsval = __result?.ToString ();");

external/Java.Interop/tests/generator-Tests/Unit-Tests/CodeGeneratorTests.cs
@@ -1053,6 +1053,26 @@ namespace generatortests
 		}
 
 		[Test]
+		public void WriteObjectField ()
+		{
+			options.SymbolTable.AddType (new TestClass (null, "Java.Lang.Object"));
+			var eClass = new TestClass ("Java.Lang.Object", "java.code.Example");
+			options.SymbolTable.AddType (eClass);
+
+			var @class = new TestClass ("Object", "java.code.MyClass");
+
+			var field   = SupportTypeBuilder.CreateField ("field", options, "java.code.Example");
+
+			@class.Fields.Add (field);
+
+			generator.Context.ContextTypes.Push (@class);
+			generator.WriteType (@class, string.Empty, new GenerationInfo ("", "", "MyAssembly"));
+			generator.Context.ContextTypes.Pop ();
+
+			AssertTargetedExpected (nameof (WriteObjectField), writer.ToString ());
+		}
+
+		[Test]
 		public void WriteInterface ()
 		{
 			var iface = SupportTypeBuilder.CreateInterface ("java.code.IMyInterface", options);
@@ -1156,6 +1176,33 @@ namespace generatortests
 			Assert.AreEqual (1, handlers.Count);
 			Assert.AreEqual ("GetCountForKey", handlers [0]);
 			AssertOriginalExpected (nameof (WriteInterfaceEventHandlerImplContent), writer.ToString ());
+		}
+
+		[Test]
+		public void WriteMethodWithCharSequenceArrays ()
+		{
+			var gens = ParseApiDefinition (@"<api>
+			  <package name='java.lang' jni-name='java/lang'>
+			    <class abstract='false' deprecated='not deprecated' final='false' name='Object' static='false' visibility='public' jni-signature='Ljava/lang/Object;' />
+			  </package>
+
+			  <package name='com.example' jni-name='com/example'>
+			    <class abstract='false' deprecated='not deprecated' extends='java.lang.Object' extends-generic-aware='java.lang.Object' jni-extends='Ljava/lang/Object;' final='false' name='MyClass' static='false' visibility='public' jni-signature='Lcom/example/MyClass;'>
+			      <method abstract='false' deprecated='not deprecated' final='false' name='echo' jni-signature='([Ljava/lang/Charsequence;])[Ljava/lang/CharSequence;' bridge='false' native='false' return='java.lang.CharSequence[]' jni-return='[Ljava/lang/CharSequence;' static='false' synchronized='false' synthetic='false' visibility='public'>
+			        <parameter name='messages' type='java.lang.CharSequence[]' jni-type='[Ljava/lang/CharSequence;'>
+			        </parameter>
+			      </method>
+			    </class>
+			  </package>
+			</api>");
+
+			var @class = gens.OfType<ClassGen> ().First (c => c.Name == "MyClass");
+
+			generator.Context.ContextTypes.Push (@class);
+			generator.WriteType (@class, string.Empty, new GenerationInfo ("", "", "MyAssembly"));
+			generator.Context.ContextTypes.Pop ();
+
+			AssertOriginalTargetExpected (nameof (WriteMethodWithCharSequenceArrays), writer.ToString ());
 		}
 
 		[Test]

external/Java.Interop/tests/generator-Tests/Unit-Tests/SupportTypes.cs
@@ -266,6 +266,18 @@ namespace generatortests
 			return iface;
 		}
 
+		public static TestField CreateField (string fieldName, CodeGenerationOptions options, string fieldType, bool isStatic = false)
+		{
+			var field = new TestField (fieldType, fieldName);
+
+			if (isStatic)
+				field.SetStatic ();
+
+			field.Validate (options, null, new CodeGeneratorContext ());
+
+			return field;
+		}
+
 		public static TestInterface CreateInterface (string interfaceName, CodeGenerationOptions options)
 		{
 			var iface = CreateEmptyInterface (interfaceName);