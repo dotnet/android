e6bd561f0c517ef2b05c835108d666577e5c3ff1
Dean Ellis <dellis1972@googlemail.com>
2022-05-19 11:03:24 +0100

    Add .NET 6+ template for Android wear
    Fixes #7003
    
    This speaks for itself, lets add a template for Wear applications.

---
+++ src/Microsoft.Android.Templates/android-wear/MainActivity.cs
@@ -0,0 +1,13 @@
+namespace AndroidApp1;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+    }
+}
