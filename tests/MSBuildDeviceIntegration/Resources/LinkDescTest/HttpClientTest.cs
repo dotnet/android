8e6eadb36c5e09f69838aa1b7b558ad69d000200
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-06-01 15:12:04 -0500

    [tests] add linker test that makes HttpClient request (#6891)
    Context: https://github.com/dotnet/runtime/issues/67402
    
    This is a new regression test that should catch a problem like
    dotnet/runtime#67402 going forward.
    
    This code runs in a class library that gets trimmed.
    `Mono.Android-Tests` has many tests including usage of
    `HttpWebRequest`, that I believe avoids the problem.

--- tests/MSBuildDeviceIntegration/Resources/LinkDescTest/HttpClientTest.cs
+++ tests/MSBuildDeviceIntegration/Resources/LinkDescTest/HttpClientTest.cs
@@ -1,19 +1,24 @@
 using System;
+using System.Text;
 using System.Net.Http;
 
 public class HttpClientTest
 {
 	// [Test]
-	public static string NewHttpClient()
+	public static string Post ()
 	{
 		try
 		{
-			new HttpClient();
-			return $"[PASS] {nameof(NewHttpClient)}";
+			var client = new HttpClient ();
+			var data = new StringContent ("{\"foo\": \"bar\" }", Encoding.UTF8, "application/json");
+			var response = client.PostAsync ("https://httpbin.org/post", data).Result;
+			response.EnsureSuccessStatusCode ();
+			var json = response.Content.ReadAsStringAsync ().Result;
+			return $"[PASS] {nameof (HttpClientTest)}.{nameof (Post)}";
 		}
 		catch (Exception ex)
 		{
-			return $"[FAIL] {nameof(NewHttpClient)} FAILED: {ex}";
+			return $"[FAIL] {nameof (HttpClientTest)}.{nameof (Post)} FAILED: {ex}";
 		}
 	}
 }
