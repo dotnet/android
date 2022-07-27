de836a82bd0c7114229dc2a23a536b04e0c9bb71
Jonathan Pryor <jonpryor@vt.edu>
2022-07-27 12:46:47 -0400

    [Mono.Android] Remove System.Linq usage (#7210)
    Fixes: https://github.com/xamarin/xamarin-android/issues/5442
    
    Remove all `using System.Linq` statements and all use of LINQ
    extension methods from `Mono.Android.dll`.
    
    Primary motivation was to reduce `.apk` size (09baa52e), and we don't
    want to inadvertently allow LINQ usage back in…

--- src/Mono.Android/Android.Runtime/AndroidRuntime.cs
+++ src/Mono.Android/Android.Runtime/AndroidRuntime.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
