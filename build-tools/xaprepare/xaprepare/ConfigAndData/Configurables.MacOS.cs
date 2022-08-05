51acc438ef547c3ad3b2a8113a014c3539c231cd
Jonathan Peppers <jonathan.peppers@microsoft.com>
2022-08-05 16:07:12 -0500

    [build] update to latest JDKs (#7236)
    Context: https://docs.microsoft.com/java/openjdk/download#openjdk-11016-lts--see-previous-releases
    Context: https://adoptium.net/temurin/releases/?version=8
    
    The IDE teams are shipping these updates in Visual Studio shortly.
    
    Update to these builds, so we can test them out.

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.MacOS.cs
@@ -4,8 +4,11 @@ namespace Xamarin.Android.Prepare
 {
 	partial class Configurables
 	{
+		const string AdoptOpenJDKUpdate = "332";
+		const string AdoptOpenJDKBuild = "b09";
+
 		const string JetBrainsOpenJDKOperatingSystem = "osx-x64";
-		const string MicrosoftOpenJDKOperatingSystem = "macOS/x64";
+		const string MicrosoftOpenJDKOperatingSystem = "macOS-x64";
 		const string AdoptOpenJDKOperatingSystem = "x64_mac";
 
 		partial class Defaults
