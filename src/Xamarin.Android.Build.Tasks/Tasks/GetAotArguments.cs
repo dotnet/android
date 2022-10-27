523a2bbe956f6253d85649d6572b43b8476971ef
Marek Habersack <grendel@twistedcode.net>
2022-10-27 19:48:22 +0200

    Close yer parentheses, fool

--- src/Xamarin.Android.Build.Tasks/Tasks/GetAotArguments.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/GetAotArguments.cs
@@ -300,7 +300,7 @@ namespace Xamarin.Android.Tasks
 					// the `binutils` directory is one level down (${OS}/binutils) than the Windows one
 					relPath = Path.Combine (relPath, "..");
 				}
-				string libstubsPath = Path.GetFullPath (Path.Combine (AndroidBinUtilsDirectory, relPath, "libstubs", ArchToRid (arch));
+				string libstubsPath = Path.GetFullPath (Path.Combine (AndroidBinUtilsDirectory, relPath, "libstubs", ArchToRid (arch)));
 				libs.Add (Path.Combine (libstubsPath, "libc.so"));
 				libs.Add (Path.Combine (libstubsPath, "libm.so"));
 			}
