e8a03d3a182defa8a930bc34cd04ffae80e6bc35
Marek Habersack <grendel@twistedcode.net>
2022-10-27 23:37:32 +0200

    Revert "Revert "Bump to dotnet/installer/main@d5520378 8.0.200-alpha.1.22511.1 (#7451)""
    This reverts commit 87a4e8b4cc1b57fd31a48b9ac7b5bed69630cdb2.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/DotNetStandard.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/DotNetStandard.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,6 +34,25 @@ namespace Xamarin.ProjectTools
 			get { return GetProperty ("TargetFramework"); }
 			set { SetProperty ("TargetFramework", value); }
 		}
+
+		/// <summary>
+		/// Projects targeting net6.0/net7.0 require ref/runtime packs on NuGet.org or dotnet6/dotnet7
+		/// </summary>
+		public void AddNuGetSourcesForOlderTargetFrameworks ()
+		{
+			if (TargetFramework.IndexOf ("net6.0", StringComparison.OrdinalIgnoreCase) != -1) {
+				ExtraNuGetConfigSources = new List<string> {
+					"https://api.nuget.org/v3/index.json",
+					"https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json",
+				};
+			} else if (TargetFramework.IndexOf ("net7.0", StringComparison.OrdinalIgnoreCase) != -1) {
+				ExtraNuGetConfigSources = new List<string> {
+					"https://api.nuget.org/v3/index.json",
+					"https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet7/nuget/v3/index.json",
+				};
+			}
+		}
+
 		public string Sdk { get; set; }
 
 		public IList<BuildItem> OtherBuildItems { get; private set; }
