837480c17cb68e2362670be4eef77c06f08c0892
Dean Ellis <dellis1972@googlemail.com>
2022-11-04 12:41:07 +0000

    update vswhere.cmd to also include prerelease

--- build-tools/scripts/vswhere.cmd
+++ build-tools/scripts/vswhere.cmd
@@ -1,2 +1,7 @@
 @echo off
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
+SET result = ""
+FOR /F "tokens=1 delims=" %%F IN ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath') DO SET result=%%F
+if "%result%" == "" (
+    FOR /F "tokens=1 delims=" %%F IN ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath') DO SET result=%%F
+)
+echo %result%
