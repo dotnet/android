311b41e864a0162895d079477cb9398fbec5ca6e
Dean Ellis <dellis1972@googlemail.com>
2022-12-12 19:59:29 +0000

    [Xamarin.Android.Build.Tasks] Add XA1031 error (#7448)
    Fixes: https://github.com/xamarin/xamarin-android/issues/7326
    
    Commit d3e87674 added a requirement that, under .NET 6+, the
    `$(AndroidHttpClientHandlerType)` MUST inherit from
    [`System.Net.Http.HttpMessageHandler`][0].  This was unfortunately a
    change from Classic, which allowed/required that
    [`System.Net.Http.HttpClientHandler`][1] be the base class.
    Allowing `HttpClientHandler` to be in the inheritance hierarchy for
    `$(AndroidHttpClientHandlerType)` would result in a
    `NullReferenceException` at runtime under .NET 6.
    
    Commit d3e87674 contained a TODO:
    
    > TODO: [Emit a build-time warning][2] if
    > `$(AndroidHttpClientHandlerType)` is set to an invalid value
    
    Implement this TODO: add a build-time check that verifies that
    `$(AndroidHttpClientHandlerType)` is a valid type for the target,
    e.g. is an `HttpMessageHandler` subclass on .NET 6.
    
    If `$(AndroidHttpClientHandlerType)` is not a valid type, then an
    XA1031 error is raised:
    
    	error XA1031: The 'AndroidHttpClientHandlerType' has an invalid value of '{0}' please check your project settings.
    
    Additionally, if `$(AndroidHttpClientHandlerType)` is
    `System.Net.Http.SocketsHttpHandler, System.Net.Http`, *and*
    `$(UseNativeHttpHandler)` isn't set, then
    set `$(UseNativeHttpHandler)` to false.  This is necessary to ensure
    that `SocketsHttpHandler` is preserved by the linker.
    
    [0]: https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpmessagehandler?view=net-6.0
    [1]: https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpclienthandler?view=net-6.0
    [2]: https://github.com/xamarin/xamarin-android/issues/7326

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/CheckClientHandlerType.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+using Microsoft.Android.Build.Tasks;
+using Java.Interop.Tools.Cecil;
+using Mono.Cecil;
+
+namespace Xamarin.Android.Tasks
+{
+	public class CheckClientHandlerType : AndroidTask
+	{
+		public override string TaskPrefix => "CCHT";
+
+		[Required]
+		public string ClientHandlerType { get; set; }
+		[Required]
+		public string ValidHandlerType { get; set; }
+		[Required]
+		public ITaskItem[] ResolvedAssemblies { get; set; }
+
+		public override bool RunTask ()
+		{
+			string[] types = ClientHandlerType.Split (',');
+			string type = types[0].Trim ();
+			string assembly = "Mono.Android";
+
+			if (types.Length > 1) {
+				assembly = types[1].Trim ();
+			}
+			// load the assembly.
+			ITaskItem foundAssembly = null;
+			foreach (var asm in ResolvedAssemblies) {
+				string filename = Path.GetFileNameWithoutExtension (asm.ItemSpec);
+				if (string.CompareOrdinal (assembly, filename) == 0) {
+					foundAssembly = asm;
+					break;
+				}
+			}
+			if (foundAssembly == null) {
+				Log.LogCodedError ("XA1033", Xamarin.Android.Tasks.Properties.Resources.XA1033, assembly);
+				return !Log.HasLoggedErrors;
+			}
+			// find the type.
+			var readerParameters = new ReaderParameters {
+				ReadSymbols = false,
+			};
+			using (var resolver = new DirectoryAssemblyResolver (this.CreateTaskLogger (), loadDebugSymbols: false, loadReaderParameters: readerParameters)) {
+				foreach (var asm in ResolvedAssemblies) {
+					var path = Path.GetFullPath (Path.GetDirectoryName (asm.ItemSpec));
+					if (!resolver.SearchDirectories.Contains (path)) {
+						resolver.SearchDirectories.Add (path);
+					}
+				}
+
+				var assemblyDefinition = resolver.GetAssembly (Path.GetFullPath (foundAssembly.ItemSpec));
+				TypeDefinition handlerType = null;
+				foreach (var model in assemblyDefinition.Modules) {
+					handlerType = assemblyDefinition.MainModule.GetType (type);
+					if (handlerType != null)
+						break;
+				}
+				if (handlerType == null) {
+					Log.LogCodedError ("XA1032", Xamarin.Android.Tasks.Properties.Resources.XA1032, type, assembly);
+					return false;
+				}
+
+				string[] valueHandlerTypes = ValidHandlerType.Split (',');
+				if (!Extends (handlerType, valueHandlerTypes [0].Trim ())) {
+					Log.LogCodedError ("XA1031", Xamarin.Android.Tasks.Properties.Resources.XA1031, type, valueHandlerTypes [0]);
+				}
+
+				return !Log.HasLoggedErrors;
+			}
+		}
+
+		static bool Extends (TypeDefinition type, string validBase) {
+			var bt = type.Resolve ();
+			while (bt != null) {
+				if (bt.FullName == validBase)
+					return true;
+				bt = bt.BaseType?.Resolve () ?? null;
+			}
+			return false;
+		}
+	}
+}
