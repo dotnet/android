317bcdab674efa314e09c01d4a13be1a931874a8
Jonathan Pobst <jonathan.pobst@microsoft.com>
2023-01-23 10:53:56 -1000

    [Mono.Android] Fix WindowManagerLayoutParams.SystemUiVisibility enumification.

---
+++ src/Mono.Android/Android.Views/WindowManagerLayoutParams.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Android.Views
+{
+	partial class WindowManagerLayoutParams
+	{
+#if NET
+		[global::System.Runtime.Versioning.ObsoletedOSPlatform ("android30.0", "These flags are deprecated. Use WindowInsetsController instead.")]
+		public SystemUiFlags SystemUiFlags {
+			get => (SystemUiFlags) SystemUiVisibility;
+			set => SystemUiVisibility = (Android.Views.StatusBarVisibility) value;
+		}
+#endif
+	}
+}
