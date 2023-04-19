0b53dcae333d93da7a0f1a293f435b5a2bf37062
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-04-19 09:37:59 -0500

    [Xamarin.Android.Build.Tasks] disable Switch.System.Reflection.ForceInterpretedInvoke
    Fixes: https://github.com/dotnet/runtime/issues/83893
    
    In .NET 8, `System.Reflection.ConstructorInfo/MethodInfo.Invoke()` will
    call `System.Reflection.Emit` when called more than once. This impacts
    startup in mobile applications, so it may not be a desired feature.
    
    Unfortunately, this appears to happen quite easily in Android apps,
    some examples:
    
    * https://gist.github.com/ivanpovazan/2563ea9d2fea320e6425cfcc58da3ee5
    * https://gist.github.com/ivanpovazan/d2546d4abad17900d4366cc29e1689b2
    
    The types of situations this happens:
    
    * You call a Java method from C# that returns a `Java.Lang.Object`
      subclass.
    
    * You override a Java method in C#, that has a `Java.Lang.Object`
      parameter.
    
    To solve this problem, we can set:
    
        <ItemGroup>
          <RuntimeHostConfigurationOption Include="Switch.System.Reflection.ForceInterpretedInvoke" Value="$(_SystemReflectionForceInterpretedInvoke)" Trim="true" />
        </ItemGroup>
    
    And we can set `$(_SystemReflectionForceInterpretedInvoke)` to test
    out the setting in various apps.
    
    I added a test to verify the "private" switch is actually set.
    
    I also updated the `.aotprofile` to verify that all
    `System.Reflection.Emit` code paths disappear from `dotnet new
    android` applications.

--- tests/Mono.Android-Tests/Runtime-Microsoft.Android.Sdk/System/AppContextTests.cs
+++ tests/Mono.Android-Tests/Runtime-Microsoft.Android.Sdk/System/AppContextTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Reflection;
 
 namespace SystemTests
 {
@@ -26,6 +27,22 @@ namespace SystemTests
 		public void GetData (string name, string expected)
 		{
 			Assert.AreEqual (expected, AppContext.GetData (name));
+		}
+
+		static readonly object [] TestPrivateSwitchesSource = new object [] {
+			new object [] {
+				/* name */     "ForceInterpretedInvoke",
+				/* expected */ true,
+			},
+		};
+
+		[Test]
+		[TestCaseSource (nameof (TestPrivateSwitchesSource))]
+		public void TestPrivateSwitches (string name, object expected)
+		{
+			var type = Type.GetType ("System.LocalAppContextSwitches, System.Private.CoreLib", throwOnError: true);
+			var property = type.GetProperty (name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+			Assert.AreEqual (expected, property.GetValue (null));
 		}
 	}
 }
