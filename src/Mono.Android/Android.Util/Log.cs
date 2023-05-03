cd8d1aa58d9a02463a262493109899d147f08396
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-05-03 12:50:18 -0500

    Add static property `Log.FormatProvider`

--- src/Mono.Android/Android.Util/Log.cs
+++ src/Mono.Android/Android.Util/Log.cs
@@ -6,9 +6,14 @@ namespace Android.Util {
 
 	public partial class Log {
 
+		/// <summary>
+		/// IFormatProvider passed to any underlying string.Format() calls. Defaults to System.Globalization.CultureInfo.CurrentCulture.
+		/// </summary>
+		public static IFormatProvider FormatProvider { get; set; } = CultureInfo.CurrentCulture;
+
 		public static int Debug (string tag, string format, params object[] args)
 		{
-			return Debug (tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return Debug (tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Debug (string tag, Java.Lang.Throwable tr, string msg)
@@ -18,12 +23,12 @@ namespace Android.Util {
 
 		public static int Debug (string tag, Java.Lang.Throwable tr, string format, params object[] args)
 		{
-			return Debug (tag, string.Format (CultureInfo.InvariantCulture, format, args), tr);
+			return Debug (tag, string.Format (FormatProvider, format, args), tr);
 		}
 
 		public static int Error (string tag, string format, params object[] args)
 		{
-			return Error (tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return Error (tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Error (string tag, Java.Lang.Throwable tr, string msg)
@@ -33,12 +38,12 @@ namespace Android.Util {
 
 		public static int Error (string tag, Java.Lang.Throwable tr, string format, params object[] args)
 		{
-			return Error (tag, string.Format (CultureInfo.InvariantCulture, format, args), tr);
+			return Error (tag, string.Format (FormatProvider, format, args), tr);
 		}
 
 		public static int Info (string tag, string format, params object[] args)
 		{
-			return Info (tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return Info (tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Info (string tag, Java.Lang.Throwable tr, string msg)
@@ -48,17 +53,17 @@ namespace Android.Util {
 
 		public static int Info (string tag, Java.Lang.Throwable tr, string format, params object[] args)
 		{
-			return Info (tag, string.Format (CultureInfo.InvariantCulture, format, args), tr);
+			return Info (tag, string.Format (FormatProvider, format, args), tr);
 		}
 
 		public static int WriteLine (LogPriority priority, string tag, string format, params object[] args)
 		{
-			return WriteLine (priority, tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return WriteLine (priority, tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Verbose (string tag, string format, params object[] args)
 		{
-			return Verbose (tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return Verbose (tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Verbose (string tag, Java.Lang.Throwable tr, string msg)
@@ -68,12 +73,12 @@ namespace Android.Util {
 
 		public static int Verbose (string tag, Java.Lang.Throwable tr, string format, params object[] args)
 		{
-			return Verbose (tag, string.Format (CultureInfo.InvariantCulture, format, args), tr);
+			return Verbose (tag, string.Format (FormatProvider, format, args), tr);
 		}
 
 		public static int Warn (string tag, string format, params object[] args)
 		{
-			return Warn (tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return Warn (tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Warn (string tag, Java.Lang.Throwable tr, string msg)
@@ -83,12 +88,12 @@ namespace Android.Util {
 
 		public static int Warn (string tag, Java.Lang.Throwable tr, string format, params object[] args)
 		{
-			return Warn (tag, string.Format (CultureInfo.InvariantCulture, format, args), tr);
+			return Warn (tag, string.Format (FormatProvider, format, args), tr);
 		}
 
 		public static int Wtf (string tag, string format, params object[] args)
 		{
-			return Wtf (tag, string.Format (CultureInfo.InvariantCulture, format, args));
+			return Wtf (tag, string.Format (FormatProvider, format, args));
 		}
 
 		public static int Wtf (string tag, Java.Lang.Throwable tr, string msg)
@@ -98,7 +103,7 @@ namespace Android.Util {
 
 		public static int Wtf (string tag, Java.Lang.Throwable tr, string format, params object[] args)
 		{
-			return Wtf (tag, string.Format (CultureInfo.InvariantCulture, format, args), tr);
+			return Wtf (tag, string.Format (FormatProvider, format, args), tr);
 		}
 	}
 }
