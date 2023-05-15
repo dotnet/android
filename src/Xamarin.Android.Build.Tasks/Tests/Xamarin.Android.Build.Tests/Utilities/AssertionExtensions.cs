dotnet/android

commit c537dd28c30f482f365ef756214be35aa1553da2

[Xamarin.Android.Build.Tasks] Use ProGuard config files from .aar files. (#5310)
Fixes: https://github.com/xamarin/xamarin-android/issues/3752

`.aar` [can contain `proguard.txt` files][0]

> **Library dependencies**
> *Location*: AAR libraries: `<library-dir>/proguard.txt`
> *Description*: If an AAR library is published with its own ProGuard
> rules file, and you include that AAR as a compile-time dependency,
> R8 automatically applies its rules when compiling your project.

Update the `@(ProguardConfiguration)` item group to include
`proguard.cfg` and `proguard.txt` files in the project directory, and
for Library project builds add the contents of all
`@(ProguardConfiguration)` files into a `proguard.txt` entry within
the generated `.aar` file (fcd7cf8f).

Update the `_ResolveLibraryProjectImports` target and the
`<ResolveLibraryProjectImports/>` MSBuild task to look for
`proguard.txt` files contained within `.aar` packages, and update the
`<ReadLibraryProjectImportsCache/>` MSBuild task so that
`proguard.txt` files within `.aar` packages are added to the
`@(ProguardConfiguration)` item group of App builds, so that they are
appropriately passed to the `_CompileToDalvik` target and used when
converting Java bytecode into Dalvik bytecode.

TODO?

The Android documentation mentions that `.jar` files can also contain
ProGuard files:

> *Location*: JAR libraries: `<library-dir>/META-INF/proguard/`

We do not currently use ProGuard files contained within `.jar` files.

[0]: https://developer.android.com/studio/build/shrink-code

src/Xamarin.Android.Build.Tasks/Tasks/ResolveLibraryProjectImports.cs
@@ -59,6 +59,9 @@ namespace Xamarin.Android.Tasks
 		[Output]
 		public ITaskItem [] ResolvedResourceDirectoryStamps { get; set; }
 
+		[Output]
+		public ITaskItem [] ProguardConfigFiles { get; set; }
+
 		internal const string OriginalFile = "OriginalFile";
 		internal const string AndroidSkipResourceProcessing = "AndroidSkipResourceProcessing";
 
@@ -83,10 +86,11 @@ namespace Xamarin.Android.Tasks
 			var resolvedResourceDirectories   = new List<ITaskItem> ();
 			var resolvedAssetDirectories      = new List<ITaskItem> ();
 			var resolvedEnvironmentFiles      = new List<ITaskItem> ();
+			var proguardConfigFiles           = new List<ITaskItem> ();
 
 			assemblyMap.Load (AssemblyIdentityMapFile);
 			try {
-				Extract (jars, resolvedResourceDirectories, resolvedAssetDirectories, resolvedEnvironmentFiles);
+				Extract (jars, resolvedResourceDirectories, resolvedAssetDirectories, resolvedEnvironmentFiles, proguardConfigFiles);
 			} catch (ZipIOException ex) {
 				Log.LogCodedError ("XA1004", ex.Message);
 				Log.LogDebugMessage (ex.ToString ());
@@ -96,6 +100,7 @@ namespace Xamarin.Android.Tasks
 			ResolvedResourceDirectories = resolvedResourceDirectories.ToArray ();
 			ResolvedAssetDirectories    = resolvedAssetDirectories.ToArray ();
 			ResolvedEnvironmentFiles    = resolvedEnvironmentFiles.ToArray ();
+			ProguardConfigFiles         = proguardConfigFiles.ToArray ();
 
 			ResolvedResourceDirectoryStamps = ResolvedResourceDirectories
 				.Select (s => new TaskItem (Path.GetFullPath (Path.Combine (s.ItemSpec, "../..")) + ".stamp"))
@@ -116,14 +121,15 @@ namespace Xamarin.Android.Tasks
 						new XElement ("Jars",
 							Jars.Select(e => new XElement ("Jar", e))),
 						new XElement ("ResolvedResourceDirectories",
-							ResolvedResourceDirectories.ToXElements ("ResolvedResourceDirectory", knownMetadata)
-							),
+							ResolvedResourceDirectories.ToXElements ("ResolvedResourceDirectory", knownMetadata)),
 						new XElement ("ResolvedAssetDirectories",
 							ResolvedAssetDirectories.ToXElements ("ResolvedAssetDirectory", knownMetadata)),
 						new XElement ("ResolvedEnvironmentFiles",
 							ResolvedEnvironmentFiles.ToXElements ("ResolvedEnvironmentFile", knownMetadata)),
 						new XElement ("ResolvedResourceDirectoryStamps",
-							ResolvedResourceDirectoryStamps.ToXElements ("ResolvedResourceDirectoryStamp", knownMetadata))
+							ResolvedResourceDirectoryStamps.ToXElements ("ResolvedResourceDirectoryStamp", knownMetadata)),
+						new XElement ("ProguardConfigFiles",
+							ProguardConfigFiles.ToXElements ("ProguardConfigFile", knownMetadata))
 					));
 				document.SaveIfChanged (CacheFile);
 			}
@@ -135,6 +141,7 @@ namespace Xamarin.Android.Tasks
 			Log.LogDebugTaskItems ("  ResolvedAssetDirectories: ", ResolvedAssetDirectories);
 			Log.LogDebugTaskItems ("  ResolvedEnvironmentFiles: ", ResolvedEnvironmentFiles);
 			Log.LogDebugTaskItems ("  ResolvedResourceDirectoryStamps: ", ResolvedResourceDirectoryStamps);
+			Log.LogDebugTaskItems ("  ProguardConfigFiles:", ProguardConfigFiles);
 
 			return !Log.HasLoggedErrors;
 		}
@@ -145,7 +152,8 @@ namespace Xamarin.Android.Tasks
 				IDictionary<string, ITaskItem> jars,
 				ICollection<ITaskItem> resolvedResourceDirectories,
 				ICollection<ITaskItem> resolvedAssetDirectories,
-				ICollection<ITaskItem> resolvedEnvironments)
+				ICollection<ITaskItem> resolvedEnvironments,
+				ICollection<ITaskItem> proguardConfigFiles)
 		{
 			// lets "upgrade" the old directory.
 			string oldPath = Path.GetFullPath (Path.Combine (OutputImportDirectory, "..", "__library_projects__"));
@@ -345,6 +353,7 @@ namespace Xamarin.Android.Tasks
 				string resDir = Path.Combine (importsDir, "res");
 				string resDirArchive = Path.Combine (resDir, "..", "res.zip");
 				string assetsDir = Path.Combine (importsDir, "assets");
+				string proguardFile = Path.Combine (importsDir, "proguard.txt");
 
 				bool updated = false;
 				string aarHash = Files.HashFile (aarFile.ItemSpec);
@@ -431,6 +440,11 @@ namespace Xamarin.Android.Tasks
 					resolvedAssetDirectories.Add (new TaskItem (Path.GetFullPath (assetsDir), new Dictionary<string, string> {
 						{ OriginalFile, aarFullPath },
 					}));
+				if (File.Exists (proguardFile)) {
+					proguardConfigFiles.Add (new TaskItem (Path.GetFullPath (proguardFile), new Dictionary<string, string> {
+						{ OriginalFile, aarFullPath },
+					}));
+				}
 			}
 		}
 

src/Xamarin.Android.Build.Tasks/Xamarin.Android.EmbeddedResource.targets
@@ -55,6 +55,7 @@ This file is used by all project types, including binding projects.
       <Output TaskParameter="ResolvedAssetDirectories" ItemName="LibraryAssetDirectories" />
       <Output TaskParameter="ResolvedEnvironmentFiles" ItemName="LibraryEnvironments" />
       <Output TaskParameter="ResolvedResourceDirectoryStamps" ItemName="_LibraryResourceDirectoryStamps" />
+      <Output TaskParameter="ProguardConfigFiles" ItemName="ProguardConfiguration" />
     </ReadLibraryProjectImportsCache>
     <ItemGroup>
       <FileWrites Include="@(ResolvedResourceDirectories->'%(ResourceDirectoryArchive)')"

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/XASdkTests.cs
@@ -62,6 +62,9 @@ namespace Xamarin.Android.Build.Tests
 					new AndroidItem.AndroidResource (() => "Resources\\drawable\\IMALLCAPS.png") {
 						BinaryContent = () => XamarinAndroidApplicationProject.icon_binary_mdpi,
 					},
+					new AndroidItem.ProguardConfiguration ("proguard.txt") {
+						TextContent = () => @"-ignorewarnings",
+					},
 				}
 			};
 			libC.OtherBuildItems.Add (new AndroidItem.AndroidAsset ("Assets\\bar\\bar.txt") {
@@ -72,6 +75,13 @@ namespace Xamarin.Android.Build.Tests
 				libC.Sources.Remove (activity);
 			var libCBuilder = CreateDotNetBuilder (libC, Path.Combine (path, libC.ProjectName));
 			Assert.IsTrue (libCBuilder.Build (), $"{libC.ProjectName} should succeed");
+
+			var aarPath = Path.Combine (FullProjectDirectory, libC.OutputPath, $"{libC.ProjectName}.aar");
+			FileAssert.Exists (aarPath);
+			using (var aar = ZipHelper.OpenZip (aarPath)) {
+				aar.AssertContainsEntry (aarPath, "assets/bar/bar.txt");
+				aar.AssertContainsEntry (aarPath, "proguard.txt");
+			}
 
 			var libB = new XASdkProject (outputType: "Library") {
 				ProjectName = "LibraryB",
@@ -111,6 +121,9 @@ namespace Xamarin.Android.Build.Tests
 						Encoding = Encoding.ASCII,
 						TextContent = () => ResourceData.JavaSourceTestExtension,
 					},
+					new AndroidItem.ProguardConfiguration ("proguard.txt") {
+						TextContent = () => @"-ignorewarnings",
+					},
 				}
 			};
 			libB.OtherBuildItems.Add (new AndroidItem.AndroidEnvironment ("env.txt") {
@@ -140,7 +153,7 @@ namespace Xamarin.Android.Build.Tests
 
 			// Check .aar file for class library
 			var libBOutputPath = Path.Combine (FullProjectDirectory, libB.OutputPath);
-			var aarPath = Path.Combine (libBOutputPath, $"{libB.ProjectName}.aar");
+			aarPath = Path.Combine (libBOutputPath, $"{libB.ProjectName}.aar");
 			FileAssert.Exists (aarPath);
 			FileAssert.Exists (Path.Combine (libBOutputPath, "bar.aar"));
 			using (var aar = ZipHelper.OpenZip (aarPath)) {
@@ -196,6 +209,8 @@ namespace Xamarin.Android.Build.Tests
 			var intermediate = Path.Combine (FullProjectDirectory, appA.IntermediateOutputPath);
 			var dexFile = Path.Combine (intermediate, "android", "bin", "classes.dex");
 			FileAssert.Exists (dexFile);
+			var proguardFiles = Directory.GetFiles (Path.Combine (intermediate, "lp"), "proguard.txt", SearchOption.AllDirectories);
+			Assert.AreEqual (2, proguardFiles.Length, "There should be only two proguard.txt files.");
 			string className = "Lcom/xamarin/android/test/msbuildtest/JavaSourceJarTest;";
 			Assert.IsTrue (DexUtils.ContainsClass (className, dexFile, AndroidSdkPath), $"`{dexFile}` should include `{className}`!");
 			className = "Lcom/xamarin/android/test/msbuildtest/JavaSourceTestExtension;";

src/Xamarin.Android.Build.Tasks/Microsoft.Android.Sdk/Sdk/AutoImport.props
@@ -45,6 +45,8 @@ https://github.com/dotnet/designs/blob/4703666296f5e59964961464c25807c727282cae/
     <AndroidNativeLibrary Include="**\*.so"  Exclude="$(DefaultItemExcludes);$(DefaultExcludesInProjectFolder)" />
     <JavaSourceJar        Include="**\*-source.jar;**\*-sources.jar;**\*-src.jar"   Exclude="$(DefaultItemExcludes);$(DefaultExcludesInProjectFolder)" />
     <AndroidJavaSource    Include="**\*.java" Exclude="$(DefaultItemExcludes);$(DefaultExcludesInProjectFolder)" />
+    <ProguardConfiguration Include="**\proguard.cfg" Exclude="$(DefaultItemExcludes);$(DefaultExcludesInProjectFolder)" />
+    <ProguardConfiguration Include="**\proguard.txt" Exclude="$(DefaultItemExcludes);$(DefaultExcludesInProjectFolder)" />
   </ItemGroup>
 
 </Project>

src/Xamarin.Android.Build.Tasks/Microsoft.Android.Sdk/targets/Microsoft.Android.Sdk.AndroidLibraries.targets
@@ -49,6 +49,7 @@ projects.
       <_CreateAarInputs Include="@(AndroidJavaLibrary)" />
       <_CreateAarInputs Include="@(EmbeddedJar)" />
       <_CreateAarInputs Include="@(EmbeddedNativeLibrary)" />
+      <_CreateAarInputs Include="@(ProguardConfiguration)" />
     </ItemGroup>
     <Hash
         ItemsToHash="@(_CreateAarInputs)"
@@ -80,6 +81,7 @@ projects.
         AndroidEnvironment="@(AndroidEnvironment)"
         JarFiles="@(AndroidJavaLibrary);@(EmbeddedJar)"
         NativeLibraries="@(EmbeddedNativeLibrary)"
+        ProguardConfigurationFiles="@(ProguardConfiguration)"
         OutputFile="$(_AarOutputPath)"
     />
     <ItemGroup>

src/Xamarin.Android.Build.Tasks/Tasks/CreateAar.cs
@@ -23,6 +23,8 @@ namespace Xamarin.Android.Tasks
 
 		public ITaskItem [] NativeLibraries { get; set; }
 
+		public ITaskItem [] ProguardConfigurationFiles { get; set; }
+
 		[Required]
 		public string AssetDirectory { get; set; }
 
@@ -102,6 +104,13 @@ namespace Xamarin.Android.Tasks
 						aar.AddStream (File.OpenRead (lib.ItemSpec), archivePath);
 						existingEntries.Remove (archivePath);
 					}
+				}
+				if (ProguardConfigurationFiles != null) {
+					var sb = new StringBuilder ();
+					foreach (var file in ProguardConfigurationFiles) {
+						sb.AppendLine (File.ReadAllText (file.ItemSpec));
+					}
+					aar.AddEntry ("proguard.txt", sb.ToString (), Files.UTF8withoutBOM);
 				}
 				foreach (var entry in existingEntries) {
 					Log.LogDebugMessage ($"Removing {entry} as it is not longer required.");

src/Xamarin.Android.Build.Tasks/Tasks/ReadLibraryProjectImportsCache.cs
@@ -1,21 +1,21 @@
-// 
+//
 // ReadLibraryProjectImportsCache.cs
-//  
+//
 // Author:
 //       Dean Ellis <dean.ellis@xamarin.com>
-// 
+//
 // Copyright (c) 2015 Xamarin Inc.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,7 +38,7 @@ namespace Xamarin.Android.Tasks
 		public override string TaskPrefix => "RLC";
 
 		[Required]
-		public string CacheFile { get; set;} 
+		public string CacheFile { get; set;}
 
 		[Output]
 		public ITaskItem [] Jars { get; set; }
@@ -55,6 +55,9 @@ namespace Xamarin.Android.Tasks
 		[Output]
 		public ITaskItem [] ResolvedResourceDirectoryStamps { get; set; }
 
+		[Output]
+		public ITaskItem [] ProguardConfigFiles { get; set; }
+
 		public override bool RunTask ()
 		{
 			Log.LogDebugMessage ("Task ReadLibraryProjectImportsCache");
@@ -70,12 +73,14 @@ namespace Xamarin.Android.Tasks
 			ResolvedEnvironmentFiles = doc.GetPathsAsTaskItems ("ResolvedEnvironmentFiles", "ResolvedEnvironmentFile");
 			ResolvedResourceDirectoryStamps = doc.GetPathsAsTaskItems ("ResolvedResourceDirectoryStamps"
 				, "ResolvedResourceDirectoryStamp");
-			
+			ProguardConfigFiles = doc.GetPathsAsTaskItems ("ProguardConfigFiles", "ProguardConfigFile");
+
 			Log.LogDebugTaskItems ("  Jars: ", Jars);
 			Log.LogDebugTaskItems ("  ResolvedAssetDirectories: ", ResolvedAssetDirectories);
 			Log.LogDebugTaskItems ("  ResolvedResourceDirectories: ", ResolvedResourceDirectories);
 			Log.LogDebugTaskItems ("  ResolvedEnvironmentFiles: ", ResolvedEnvironmentFiles);
 			Log.LogDebugTaskItems ("  ResolvedResourceDirectoryStamps: ", ResolvedResourceDirectoryStamps);
+			Log.LogDebugTaskItems ("  ProguardConfigFiles: ", ProguardConfigFiles);
 
 			return !Log.HasLoggedErrors;
 		}

commit d50b519f564b0070a24f9ced3429f8aaf19acf64

[Xamarin.Android.Build.Tasks] skip `proguard.txt` files in !app (#8025)
Context: c537dd28c30f482f365ef756214be35aa1553da2
Context: https://github.com/xamarin/AndroidX/pull/729
Context: https://github.com/xamarin/GooglePlayServicesComponents/pull/774

A customer reported a build error such as:

	C:\Program Files\dotnet\packs\Microsoft.Android.Sdk.Windows\33.0.46\targets\Microsoft.Android.Sdk.AndroidLibraries.targets(77,5):
	error XACAAR7014: System.OutOfMemoryException: Exception of type 'System.OutOfMemoryException' was thrown.
	   at System.Text.StringBuilder.ToString()
	   at Xamarin.Android.Tasks.CreateAar.RunTask()
	   at Microsoft.Android.Build.Tasks.AndroidTask.Execute()

Somewhere within [`<CreateAar/>`][0]:

	if (ProguardConfigurationFiles != null) {
	    var sb = new StringBuilder ();
	    foreach (var file in ProguardConfigurationFiles) {
	        sb.AppendLine (File.ReadAllText (file.ItemSpec));
	    }
	    aar.AddEntry ("proguard.txt", sb.ToString (), Files.UTF8withoutBOM);
	}

The problem appears to be triggered by:

 1. Library project `Lib.csproj` which bind `.aar` files include the
    `proguard.txt` from the `.aar` into the output `Lib.aar`, as
    `Lib.aar!proguard.txt`.

 2. Library project `References.csproj` which has a `@(Reference)` to
    `Lib.csproj` will include the contents of `Lib.aar!proguard.txt`
    into `References.aar!proguard.txt`.  If `References.csproj` has a
    `@(PackageReference)` which has `AnotherLib.aar`, then
    `AnotherLib.aar!proguard.txt` will *also* be copied into
    `References.aar!proguard.txt`.
    (Repeat for all `@(Reference)` and `@(projectreference)`.)
    (Note combinatorial explosion in `proguard.txt` size…)

 3. App project `App.csproj` references (2), merging all the contents
    of all the `proguard.txt` files from all the `.aar` files into a
    single unified `proguard.txt` file.

If the project has lots of library projects or package references,
step (3) could OOM.  "Lots" of references, such as:

	ProguardConfigurationFiles=
	    ~\.nuget\packages\xamarin.androidx.versionedparcelable\1.1.1.15\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.lifecycle.runtime\2.5.1.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.core\1.9.0.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.savedstate\1.2.0.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.recyclerview\1.2.1.7\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.lifecycle.viewmodel\2.5.1.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.lifecycle.viewmodelsavedstate\2.5.1.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.fragment\1.5.3.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.vectordrawable.animated\1.1.0.14\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.transition\1.4.1.8\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.startup.startupruntime\1.1.1.2\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.lifecycle.process\2.5.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.coordinatorlayout\1.2.0.3\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.appcompat\1.5.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.google.android.material\1.7.0\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.window\1.0.0.10\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.navigation.common\2.5.2.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.navigation.ui\2.5.2.1\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ~\.nuget\packages\xamarin.androidx.media\1.6.0.2\buildTransitive\net6.0-android31.0\..\..\proguard\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\160\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\155\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\161\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\162\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\156\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\157\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\163\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\164\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\165\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\166\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\167\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\170\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\168\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\169\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\171\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\172\jl\proguard.txt
	    ..\path\to\projectreference\obj\Debug\lp\173\jl\proguard.txt
	    …

I believe this problem was partially introduced in c537dd28, but we

src/Xamarin.Android.Build.Tasks/Tasks/ResolveLibraryProjectImports.cs
@@ -44,6 +44,8 @@ namespace Xamarin.Android.Tasks
 		[Required]
 		public bool DesignTimeBuild { get; set; }
 
+		public bool AndroidApplication { get; set; }
+
 		[Output]
 		public ITaskItem [] Jars { get; set; }
 
@@ -442,7 +444,7 @@ namespace Xamarin.Android.Tasks
 					resolvedAssetDirectories.Add (new TaskItem (Path.GetFullPath (assetsDir), new Dictionary<string, string> {
 						{ OriginalFile, aarFullPath },
 					}));
-				if (File.Exists (proguardFile)) {
+				if (AndroidApplication && File.Exists (proguardFile)) {
 					proguardConfigFiles.Add (new TaskItem (Path.GetFullPath (proguardFile), new Dictionary<string, string> {
 						{ OriginalFile, aarFullPath },
 					}));

src/Xamarin.Android.Build.Tasks/Xamarin.Android.EmbeddedResource.targets
@@ -38,6 +38,7 @@ This file is used by all project types, including binding projects.
       Outputs="$(_AndroidStampDirectory)_ResolveLibraryProjectImports.stamp">
     <ResolveLibraryProjectImports
         ContinueOnError="$(DesignTimeBuild)"
+        AndroidApplication="$(AndroidApplication)"
         CacheFile="$(_AndroidLibraryProjectImportsCache)"
         DesignTimeBuild="$(DesignTimeBuild)"
         Assemblies="@(_MonoAndroidReferencePath);@(_MonoAndroidReferenceDependencyPaths)"

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Utilities/AssertionExtensions.cs
@@ -55,6 +55,19 @@ namespace Xamarin.Android.Build.Tests
 		}
 
 		[DebuggerHidden]
+		public static void AssertEntryEquals (this ZipArchive zip, string zipPath, string archivePath, string expected)
+		{
+			zip.AssertContainsEntry (zipPath, archivePath);
+
+			var entry = zip.ReadEntry (archivePath);
+			using var stream = new MemoryStream ();
+			entry.Extract (stream);
+			stream.Position = 0;
+			using var reader = new StreamReader (stream);
+			Assert.AreEqual (expected, reader.ReadToEnd ().Trim ());
+		}
+
+		[DebuggerHidden]
 		public static void AssertContainsEntry (this ZipArchive zip, string zipPath, string archivePath)
 		{
 			Assert.IsTrue (zip.ContainsEntry (archivePath), $"{zipPath} should contain {archivePath}");

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/XASdkTests.cs
@@ -79,7 +79,7 @@ namespace Xamarin.Android.Build.Tests
 						BinaryContent = () => XamarinAndroidApplicationProject.icon_binary_mdpi,
 					},
 					new AndroidItem.ProguardConfiguration ("proguard.txt") {
-						TextContent = () => @"-ignorewarnings",
+						TextContent = () => "# LibraryC",
 					},
 				}
 			};
@@ -97,7 +97,7 @@ namespace Xamarin.Android.Build.Tests
 			FileAssert.Exists (aarPath);
 			using (var aar = ZipHelper.OpenZip (aarPath)) {
 				aar.AssertContainsEntry (aarPath, "assets/bar/bar.txt");
-				aar.AssertContainsEntry (aarPath, "proguard.txt");
+				aar.AssertEntryEquals (aarPath, "proguard.txt", "# LibraryC");
 			}
 
 			var libB = new XASdkProject (outputType: "Library") {
@@ -139,7 +139,7 @@ namespace Xamarin.Android.Build.Tests
 						TextContent = () => ResourceData.JavaSourceTestExtension,
 					},
 					new AndroidItem.ProguardConfiguration ("proguard.txt") {
-						TextContent = () => @"-ignorewarnings",
+						TextContent = () => "# LibraryB",
 					},
 				}
 			};
@@ -185,6 +185,8 @@ namespace Xamarin.Android.Build.Tests
 				aar.AssertContainsEntry (aarPath, $"libs/{projectJarHash}.jar");
 				aar.AssertContainsEntry (aarPath, "jni/arm64-v8a/libfoo.so");
 				aar.AssertContainsEntry (aarPath, "jni/x86/libfoo.so");
+				// proguard.txt from Library C should not flow to Library B and "double"
+				aar.AssertEntryEquals (aarPath, "proguard.txt", "# LibraryB");
 			}
 
 			// Check EmbeddedResource files do not exist