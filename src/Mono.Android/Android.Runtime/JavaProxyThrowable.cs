ee565e4d443efe1f35306191b6412ecace13cb7e
Marek Habersack <grendel@twistedcode.net>
2023-07-12 12:07:52 +0200

    Improve JavaProxyThrowable by translating managed stack trace
    Fixes: https://github.com/xamarin/xamarin-android/issues/1198
    
    Given an instance of `System.Exception`, we can use the
    `System.Diagnostics.StackTrace` class to iterate over the list of
    managed stack frames, translating each of them to equivalent
    `Java.Lang.StackTraceElement` and afterwards passing the entire
    collection to `Java.Lang.Throwable.SetStackTrace`.

--- src/Mono.Android/Android.Runtime/JavaProxyThrowable.cs
+++ src/Mono.Android/Android.Runtime/JavaProxyThrowable.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Diagnostics;
+using System.Reflection;
+
+using StackTraceElement = Java.Lang.StackTraceElement;
 
 namespace Android.Runtime {
 
@@ -6,18 +10,55 @@ namespace Android.Runtime {
 
 		public  readonly Exception InnerException;
 
-		public JavaProxyThrowable (Exception innerException)
-			: base (GetDetailMessage (innerException))
+		protected JavaProxyThrowable (string message, Exception innerException)
+			: base (message)
 		{
 			InnerException  = innerException;
 		}
 
-		static string GetDetailMessage (Exception innerException)
+		public static JavaProxyThrowable Create (Exception? innerException)
 		{
-			if (innerException == null)
-				throw new ArgumentNullException ("innerException");
+			if (innerException == null) {
+				throw new ArgumentNullException (nameof (innerException));
+			}
 
-			return innerException.ToString ();
+			var proxy = new JavaProxyThrowable (innerException.Message, innerException);
+
+			try {
+				proxy.TranslateStackTrace ();
+			} catch {
+				// We shouldn't throw here, just try to do the best we can do
+				proxy = new JavaProxyThrowable (innerException.ToString (), innerException);
+			}
+
+			return proxy;
+		}
+
+		void TranslateStackTrace ()
+		{
+			var trace = new StackTrace (InnerException, fNeedFileInfo: true);
+			if (trace.FrameCount <= 0) {
+				return;
+			}
+
+			StackFrame[] frames = trace.GetFrames ();
+			StackTraceElement[] elements = new StackTraceElement[frames.Length];
+
+			for (int i = 0; i < frames.Length; i++) {
+				StackFrame managedFrame = frames[i];
+				MethodBase? managedMethod = managedFrame.GetMethod ();
+
+				var throwableFrame = new StackTraceElement (
+					declaringClass: managedMethod?.DeclaringType?.FullName,
+					methodName: managedMethod?.Name,
+					fileName: managedFrame?.GetFileName (),
+					lineNumber: managedFrame?.GetFileLineNumber () ?? 0
+				);
+
+				elements[i] = throwableFrame;
+			}
+
+			SetStackTrace (elements);
 		}
 	}
 }
