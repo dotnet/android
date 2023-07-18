dotnet/android

commit 26ffd5d763b36dde528bc324967f4925e9101733

[Xamarin.Android.Build.Tasks] fix `.aar` files flowing from project references (#8193)
Fixes: https://github.com/xamarin/xamarin-android/issues/8190

In a customer sample, they have an `.aar` file they need the Java code
from, but do not want a C# binding for it:

    <AndroidLibrary Update="FooNonBinding-release.aar" Bind="false" />

`Bind="false"` looks to have the side effect where:

1. It does not get copied to the output directory.

2. The Java types don't make it to the final app.

3. Crash at runtime:

    java.lang.ClassNotFoundException: Didn't find class "com.example.foononbinding.FooSample" on path

A workaround is to add a line such as:

    <None Include="FooNonBinding-release.aar" CopyToOutputDirectory="PreserveNewest" />

I could reproduce this issue by updating our existing
`DotNetBuildLibrary` test. I could assert the file exists in the output
directory, as well as actually using `dexdump` to verify Java classes
make it to the app. They did not!

The solution here being that we are missing a line such as:

    <!-- .aar files should be copied to $(OutputPath) in .NET 6-->
    ++<None Include="@(AndroidAarLibrary)" TfmSpecificPackageFile="%(AndroidAarLibrary.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
    <None Include="@(LibraryProjectZip)" TfmSpecificPackageFile="%(LibraryProjectZip.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />

Now the `DotNetBuildLibrary` test passes.

src/Xamarin.Android.Build.Tasks/MSBuild/Xamarin/Android/Xamarin.Android.AvailableItems.targets
@@ -103,6 +103,7 @@ This item group populates the Build Action drop-down in IDEs.
       <AndroidJavaLibrary Include="@(AndroidLibrary)" Condition=" '%(AndroidLibrary.Extension)' == '.jar' and '%(AndroidLibrary.Bind)' != 'true' " />
       <EmbeddedJar        Include="@(AndroidLibrary)" Condition=" '%(AndroidLibrary.Extension)' == '.jar' and '%(AndroidLibrary.Bind)' == 'true' " />
       <!-- .aar files should be copied to $(OutputPath) in .NET 6-->
+      <None Include="@(AndroidAarLibrary)" TfmSpecificPackageFile="%(AndroidAarLibrary.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
       <None Include="@(LibraryProjectZip)" TfmSpecificPackageFile="%(LibraryProjectZip.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
     </ItemGroup>
     <!-- Legacy binding projects -->

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/XASdkTests.cs
@@ -132,6 +132,10 @@ namespace Xamarin.Android.Build.Tests
 					new AndroidItem.AndroidLibrary ("sub\\directory\\bar.aar") {
 						WebContent = "https://repo1.maven.org/maven2/com/balysv/material-menu/1.1.0/material-menu-1.1.0.aar",
 					},
+					new AndroidItem.AndroidLibrary ("sub\\directory\\baz.aar") {
+						WebContent = "https://repo1.maven.org/maven2/com/soundcloud/android/android-crop/1.0.1/android-crop-1.0.1.aar",
+						MetadataValues = "Bind=false",
+					},
 					new AndroidItem.AndroidJavaSource ("JavaSourceTestExtension.java") {
 						Encoding = Encoding.ASCII,
 						TextContent = () => ResourceData.JavaSourceTestExtension,
@@ -149,6 +153,10 @@ namespace Xamarin.Android.Build.Tests
 			});
 			libB.OtherBuildItems.Add (new AndroidItem.AndroidLibrary ("sub\\directory\\arm64-v8a\\libfoo.so") {
 				BinaryContent = () => Array.Empty<byte> (),
+			});
+			libB.OtherBuildItems.Add (new AndroidItem.AndroidLibrary (default (Func<string>)) {
+				Update = () => "sub\\directory\\baz.aar",
+				MetadataValues = "Bind=false",
 			});
 			libB.OtherBuildItems.Add (new AndroidItem.AndroidNativeLibrary (default (Func<string>)) {
 				Update = () => "libfoo.so",
@@ -172,6 +180,7 @@ namespace Xamarin.Android.Build.Tests
 			aarPath = Path.Combine (libBOutputPath, $"{libB.ProjectName}.aar");
 			FileAssert.Exists (aarPath);
 			FileAssert.Exists (Path.Combine (libBOutputPath, "bar.aar"));
+			FileAssert.Exists (Path.Combine (libBOutputPath, "baz.aar"));
 			using (var aar = ZipHelper.OpenZip (aarPath)) {
 				aar.AssertContainsEntry (aarPath, "assets/foo/foo.txt");
 				aar.AssertContainsEntry (aarPath, "res/layout/mylayout.xml");
@@ -234,6 +243,10 @@ namespace Xamarin.Android.Build.Tests
 			string className = "Lcom/xamarin/android/test/msbuildtest/JavaSourceJarTest;";
 			Assert.IsTrue (DexUtils.ContainsClass (className, dexFile, AndroidSdkPath), $"`{dexFile}` should include `{className}`!");
 			className = "Lcom/xamarin/android/test/msbuildtest/JavaSourceTestExtension;";
+			Assert.IsTrue (DexUtils.ContainsClass (className, dexFile, AndroidSdkPath), $"`{dexFile}` should include `{className}`!");
+			className = "Lcom/balysv/material/drawable/menu/MaterialMenu;"; // from material-menu-1.1.0.aar
+			Assert.IsTrue (DexUtils.ContainsClass (className, dexFile, AndroidSdkPath), $"`{dexFile}` should include `{className}`!");
+			className = "Lcom/soundcloud/android/crop/Crop;"; // from android-crop-1.0.1.aar
 			Assert.IsTrue (DexUtils.ContainsClass (className, dexFile, AndroidSdkPath), $"`{dexFile}` should include `{className}`!");
 
 			// Check environment variable

commit 359f66d8e24e010b14102a283cdd6ebee05a7280

[Xamarin.Android.Build.Tasks] fix duplicate `.aar` files
Context: https://github.com/dotnet/maui/pull/16024#issuecomment-1640461682

.NET MAUI's build currently fails with:

    Xamarin.Android.Aapt2.targets(123,3): error APT2144: invalid file path 'D:\a\_work\1\s\src\Core\src\obj\Debug\net8.0-android\lp\129.stamp'.

What is very wrong about this, it is trying to `aapt2 compile` a
`*.stamp` file:

    Executing compile -o /Users/builder/azdo/_work/1/s/src/Core/src/obj/Release/net8.0-android/lp/87/jl/res/../flat/ /Users/builder/azdo/_work/1/s/src/Core/src/obj/Release/net8.0-android/lp/87.stamp

Normally this runs against `*.flat` or `*.flata` files.

This problem was introduced in 26ffd5d7:

1. Library A uses an AndroidX package, the AndroidX `.aar` file is added
   to `@(AndroidAarLibrary)`. The NuGet package does this in a
   `.targets` file.

2. With the change in 26ffd5d7, the `.aar` is copied to Library A's
   build output.

3. Library B uses the same AndroidX package and references Library A.

4. Library B now has duplicate `.aar` files & has the weird build error!

I could reproduce the issue in a test.

There *may* be a second bug here, but we should update our logic to be:

    <!-- .aar files should be copied to $(OutputPath) in .NET 6-->
    <None Include="@(AndroidLibrary)" Condition=" '%(AndroidLibrary.Extension)' == '.aar' " ... />
    <!-- @(LibraryProjectZip) items that are not in @(AndroidLibrary) -->
    <None Include="@(LibraryProjectZip)" Exclude="@(AndroidLibrary)" ... />

So we now only copy:

* The new `@(AndroidLibrary)` item group with an `.aar` extension. *Not*
  `@(AndroidAarLibrary)`.

* Any `@(LibraryProjectZip)` that are *not* in `@(AndroidLibrary)`. This
  supports the classic item group name, keeping our behavior before.

Now the new test and the test updated in 26ffd5d7 both pass.

src/Xamarin.Android.Build.Tasks/MSBuild/Xamarin/Android/Xamarin.Android.AvailableItems.targets
@@ -103,8 +103,9 @@ This item group populates the Build Action drop-down in IDEs.
       <AndroidJavaLibrary Include="@(AndroidLibrary)" Condition=" '%(AndroidLibrary.Extension)' == '.jar' and '%(AndroidLibrary.Bind)' != 'true' " />
       <EmbeddedJar        Include="@(AndroidLibrary)" Condition=" '%(AndroidLibrary.Extension)' == '.jar' and '%(AndroidLibrary.Bind)' == 'true' " />
       <!-- .aar files should be copied to $(OutputPath) in .NET 6-->
-      <None Include="@(AndroidAarLibrary)" TfmSpecificPackageFile="%(AndroidAarLibrary.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
-      <None Include="@(LibraryProjectZip)" TfmSpecificPackageFile="%(LibraryProjectZip.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
+      <None Include="@(AndroidLibrary)" Condition=" '%(AndroidLibrary.Extension)' == '.aar' " TfmSpecificPackageFile="%(AndroidLibrary.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
+      <!-- @(LibraryProjectZip) items that are not in @(AndroidLibrary) -->
+      <None Include="@(LibraryProjectZip)" Exclude="@(AndroidLibrary)" TfmSpecificPackageFile="%(LibraryProjectZip.Pack)" Pack="false" CopyToOutputDirectory="PreserveNewest" Link="%(Filename)%(Extension)" />
     </ItemGroup>
     <!-- Legacy binding projects -->
     <ItemGroup Condition=" '$(_AndroidIsBindingProject)' == 'true' and '$(UsingAndroidNETSdk)' != 'true' ">

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/BuildTest.cs
@@ -2577,5 +2577,38 @@ namespace UnnamedProject
 			using var builder = CreateApkBuilder ();
 			Assert.IsTrue (builder.Build (proj), "Build should have succeeded.");
 		}
+
+		// Combination of class libraries that triggered the problem:
+		// error APT2144: invalid file path 'obj/Release/net8.0-android/lp/86.stamp'.
+		[Test]
+		public void ClassLibraryAarDependencies ()
+		{
+			var path = Path.Combine ("temp", TestName);
+			var material = new Package { Id = "Xamarin.Google.Android.Material", Version = "1.9.0.1" };
+			var libraryA = new XamarinAndroidLibraryProject {
+				ProjectName = "LibraryA",
+				Sources = {
+					new BuildItem.Source ("Bar.cs") {
+						TextContent = () => "public class Bar { }",
+					},
+				},
+				PackageReferences = { material },
+			};
+			using var builderA = CreateDllBuilder (Path.Combine (path, libraryA.ProjectName));
+			Assert.IsTrue (builderA.Build (libraryA), "Build should have succeeded.");
+
+			var libraryB = new XamarinAndroidLibraryProject {
+				ProjectName = "LibraryB",
+				Sources = {
+					new BuildItem.Source ("Foo.cs") {
+						TextContent = () => "public class Foo : Bar { }",
+					}
+				},
+				PackageReferences = { material },
+			};
+			libraryB.AddReference (libraryA);
+			using var builderB = CreateDllBuilder (Path.Combine (path, libraryB.ProjectName));
+			Assert.IsTrue (builderB.Build (libraryB), "Build should have succeeded.");
+		}
 	}
 }