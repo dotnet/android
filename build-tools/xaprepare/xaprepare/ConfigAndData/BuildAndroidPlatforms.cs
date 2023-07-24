028709cbe7d9ae196e5fe67300ff1c818af7cc3c
Marek Habersack <grendel@twistedcode.net>
2023-07-24 22:51:51 +0200

    Bump NDK to r26
    Changes: https://github.com/android/ndk/wiki/Changelog-r26#announcements
    
    NDK r26 has the following changes which are interesting to us:
    
      * libc++ has been updated. The NDK's libc++ now comes directly from
        our LLVM toolchain, so every future LLVM update is also a libc++
        update.
      * Updated LLVM to clang-r487747c, based on LLVM 17 development
    
    The `libc++` update brings internal implementation changes, and as the
    result we no longer need to partially instantiate a couple of classes
    previously underlying the `vector<T>` and `basic_string<T>`
    implementations.

--- build-tools/xaprepare/xaprepare/ConfigAndData/BuildAndroidPlatforms.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/BuildAndroidPlatforms.cs
@@ -5,8 +5,8 @@ namespace Xamarin.Android.Prepare
 {
 	class BuildAndroidPlatforms
 	{
-		public const string AndroidNdkVersion = "25c";
-		public const string AndroidNdkPkgRevision = "25.2.9519653";
+		public const string AndroidNdkVersion = "26-beta1";
+		public const string AndroidNdkPkgRevision = "26.0.10404224-beta1";
 		public const int NdkMinimumAPI = 21;
 		public const int NdkMinimumAPILegacy32 = 19;
 
