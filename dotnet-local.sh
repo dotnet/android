810756cb19ba50baf06dd4966b30f09d38cdfaec
Dean Ellis <dellis1972@googlemail.com>
2023-07-27 13:11:10 +0100

    Fix dotnet-local script to work.
    The recent bump to dotnet broke the dotnet-local scripts.
    It now seems that the "local" path must be FIRST in the PATH
    in order to pick up the packs.
    
    Also added a smoke test step to make sure the script works, at
    least one Mac and Linux.

--- dotnet-local.sh
+++ dotnet-local.sh
@@ -2,9 +2,11 @@
 ROOT="$(dirname "${BASH_SOURCE}")"
 FULLROOT="$(cd "${ROOT}"; pwd)"
 if [[ -x "${ROOT}/bin/Release/dotnet/dotnet" ]]; then
-    DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=${FULLROOT}/bin/Release/lib/sdk-manifests DOTNETSDK_WORKLOAD_PACK_ROOTS=${FULLROOT}/bin/Release/lib exec ${ROOT}/bin/Release/dotnet/dotnet "$@"
+    DOTNET_ROOT=${FULLROOT}/bin/Release/dotnet
+    PATH=${DOTNET_ROOT}:PATH DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=${FULLROOT}/bin/Release/lib/sdk-manifests DOTNETSDK_WORKLOAD_PACK_ROOTS=${FULLROOT}/bin/Release/lib exec ${ROOT}/bin/Release/dotnet/dotnet "$@"
 elif [[ -x "${ROOT}/bin/Debug/dotnet/dotnet" ]]; then
-    DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=${FULLROOT}/bin/Debug/lib/sdk-manifests DOTNETSDK_WORKLOAD_PACK_ROOTS=${FULLROOT}/bin/Debug/lib exec ${ROOT}/bin/Debug/dotnet/dotnet "$@"
+    DOTNET_ROOT=${FULLROOT}/bin/Debug/dotnet
+    PATH=${DOTNET_ROOT}:PATH DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=${FULLROOT}/bin/Debug/lib/sdk-manifests DOTNETSDK_WORKLOAD_PACK_ROOTS=${FULLROOT}/bin/Debug/lib exec ${ROOT}/bin/Debug/dotnet/dotnet "$@"
 else
     echo "You need to run 'make prepare' first."
 fi
