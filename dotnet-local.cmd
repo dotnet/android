7e767893ffa68eec8ae69619beca470ea466b520
Jonathan Pryor <jonpryor@vt.edu>
2023-08-08 15:40:48 -0400

    Avoid DOTNET_ROOT, just use PATH.
    Also, override %PATH% on Windows.
    
    This way the scripts are consistent between Unix & Windows.

--- dotnet-local.cmd
+++ dotnet-local.cmd
@@ -1,12 +1,14 @@
 @echo off
 SET ROOT=%~dp0
 IF EXIST "%ROOT%\bin\Release\dotnet\dotnet.exe" (
-    SET DOTNET_ROOT=%ROOT%\bin\Release\dotnet
+    SET XA_DOTNET_ROOT=%ROOT%\bin\Release\dotnet
+    SET PATH=%XA_DOTNET_ROOT%;%PATH%
     SET DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=%ROOT%\bin\Release\lib\sdk-manifests
     SET DOTNETSDK_WORKLOAD_PACK_ROOTS=%ROOT%\bin\Release\lib
     call "%ROOT%\bin\Release\dotnet\dotnet.exe" %*
 ) ELSE IF EXIST "%ROOT%\bin\Debug\dotnet\dotnet.exe" (
-    SET DOTNET_ROOT=%ROOT%\bin\Debug\dotnet
+    SET XA_DOTNET_ROOT=%ROOT%\bin\Debug\dotnet
+    SET PATH=%XA_DOTNET_ROOT%;%PATH%
     SET DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=%ROOT%\bin\Debug\lib\sdk-manifests
     SET DOTNETSDK_WORKLOAD_PACK_ROOTS=%ROOT%\bin\Debug\lib
     call "%ROOT%\bin\Debug\dotnet\dotnet.exe" %*
--- dotnet-local.sh
+++ dotnet-local.sh
@@ -5,8 +5,8 @@ for config in Release Debug ; do
     if [[ ! -x "${ROOT}/bin/${config}/dotnet/dotnet" ]] ; then
         continue
     fi
-    export DOTNET_ROOT="${FULLROOT}/bin/${config}/dotnet"
-    export PATH="${DOTNET_ROOT}:${PATH}"
+    XA_DOTNET_ROOT="${FULLROOT}/bin/${config}/dotnet"
+    export PATH="${XA_DOTNET_ROOT}:${PATH}"
     export DOTNETSDK_WORKLOAD_MANIFEST_ROOTS="${FULLROOT}/bin/${config}/lib/sdk-manifests"
     export DOTNETSDK_WORKLOAD_PACK_ROOTS="${FULLROOT}/bin/${config}/lib"
     exec "${ROOT}/bin/${config}/dotnet/dotnet" "$@"
