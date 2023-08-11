8e43cb7594a085030e31eeadf8acd71f7419aba4
Jonathan Pryor <jonpryor@vt.edu>
2023-08-10 22:01:53 -0400

    Update dotnet-local.cmd
    Cleanup

--- dotnet-local.cmd
+++ dotnet-local.cmd
@@ -4,16 +4,15 @@ SETLOCAL
 SET ROOT=%~dp0
 
 IF EXIST "%ROOT%\bin\Release\dotnet\dotnet.exe" (
-    SET XA_DOTNET_ROOT=%ROOT%\bin\Release\dotnet
     SET XA_CONFIG=Release
 ) ELSE IF EXIST "%ROOT%\bin\Debug\dotnet\dotnet.exe" (
-    SET XA_DOTNET_ROOT=%ROOT%\bin\Debug\dotnet
     SET XA_CONFIG=Debug
 ) ELSE (
     echo "You need to run 'msbuild Xamarin.Android.sln /t:Prepare' first."
     goto :exit
 )
 
+SET XA_DOTNET_ROOT=%ROOT%\bin\%XA_CONFIG%\dotnet
 SET PATH=%XA_DOTNET_ROOT%;%PATH%
 SET DOTNETSDK_WORKLOAD_MANIFEST_ROOTS=%ROOT%\bin\%XA_CONFIG%\lib\sdk-manifests
 SET DOTNETSDK_WORKLOAD_PACK_ROOTS=%ROOT%\bin\%XA_CONFIG%\lib
