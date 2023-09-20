29c0097893bee4b3609bae4ff950499219cfe101
Marek Habersack <grendel@twistedcode.net>
2023-09-20 09:47:48 +0200

    Bump our binary utilities toolchain to LLVM 17.0.1
    Changes: https://releases.llvm.org/17.0.1/docs/ReleaseNotes.html
    Changes: https://releases.llvm.org/17.0.1/tools/lld/docs/ReleaseNotes.html
    Changes: https://github.com/xamarin/xamarin-android-binutils/commit/83a894fcd5d57e6c53ca3b06c950cde34574b9b7
    
    LLVM 17.0.1 has just been released.  For full set of changes in this
    release please see the links above.  This is the same version of LLVM
    that is used in just-released Android NDK r26.
    
    The changes that are relevant to Xamarin.Android are:
    
        * Typed pointers are no longer supported and the -opaque-pointers
          option has been removed. See the opaque pointers documentation for
          migration instructions.
        * The nofpclass attribute was introduced. This allows more
          optimizations around special floating point value comparisons.
    
    Additionally, Vlad Braeze fixed Unicode process arguments handling on
    Windows in our `gas` wrapper.

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
@@ -15,7 +15,7 @@ namespace Xamarin.Android.Prepare
 	//
 	partial class Configurables
 	{
-		const string BinutilsVersion                = "L_16.0.6-6.0.0";
+		const string BinutilsVersion                = "L_17.0.1-7.0.0";
 
 		const string MicrosoftOpenJDK17Version      = "17.0.8";
 		const string MicrosoftOpenJDK17Release      = "17.0.8.7";
