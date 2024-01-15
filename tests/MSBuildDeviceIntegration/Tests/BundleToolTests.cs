dee8cf36006c31f2fe74a6f106955818ebc9af99
Marek Habersack <grendel@twistedcode.net>
2024-01-15 12:53:15 +0100

    Fix the `BaseZip` test

--- tests/MSBuildDeviceIntegration/Tests/BundleToolTests.cs
+++ tests/MSBuildDeviceIntegration/Tests/BundleToolTests.cs
@@ -163,26 +163,26 @@ namespace Xamarin.Android.Build.Tests
 			foreach (var abi in Abis) {
 				// All assemblies are in per-abi directories now
 				if (usesAssemblyBlobs) {
-					expectedFiles.Add ($"{blobEntryPrefix}{abi}/Java.Interop.dll");
-					expectedFiles.Add ($"{blobEntryPrefix}{abi}/Mono.Android.dll");
-					expectedFiles.Add ($"{blobEntryPrefix}{abi}/Localization.dll");
-					expectedFiles.Add ($"{blobEntryPrefix}{abi}/es/Localization.resources.dll");
-					expectedFiles.Add ($"{blobEntryPrefix}{abi}/UnnamedProject.dll");
+					expectedFiles.Add ($"{blobEntryPrefix}{abi}/#Java.Interop.dll.so");
+					expectedFiles.Add ($"{blobEntryPrefix}{abi}/#Mono.Android.dll.so");
+					expectedFiles.Add ($"{blobEntryPrefix}{abi}/#Localization.dll.so");
+					expectedFiles.Add ($"{blobEntryPrefix}{abi}/%es%Localization.resources.dll.so");
+					expectedFiles.Add ($"{blobEntryPrefix}{abi}/#UnnamedProject.dll.so");
 				} else {
-					expectedFiles.Add ($"lib/{abi}/Java.Interop.dll.so");
-					expectedFiles.Add ($"lib/{abi}/Mono.Android.dll.so");
-					expectedFiles.Add ($"lib/{abi}/Localization.dll.so");
-					expectedFiles.Add ($"lib/{abi}/es/Localization.resources.dll.so");
-					expectedFiles.Add ($"lib/{abi}/UnnamedProject.dll.so");
+					expectedFiles.Add ($"lib/{abi}/#Java.Interop.dll.so");
+					expectedFiles.Add ($"lib/{abi}/#Mono.Android.dll.so");
+					expectedFiles.Add ($"lib/{abi}/#Localization.dll.so");
+					expectedFiles.Add ($"lib/{abi}/%es%Localization.resources.dll.so");
+					expectedFiles.Add ($"lib/{abi}/#UnnamedProject.dll.so");
 				}
 
 				expectedFiles.Add ($"lib/{abi}/libmonodroid.so");
 				expectedFiles.Add ($"lib/{abi}/libmonosgen-2.0.so");
 				expectedFiles.Add ($"lib/{abi}/libxamarin-app.so");
 				if (usesAssemblyBlobs) {
-					expectedFiles.Add ($"{blobEntryPrefix}{abi}/System.Private.CoreLib.dll");
+					expectedFiles.Add ($"{blobEntryPrefix}{abi}/#System.Private.CoreLib.dll.so");
 				} else {
-					expectedFiles.Add ($"lib/{abi}/System.Private.CoreLib.dll.so");
+					expectedFiles.Add ($"lib/{abi}/#System.Private.CoreLib.dll.so");
 				}
 				expectedFiles.Add ($"lib/{abi}/libSystem.IO.Compression.Native.so");
 				expectedFiles.Add ($"lib/{abi}/libSystem.Native.so");
