593bb94e6594e9362e2b00b1bddbd79aade79283
Marek Habersack <grendel@twistedcode.net>
2024-02-16 12:45:27 +0100

    Fix p/invoke tables check script

--- build-tools/scripts/generate-pinvoke-tables.sh
+++ build-tools/scripts/generate-pinvoke-tables.sh
@@ -63,7 +63,7 @@ case ${HOST} in
 	*) die Unsupported OS ;;
 esac
 
-${COMPILER} -O2 -std=c++20 -I${EXTERNAL_DIR}/xxHash -I${EXTERNAL_DIR}/constexpr-xxh3 "${GENERATOR_SOURCE}" -o "${GENERATOR_BINARY}"
+${COMPILER} -O2 -std=c++20 -I${EXTERNAL_DIR} -I${EXTERNAL_DIR}/constexpr-xxh3 "${GENERATOR_SOURCE}" -o "${GENERATOR_BINARY}"
 "${GENERATOR_BINARY}" "${GENERATED_FILE}"
 
 FILES_DIFFER="no"
