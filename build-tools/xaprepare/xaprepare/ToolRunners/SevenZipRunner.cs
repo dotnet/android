36b33e7a59ec0f269a6e56e120c9dbcd5b1b9974
Marek Habersack <grendel@twistedcode.net>
2024-02-19 18:01:23 +0100

    [xaprepare] Make 7zip work with "dangerous" symlinks in ZIPs
    From time to time, `7zip` invoked by `xaprepare` with an error
    similar to:
    
        ERROR: Dangerous symbolic link path was ignored : android-ndk-r26c/toolchains/llvm/prebuilt/linux-x86_64/lib/python3.10/site-packages/lldb/lldb-argdumper : ../../../../bin/lldb-argdumper
        ERROR: Dangerous symbolic link path was ignored : android-ndk-r26c/toolchains/llvm/prebuilt/linux-x86_64/lib/python3.10/site-packages/lldb/_lldb.cpython-310-x86_64-linux-gnu.so : ../../../liblldb.so
    
    The problem is that this symlink is **not** a dangerous one, as it
    doesn't point outside the archived directory tree.
    
    This happened on mac, Windows and Linux alike.
    
    Try to work around the issue by using an undocumented `-snld` 7zip
    option when extracting ZIP archives.

--- build-tools/xaprepare/xaprepare/ToolRunners/SevenZipRunner.cs
+++ build-tools/xaprepare/xaprepare/ToolRunners/SevenZipRunner.cs
@@ -38,6 +38,13 @@ namespace Xamarin.Android.Prepare
 			ProcessRunner runner = CreateProcessRunner ("x");
 			AddStandardArguments (runner);
 			AddArguments (runner, extraArguments);
+
+			// Ignore some "dangerous" symbolic symlinks in the ZIP archives. This allows 7zip to unpack Android NDK archives
+			// without error.  The option appears to be undocumented, but was mentioned by the 7zip author here:
+			//
+			//   https://sourceforge.net/p/sevenzip/discussion/45798/thread/187ce54fb0/
+			//
+			runner.AddArgument ("-snld");
 			runner.AddQuotedArgument ($"-o{outputDirectory}");
 			runner.AddQuotedArgument (archivePath);
 
