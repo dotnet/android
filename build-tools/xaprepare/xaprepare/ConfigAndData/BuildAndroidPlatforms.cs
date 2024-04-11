512a7c02504e3ee8d26d1316c58a9422b6e1e7fc
Marek Habersack <grendel@twistedcode.net>
2024-04-11 22:15:16 +0200

    Bump NDK to r26d
    Changes: https://github.com/android/ndk/wiki/Changelog-r26#r26d
    
    The release appears to contain a single bugfix for ndk-gdb/ndk-lldb
    which doesn't affect us.  Toolchain version is unchanged.

--- build-tools/xaprepare/xaprepare/ConfigAndData/BuildAndroidPlatforms.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/BuildAndroidPlatforms.cs
@@ -5,8 +5,8 @@ namespace Xamarin.Android.Prepare
 {
 	class BuildAndroidPlatforms
 	{
-		public const string AndroidNdkVersion = "26c";
-		public const string AndroidNdkPkgRevision = "26.2.11394342";
+		public const string AndroidNdkVersion = "26d";
+		public const string AndroidNdkPkgRevision = "26.3.11579264";
 		public const int NdkMinimumAPI = 21;
 		public const int NdkMinimumAPILegacy32 = 21;
 
