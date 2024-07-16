a2bf1e7a43b8b8199f9bbe790a31a29d1b4a7e38
Marek Habersack <grendel@twistedcode.net>
2024-07-16 14:38:24 +0200

    Let's see what breaks

--- src/native/pinvoke-override/common.cc
+++ src/native/pinvoke-override/common.cc
@@ -3,4 +3,8 @@
 using namespace xamarin::android;
 
 PinvokeOverride::pinvoke_library_map PinvokeOverride::other_pinvoke_map (PinvokeOverride::LIBRARY_MAP_INITIAL_BUCKET_COUNT);
+<<<<<<< HEAD
 xamarin::android::mutex PinvokeOverride::pinvoke_map_write_lock;
+=======
+std::mutex PinvokeOverride::pinvoke_map_write_lock;
+>>>>>>> 13ba4b152 (Let's see what breaks)
