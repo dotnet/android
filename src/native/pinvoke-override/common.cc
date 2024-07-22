add253c3198ee82a043d9a7499623d8ab85a8beb
Marek Habersack <grendel@twistedcode.net>
2024-07-22 12:38:24 +0200

    Adjust after rebase

--- src/native/pinvoke-override/common.cc
+++ src/native/pinvoke-override/common.cc
@@ -3,4 +3,4 @@
 using namespace xamarin::android;
 
 PinvokeOverride::pinvoke_library_map PinvokeOverride::other_pinvoke_map (PinvokeOverride::LIBRARY_MAP_INITIAL_BUCKET_COUNT);
-std::mutex PinvokeOverride::pinvoke_map_write_lock;
+xamarin::android::mutex PinvokeOverride::pinvoke_map_write_lock;
