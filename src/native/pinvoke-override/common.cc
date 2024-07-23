71f70a92a5bc194d6695e613de308a10e65a422a
Marek Habersack <grendel@twistedcode.net>
2024-07-23 18:23:56 +0200

    Fix post-rebase issues and conflicts

--- src/native/pinvoke-override/common.cc
+++ src/native/pinvoke-override/common.cc
@@ -4,7 +4,11 @@ using namespace xamarin::android;
 
 PinvokeOverride::pinvoke_library_map PinvokeOverride::other_pinvoke_map (PinvokeOverride::LIBRARY_MAP_INITIAL_BUCKET_COUNT);
 <<<<<<< HEAD
+<<<<<<< HEAD
 xamarin::android::mutex PinvokeOverride::pinvoke_map_write_lock;
 =======
 std::mutex PinvokeOverride::pinvoke_map_write_lock;
 >>>>>>> 13ba4b152 (Let's see what breaks)
+=======
+xamarin::android::mutex PinvokeOverride::pinvoke_map_write_lock;
+>>>>>>> da87011a5 (Fix post-rebase issues and conflicts)
