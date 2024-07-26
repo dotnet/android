16cdbb0e0158544e26c4a2b1c76bb69cd46a9e91
Šimon Rozsíval <simon@rozsival.com>
2024-07-26 19:52:25 +0200

    [Mono.Android] Add convenience KeyChain APIs (#9047)
    Context: https://github.com/dotnet/runtime/issues/99874
    Context: https://github.com/dotnet/runtime/pull/103337
    
    dotnet/runtime#103337 added the ability for the
    [`X509Certificate2(IntPtr)` constructor][0] to accept a
    [`java.security.KeyStore.PrivateKeyEntry`][1] instance.
    
    Add convenience methods to [`Android.Security.KeyChain`][2] to make
    it easier for developers to access certificates with non-exportable
    private keys:
    
    	partial class KeyChain {
    	    public static X509Certificate2? GetX509Certificate2WithPrivateKey (
    	            Android.Content.Context context,
    	            string alias);
    	    public static async Task<string?> ChoosePrivateKeyAliasAsync (
    	            Android.App.Activity activity,
    	            string[]? keyTypes,
    	            Java.Security.IPrincipal[]? issuers,
    	            Android.Net.Uri? uri,
    	            string? alias);
    	    public static async Task<string?> ChoosePrivateKeyAliasAsync (
    	            Android.App.Activity activity,
    	            string[]? keyTypes,
    	            Java.Security.IPrincipal[]? issuers,
    	            string? host,
    	            int port,
    	            string? alias);
    	    public static async Task<X509Certificate2?> ChooseX509Certificate2WithPrivateKeyAsync (
    	            Android.App.Activity activity,
    	            string[]? keyTypes,
    	            Java.Security.IPrincipal[]? issuers,
    	            Android.Net.Uri? uri,
    	            string? alias);
    	    public static async Task<X509Certificate2?> ChooseX509Certificate2WithPrivateKeyAsync (
    	            Android.App.Activity activity,
    	            string[]? keyTypes,
    	            Java.Security.IPrincipal[]? issuers,
    	            string? host,
    	            int port,
    	            string? alias);
    	}
    
    [0]: https://learn.microsoft.com/dotnet/api/system.security.cryptography.x509certificates.x509certificate2.-ctor?view=net-8.0#system-security-cryptography-x509certificates-x509certificate2-ctor(system-intptr)
    [1]: https://developer.android.com/reference/java/security/KeyStore.PrivateKeyEntry
    [2]: https://learn.microsoft.com/dotnet/api/android.security.keychain?view=net-android-34.0

---
+++ src/Mono.Android/Android.Security/KeyChain.cs
@@ -0,0 +1,93 @@
+#nullable enable
+using System;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Android.Security
+{
+	public partial class KeyChain
+	{
+		public static X509Certificate2? GetX509Certificate2WithPrivateKey (Android.Content.Context context, string alias)
+		{
+			var privateKey = KeyChain.GetPrivateKey (context, alias);
+			if (privateKey is null) {
+				return null;
+			}
+
+			var chain = KeyChain.GetCertificateChain (context, alias);
+			if (chain is null) {
+				return null;
+			}
+
+			var privateKeyEntry = new Java.Security.KeyStore.PrivateKeyEntry (privateKey, chain);
+			var certificate = new X509Certificate2 (privateKeyEntry.Handle);
+			GC.KeepAlive (privateKeyEntry);
+			return certificate;
+		}
+
+		[SupportedOSPlatform("android23.0")]
+		public static async Task<string?> ChoosePrivateKeyAliasAsync (
+			Android.App.Activity activity,
+			string[]? keyTypes,
+			Java.Security.IPrincipal[]? issuers,
+			Android.Net.Uri? uri,
+			string? alias)
+		{
+			var tcs = new TaskCompletionSource<string?> ();
+			KeyChain.ChoosePrivateKeyAlias (activity, new KeyChainAliasCallback(tcs), keyTypes, issuers, uri, alias);
+			return await tcs.Task;
+		}
+
+		public static async Task<string?> ChoosePrivateKeyAliasAsync (
+			Android.App.Activity activity,
+			string[]? keyTypes,
+			Java.Security.IPrincipal[]? issuers,
+			string? host,
+			int port,
+			string? alias)
+		{
+			var tcs = new TaskCompletionSource<string?> ();
+			KeyChain.ChoosePrivateKeyAlias (activity, new KeyChainAliasCallback(tcs), keyTypes, issuers, host, port, alias);
+			return await tcs.Task;
+		}
+
+		[SupportedOSPlatform("android23.0")]
+		public static async Task<X509Certificate2?> ChooseX509Certificate2WithPrivateKeyAsync (
+			Android.App.Activity activity,
+			string[]? keyTypes,
+			Java.Security.IPrincipal[]? issuers,
+			Android.Net.Uri? uri,
+			string? alias)
+		{
+			alias = await ChoosePrivateKeyAliasAsync (activity, keyTypes, issuers, uri, alias);
+			if (alias is null) {
+				return null;
+			}
+
+			return GetX509Certificate2WithPrivateKey (activity, alias);
+		}
+
+		public static async Task<X509Certificate2?> ChooseX509Certificate2WithPrivateKeyAsync (
+			Android.App.Activity activity,
+			string[]? keyTypes,
+			Java.Security.IPrincipal[]? issuers,
+			string? host,
+			int port,
+			string? alias)
+		{
+			alias = await ChoosePrivateKeyAliasAsync (activity, keyTypes, issuers, host, port, alias);
+			if (alias is null) {
+				return null;
+			}
+
+			return GetX509Certificate2WithPrivateKey (activity, alias);
+		}
+
+		private sealed class KeyChainAliasCallback(TaskCompletionSource<string?> tcs)
+			: Java.Lang.Object, IKeyChainAliasCallback
+		{
+			public void Alias (string? alias) => tcs.SetResult (alias);
+		}
+	}
+}
