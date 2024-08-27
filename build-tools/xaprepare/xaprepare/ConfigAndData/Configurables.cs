3f7b329533018b7373395119f436430272d57a14
Marek Habersack <grendel@twistedcode.net>
2024-08-27 10:47:22 +0200

    Back to LLVM 18.1.6

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.cs
@@ -15,7 +15,7 @@ namespace Xamarin.Android.Prepare
 	//
 	partial class Configurables
 	{
-		const string BinutilsVersion                = "L_18.1.8-8.0.0";
+		const string BinutilsVersion                = "L_18.1.6-8.0.0-1";
 
 		const string MicrosoftOpenJDK17Version      = "17.0.12";
 		const string MicrosoftOpenJDK17Release      = "17.0.12";
