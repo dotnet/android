a2b4b5a6530d8a86cd76f302f6e00fb46807cb05
Marek Habersack <grendel@twistedcode.net>
2024-09-05 21:31:31 +0200

    Beginnings of real code generation

--- src/Xamarin.Android.Build.Tasks/Utilities/PreservePinvokesNativeAssemblyGenerator.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/PreservePinvokesNativeAssemblyGenerator.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
 using Microsoft.Android.Build.Tasks;
 using Microsoft.Build.Framework;
@@ -11,13 +9,43 @@ using Microsoft.Build.Utilities;
 using Xamarin.Android.Tasks.LLVMIR;
 using Xamarin.Android.Tools;
 
-using CecilMethodDefinition = global::Mono.Cecil.MethodDefinition;
-using CecilParameterDefinition = global::Mono.Cecil.ParameterDefinition;
-
 namespace Xamarin.Android.Tasks;
 
 class PreservePinvokesNativeAssemblyGenerator : LlvmIrComposer
 {
+	sealed class PInvoke
+	{
+		public readonly LlvmIrFunction NativeFunction;
+		public readonly PinvokeScanner.PinvokeEntryInfo Info;
+
+		public PInvoke (LlvmIrModule module, PinvokeScanner.PinvokeEntryInfo pinfo)
+		{
+			Info = pinfo;
+
+			// All the p/invoke functions use the same dummy signature.  The only thing we care about is
+			// a way to reference to the symbol at build time so that we can return pointer to it.  For
+			// that all we need is a known name, signature doesn't matter to us.
+			var funcSig = new LlvmIrFunctionSignature (name: pinfo.EntryName, returnType: typeof(void));
+			NativeFunction = module.DeclareExternalFunction (funcSig);
+		}
+	}
+
+	sealed class Component
+	{
+		public readonly string Name;
+		public readonly ulong NameHash;
+		public readonly List<PInvoke> PInvokes;
+		public bool Is64Bit;
+
+		public Component (string name, ulong nameHash, List<PInvoke> pinvokes, bool is64Bit)
+		{
+			Name = name;
+			NameHash = nameHash;
+			PInvokes = pinvokes;
+			Is64Bit = is64Bit;
+		}
+	}
+
 	// Maps a component name after ridding it of the `lib` prefix and the extension to a "canonical"
 	// name of a library, as used in `[DllImport]` attributes.
 	readonly Dictionary<string, string> libraryNameMap = new (StringComparer.Ordinal) {
@@ -42,7 +70,7 @@ class PreservePinvokesNativeAssemblyGenerator : LlvmIrComposer
 
 	protected override void Construct (LlvmIrModule module)
 	{
-		Log.LogDebugMessage ("Constructing p/invoke preserve code");
+		Log.LogDebugMessage ($"[{state.TargetArch}] Constructing p/invoke preserve code");
 		List<PinvokeScanner.PinvokeEntryInfo> pinvokeInfos = state.PinvokeInfos!;
 		if (pinvokeInfos.Count == 0) {
 			// This is a very unlikely scenario, but we will work just fine.  The module that this generator produces will merely result
@@ -67,14 +95,50 @@ class PreservePinvokesNativeAssemblyGenerator : LlvmIrComposer
 			return;
 		}
 
+		bool is64Bit = state.TargetArch switch {
+			AndroidTargetArch.Arm64  => true,
+			AndroidTargetArch.X86_64 => true,
+			AndroidTargetArch.Arm    => false,
+			AndroidTargetArch.X86    => false,
+			_                        => throw new NotSupportedException ($"Architecture {state.TargetArch} is not supported here")
+		};
+
 		Log.LogDebugMessage ("  Checking discovered p/invokes against the list of components");
+		var preservedPerComponent = new Dictionary<string, Component> (StringComparer.OrdinalIgnoreCase);
+		var processedCache = new HashSet<string> (StringComparer.OrdinalIgnoreCase);
+
 		foreach (PinvokeScanner.PinvokeEntryInfo pinfo in pinvokeInfos) {
 			Log.LogDebugMessage ($"    p/invoke: {pinfo.EntryName} in {pinfo.LibraryName}");
-			if (MustPreserve (pinfo, componentNames)) {
-				Log.LogDebugMessage ("      must be preserved");
-			} else {
-				Log.LogDebugMessage ("      no need to preserve");
+			string key = $"{pinfo.LibraryName}/${pinfo.EntryName}";
+			if (processedCache.Contains (key)) {
+				Log.LogDebugMessage ($"      already processed");
+				continue;
 			}
+
+			processedCache.Add (key);
+			if (!MustPreserve (pinfo, componentNames)) {
+				Log.LogDebugMessage ("      no need to preserve");
+				continue;
+			}
+			Log.LogDebugMessage ("      must be preserved");
+
+			if (!preservedPerComponent.TryGetValue (pinfo.LibraryName, out Component? component)) {
+				component = new Component (
+					pinfo.LibraryName,
+					MonoAndroidHelper.GetXxHash (pinfo.LibraryName, is64Bit),
+					new List<PInvoke> (),
+					is64Bit
+				);
+				preservedPerComponent.Add (pinfo.LibraryName, component);
+			}
+			component.PInvokes.Add (new PInvoke (module, pinfo));
+		}
+
+		Log.LogDebugMessage ("  Components to be preserved:");
+
+		foreach (var kvp in preservedPerComponent) {
+			var component = kvp.Value;
+			Log.LogDebugMessage ($"    {component.Name} (hash: 0x{component.NameHash:x}; {component.PInvokes.Count} p/invoke(s))");
 		}
 	}
 
