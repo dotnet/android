1b84eb0d343d7a2c58ed6d1d307085b7bbc09f12
Dean Ellis <dellis1972@googlemail.com>
2024-09-13 15:12:13 +0100

    Fix build error

--- src/Xamarin.Android.Build.Tasks/Tasks/CreateAar.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/CreateAar.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Xml.Linq;
 using Microsoft.Build.Framework;
 using Xamarin.Android.Tools;
 using Xamarin.Tools.Zip;
