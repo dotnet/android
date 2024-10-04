3be2474ec53fa40e2857223a8055c6ecb18ff672
Dean Ellis <dellis1972@googlemail.com>
2024-10-04 15:36:17 +0100

    Handle more Attribute Types when Filtering Assemblies
    Fixes #9369
    
    When System.Reflection.Metadata comes across an Attribute
    which has a generic, it does NOT return a `TypeReferenceHandle`.
    It returns a `TypeSpecificationHandle`.
    
    Tying to cast the type results in the following error
    
    ```
    System.InvalidCastException: Die angegebene Umwandlung ist ungültig.
    error XAFLT7007:    bei System.Reflection.Throw.InvalidCast()
    error XAFLT7007:    bei System.Reflection.Metadata.TypeReferenceHandle.op_Explicit(EntityHandle handle)
    error XAFLT7007:    bei Xamarin.Android.Tasks.MetadataExtensions.GetCustomAttributeFullName(MetadataReader reader, CustomAttribute attribute)
    error XAFLT7007:    bei Xamarin.Android.Tasks.FilterAssemblies.IsAndroidAssembly(AssemblyDefinition assembly, MetadataReader reader)
    error XAFLT7007:    bei Xamarin.Android.Tasks.FilterAssemblies.ProcessAssembly(ITaskItem assemblyItem, List`1 output)
    error XAFLT7007:    bei Xamarin.Android.Tasks.FilterAssemblies.RunTask()
    error XAFLT7007:    bei Microsoft.Android.Build.Tasks.AndroidTask.Execute()
    ```
    
    So we need to handle this case, and put in place a backup to handle
    an `InvalidCastException` so that we never hit this issue again.

--- src/Xamarin.Android.Build.Tasks/Utilities/MetadataExtensions.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/MetadataExtensions.cs
@@ -12,8 +12,23 @@ namespace Xamarin.Android.Tasks
 		{
 			if (attribute.Constructor.Kind == HandleKind.MemberReference) {
 				var ctor = reader.GetMemberReference ((MemberReferenceHandle)attribute.Constructor);
-				var type = reader.GetTypeReference ((TypeReferenceHandle)ctor.Parent);
-				return reader.GetString (type.Namespace) + "." + reader.GetString (type.Name);
+				try {
+					if (ctor.Parent.Kind == HandleKind.TypeReference) {
+						var type = reader.GetTypeReference ((TypeReferenceHandle)ctor.Parent);
+						return reader.GetString (type.Namespace) + "." + reader.GetString (type.Name);
+					} else if (ctor.Parent.Kind == HandleKind.TypeSpecification) {
+						var type = reader.GetTypeSpecification ((TypeSpecificationHandle)ctor.Parent);
+						BlobReader blobReader = reader.GetBlobReader (type.Signature);
+						SignatureTypeCode typeCode = blobReader.ReadSignatureTypeCode ();
+						EntityHandle typeHandle = blobReader.ReadTypeHandle ();
+						TypeReference typeRef = reader.GetTypeReference ((TypeReferenceHandle)typeHandle);
+						return reader.GetString (typeRef.Namespace) + "." + reader.GetString (typeRef.Name);
+					}
+				}
+				catch (InvalidCastException)
+				{
+					return null;
+				}
 			} else if (attribute.Constructor.Kind == HandleKind.MethodDefinition) {
 				var ctor = reader.GetMethodDefinition ((MethodDefinitionHandle)attribute.Constructor);
 				var type = reader.GetTypeDefinition (ctor.GetDeclaringType ());
