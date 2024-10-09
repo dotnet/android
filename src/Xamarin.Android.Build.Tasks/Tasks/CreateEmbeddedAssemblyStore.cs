1a83ccba005e51a37408025abd9afd6373bd40c1
Marek Habersack <grendel@twistedcode.net>
2024-10-09 17:24:11 +0200

    New task to build embedded assembly store

--- src/Xamarin.Android.Build.Tasks/Tasks/CreateEmbeddedAssemblyStore.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/CreateEmbeddedAssemblyStore.cs
@@ -1,11 +1,31 @@
-using Xamarin.Android.Tools;
+
 using Microsoft.Android.Build.Tasks;
+using Microsoft.Build.Framework;
+using Xamarin.Android.Tools;
 
 namespace Xamarin.Android.Tasks;
 
 public class CreateEmbeddedAssemblyStore : AndroidTask
 {
 	public override string TaskPrefix => "CEAS";
+
+	[Required]
+	public string AndroidBinUtilsDirectory { get; set; }
+
+	[Required]
+	public string AppSharedLibrariesDir { get; set; }
+
+	[Required]
+	public ITaskItem[] ResolvedUserAssemblies { get; set; }
+
+	[Required]
+	public ITaskItem[] ResolvedFrameworkAssemblies { get; set; }
+
+	[Output]
+	public ITaskItem[] NativeAssemblySources { get; set; }
+
+	[Output]
+	public ITaskItem[] EmbeddedObjectFiles { get; set; }
 
 	public override bool RunTask ()
 	{
