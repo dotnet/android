f8d30553346b81b716f3cf3f3167977cb165c638
Marek Habersack <grendel@twistedcode.net>
2024-10-16 17:39:06 +0200

    Teach assembly store explorer about embedded stores

--- tools/assembly-store-reader-mk2/AssemblyStore/StoreReader_V2.cs
+++ tools/assembly-store-reader-mk2/AssemblyStore/StoreReader_V2.cs
@@ -86,6 +86,19 @@ partial class StoreReader_V2 : AssemblyStoreReader
 
 	protected override ulong GetStoreStartDataOffset () => elfOffset;
 
+	string PayloadErrorToString (ELFPayloadError error)
+	{
+		return error switch {
+			ELFPayloadError.NotELF           => $"Store '{StorePath}' is not a valid ELF binary",
+			ELFPayloadError.LoadFailed       => $"Store '{StorePath}' could not be loaded",
+			ELFPayloadError.NotSharedLibrary => $"Store '{StorePath}' is not a shared ELF library",
+			ELFPayloadError.NotLittleEndian  => $"Store '{StorePath}' is not a little-endian ELF image",
+			ELFPayloadError.NoPayloadSection => $"Store '{StorePath}' does not contain the 'payload' section",
+			ELFPayloadError.NoEmbeddedStore  => $"Store '{StorePath}' does not contain embedded data blob",
+			_                                => $"Unknown ELF payload section error for store '{StorePath}': {error}"
+		};
+	}
+
 	protected override bool IsSupported ()
 	{
 		StoreStream.Seek (0, SeekOrigin.Begin);
@@ -94,22 +107,19 @@ partial class StoreReader_V2 : AssemblyStoreReader
 		uint magic = reader.ReadUInt32 ();
 		if (magic == Utils.ELF_MAGIC) {
 			ELFPayloadError error;
-			(elfOffset, _, error) = Utils.FindELFPayloadSectionOffsetAndSize (StoreStream);
-
+			(elfOffset, _, error) = Utils.FindEmbeddedStoreOffsetAndSize (StoreStream);
 			if (error != ELFPayloadError.None) {
-				string message = error switch {
-					ELFPayloadError.NotELF           => $"Store '{StorePath}' is not a valid ELF binary",
-					ELFPayloadError.LoadFailed       => $"Store '{StorePath}' could not be loaded",
-					ELFPayloadError.NotSharedLibrary => $"Store '{StorePath}' is not a shared ELF library",
-					ELFPayloadError.NotLittleEndian  => $"Store '{StorePath}' is not a little-endian ELF image",
-					ELFPayloadError.NoPayloadSection => $"Store '{StorePath}' does not contain the 'payload' section",
-					_                                => $"Unknown ELF payload section error for store '{StorePath}': {error}"
-				};
-				Log.Debug (message);
-			} else if (elfOffset >= 0) {
-				StoreStream.Seek ((long)elfOffset, SeekOrigin.Begin);
-				magic = reader.ReadUInt32 ();
+				MaybeLogError (error);
+
+				(elfOffset, _, error) = Utils.FindELFPayloadSectionOffsetAndSize (StoreStream);
+				if (error != ELFPayloadError.None) {
+					MaybeLogError (error);
+					return false;
+				}
 			}
+
+			StoreStream.Seek ((long)elfOffset, SeekOrigin.Begin);
+			magic = reader.ReadUInt32 ();
 		}
 
 		if (magic != Utils.ASSEMBLY_STORE_MAGIC) {
@@ -129,6 +139,15 @@ partial class StoreReader_V2 : AssemblyStoreReader
 
 		header = new Header (magic, version, entry_count, index_entry_count, index_size);
 		return true;
+
+		void MaybeLogError (ELFPayloadError error)
+		{
+			if (error == ELFPayloadError.None) {
+				return;
+			}
+
+			Log.Debug (PayloadErrorToString (error));
+		}
 	}
 
 	protected override void Prepare ()
