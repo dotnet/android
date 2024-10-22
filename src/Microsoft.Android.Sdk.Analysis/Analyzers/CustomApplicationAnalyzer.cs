00dcccb5fa4b6faec56c98fc296a69cdfa62e708
Dean Ellis <dellis1972@googlemail.com>
2024-10-22 16:58:09 +0100

    Add Code Analyzer and CodeFixProvider for Custom Application classes

---
+++ src/Microsoft.Android.Sdk.Analysis/Analyzers/CustomApplicationAnalyzer.cs
@@ -0,0 +1,53 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+public class CustomApplicationAnalyzer : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "XAA001";
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor (
+        DiagnosticId,                          // Diagnostic ID
+        "Application class does not have an Activation Constructor.",      // Title
+        "Application class '{0}' does not have an Activation Constructor.",  // Message format
+        "Code",                          // Category
+        DiagnosticSeverity.Warning,        // Default severity
+        isEnabledByDefault: true  // Enabled by default
+    );
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (Rule);
+
+	public override void Initialize (AnalysisContext context)
+	{
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+    
+        // Register a syntax node action to analyze method declarations
+        context.RegisterSyntaxNodeAction(AnalyzeClass, SyntaxKind.ClassDeclaration);
+	}
+
+    private static void AnalyzeClass(SyntaxNodeAnalysisContext context)
+    {
+        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+        var constructors = classDeclarationSyntax.Members
+            .OfType<ConstructorDeclarationSyntax>();
+
+        bool foundActivationConstructor = false;
+        foreach (var constructor in constructors) {
+            var parameters = constructor.ParameterList.Parameters;
+            if (parameters.Count != 2)
+                continue;
+            if (parameters[0].Type.ToString () != "IntPtr")
+                continue;
+            if (parameters[1].Type.ToString () != "JniHandleOwnership")
+                continue;
+            foundActivationConstructor = true;
+        }
+        if (!foundActivationConstructor) {
+            var diagnostic = Diagnostic.Create(Rule, classDeclarationSyntax.Identifier.GetLocation(), classDeclarationSyntax.Identifier.Value);
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
