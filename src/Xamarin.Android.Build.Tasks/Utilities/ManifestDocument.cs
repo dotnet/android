0a1ff37dea86a86aba88773de84d8a47f9c8a439
Marek Habersack <grendel@twistedcode.net>
2024-10-23 18:29:51 +0200

    Log it when an assembly is missing in ManifestDocument

--- src/Xamarin.Android.Build.Tasks/Utilities/ManifestDocument.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/ManifestDocument.cs
@@ -287,7 +287,7 @@ namespace Xamarin.Android.Tasks {
 					string.IsNullOrEmpty (VersionName) ? "1.0" : VersionName);
 			}
 
-			app = CreateApplicationElement (manifest, applicationClass, subclasses, cache);
+			app = CreateApplicationElement (log, manifest, applicationClass, subclasses, cache);
 
 			if (app.Attribute (androidNs + "label") == null && !string.IsNullOrEmpty (ApplicationLabel))
 				app.SetAttributeValue (androidNs + "label", ApplicationLabel);
@@ -567,7 +567,7 @@ namespace Xamarin.Android.Tasks {
 			return null;
 		}
 
-		XElement CreateApplicationElement (XElement manifest, string applicationClass, List<TypeDefinition> subclasses, TypeDefinitionCache cache)
+		XElement CreateApplicationElement (TaskLoggingHelper log, XElement manifest, string applicationClass, List<TypeDefinition> subclasses, TypeDefinitionCache cache)
 		{
 			var application = manifest.Descendants ("application").FirstOrDefault ();
 
@@ -579,6 +579,7 @@ namespace Xamarin.Android.Tasks {
 			foreach (var assemblyPath in Assemblies) {
 				var assembly = Resolver.GetAssembly (assemblyPath);
 				if (assembly == null) {
+					log.LogDebugMessage ($"Assembly '{assemblyPath}' not found.");
 					continue;
 				}
 
