3b78840bf33934051b3be6bb888a84c5fd26e465
Dean Ellis <dellis1972@googlemail.com>
2024-10-23 16:24:12 +0100

    Fix formatting

--- src/Microsoft.Android.Sdk.Analysis/Analyzers/CustomApplicationAnalyzer.cs
+++ src/Microsoft.Android.Sdk.Analysis/Analyzers/CustomApplicationAnalyzer.cs
@@ -5,7 +5,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-[DiagnosticAnalyzer(LanguageNames.CSharp)]
+[DiagnosticAnalyzer (LanguageNames.CSharp)]
 public class CustomApplicationAnalyzer : DiagnosticAnalyzer
 {
     private const string AndroidApplication = "Android.App.Application";
@@ -19,44 +19,44 @@ public class CustomApplicationAnalyzer : DiagnosticAnalyzer
         isEnabledByDefault: true
     );
 
-	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (Rule);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (Rule);
 
-	public override void Initialize (AnalysisContext context)
-	{
-		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-        context.EnableConcurrentExecution();
-    
-        // Register a syntax node action to analyze method declarations
-        context.RegisterSyntaxNodeAction(AnalyzeClass, SyntaxKind.ClassDeclaration);
-	}
-
-    private static void AnalyzeClass(SyntaxNodeAnalysisContext context)
+    public override void Initialize (AnalysisContext context)
     {
-        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
-        var classSymbol = context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax) as INamedTypeSymbol;
+        context.ConfigureGeneratedCodeAnalysis (GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution ();
+
+        // Register a syntax node action to analyze method declarations
+        context.RegisterSyntaxNodeAction (AnalyzeClass, SyntaxKind.ClassDeclaration);
+    }
+
+    private static void AnalyzeClass (SyntaxNodeAnalysisContext context)
+    {
+        var classDeclarationSyntax = (ClassDeclarationSyntax) context.Node;
+        var classSymbol = context.SemanticModel.GetDeclaredSymbol (classDeclarationSyntax) as INamedTypeSymbol;
         if (classSymbol == null)
             return;
 
         if (!Utilities.IsDerivedFrom (classSymbol, AndroidApplication))
             return;
-        
+
         var constructors = classDeclarationSyntax.Members
-            .OfType<ConstructorDeclarationSyntax>();
+            .OfType<ConstructorDeclarationSyntax> ();
 
         bool foundActivationConstructor = false;
         foreach (var constructor in constructors) {
             var parameters = constructor.ParameterList.Parameters;
             if (parameters.Count != 2)
                 continue;
-            if (parameters[0].Type.ToString () != "IntPtr")
+            if (parameters [0].Type.ToString () != "IntPtr")
                 continue;
-            if (parameters[1].Type.ToString () != "JniHandleOwnership")
+            if (parameters [1].Type.ToString () != "JniHandleOwnership")
                 continue;
             foundActivationConstructor = true;
         }
         if (!foundActivationConstructor) {
-            var diagnostic = Diagnostic.Create(Rule, classDeclarationSyntax.Identifier.GetLocation(), classDeclarationSyntax.Identifier.Text);
-            context.ReportDiagnostic(diagnostic);
+            var diagnostic = Diagnostic.Create (Rule, classDeclarationSyntax.Identifier.GetLocation (), classDeclarationSyntax.Identifier.Text);
+            context.ReportDiagnostic (diagnostic);
         }
     }
 }
