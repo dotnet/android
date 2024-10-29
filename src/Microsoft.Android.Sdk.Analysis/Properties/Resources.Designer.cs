7b2e8dc5653e28b5db18227d043a1cb6b2babdff
Dean Ellis <dellis1972@googlemail.com>
2024-10-29 16:58:24 +0000

    Fix error

--- src/Microsoft.Android.Sdk.Analysis/Properties/Resources.Designer.cs
+++ src/Microsoft.Android.Sdk.Analysis/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Android.Sdk.Analysis {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xamarin.Android.Tasks.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Android.Sdk.Analysis.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
