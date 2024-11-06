715a36af86c68bac623a4174477165bb14b4c371
Dean Ellis <dellis1972@googlemail.com>
2024-11-06 19:30:38 +0000

    [Microsoft.Android.Sdk.Analysis] Warn on missing activation ctors (#9447)
    Fixes: https://github.com/dotnet/android/issues/8410
    
    Context: b3079db44e4f1f626d5708e47f0b83153b8548e6
    Context: https://learn.microsoft.com/dotnet/csharp/roslyn-sdk/tutorials/how-to-write-csharp-analyzer-code-fix
    
    Commit b3079db4 added a `Microsoft.Android.Sdk.Analysis.dll` assembly
    which contained a [`DiagnosticAnalyzer`][0] to suppress IDE0002
    warnings.
    
    Extend that support to add a `CustomApplicationAnalyzer` which checks
    for the [`(IntPtr, JniHandleOwnership)`][1] constructor on
    [`Android.App.Application`][2] subclasses, as this constructor is
    currently required.
    
    If the constructor is missing:
    
    	[Application]
    	public class MyApp : Application {
    	}
    
    then the app will crash during startup:
    
    	System.NotSupportedException: Unable to activate instance of type MyApp from native handle 0x7ff1f3b468 (key_handle 0x466b26f).
    
    If we find an `Application` subclass that is missing the required
    constructor, we'll emit a DNAA0001 warning:
    
    	warning DNAA0001: Application class 'MyApp' does not have an Activation Constructor.
    
    Additionally, provide a `CustomApplicationCodeFixProvider` which will
    inject the required constructor into the syntax tree.
    
    Finally, rename the previous `XAD0001` warning to `DNAS0001`.
    Our convention is as follows:
    
      * `DNA`: prefix for .NET for Android messages
      * `A` for *Analyzers*, `S` for *Suppressors*
      * 4 digit code.
    
    [0]: https://learn.microsoft.com/en-us/dotnet/api/microsoft.codeanalysis.diagnostics.diagnosticanalyzer?view=roslyn-dotnet-4.9.0
    [1]: https://learn.microsoft.com/en-us/dotnet/api/android.app.application.-ctor?view=net-android-34.0#android-app-application-ctor(system-intptr-android-runtime-jnihandleownership)
    [2]: https://learn.microsoft.com/en-us/dotnet/api/android.app.application?view=net-android-34.0

---
+++ src/Microsoft.Android.Sdk.Analysis/Analyzers/CustomApplicationAnalyzer.cs
@@ -0,0 +1,69 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.Android.Sdk.Analysis;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+[DiagnosticAnalyzer (LanguageNames.CSharp)]
+public class CustomApplicationAnalyzer : DiagnosticAnalyzer
+{
+	private const string AndroidApplication = "Android.App.Application";
+	public const string DiagnosticId = "DNAA0001";
+	private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor (
+		id: DiagnosticId,
+		title: Resources.DNAA0001_Title,
+		messageFormat: Resources.DNAA0001_MessageFormat,
+		category: "Usage",
+		defaultSeverity: DiagnosticSeverity.Warning,
+		isEnabledByDefault: true
+	);
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (Rule);
+
+	public override void Initialize (AnalysisContext context)
+	{
+		context.ConfigureGeneratedCodeAnalysis (GeneratedCodeAnalysisFlags.None);
+		context.EnableConcurrentExecution ();
+
+		// Register a syntax node action to analyze method declarations
+		context.RegisterSyntaxNodeAction (AnalyzeClass, SyntaxKind.ClassDeclaration);
+	}
+
+	private static void AnalyzeClass (SyntaxNodeAnalysisContext context)
+	{
+		var classDeclarationSyntax = context.Node as ClassDeclarationSyntax;
+		if (classDeclarationSyntax == null)
+			return;
+
+		var classSymbol = context.SemanticModel.GetDeclaredSymbol (classDeclarationSyntax) as INamedTypeSymbol;
+		if (classSymbol == null)
+			return;
+
+		if (!Utilities.IsDerivedFrom (classSymbol, AndroidApplication))
+			return;
+
+		var constructors = classDeclarationSyntax.Members
+			.OfType<ConstructorDeclarationSyntax> ();
+
+		bool foundActivationConstructor = false;
+		foreach (var constructor in constructors) {
+			var parameters = constructor.ParameterList.Parameters;
+			if (parameters.Count != 2)
+				continue;
+			if (parameters [0].Type.ToString () != "IntPtr")
+				continue;
+			var ns = Utilities.GetNamespaceForParameterType (parameters [1], context.SemanticModel);
+            var type = parameters [1].Type.ToString();
+			var isJniHandle = (ns == "Android.Runtime") && (type == "JniHandleOwnership") || (type == "Android.Runtime.JniHandleOwnership");
+			if (!isJniHandle)
+				continue;
+			foundActivationConstructor = true;
+		}
+		if (!foundActivationConstructor) {
+			var diagnostic = Diagnostic.Create (Rule, classDeclarationSyntax.Identifier.GetLocation (), classDeclarationSyntax.Identifier.Text);
+			context.ReportDiagnostic (diagnostic);
+		}
+	}
+}
