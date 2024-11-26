57648696267f92e6c5fd96559e24c8f38489461a
Dean Ellis <dellis1972@googlemail.com>
2024-11-26 11:10:44 +0000

    Add NugetPackageId Metadata to TaskItems

--- src/Xamarin.Android.Build.Tasks/Tasks/GetImportedLibraries.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/GetImportedLibraries.cs
@@ -20,7 +20,7 @@ namespace Xamarin.Android.Tasks
 		};
 
 		[Required]
-		public string TargetDirectory { get; set; }
+		public ITaskItem[] ExtractedDirectories { get; set; }
 
 		public string CacheFile { get; set;} 
 
@@ -35,22 +35,37 @@ namespace Xamarin.Android.Tasks
 
 		public override bool RunTask ()
 		{
-			if (!Directory.Exists (TargetDirectory)) {
-				Log.LogDebugMessage ("Target directory was not found");
-				return true;
-			}
-
 			var manifestDocuments = new List<ITaskItem> ();
 			var nativeLibraries   = new List<ITaskItem> ();
 			var jarFiles          = new List<ITaskItem> ();
-			foreach (var file in Directory.EnumerateFiles (TargetDirectory, "*", SearchOption.AllDirectories)) {
-				if (file.EndsWith (".so", StringComparison.OrdinalIgnoreCase)) {
-					if (AndroidRidAbiHelper.GetNativeLibraryAbi (file) != null)
-						nativeLibraries.Add (new TaskItem (file));
-				} else if (file.EndsWith (".jar", StringComparison.OrdinalIgnoreCase)) {
-					jarFiles.Add (new TaskItem (file));
-				} else if (file.EndsWith (".xml", StringComparison.OrdinalIgnoreCase)) {
-					if (Path.GetFileName (file) == "AndroidManifest.xml") {
+			foreach (var extractedDirectory in ExtractedDirectories) {
+				if (!Directory.Exists (extractedDirectory.ItemSpec)) {
+					continue;
+				}
+				string originalFile = extractedDirectory.GetMetadata (ResolveLibraryProjectImports.OriginalFile);
+				string nuGetPackageId = extractedDirectory.GetMetadata (ResolveLibraryProjectImports.NuGetPackageId);
+				string nuGetPackageVersion = extractedDirectory.GetMetadata (ResolveLibraryProjectImports.NuGetPackageVersion);
+				foreach (var file in Directory.EnumerateFiles (extractedDirectory.ItemSpec, "*", SearchOption.AllDirectories)) {
+					if (file.EndsWith (".so", StringComparison.OrdinalIgnoreCase)) {
+						if (AndroidRidAbiHelper.GetNativeLibraryAbi (file) != null)
+							nativeLibraries.Add (new TaskItem (file, new Dictionary<string, string> {
+								{ ResolveLibraryProjectImports.OriginalFile, originalFile },
+								{ ResolveLibraryProjectImports.NuGetPackageId, nuGetPackageId },
+								{ ResolveLibraryProjectImports.NuGetPackageVersion, nuGetPackageVersion }
+							}));
+						continue;
+					}
+					if (file.EndsWith (".jar", StringComparison.OrdinalIgnoreCase)) {
+						jarFiles.Add (new TaskItem (file, new Dictionary<string, string> {
+								{ ResolveLibraryProjectImports.OriginalFile, originalFile },
+								{ ResolveLibraryProjectImports.NuGetPackageId, nuGetPackageId },
+								{ ResolveLibraryProjectImports.NuGetPackageVersion, nuGetPackageVersion }
+							}));
+						continue;
+					}
+					if (file.EndsWith (".xml", StringComparison.OrdinalIgnoreCase)) {
+						if (Path.GetFileName (file) != "AndroidManifest.xml")
+							continue;
 						// there could be ./AndroidManifest.xml and bin/AndroidManifest.xml, which will be the same. So, ignore "bin" ones.
 						var directory = Path.GetFileName (Path.GetDirectoryName (file));
 						if (IgnoredManifestDirectories.Contains (directory))
@@ -60,7 +75,11 @@ namespace Xamarin.Android.Tasks
 							Log.LogCodedWarning ("XA4315", file, 0, Properties.Resources.XA4315, file);
 							continue;
 						}
-						manifestDocuments.Add (new TaskItem (file));
+						manifestDocuments.Add (new TaskItem (file, new Dictionary<string, string> {
+							{ ResolveLibraryProjectImports.OriginalFile, originalFile },
+							{ ResolveLibraryProjectImports.NuGetPackageId, nuGetPackageId },
+							{ ResolveLibraryProjectImports.NuGetPackageVersion, nuGetPackageVersion }
+						}));
 					}
 				}
 			}
@@ -73,9 +92,9 @@ namespace Xamarin.Android.Tasks
 				var document = new XDocument (
 							new XDeclaration ("1.0", "UTF-8", null),
 							new XElement ("Paths",
-									new XElement ("ManifestDocuments", ManifestDocuments.Select(e => new XElement ("ManifestDocument", e.ItemSpec))),
-									new XElement ("NativeLibraries", NativeLibraries.Select(e => new XElement ("NativeLibrary", e.ItemSpec))),
-									new XElement ("Jars", Jars.Select(e => new XElement ("Jar", e.ItemSpec)))
+									new XElement ("ManifestDocuments", ManifestDocuments.ToXElements ("ManifestDocument", ResolveLibraryProjectImports.KnownMetadata)),
+									new XElement ("NativeLibraries", NativeLibraries.ToXElements ("NativeLibrary", ResolveLibraryProjectImports.KnownMetadata)),
+									new XElement ("Jars", Jars.ToXElements ("Jar", ResolveLibraryProjectImports.KnownMetadata))
 						));
 				document.SaveIfChanged (CacheFile);
 			}
