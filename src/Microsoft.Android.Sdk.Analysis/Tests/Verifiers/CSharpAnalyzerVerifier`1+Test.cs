88e6b60a6886321397b16cfda9af76c9dd725c5e
Dean Ellis <dellis1972@googlemail.com>
2024-11-26 19:04:17 +0000

    [Microsoft.Android.Sdk.Analysis] Add Unit Tests for Code Analyzers (#9492)
    Context: 715a36af86c68bac623a4174477165bb14b4c371
    Context: https://github.com/dotnet/roslyn-sdk/issues/1175
    
    Commit 715a36af added and updated code compilation analyzers and
    suppressors, but was not able to add *unit tests* for them.
    
    Add unit test infrastructure and unit tests for the
    `DNAA0001` and `DNAS0001` messages.
    
    TODO: enable the `DNAS0001Tests.IDE0002IsSuppressed()` test
    [once we figure out how to actually run it][0].
    
    [0]: https://github.com/dotnet/roslyn-sdk/issues/1175#issuecomment-2485739878

---
+++ src/Microsoft.Android.Sdk.Analysis/Tests/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+
+public static partial class CSharpAnalyzerVerifier<TAnalyzer>
+	where TAnalyzer : DiagnosticAnalyzer, new ()
+{
+	public class Test : CSharpAnalyzerTest<TAnalyzer, DefaultVerifier>
+	{
+		List<DiagnosticAnalyzer> analyzers = new List<DiagnosticAnalyzer> ();
+
+		public List<DiagnosticAnalyzer> Analyzers => analyzers;
+		public Test ()
+		{
+			SolutionTransforms.Add ((solution, projectId) => {
+				var project = solution.GetProject (projectId);
+				var compilationOptions = project.CompilationOptions;
+				compilationOptions = compilationOptions.WithSpecificDiagnosticOptions (
+					compilationOptions.SpecificDiagnosticOptions.SetItems (CSharpVerifierHelper.NullableWarnings));
+				solution = solution.WithProjectCompilationOptions (projectId, compilationOptions);
+				return solution;
+			});
+
+			var a = new AnalyzerFileReference (Path.GetFullPath ("Microsoft.CodeAnalysis.CSharp.Features.dll"), assemblyLoader: AssemblyLoader.Instance);
+			foreach (var a1 in a.GetAnalyzers (LanguageNames.CSharp)) {
+				if (a1.SupportedDiagnostics.Any (x => x.Id == "IDE0002"))
+					analyzers.Add (a1);
+			}
+		}
+
+		protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers ()
+		{
+			return Analyzers;
+		}
+	}
+}
