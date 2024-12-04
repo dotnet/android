308733179124a8095c72f3dcb55aa238cc32b36c
Marek Habersack <grendel@twistedcode.net>
2024-12-04 23:10:03 +0100

    Beginnings of initialization code

--- src/native/mono/shared/cpp-util.hh
+++ src/native/mono/shared/cpp-util.hh
@@ -129,7 +129,7 @@ abort_if_negative_integer_argument (int arg, const char *arg_name, std::source_l
 		arg > 0,
 		[&arg_name, &sloc] {
 			return xamarin::android::detail::_format_message (
-				"%s: parameter '%s' must be a valid pointer",
+				"%s: parameter '%s' must be a positive integer",
 				xamarin::android::detail::get_function_name (sloc.function_name ()).c_str (),
 				arg_name
 			);
