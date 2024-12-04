6a9491c8e79b87833005ba75642c02d78a8af067
Marek Habersack <grendel@twistedcode.net>
2024-12-04 19:46:46 +0100

    Moving on: logging + timing

--- src/native-clr/host/host.cc
+++ src/native-clr/host/host.cc
@@ -1,9 +1,27 @@
 #include <host/host.hh>
 #include <host/host-jni.hh>
 #include <runtime-base/android-system.hh>
+#include <runtime-base/logger.hh>
+#include <runtime-base/timing-internal.hh>
 #include <shared/log_types.hh>
 
 using namespace xamarin::android;
+
+void Host::Java_mono_android_Runtime_initInternal (JNIEnv *env, jclass klass, jstring lang, jobjectArray runtimeApksJava,
+	jstring runtimeNativeLibDir, jobjectArray appDirs, jint localDateTimeOffset, jobject loader,
+	jobjectArray assembliesJava, jboolean isEmulator, jboolean haveSplitApks)
+{
+	Logger::init_logging_categories ();
+
+	// If fast logging is disabled, log messages immediately
+	FastTiming::initialize ((Logger::log_timing_categories() & LogTimingCategories::FastBare) != LogTimingCategories::FastBare);
+
+	size_t total_time_index;
+    if (FastTiming::enabled ()) [[unlikely]] {
+        _timing = std::make_unique<Timing> ();
+        total_time_index = internal_timing->start_event (TimingEventKind::TotalRuntimeInit);
+    }
+}
 
 auto Host::Java_JNI_OnLoad (JavaVM *vm, [[maybe_unused]] void *reserved) noexcept -> jint
 {
