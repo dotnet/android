d86e512f027bf369811d7f78f5b1956e06f51698
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-12-12 16:37:06 -0600

    Create Runtime.cs

---
+++ samples/NativeAOT/Runtime.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Java.Interop.Samples.NativeAotFromAndroid;
+
+class Runtime
+{
+    static MethodInfo? activate;
+
+    [UnmanagedCallersOnly (EntryPoint="Java_mono_android_Runtime_register")]
+    public static void Register (IntPtr jnienv, IntPtr managedType, IntPtr nativeClass, IntPtr methods)
+    {
+        AndroidLog.Print (AndroidLogLevel.Info, "Runtime", $"Runtime.register() called");
+
+        //TODO: might need to call something
+    }
+
+    [UnmanagedCallersOnly (EntryPoint="Java_mono_android_TypeManager_n_1activate")]
+    static void Activate (IntPtr jnienv, IntPtr jclass, IntPtr typename_ptr, IntPtr signature_ptr, IntPtr jobject, IntPtr parameters_ptr)
+    {
+        AndroidLog.Print (AndroidLogLevel.Info, "Runtime", $"Runtime.Activate() called");
+
+        try
+        {
+            activate ??= typeof(TypeManager).GetMethod ("Activate");
+            ArgumentNullException.ThrowIfNull (activate);
+            activate.Invoke (null, [ jnienv, jclass, typename_ptr, signature_ptr, jobject, parameters_ptr ]);
+        }
+        catch (Exception exc)
+        {
+            AndroidLog.Print (AndroidLogLevel.Error, "Runtime", $"Runtime.Activate() failed: {exc}");
+        }
+    }
+}
