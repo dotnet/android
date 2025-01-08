3d2ae4bc4518639afd7bf8aeffc6d3400697110c
Peter Collins <pecolli@microsoft.com>
2025-01-08 13:10:42 -0500

    [Mono.Android] Generate API docs for API level 35 (#9647)
    Changes: https://github.com/dotnet/java-interop/compare/2c06b3c2a11833aea0e9b51aac2a72195bd64539...fe00cef5290a7e75b8e73e3044966a14e6c312c9
    
      * dotnet/java-interop@fe00cef5: [Java.Interop.Tools.JavaSource] Support html tags with attributes (dotnet/java-interop#1286)
    
    Bump to dotnet/java-interop@fe00cef5 to get Javadoc import fixes.
    
    Updates xaprepare and Mono.Android to generate API Docs against
    API-35 sources.
    
    The `azure-pipelines-apidocs.yaml` pipeline has been updated to use
    the 1ES pipeline template to improve compatibility with existing
    yaml templates.
    
    The semi-automated API docs build and update workflow uses the
    .NET Framework version of Mdoc and the `RunMdoc` target has been
    updated to continue to mirror that workflow.

--- build-tools/xaprepare/xaprepare/ConfigAndData/Dependencies/AndroidToolchain.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Dependencies/AndroidToolchain.cs
@@ -75,11 +75,11 @@ namespace Xamarin.Android.Prepare
 				new AndroidPlatformComponent ("platform-35_r01",   apiLevel: "35", pkgRevision: "1", isLatestStable: true),
 				new AndroidPlatformComponent ("platform-Baklava_r03",   apiLevel: "Baklava", pkgRevision: "3", isLatestStable: true),
 
-				new AndroidToolchainComponent ("sources-34_r01",
-					destDir: Path.Combine ("sources", "android-34"),
+				new AndroidToolchainComponent ("source-35_r01",
+					destDir: Path.Combine ("sources", "android-35"),
 					pkgRevision: "1",
 					dependencyType: AndroidToolchainComponentType.BuildDependency,
-					buildToolVersion: "34.1"
+					buildToolVersion: "35.1"
 				),
 				new AndroidToolchainComponent ("docs-24_r01",
 					destDir: "docs",
