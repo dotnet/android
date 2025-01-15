c68c2ea335ed56d8b0d6c3e1e0a6ffd5149b5305
Marek Habersack <grendel@twistedcode.net>
2025-01-15 14:19:41 +0100

    Don't package runtime stubs

--- src/Xamarin.Android.Build.Tasks/Tasks/RecreateResolvedRuntimePacks.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/RecreateResolvedRuntimePacks.cs
@@ -19,18 +19,51 @@ public class RecreateResolvedRuntimePacks : AndroidTask
 		"libnet-android.release.so",
 	];
 
+	static readonly string[] LibraryNamesToIgnore = [
+		"libarchive-dso-stub.so",
+		"libc.so",
+		"libdl.so",
+		"liblog.so",
+		"libm.so",
+		"libz.so",
+	];
+
 	[Required]
 	public ITaskItem[] ResolvedNativeLibraries { get; set; }
 
 	[Output]
 	public ITaskItem[] ResolvedRuntimePacks { get; set; }
 
+	// Contains items for libraries which are to be removed from @(_ResolvedNativeLibraries), so that
+	// they aren't copied to the AAB/APK.  There's a number of such libraries in our runtime packs, they
+	// are used only at build time and mustn't be included in the application archives.
+	[Output]
+	public ITaskItem[] SharedLibrariesToIgnore { get; set; }
+
 	public override bool RunTask ()
 	{
+		var ignoreLibNames = new List<string> ();
+		foreach (string libName in LibraryNamesToIgnore) {
+			ignoreLibNames.Add (String.Format ("{0}native{0}{1}", Path.DirectorySeparatorChar, libName));
+		}
+
 		// We need to find `libc.so` that comes from one of our runtime packs
 		var libcPath = String.Format ("{0}native{0}libc.so", Path.DirectorySeparatorChar);
 		var runtimePacks = new Dictionary <string, ITaskItem> (StringComparer.OrdinalIgnoreCase);
+		var maybeIgnoreLibs = new Dictionary<string, ITaskItem> (StringComparer.OrdinalIgnoreCase);
+		var runtimePackPaths = new List<string> ();
+
 		foreach (ITaskItem library in ResolvedNativeLibraries) {
+			foreach (string libPathTail in ignoreLibNames) {
+				if (!library.ItemSpec.EndsWith (libPathTail, StringComparison.OrdinalIgnoreCase)) {
+					continue;
+				}
+
+				string libraryRid = library.GetMetadata ("RuntimeIdentifier") ?? String.Empty;
+				maybeIgnoreLibs[$"{libraryRid}{libPathTail}"] = library;
+				break;
+			}
+
 			if (!library.ItemSpec.EndsWith (libcPath, StringComparison.OrdinalIgnoreCase)) {
 				continue;
 			}
@@ -85,9 +118,27 @@ public class RecreateResolvedRuntimePacks : AndroidTask
 			pack.SetMetadata ("RuntimeIdentifier", rid);
 			pack.SetMetadata ("PackageDirectory", packageDir);
 
+			runtimePackPaths.Add (packageDir);
 			runtimePacks.Add (rid, pack);
 		}
 		ResolvedRuntimePacks = runtimePacks.Values.ToArray ();
+
+		var librariesToIgnore = new List<ITaskItem> ();
+		foreach (string path in runtimePackPaths) {
+			string runtimePackPath = path;
+			if (path[path.Length - 1] != Path.DirectorySeparatorChar) {
+				runtimePackPath = $"{path}{Path.DirectorySeparatorChar}";
+			}
+
+			Log.LogDebugMessage ($"Runtime pack path: {runtimePackPath}");
+			foreach (ITaskItem library in maybeIgnoreLibs.Values) {
+				if (library.ItemSpec.StartsWith (runtimePackPath)) {
+					librariesToIgnore.Add (library);
+					Log.LogDebugMessage ($"  ignore library: {library}");
+				}
+			}
+		};
+		SharedLibrariesToIgnore = librariesToIgnore.ToArray ();
 
 		return !Log.HasLoggedErrors;
 
