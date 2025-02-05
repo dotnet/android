76a5c59e0289e8748d6925e4ccec3075365d4306
Marek Habersack <grendel@twistedcode.net>
2025-02-05 17:37:56 +0100

    Notes + logging

--- src/native/clr/host/pinvoke-override.cc
+++ src/native/clr/host/pinvoke-override.cc
@@ -88,5 +88,8 @@ auto PinvokeOverride::monodroid_pinvoke_override (const char *library_name, cons
 
 const void* Host::clr_pinvoke_override (const char *library_name, const char *entry_point_name) noexcept
 {
-	return PinvokeOverride::monodroid_pinvoke_override (library_name, entry_point_name);
+	log_info (LOG_ASSEMBLY, "clr_pinvoke_override (\"{}\", \"{}\")", library_name, entry_point_name);
+	void *ret = PinvokeOverride::monodroid_pinvoke_override (library_name, entry_point_name);
+	log_debug (LOG_DEFAULT, "p/invoke {}found", ret == nullptr ? "not"sv : ""sv);
+	return ret;
 }
