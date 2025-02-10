b1a98e1ee6f1abdc48639e535c4e67461901c85e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-02-10 12:48:44 -0600

    Update MonoPackageManager.java

--- src/java-runtime/java/mono/android/MonoPackageManager.java
+++ src/java-runtime/java/mono/android/MonoPackageManager.java
@@ -24,6 +24,8 @@ public class MonoPackageManager {
 	static Object lock = new Object ();
 	static boolean initialized;
 
+	static android.content.Context Context;
+
 	public static void LoadApplication (Context context)
 	{
 		synchronized (lock) {
@@ -38,6 +40,9 @@ public class MonoPackageManager {
 				apks = new String[] { runtimePackage.sourceDir };
 			}
 
+			if (context instanceof android.app.Application) {
+				Context = context;
+			}
 			if (!initialized) {
 				android.content.IntentFilter timezoneChangedFilter  = new android.content.IntentFilter (
 						android.content.Intent.ACTION_TIMEZONE_CHANGED
@@ -122,7 +127,7 @@ public class MonoPackageManager {
 						MonoPackageManager_Resources.Assemblies,
 						isEmulator (),
 						haveSplitApks,
-						context
+						Context
 					);
 
 				net.dot.android.ApplicationRegistration.registerApplications ();
