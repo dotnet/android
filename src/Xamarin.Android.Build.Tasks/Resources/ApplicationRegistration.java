21c413195e300b6440eb437dade4f3a114e795f7
Jonathan Peppers <jonathan.peppers@microsoft.com>
2025-02-11 13:10:34 -0600

    [java-runtime, Mono.Android] Use `ApplicationRegistration.Context` (#9779)
    Context: https://github.com/dotnet/android/pull/9747
    
    Calling `Android.App.Application.Context` currently reads the
    `mono.MonoPackageManager.Context` field, which is set on startup by a
    `ContentProvider`.  `mono.MonoPackageManager` does not exist in
    NativeAOT (#9747), so let's move the field to the
    `ApplicationRegistration` type, which *does* exist.

--- src/Xamarin.Android.Build.Tasks/Resources/ApplicationRegistration.java
+++ src/Xamarin.Android.Build.Tasks/Resources/ApplicationRegistration.java
@@ -2,6 +2,8 @@
 
 public class ApplicationRegistration {
 
+	public static android.content.Context Context;
+
 	public static void registerApplications ()
 	{
 		// REGISTER_APPLICATION_AND_INSTRUMENTATION_CLASSES_HERE		
