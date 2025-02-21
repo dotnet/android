e9ac79d7eefbc0d990c86d23df1a1a4b2dc59fb2
Dean Ellis <dellis1972@googlemail.com>
2025-02-21 16:46:34 +0000

    Formatting

--- src/Xamarin.Android.Build.Tasks/Utilities/JsonExtensions.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/JsonExtensions.cs
@@ -5,54 +5,46 @@ using System.Text.Json.Nodes;
 
 public static class JsonExtensions
 {
-    public static JsonNode Merge(this JsonNode jsonBase, JsonNode jsonMerge)
+    public static JsonNode Merge (this JsonNode jsonBase, JsonNode jsonMerge)
     {
         if (jsonBase == null || jsonMerge == null)
             return jsonBase;
 
         switch (jsonBase)
         {
-            case JsonObject jsonBaseObj when jsonMerge is JsonObject jsonMergeObj:
-                {
-                    var mergeNodesArray = new KeyValuePair<string, JsonNode?>[jsonMergeObj.Count];
-                    int index = 0;
-                    foreach (var prop in jsonMergeObj)
-                    {
-                        mergeNodesArray[index++] = prop;
-                    }
-                    jsonMergeObj.Clear();
+            case JsonObject jsonBaseObj when jsonMerge is JsonObject jsonMergeObj: {
+                var mergeNodesArray = new KeyValuePair<string, JsonNode?> [jsonMergeObj.Count];
+                int index = 0;
+                foreach (var prop in jsonMergeObj) {
+                    mergeNodesArray [index++] = prop;
+                }
+                jsonMergeObj.Clear ();
 
-                    foreach (var prop in mergeNodesArray)
-                    {
-                        jsonBaseObj[prop.Key] = jsonBaseObj[prop.Key] switch
-                        {
-                            JsonObject jsonBaseChildObj when prop.Value is JsonObject jsonMergeChildObj => jsonBaseChildObj.Merge(jsonMergeChildObj),
-                            JsonArray jsonBaseChildArray when prop.Value is JsonArray jsonMergeChildArray => jsonBaseChildArray.Merge(jsonMergeChildArray),
-                            _ => prop.Value
-                        };
-                    }
-                    break;
+                foreach (var prop in mergeNodesArray) {
+                    jsonBaseObj [prop.Key] = jsonBaseObj [prop.Key] switch {
+                        JsonObject jsonBaseChildObj when prop.Value is JsonObject jsonMergeChildObj => jsonBaseChildObj.Merge (jsonMergeChildObj),
+                        JsonArray jsonBaseChildArray when prop.Value is JsonArray jsonMergeChildArray => jsonBaseChildArray.Merge (jsonMergeChildArray),
+                        _ => prop.Value
+                    };
                 }
-            case JsonArray jsonBaseArray when jsonMerge is JsonArray jsonMergeArray:
-                {
-                    var mergeNodesArray = new JsonNode?[jsonMergeArray.Count];
-                    int index = 0;
-                    foreach (var mergeNode in jsonMergeArray)
-                    {
-                        mergeNodesArray[index++] = mergeNode;
-                    }
-                    jsonMergeArray.Clear();
-                    foreach (var mergeNode in mergeNodesArray)
-                    {
-                        jsonBaseArray.Add(mergeNode);
-                    }
-                    break;
+                break;
+            }
+            case JsonArray jsonBaseArray when jsonMerge is JsonArray jsonMergeArray: {
+                var mergeNodesArray = new JsonNode? [jsonMergeArray.Count];
+                int index = 0;
+                foreach (var mergeNode in jsonMergeArray) {
+                    mergeNodesArray [index++] = mergeNode;
                 }
+                jsonMergeArray.Clear ();
+                foreach (var mergeNode in mergeNodesArray) {
+                    jsonBaseArray.Add (mergeNode);
+                }
+                break;
+            }
             default:
-                throw new ArgumentException($"The JsonNode type [{jsonBase.GetType().Name}] is incompatible for merging with the target/base " +
-                                            $"type [{jsonMerge.GetType().Name}]; merge requires the types to be the same.");
+                throw new ArgumentException ($"The JsonNode type [{jsonBase.GetType ().Name}] is incompatible for merging with the target/base " +
+                    $"type [{jsonMerge.GetType ().Name}]; merge requires the types to be the same.");
         }
-
         return jsonBase;
     }
 }
