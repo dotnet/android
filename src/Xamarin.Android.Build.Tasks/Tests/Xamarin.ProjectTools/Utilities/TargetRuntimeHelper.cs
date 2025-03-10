bf5f52ed5e1879e6d2388a57c424a20549229dcb
Marek Habersack <grendel@twistedcode.net>
2025-03-10 13:08:40 +0100

    CoreCLR doesn't support all RIDs

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Utilities/TargetRuntimeHelper.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Utilities/TargetRuntimeHelper.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 
+using Xamarin.Android.Tasks;
+
 namespace Xamarin.ProjectTools;
 
 public class TargetRuntimeHelper
@@ -19,9 +21,30 @@ public class TargetRuntimeHelper
 		}
 	}
 
+	/// <summary>
+	/// This must be changed when we're ready to release. It is used to make tests which require a certain amount of warnings to
+	/// work when the XA1040 warning is issued (see Xamarin.Android.Common.targets, the `_CheckNonIdealAppConfigurations` target)
+	/// </summary>
+	public static bool CoreClrIsExperimental => true;
 	public static bool UseMonoRuntime => useMonoRuntime;
 	public static bool UseCoreCLR => !useMonoRuntime;
 	public static string[] CoreClrSupportedAbis => coreClrSupportedAbis;
 
 	public static bool CoreClrSupportsAbi (string abiName) => coreClrAbis.Contains (abiName);
+
+	/// <summary>
+	/// <param ref="runtimeIdentifiers" /> contains a list of semicolon-separated RIDs (a single RID without
+	/// semicolons is also fine) which will be checked against the list of RIDs supported by CoreCLR. If even
+	/// a single RID isn't supported, `false` is returned.
+	/// </summary>
+	public static bool CoreClrSupportsAllRIDs (string runtimeIdentifiers)
+	{
+		foreach (string rid in runtimeIdentifiers.Split (';', StringSplitOptions.RemoveEmptyEntries)) {
+			if (!CoreClrSupportsAbi (MonoAndroidHelper.RidToAbi (rid))) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
