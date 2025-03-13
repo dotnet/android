f54864a1d495f5ebfab480b88664ade122efbcd1
Marek Habersack <grendel@twistedcode.net>
2025-03-13 12:04:07 +0100

    A few more steps forward

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Utilities/AbiUtils.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Utilities/AbiUtils.cs
@@ -1,21 +1,14 @@
 using System;
 
+using Xamarin.Android.Tasks;
+
 namespace Xamarin.ProjectTools
 {
 	public static class AbiUtils
 	{
 		public static string AbiToRuntimeIdentifier (string androidAbi)
 		{
-			if (androidAbi == "armeabi-v7a") {
-				return "android-arm";
-			} else if (androidAbi == "arm64-v8a") {
-				return "android-arm64";
-			} else if (androidAbi == "x86") {
-				return "android-x86";
-			} else if (androidAbi == "x86_64") {
-				return "android-x64";
-			}
-			throw new InvalidOperationException ($"Unknown abi: {androidAbi}");
+			return MonoAndroidHelper.AbiToRidMaybe (androidAbi);
 		}
 	}
 }
