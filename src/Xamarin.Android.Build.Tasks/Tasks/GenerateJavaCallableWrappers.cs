ea399edb872588c16b124c4b9ab2232243b93016
Jonathan Pobst <jonathan.pobst@microsoft.com>
2025-03-18 08:28:17 -1000

    ﻿[XABT] Scan for JLO needed wrappers in `LinkAssembliesNoShrink`. (#9893)
    Today, application builds use Cecil to scan through all Android assemblies at least twice:
    
    - Debug: `_LinkAssembliesNoShrink`, `_GenerateJavaStubs`
    - Release: `_ILLink`, `_GenerateJavaStubs`, `_RemoveRegisterAttribute`
    
    This is a costly operation that we would like to only perform once to help speed up application builds.  The long-term goal is to move the (many) steps currently performed by `_GenerateJavaStubs` into "linker" steps.
    
    This PR moves the Cecil scanning required for the first step: "generating Java Callable Wrappers".
    
    ## Implementation
    
    This does not move generating JCW Java code to the linker, it only moves scanning for the JLO information needed to generate JCWs to the linker.  This information is persisted as an XML file in `/obj` next to the processed assembly.
    
    Example:
    
    - `obj/Debug/net10.0-android/android/assets/MyApplication.dll`
    - `obj/Debug/net10.0-android/android/assets/MyApplication.jlo.xml`
    
    Doing it this way has two benefits: it helps keep the linker from getting too complex (eg: generating Java code) and it helps incremental builds.  If an assembly has not changed since the last build, the JLO information in the `.jlo.xml` is still valid and will be reused.  The assembly does not need to be re-scanned.  (For example, the ~70 AndroidX assemblies that the MAUI template uses do not need to be re-scanned on every Debug build.)
    
    The process of actually generating the JCWs is done by a new `GenerateJavaCallableWrappers` task in the `_GenerateJavaStubs` target that consumes the `.jlo.xml` files and outputs the `.java` files.
    
    ## Release Builds
    
    In an ideal Release build, we would (probably?) run this new step as part of the `ILLink` invocation.  However there are some tricky limitations to running in the linker pipeline, chiefly being that the linker cannot load external assemblies like `Java.Interop.Tool.JavaCallableWrappers.dll` (and dependencies) that we need.  This can be worked around by including the needed source files in the linker assembly, but this will require additional work.
    
    Instead, for now, we will add an invocation a new `_LinkAssembliesAdditionalSteps` target to linked builds that runs after `ILLink`. This calls the existing `LinkAssembliesNoShrink` task to scan for JLOs but does not execute the other linker tasks that have already been run. This allows us to move forward with converting to "linker steps" for making Debug builds faster without the extra work required to move these steps into `ILLink`.  A future enhancement can perform this work if desired.  (There is also [some question](https://github.com/dotnet/runtime/issues/107211) as to whether we actually want to create new `ILLink` linker steps.)
    
    Note this temporarily leaves the `$(_AndroidJLOCheckedBuild)` flag that compares the generated Java files to the previous ones.  This will likely be useful ensuring future moved steps are correct as well.

---
+++ src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaCallableWrappers.cs
@@ -0,0 +1,105 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Java.Interop.Tools.JavaCallableWrappers;
+using Java.Interop.Tools.JavaCallableWrappers.Adapters;
+using Java.Interop.Tools.JavaCallableWrappers.CallableWrapperMembers;
+using Java.Interop.Tools.TypeNameMappings;
+using Microsoft.Android.Build.Tasks;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using PackageNamingPolicyEnum = Java.Interop.Tools.TypeNameMappings.PackageNamingPolicy;
+
+namespace Xamarin.Android.Tasks;
+
+/// <summary>
+/// Transforms the XML files of JLOs found in the linker step into JavaCallableWrappers.
+/// </summary>
+public class GenerateJavaCallableWrappers : AndroidTask
+{
+	public override string TaskPrefix => "JCW";
+
+	[Required]
+	public string CodeGenerationTarget { get; set; } = string.Empty;
+
+	[Required]
+	public string OutputDirectory { get; set; } = string.Empty;
+
+	[Required]
+	public string PackageNamingPolicy { get; set; } = string.Empty;
+
+	[Required]
+	public ITaskItem [] ResolvedAssemblies { get; set; } = [];
+
+	[Required]
+	public string [] SupportedAbis { get; set; } = [];
+
+	[Output]
+	public ITaskItem [] GeneratedJavaFilesOutput { get; set; } = [];
+
+	public override bool RunTask ()
+	{
+		JavaNativeTypeManager.PackageNamingPolicy = Enum.TryParse (PackageNamingPolicy, out PackageNamingPolicyEnum pnp) ? pnp : PackageNamingPolicyEnum.LowercaseCrc64;
+
+		// Get the set of assemblies for the "first" ABI. JavaCallableWrappers are
+		// not ABI-specific, so we can use any ABI to generate the wrappers.
+		var allAssembliesPerArch = MonoAndroidHelper.GetPerArchAssemblies (ResolvedAssemblies, SupportedAbis, validate: true);
+		var singleArchAssemblies = allAssembliesPerArch.First ().Value.Values.ToList ();
+
+		GenerateWrappers (singleArchAssemblies);
+
+		return !Log.HasLoggedErrors;
+	}
+
+	void GenerateWrappers (List<ITaskItem> assemblies)
+	{
+		Directory.CreateDirectory (OutputDirectory);
+
+		// Deserialize JavaCallableWrappers XML files
+		var wrappers = new List<CallableWrapperType> ();
+		var sw = Stopwatch.StartNew ();
+
+		foreach (var assembly in assemblies) {
+			var assemblyPath = assembly.ItemSpec;
+			var assemblyName = Path.GetFileNameWithoutExtension (assemblyPath);
+			var wrappersPath = Path.Combine (Path.GetDirectoryName (assemblyPath), $"{assemblyName}.jlo.xml");
+
+			if (!File.Exists (wrappersPath)) {
+				Log.LogError ($"'{wrappersPath}' not found.");
+				return;
+			}
+
+			wrappers.AddRange (XmlImporter.Import (wrappersPath, out var _));
+		}
+
+		Log.LogDebugMessage ($"Deserialized {wrappers.Count} Java callable wrappers in {sw.ElapsedMilliseconds}ms");
+		sw.Restart ();
+
+		// Write JavaCallableWrappers to Java files
+		var writer_options = new CallableWrapperWriterOptions {
+			CodeGenerationTarget = MonoAndroidHelper.ParseCodeGenerationTarget (CodeGenerationTarget)
+		};
+
+		var generated_files = new List<ITaskItem> ();
+
+		foreach (var generator in wrappers) {
+			using var writer = MemoryStreamPool.Shared.CreateStreamWriter ();
+
+			generator.Generate (writer, writer_options);
+			writer.Flush ();
+
+			var path = generator.GetDestinationPath (OutputDirectory);
+
+			var changed = Files.CopyIfStreamChanged (writer.BaseStream, path);
+			Log.LogDebugMessage ($"*NEW* Generated Java callable wrapper code: '{path}' (changed: {changed})");
+
+			generated_files.Add (new TaskItem (path));
+		}
+
+		Log.LogDebugMessage ($"Generated {generated_files.Count} Java callable wrapper files in {sw.ElapsedMilliseconds}ms");
+		GeneratedJavaFilesOutput = generated_files.ToArray ();
+	}
+}
--- src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaStubs.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/GenerateJavaStubs.cs
@@ -1,22 +1,21 @@
 // Copyright (C) 2011 Xamarin, Inc. All rights reserved.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Microsoft.Build.Framework;
-using Mono.Cecil;
-using Microsoft.Build.Utilities;
-
+using System.Threading.Tasks;
 using Java.Interop.Tools.Cecil;
 using Java.Interop.Tools.Diagnostics;
 using Java.Interop.Tools.JavaCallableWrappers;
+using Java.Interop.Tools.JavaCallableWrappers.Adapters;
 using Java.Interop.Tools.TypeNameMappings;
-
-using Xamarin.Android.Tools;
 using Microsoft.Android.Build.Tasks;
-using System.Threading.Tasks;
-using System.Collections.Concurrent;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.Cecil;
+using Xamarin.Android.Tools;
 
 namespace Xamarin.Android.Tasks
 {
@@ -56,7 +55,16 @@ namespace Xamarin.Android.Tasks
 
 		public string CodeGenerationTarget { get; set; } = "";
 
+		// These two properties are temporary and are used to ensure we still generate the
+		// same files as before using the new _LinkAssembliesNoShrink JLO scanning. They will be removed in the future.
+		public bool RunCheckedBuild { get; set; }
+
+		public ITaskItem [] GeneratedJavaFiles { get; set; } = [];
+
 		JavaPeerStyle codeGenerationTarget;
+
+		//[Output]
+		//public ITaskItem [] GeneratedJavaFilesOutput { get; set; }
 
 		internal const string AndroidSkipJavaStubGeneration = "AndroidSkipJavaStubGeneration";
 
@@ -181,6 +189,9 @@ namespace Xamarin.Android.Tasks
 			}
 			JCWGenerator.EnsureAllArchitecturesAreIdentical (Log, nativeCodeGenStates);
 
+			if (RunCheckedBuild)
+				CompareScannedAssemblies ();
+
 			// Save NativeCodeGenState for later tasks
 			Log.LogDebugMessage ($"Saving {nameof (NativeCodeGenState)} to {nameof (NativeCodeGenStateRegisterTaskKey)}");
 			BuildEngine4.RegisterTaskObjectAssemblyLocal (MonoAndroidHelper.GetProjectBuildSpecificTaskObjectKey (NativeCodeGenStateRegisterTaskKey, WorkingDirectory, IntermediateOutputDirectory), nativeCodeGenStates, RegisteredTaskObjectLifetime.Build);
@@ -206,8 +217,10 @@ namespace Xamarin.Android.Tasks
 			};
 			bool success;
 
-			if (generateJavaCode) {
+			if (generateJavaCode && RunCheckedBuild) {
 				success = jcwGenerator.GenerateAndClassify (AndroidSdkPlatform, outputPath: Path.Combine (OutputDirectory, "src"), ApplicationJavaClass);
+
+				generatedJavaFiles = jcwGenerator.GeneratedJavaFiles;
 			} else {
 				success = jcwGenerator.Classify (AndroidSdkPlatform);
 			}
@@ -224,13 +237,24 @@ namespace Xamarin.Android.Tasks
 			var scanner = new XAJavaTypeScanner (res.TargetArch, Log, cache) {
 				ErrorOnCustomJavaObject     = ErrorOnCustomJavaObject,
 			};
-			List<TypeDefinition> allJavaTypes = scanner.GetJavaTypes (assemblies.Values, res);
+			List<TypeDefinition> allJavaTypes = scanner.GetJavaTypes (assemblies.Values, res, scannedAssemblies);
+
 			var javaTypesForJCW = new List<TypeDefinition> ();
 
 			// When marshal methods or non-JavaPeerStyle.XAJavaInterop1 are in use we do not want to skip non-user assemblies (such as Mono.Android) - we need to generate JCWs for them during
 			// application build, unlike in Debug configuration or when marshal methods are disabled, in which case we use JCWs generated during Xamarin.Android
 			// build and stored in a jar file.
 			bool shouldSkipNonUserAssemblies = !useMarshalMethods && codeGenerationTarget == JavaPeerStyle.XAJavaInterop1;
+			Log.LogDebugMessage ($"Should skip non-user assemblies: {shouldSkipNonUserAssemblies} (useMarshalMethods: {useMarshalMethods})");
+
+			if (shouldSkipNonUserAssemblies) {
+				foreach (var item in scannedAssemblies.Values.ToArray ())
+					if (!ResolvedUserAssemblies.Any (a => a.ItemSpec == item.ItemSpec)) {
+						var rm = scannedAssemblies.TryRemove (item.ItemSpec, out var _);
+						Log.LogDebugMessage ($"Removing assembly '{item.ItemSpec}' from scanned assemblies ({rm})");
+					}
+			}
+
 			foreach (TypeDefinition type in allJavaTypes) {
 				if ((shouldSkipNonUserAssemblies && !userAssemblies.ContainsKey (type.Module.Assembly.Name.Name)) || JavaTypeScanner.ShouldSkipJavaCallableWrapperGeneration (type, cache)) {
 					continue;
@@ -239,6 +263,79 @@ namespace Xamarin.Android.Tasks
 			}
 
 			return (allJavaTypes, javaTypesForJCW);
+		}
+
+		ConcurrentDictionary<string, ITaskItem> scannedAssemblies = new ();
+		List<string> generatedJavaFiles = new ();
+
+		void CompareScannedAssemblies ()
+		{
+			// First we want to ensure that we scanned the same set of assemblies, as it can
+			// be tricky to ensure we found all the assemblies we need to scan
+			var linker_scanned_assemblies = new List<string> ();
+
+			// Find every assembly that was scanned by the linker by looking at the .jlo.xml files
+			foreach (var assembly in ResolvedAssemblies) {
+				var assemblyPath = assembly.ItemSpec;
+				var assemblyName = Path.GetFileNameWithoutExtension (assemblyPath);
+				var wrappersPath = Path.Combine (Path.GetDirectoryName (assemblyPath), $"{assemblyName}.jlo.xml");
+
+				if (!File.Exists (wrappersPath))
+					Log.LogError ($"'{wrappersPath}' not found.");
+
+				XmlImporter.Import (wrappersPath, out var wasScanned);
+
+				if (wasScanned) {
+					Log.LogDebugMessage ($"CompareScannedAssemblies: Found scanned assembly .jlo.xml '{assemblyPath}'");
+					linker_scanned_assemblies.Add (assembly.ItemSpec);
+				}
+			}
+
+			// These are the assemblies that were scanned by this task (GenerateJavaStubs)
+			var legacy_scanned_assemblies = scannedAssemblies.Values.Select (a => a.ItemSpec).ToList ();
+
+			CompareLists ("Scanned assemblies", legacy_scanned_assemblies, linker_scanned_assemblies);
+
+			// Next we want to ensure that we generated the same .java files
+			var linker_generated_files = GeneratedJavaFiles.Select (a => a.ItemSpec).ToList ();
+			var legacy_generated_files = generatedJavaFiles;
+
+			CompareLists ("Generated Java files", legacy_generated_files, linker_generated_files);
+		}
+
+		void CompareLists (string name, List<string> list1, List<string> list2)
+		{
+			var had_differences = false;
+
+			Log.LogDebugMessage ($"Comparing {name} lists");
+
+			if (list1.Count != list2.Count) {
+				had_differences = true;
+				Log.LogError ($"Number of assemblies scanned for Java types changed from {list1.Count} to {list2.Count}");
+			}
+
+			var missingAssemblies = list1.Except (list2).ToList ();
+
+			if (missingAssemblies.Count > 0) {
+				had_differences = true;
+				Log.LogDebugMessage ($"The following assemblies are missing from the new set:");
+
+				foreach (var assembly in missingAssemblies)
+					Log.LogDebugMessage ($"  {assembly}");
+			}
+
+			var extraAssemblies = list2.Except (list1).ToList ();
+
+			if (extraAssemblies.Count > 0) {
+				had_differences = true;
+				Log.LogDebugMessage ($"The following assemblies were found in the new set but not in the old set:");
+
+				foreach (var assembly in extraAssemblies)
+					Log.LogDebugMessage ($"  {assembly}");
+			}
+
+			if (!had_differences)
+				Log.LogDebugMessage ($"No differences");
 		}
 	}
 }
