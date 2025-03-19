7a7ee0db5f05266b3afb59c1fdfa3e8d6190ed92
Marek Habersack <grendel@twistedcode.net>
2025-03-19 20:03:29 +0100

    Build's broken atm, tbc tomorrow

--- src/Xamarin.Android.Build.Tasks/Utilities/KnownMetadata.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/KnownMetadata.cs
@@ -3,9 +3,7 @@ namespace Xamarin.Android.Tasks;
 static class KnownMetadata
 {
 	public const string Abi = "Abi";
-	public const string NativeDontExportSymbols = "DontExportSymbols";
-	public const string NativeLinkItemSet = "NativeLinkItemSet";
 	public const string NativeLinkWholeArchive = "LinkWholeArchive";
-	public const string NativeSharedLibrary = "NativeSharedLibrary";
 	public const string RuntimeIdentifier = "RuntimeIdentifier";
+	public const string NativeDontExportSymbols = "DontExportSymbols";
 }
