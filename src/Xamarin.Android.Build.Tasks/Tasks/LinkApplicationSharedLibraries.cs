d5a011095f0211b2e62d945f7b4aae2f64a4dcad
Marek Habersack <grendel@twistedcode.net>
2025-03-19 20:03:29 +0100

    Build's broken atm, tbc tomorrow

--- src/Xamarin.Android.Build.Tasks/Tasks/LinkApplicationSharedLibraries.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/LinkApplicationSharedLibraries.cs
@@ -184,11 +184,11 @@ namespace Xamarin.Android.Tasks
 				targetLinkerArgs.Add (elf_arch);
 
 				foreach (string file in inputs.ObjectFiles) {
-					targetLinkerArgs.Add (QuoteFileName (file));
+					targetLinkerArgs.Add (MonoAndroidHelper.QuoteFileNameArgument (file));
 				}
 
 				targetLinkerArgs.Add ("-o");
-				targetLinkerArgs.Add (QuoteFileName (inputs.OutputSharedLibrary));
+				targetLinkerArgs.Add (MonoAndroidHelper.QuoteFileNameArgument (inputs.OutputSharedLibrary));
 
 				if (inputs.ExtraLibraries != null) {
 					foreach (string lib in inputs.ExtraLibraries) {
@@ -253,13 +253,6 @@ namespace Xamarin.Android.Tasks
 		{
 			if (e.Data != null)
 				LogMessage ($"[{linkerName} stderr] {e.Data}");
-		}
-
-		static string QuoteFileName (string fileName)
-		{
-			var builder = new CommandLineBuilder ();
-			builder.AppendFileNameIfNotNull (fileName);
-			return builder.ToString ();
 		}
 	}
 }
