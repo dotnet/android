13311cf4bbd8f762bfb8745cce1c9ac4d0c41a02
Alexander Köplinger <alex.koeplinger@outlook.com>
2025-03-24 18:36:29 +0100

    Use CDN URL for dotnet-install script
    The dot.net redirector has issues currently, using the CDN URL directly is more reliable anyway.

--- build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.Unix.cs
+++ build-tools/xaprepare/xaprepare/ConfigAndData/Configurables.Unix.cs
@@ -18,7 +18,7 @@ namespace Xamarin.Android.Prepare
 			// This is the "public" url that we really should be using, but it keeps failing with:
 			// AuthenticationException: The remote certificate is invalid because of errors in the certificate chain: RevocationStatusUnknown
 			// For now we'll grab it directly from GitHub
-			// public static readonly Uri DotNetInstallScript = new Uri ("https://dot.net/v1/dotnet-install.sh");
+			// public static readonly Uri DotNetInstallScript = new Uri ("https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh");
 			public static readonly Uri DotNetInstallScript = new Uri ("https://raw.githubusercontent.com/dotnet/install-scripts/refs/heads/main/src/dotnet-install.sh");
 		}
 	}
