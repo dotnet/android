1494a1edd03e1aec48c564a605b5fdac35c81b9e
Marek Habersack <grendel@twistedcode.net>
2025-03-24 16:53:47 +0100

    Enable a bunch of p/invokes

--- src/native/clr/include/runtime-base/internal-pinvokes.hh
+++ src/native/clr/include/runtime-base/internal-pinvokes.hh
@@ -19,5 +19,20 @@ extern "C" {
 	void monodroid_free (void *ptr) noexcept;
 	const char* _monodroid_lookup_replacement_type (const char *jniSimpleReference);
 	const JniRemappingReplacementMethod* _monodroid_lookup_replacement_method_info (const char *jniSourceType, const char *jniMethodName, const char *jniMethodSignature);
-}
+	xamarin::android::managed_timing_sequence* monodroid_timing_start (const char *message);
+	void monodroid_timing_stop (xamarin::android::managed_timing_sequence *sequence, const char *message);
 
+	void _monodroid_weak_gref_new (jobject curHandle, char curType, jobject newHandle, char newType, const char *threadName, int threadId, const char *from, int from_writable);
+	int _monodroid_weak_gref_get ();
+	int _monodroid_max_gref_get ();
+	void _monodroid_weak_gref_delete (jobject handle, char type, const char *threadName, int threadId, const char *from, int from_writable);
+
+	void _monodroid_lref_log_new (int lrefc, jobject handle, char type, const char *threadName, int threadId, const char *from, int from_writable);
+	void _monodroid_lref_log_delete (int lrefc, jobject handle, char type, const char *threadName, int threadId, const char  *from, int from_writable);
+	void _monodroid_gc_wait_for_bridge_processing ();
+	void* _monodroid_timezone_get_default_id ();
+	void _monodroid_detect_cpu_and_architecture (unsigned short *built_for_cpu, unsigned short *running_on_cpu, unsigned char *is64bit);
+
+	int  _monodroid_getifaddrs (struct ifaddrs **ifap);
+	void _monodroid_freeifaddrs (struct ifaddrs *ifa);
+}
