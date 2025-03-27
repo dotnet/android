b62b4ded5ae7ef0671f4737748919e190bfc736e
Marek Habersack <grendel@twistedcode.net>
2025-03-27 12:50:59 +0100

    [WIP] reshaping linking

--- src/Xamarin.Android.Build.Tasks/Tasks/LinkNativeRuntime.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/LinkNativeRuntime.cs
@@ -76,15 +76,65 @@ public class LinkNativeRuntime : AsyncTask
 			NdkRootPath = AndroidNdkDirectory,
 			NdkApiLevel = AndroidApiLevel,
 		};
-		linker.Link (
+
+		List<ITaskItem> items = OrganizeCommandLineItemsCLR (abi);
+		bool success = linker.Link (
 			outputRuntime,
-			GetAbiItems (NativeObjectFiles, "_NativeAssemblyTarget", abi),
-			GetAbiItems (NativeArchives, "_SelectedNativeArchive", abi),
-			GetAbiItems (LinkLibraries, "_RequiredLinkLibraries", abi),
+			items,
 			GetAbiItems (NativeLinkStartFiles, "_NativeLinkStartFiles", abi),
 			GetAbiItems (NativeLinkEndFiles, "_NativeLinkEndFiles", abi),
 			GetAbiItems (NativeSymbolsToExport, "_NativeSymbolsToExport", abi)
 		);
+
+		if (!success) {
+			Log.LogError ($"Failed to link native runtime {outputRuntime}");
+		}
+	}
+
+	// Puts object files, static archives in the correct order. This is a bit clumsy, but unfortunately necessary
+	List<ITaskItem> OrganizeCommandLineItemsCLR (string abi)
+	{
+		// Code farther down the method does NOT check whether a set is present, it assumes that. This is on purpose, to
+		// let the exception be thrown should a required (and assumed to be present) set be missing.
+		var sets = new Dictionary<string, List<ITaskItem>> (StringComparer.Ordinal);
+		foreach (ITaskItem item in GetAbiItems (NativeArchives, "_SelectedNativeArchive", abi)) {
+			string setName = item.GetRequiredMetadata ("_SelectedNativeArchive", KnownMetadata.NativeLinkItemSet, Log);
+			if (!sets.TryGetValue (setName, out List<ITaskItem>? items)) {
+				items = new List<ITaskItem> ();
+				sets.Add (setName, items);
+			}
+
+			items.Add (item);
+		}
+
+		var ret = new List<ITaskItem> ();
+
+		// First go our own object files...
+		ret.AddRange (GetAbiItems (NativeObjectFiles, "_NativeAssemblyTarget", abi));
+
+		// ...then go our runtime archives...
+		ret.AddRange (sets[NativeRuntimeComponents.KnownSets.XamarinAndroidRuntime]);
+
+		// ...followed by CoreCLR components...
+		ret.AddRange (sets[NativeRuntimeComponents.KnownSets.CoreClrRuntime]);
+
+		// ...and after them the BCL PAL libraries...
+		ret.AddRange (sets[NativeRuntimeComponents.KnownSets.BCL]);
+
+		// ...and then the C/C++ runtime libraries
+		var systemLibs = new Dictionary<string, ITaskItem> (StringComparer.Ordinal);
+		foreach (ITaskItem item in GetAbiItems (LinkLibraries, "_RequiredLinkLibraries", abi)) {
+			systemLibs.Add (Path.GetFileName (item.ItemSpec), item);
+		}
+
+		ret.Add (systemLibs["log"]);
+		ret.AddRange (sets[NativeRuntimeComponents.KnownSets.CplusPlusRuntime]);
+		ret.Add (systemLibs["z"]);
+		ret.Add (systemLibs["m"]);
+		ret.Add (systemLibs["dl"]);
+		ret.Add (systemLibs["c"]);
+
+		return ret;
 	}
 
 	List<ITaskItem> GetAbiItems (ITaskItem[] source, string itemName, string abi)
