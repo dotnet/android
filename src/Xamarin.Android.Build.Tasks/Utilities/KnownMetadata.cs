bbcd121d4f8d4539d647eddc7e92340c70306505
Marek Habersack <grendel@twistedcode.net>
2025-03-27 12:50:59 +0100

    [WIP] reshaping linking

--- src/Xamarin.Android.Build.Tasks/Utilities/KnownMetadata.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/KnownMetadata.cs
@@ -3,7 +3,9 @@ namespace Xamarin.Android.Tasks;
 static class KnownMetadata
 {
 	public const string Abi = "Abi";
-	public const string NativeLinkWholeArchive = "LinkWholeArchive";
-	public const string RuntimeIdentifier = "RuntimeIdentifier";
 	public const string NativeDontExportSymbols = "DontExportSymbols";
+	public const string NativeLinkItemSet = "NativeLinkItemSet";
+	public const string NativeLinkWholeArchive = "LinkWholeArchive";
+	public const string NativeSharedLibrary = "NativeSharedLibrary";
+	public const string RuntimeIdentifier = "RuntimeIdentifier";
 }
