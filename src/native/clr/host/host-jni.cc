d1380d9ed468b2bce9706706519ce11ff4859d8e
Ivan Povazan <55002338+ivanpovazan@users.noreply.github.com>
2025-03-31 20:12:37 +0200

    [CoreCLR] Enable dumping `FastTiming` data with CoreCLR (#9961)
    Context: a7b576885624d09c4cc9d65412da2a7558fd3b36
    Context: https://github.com/grendello/XAPerfTestRunner
    
    It seems that as part of refactoring and splitting the big CoreCLR
    host PR into pieces, the dumping of timing data got disabled for
    CoreCLR.
    
    Reenable this feature so that we can measure startup performance of
    CoreCLR with grendello/XAPerfTestRunner.

--- src/native/clr/host/host-jni.cc
+++ src/native/clr/host/host-jni.cc
@@ -1,6 +1,7 @@
 #include <host/host.hh>
 #include <host/host-jni.hh>
 #include <shared/log_types.hh>
+#include <runtime-base/timing-internal.hh>
 
 using namespace xamarin::android;
 
@@ -15,11 +16,9 @@ JNI_OnLoad (JavaVM *vm, void *reserved)
 JNIEXPORT void
 JNICALL Java_mono_android_Runtime_dumpTimingData ([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass klass)
 {
-	// if (internal_timing == nullptr) {
-	// 	return;
-	// }
-
-	// internal_timing.dump ();
+	if (FastTiming::enabled ()) [[unlikely]] {
+		internal_timing.dump ();
+	}
 }
 
 JNIEXPORT void
