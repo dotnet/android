d5163cce0a0fb26304276db5cb363053cdf0b08c
Simon Rozsival <simon@rozsival.com>
2025-04-03 13:34:11 +0200

    Put code for resolving HttpMessageHandler type from environment variable or MSBuild property behind a feature switch

--- src/Mono.Android/Microsoft.Android.Runtime/RuntimeFeature.cs
+++ src/Mono.Android/Microsoft.Android.Runtime/RuntimeFeature.cs
@@ -10,4 +10,9 @@ static class RuntimeFeature
 	[FeatureSwitchDefinition ($"{FeatureSwitchPrefix}{nameof (ManagedTypeMap)}")]
 	internal static bool ManagedTypeMap { get; } =
 		AppContext.TryGetSwitch ($"{FeatureSwitchPrefix}{nameof (ManagedTypeMap)}", out bool isEnabled) ? isEnabled : false;
+
+	[FeatureSwitchDefinition ($"{FeatureSwitchPrefix}{nameof (XaHttpClientHandlerTypeEnvironmentVariable)}")]
+	[FeatureGuard (typeof (RequiresUnreferencedCodeAttribute))]
+	internal static bool XaHttpClientHandlerTypeEnvironmentVariable { get; } =
+		AppContext.TryGetSwitch ($"{FeatureSwitchPrefix}{nameof (XaHttpClientHandlerTypeEnvironmentVariable)}", out bool isEnabled) ? isEnabled : false;
 }
