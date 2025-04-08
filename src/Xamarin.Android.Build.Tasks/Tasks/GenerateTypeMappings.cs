9b84897cb1fb456f8505fe299d5292c56d2f51cd
Jonathan Pobst <jonathan.pobst@microsoft.com>
2025-04-08 09:39:38 -1000

    [XABT] Move JLO scanning needed for typemap generation to a linker step.

--- src/Xamarin.Android.Build.Tasks/Tasks/GenerateTypeMappings.cs
+++ src/Xamarin.Android.Build.Tasks/Tasks/GenerateTypeMappings.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Java.Interop.Tools.Cecil;
 using Microsoft.Android.Build.Tasks;
 using Microsoft.Build.Framework;
@@ -20,46 +22,94 @@ public class GenerateTypeMappings : AndroidTask
 
 	public bool Debug { get; set; }
 
+	[Output]
+	public ITaskItem [] GeneratedBinaryTypeMaps { get; set; } = [];
+
 	[Required]
 	public string IntermediateOutputDirectory { get; set; } = "";
 
 	public bool SkipJniAddNativeMethodRegistrationAttributeScan { get; set; }
 
 	[Required]
-	public string TypemapOutputDirectory { get; set; } = "";
+	public ITaskItem [] ResolvedAssemblies { get; set; } = [];
 
-	[Output]
-	public ITaskItem [] GeneratedBinaryTypeMaps { get; set; } = [];
+	// This property is temporary and is used to ensure that the new "linker step"
+	// JLO scanning produces the same results as the old process. It will be removed
+	// once the process is complete.
+	public bool RunCheckedBuild { get; set; }
+
+	[Required]
+	public string [] SupportedAbis { get; set; } = [];
 
 	public string TypemapImplementation { get; set; } = "llvm-ir";
+
+	[Required]
+	public string TypemapOutputDirectory { get; set; } = "";
 
 	AndroidRuntime androidRuntime;
 
 	public override bool RunTask ()
 	{
-		androidRuntime = MonoAndroidHelper.ParseAndroidRuntime (AndroidRuntime);
+		// Temporarily used to ensure we still generate the same as the old code
+		if (RunCheckedBuild) {
+			androidRuntime = MonoAndroidHelper.ParseAndroidRuntime (AndroidRuntime);
 
-		// Retrieve the stored NativeCodeGenState
-		var nativeCodeGenStates = BuildEngine4.GetRegisteredTaskObjectAssemblyLocal<ConcurrentDictionary<AndroidTargetArch, NativeCodeGenState>> (
-			MonoAndroidHelper.GetProjectBuildSpecificTaskObjectKey (GenerateJavaStubs.NativeCodeGenStateRegisterTaskKey, WorkingDirectory, IntermediateOutputDirectory),
-			RegisteredTaskObjectLifetime.Build
-		);
+			// Retrieve the stored NativeCodeGenState
+			var nativeCodeGenStates = BuildEngine4.GetRegisteredTaskObjectAssemblyLocal<ConcurrentDictionary<AndroidTargetArch, NativeCodeGenState>> (
+				MonoAndroidHelper.GetProjectBuildSpecificTaskObjectKey (GenerateJavaStubs.NativeCodeGenStateRegisterTaskKey, WorkingDirectory, IntermediateOutputDirectory),
+				RegisteredTaskObjectLifetime.Build
+			);
 
-		NativeCodeGenState? templateCodeGenState = null;
+			NativeCodeGenState? templateCodeGenState = null;
 
-		foreach (var kvp in nativeCodeGenStates) {
-			NativeCodeGenState state = kvp.Value;
-			templateCodeGenState = state;
-			WriteTypeMappings (state);
+			foreach (var kvp in nativeCodeGenStates) {
+				NativeCodeGenState state = kvp.Value;
+				templateCodeGenState = state;
+				WriteTypeMappings (state);
+			}
+
+			if (templateCodeGenState is null)
+				throw new InvalidOperationException ($"Internal error: no native code generator state defined");
+
+			// Set for use by <GeneratePackageManagerJava/> task later
+			NativeCodeGenState.TemplateJniAddNativeMethodRegistrationAttributePresent = templateCodeGenState.JniAddNativeMethodRegistrationAttributePresent;
+
+			return !Log.HasLoggedErrors;
 		}
 
-		if (templateCodeGenState is null)
-			throw new InvalidOperationException ($"Internal error: no native code generator state defined");
+		if (androidRuntime == Xamarin.Android.Tasks.AndroidRuntime.NativeAOT) {
+			// NativeAOT typemaps are generated in `Microsoft.Android.Sdk.ILLink.TypeMappingStep`
+			Log.LogDebugMessage ("Skipping type maps for NativeAOT.");
+			return !Log.HasLoggedErrors;
+		}
 
-		// Set for use by <GeneratePackageManagerJava/> task later
-		NativeCodeGenState.TemplateJniAddNativeMethodRegistrationAttributePresent = templateCodeGenState.JniAddNativeMethodRegistrationAttributePresent;
+		GenerateAllTypeMappings ();
 
 		return !Log.HasLoggedErrors;
+	}
+
+	void GenerateAllTypeMappings ()
+	{
+		var allAssembliesPerArch = MonoAndroidHelper.GetPerArchAssemblies (ResolvedAssemblies, SupportedAbis, validate: true);
+
+		foreach (var set in allAssembliesPerArch)
+			GenerateTypeMap (set.Key, set.Value.Values.ToList ());
+	}
+
+	void GenerateTypeMap (AndroidTargetArch arch, List<ITaskItem> assemblies)
+	{
+		Log.LogDebugMessage ($"Generating type maps for architecture '{arch}'");
+
+		var state = TypeMapObjectsFileAdapter.Create (arch, assemblies, Log);
+
+		// An error was already logged to Log.LogError
+		if (state is null)
+			return;
+
+		var tmg = new TypeMapGenerator (Log, state, androidRuntime) { RunCheckedBuild = RunCheckedBuild };
+		tmg.Generate (Debug, SkipJniAddNativeMethodRegistrationAttributeScan, TypemapOutputDirectory);
+
+		AddOutputTypeMaps (tmg, state.TargetArch);
 	}
 
 	void WriteTypeMappings (NativeCodeGenState state)
@@ -76,11 +126,17 @@ public class GenerateTypeMappings : AndroidTask
 			state = new NativeCodeGenState (state.TargetArch, new TypeDefinitionCache (), state.Resolver, [], [], state.Classifier);
 		}
 
-		var tmg = new TypeMapGenerator (Log, state, androidRuntime);
+		var tmg = new TypeMapGenerator (Log, new NativeCodeGenStateAdapter (state), androidRuntime) { RunCheckedBuild = RunCheckedBuild };
 		tmg.Generate (Debug, SkipJniAddNativeMethodRegistrationAttributeScan, TypemapOutputDirectory);
 
-		string abi = MonoAndroidHelper.ArchToAbi (state.TargetArch);
+		AddOutputTypeMaps (tmg, state.TargetArch);
+	}
+
+	void AddOutputTypeMaps (TypeMapGenerator tmg, AndroidTargetArch arch)
+	{
+		string abi = MonoAndroidHelper.ArchToAbi (arch);
 		var items = new List<ITaskItem> ();
+
 		foreach (string file in tmg.GeneratedBinaryTypeMaps) {
 			var item = new TaskItem (file);
 			string fileName = Path.GetFileName (file);
