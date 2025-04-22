dd401b6605d568c235de3d5c03aaaf174ce2a1e4
Marek Habersack <grendel@twistedcode.net>
2025-04-22 12:17:38 +0200

    Update after the recent timing changes

--- src/Xamarin.Android.Build.Tasks/Utilities/NativeRuntimeComponents.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/NativeRuntimeComponents.cs
@@ -130,6 +130,7 @@ class NativeRuntimeComponents
 			// .NET for Android
 			new AndroidArchive ("libnet-android.release-static-release.a", wholeArchive: true),
 			new AndroidArchive ("libpinvoke-override-dynamic-release.a", wholeArchive: true),
+			new AndroidArchive ("libruntime-base-common-release.a"),
 			new AndroidArchive ("libruntime-base-release.a"),
 			new AndroidArchive ("libxa-java-interop-release.a"),
 			new AndroidArchive ("libxa-lz4-release.a"),
