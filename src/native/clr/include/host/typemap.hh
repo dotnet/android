c422e106092d3ee25440637935d13b15a17201ae
Marek Habersack <grendel@twistedcode.net>
2025-04-25 18:42:53 +0200

    Optimize debug maps a bit

--- src/native/clr/include/host/typemap.hh
+++ src/native/clr/include/host/typemap.hh
@@ -27,7 +27,7 @@ namespace xamarin::android {
 
 		static auto find_java_to_managed_entry (hash_t name_hash) noexcept -> const TypeMapJava*;
 #else
-		static auto typemap_type_to_type_debug (const char *typeName, const TypeMapEntry *map, std::string_view const& from_name, std::string_view const& to_name) noexcept -> const char*;
+		static auto typemap_type_to_type_debug (const char *typeName, const TypeMapEntry *map, const char (&name_map)[], std::string_view const& from_name, std::string_view const& to_name) noexcept -> const char*;
 		static auto typemap_managed_to_java_debug (const char *typeName, const uint8_t *mvid) noexcept -> const char*;
 		static auto typemap_java_to_managed_debug (const char *java_type_name, char const** assembly_name, uint32_t *managed_type_token_id) noexcept -> bool;
 #endif
--- src/native/clr/include/xamarin-app.hh
+++ src/native/clr/include/xamarin-app.hh
@@ -56,10 +56,15 @@ struct TypeMapIndexHeader
 	uint32_t module_file_name_width;
 };
 
+// If any of the members is set to maximum uint32_t value it means the entry is ignored (treated
+// as equivalent to `nullptr` if the member was a pointer). The reasoning is that no string could
+// begin at this offset (well, an empty string could, but we don't have those here)
 struct TypeMapEntry
 {
-	const char *from;
-	const char *to;
+	const uint32_t from;
+	const xamarin::android::hash_t from_hash;
+	const uint32_t to;
+	const xamarin::android::hash_t to_hash;
 };
 
 // MUST match src/Xamarin.Android.Build.Tasks/Utilities/TypeMappingDebugNativeAssemblyGeneratorCLR.cs
@@ -325,6 +330,8 @@ extern "C" {
 	[[gnu::visibility("default")]] extern const TypeMap type_map; // MUST match src/Xamarin.Android.Build.Tasks/Utilities/TypeMappingDebugNativeAssemblyGeneratorCLR.cs
 	[[gnu::visibility("default")]] extern const TypeMapAssembly type_map_unique_assemblies[];
 	[[gnu::visibility("default")]] extern const char type_map_assembly_names[];
+	[[gnu::visibility("default")]] extern const char type_map_managed_type_names[];
+	[[gnu::visibility("default")]] extern const char type_map_java_type_names[];
 #else
 	[[gnu::visibility("default")]] extern const uint32_t managed_to_java_map_module_count;
 	[[gnu::visibility("default")]] extern const uint32_t java_type_count;
