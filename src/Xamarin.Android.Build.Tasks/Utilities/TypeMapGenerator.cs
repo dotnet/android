a45e495b37eda59a1287e536165610151ebb9e6c
Marek Habersack <grendel@twistedcode.net>
2025-04-28 15:27:01 +0200

    java-to-managed map should be fully functional now

--- src/Xamarin.Android.Build.Tasks/Utilities/TypeMapGenerator.cs
+++ src/Xamarin.Android.Build.Tasks/Utilities/TypeMapGenerator.cs
@@ -64,6 +64,7 @@ namespace Xamarin.Android.Tasks
 			public string ManagedName;
 			public bool SkipInJavaToManaged;
 			public TypeMapDebugEntry DuplicateForJavaToManaged;
+			public string AssemblyName;
 
 			// This field is only used by the Cecil adapter for temp storage while reading.
 			// It is not used to create the typemap.
