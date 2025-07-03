9ecec100fd1801759c3cd7524838befba465bb60
Marek Habersack <grendel@twistedcode.net>
2025-07-03 16:01:30 +0200

    Scaffolding for the new app

---
+++ tools/apput/src/ApplicationAssembly/ApplicationAssembly.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+
+namespace ApplicationUtility;
+
+public class ApplicationAssembly : IAspect
+{
+	public static string AspectName { get; } = "Application assembly";
+
+	public bool IsCompressed    { get; private set; }
+	public string Name          { get; private set; } = "";
+	public ulong CompressedSize { get; private set; }
+	public ulong Size           { get; private set; }
+	public bool IgnoreOnLoad    { get; private set; }
+
+	public static IAspect LoadAspect (Stream stream, string description)
+	{
+		throw new NotImplementedException ();
+	}
+
+	public static bool ProbeAspect (Stream stream)
+	{
+		throw new NotImplementedException ();
+	}
+}
