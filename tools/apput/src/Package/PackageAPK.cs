4db5382936ee9f76d245a1b8e788e411aac9d83c
Marek Habersack <grendel@twistedcode.net>
2025-07-04 17:41:43 +0200

    Slight IAspect changes + some flesh for the skeleton

--- tools/apput/src/Package/PackageAPK.cs
+++ tools/apput/src/Package/PackageAPK.cs
@@ -1,6 +1,12 @@
+using System.IO.Compression;
+
 namespace ApplicationUtility;
 
 class PackageAPK : ApplicationPackage
 {
 	public override string PackageFormat { get; } = "APK package";
+
+	public PackageAPK (ZipArchive zip, string? description)
+		: base (zip, description)
+	{}
 }
