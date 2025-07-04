d3483f17a447580cba906e472ce5e85624d84a4e
Marek Habersack <grendel@twistedcode.net>
2025-07-04 17:41:43 +0200

    Slight IAspect changes + some flesh for the skeleton

--- tools/apput/src/Common/NativeArchitecture.cs
+++ tools/apput/src/Common/NativeArchitecture.cs
@@ -1,11 +1,14 @@
+using System;
+
 namespace ApplicationUtility;
 
+[Flags]
 public enum NativeArchitecture
 {
-	Unknown,
+	Unknown = 0x00,
 
-	Arm,
-	Arm64,
-	X86,
-	X64,
+	Arm     = 0x01,
+	Arm64   = 0x02,
+	X86     = 0x04,
+	X64     = 0x08,
 }
--- tools/apput/src/Package/PackageAAB.cs
+++ tools/apput/src/Package/PackageAAB.cs
@@ -1,6 +1,12 @@
+using System.IO.Compression;
+
 namespace ApplicationUtility;
 
 class PackageAAB : ApplicationPackage
 {
 	public override string PackageFormat { get; } = "AAB package";
+
+	public PackageAAB (ZipArchive zip, string? description)
+		: base (zip, description)
+	{}
 }
