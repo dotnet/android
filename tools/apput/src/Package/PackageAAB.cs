04e6a0403a2eb3429f2abd8d7cbd8ce31fd1b3ae
Marek Habersack <grendel@twistedcode.net>
2025-07-07 15:02:20 +0200

    A few steps more

--- tools/apput/src/Package/PackageAAB.cs
+++ tools/apput/src/Package/PackageAAB.cs
@@ -5,6 +5,8 @@ namespace ApplicationUtility;
 class PackageAAB : ApplicationPackage
 {
 	public override string PackageFormat { get; } = "AAB package";
+	protected override string NativeLibDirBase => "base/lib";
+	protected override string AndroidManifestPath => "base/manifest/AndroidManifest.xml";
 
 	public PackageAAB (ZipArchive zip, string? description)
 		: base (zip, description)
