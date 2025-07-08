6b3d53d0a8691bb00c98bb699c2854c347220289
Marek Habersack <grendel@twistedcode.net>
2025-07-08 16:55:15 +0200

    Progressing with assembly stores + shared libraries

--- tools/apput/src/AssemblyStore/Format_V3.cs
+++ tools/apput/src/AssemblyStore/Format_V3.cs
@@ -1,5 +1,17 @@
+using System.IO;
+
 namespace ApplicationUtility;
 
-class Format_V3
+class Format_V3 : FormatBase
 {
+	public Format_V3 (Stream storeStream, string? description)
+		: base (storeStream, description)
+	{}
+
+	public override IAspectState Validate ()
+	{
+		// TODO: validate that the store has correct format and populate the state below accordingly
+		//       to save time later.
+		return new AssemblyStoreAspectState (true);
+	}
 }
