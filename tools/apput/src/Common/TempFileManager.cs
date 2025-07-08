cd7304b337ea3b369ce50ece7602530ef597e664
Marek Habersack <grendel@twistedcode.net>
2025-07-08 09:18:34 +0200

    Oops, forgot to add this one, doh

---
+++ tools/apput/src/Common/TempFileManager.cs
@@ -0,0 +1,14 @@
+namespace ApplicationUtility;
+
+class TempFileManager
+{
+	public static void RegisterFile (string path)
+	{
+		// TODO: implement
+	}
+
+	public static void Cleanup ()
+	{
+		// TODO: implement
+	}
+}
