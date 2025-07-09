32a91c9ca41eb0d347b7bf4080a1e56838ccc974
Marek Habersack <grendel@twistedcode.net>
2025-07-09 18:21:53 +0200

    More assembly store code

---
+++ tools/apput/src/AssemblyStore/AssemblyStoreAssemblyDescriptorV3.cs
@@ -0,0 +1,5 @@
+namespace ApplicationUtility;
+
+// Format is identical to V2, class exists merely for versioning consistency
+class AssemblyStoreAssemblyDescriptorV3 : AssemblyStoreAssemblyDescriptorV2
+{}
