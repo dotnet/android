5949478823f4946e06ddb3fb0ea3fc6a281745ca
Marek Habersack <grendel@twistedcode.net>
2025-07-15 16:41:04 +0200

    Android manifest almost finished (protobuf missing) + native bits
    Beginnings of support for reading ELF binaries, needed for typemaps and friends

--- tools/apput/src/Android/AndroidManifestAspectState.cs
+++ tools/apput/src/Android/AndroidManifestAspectState.cs
@@ -1,12 +1,20 @@
+using System.Xml;
+
 namespace ApplicationUtility;
 
 class AndroidManifestAspectState : IAspectState
 {
 	public bool Success => true;
 	public AXMLParser? BinaryParser { get; }
+	public XmlDocument? Xml { get; }
 
 	public AndroidManifestAspectState (AXMLParser? binaryParser)
 	{
 		BinaryParser = binaryParser;
+	}
+
+	public AndroidManifestAspectState (XmlDocument? xmlDoc)
+	{
+		Xml = xmlDoc;
 	}
 }
