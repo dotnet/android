dotnet/android

commit 1f8f719573ce4ab9f04bf87a689f804311052d48

[CoreCLR] Implement GC bridge (#10198)
Replaces: https://github.com/dotnet/android/pull/10185
Implements: https://github.com/dotnet/runtime/issues/115506
Builds on top of: https://github.com/dotnet/runtime/pull/116310

### Description

This PR implements GC bridge for CoreCLR using the `JavaMarshal` APIs
introduced in https://github.com/dotnet/runtime/pull/116310. The code
in this PR is CoreCLR specific and while it will build with other
runtimes on .NET 10, the `ManagedValueManager` class will throw on any
other runtime other than CoreCLR. In the future, the same GC bridge
mechanism should be also supported by Native AOT, so this code might
be reused for that platform as well at some point.

The code of the GC bridge is placed in 3 main locations:

#### `ManagedValueManager.cs`

Code in this class interfaces with the `JavaMarshal` APIs.

This class keeps a dictionary of mapping between .NET and Java objects
`RegisteredInstances`.

The class carefuly manages the lifetimes of the bridge objects and
their associated native memory ("GC bridge context" -
`HandleContext`). The implementation follows these rules:

* Do not access the `Target` of the reference tracking `GCHandles`.
  Doing this could cause a race condition with the GC collecting
  handles in background thread. Instead, always access the peers via
  `WeakReference<IJavaPeerable>.Target` which blocks if there is an
  ongoing bridge processing.
* Do not modify the `RegisteredInstances` during _bridge processing_.
  Doing this would require taking a lock on `RegisteredInstances`
  which might already be locked in some other method of
  `ManagedValueManager` called from another thread (for example the
  `AddPeer` method) which might be blocked waiting on
  `WeakReference<IJavaPeerable>.Target` to return. For this reason, we
  have a queue of known dead weak references stored in the
  `RegisteredInstances` method which we fill at the end of bridge

src/Mono.Android/Android.Runtime/AndroidRuntimeInternal.cs
@@ -64,9 +64,7 @@ namespace Android.Runtime
 
 		public static void WaitForBridgeProcessing ()
 		{
-			if (!BridgeProcessing)
-				return;
-			RuntimeNativeMethods._monodroid_gc_wait_for_bridge_processing ();
+			Java.Interop.JniEnvironment.Runtime.ValueManager.WaitForGCBridgeProcessing ();
 		}
 	}
 }

src/native/clr/host/host.cc
@@ -9,6 +9,7 @@
 
 #include <xamarin-app.hh>
 #include <host/assembly-store.hh>
+#include <host/gc-bridge.hh>
 #include <host/fastdev-assemblies.hh>
 #include <host/host.hh>
 #include <host/host-jni.hh>
@@ -451,9 +452,8 @@ void Host::Java_mono_android_Runtime_initInternal (
 
 	log_info (LOG_GC, "GREF GC Threshold: {}"sv, init.grefGcThreshold);
 
-	// TODO: GC bridge to initialize here
-
 	OSBridge::initialize_on_runtime_init (env, runtimeClass);
+	GCBridge::initialize_on_runtime_init (env, runtimeClass);
 
 	if (FastTiming::enabled ()) [[unlikely]] {
 		internal_timing.start_event (TimingEventKind::NativeToManagedTransition);
@@ -572,6 +572,7 @@ auto Host::Java_JNI_OnLoad (JavaVM *vm, [[maybe_unused]] void *reserved) noexcep
 	JNIEnv *env = nullptr;
 	vm->GetEnv ((void**)&env, JNI_VERSION_1_6);
 	OSBridge::initialize_on_onload (vm, env);
+	GCBridge::initialize_on_onload (env);
 
 	AndroidSystem::init_max_gref_count ();
 	return JNI_VERSION_1_6;

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Resources/Base/BuildReleaseArm64SimpleDotNet.apkdesc
@@ -5,67 +5,67 @@
       "Size": 3036
     },
     "classes.dex": {
-      "Size": 22484
+      "Size": 22488
     },
     "lib/arm64-v8a/lib__Microsoft.Android.Resource.Designer.dll.so": {
       "Size": 18288
     },
     "lib/arm64-v8a/lib_Java.Interop.dll.so": {
-      "Size": 86688
+      "Size": 88472
     },
     "lib/arm64-v8a/lib_Mono.Android.dll.so": {
-      "Size": 117712
+      "Size": 124040
     },
     "lib/arm64-v8a/lib_Mono.Android.Runtime.dll.so": {
-      "Size": 22384
+      "Size": 23424
     },
     "lib/arm64-v8a/lib_System.Console.dll.so": {
-      "Size": 24392
+      "Size": 24408
     },
     "lib/arm64-v8a/lib_System.Linq.dll.so": {
-      "Size": 25336
+      "Size": 25488
     },
     "lib/arm64-v8a/lib_System.Private.CoreLib.dll.so": {
-      "Size": 628216
+      "Size": 692528
     },
     "lib/arm64-v8a/lib_System.Runtime.dll.so": {
-      "Size": 20056
+      "Size": 20104
     },
     "lib/arm64-v8a/lib_System.Runtime.InteropServices.dll.so": {
-      "Size": 21480
+      "Size": 21528
     },
     "lib/arm64-v8a/lib_UnnamedProject.dll.so": {
       "Size": 20024
     },
     "lib/arm64-v8a/libarc.bin.so": {
-      "Size": 18872
+      "Size": 18984
     },
     "lib/arm64-v8a/libmono-component-marshal-ilgen.so": {
-      "Size": 36440
+      "Size": 36616
     },
     "lib/arm64-v8a/libmonodroid.so": {
-      "Size": 1524752
+      "Size": 1508312
     },
     "lib/arm64-v8a/libmonosgen-2.0.so": {
-      "Size": 3101112
+      "Size": 3119744
     },
     "lib/arm64-v8a/libSystem.Globalization.Native.so": {
-      "Size": 71976
+      "Size": 71952
     },
     "lib/arm64-v8a/libSystem.IO.Compression.Native.so": {
-      "Size": 758896
+      "Size": 759304
     },
     "lib/arm64-v8a/libSystem.Native.so": {
-      "Size": 103520
+      "Size": 104216
     },
     "lib/arm64-v8a/libSystem.Security.Cryptography.Native.Android.so": {
-      "Size": 165000
+      "Size": 165240
     },
     "lib/arm64-v8a/libxamarin-app.so": {
-      "Size": 19536
+      "Size": 19800
     },
     "META-INF/BNDLTOOL.RSA": {
-      "Size": 1221
+      "Size": 1223
     },
     "META-INF/BNDLTOOL.SF": {
       "Size": 3266
@@ -98,5 +98,5 @@
       "Size": 1904
     }
   },
-  "PackageSize": 3078677
+  "PackageSize": 3148309
 }

src/Mono.Android/Android.Runtime/AndroidRuntime.cs
@@ -629,7 +629,9 @@ namespace Android.Runtime {
 
 		public override void WaitForGCBridgeProcessing ()
 		{
-			AndroidRuntimeInternal.WaitForBridgeProcessing ();
+			if (!AndroidRuntimeInternal.BridgeProcessing)
+				return;
+			RuntimeNativeMethods._monodroid_gc_wait_for_bridge_processing ();
 		}
 
 		public override IJavaPeerable? CreatePeer (

src/Mono.Android/Android.Runtime/RuntimeNativeMethods.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Java;
 using System.Text;
 
 namespace Android.Runtime
@@ -18,7 +19,7 @@ namespace Android.Runtime
 		All     = Java | Managed | Native | Signals,
 	}
 
-	internal static class RuntimeNativeMethods
+	internal unsafe static class RuntimeNativeMethods
 	{
 		[DllImport (RuntimeConstants.InternalDllName, CallingConvention = CallingConvention.Cdecl)]
 		internal extern static void monodroid_log (LogLevel level, LogCategories category, string message);
@@ -91,6 +92,11 @@ namespace Android.Runtime
 
 		[DllImport (RuntimeConstants.InternalDllName, CallingConvention = CallingConvention.Cdecl)]
 		internal static extern bool clr_typemap_java_to_managed (string java_type_name, out IntPtr managed_assembly_name, out uint managed_type_token_id);
+
+		[DllImport (RuntimeConstants.InternalDllName, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern delegate* unmanaged<MarkCrossReferencesArgs*, void> clr_initialize_gc_bridge (
+			delegate* unmanaged<MarkCrossReferencesArgs*, void> bridge_processing_started_callback,
+			delegate* unmanaged<MarkCrossReferencesArgs*, void> bridge_processing_finished_callback);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		internal static extern void monodroid_unhandled_exception (Exception javaException);

commit fdbb7f89d91ff277055b95b33ecb832c1543e99f

[Mono.Android] trimmer feature switches replace `DotNetRuntimeType` (#10296)
Context: https://github.com/dotnet/android/pull/10198#discussion_r2205702701

`ManagedValueManager` is not currently trimmed away in Android apps
running on Mono.

We can make this happen by:

* Remove `DotNetRuntimeType` enum

* Introduce `RuntimeFeature.IsMonoRuntime` and
  `RuntimeFeature.IsCoreClrRuntime`

* Set switches by default for each runtime:

    <ItemGroup>
      <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsMonoRuntime"
          Value="false"
          Trim="true"
      />
      <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsCoreClrRuntime"
          Value="true"
          Trim="true"
      />
    </ItemGroup>

I have not yet seen a need within the code to introduce
`RuntimeFeature.IsNativeAotRuntime`, but we could do so in the future.

Currently, NativeAOT works via the other two runtime feature switches
being `false` and then none of the problematic code paths are hit.

* Update `BuildReleaseArm64SimpleDotNet.apkdesc`

This fixes the minor app size regression in 1f8f7195.

src/Mono.Android.Runtime/Mono.Android.Runtime.csproj
@@ -52,11 +52,11 @@
   <ItemGroup>
     <Compile Include="$(IntermediateOutputPath)AssemblyInfo.cs" />
     <Compile Include="..\Mono.Android\Android.Runtime\AndroidRuntimeInternal.cs" />
-    <Compile Include="..\Mono.Android\Android.Runtime\DotNetRuntimeType.cs" />
     <Compile Include="..\Mono.Android\Android.Runtime\LogCategories.cs" />
     <Compile Include="..\Mono.Android\Android.Runtime\LogLevel.cs" />
     <Compile Include="..\Mono.Android\Android.Runtime\RuntimeConstants.cs" />
     <Compile Include="..\Mono.Android\Android.Runtime\RuntimeNativeMethods.cs" />
+    <Compile Include="..\Mono.Android\Microsoft.Android.Runtime\RuntimeFeature.cs" />
     <Compile Include="Android.Runtime\AndroidEnvironmentInternal.cs" />
   </ItemGroup>
 

src/Mono.Android/Android.Runtime/AndroidRuntimeInternal.cs
@@ -1,36 +1,10 @@
 #if INSIDE_MONO_ANDROID_RUNTIME
 using System;
 using System.Reflection;
+using Microsoft.Android.Runtime;
 
 namespace Android.Runtime
 {
-	// The existence of InternalRuntimeTypeHolder and DotNetRuntimeTypeConverter classes looks weird, but
-	// we must handle a weird situation.  AndroidRuntimeInternal needs to know in its static constructor
-	// what is the current runtime type, but it cannot query JNIEnvInit.RuntimeType, since that type lives
-	// in the Mono.Android assembly, while AndroidRuntimeInternal lives in Mono.Android.Runtime and it cannot
-	// access JNIEnvInit and Mono.Android.Runtime doesn't reference Mono.Android but Mono.Android **does** reference
-	// Mono.Android.Runtime and has access to its internals.
-	//
-	// Mono.Android.Runtime, also, includes several source files from Mono.Android - **both** assemblies
-	// include the same source files.  In case of the DotNetRuntimeType enum, this declares two distinct types - one
-	// in Mono.Android and another in Mono.Android.Runtime, and so if JNIEnvInit.Initialize were to try to set the
-	// `DotNetRuntimeType RuntimeType;` field/property in either of the classes below, we'd get a compilation error
-	// to the effect of it being unable to cast `Android.Runtime.DotNetRuntimeType` to `Android.Runtime.DotNetRuntimeType`,
-	// which is usually as clear as mud :)
-	//
-	// To solve this and not duplicate code, the InternalRuntimeTypeHolder class is introduced which acts as a proxy since
-	// the AndroidRuntimeInternal static constructor must know the runtime type and JNIEnvInit.Initialize takes care of it by
-	// calling `SetRuntimeType` below long before AndroidRuntimeInternal cctor is invoked.
-	public static class InternalRuntimeTypeHolder
-	{
-		internal static DotNetRuntimeType RuntimeType = DotNetRuntimeType.Unknown;
-
-		internal static void SetRuntimeType (uint runtimeType)
-		{
-			RuntimeType = DotNetRuntimeTypeConverter.Convert (runtimeType);
-		}
-	}
-
 	public static class AndroidRuntimeInternal
 	{
 		internal static readonly Action<Exception> mono_unhandled_exception;
@@ -41,11 +15,13 @@ namespace Android.Runtime
 
 		static AndroidRuntimeInternal ()
 		{
-			mono_unhandled_exception = InternalRuntimeTypeHolder.RuntimeType switch {
-				DotNetRuntimeType.MonoVM  => MonoUnhandledException,
-				DotNetRuntimeType.CoreCLR => CoreClrUnhandledException,
-				_                         => throw new NotSupportedException ($"Internal error: runtime type {InternalRuntimeTypeHolder.RuntimeType} not supported")
-			};
+			if (RuntimeFeature.IsMonoRuntime) {
+				mono_unhandled_exception = MonoUnhandledException;
+			} else if (RuntimeFeature.IsCoreClrRuntime) {
+				mono_unhandled_exception = CoreClrUnhandledException;
+			} else {
+				throw new NotSupportedException ("Internal error: unknown runtime not supported");
+			}
 		}
 
 		static void CoreClrUnhandledException (Exception ex)

src/Mono.Android/Android.Runtime/DotNetRuntimeType.cs
@@ -1,32 +1,0 @@
-using System;
-
-namespace Android.Runtime;
-
-enum DotNetRuntimeType
-{
-	Unknown,
-	MonoVM,
-	CoreCLR,
-	NativeAOT,
-}
-
-// This looks weird, see comments in RuntimeTypeInternal.cs
-class DotNetRuntimeTypeConverter
-{
-	// Values for the JnienvInitializeArgs.runtimeType field
-	//
-	// NOTE: Keep this in sync with managed side in src/native/common/include/managed-interface.hh
-	const uint RuntimeTypeMonoVM    = 0x0001;
-	const uint RuntimeTypeCoreCLR   = 0x0002;
-	const uint RuntimeTypeNativeAOT = 0x0004;
-
-	public static DotNetRuntimeType Convert (uint runtimeType)
-	{
-		return runtimeType switch {
-			RuntimeTypeMonoVM => DotNetRuntimeType.MonoVM,
-			RuntimeTypeCoreCLR => DotNetRuntimeType.CoreCLR,
-			RuntimeTypeNativeAOT => DotNetRuntimeType.NativeAOT,
-			_ => throw new NotSupportedException ($"Internal error: unsupported runtime type {runtimeType:x}")
-		};
-	}
-}

src/Mono.Android/Android.Runtime/JNIEnv.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
@@ -12,7 +13,7 @@ using System.Text;
 
 using Java.Interop;
 using Java.Interop.Tools.TypeNameMappings;
-using System.Diagnostics.CodeAnalysis;
+using Microsoft.Android.Runtime;
 
 namespace Android.Runtime {
 	public static partial class JNIEnv {
@@ -100,16 +101,12 @@ namespace Android.Runtime {
 				Logger.Log (LogLevel.Info, "MonoDroid", "UNHANDLED EXCEPTION:");
 				Logger.Log (LogLevel.Info, "MonoDroid", javaException.ToString ());
 
-				switch (JNIEnvInit.RuntimeType) {
-					case DotNetRuntimeType.MonoVM:
-						MonoDroidUnhandledException (innerException ?? javaException);
-						break;
-					case DotNetRuntimeType.CoreCLR:
-						// TODO: what to do here?
-						break;
-
-					default:
-						throw new NotSupportedException ($"Internal error: runtime type {JNIEnvInit.RuntimeType} not supported");
+				if (RuntimeFeature.IsMonoRuntime) {
+					MonoDroidUnhandledException (innerException ?? javaException);
+				} else if (RuntimeFeature.IsCoreClrRuntime) {
+					// TODO: what to do here?
+				} else {
+					throw new NotSupportedException ("Internal error: unknown runtime not supported");
 				}
 			} catch (Exception e) {
 				Logger.Log (LogLevel.Error, "monodroid", "Exception thrown while raising AppDomain.UnhandledException event: " + e.ToString ());
@@ -453,11 +450,13 @@ namespace Android.Runtime {
 
 			IntPtr ret;
 			fixed (byte* mvidptr = mvid_data) {
-				ret = JNIEnvInit.RuntimeType switch {
-					DotNetRuntimeType.MonoVM  => monovm_typemap_managed_to_java (type, mvidptr),
-					DotNetRuntimeType.CoreCLR => RuntimeNativeMethods.clr_typemap_managed_to_java (type.FullName, (IntPtr)mvidptr),
-					_                         => throw new NotSupportedException ($"Internal error: runtime type {JNIEnvInit.RuntimeType} not supported")
-				};
+				if (RuntimeFeature.IsMonoRuntime) {
+					ret = monovm_typemap_managed_to_java (type, mvidptr);
+				} else if (RuntimeFeature.IsCoreClrRuntime) {
+					ret = RuntimeNativeMethods.clr_typemap_managed_to_java (type.FullName, (IntPtr)mvidptr);
+				} else {
+					throw new NotSupportedException ("Internal error: unknown runtime not supported");
+				}
 			}
 
 			if (ret == IntPtr.Zero) {

src/Mono.Android/Android.Runtime/JNIEnvInit.cs
@@ -34,7 +34,6 @@ namespace Android.Runtime
 			public bool            jniRemappingInUse;
 			public bool            marshalMethodsEnabled;
 			public IntPtr          grefGCUserPeerable;
-			public uint            runtimeType;
 			public bool            managedMarshalMethodsLookupEnabled;
 		}
 #pragma warning restore 0649
@@ -49,8 +48,6 @@ namespace Android.Runtime
 		internal static IntPtr java_class_loader;
 
 		internal static JniRuntime? androidRuntime;
-
-		public static DotNetRuntimeType RuntimeType { get; private set; } = DotNetRuntimeType.Unknown;
 
 		[UnmanagedCallersOnly]
 		static unsafe void RegisterJniNatives (IntPtr typeName_ptr, int typeName_len, IntPtr jniClass, IntPtr methods_ptr, int methods_len)
@@ -91,9 +88,10 @@ namespace Android.Runtime
 		[UnmanagedCallersOnly]
 		internal static unsafe void Initialize (JnienvInitializeArgs* args)
 		{
-			// This looks weird, see comments in RuntimeTypeInternal.cs
-			RuntimeType = DotNetRuntimeTypeConverter.Convert (args->runtimeType);
-			InternalRuntimeTypeHolder.SetRuntimeType (args->runtimeType);
+			// Should not be allowed
+			if (RuntimeFeature.IsMonoRuntime && RuntimeFeature.IsCoreClrRuntime) {
+				throw new NotSupportedException ("Internal error: both RuntimeFeature.IsMonoRuntime and RuntimeFeature.IsCoreClrRuntime are enabled");
+			}
 
 			IntPtr total_timing_sequence = IntPtr.Zero;
 			IntPtr partial_timing_sequence = IntPtr.Zero;
@@ -114,12 +112,13 @@ namespace Android.Runtime
 			} else {
 				typeManager     = new AndroidTypeManager (args->jniAddNativeMethodRegistrationAttributePresent != 0);
 			}
-			valueManager = RuntimeType switch
-			{
-				DotNetRuntimeType.MonoVM => new AndroidValueManager(),
-				DotNetRuntimeType.CoreCLR => ManagedValueManager.GetOrCreateInstance(),
-				_ => throw new NotSupportedException ($"No value manager for runtime type: {RuntimeType}"),
-			};
+			if (RuntimeFeature.IsMonoRuntime) {
+				valueManager = new AndroidValueManager ();
+			} else if (RuntimeFeature.IsCoreClrRuntime) {
+				valueManager = ManagedValueManager.GetOrCreateInstance ();
+			} else {
+				throw new NotSupportedException ("Internal error: unknown runtime not supported");
+			}
 			androidRuntime = new AndroidRuntime (
 					args->env,
 					args->javaVm,

src/Mono.Android/Java.Interop/TypeManager.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 using Java.Interop.Tools.TypeNameMappings;
 
 using Android.Runtime;
+using Microsoft.Android.Runtime;
 
 namespace Java.Interop {
 
@@ -267,11 +268,13 @@ namespace Java.Interop {
 				return type;
 			}
 
-			type = JNIEnvInit.RuntimeType switch {
-				DotNetRuntimeType.MonoVM  => monovm_typemap_java_to_managed (class_name),
-				DotNetRuntimeType.CoreCLR => clr_typemap_java_to_managed (class_name),
-				_                         => throw new NotSupportedException ($"Internal error: runtime type {JNIEnvInit.RuntimeType} not supported")
-			};
+			if (RuntimeFeature.IsMonoRuntime) {
+				type = monovm_typemap_java_to_managed (class_name);
+			} else if (RuntimeFeature.IsCoreClrRuntime) {
+				type = clr_typemap_java_to_managed (class_name);
+			} else {
+				throw new NotSupportedException ("Internal error: unknown runtime not supported");
+			}
 
 			if (type != null) {
 				TypeManagerMapDictionaries.JniToManaged.Add (class_name, type);

src/Mono.Android/Microsoft.Android.Runtime/RuntimeFeature.cs
@@ -5,9 +5,21 @@ namespace Microsoft.Android.Runtime;
 
 static class RuntimeFeature
 {
+	const bool ManagedTypeMapEnabledByDefault = false;
+	const bool IsMonoRuntimeEnabledByDefault = true;
+	const bool IsCoreClrRuntimeEnabledByDefault = false;
+
 	const string FeatureSwitchPrefix = "Microsoft.Android.Runtime.RuntimeFeature.";
 
 	[FeatureSwitchDefinition ($"{FeatureSwitchPrefix}{nameof (ManagedTypeMap)}")]
 	internal static bool ManagedTypeMap { get; } =
-		AppContext.TryGetSwitch ($"{FeatureSwitchPrefix}{nameof (ManagedTypeMap)}", out bool isEnabled) ? isEnabled : false;
+		AppContext.TryGetSwitch ($"{FeatureSwitchPrefix}{nameof (ManagedTypeMap)}", out bool isEnabled) ? isEnabled : ManagedTypeMapEnabledByDefault;
+
+	[FeatureSwitchDefinition ($"{FeatureSwitchPrefix}{nameof (IsMonoRuntime)}")]
+	internal static bool IsMonoRuntime { get; } =
+		AppContext.TryGetSwitch ($"{FeatureSwitchPrefix}{nameof (IsMonoRuntime)}", out bool isEnabled) ? isEnabled : IsMonoRuntimeEnabledByDefault;
+		
+	[FeatureSwitchDefinition ($"{FeatureSwitchPrefix}{nameof (IsCoreClrRuntime)}")]
+	internal static bool IsCoreClrRuntime { get; } =
+		AppContext.TryGetSwitch ($"{FeatureSwitchPrefix}{nameof (IsCoreClrRuntime)}", out bool isEnabled) ? isEnabled : IsCoreClrRuntimeEnabledByDefault;
 }

src/Mono.Android/Mono.Android.csproj
@@ -220,7 +220,6 @@
     <Compile Include="Android.Runtime\BoundExceptionType.cs" />
     <Compile Include="Android.Runtime\CharSequence.cs" />
     <Compile Include="Android.Runtime\CPUArchitecture.cs" />
-    <Compile Include="Android.Runtime\DotNetRuntimeType.cs" />
     <Compile Include="Android.Runtime\Extensions.cs" />
     <Compile Include="Android.Runtime\GeneratedEnumAttribute.cs" />
     <Compile Include="Android.Runtime\IJavaObject.cs" />

src/Xamarin.Android.Build.Tasks/Microsoft.Android.Sdk/targets/Microsoft.Android.Sdk.CoreCLR.targets
@@ -20,6 +20,18 @@ This file contains the CoreCLR-specific MSBuild logic for .NET for Android.
     <AllowReadyToRunWithoutRuntimeIdentifier Condition=" '$(PublishReadyToRun)' == 'true' and '$(RuntimeIdentifiers)' != '' ">true</AllowReadyToRunWithoutRuntimeIdentifier>
   </PropertyGroup>
 
+  <!-- Default feature switches -->
+  <ItemGroup>
+    <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsMonoRuntime"
+        Value="false"
+        Trim="true"
+    />
+    <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsCoreClrRuntime"
+        Value="true"
+        Trim="true"
+    />
+  </ItemGroup>
+
   <Target Name="_CLRUseLocalRuntimePacks" AfterTargets="ResolveFrameworkReferences"
           Condition=" '$(_CLRLocalRuntimePath)' != '' And '$(_AndroidRuntime)' == 'CoreCLR' ">
     <PropertyGroup>

src/Xamarin.Android.Build.Tasks/Microsoft.Android.Sdk/targets/Microsoft.Android.Sdk.MonoVM.targets
@@ -1,0 +1,22 @@
+<!--
+***********************************************************************************************
+Microsoft.Android.Sdk.MonoVM.targets
+
+This file contains the MonoVM-specific MSBuild logic for .NET for Android.
+***********************************************************************************************
+-->
+<Project>
+
+  <!-- Default feature switches -->
+  <ItemGroup>
+    <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsMonoRuntime"
+        Value="true"
+        Trim="true"
+    />
+    <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsCoreClrRuntime"
+        Value="false"
+        Trim="true"
+    />
+  </ItemGroup>
+
+</Project>

src/Xamarin.Android.Build.Tasks/Microsoft.Android.Sdk/targets/Microsoft.Android.Sdk.NativeAOT.targets
@@ -29,6 +29,18 @@ This file contains the NativeAOT-specific MSBuild logic for .NET for Android.
     <CompressSymbols>false</CompressSymbols>
   </PropertyGroup>
 
+  <!-- Default feature switches -->
+  <ItemGroup>
+    <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsMonoRuntime"
+        Value="false"
+        Trim="true"
+    />
+    <RuntimeHostConfigurationOption Include="Microsoft.Android.Runtime.RuntimeFeature.IsCoreClrRuntime"
+        Value="false"
+        Trim="true"
+    />
+  </ItemGroup>
+
   <!-- Make IlcCompile depend on the trimmer -->
   <PropertyGroup>
     <IlcCompileDependsOn>

src/Xamarin.Android.Build.Tasks/Microsoft.Android.Sdk/targets/Microsoft.Android.Sdk.targets
@@ -33,7 +33,6 @@
   <Import Project="Microsoft.Android.Sdk.DefaultProperties.targets" />
   <Import Project="$(MSBuildThisFileDirectory)..\tools\Xamarin.Android.Common.Debugging.props"
       Condition="Exists('$(MSBuildThisFileDirectory)..\tools\Xamarin.Android.Common.Debugging.props') And '$(DesignTimeBuild)' != 'true' "/>
-  <Import Project="Microsoft.Android.Sdk.CoreCLR.targets"   Condition=" '$(_AndroidRuntime)' == 'CoreCLR' " />
-  <Import Project="Microsoft.Android.Sdk.NativeAOT.targets" Condition=" '$(_AndroidRuntime)' == 'NativeAOT' " />
+  <Import Project="Microsoft.Android.Sdk.$(_AndroidRuntime).targets" Condition=" '$(_AndroidRuntime)' != '' " />
 
 </Project>

src/native/clr/host/host.cc
@@ -422,7 +422,6 @@ void Host::Java_mono_android_Runtime_initInternal (
 	);
 
 	struct JnienvInitializeArgs init = {};
-	init.runtimeType                                    = RuntimeTypeCoreCLR;
 	init.javaVm                                         = jvm;
 	init.env                                            = env;
 	init.logCategories                                  = log_categories;

src/native/common/include/managed-interface.hh
@@ -5,13 +5,6 @@
 #include <jni.h>
 
 namespace xamarin::android {
-	// Values for the JnienvInitializeArgs.runtimeType field
-	//
-	// NOTE: Keep this in sync with managed side in src/Mono.Android/Android.Runtime/DotNetRuntimeType.cs
-	static inline constexpr uint32_t RuntimeTypeMonoVM    = 0x0001;
-	static inline constexpr uint32_t RuntimeTypeCoreCLR   = 0x0002;
-	static inline constexpr uint32_t RuntimeTypeNativeAOT = 0x0004;
-
 	// Values must be identical to those in src/Mono.Android/Android.Runtime/RuntimeNativeMethods.cs
 	enum class TraceKind : uint32_t
 	{
@@ -39,7 +32,6 @@ namespace xamarin::android {
 		bool            jniRemappingInUse;
 		bool            marshalMethodsEnabled;
 		jobject         grefGCUserPeerable;
-		uint32_t        runtimeType;
 		bool            managedMarshalMethodsLookupEnabled;
 	};
 

src/native/mono/monodroid/monodroid-glue.cc
@@ -822,7 +822,6 @@ MonodroidRuntime::init_android_runtime (JNIEnv *env, jclass runtimeClass, jobjec
 	mono_add_internal_call ("Android.Runtime.RuntimeNativeMethods::monodroid_unhandled_exception", reinterpret_cast<const void*>(monodroid_unhandled_exception));
 
 	struct JnienvInitializeArgs init = {};
-	init.runtimeType            = RuntimeTypeMonoVM;
 	init.javaVm                 = osBridge.get_jvm ();
 	init.env                    = env;
 	init.logCategories          = log_categories;

src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Resources/Base/BuildReleaseArm64SimpleDotNet.apkdesc
@@ -11,13 +11,13 @@
       "Size": 18288
     },
     "lib/arm64-v8a/lib_Java.Interop.dll.so": {
-      "Size": 88472
+      "Size": 88000
     },
     "lib/arm64-v8a/lib_Mono.Android.dll.so": {
-      "Size": 124040
+      "Size": 118240
     },
     "lib/arm64-v8a/lib_Mono.Android.Runtime.dll.so": {
-      "Size": 23424
+      "Size": 23480
     },
     "lib/arm64-v8a/lib_System.Console.dll.so": {
       "Size": 24408
@@ -26,10 +26,10 @@
       "Size": 25488
     },
     "lib/arm64-v8a/lib_System.Private.CoreLib.dll.so": {
-      "Size": 692528
+      "Size": 635296
     },
     "lib/arm64-v8a/lib_System.Runtime.dll.so": {
-      "Size": 20104
+      "Size": 20136
     },
     "lib/arm64-v8a/lib_System.Runtime.InteropServices.dll.so": {
       "Size": 21528
@@ -38,13 +38,13 @@
       "Size": 20024
     },
     "lib/arm64-v8a/libarc.bin.so": {
-      "Size": 18984
+      "Size": 19112
     },
     "lib/arm64-v8a/libmono-component-marshal-ilgen.so": {
       "Size": 36616
     },
     "lib/arm64-v8a/libmonodroid.so": {
-      "Size": 1508312
+      "Size": 1508296
     },
     "lib/arm64-v8a/libmonosgen-2.0.so": {
       "Size": 3119744
@@ -62,10 +62,10 @@
       "Size": 165240
     },
     "lib/arm64-v8a/libxamarin-app.so": {
-      "Size": 19800
+      "Size": 19808
     },
     "META-INF/BNDLTOOL.RSA": {
-      "Size": 1223
+      "Size": 1221
     },
     "META-INF/BNDLTOOL.SF": {
       "Size": 3266
@@ -98,5 +98,5 @@
       "Size": 1904
     }
   },
-  "PackageSize": 3148309
+  "PackageSize": 3090965
 }