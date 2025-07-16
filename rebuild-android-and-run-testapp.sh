f453805c71d78950e6d92185b9ba2d2ea7d7ce1e
Simon Rozsival <simon@rozsival.com>
2025-07-16 09:09:52 +0200

    Remove unnecessary script file

--- rebuild-android-and-run-testapp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-make CONFIGURATION=Debug prepare all
-rm -rf ~/.nuget/packages/*
-./dotnet-local.sh build Xamarin.Android.sln -c Debug -t:InstallMaui -p:MauiVersion=10.0.0-preview.5.25306.5 -p:MauiVersionBand=10.0.100-preview.5
-./dotnet-local.sh build -t:Run -c Debug -f net10.0-android TestApp -p:UseMonoRuntime=false
