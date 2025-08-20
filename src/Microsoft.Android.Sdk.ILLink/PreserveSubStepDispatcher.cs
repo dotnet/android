725e0cb1cc8f5cbb11d2b60094b9ed6673a2ecb7
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-08-20 16:58:58 -0700

    Rework PreserveX steps

--- src/Microsoft.Android.Sdk.ILLink/PreserveSubStepDispatcher.cs
+++ src/Microsoft.Android.Sdk.ILLink/PreserveSubStepDispatcher.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Mono.Linker.Steps;
 using Mono.Tuner;
 
 namespace Microsoft.Android.Sdk.ILLink
 {
-	public class PreserveSubStepDispatcher : MarkSubStepsDispatcher
+	public class PreserveSubStepDispatcher : PreMarkSubStepsDispatcher
 	{
 		public PreserveSubStepDispatcher ()
 			: base (new ISubStep[] {
@@ -14,5 +13,15 @@ namespace Microsoft.Android.Sdk.ILLink
 			})
 		{
 		}
+	}
+
+	internal struct CategorizedSubSteps
+	{
+		public List<MyBaseSubStep> on_assemblies { get; set; }
+		public List<MyBaseSubStep> on_types { get; set; }
+		public List<MyBaseSubStep> on_fields { get; set; }
+		public List<MyBaseSubStep> on_methods { get; set; }
+		public List<MyBaseSubStep> on_properties { get; set; }
+		public List<MyBaseSubStep> on_events { get; set; }
 	}
 }
