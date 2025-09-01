dc5765a43ebb46f700b170f99d8567c619779f34
Marek Habersack <grendel@twistedcode.net>
2025-09-01 11:34:08 +0200

    NativeAOT runtime skeleton
    Builds, installs, does nothing yet.

--- src/native/nativeaot/host/host-jni.cc
+++ src/native/nativeaot/host/host-jni.cc
@@ -1,15 +1,10 @@
+#include <host/host.hh>
 #include <host/host-jni.hh>
-#include <host/host-nativeaot.hh>
-#include <runtime-base/logger.hh>
 
 using namespace xamarin::android;
 
-auto XA_Host_NativeAOT_JNI_OnLoad (JavaVM *vm, void *reserved) -> int
+JNIEXPORT jint JNICALL
+JNI_OnLoad (JavaVM *vm, void *reserved)
 {
 	return Host::Java_JNI_OnLoad (vm, reserved);
-}
-
-void XA_Host_NativeAOT_OnInit ()
-{
-	Host::OnInit ();
 }
--- src/native/nativeaot/include/host/host-jni.hh
+++ src/native/nativeaot/include/host/host-jni.hh
@@ -3,7 +3,6 @@
 #include <jni.h>
 
 extern "C" {
-	[[gnu::visibility("default")]]
-	auto XA_Host_NativeAOT_JNI_OnLoad (JavaVM *vm, void *reserved) -> int;
-	void XA_Host_NativeAOT_OnInit ();
+
+
 }
