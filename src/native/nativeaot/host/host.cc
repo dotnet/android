d99bc2d67adf570f7abba1c91f276bba11c72269
Marek Habersack <grendel@twistedcode.net>
2025-09-02 15:05:26 +0200

    Add logging to the naot android runtime
    Linking seems to work, not tested yet

--- src/native/nativeaot/host/host.cc
+++ src/native/nativeaot/host/host.cc
@@ -1,9 +1,12 @@
 #include <host/host.hh>
+#include <runtime-base/logger.hh>
 
 using namespace xamarin::android;
 
 auto Host::Java_JNI_OnLoad (JavaVM *vm, [[maybe_unused]] void *reserved) noexcept -> jint
 {
+	log_debug (LOG_ASSEMBLY, "{}", __PRETTY_FUNCTION__);
+
 	jvm = vm;
 
 	return JNI_VERSION_1_6;
