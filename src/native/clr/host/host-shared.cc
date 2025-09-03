22fd8f851c9f7ba9ebb5feb342dd649f5f7193d9
Marek Habersack <grendel@twistedcode.net>
2025-09-03 14:15:31 +0200

    Shared code (gc bridge, os bridge), p/invokes
    NativeAOT sample now links without errors about missing symbols (added
    linker options for it to fail if missing symbols are detected)

---
+++ src/native/clr/host/host-shared.cc
@@ -0,0 +1,37 @@
+#include <host/host.hh>
+#include <host/os-bridge.hh>
+
+using namespace xamarin::android;
+
+auto Host::get_java_class_name_for_TypeManager (jclass klass) noexcept -> char*
+{
+	if (klass == nullptr || Class_getName == nullptr) {
+		return nullptr;
+	}
+
+	JNIEnv *env = OSBridge::ensure_jnienv ();
+	jstring name = reinterpret_cast<jstring> (env->CallObjectMethod (klass, Class_getName));
+	if (name == nullptr) {
+		log_error (LOG_DEFAULT, "Failed to obtain Java class name for object at {:p}", reinterpret_cast<void*>(klass));
+		return nullptr;
+	}
+
+	const char *mutf8 = env->GetStringUTFChars (name, nullptr);
+	if (mutf8 == nullptr) {
+		log_error (LOG_DEFAULT, "Failed to convert Java class name to UTF8 (out of memory?)"sv);
+		env->DeleteLocalRef (name);
+		return nullptr;
+	}
+	char *ret = strdup (mutf8);
+
+	env->ReleaseStringUTFChars (name, mutf8);
+	env->DeleteLocalRef (name);
+
+	char *dot = strchr (ret, '.');
+	while (dot != nullptr) {
+		*dot = '/';
+		dot = strchr (dot + 1, '.');
+	}
+
+	return ret;
+}
