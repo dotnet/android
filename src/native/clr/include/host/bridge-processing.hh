45cef3eb0c5962896978db4c7120ec9a11878edd
Marek Habersack <grendel@twistedcode.net>
2025-09-04 13:19:37 +0200

    Hook up NAOT-specific GC bridge code

--- src/native/clr/include/host/bridge-processing.hh
+++ src/native/clr/include/host/bridge-processing.hh
@@ -14,11 +14,13 @@ public:
 private:
 	auto maybe_call_gc_user_peerable_add_managed_reference ([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jobject from, [[maybe_unused]] jobject to) noexcept -> bool override final
 	{
+		log_warn (LOG_ASSEMBLY, "{}", __PRETTY_FUNCTION__);
 		return false; // no-op for CoreCLR, we didn't process the call
 	}
 
 	auto maybe_call_gc_user_peerable_clear_managed_references ([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jobject handle) noexcept -> bool override final
 	{
+		log_warn (LOG_ASSEMBLY, "{}", __PRETTY_FUNCTION__);
 		return false; // no-op for CoreCLR, we didn't process the call
 	}
 };
