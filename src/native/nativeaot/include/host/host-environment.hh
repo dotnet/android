593a075181246cb4289bceecdc7d48c58e0ba6d1
Marek Habersack <grendel@twistedcode.net>
2025-09-12 16:06:57 +0200

    Partial support for system properties
    Need to have a think about it next week

--- src/native/nativeaot/include/host/host-environment.hh
+++ src/native/nativeaot/include/host/host-environment.hh
@@ -8,11 +8,17 @@
 #include <runtime-base/logger.hh>
 
 namespace xamarin::android {
+	struct AppEnvironmentVariable {
+		uint32_t name_index;
+		uint32_t value_index;
+	};
+
 	class HostEnvironment
 	{
 	public:
 		static void init () noexcept;
 
+		[[gnu::flatten, gnu::always_inline]]
 		static void set_variable (const char *name, const char *value) noexcept
 		{
 			log_debug (LOG_DEFAULT, " Variable {} = '{}'", name, value);
@@ -21,9 +27,28 @@ namespace xamarin::android {
 			}
 		}
 
+		[[gnu::flatten, gnu::always_inline]]
 		static void set_variable (std::string_view const& name, std::string_view const& value) noexcept
 		{
 			set_variable (name.data (), value.data ());
+		}
+
+		[[gnu::flatten, gnu::always_inline]]
+		static void set_system_property (const char *name, const char *value) noexcept
+		{
+		}
+
+	private:
+		template<void (*setter)(const char *name, const char *value) noexcept> [[gnu::flatten, gnu::always_inline]]
+		static void set_values (uint32_t const& count, AppEnvironmentVariable const (&entries)[], const char (&contents)[]) noexcept
+		{
+			for (size_t i = 0; i < count; i++) {
+				AppEnvironmentVariable const& env_var = entries[i];
+				const char *var_name = &contents[env_var.name_index];
+				const char *var_value = &contents[env_var.value_index];
+
+				setter (var_name, var_value);
+			}
 		}
 	};
 }
