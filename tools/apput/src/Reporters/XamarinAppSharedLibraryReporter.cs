c7034e83de0b768a8e61f76daccd2d95953811fa
Marek Habersack <grendel@twistedcode.net>
2025-09-26 11:10:53 +0200

    libxamarin-app.so reporter (beginnings)

---
+++ tools/apput/src/Reporters/XamarinAppSharedLibraryReporter.cs
@@ -0,0 +1,23 @@
+namespace ApplicationUtility;
+
+[AspectReporter (typeof (XamarinAppSharedLibrary))]
+class XamarinAppSharedLibraryReporter : SharedLibraryReporter
+{
+	readonly XamarinAppSharedLibrary library;
+
+	protected override string AspectName => XamarinAppSharedLibrary.AspectName;
+	protected override string LibraryKind => "Xamarin.Android App shared library";
+
+	public XamarinAppSharedLibraryReporter (XamarinAppSharedLibrary library)
+		: base (library)
+	{
+		this.library = library;
+	}
+
+	protected override void DoReport ()
+	{
+		base.DoReport ();
+		WriteSubsectionBanner ("Xamarin.Android app library info");
+		WriteItem ("Format tag", $"0x{library.FormatTag:x}");
+	}
+}
