b2a36f18d2668090440c628116b25cf11250dccf
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-10-08 14:47:19 -0700

    Wip

--- src/Microsoft.Android.Sdk.ILLink/GenerateTypeMapAttributesStep.cs
+++ src/Microsoft.Android.Sdk.ILLink/GenerateTypeMapAttributesStep.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -18,19 +19,16 @@ namespace Microsoft.Android.Sdk.ILLink;
 public class GenerateTypeMapAttributesStep : BaseStep
 {
 	const string TypeMapAttributeTypeName = "System.Runtime.InteropServices.TypeMapAttribute`1";
-	TypeReference TypeMapAttribute;
 	MethodReference TypeMapAttributeCtor;
 
 	const string TypeMapAssociationAttributeTypeName = "System.Runtime.InteropServices.TypeMapAssociationAttribute`1";
-	TypeReference TypeMapAssociationAttribute;
 	MethodReference TypeMapAssociationAttributeCtor;
 
 	const string TypeMapProxyAttributeTypeName = "Java.Interop.TypeMapProxyAttribute";
-	TypeReference TypeMapProxyAttribute;
+	//TypeReference TypeMapProxyAttribute;
 	MethodReference TypeMapProxyAttributeCtor;
 
 	const string TypeMapAssemblyTargetAttributeTypeName = "System.Runtime.InteropServices.TypeMapAssemblyTargetAttribute`1";
-	TypeReference TypeMapAssemblyTargetAttribute;
 	MethodReference TypeMapAssemblyTargetAttributeCtor;
 
 	const string JavaTypeMapUniverseTypeName = "Java.Lang.Object";
@@ -39,7 +37,7 @@ public class GenerateTypeMapAttributesStep : BaseStep
 	TypeReference SystemTypeType { get; set; }
 	TypeReference SystemStringType { get; set; }
 
-	AssemblyDefinition EntryPointAssembly { get; set; }
+	AssemblyDefinition AssemblyToInjectTypeMap { get; set; }
 	AssemblyDefinition MonoAndroidAssembly { get; set; }
 
 	void GetTypeMapAttributeReferences (
@@ -47,11 +45,10 @@ public class GenerateTypeMapAttributesStep : BaseStep
 		Func<MethodDefinition, bool> ctorSelector,
 		AssemblyDefinition addReferencesTo,
 		TypeReference typeMapUniverse,
-		out TypeReference attributeType,
 		out MethodReference ctor)
 	{
 		var typeMapAttributeDefinition = Context.GetType (attributeTypeName);
-		attributeType = addReferencesTo.MainModule.ImportReference (typeMapAttributeDefinition.MakeGenericInstanceType (typeMapUniverse));
+		var attributeType = addReferencesTo.MainModule.ImportReference (typeMapAttributeDefinition.MakeGenericInstanceType (typeMapUniverse));
 
 		var typeMapAttributeCtorDefinition = typeMapAttributeDefinition.Methods
 			.FirstOrDefault (ctorSelector) ?? throw new InvalidOperationException ($"Couldn't find {attributeTypeName}..ctor()");
@@ -74,9 +71,9 @@ public class GenerateTypeMapAttributesStep : BaseStep
 
 	protected override void Process ()
 	{
-		EntryPointAssembly = Context.Annotations.GetType ().GetMethod ("GetEntryPointAssembly")?.Invoke (Context.Annotations, null) as AssemblyDefinition ?? throw new NotImplementedException ("asdfasdf NoEntryPoint");
+		AssemblyToInjectTypeMap = Context.Annotations.GetType ().GetMethod ("GetEntryPointAssembly")?.Invoke (Context.Annotations, null) as AssemblyDefinition ?? throw new NotImplementedException ("asdfasdf NoEntryPoint");
 		var javaTypeMapUniverseTypeDefinition = Context.GetType (JavaTypeMapUniverseTypeName);
-		JavaTypeMapUniverseType = EntryPointAssembly.MainModule.ImportReference (javaTypeMapUniverseTypeDefinition);
+		JavaTypeMapUniverseType = AssemblyToInjectTypeMap.MainModule.ImportReference (javaTypeMapUniverseTypeDefinition);
 
 		GetTypeMapAttributeReferences (TypeMapAttributeTypeName,
 			m => m.IsConstructor
@@ -84,9 +81,8 @@ public class GenerateTypeMapAttributesStep : BaseStep
 				{ ParameterType.FullName: "System.String" },
 				{ ParameterType.FullName: "System.Type" },
 				{ ParameterType.FullName: "System.Type" }],
-			EntryPointAssembly,
+			AssemblyToInjectTypeMap,
 			JavaTypeMapUniverseType,
-			out TypeMapAttribute,
 			out TypeMapAttributeCtor);
 
 		GetTypeMapAttributeReferences (TypeMapAssociationAttributeTypeName,
@@ -94,9 +90,8 @@ public class GenerateTypeMapAttributesStep : BaseStep
 				&& m.Parameters is [
 				{ ParameterType.FullName: "System.Type" },
 				{ ParameterType.FullName: "System.Type" }],
-			EntryPointAssembly,
+			AssemblyToInjectTypeMap,
 			JavaTypeMapUniverseType,
-			out TypeMapAssociationAttribute,
 			out TypeMapAssociationAttributeCtor);
 
 		MonoAndroidAssembly = javaTypeMapUniverseTypeDefinition.Module.Assembly;
@@ -105,16 +100,15 @@ public class GenerateTypeMapAttributesStep : BaseStep
 				&& m.Parameters is [{ ParameterType.FullName: "System.String" }],
 			MonoAndroidAssembly,
 			JavaTypeMapUniverseType,
-			out TypeMapAssemblyTargetAttribute,
 			out TypeMapAssemblyTargetAttributeCtor);
 
-		var typeMapProxyAttrTypeDef = Context.GetType ("Java.Interop.TypeMapProxyAttribute");
+		var typeMapProxyAttrTypeDef = Context.GetType (TypeMapProxyAttributeTypeName);
+		var typeMapProxyAttribute = AssemblyToInjectTypeMap.MainModule.ImportReference (typeMapProxyAttrTypeDef);
 		var typeMapProxyAttrCtor = typeMapProxyAttrTypeDef.Methods.Single (m => m.IsConstructor);
-		TypeMapProxyAttribute = EntryPointAssembly.MainModule.ImportReference (typeMapProxyAttrTypeDef);
-		TypeMapProxyAttributeCtor = EntryPointAssembly.MainModule.ImportReference (typeMapProxyAttrCtor);
+		TypeMapProxyAttributeCtor = AssemblyToInjectTypeMap.MainModule.ImportReference (typeMapProxyAttrCtor);
 
-		SystemTypeType = EntryPointAssembly.MainModule.ImportReference (Context.GetType ("System.Type"));
-		SystemStringType = EntryPointAssembly.MainModule.ImportReference (Context.GetType ("System.String"));
+		SystemTypeType = AssemblyToInjectTypeMap.MainModule.ImportReference (Context.GetType ("System.Type"));
+		SystemStringType = AssemblyToInjectTypeMap.MainModule.ImportReference (Context.GetType ("System.String"));
 	}
 
 
@@ -125,19 +119,59 @@ public class GenerateTypeMapAttributesStep : BaseStep
 		}
 	}
 
-	List<CustomAttribute> injectedAttributes = new ();
-	List<TypeDefinition> injectedTypes = new ();
+	// Need to find all possible mappings and pick the best before emitting
+	Dictionary<string, TypeDefinition> externalMappings = new ();
+	// Need to inject the Proxy type, but cannot modify the assembly while iterating through types
+	Dictionary<TypeDefinition, TypeDefinition> proxyMappings = new ();
+	// list of proxy types to inject into AssemblyToInjectInto in EndProcess
+	List<TypeDefinition> typesToInject = new ();
 
+	/// <summary>
+	/// Selects the best type that would be mapped to in the AndroidValueManager/AndroidTypeManager
+	/// </summary>
+	private TypeDefinition PickBestTargetType (TypeDefinition existing, TypeDefinition type)
+	{
+		if (type == existing)
+			return existing;
+		// Types in Mono.Android assembly should be chosen first
+		if (existing.Module.Assembly.Name.Name != "Mono.Android" &&
+				type.Module.Assembly.Name.Name == "Mono.Android") {
+			return type;
+		}
+		// We found the `Invoker` type *before* the declared type
+		// Fix things up so the abstract type is first, and the `Invoker` is considered a duplicate.
+		if ((type.IsAbstract || type.IsInterface) &&
+				!existing.IsAbstract &&
+				!existing.IsInterface &&
+				type.IsAssignableFrom ((TypeReference) existing, Context)) {
+			return type;
+		}
+		// we found a generic subclass of a non-generic type
+		if (type.IsGenericInstance &&
+				!existing.IsGenericInstance &&
+				type.IsAssignableFrom ((TypeReference) existing, Context)) {
+			return type;
+		}
+		return existing;
+	}
+
+	/// <summary>
+	/// Iterates through all types to find types that map to/from java types, and stores
+	/// them for modifying the assemblies during EndProcess
+	/// </summary>
 	private void ProcessType (AssemblyDefinition assembly, TypeDefinition type)
 	{
 		if (type.HasJavaPeer (Context)) {
 			string javaName = JavaNativeTypeManager.ToJniName (type, Context);
+			if (externalMappings.TryGetValue (javaName, out var existing)) {
+				externalMappings [javaName] = PickBestTargetType (existing, type);
+			} else {
+				externalMappings.Add (javaName, type);
+			}
 			Context.LogMessage (MessageContainer.CreateInfoMessage ($"Type '{type.FullName}' has peer '{javaName}'"));
-			injectedAttributes.Add (GenerateTypeMapAttribute (type, javaName));
-
 			var proxyType = GenerateTypeMapProxyType (javaName, type);
-			injectedTypes.Add (proxyType);
-			injectedAttributes.Add (GenerateTypeMapAssociationAttribute (type, proxyType));
+			typesToInject.Add (proxyType);
+			proxyMappings.Add (type, proxyType);
 		} else {
 			Context.LogMessage (MessageContainer.CreateInfoMessage ($"Type '{type.FullName}' has no peer"));
 		}
@@ -152,8 +186,8 @@ public class GenerateTypeMapAttributesStep : BaseStep
 	CustomAttribute GenerateTypeMapAssociationAttribute (TypeDefinition type, TypeDefinition proxyType)
 	{
 		var ca = new CustomAttribute (TypeMapAssociationAttributeCtor);
-		ca.ConstructorArguments.Add (new (SystemTypeType, type));
-		ca.ConstructorArguments.Add (new (SystemTypeType, proxyType));
+		ca.ConstructorArguments.Add (new (SystemTypeType, AssemblyToInjectTypeMap.MainModule.ImportReference(type)));
+		ca.ConstructorArguments.Add (new (SystemTypeType, AssemblyToInjectTypeMap.MainModule.ImportReference(proxyType)));
 		return ca;
 	}
 
@@ -161,29 +195,43 @@ public class GenerateTypeMapAttributesStep : BaseStep
 	{
 		CustomAttribute ca = new (TypeMapAttributeCtor);
 		ca.ConstructorArguments.Add (new (SystemStringType, javaName));
-		ca.ConstructorArguments.Add (new (SystemTypeType, type));
-		ca.ConstructorArguments.Add (new (SystemTypeType, type));
+		ca.ConstructorArguments.Add (new (SystemTypeType, AssemblyToInjectTypeMap.MainModule.ImportReference(type)));
+		ca.ConstructorArguments.Add (new (SystemTypeType, AssemblyToInjectTypeMap.MainModule.ImportReference(type)));
 		return ca;
 	}
 
 	protected override void EndProcess ()
 	{
+		// HACK ALERT
+		// We override the entry_assembly so that the TypeMapHandler in illink can have a starting point for TypeMapTargetAssemblies.
+		// Mono.Android should be the entrypoint assembly so that we can call Assembly.SetEntryAssembly() during application initialization.
+		// TODO:
+		// - Add support for "EntryPointAssembly"s that don't have a .entrypoint or Main() method
+		// - Use MSBuild logic to set the EntryPointAssembly to Mono.Android
 		Context.Annotations.GetType ().GetField ("entry_assembly", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue (Context.Annotations, MonoAndroidAssembly);
-		foreach(var type in injectedTypes) {
-			EntryPointAssembly.MainModule.Types.Add (type);
+		foreach (var type in typesToInject) {
+			AssemblyToInjectTypeMap.MainModule.Types.Add (type);
+			Debug.Assert (type.Module.Assembly is not null);
 		}
-		foreach (var attr in injectedAttributes) {
-			Context.LogMessage (MessageContainer.CreateInfoMessage ($"Injecting [{attr.AttributeType.FullName}({string.Join (", ", attr.ConstructorArguments.Select (caa => caa.ToString ()))})] into {EntryPointAssembly.Name}"));
-			EntryPointAssembly.CustomAttributes.Add (attr);
+		foreach (var mapping in externalMappings) {
+			var attr = GenerateTypeMapAttribute (mapping.Value, mapping.Key);
+			Context.LogMessage (MessageContainer.CreateInfoMessage ($"Injecting [{attr.AttributeType.FullName}({string.Join (", ", attr.ConstructorArguments.Select (caa => caa.ToString ()))})] into {AssemblyToInjectTypeMap.Name}"));
+			AssemblyToInjectTypeMap.CustomAttributes.Add (attr);
 		}
-		EntryPointAssembly.Write (Context.GetAssemblyLocation (EntryPointAssembly) + Random.Shared.GetHexString(4) + ".injected.dll");
+		foreach (var mapping in proxyMappings) {
+			var attr = GenerateTypeMapAssociationAttribute (mapping.Key, mapping.Value);
+			Context.LogMessage (MessageContainer.CreateInfoMessage ($"Injecting [{attr.AttributeType.FullName}({string.Join (", ", attr.ConstructorArguments.Select (caa => caa.ToString ()))})] into {AssemblyToInjectTypeMap.Name}"));
+			AssemblyToInjectTypeMap.CustomAttributes.Add (attr);
+		}
+
+		AssemblyToInjectTypeMap.Write (Context.GetAssemblyLocation (AssemblyToInjectTypeMap) + Random.Shared.GetHexString (4) + ".injected.dll");
 
 		// JNIEnvInit sets Mono.Android as the entrypoint assembly. Forward the typemap logic to the user/custom assembly;
 		CustomAttribute targetAssembly = new (TypeMapAssemblyTargetAttributeCtor);
-		targetAssembly.ConstructorArguments.Add (new (SystemStringType, EntryPointAssembly.Name.FullName));
+		targetAssembly.ConstructorArguments.Add (new (SystemStringType, AssemblyToInjectTypeMap.Name.FullName));
 		MonoAndroidAssembly.CustomAttributes.Add (targetAssembly);
-		MonoAndroidAssembly.Write (Path.Combine(
-			Path.GetDirectoryName(Context.GetAssemblyLocation (EntryPointAssembly)),
+		MonoAndroidAssembly.Write (Path.Combine (
+			Path.GetDirectoryName (Context.GetAssemblyLocation (AssemblyToInjectTypeMap)),
 			"Mono.Android." + Random.Shared.GetHexString (4) + ".injected.dll"));
 	}
 
@@ -200,14 +248,13 @@ public class GenerateTypeMapAttributesStep : BaseStep
 		}
 		var proxyType = new TypeDefinition (
 			mappedType.Module.Assembly.Name.Name + "._." + declaringType.Namespace,
-			mappedName.ToString() +  "_<androidproxy>",
+			mappedName.ToString () + "_<androidproxy>",
 			TypeAttributes.Class | TypeAttributes.NotPublic | TypeAttributes.Sealed,
-			EntryPointAssembly.MainModule.TypeSystem.Object);
+			AssemblyToInjectTypeMap.MainModule.TypeSystem.Object);
 
 		var ca = new CustomAttribute (TypeMapProxyAttributeCtor);
 		ca.ConstructorArguments.Add (new CustomAttributeArgument (SystemStringType, javaClassName));
 		proxyType.CustomAttributes.Add (ca);
-
 		return proxyType;
 	}
 }
