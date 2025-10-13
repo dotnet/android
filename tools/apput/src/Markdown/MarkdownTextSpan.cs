9f84ef78cb4ae826102a5cd4246b05a68a57cf67
Marek Habersack <grendel@twistedcode.net>
2025-10-13 17:08:20 +0200

    Rendering is a mess, need a different way to do it. TBC tomorrow

--- tools/apput/src/Markdown/MarkdownTextSpan.cs
+++ tools/apput/src/Markdown/MarkdownTextSpan.cs
@@ -1,19 +1,39 @@
+using System;
 using System.Collections.Generic;
 
 namespace ApplicationUtility;
 
 class MarkdownTextSpan : MarkdownElement
 {
-	List<MarkdownTextSpan> fragments = new ();
+	readonly List<MarkdownTextSpan> fragments = new ();
+	MarkdownTextStyle textStyle;
 
 	public List<MarkdownTextSpan> Fragments => fragments;
-	public bool IsEmpty => fragments.Count == 0;
+	public bool IsEmpty => fragments.Count == 0 && String.IsNullOrEmpty (Text);
+	public MarkdownTextStyle Style => textStyle;
 
-	public bool Bold { get; set; }
+	public bool Bold {
+		get => HasStyle (MarkdownTextStyle.Bold);
+		set => SetStyle (MarkdownTextStyle.Bold, value);
+	}
 
-	public MarkdownTextSpan (string text)
+	public bool Italic {
+		get => HasStyle (MarkdownTextStyle.Italic);
+		set => SetStyle (MarkdownTextStyle.Italic, value);
+	}
+
+	public bool Monospace {
+		get => HasStyle (MarkdownTextStyle.Monospace);
+		set => SetStyle (MarkdownTextStyle.Monospace, value);
+	}
+
+	public bool RemoveTailWhitespace { get; set; }
+
+	public MarkdownTextSpan (string text, MarkdownTextStyle style = MarkdownTextStyle.Plain)
 	{
+		Console.WriteLine ($"New span '{text}'; style: {style}");
 		Text = text;
+		textStyle = style;
 	}
 
 	public void AddNewline ()
@@ -24,5 +44,16 @@ class MarkdownTextSpan : MarkdownElement
 	public void AddText (string text)
 	{
 		fragments.Add (new MarkdownTextSpan (text));
+	}
+
+	bool HasStyle (MarkdownTextStyle style) => textStyle.HasFlag (style);
+
+	void SetStyle (MarkdownTextStyle style, bool enable)
+	{
+		if (enable) {
+			textStyle |= style;
+		} else {
+			textStyle &= ~style;
+		}
 	}
 }
