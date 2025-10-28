d26e3af31ac086356ed929314887149886a7d9e5
Marek Habersack <grendel@twistedcode.net>
2025-10-28 11:05:57 +0100

    Missed that

--- tests/MSBuildDeviceIntegration/Tests/BundleToolNoAbiSplitTests.cs
+++ tests/MSBuildDeviceIntegration/Tests/BundleToolNoAbiSplitTests.cs
@@ -69,6 +69,7 @@ namespace Xamarin.Android.Build.Tests
 				AotAssemblies = false, // Release defaults to Profiled AOT for .NET 6
 				PackageName = "com.xamarin.bundletoolnoabisplittests",
 			};
+			app.SetRuntime (runtime);
 			app.OtherBuildItems.Add (
 				MakeAndroidAsset (
 					include: "Assets\\Textures\\asset1.txt",
