4856175e418cb1e68642b5b0d8034e1b49b65934
Marek Habersack <grendel@twistedcode.net>
2025-10-31 12:10:03 +0100

    [nativeaot] Log stack traces when gref+ is enabled

--- src/native/nativeaot/include/host/host-jni.hh
+++ src/native/nativeaot/include/host/host-jni.hh
@@ -6,4 +6,5 @@ extern "C" {
 	[[gnu::visibility("default")]]
 	auto XA_Host_NativeAOT_JNI_OnLoad (JavaVM *vm, void *reserved) -> int;
 	void XA_Host_NativeAOT_OnInit (jstring language, jstring filesDir, jstring cacheDir);
+	uint32_t XA_Host_NativeAOT_GetLoggingCategories ();
 }
