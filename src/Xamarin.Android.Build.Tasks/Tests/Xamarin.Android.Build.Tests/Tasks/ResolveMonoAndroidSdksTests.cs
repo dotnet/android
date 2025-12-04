f9fdb8f3a5514cbbdbaaf16b38eea87f0747b4e9
Marek Habersack <grendel@twistedcode.net>
2025-12-04 12:04:11 +0100

    ResolveMonoAndroidSdksTests.NormalInputs works

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/ResolveMonoAndroidSdksTests.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/Tasks/ResolveMonoAndroidSdksTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
+using Xamarin.Android.Tasks;
 using Xamarin.ProjectTools;
 
 namespace Xamarin.Android.Build.Tests
@@ -40,9 +41,17 @@ namespace Xamarin.Android.Build.Tests
 		}
 
 		[Test]
-		public void NormalInputs ()
+		public void NormalInputs ([Values] AndroidRuntime runtime)
 		{
-			var proj = new XamarinAndroidApplicationProject ();
+			bool isRelease = runtime == AndroidRuntime.NativeAOT;
+			if (IgnoreUnsupportedConfiguration (runtime, release: isRelease)) {
+				return;
+			}
+
+			var proj = new XamarinAndroidApplicationProject {
+				IsRelease = isRelease,
+			};
+			proj.SetRuntime (runtime);
 			proj.SetProperty ("MonoAndroidToolsDirectory", "xat");
 			proj.SetProperty ("_JavaSdkDirectory", "jdk");
 			proj.SetProperty ("_AndroidSdkDirectory", "sdk");
