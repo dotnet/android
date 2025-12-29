79509f03a87be1c7fbc28a70171829bf77dec5af
Simon Rozsival <simon@rozsival.com>
2025-12-29 10:35:37 +0100

    Use ExceptionHandling.RaiseAppDomainUnhandledExceptionEvent instead of Environment.FailFast

--- src/Mono.Android/Android.Runtime/JNIEnv.cs
+++ src/Mono.Android/Android.Runtime/JNIEnv.cs
@@ -104,9 +104,7 @@ namespace Android.Runtime {
 				if (RuntimeFeature.IsMonoRuntime) {
 					MonoDroidUnhandledException (innerException ?? javaException);
 				} else if (RuntimeFeature.IsCoreClrRuntime) {
-					// CoreCLR doesn't have mono_unhandled_exception, so we use Environment.FailFast
-					// to terminate the process with proper exception information.
-					Environment.FailFast ("Unhandled Java exception", innerException ?? javaException);
+					ExceptionHandling.RaiseAppDomainUnhandledExceptionEvent (innerException ?? javaException);
 				} else {
 					throw new NotSupportedException ("Internal error: unknown runtime not supported");
 				}
