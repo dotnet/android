914127f00753475861a22659d55a08a767676fd0
Jonathan Peppers <jonathan.peppers@microsoft.com>
2026-01-08 10:31:59 -0600

    [illink] set `$(StartupHookSupport)=false` *only* for Release mode
    Context: https://github.com/dotnet/runtime/blob/242f7b23752599f22157268de41fee91cb97ef6c/docs/design/features/host-startup-hook.md
    
    .NET has a concept of a "startup hook", which appears to work fine
    on Mono if you set:
    
        env.txt
            DOTNET_STARTUP_HOOKS=StartupHook
        (MSBuild)
            <StartupHookSupport>true</StartupHookSupport>
            <RuntimeHostConfigurationOption Include="STARTUP_HOOKS" Value="StartupHook" />
    
    The startup hook is a managed assembly with a static method, in the
    "global" namespace:
    
        class StartupHook
        {
            public static void Initialize()
            {
                // ...
            }
        }
    
    When the runtime starts, it will call `StartupHook.Initialize()` before
    any other managed code is executed.
    
    `dotnet watch` (Hot Reload) relies on this feature.
    
    I added a test to `Mono.Android-Tests` to verify that the startup hook
    was called.
    
    This is not yet working on CoreCLR on Android, investigation ongoing.

---
+++ tests/Mono.Android-Tests/Mono.Android-Tests/System/StartupHookTest.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace SystemTests
+{
+	[TestFixture]
+	[Category ("FOO")]
+	public class StartupHookTest
+	{
+		[Test]
+		public void IsInitialized ()
+		{
+			var type = Type.GetType ("StartupHook, StartupHook", throwOnError: true);
+			Assert.IsNotNull (type, "StartupHook type should be loaded");
+
+			var property = type.GetProperty ("IsInitialized", BindingFlags.Public | BindingFlags.Static);
+			Assert.IsNotNull (property, "IsInitialized property should exist");
+
+			var value = (bool) property.GetValue (null);
+			Assert.IsTrue (value, "StartupHook.Initialize() should have been called");
+		}
+	}
+}
