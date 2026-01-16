394608baf4c69a412ada3b3b6c2cf7c4ceba3908
Marek Habersack <grendel@twistedcode.net>
2026-01-16 17:35:24 +0100

    Beginnings of an LLVM sources update utility

---
+++ tools/update-llvm-source/Program.cs
@@ -0,0 +1,11 @@
+namespace Xamarin.Android.Tools;
+
+class App
+{
+	// URLs finally look like: https://android.googlesource.com/toolchain/llvm-project/+/5e96669f06077099aa41290cdb4c5e6fa0f59349/libcxx/src/hash.cpp?format=TEXT
+	// Returned text is base64-encoded
+	static int Main ()
+	{
+		return 0;
+	}
+}
