0ef6c1bbc29b548f6783f7db852a138d23eab0b6
Simon Rozsival <simon@rozsival.com>
2026-01-28 19:15:52 +0100

    Add AsyncTask callback test for Object[] parameters
    Tests Java-to-C# callbacks with Object[] array parameters:
    - MyAsyncTask subclass overrides DoInBackground with Object[] params
    - Verifies OnPostExecute void callback works correctly
    - Logs parameter types and values for verification

--- samples/HelloWorld/HelloWorld/MainActivity.cs
+++ samples/HelloWorld/HelloWorld/MainActivity.cs
@@ -1,14 +1,63 @@
 using Android.App;
 using Android.OS;
+using Android.Util;
 
 namespace HelloWorld;
 
 [Activity (Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : Activity
 {
-protected override void OnCreate (Bundle? savedInstanceState)
+	const string TAG = "HelloWorld";
+
+	protected override void OnCreate (Bundle? savedInstanceState)
+	{
+		base.OnCreate (savedInstanceState);
+		SetContentView (Resource.Layout.Main);
+
+		// Test object array callback - Java calls our DoInBackground with Object[] params
+		TestObjectArrayCallback ();
+	}
+
+	void TestObjectArrayCallback ()
+	{
+		Log.Info (TAG, "=== Testing Object[] callback from Java to C# ===");
+
+		// Create custom AsyncTask that receives Object[] from Java
+		var task = new MyAsyncTask ();
+
+		// Execute with string parameters - Java will call DoInBackground with these as Object[]
+		task.Execute ("Hello", "World", "From", "Java");
+
+		Log.Info (TAG, "AsyncTask executed, check logs for callback results");
+	}
+}
+
+// Custom AsyncTask that overrides DoInBackground - Java calls this with Object[] params
+#pragma warning disable CS0618 // AsyncTask is obsolete
+[Android.Runtime.Register ("crc649c7c8361742aa92e/MyAsyncTask")]
+public class MyAsyncTask : AsyncTask
 {
-base.OnCreate (savedInstanceState);
-SetContentView (Resource.Layout.Main);
+	const string TAG = "MyAsyncTask";
+
+	protected override Java.Lang.Object? DoInBackground (params Java.Lang.Object[]? @params)
+	{
+		Log.Info (TAG, $"DoInBackground called with {@params?.Length ?? 0} parameters:");
+
+		if (@params != null) {
+			for (int i = 0; i < @params.Length; i++) {
+				var p = @params[i];
+				Log.Info (TAG, $"  param[{i}]: {p} (type: {p?.GetType ().Name ?? "null"})");
+			}
+		}
+
+		// Return a result
+		return new Java.Lang.String ($"Processed {@params?.Length ?? 0} items");
+	}
+
+	protected override void OnPostExecute (Java.Lang.Object? result)
+	{
+		Log.Info (TAG, $"OnPostExecute: result = {result}");
+		Log.Info (TAG, "=== Object[] callback test completed ===");
+	}
 }
-}
+#pragma warning restore CS0618
