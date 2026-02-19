e1bfb2f8db2e1b1fdb15fd721cde3e1a311fabcd
Marek Habersack <grendel@twistedcode.net>
2026-02-19 09:57:05 +0100

    Fix after rebase

--- tests/MSBuildDeviceIntegration/Tests/InstallAndRunTests.cs
+++ tests/MSBuildDeviceIntegration/Tests/InstallAndRunTests.cs
@@ -86,6 +86,8 @@ namespace Xamarin.Android.Build.Tests
 		[Test]
 		public void DotNetRunWaitForExit ()
 		{
+			AssertCommercialBuild (); //FIXME: https://github.com/dotnet/android/issues/10832
+
 			const string logcatMessage = "DOTNET_RUN_TEST_MESSAGE_12345";
 			var proj = new XamarinAndroidApplicationProject ();
 
@@ -153,6 +155,8 @@ namespace Xamarin.Android.Build.Tests
 		[Test]
 		public void DotNetRunWithDeviceParameter ()
 		{
+			AssertCommercialBuild (); //FIXME: https://github.com/dotnet/android/issues/10832
+
 			const string logcatMessage = "DOTNET_RUN_DEVICE_TEST_67890";
 			var proj = new XamarinAndroidApplicationProject ();
 
