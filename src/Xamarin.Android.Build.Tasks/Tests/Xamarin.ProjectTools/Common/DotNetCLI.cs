f30c07d5bc3bb0b2b4b31cff6e3a052a30155f2b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2026-02-25 14:13:45 -0600

    [xabt] support `@(RuntimeEnvironmentVariable)` items (#10770)
    Context: https://github.com/dotnet/sdk/commit/bd5d3af9804ba358bf3f2e1be209418bcc949d16
    
    `dotnet run` now passes in `dotnet run -e FOO=BAR` as
    `@(RuntimeEnvironmentVariable)` MSBuild items.
    
    To opt in to this new feature, we need to add:
    
        <ProjectCapability Include="RuntimeEnvironmentVariableSupport" />
    
    As well as update the `_GenerateEnvironmentFiles` MSBuild target:
    
        <!-- RuntimeEnvironmentVariable items come from 'dotnet run -e NAME=VALUE' -->
        <_GeneratedAndroidEnvironment Include="@(RuntimeEnvironmentVariable->'%(Identity)=%(Value)')" />
    
    I added a new test to verify we have the env vars on-device at runtime.
    
    Note that I tested this in combination with a local .NET SDK build:
    
    * https://github.com/dotnet/android/pull/10769
    
    We won't be able to merge this until we have a .NET SDK here that
    includes the above commit. Merging with nightly .NET 10.0.3xx SDK.

--- src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/DotNetCLI.cs
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.ProjectTools/Common/DotNetCLI.cs
@@ -131,20 +131,20 @@ namespace Xamarin.ProjectTools
 			return Execute (arguments.ToArray ());
 		}
 
-		public bool Run (bool waitForExit = false, string [] parameters = null)
+		public bool Run (bool waitForExit = false, bool noBuild = true, string [] parameters = null)
 		{
 			string binlog = Path.Combine (Path.GetDirectoryName (projectOrSolution), "run.binlog");
 			var arguments = new List<string> {
 				"run",
 				"--project", $"\"{projectOrSolution}\"",
-				"--no-build",
-				$"/bl:\"{binlog}\"",
-				$"/p:WaitForExit={waitForExit.ToString (CultureInfo.InvariantCulture)}"
 			};
+			if (noBuild) {
+				arguments.Add ("--no-build");
+			}
+			arguments.Add ($"/bl:\"{binlog}\"");
+			arguments.Add ($"/p:WaitForExit={waitForExit.ToString (CultureInfo.InvariantCulture)}");
 			if (parameters != null) {
-				foreach (var parameter in parameters) {
-					arguments.Add ($"/p:{parameter}");
-				}
+				arguments.AddRange (parameters);
 			}
 			return Execute (arguments.ToArray ());
 		}
@@ -153,7 +153,7 @@ namespace Xamarin.ProjectTools
 		/// Starts `dotnet run` and returns a running Process that can be monitored and killed.
 		/// </summary>
 		/// <param name="waitForExit">Whether to use Microsoft.Android.Run tool which waits for app exit and streams logcat.</param>
-		/// <param name="parameters">Optional MSBuild properties to pass (e.g., "Device=emulator-5554").</param>
+		/// <param name="parameters">Additional arguments to pass to `dotnet run`.</param>
 		/// <returns>A running Process instance. Caller is responsible for disposing.</returns>
 		public Process StartRun (bool waitForExit = true, string [] parameters = null)
 		{
@@ -166,9 +166,7 @@ namespace Xamarin.ProjectTools
 				$"/p:WaitForExit={waitForExit.ToString (CultureInfo.InvariantCulture)}"
 			};
 			if (parameters != null) {
-				foreach (var parameter in parameters) {
-					arguments.Add ($"/p:{parameter}");
-				}
+				arguments.AddRange (parameters);
 			}
 
 			return ExecuteProcess (arguments.ToArray ());
