222cdcca28879b0bb2d641bf78b5bef01b4f6d9a
Marek Habersack <grendel@twistedcode.net>
2026-03-04 17:40:28 +0100

    [WIP] More of the assembly file extractor

--- tools/apput/src/Extractors/AssemblyExtractorOptions.cs
+++ tools/apput/src/Extractors/AssemblyExtractorOptions.cs
@@ -8,9 +8,11 @@ public class AssemblyExtractorOptions
 	public bool UseRegex { get; set; }
 	public bool ExtractPDB { get; set; }
 	public string TargetDir { get; }
+	public List<string> AssemblyPatterns { get; }
 
-	public AssemblyExtractorOptions (string targetDir)
+	public AssemblyExtractorOptions (string targetDir, List<string> assemblyPatterns)
 	{
 		TargetDir = targetDir;
+		AssemblyPatterns = assemblyPatterns;
 	}
 }
