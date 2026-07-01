f18ab1891873ce7a2f592577dd87ff88d534bcb2
Simon Rozsival <simon@rozsival.com>
2026-07-02 00:35:18 +0200

    enable export tests for the trimmable typemap

--- tests/Mono.Android-Tests/Mono.Android-Tests/Xamarin.Android.RuntimeTests/TestInstrumentation.cs
+++ tests/Mono.Android-Tests/Mono.Android-Tests/Xamarin.Android.RuntimeTests/TestInstrumentation.cs
@@ -29,7 +29,6 @@ namespace Xamarin.Android.RuntimeTests
 
 				if (Microsoft.Android.Runtime.RuntimeFeature.TrimmableTypeMap) {
 					categories.Add ("NativeTypeMap");
-					// categories.Add ("Export");
 					categories.Add ("TrimmableTypeMapUnsupported");
 				}
 
@@ -40,7 +39,10 @@ namespace Xamarin.Android.RuntimeTests
 					categories.Add ("NativeAOTIgnore");
 					categories.Add ("SSL");
 					categories.Add ("NTLM");
-					// categories.Add ("Export");
+
+					if (!Microsoft.Android.Runtime.RuntimeFeature.TrimmableTypeMap) {
+						categories.Add ("Export");
+					}
 				}
 
 				if (HasAppContextSwitch ("EnableLLVM")) {
